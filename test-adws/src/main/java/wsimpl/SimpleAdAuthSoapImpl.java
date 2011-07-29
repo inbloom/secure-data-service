296b76259e376f9fb8fadd03251be9c7e8997cdf
Dave Coleman <dcoleman@wgen.net>
2011-07-28 21:04:40 -0400

    jax-ws web service

--- test-adws/src/main/java/wsimpl/SimpleAdAuthSoapImpl.java
+++ test-adws/src/main/java/wsimpl/SimpleAdAuthSoapImpl.java
@@ -1,4 +1,4 @@
-package _80._1._15._10._8443.sftest;
+package net.wgenhq.dcoleman_wks3._8443.sftest;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -6,6 +6,8 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.xml.bind.annotation.XmlSeeAlso;
+
+import java.util.logging.Logger;
 
 import com.sforce.soap.authentication.AuthenticateResult;
 
@@ -15,23 +17,26 @@ import com.sforce.soap.authentication.AuthenticateResult;
  * Generated source version: 2.4.1
  */
  
-//@WebService(targetNamespace = "http://10.15.1.80:8443/sftest/", name = "SimpleAdAuthSoap")
-//@XmlSeeAlso({com.sforce.soap.authentication.ObjectFactory.class})
-//@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE) 
+@WebService(targetNamespace = "http://dcoleman-wks3.wgenhq.net:8443/sftest/", name = "SimpleAdAuthSoap")
+@XmlSeeAlso({com.sforce.soap.authentication.ObjectFactory.class})
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE) 
 public class SimpleAdAuthSoapImpl implements SimpleAdAuthSoap {
 
-//    @WebResult(name = "AuthenticateResult", targetNamespace = "urn:authentication.soap.sforce.com", partName = "parameters")
-//    @WebMethod(operationName = "Authenticate")
+    @WebResult(name = "AuthenticateResult", targetNamespace = "urn:authentication.soap.sforce.com", partName = "parameters")
+    @WebMethod(operationName = "Authenticate")
     @Override
     public AuthenticateResult authenticate(
         @WebParam(partName = "parameters", name = "Authenticate", targetNamespace = "urn:authentication.soap.sforce.com")
         com.sforce.soap.authentication.Authenticate parameters
     ) {
+        Logger log = Logger.getLogger(SimpleAdAuthSoapImpl.class.getName());
+        log.info("starting SimpleAdAuthSoapImpl");
         AuthenticateResult result = new AuthenticateResult();
         result.setAuthenticated(false);
         if(parameters.getUsername().equals("dave")) {
             result.setAuthenticated(true);
         }
+        log.info("done SimpleAdAuthSoapImpl");
         return result;
     }
 }
