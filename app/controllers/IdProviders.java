c8f66e817e4520d81ed576bfdb79833ac1416d50
Matthew Lane <lanemt@gmail.com>
2011-11-22 11:50:31 -0500

    Added CRUD for IdProvider

---
+++ app/controllers/IdProviders.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import controllers.CRUD;
+
+public class IdProviders extends CRUD {
+
+}
--- app/models/IdProvider.java
+++ app/models/IdProvider.java
@@ -2,17 +2,27 @@ package models;
 
 import javax.persistence.*;
 
+import play.data.validation.Required;
 import play.db.jpa.*;
 
 @Entity
 public class IdProvider extends Model {
 
+    @Required
+    public String name;
+    @Required
     public String domain;
+    @Required
     public String redirect;
 
-    public IdProvider(String domain, String redirect) {
+    public IdProvider(String name, String domain, String redirect) {
+        this.name = name;
         this.domain = domain;
         this.redirect = redirect;
+    }
+    
+    public String toString() {
+        return name + " - " + domain;
     }
 
 }
