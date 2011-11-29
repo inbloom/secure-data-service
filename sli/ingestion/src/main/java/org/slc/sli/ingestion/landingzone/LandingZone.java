1f2d6ce1cb922d4f325af9ab14981ba51fa400a1
Jim Abramson <jabramson@wgen.net>
2011-11-29 11:39:45 -0500

    minor documentation change to LandingZone interface

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/LandingZone.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/LandingZone.java
@@ -3,18 +3,21 @@ package org.slc.sli.ingestion.landingzone;
 import java.io.File;
 import java.io.IOException;
 
-interface LandingZone {
+public interface LandingZone {
 
-    /**
-     * @return File object for the given fileName
-     */
-    public File getFile(String fileName);
-    
-    /**
-     * @return md5Hex string for the given File object
-     * @throws IOException 
-     */
-    public String getMd5Hex(File file) throws IOException;
+	/**
+	 * Checks for a file in the landing zone with this name, and if found,
+	 * returns a java.io.File for it.  Returns null if no file with the name 
+	 * can be found.
+	 * 
+	 * @return File object
+	 */
+	public File getFile(String fileName);
+
+	/**
+	 * @return md5Hex string for the given File object
+	 * @throws IOException 
+	 */
+	public String getMd5Hex(File file) throws IOException;
 
 }
-
