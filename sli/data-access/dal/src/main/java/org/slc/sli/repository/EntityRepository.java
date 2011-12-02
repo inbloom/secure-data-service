12be714d34aa8bb73016ad67c59a9ac4be39257d
Dong Liu <dliu@wgen.net>
2011-12-02 13:24:38 -0500

    add the generic entity repository interface

---
+++ sli/data-access/dal/src/main/java/org/slc/sli/repository/EntityRepository.java
@@ -0,0 +1,63 @@
+package org.slc.sli.repository;
+
+import java.util.Map;
+
+import org.slc.sli.domain.Entity;
+
+/**
+ * Define the entity repository interface that provides basic CRUD and field
+ * query methods for entities including core entities and association entities
+ * 
+ * @author Dong Liu dliu@wgen.net
+ * 
+ */
+public interface EntityRepository {
+
+	/**
+	 * @param entityType
+	 *            the entity type need to be retrieved, can be entity type for
+	 *            core entity or association entity
+	 * @param id
+	 *            the global unique id for this entity
+	 * @return the entity retrieved
+	 */
+	Entity find(String entityType, String id);
+
+	/**
+	 * @param entityType
+	 *            the entity type need to be retrieved, can be entity type for
+	 *            core entity or association entity
+	 * @param skip the beginning index of the entity that will be returned
+	 * @param max the max number of entities that will be returned
+	 * @return the collection of entities
+	 */
+	Iterable<Entity> finalAll(String entityType, int skip, int max);
+
+	/**
+	 * @param entity the entity that will be updated
+	 */
+	void update(Entity entity);
+
+	/**
+	 * @param entity the entity that will be persisted
+	 * @return
+	 */
+	String create(Entity entity);
+
+	/**
+	 * @param entity the entity that will be deleted
+	 */
+	void delete(Entity entity);
+
+	/**
+	 * @param entityType the entity type need to be retrieved, can be entity type for
+	 *            core entity or association entity
+	 * @param fields a map with key value pairs that define the search criteria 
+	 * @param skip the beginning index of the entity that will be returned
+	 * @param max the max number of entities that will be returned
+	 * @return the collection of entities
+	 */
+	Iterable<Entity> findByFields(String entityType,
+			Map<String, String> fields, int skip, int max);
+
+}
