12a1bcc689f63f950e3765a8236fdff88a0896fd
Nicholas Brown <nbrown@wgen.net>
2011-12-08 10:38:34 -0500

    Checkstyle fixes...

--- sli/api/src/main/java/org/slc/sli/api/security/SecurityTokenResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/SecurityTokenResolver.java
@@ -1,8 +1,7 @@
 package org.slc.sli.api.security;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 
 public interface SecurityTokenResolver {
-    public Authentication resolve(String token) throws AuthenticationException;
+    public Authentication resolve(String token);
 }
--- sli/api/src/test/java/org/slc/sli/api/security/OpenamRestTokenResolverTest.java
+++ sli/api/src/test/java/org/slc/sli/api/security/OpenamRestTokenResolverTest.java
@@ -9,31 +9,50 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slc.sli.api.security.openam.OpenamRestTokenResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.web.client.RestTemplate;
 
-import org.slc.sli.api.security.openam.OpenamRestTokenResolver;
-
 public class OpenamRestTokenResolverTest {
     
-    private static final String     MOCK_URL      = "mock";
-    private static final String     VALID_TOKEN   = "valid_token";
-    private static final String     INVALID_TOKEN = "invalid_token";
+    private static final String MOCK_URL = "mock";
+    private static final String VALID_TOKEN = "valid_token";
+    private static final String INVALID_TOKEN = "invalid_token";
     
-    private static final String     PAYLOAD       = "userdetails.token.id=AQIC5wM2LY4Sfczq7rqe5T89hyJzxvtfmlagB2VFZEFxE2I.*AAJTSQACMDE.*\r\n" + "userdetails.role=id=student,ou=group,dc=opensso,dc=java,dc=net\r\n"
-                                                          + "userdetails.role=id=parent,ou=group,dc=opensso,dc=java,dc=net\r\n" + "userdetails.role=id=teacher,ou=group,dc=opensso,dc=java,dc=net\r\n" + "userdetails.attribute.name=uid\r\n"
-                                                          + "userdetails.attribute.value=demo\r\n" + "userdetails.attribute.name=userpassword\r\n" + "userdetails.attribute.value={SSHA}lDuocwqvaaTzkN5Bi+beD2BJBP4sI/dtzD/BfQ==\r\n"
-                                                          + "userdetails.attribute.name=sn\r\n" + "userdetails.attribute.value=demo\r\n" + "userdetails.attribute.name=cn\r\n" + "userdetails.attribute.value=demo demo\r\n"
-                                                          + "userdetails.attribute.name=givenname\r\n" + "userdetails.attribute.value=demo\r\n" + "userdetails.attribute.name=inetuserstatus\r\n" + "userdetails.attribute.value=Active\r\n"
-                                                          + "userdetails.attribute.name=dn\r\n" + "userdetails.attribute.value=uid=demo,ou=people,dc=slidev,dc=net\r\n" + "userdetails.attribute.name=objectclass\r\n"
-                                                          + "userdetails.attribute.value=person\r\n" + "userdetails.attribute.value=sunIdentityServerLibertyPPService\r\n" + "userdetails.attribute.value=inetorgperson\r\n"
-                                                          + "userdetails.attribute.value=sunFederationManagerDataStore\r\n" + "userdetails.attribute.value=iPlanetPreferences\r\n"
-                                                          + "userdetails.attribute.value=iplanet-am-auth-configuration-service\r\n" + "userdetails.attribute.value=organizationalperson\r\n"
-                                                          + "userdetails.attribute.value=sunFMSAML2NameIdentifier\r\n" + "userdetails.attribute.value=inetuser\r\n" + "userdetails.attribute.value=iplanet-am-managed-person\r\n"
-                                                          + "userdetails.attribute.value=iplanet-am-user-service\r\n" + "userdetails.attribute.value=sunAMAuthAccountLockout\r\n" + "userdetails.attribute.value=top";
+    private static final String PAYLOAD = "userdetails.token.id=AQIC5wM2LY4Sfczq7rqe5T89hyJzxvtfmlagB2VFZEFxE2I.*AAJTSQACMDE.*\r\n"
+            + "userdetails.role=id=student,ou=group,dc=opensso,dc=java,dc=net\r\n"
+            + "userdetails.role=id=parent,ou=group,dc=opensso,dc=java,dc=net\r\n"
+            + "userdetails.role=id=teacher,ou=group,dc=opensso,dc=java,dc=net\r\n"
+            + "userdetails.attribute.name=uid\r\n"
+            + "userdetails.attribute.value=demo\r\n"
+            + "userdetails.attribute.name=userpassword\r\n"
+            + "userdetails.attribute.value={SSHA}lDuocwqvaaTzkN5Bi+beD2BJBP4sI/dtzD/BfQ==\r\n"
+            + "userdetails.attribute.name=sn\r\n"
+            + "userdetails.attribute.value=demo\r\n"
+            + "userdetails.attribute.name=cn\r\n"
+            + "userdetails.attribute.value=demo demo\r\n"
+            + "userdetails.attribute.name=givenname\r\n"
+            + "userdetails.attribute.value=demo\r\n"
+            + "userdetails.attribute.name=inetuserstatus\r\n"
+            + "userdetails.attribute.value=Active\r\n"
+            + "userdetails.attribute.name=dn\r\n"
+            + "userdetails.attribute.value=uid=demo,ou=people,dc=slidev,dc=net\r\n"
+            + "userdetails.attribute.name=objectclass\r\n"
+            + "userdetails.attribute.value=person\r\n"
+            + "userdetails.attribute.value=sunIdentityServerLibertyPPService\r\n"
+            + "userdetails.attribute.value=inetorgperson\r\n"
+            + "userdetails.attribute.value=sunFederationManagerDataStore\r\n"
+            + "userdetails.attribute.value=iPlanetPreferences\r\n"
+            + "userdetails.attribute.value=iplanet-am-auth-configuration-service\r\n"
+            + "userdetails.attribute.value=organizationalperson\r\n"
+            + "userdetails.attribute.value=sunFMSAML2NameIdentifier\r\n"
+            + "userdetails.attribute.value=inetuser\r\n"
+            + "userdetails.attribute.value=iplanet-am-managed-person\r\n"
+            + "userdetails.attribute.value=iplanet-am-user-service\r\n"
+            + "userdetails.attribute.value=sunAMAuthAccountLockout\r\n" + "userdetails.attribute.value=top";
     
     private OpenamRestTokenResolver resolver;
     
@@ -64,9 +83,15 @@ public class OpenamRestTokenResolverTest {
         ResponseEntity<String> validationFail = new ResponseEntity<String>("boolean=true", HttpStatus.UNAUTHORIZED);
         ResponseEntity<String> attributesOK = new ResponseEntity<String>(PAYLOAD, HttpStatus.OK);
         
-        when(rest.getForEntity(MOCK_URL + "/identity/isTokenValid?tokenid=" + VALID_TOKEN, String.class, Collections.<String, Object> emptyMap())).thenReturn(validationOK);
-        when(rest.getForEntity(MOCK_URL + "/identity/isTokenValid?tokenid=" + INVALID_TOKEN, String.class, Collections.<String, Object> emptyMap())).thenReturn(validationFail);
-        when(rest.getForEntity(MOCK_URL + "/identity/attributes?subjectid=" + VALID_TOKEN, String.class, Collections.<String, Object> emptyMap())).thenReturn(attributesOK);
+        when(
+                rest.getForEntity(MOCK_URL + "/identity/isTokenValid?tokenid=" + VALID_TOKEN, String.class,
+                        Collections.emptyMap())).thenReturn(validationOK);
+        when(
+                rest.getForEntity(MOCK_URL + "/identity/isTokenValid?tokenid=" + INVALID_TOKEN, String.class,
+                        Collections.emptyMap())).thenReturn(validationFail);
+        when(
+                rest.getForEntity(MOCK_URL + "/identity/attributes?subjectid=" + VALID_TOKEN, String.class,
+                        Collections.emptyMap())).thenReturn(attributesOK);
         
         return rest;
     }
