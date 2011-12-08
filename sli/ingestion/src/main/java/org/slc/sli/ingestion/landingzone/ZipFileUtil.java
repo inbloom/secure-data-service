e6679ac4538e5fe81210444f0fa170de25e687ca
unknown <ccheng@CCHENG-WKS2.wgenhq.net>
2011-12-08 11:14:48 -0500

    added ZipFileUtil and ZipFileProcessor to handle zip files dropped in LZ

---
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/ZipFileUtil.java
@@ -0,0 +1,84 @@
+package org.slc.sli.ingestion.landingzone;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.slc.sli.ingestion.processors.ZipFileProcessor;
+
+public class ZipFileUtil {
+
+    static Logger log = LoggerFactory.getLogger(ZipFileUtil.class);
+
+    static final int BUFFER = 2048;
+
+    public static File extract(File zipFile) throws IOException {
+
+        File path = new File("C:/sli/lz/inbound/zip");
+
+        try {
+            // Create input stream
+            FileInputStream fis = new FileInputStream(zipFile);
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+
+            ZipEntry entry;
+
+            // Extract files
+            while ((entry = zis.getNextEntry()) != null) {
+
+                int count;
+                byte[] data = new byte[BUFFER];
+
+                // Create output stream
+                FileOutputStream fos = new FileOutputStream(path.getAbsolutePath() + "/" + entry.getName());
+                BufferedOutputStream bos = new BufferedOutputStream(fos, BUFFER);
+
+                while ((count = zis.read(data, 0, BUFFER)) != -1) {
+                    bos.write(data, 0, count);
+                }
+                bos.flush();
+                bos.close();
+            }
+
+            zis.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return path;
+    }
+
+    public static File findCtlFile(File dir) throws IOException {
+
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                //if the file extension is .ctl return true, else false
+                return name.endsWith(".ctl");
+            }
+        };
+
+        File[] fileList = dir.listFiles(filter);
+        File ctlFile = null;
+
+        if (fileList.length > 0) {
+            ctlFile = fileList[0];
+            log.info("Found control file: " + ctlFile.getName());
+        } else {
+            log.info("No control file found in " + dir.getAbsolutePath());
+        }
+
+        return ctlFile;
+    }
+}
