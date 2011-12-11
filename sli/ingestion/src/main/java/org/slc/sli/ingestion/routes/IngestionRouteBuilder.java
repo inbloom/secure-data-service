20149719dedd6c89c2c311fc13d1d144b9fada1f
Jim Abramson <jabramson@wgen.net>
2011-12-11 15:41:35 -0500

    added camel routing to reject the assembled BatchJob in case of errors, and to generate a simple log file in the lz

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/routes/IngestionRouteBuilder.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/routes/IngestionRouteBuilder.java
@@ -1,14 +1,17 @@
 package org.slc.sli.ingestion.routes;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.spring.SpringRouteBuilder;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import org.slc.sli.ingestion.BatchJob;
+import org.slc.sli.ingestion.Fault;
 import org.slc.sli.ingestion.landingzone.LocalFileSystemLandingZone;
 import org.slc.sli.ingestion.processors.ControlFilePreProcessor;
 import org.slc.sli.ingestion.processors.ControlFileProcessor;
@@ -42,30 +45,92 @@ public class IngestionRouteBuilder extends SpringRouteBuilder {
 
         String inboundDir = lz.getDirectory().getPath();
 
-        from("file:" + inboundDir + "?include=^(.*)\\.ctl$&move=" + inboundDir + "/.done&moveFailed="
-                + inboundDir + "/.error").process(new ControlFilePreProcessor(lz)).to("seda:CtrlFilePreProcessor");
+        // routeId: ctlFilePoller
+        from(
+                "file:" + inboundDir + "?include=^(.*)\\.ctl$&move="
+                        + inboundDir + "/.done&moveFailed=" + inboundDir
+                        + "/.error").routeId("ctlFilePoller")
+                .process(new ControlFilePreProcessor(lz))
+                .to("seda:CtrlFilePreProcessor");
 
-        from("seda:CtrlFilePreProcessor").process(ctlFileProcessor).to("seda:jobs");
+        // routeId: ctlFilePreprocessor
+        from("seda:CtrlFilePreProcessor").process(ctlFileProcessor).to(
+                "seda:assembledJobs");
 
-        from("file:" + inboundDir + "?include=^(.*)\\.zip$&move=" + inboundDir + "/.done&moveFailed="
-                + inboundDir + "/.error").process(zipFileProcessor)
+        // routeId: zipFilePoller
+        from(
+                "file:" + inboundDir + "?include=^(.*)\\.zip$&move="
+                        + inboundDir + "/.done&moveFailed=" + inboundDir
+                        + "/.error")
+                .routeId("zipFilePoller")
+                .process(zipFileProcessor)
                 .process(new Processor() {
 
-                	// set temporary path to where the files were unzipped
-                	@Override
+                    // set temporary path to where the files were unzipped
+                    @Override
                     public void process(Exchange exchange) throws Exception {
                         File ctlFile = exchange.getIn().getBody(File.class);
                         temp.setDirectory(ctlFile.getParentFile());
                     }
-                }).process(new ControlFilePreProcessor(temp)).to("seda:CtrlFilePreProcessor");
+                }).process(new ControlFilePreProcessor(temp))
+                .to("seda:CtrlFilePreProcessor");
 
-        from("seda:jobs").process(new Processor() {
+        // routeId: jobDispatch
+        from("seda:assembledJobs")
+                .routeId("jobDispatch")
+                .wireTap("direct:jobReporting")
+                .choice()
+                    .when(header("hasErrors").isEqualTo(true))
+                    .stop()
+                .otherwise()
+                    .to("seda:acceptedJobs");
+
+        // routeId: jobReporting
+        from("direct:jobReporting")
+                .routeId("jobReporting")
+                .process(new Processor() {
+
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+
+                        // get job from exchange
+                        BatchJob job = exchange.getIn().getBody(BatchJob.class);
+                        
+                        // create a log file
+                        File logFile = 
+                                lz.createFile("job-" + job.getId() + ".log");
+                        
+                        // create a holder for lines
+                        ArrayList<String> lines = new ArrayList<String>();
+
+                        // add output as lines
+                        lines.add("jobId: " + job.getId());
+                        for (Fault fault: job.getFaults()) {
+                            lines.add(fault.toString());
+                        }
+                        if (job.hasErrors()) {
+                            lines.add("job has been rejected due to errors");
+                        }
+                        
+                        // write lines to file
+                        FileUtils.writeLines(logFile, lines);
+                    }
+                    
+                });        
+        
+        // routeId: jobPipeline
+        from("seda:acceptedJobs")
+                .routeId("jobPipeline")
+                .process(
+                        
+        new Processor() {
 
             // TEMPORARY SOLUTION
-            // inline implementation exists solely to convert the input of type BatchJob to
-            // type File (the first file in that job).
-            // really we'd like to keep BatchJob as the message content, but it will
-            // require refactoring of all the downstream processors/components.
+            // inline implementation exists solely to convert the input of 
+            // type BatchJob to type File (the first file in that job).
+            // really we'd like to keep BatchJob as the message content, but it
+            // will require refactoring of all the downstream 
+            // processors/components.
 
             @Override
             public void process(Exchange exchange) throws Exception {
@@ -73,20 +138,32 @@ public class IngestionRouteBuilder extends SpringRouteBuilder {
                 File file0 = job.getFiles().get(0);
                 exchange.getOut().setBody(file0);
 
-                // we can use message headers to relay any job config params however.
+                // use message headers to relay any job config params however.
                 exchange.getOut().setHeader("jobId", job.getId());
-                exchange.getOut().setHeader("jobCreationDate", job.getCreationDate());
+                exchange.getOut().setHeader("jobCreationDate",
+                        job.getCreationDate());
+                exchange.getOut().setHeader("hasErrors",
+                        job.hasErrors());
                 if (job.getProperty("dry-run") != null) {
                     exchange.getOut().setHeader("dry-run", true);
                 }
 
             }
 
-        }).process(xmlProcessor).to("seda:persist");
+        })
+                .process(xmlProcessor)
+                .to("seda:persist");
 
-        from("seda:persist").log("persist: jobId: " + header("jobId").toString()).choice()
-                .when(header("dry-run").isEqualTo(true)).log("dry-run specified; data will not be published")
-                .otherwise().log("publishing data now!").process(persistenceProcessor);
+        // routeId: persistencePipeline
+        from("seda:persist")
+                .routeId("persistencePipeline")
+                .log("persist: jobId: " + header("jobId").toString())
+                .choice()
+                    .when(header("dry-run").isEqualTo(true))
+                        .log("dry-run specified; data will not be published")
+                    .otherwise()
+                        .log("publishing data now!")
+                        .process(persistenceProcessor);
     }
 
 }
