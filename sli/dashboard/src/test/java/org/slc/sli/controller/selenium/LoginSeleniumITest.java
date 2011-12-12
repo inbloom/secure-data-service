264805fcbd2144311dd1f19c32c79f274e7ec675
Sivan Hermon <shermon@wgen.net>
2011-12-12 18:04:32 -0500

    Updated Selenium test to test for bad username | That repository definition will have a unique id, so we can create a mirror reference for that

--- sli/dashboard/src/test/java/org/slc/sli/controller/selenium/LoginSeleniumITest.java
+++ sli/dashboard/src/test/java/org/slc/sli/controller/selenium/LoginSeleniumITest.java
@@ -14,21 +14,42 @@ public class LoginSeleniumITest {
     WebDriver driver;
     String loginUrl;
     String testUser;
+    String testBadUser;
  
     @Before
     public void setup() {
         driver = new FirefoxDriver();
         loginUrl = "http://localhost:8080/dashboard/login";
         testUser = "sravan";
+        testBadUser = "lululu";
     }
     
     @Test
     public void testLoginPage() {
         driver.get(loginUrl);
+        
+        /*
+         * Test for invalid username
+         */
         WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys(testBadUser);
+        // Before submission - error message should not be displayed
+        WebElement errorMessage = driver.findElement(By.name("errorMessage"));
+        assertTrue(errorMessage.getCssValue("display").equalsIgnoreCase("none"));
+
+        WebElement loginForm = driver.findElement(By.name("loginForm"));
+        loginForm.submit();
+        // After submission - error message should be displayed
+        errorMessage = driver.findElement(By.name("errorMessage"));
+        assertTrue(errorMessage.getCssValue("display").equalsIgnoreCase("block"));
+
+        /*
+         * Test for a valid username
+         */
+        username = driver.findElement(By.name("username"));
         username.sendKeys(testUser);
         
-        WebElement loginForm = driver.findElement(By.name("loginForm"));
+        loginForm = driver.findElement(By.name("loginForm"));
         loginForm.submit();
         WebElement body = driver.findElement(By.tagName("body"));
         String bodyText = body.getText();
