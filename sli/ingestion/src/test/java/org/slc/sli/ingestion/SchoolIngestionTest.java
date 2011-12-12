842e4bbfb902cca7a59d282900e14040f2f35a99
rbloh <rbloh@wgen.net>
2011-12-12 12:54:05 -0500

    Integrated Processors to work with new IngestionFileEntry

--- sli/ingestion/src/test/java/org/slc/sli/ingestion/SchoolIngestionTest.java
+++ sli/ingestion/src/test/java/org/slc/sli/ingestion/SchoolIngestionTest.java
@@ -10,9 +10,6 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slc.sli.ingestion.processors.EdFiProcessor;
-import org.slc.sli.ingestion.processors.IngestionProcessor;
-import org.slc.sli.ingestion.processors.PersistenceProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,186 +20,177 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.xml.sax.SAXException;
 
 import org.slc.sli.domain.School;
+import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
+import org.slc.sli.ingestion.processors.EdFiProcessor;
+import org.slc.sli.ingestion.processors.PersistenceProcessor;
+import org.slc.sli.ingestion.util.MD5;
 import org.slc.sli.repository.SchoolRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class })
 public class SchoolIngestionTest {
-
+    
     @Autowired
     private EdFiProcessor edFiProcessor;
-
+    
     @Autowired
     private PersistenceProcessor persistenceProcessor;
-
+    
     @Autowired
     private SchoolRepository schoolRepository;
-
+    
     @Test
-    public void testSchoolIngestionPersistence() throws IOException,
-            SAXException {
-
+    public void testSchoolIngestionPersistence() throws IOException, SAXException {
+        
         schoolRepository.deleteAll();
-
+        
         int numberOfSchools = 2;
-        List<NeutralRecord> neutralRecords = createSchoolIngestionNeutralRecords(
-                persistenceProcessor, numberOfSchools);
-
-        File neutralRecordsFile = IngestionTest
-                .createNeutralRecordsFile(neutralRecords);
-
-        File ingestionPersistenceProcessorOutputFile = IngestionTest
-                .createTempFile();
-
-        persistenceProcessor.processIngestionStream(neutralRecordsFile,
-                ingestionPersistenceProcessorOutputFile);
-
+        List<NeutralRecord> neutralRecords = createSchoolIngestionNeutralRecords(numberOfSchools);
+        
+        File neutralRecordsFile = IngestionTest.createNeutralRecordsFile(neutralRecords);
+        
+        File ingestionPersistenceProcessorOutputFile = IngestionTest.createTempFile();
+        
+        persistenceProcessor.processIngestionStream(neutralRecordsFile, ingestionPersistenceProcessorOutputFile);
+        
         verifySchools(schoolRepository, numberOfSchools);
-
+        
     }
-
+    
     @Test
-    public void testSchoolInterchangeXmlParsing() throws IOException,
-            SAXException {
-
+    public void testSchoolInterchangeXmlParsing() throws IOException, SAXException {
+        
         schoolRepository.deleteAll();
-
+        
         int numberOfSchools = 2;
         String xmlRecords = createSchoolInterchangeXml(numberOfSchools);
-
-        File xmlRecordsFile = IngestionTest.createTestFile(xmlRecords);
-
+        
+        File inputFile = IngestionTest.createTestFile(xmlRecords);
+        
+        // Create Ingestion File Entry
+        IngestionFileEntry inputFileEntry = new IngestionFileEntry(FileFormat.CSV, FileType.CSV_SCHOOL,
+                inputFile.getName(), MD5.calculate(inputFile));
+        inputFileEntry.setFile(inputFile);
+        
         File ingestionEdFiProcessorOutputFile = IngestionTest.createTempFile();
-
-        edFiProcessor.processIngestionStream(xmlRecordsFile,
-                ingestionEdFiProcessorOutputFile);
-
-        File ingestionPersistenceProcessorOutputFile = IngestionTest
-                .createTempFile();
-
-        persistenceProcessor.processIngestionStream(
-                ingestionEdFiProcessorOutputFile,
+        
+        edFiProcessor.processIngestionStream(inputFileEntry, ingestionEdFiProcessorOutputFile);
+        
+        File ingestionPersistenceProcessorOutputFile = IngestionTest.createTempFile();
+        
+        persistenceProcessor.processIngestionStream(ingestionEdFiProcessorOutputFile,
                 ingestionPersistenceProcessorOutputFile);
-
+        
         verifySchools(schoolRepository, 0);
-
+        
     }
-
+    
     public static String createSchoolInterchangeXml(int numberOfSchools) {
         StringBuilder builder = new StringBuilder();
-
+        
         builder.append(createSchoolInterchangeXmlHeader());
-
+        
         for (int index = 1; index <= numberOfSchools; index++) {
             School school = createSchool(index);
             builder.append(createSchoolXml(school));
         }
         builder.append(createSchoolInterchangeXmlFooter());
-
+        
         return builder.toString();
     }
-
-    public static String createSchoolIngestionJson(
-            IngestionProcessor ingestionProcessor, int numberOfSchools)
-            throws IOException, SAXException {
+    
+    public static String createSchoolIngestionJson(int numberOfSchools) throws IOException, SAXException {
         StringBuilder builder = new StringBuilder();
-
+        
         for (int index = 1; index <= numberOfSchools; index++) {
             School school = createSchool(index);
             builder.append(Translator.mapToJson(school, "create"));
             builder.append(System.getProperty("line.separator"));
         }
-
+        
         return builder.toString();
     }
-
-    public static List<NeutralRecord> createSchoolIngestionNeutralRecords(
-            IngestionProcessor ingestionProcessor, int numberOfSchools) {
+    
+    public static List<NeutralRecord> createSchoolIngestionNeutralRecords(int numberOfSchools) {
         List<NeutralRecord> list = new ArrayList<NeutralRecord>();
-
+        
         for (int index = 1; index <= numberOfSchools; index++) {
             School school = createSchool(index);
-
+            
             list.add(Translator.mapToNeutralRecord(school));
         }
-
+        
         return list;
     }
-
+    
     public static String createSchoolInterchangeXmlHeader() {
-
-        String interchangeXmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                + "\n";
+        
+        String interchangeXmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "\n";
         interchangeXmlHeader += "<InterchangeEducationOrganization xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-EducationOrganization.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
                 + "\n";
-
+        
         return interchangeXmlHeader;
     }
-
+    
     public static String createSchoolInterchangeXmlFooter() {
-
-        String interchangeXmlFooter = "</InterchangeEducationOrganization>"
-                + "\n";
-
+        
+        String interchangeXmlFooter = "</InterchangeEducationOrganization>" + "\n";
+        
         return interchangeXmlFooter;
     }
-
+    
     public static String createSchoolXml(School school) {
         String schoolXml = "";
-
+        
         schoolXml += "<School>" + "\n";
-
+        
         // Test Version Only - allow specification of Student ID
         schoolXml += "<SchoolId>" + school.getSchoolId() + "</SchoolId>" + "\n";
-
-        schoolXml += "<StateOrganizationId>" + school.getStateOrganizationId()
-                + "</StateOrganizationId>" + "\n";
-        schoolXml += "<NameOfInstitution>" + school.getFullName()
-                + "</NameOfInstitution>" + "\n";
+        
+        schoolXml += "<StateOrganizationId>" + school.getStateOrganizationId() + "</StateOrganizationId>" + "\n";
+        schoolXml += "<NameOfInstitution>" + school.getFullName() + "</NameOfInstitution>" + "\n";
         schoolXml += "</School>" + "\n";
-
+        
         return schoolXml;
     }
-
+    
     public static School createSchool(int schoolId) {
         School school = new School();
-
+        
         school.setSchoolId(schoolId);
         school.setStateOrganizationId("" + schoolId);
         school.setFullName("nameOfInstitution" + "_" + schoolId);
-
+        
         return school;
     }
-
-    public static void verifySchools(PagingAndSortingRepository<?, Integer> repository,
-            long numberOfSchools) {
-
+    
+    public static void verifySchools(PagingAndSortingRepository<?, Integer> repository, long numberOfSchools) {
+        
         long repositorySize = repository.count();
-
+        
         if (numberOfSchools > 0) {
             assertEquals(repositorySize, numberOfSchools);
         }
-
+        
         for (int index = 1; index <= repositorySize; index++) {
             School school = (School) repository.findOne(index);
             verifySchool(index, school);
         }
-
+        
     }
-
+    
     public static void verifySchool(int schoolId, School school) {
-
+        
         assertNotNull(school);
         assertEquals("" + schoolId, school.getStateOrganizationId());
         assertEquals("nameOfInstitution" + "_" + schoolId, school.getFullName());
-
+        
     }
-
+    
     public static String calculateTestDate(int studentId) {
         String testDate = "";
-
+        
         int yearId = studentId % 10000;
         testDate = "" + yearId;
         if (yearId < 10)
@@ -211,9 +199,9 @@ public class SchoolIngestionTest {
             testDate = "00" + testDate;
         else if (yearId < 1000)
             testDate = "0" + testDate;
-
+        
         testDate = testDate + "-01-01";
         return testDate;
     }
-
+    
 }
--- sli/ingestion/src/test/java/org/slc/sli/ingestion/StudentIngestionCamelTest.java
+++ sli/ingestion/src/test/java/org/slc/sli/ingestion/StudentIngestionCamelTest.java
@@ -11,68 +11,72 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slc.sli.ingestion.processors.EdFiProcessor;
-import org.slc.sli.ingestion.processors.PersistenceProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
+import org.slc.sli.ingestion.processors.EdFiProcessor;
+import org.slc.sli.ingestion.processors.PersistenceProcessor;
+import org.slc.sli.ingestion.util.MD5;
 import org.slc.sli.repository.StudentRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class })
 public class StudentIngestionCamelTest {
-
+    
     @Autowired
     private EdFiProcessor edFiProcessor;
-
+    
     @Autowired
     private PersistenceProcessor persistenceProcessor;
-
+    
     @Autowired
     private StudentRepository studentRepository;
-
+    
     @Test
     public void testDirectRoute() throws Exception {
-
+        
         studentRepository.deleteAll();
-
+        
         int numberOfStudents = 2;
-
+        
         // Set up input file
-        String xmlRecords = StudentIngestionTest
-                .createStudentInterchangeXml(numberOfStudents);
-        File xmlRecordsFile = IngestionTest.createTestFile(
-                IngestionTest.INGESTION_FILE_PREFIX,
+        String xmlRecords = StudentIngestionTest.createStudentInterchangeXml(numberOfStudents);
+        File xmlRecordsFile = IngestionTest.createTestFile(IngestionTest.INGESTION_FILE_PREFIX,
                 IngestionTest.INGESTION_XML_FILE_SUFFIX, xmlRecords);
-
+        
+        // Create Ingestion File Entry
+        IngestionFileEntry xmlRecordsFileEntry = new IngestionFileEntry(FileFormat.EDFI_XML, FileType.XML_STUDENT,
+                xmlRecordsFile.getName(), MD5.calculate(xmlRecordsFile));
+        xmlRecordsFileEntry.setFile(xmlRecordsFile);
+        
         // Create camel context w/ routes
         CamelContext context = new DefaultCamelContext();
         context.addRoutes(getRouteBuilder());
-
+        
         // Get camel producer template to send input message
         ProducerTemplate template = context.createProducerTemplate();
-
+        
         // Start camel context
         context.start();
-
+        
         // Send message
-        template.sendBody("direct:start", xmlRecordsFile);
-
+        template.sendBody("direct:start", xmlRecordsFileEntry);
+        
         Thread.sleep(1000);
-
+        
         // Stop camel context
         context.stop();
-
-        StudentIngestionTest
-                .verifyStudents(studentRepository, numberOfStudents);
-
+        
+        StudentIngestionTest.verifyStudents(studentRepository, numberOfStudents);
+        
     }
-
+    
     /*
      * @Test public void testFromFile() throws IOException, SAXException,
      * Exception {
@@ -100,58 +104,55 @@ public class StudentIngestionCamelTest {
      * 
      * }
      */
-
+    
     /*
      * This route mimics the one in IngestionRouteBuilder, but starts with
      * direct input instead of reading from the file system
      */
     protected RouteBuilder getRouteBuilder() {
-
+        
         return new RouteBuilder() {
-
+            
             public void configure() {
-
+                
                 from("direct:start").process(edFiProcessor).to("seda:persist");
-
+                
                 from("seda:persist").process(persistenceProcessor);
             }
         };
     }
-
+    
     /*
      * This route is the same as the one in IngestionRouteBuilder. TODO: figure
      * out why IngestionRouteBuilder causes problems running this test
      */
     protected RouteBuilder getRouteBuilder2() {
-
+        
         return new RouteBuilder() {
-
+            
             public void configure() {
-
-                from(
-                        "file:/home/ingestion/lz/inbound?move=/home/ingestion/lz/inbound/.done&moveFailed=.error")
+                
+                from("file:/home/ingestion/lz/inbound?move=/home/ingestion/lz/inbound/.done&moveFailed=.error")
                         .process(edFiProcessor).to("seda:persist");
-
+                
                 from("seda:persist").process(persistenceProcessor);
             }
         };
     }
-
+    
     @SuppressWarnings("unused")
-    private File createTestFile(String xmlRecords, String filePath)
-            throws IOException {
-
+    private File createTestFile(String xmlRecords, String filePath) throws IOException {
+        
         File xmlRecordsFile = new File(filePath);
         BufferedOutputStream outputStream = null;
-
+        
         try {
-            outputStream = new BufferedOutputStream(new FileOutputStream(
-                    xmlRecordsFile));
+            outputStream = new BufferedOutputStream(new FileOutputStream(xmlRecordsFile));
             outputStream.write(xmlRecords.getBytes());
         } finally {
             outputStream.close();
         }
         return xmlRecordsFile;
     }
-
+    
 }
