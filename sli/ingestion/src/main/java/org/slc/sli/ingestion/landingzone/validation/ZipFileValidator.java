67f231cbc410681f564cfe5e3f1e535339521c33
okrook <okrook@wgenhq.net>
2011-12-12 09:06:16 -0500

    Checkstyle fixes

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/ZipFileValidator.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/ZipFileValidator.java
@@ -16,17 +16,17 @@ public class ZipFileValidator extends IngestionValidator<File> {
     @Override
     public boolean isValid(File zipFile, List<Fault> faults) {
 
-    	ZipFile zf = null;
+        ZipFile zf = null;
 
-		try {
-			zf = new ZipFile(zipFile);
-		} catch (ZipException ex) {
-			// error reading zip file
-			faults.add(Fault.createError(getFailureMessage("SL_ERR_MSG4", zipFile.getName())));
-	        return false;
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+        try {
+            zf = new ZipFile(zipFile);
+        } catch (ZipException ex) {
+            // error reading zip file
+            faults.add(Fault.createError(getFailureMessage("SL_ERR_MSG4", zipFile.getName())));
+            return false;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
         Enumeration<?> entries = zf.entries();
 
@@ -34,9 +34,9 @@ public class ZipFileValidator extends IngestionValidator<File> {
 
             ZipEntry ze = (ZipEntry) entries.nextElement();
 
-            //System.out.println("Read " + ze.getName() + "?");
+            // System.out.println("Read " + ze.getName() + "?");
             if (ze.getName().endsWith(".ctl"))
-            	return true;
+                return true;
         }
 
         // no manifest (.ctl file) found in the zip file
--- sli/ingestion/src/main/java/org/slc/sli/ingestion/processors/IngestionProcessor.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/processors/IngestionProcessor.java
@@ -14,73 +14,72 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
 
-
 /**
  * Ingestion Processor.
- * 
+ *
  * Base Camel Ingestion Processor which provides common ingestion behavior.
- * 
+ *
  */
 
 @Component
 public abstract class IngestionProcessor implements Processor {
 
     Logger log = LoggerFactory.getLogger(IngestionProcessor.class);
-    
+
     public static final String SLI_DOMAIN_PACKAGE = "org.slc.sli.domain";
 
     @Autowired
     private ContextManager contextManager;
-    
+
     private ObjectMapper jsonMapper;
-    
+
     public IngestionProcessor() {
         super();
-                
+
         // Setup Jackson JSON mapper
         this.jsonMapper = new ObjectMapper();
     }
-    
+
     @PostConstruct
     public void init() {
     }
-    
+
     public ContextManager getContextManager() {
         return this.contextManager;
-    }    
-    
+    }
+
     public ObjectMapper getJsonMapper() {
         return this.jsonMapper;
     }
-    
+
     /**
      * Camel Exchange process callback method
-     * 
+     *
      * @param exchange
      */
     @Override
     public void process(Exchange exchange) throws IOException, SAXException {
-                
+
         // Indicate Camel processing
         log.info("processing: {}", exchange.getIn().getHeaders().toString());
 
         // Extract Ingestion input file
         File ingestionInputFile = exchange.getIn().getBody(File.class);
-        
+
         // Setup Ingestion processor output file
         File ingestionOutputFile = File.createTempFile("camel_", ".tmp");
         ingestionOutputFile.deleteOnExit();
-        
+
         // Allow Ingestion processor to process Camel exchange file
         this.processIngestionStream(ingestionInputFile, ingestionOutputFile);
-        
+
         // Update Camel Exchange processor output result
-        exchange.getOut().setBody(ingestionOutputFile);   
-        
-		exchange.getOut().setHeader("jobId", exchange.getIn().getHeader("jobId"));
-		exchange.getOut().setHeader("jobCreationDate", exchange.getIn().getHeader("jobCreationDate"));
-		exchange.getOut().setHeader("dry-run", exchange.getIn().getHeader("dry-run"));
-        
+        exchange.getOut().setBody(ingestionOutputFile);
+
+        exchange.getOut().setHeader("jobId", exchange.getIn().getHeader("jobId"));
+        exchange.getOut().setHeader("jobCreationDate", exchange.getIn().getHeader("jobCreationDate"));
+        exchange.getOut().setHeader("dry-run", exchange.getIn().getHeader("dry-run"));
+
     }
 
     public abstract void processIngestionStream(File inputFile, File outputFile) throws IOException, SAXException;
