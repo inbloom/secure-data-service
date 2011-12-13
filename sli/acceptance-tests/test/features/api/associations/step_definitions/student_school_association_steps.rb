6b8d9a51dfd8a8543c46acbd97728f863b82cbb8
Ryan Farris <rfarris@wgen.net>
2011-12-13 18:25:20 -0500

    Implemented stepdefs for student_school_association.feature

--- sli/acceptance-tests/test/features/api/associations/step_definitions/student_school_association_steps.rb
+++ sli/acceptance-tests/test/features/api/associations/step_definitions/student_school_association_steps.rb
@@ -1,43 +1,58 @@
+require 'rest-client'
+require 'json'
+require 'builder'
+require 'rexml/document'
+include REXML
+require_relative '../../../utils/sli_utils.rb'
 
 
 Given /^I have access to all students and schools$/ do
-  pending # express the regexp above with the code you wish you had
+  url = "http://"+PropLoader.getProps['idp_server_url']+"/idp/identity/authenticate?username="+@user+"&password="+@passwd
+  res = RestClient.get(url){|response, request, result| response }
+  @cookie = res.body[res.body.rindex('=')+1..-1]
+  assert(@cookie != nil, "Cookie retrieved was nil")
 end
 
-Given /^the school "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should receive a ID for the newly created student\-school\-association$/ do
+  headers = @res.raw_headers
+  assert(headers != nil, "Result contained no headers")
+  assert(headers['location'] != nil, "There is no location link from the previous request")
+  s = headers['location'][0]
+  assocId = s[s.rindex('/')+1..-1]
+  assert(assocId != nil, "Student-School-Association ID is nil")
 end
 
 
-Then /^I should see (\d+) students$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I navigate to POST "([^"]*)"$/ do |uri|
+  if @format == "application/json"
+    data = @fields
+    url = "http://"+PropLoader.getProps['api_server_url']+"/api/rest"+uri
+    @res = RestClient.post(url, data.to_json, {:content_type => @format, :cookies => {:sliSessionId => @cookie}}){|response, request, result| response }
+    assert(@res != nil, "Response from rest-client POST is nil")
+  elsif @format == "application/xml"
+    assert(false, "application/xml is not supported")
+  else
+    assert(false, "Unsupported MIME type")
+  end
 end
 
-Then /^I should find the student "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I navigate to PUT "([^"]*)"$/ do |uri|
+  if @format == "application/json"
+      url = "http://"+PropLoader.getProps['api_server_url']+"/api/rest"+uri
+      @res = RestClient.get(url,{:accept => @format, :cookies => {:sliSessionId => @cookie}}){|response, request, result| response }
+      assert(@res != nil, "Response from rest-client GET is nil")
+      assert(@res.code == 200, "Return code was not expected: #{@res.code.to_s} but expected 200")
+      modified = JSON.parse(@res.body)
+      @fields.each do |key, value|
+        modified[key] = value
+      end
+      
+      url = "http://"+PropLoader.getProps['api_server_url']+"/api/rest"+uri
+      @res = RestClient.put(url, modified.to_json, {:content_type => @format, :cookies => {:sliSessionId => @cookie}}){|response, request, result| response }
+      assert(@res != nil, "Response from rest-client PUT is nil")
+    elsif @format == "application/xml"
+      assert(false, "application/xml is not supported")
+    else
+      assert(false, "Unsupported MIME type")
+    end
 end
-
-Then /^I should not find the student "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^the student "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^"([^"]*)" attends "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^change the school to "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should see the student "([^"]*)" attends "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should see the student "([^"]*)" does not attend "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
