a91f436d33ee336cbc9296f81e95e8042127f54e
Matthew Lane <lanemt@gmail.com>
2011-12-14 12:47:58 -0500

    Added session debug and session check resources

--- sli/api/src/main/java/org/slc/sli/api/resources/SessionDebugResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/SessionDebugResource.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * System resource class for security session context.
@@ -16,8 +18,10 @@ import javax.ws.rs.Produces;
 @Path("/system/session")
 @Component
 @Scope("request")
+@Produces("application/json")
 public class SessionDebugResource {
-    
+
+    private String authUrl;
     /**
      * Method processing HTTP GET requests, producing "application/json" MIME media
      * type.
@@ -25,8 +29,37 @@ public class SessionDebugResource {
      * @return SecurityContext that will be send back as a response of type "application/json".
      */
     @GET
-    @Produces("application/json")
+    @Path("debug")
     public SecurityContext getSecurityContext() {
         return SecurityContextHolder.getContext();
     }
+
+    @GET
+    @Path("check")
+    public Object sessionCheck() {
+        
+        Map<String,Object> sessionDetails = new TreeMap<String, Object>();
+
+        if (isAuthenticated(SecurityContextHolder.getContext())) {
+            sessionDetails.put("authenticated","yes");
+            sessionDetails.put("sessionId", SecurityContextHolder.getContext().getAuthentication().getCredentials() );
+        } else {
+            sessionDetails.put("authenticated","no");
+            sessionDetails.put("redirect_user",authUrl);
+        }
+
+        return sessionDetails;
+    }
+
+    private boolean isAuthenticated(SecurityContext securityContext) {
+        return !(securityContext == null || securityContext.getAuthentication() == null
+                || securityContext.getAuthentication().getCredentials() == null
+                || securityContext.getAuthentication().getCredentials().equals(""));
+    }
+
+    public void setAuthUrl(String authUrl) {
+        this.authUrl = authUrl;
+    }
+
+
 }
