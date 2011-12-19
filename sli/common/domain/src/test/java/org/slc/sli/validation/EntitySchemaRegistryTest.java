7bcfbe194ecacc84f79f2494a01669e058886ea6
Dong Liu <dliu@wgen.net>
2011-12-19 17:48:19 -0500

    refactored entityschemaregistry, add unit test for entity schema registry

---
+++ sli/common/domain/src/test/java/org/slc/sli/validation/EntitySchemaRegistryTest.java
@@ -0,0 +1,35 @@
+package org.slc.sli.validation;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slc.sli.domain.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * JUnits for EntitySchemaRegistry
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+public class EntitySchemaRegistryTest {
+    @Autowired
+    EntitySchemaRegistry schemaRegistry;
+    
+    @Test
+    public void testFindSchemaForType() {
+        Entity school = new Entity() {
+            public String getEntityId() {
+                return "";
+            }
+        public String getType(){return "school";}
+        public Map<String,Object> getBody(){return null;}};
+        assertNotNull(schemaRegistry.findSchemaForType(school));
+
+    }
+
+}
