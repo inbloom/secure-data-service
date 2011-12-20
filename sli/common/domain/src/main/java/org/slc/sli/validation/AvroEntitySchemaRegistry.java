4a6960f68129e605a7ada6e7232a0cfbe21e3b22
Dong Liu <dliu@wgen.net>
2011-12-19 20:20:28 -0500

    hard coded the student, school, and studentschoolassocation schema file to avroentityschemaregistry

--- sli/common/domain/src/main/java/org/slc/sli/validation/AvroEntitySchemaRegistry.java
+++ sli/common/domain/src/main/java/org/slc/sli/validation/AvroEntitySchemaRegistry.java
@@ -1,6 +1,5 @@
 package org.slc.sli.validation;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
@@ -24,24 +23,29 @@ import org.springframework.util.ResourceUtils;
 public class AvroEntitySchemaRegistry implements EntitySchemaRegistry {
     
     private Map<String, Schema> mapSchema = new HashMap<String, Schema>();
+    private final String baseDir = "classpath:avroSchema";
+    
+    // TODO need to figure out better way to map schema file name to entity type
+    private String[] schemaTypes = new String[] { "school", "student", "studentSchoolAssociation" };
     
     @PostConstruct
     public void init() throws IOException {
-        
-     // TODO
-        // By convention, load all <entity>_body schema files.
-        
-        Parser parser = new Schema.Parser();
-        
-        // FilenameUtils.
-        URL url = ResourceUtils.getURL("classpath:avroSchema/school_body.avpr");
-        Schema schema = parser.parse(url.openStream());
-        mapSchema.put("school", schema);
+        for (String schemaType : schemaTypes) {
+            registerSchema(schemaType);
+        }
     }
     
     @Override
     public Schema findSchemaForType(Entity entity) {
         return mapSchema.get(entity.getType());
+    }
+    
+    private void registerSchema(String schemaType) throws IOException {
+        Parser parser = new Schema.Parser();
+        URL url = ResourceUtils.getURL(baseDir + "/" + schemaType + "_body.avpr");
+        Schema schema = parser.parse(url.openStream());
+        mapSchema.put(schemaType, schema);
+        
     }
     
 }
