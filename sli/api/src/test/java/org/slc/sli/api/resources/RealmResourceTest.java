ae783d0222cfbbc7f296e2445938f79e3c8f180d
Danil Kornishev <dkornishev@wgen.net>
2011-12-21 14:35:43 -0500

    Realm info api

---
+++ sli/api/src/test/java/org/slc/sli/api/resources/RealmResourceTest.java
@@ -0,0 +1,121 @@
+package org.slc.sli.api.resources;
+
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import org.slc.sli.api.config.EntityDefinition;
+import org.slc.sli.api.config.EntityDefinitionStore;
+import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.resources.admin.RealmResource;
+import org.slc.sli.api.service.EntityService;
+import org.slc.sli.api.test.WebContextTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+@TestExecutionListeners({ WebContextTestExecutionListener.class, DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class })
+public class RealmResourceTest {
+    
+    private static final String     VALID_REALM_ID   = "Calradia";
+    private static final String     INVALID_REALM_ID = "Mordor";
+    private static final String     IDP_ID_EXPECTED  = "http://devdanil.slidev.org:8080/idp";
+    
+    @Autowired
+    private RealmResource           realmer;
+    
+    @Autowired
+    @Value("${security.sso.url}")
+    private String                  ssoInitUrl;
+    
+    private Map<String, EntityBody> entities;
+    
+    @Before
+    public void init() {
+        entities = new HashMap<String, EntityBody>();
+        
+        EntityBody entity = new EntityBody();
+        entity.put("id", VALID_REALM_ID);
+        entity.put("state", "nc");
+        entity.put("idp", IDP_ID_EXPECTED);
+        
+        entities.put(VALID_REALM_ID, entity);
+        
+        realmer.setStore(mockStore());
+    }
+    
+    @Test
+    public void testRealmList() {
+        Set<EntityBody> realms = realmer.getRealms();
+        
+        Assert.assertNotNull(realms);
+        Assert.assertTrue(realms.size() > 0);
+        
+        for (EntityBody eb : realms) {
+            EntityBody expected = this.entities.get(eb.get("id"));
+            
+            if (expected != null) {
+                Assert.assertEquals(expected.get("idp"), eb.get("idp"));
+                Assert.assertEquals(expected.get("state"), eb.get("state"));
+            }
+        }
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testSSONothing() {
+        String ssoInit = realmer.getSsoInitUrl(null);
+        
+        Assert.assertNull(ssoInit);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testSSOInvalidId() {
+        
+        String ssoInit = realmer.getSsoInitUrl(INVALID_REALM_ID);
+        
+        Assert.assertNull(ssoInit);
+    }
+    
+    @Test
+    public void testSSOValidId() throws Exception {
+        
+        String ssoInit = realmer.getSsoInitUrl(VALID_REALM_ID);
+        
+        Assert.assertNotNull(ssoInit);
+        Assert.assertEquals(this.ssoInitUrl.replaceAll("\\{idpId\\}", URLEncoder.encode(IDP_ID_EXPECTED, "UTF-8")), ssoInit);
+    }
+    
+    private EntityDefinitionStore mockStore() {
+        
+        EntityDefinitionStore store = Mockito.mock(EntityDefinitionStore.class);
+        EntityDefinition ed = Mockito.mock(EntityDefinition.class);
+        EntityService es = Mockito.mock(EntityService.class);
+        
+        Mockito.when(ed.getService()).thenReturn(es);
+        Mockito.when(store.lookupByResourceName("realm")).thenReturn(ed);
+        
+        Mockito.when(es.list(0, 9999)).thenReturn(Arrays.asList(VALID_REALM_ID));
+        
+        EntityBody entity = this.entities.get(VALID_REALM_ID);
+        
+        Mockito.when(es.get(VALID_REALM_ID)).thenReturn(entity);
+        Mockito.when(es.get(Arrays.asList(VALID_REALM_ID))).thenReturn(Arrays.asList(entity));
+        
+        return store;
+    }
+}
