fb35c806b13537382be65f2a93497837a9477d00
Ryan Latta <rlatta@wgen.net>
2011-12-22 09:39:10 -0500

    Removing inline RoleMapper from class.

--- sli/api/src/main/java/org/slc/sli/api/security/openam/OpenamRestTokenResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/openam/OpenamRestTokenResolver.java
@@ -9,13 +9,12 @@ import java.util.regex.Pattern;
 import org.slc.sli.api.security.SLIAuthenticationEntryPoint;
 import org.slc.sli.api.security.SLIPrincipal;
 import org.slc.sli.api.security.SecurityTokenResolver;
-import org.slc.sli.api.security.enums.DefaultRoles;
+import org.slc.sli.api.security.roles.RoleMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -89,34 +88,6 @@ public class OpenamRestTokenResolver implements SecurityTokenResolver {
         return new PreAuthenticatedAuthenticationToken(principal, token, new RoleMapper(principal.getTheirRoles()).buildMappedRoles());
     }
 
-    /**
-     * A simple builder class to map their role to ours if it exists.
-     *
-     * TODO: Extract it and make it work against custom roles.
-     */
-    final class RoleMapper {
-        List<GrantedAuthorityImpl> mappedRoles;
-        List<String> theirRoles;
-        
-        public List<GrantedAuthorityImpl> buildMappedRoles() {
-            for (String role : theirRoles) {
-                if (DefaultRoles.ADMINISTRATOR.getRoleName().equals(role)
-                        || DefaultRoles.LEADER.getRoleName().equals(role)
-                        || DefaultRoles.EDUCATOR.getRoleName().equals(role)
-                        || DefaultRoles.AGGREGATOR.getRoleName().equals(role)) {
-                    mappedRoles.add(new GrantedAuthorityImpl(role));
-                }
-            }
-            //TODO When we have the IDP generating real roles we won't use this.
-            mappedRoles.add(new GrantedAuthorityImpl("ROLE_USER"));
-            return mappedRoles;
-        }
-        private RoleMapper(List<String> roles) {
-            theirRoles = roles;
-            mappedRoles = new ArrayList<GrantedAuthorityImpl>();
-        }
-    }
-    
     private List<String> extractRoles(String payload) {
         List<String> roles = new ArrayList<String>();
         Pattern p = Pattern.compile("userdetails\\.role=id=([^,]*)", Pattern.MULTILINE);
