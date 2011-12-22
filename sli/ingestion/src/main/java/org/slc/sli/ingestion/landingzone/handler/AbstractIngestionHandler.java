81d166d4378651ba203b2441da286bf4e7c6e352
Danilo Duran <dduran@wgen.net>
2011-12-22 09:40:38 -0500

    use Validator interface for pre and post validation

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/handler/AbstractIngestionHandler.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/handler/AbstractIngestionHandler.java
@@ -1,39 +1,60 @@
 package org.slc.sli.ingestion.landingzone.handler;
 
-import org.slc.sli.ingestion.landingzone.validation.IngestionValidator;
+import org.slc.sli.ingestion.validation.DummyValidationReport;
+import org.slc.sli.ingestion.validation.ValidationReport;
+import org.slc.sli.ingestion.validation.ValidationReportAble;
+import org.slc.sli.ingestion.validation.Validator;
 
 /**
  * Abstract class for all handlers.
- * 
+ *
  * @author dduran
- * 
+ *
  */
 public abstract class AbstractIngestionHandler<T> implements Handler<T> {
-    
-    IngestionValidator<T> preValidator;
-    IngestionValidator<T> postValidator;
-    
+
+    Validator<T> preValidator;
+
+    Validator<T> postValidator;
+
     abstract void doHandling(T item);
-    
-    void pre(T item) {
-        // TODO: fix after validators are refactored
-        // preValidator.validate();
+
+    void pre(T item, ValidationReport validationReport) {
+        if (preValidator != null) {
+            preValidator.isValid(item, validationReport);
+        }
     };
-    
-    void post(T item) {
-        // TODO: fix after validators are refactored
-        // postValidator.validate();
+
+    void post(T item, ValidationReport validationReport) {
+        if (postValidator != null) {
+            preValidator.isValid(item, validationReport);
+        }
     };
-    
+
     @Override
     public void handle(T item) {
-        
-        pre(item);
-        
+
+        ValidationReport vr = (item instanceof ValidationReportAble) ? ((ValidationReportAble) item)
+                .getValidationReport() : new DummyValidationReport();
+
+        handle(item, vr);
+    }
+
+    @Override
+    public void handle(T item, ValidationReport vr) {
+
+        pre(item, vr);
+
         doHandling(item);
-        
-        post(item);
-        
-    };
-    
+
+        post(item, vr);
+    }
+
+    public void setPreValidator(Validator<T> preValidator) {
+        this.preValidator = preValidator;
+    }
+
+    public void setPostValidator(Validator<T> postValidator) {
+        this.postValidator = postValidator;
+    }
 }
