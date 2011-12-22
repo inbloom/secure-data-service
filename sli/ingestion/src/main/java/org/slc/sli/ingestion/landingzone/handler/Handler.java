6736c923ecaef17ab02effcc8ced9ffc30671906
Danilo Duran <dduran@wgen.net>
2011-12-22 09:37:18 -0500

    add interface method for client to provide a ValidationReport to the handler. added javadoc comments.

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/handler/Handler.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/handler/Handler.java
@@ -1,13 +1,29 @@
 package org.slc.sli.ingestion.landingzone.handler;
 
+import org.slc.sli.ingestion.validation.ValidationReport;
+
 /**
- * 
+ *
  * @author dduran
  *
  * @param <T>
  */
 public interface Handler<T> {
-    
+
+    /**
+     * Handle the provided type.
+     *
+     * @param item
+     */
     void handle(T item);
-    
+
+    /**
+     * Handle the provided type and utilize the provided ValidationReport for any type of validation
+     * that may be implemented.
+     *
+     * @param item
+     * @param vr
+     */
+    void handle(T item, ValidationReport vr);
+
 }
