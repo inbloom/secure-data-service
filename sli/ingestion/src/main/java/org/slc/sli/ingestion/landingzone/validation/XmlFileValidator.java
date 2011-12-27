a8e1d1018f4bd40dda27ff617e2312b039b8e60c
Oleg Krook <okrook@wgen.net>
2011-12-27 09:11:41 -0500

    Fixed merge issue

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/XmlFileValidator.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/XmlFileValidator.java
@@ -1,10 +1,11 @@
 package org.slc.sli.ingestion.landingzone.validation;
 
-import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
-import org.slc.sli.ingestion.validation.SimpleValidator;
-import org.slc.sli.ingestion.validation.ValidationReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
+import org.slc.sli.ingestion.validation.ErrorReport;
+import org.slc.sli.ingestion.validation.SimpleValidator;
 
 /**
  * Validator for EdFi xml ingestion files.
@@ -17,7 +18,7 @@ public class XmlFileValidator extends SimpleValidator<IngestionFileEntry> {
     private static final Logger LOG = LoggerFactory.getLogger(XmlFileValidator.class);
 
     @Override
-    public boolean isValid(IngestionFileEntry object, ValidationReport callback) {
+    public boolean isValid(IngestionFileEntry object, ErrorReport callback) {
         LOG.info("validating...");
         return false;
     }
