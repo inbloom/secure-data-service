f5ff21bd0338c3b1d083ed54d633f481d62d2bc2
Jeffrey Stokes <jstokes@wgen.net>
2011-12-28 16:31:51 -0500

    implementing gherkin tests

--- sli/acceptance-tests/test/features/api/associations/section_student_association/step_definitions/student_section_association_steps.rb
+++ sli/acceptance-tests/test/features/api/associations/section_student_association/step_definitions/student_section_association_steps.rb
@@ -5,25 +5,38 @@ require 'rexml/document'
 include REXML
 require_relative '../../common.rb'
 
+#transform to section or student uri
 Transform /^(Section|Student) "([^"]*)"$/ do |type, id|
   puts "called with type = #{type} and id = #{id}"
   if type == "Student"
-    uri = "TODO" if id == "Jane Doe"
-    uri = "TODO" if id == "Albert Wright"
-    uri = "TODO" if id == "Kevin Smith"
+    uri = "4efb3a11-bc49-f388-0000-0000c93556fb" if id == "Jane Doe"
+    uri = "4efb3a5e-bc49-f388-0000-0000c93556fc" if id == "Albert Wright"
+    uri = "4efb3a7f-bc49-f388-0000-0000c93556fd" if id == "Kevin Smith"
   elsif type == "Section"
-    uri = "TODO" if id == "Biology II - C"
-    uri = "TODO" if id == "Foreign Language - A"
-    uri = "TODO" if id == "Physics I - B"
-    uri = "TODO" if id == "Chemistry I - A"
+    uri = "4efb4262-bc49-f388-0000-0000c9355700" if id == "Biology II - C"
+    uri = "4efb4292-bc49-f388-0000-0000c9355701" if id == "Foreign Language - A"
+    uri = "4efb4243-bc49-f388-0000-0000c93556ff" if id == "Physics I - B"
+    uri = "4efb4238-bc49-f388-0000-0000c93556fe" if id == "Chemistry I - A"
   end
   uri
 end
 
+#transform to student section association by student AND section
 Transform /^Student Section Association for Student "([^"]*)" and Section "([^"]*)"$/ do |student, section|
   puts "called with student:#{student} and section #{section}"
-  uri = "TODO" if student == "Jane Doe" and section == "Biology II - C"
+  uri = "4efb4b14-bc49-f388-0000-0000c9355702" if student == "Jane Doe" and section == "Foreign Language - A"
+  uri = "4efb4b2f-bc49-f388-0000-0000c9355703" if student == "Jane Doe" and section == "Physics I - B"
+  uri = "4efb4b38-bc49-f388-0000-0000c9355704" if student == "Jane Doe" and section == "Chemistry I - A"
+  uri = "4efb5272-bc49-f388-0000-0000c9355705" if student == "Albert Wright" and section == "Chemistry I - A"
+  uri = "4efb4b14-bc49-f388-0000-0000c9355702" if student == "Kevin Smith" and section == "Chemistry I - A"
+  uri = "4efb7614-8488-7b01-0000-000059f9ba55" if student == "Albert Wright" and section == "Foreign Language - A"
   uri
+end
+
+#transform to student section associations by student OR section
+Transform /^Student Section Associations for the (Student|Section) "([^"]*)"$/ do |type, id|
+  puts "called with type = #{type} and id = #{id}"
+  Transform type + ' "' + id + '"'
 end
 
 #vnd.slc+json format is not ready for testing
@@ -45,7 +58,13 @@ end
 
 #Whens
 
-When /^I navigate to GET (Student Section Association for Student "[^"]*" and Section "[^"]*")$/ do |uri|
+When /^I navigate to (GET|DELETE|PUT) (Student Section Association for Student "[^"]*" and Section "[^"]*")$/ do |method, uri|
+  puts "uri = #{uri}"
+  puts "method = #{method}"
+  step 'I navigate to ' + method +  ' "/student-section-associations/' + uri + '"'
+end
+
+When /^I navigate to GET (Student Section Associations for the (Student|Section) "[^"]*")$/ do |uri, student_or_section|
   puts "uri = #{uri}"
   step 'I navigate to GET "/student-section-associations/' + uri + '"'
 end
--- sli/acceptance-tests/test/features/api/associations/section_student_association/student_section_association.feature
+++ sli/acceptance-tests/test/features/api/associations/section_student_association/student_section_association.feature
@@ -20,15 +20,14 @@ Then I should receive a return code of 201
 
 Scenario: Read a student-section-association
 Given format "application/vnd.slc+json"
-When I navigate to GET Student Section Association for Student "Jane Doe" and Section "Biology II - C"
+When I navigate to GET Student Section Association for Student "Albert Wright" and Section "Foreign Language - A"
 Then I should receive a return code of 200
 	And I should receive 1 student-section-assoications
-	And I should receive a link named "getStudent" with URI for Student "Jane Doe"
-	And I should receive a link named "getSection" with URI for Section "Biology II - C"
+	And I should receive a link named "getStudent" with URI for Student "Albert Wright"
+	And I should receive a link named "getSection" with URI for Section "Foreign Language - A"
 	And the begin date should be "09/15/2011"
 	And the end date should be "12/15/2011"
-	And the sequence of course should be 1
-	And the Repeat Identifier should be 1
+	And the Repeat Identifier should be "REPEATED_COUNTED_IN_GRADE_POINT_AVERAGE"
 
 Scenario: Reading a student-section-association for a student
 Given format "application/vnd.slc+json"
@@ -53,18 +52,16 @@ Then I should receive a return code of 200
 
 Scenario: Update a student-section-association 
 Given  format "application/vnd.slc+json"
-And I navigate to GET Student Section Associations for the Section "Biology II - C" and Student "Jane Doe"
-	#And Medium of Instruction is "Center-Based"
-When I set the Medium of Instruction to "Correspondence Instruction"
-	And I navigate to PUT /student-section-associations/<the previous association ID>
-Then I should get a return code of 2xx
-	And I navigate to GET /student-section-associations/<the previous association ID>
-	#And the Medium of Instruction should be "Correspondence Instruction"
+	And I navigate to GET Student Section Association for Student "Albert Wright" and Section "Foreign Language - A" 
+When I set Repeat Identifier to "NOT_REPEATED"
+	And I navigate to PUT Student Section Association for Student "Albert Wright" and Section "Foreign Language - A"
+Then I should receive a return code of 204
+When I navigate to GET Student Section Association for Student "Albert Wright" and Section "Foreign Language - A"
+Then Repeat Identifier should be "NOT_REPEATED"
 
 Scenario: Delete a student-section-association
 Given format "application/vnd.slc+json"
-And I navigate to DELETE /student-section-associations/<the previous association Id>
-Then I should get a return code of 2xx
-	And I navigate to GET /student-section-associations/<the previous association Id>
+And I navigate to DELETE Student Section Association for Student "Albert Wright" and Section "Foreign Language - A"
+Then I should receive a return code of 204
+	And I navigate to GET Student Section Association for Student "Albert Wright" and Section "Foreign Language - A"
 	And I should receive a return code of 404
-
