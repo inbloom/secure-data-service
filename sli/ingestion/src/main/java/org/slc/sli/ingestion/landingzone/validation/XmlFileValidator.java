1492610b403b8d420bf8cefbf100aa2a262a25ae
Danilo Duran <dduran@wgen.net>
2011-12-29 09:27:03 -0500

    add validation for empty or unreadable file to these validators.

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/XmlFileValidator.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/landingzone/validation/XmlFileValidator.java
@@ -1,11 +1,15 @@
 package org.slc.sli.ingestion.landingzone.validation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
 import org.slc.sli.ingestion.validation.ErrorReport;
 import org.slc.sli.ingestion.validation.SimpleValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Validator for EdFi xml ingestion files.
@@ -18,9 +22,34 @@ public class XmlFileValidator extends SimpleValidator<IngestionFileEntry> {
     private static final Logger LOG = LoggerFactory.getLogger(XmlFileValidator.class);
 
     @Override
-    public boolean isValid(IngestionFileEntry object, ErrorReport callback) {
-        LOG.info("validating...");
-        return false;
+    public boolean isValid(IngestionFileEntry fileEntry, ErrorReport errorReport) {
+        LOG.debug("validating csv...");
+
+        if (isEmptyOrUnreadable(fileEntry, errorReport))
+            return false;
+
+        return true;
+    }
+
+    private boolean isEmptyOrUnreadable(IngestionFileEntry fileEntry, ErrorReport errorReport) {
+        boolean isEmpty = false;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileEntry.getFile()));
+            if (br.read() == -1) {
+                errorReport.fatal("Input file is empty: " + fileEntry.getFileName(), CsvFileValidator.class);
+                isEmpty = true;
+            }
+
+        } catch (FileNotFoundException e) {
+            LOG.error("File not found: " + fileEntry.getFileName());
+            errorReport.error("File not found: " + fileEntry.getFileName(), CsvFileValidator.class);
+            isEmpty = true;
+        } catch (IOException e) {
+            LOG.error("Problem reading file: " + fileEntry.getFileName());
+            errorReport.error("Problem reading file: " + fileEntry.getFileName(), CsvFileValidator.class);
+            isEmpty = true;
+        }
+        return isEmpty;
     }
 
 }
