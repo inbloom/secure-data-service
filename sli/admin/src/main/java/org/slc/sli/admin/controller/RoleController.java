8d7bf78cb57420de642976f79cbc49d7e741f15a
scole <scole@wgen.net>
2011-12-29 14:36:55 -0500

    Adding authentication and real API calls

--- sli/admin/src/main/java/org/slc/sli/admin/controller/RoleController.java
+++ sli/admin/src/main/java/org/slc/sli/admin/controller/RoleController.java
@@ -6,14 +6,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 
@@ -23,7 +25,6 @@ import com.google.gson.JsonObject;
 @Controller
 @RequestMapping("/roles")
 public class RoleController extends AdminController {
-
     
     /**
      * Queries the JSON data containing the role info and passes that to the
@@ -31,48 +32,49 @@ public class RoleController extends AdminController {
      * 
      * @param view
      * @return a view containing the "roleJsonData" object
+     * @throws NoSessionException
      */
     @RequestMapping(method = RequestMethod.GET)
-    protected ModelAndView getRoles(ModelAndView view) {
-        //TODO: replace null with a real username/token
-        JsonArray jsonRoles = this.restClient.getRoles(null);
+    protected ModelAndView getRoles(ModelAndView view, HttpSession session) {
+        JsonArray jsonRoles = this.restClient.getRoles(getToken(session));
+        
         List<Map<String, Object>> roleList = new ArrayList<Map<String, Object>>();
-
+        
         for (Iterator<?> i = jsonRoles.iterator(); i.hasNext();) {
             JsonObject jsonRole = (JsonObject) i.next();
             Map<String, Object> roleData = new HashMap<String, Object>();
             
-            //Name
+            // Name
             roleData.put("name", jsonRole.get("name").getAsString());
             
-            //Aggregate
-            List<String> rights = (new Gson()).fromJson(jsonRole.get("rights").getAsJsonArray(), 
+            // Aggregate
+            @SuppressWarnings("unchecked")
+            List<String> rights = (new Gson()).fromJson(jsonRole.get("rights").getAsJsonArray(),
                     (new ArrayList<String>()).getClass());
             roleData.put("aggregate", rights.contains("AGGREGATE_READ"));
             
-            //Individual
-            roleData.put("individual", "");   //TODO:  Not currently set in data
+            // Individual
+            roleData.put("individual", "");   // TODO: Not currently set in data
             
-            //General student data
+            // General student data
             String generalRight = null;
             if (rights.contains("READ_GENERAL") && rights.contains("WRITE_GENERAL"))
                 generalRight = "R/W";
             else if (rights.contains("READ_GENERAL"))
                 generalRight = "R";
             else if (rights.contains("WRITE_GENERAL"))
-                generalRight = "W"; //This possible?
+                generalRight = "W"; // This possible?
             roleData.put("general", generalRight);
             
-            //Restircted student data
+            // Restricted student data
             String restrictedRight = null;
             if (rights.contains("READ_RESTRICTED") && rights.contains("WRITE_RESTRICTED"))
                 restrictedRight = "R/W";
             else if (rights.contains("READ_RESTRICTED"))
                 restrictedRight = "R";
             else if (rights.contains("WRITE_RESTRICTED"))
-                restrictedRight = "W"; //This possible?
+                restrictedRight = "W"; // This possible?
             roleData.put("restricted", restrictedRight);
-            
             
             roleList.add(roleData);
         }
@@ -80,5 +82,5 @@ public class RoleController extends AdminController {
         view.addObject("roleJsonData", roleList);
         return view;
     }
-
+    
 }
