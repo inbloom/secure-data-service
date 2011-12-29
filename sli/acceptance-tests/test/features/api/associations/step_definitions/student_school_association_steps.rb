2fdc7f7ba0205ae3df2f6a3b2e07e92d562d58bd
James Short <jshort@wgen.net>
2011-12-29 13:05:32 -0500

    Updated SSA tests

--- sli/acceptance-tests/test/features/api/associations/step_definitions/student_school_association_steps.rb
+++ sli/acceptance-tests/test/features/api/associations/step_definitions/student_school_association_steps.rb
@@ -5,6 +5,16 @@ require 'rexml/document'
 include REXML
 require_relative '../../../utils/sli_utils.rb'
 
+Transform /^\/student-school-associations\/<([^"]*)>$/ do |step_arg|
+  s = "/student-school-associations/"
+  id = s+"eb3b8c35-f582-df23-e406-6947249a19f2" if step_arg == "Apple Alternative Elementary School ID"
+  id = s+"714c1304-8a04-4e23-b043-4ad80eb60992" if step_arg == "Alfonso's ID"
+  id = s+"122a340e-e237-4766-98e3-4d2d67786572" if step_arg == "Alfonso at Apple Alternative Elementary School ID"
+  id = s+"11111111-1111-1111-1111-111111111111" if step_arg == "Invalid ID"
+  id = s                                        if step_arg == "No GUID"
+  id
+end
+
 
 Given /^I have access to all students and schools$/ do
   idpLogin(@user,@passwd)
@@ -34,7 +44,7 @@ When /^I navigate to POST "([^"]*)"$/ do |uri|
   assert(@res != nil, "Response from rest-client POST is nil")
 end
 
-When /^I navigate to PUT "([^"]*)"$/ do |uri|
+When /^I navigate to PUT "(\/student-school-associations\/<[^"]*>)"$/ do |uri|
   restHttpGet(uri)
   assert(@res != nil, "Response from rest-client GET is nil")
   assert(@res.code == 200, "Return code was not expected: "+@res.code.to_s+" but expected 200")
@@ -54,7 +64,7 @@ When /^I navigate to PUT "([^"]*)"$/ do |uri|
     assert(@res != nil, "Response from rest-client PUT is nil")
 end
 
-When /^I attempt to update a non\-existing association "([^"]*)"$/ do |uri|
+When /^I attempt to update a non\-existing association "(\/student-school-associations\/<[^"]*>)"$/ do |uri|
   data = {}
   restHttpPut(uri, data.to_json)
   assert(@res != nil, "Response from rest-client PUT is nil")
