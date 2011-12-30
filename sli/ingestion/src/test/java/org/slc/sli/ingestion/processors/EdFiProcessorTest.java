e76cb64f5f1f20b44ef845cbc0af8241ba0cd8b4
Danilo Duran <dduran@wgen.net>
2011-12-30 09:28:20 -0500

    remove unused import

--- sli/ingestion/src/test/java/org/slc/sli/ingestion/processors/EdFiProcessorTest.java
+++ sli/ingestion/src/test/java/org/slc/sli/ingestion/processors/EdFiProcessorTest.java
@@ -1,7 +1,6 @@
 package org.slc.sli.ingestion.processors;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
