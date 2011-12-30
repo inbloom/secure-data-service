c3b8ebf15d5f4607dd78c13476fabcf2f2a39734
Stanley Halka <shalka@wgen.net>
2011-12-30 11:17:41 -0500

    inject administrator security context prior to running tests

--- sli/api/src/test/java/org/slc/sli/api/service/EntityServiceLayerTest.java
+++ sli/api/src/test/java/org/slc/sli/api/service/EntityServiceLayerTest.java
@@ -22,6 +22,7 @@ import org.slc.sli.api.config.AssociationDefinition;
 import org.slc.sli.api.config.EntityDefinition;
 import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.resources.SecurityContextInjection;
 import org.slc.sli.dal.repository.EntityRepository;
 import org.slc.sli.validation.EntityValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,6 +50,9 @@ public class EntityServiceLayerTest {
     
     @Before
     public void setUp() {
+        // inject administrator security context for unit testing
+        SecurityContextInjection.setAdminContext();
+        
         defs.init();
         repo.deleteAll("student");
         repo.deleteAll("school");
@@ -259,14 +263,14 @@ public class EntityServiceLayerTest {
         assoc1.put("startDate", (new Date()).getTime());
         studentSchoolAssociationService.create(assoc1);
     }
-
+    
     // test delete source entity also remove association entity
     @Test(expected = EntityNotFoundException.class)
     public void testDeleteWithAssoc1() {
         Map<String, String> ids = setupTestDeleteWithAssoc();
         String studentId = ids.get("studentId");
         String assocId = ids.get("assocId");
-
+        
         EntityBody assocEntity = studentSchoolAssociationService.get(assocId);
         assertNotNull(assocEntity);
         assertEquals(assocEntity.get("studentId"), studentId);
@@ -291,7 +295,7 @@ public class EntityServiceLayerTest {
         // studentSchoolAssociationService.get(assocId);
         studentSchoolAssociationService.get(assoc2Id);
     }
-
+    
     private <T> List<T> iterableToList(Iterable<T> itr) {
         List<T> result = new ArrayList<T>();
         for (T item : itr) {
@@ -312,8 +316,6 @@ public class EntityServiceLayerTest {
         student1.put("lastName", "Doe");
         String student2Id = studentService.create(student2);
         
-        
-        
         EntityBody school = new EntityBody();
         school.put("name", "Battle School");
         String schoolId = schoolService.create(school);
@@ -333,11 +335,11 @@ public class EntityServiceLayerTest {
         ids.put("studentId", studentId);
         ids.put("schoolId", schoolId);
         ids.put("assocId", assocId);
-
+        
         ids.put("student2Id", student2Id);
         ids.put("assoc2Id", assoc2Id);
-
+        
         return ids;
     }
-
+    
 }
