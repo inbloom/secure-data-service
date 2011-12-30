251454d36bdf67af412b5b32964ef0829b22f8f8
Matthew Lane <lanemt@gmail.com>
2011-12-30 16:04:30 -0500

    added session logout

--- sli/api/src/main/java/org/slc/sli/api/security/SliRequestFilter.java
+++ sli/api/src/main/java/org/slc/sli/api/security/SliRequestFilter.java
@@ -1,34 +1,31 @@
 package org.slc.sli.api.security;
 
-import java.io.IOException;
-import java.util.Enumeration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import java.io.IOException;
 
 /**
  * A security filter responsible for checking SLI session
- * 
+ *
  * @author dkornishev
- * 
  */
 public class SliRequestFilter extends GenericFilterBean {
-    
-    private static final Logger   LOG                 = LoggerFactory.getLogger(SliRequestFilter.class);
-    
-    private static final String   PARAM_SESSION       = "sessionId";
-    private static final String   HEADER_SESSION_NAME = "sessionId";
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(SliRequestFilter.class);
+
+    private static final String PARAM_SESSION = "sessionId";
+    private static final String HEADER_SESSION_NAME = "sessionId";
+
     private SecurityTokenResolver resolver;
-    
+
     /**
      * Intercepter method called by spring
      * Checks cookies to see if SLI session id exists
@@ -36,37 +33,34 @@ public class SliRequestFilter extends GenericFilterBean {
      */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        
-        String sessionId = null;
-        
+
         @SuppressWarnings("unchecked")
-        Enumeration<String> e = req.getHeaderNames();
-        
-        while (e.hasMoreElements()) {
-            String header = e.nextElement();
-            String headerValue = req.getHeader(header);
-            
-            LOG.debug("[H]" + header + "->" + headerValue);
-            
-            if (HEADER_SESSION_NAME.equalsIgnoreCase(header)) {
-                sessionId = headerValue;
-                break;
-            }
-            
-        }
-        
-        if (req.getParameter(PARAM_SESSION) != null) {
-            sessionId = req.getParameter(PARAM_SESSION);
-        }
-        
+        String sessionId = getSessionIdFromRequest((HttpServletRequest) request);
+
         if (sessionId != null) {
             SecurityContextHolder.getContext().setAuthentication(resolver.resolve(sessionId));
         }
-        
+
         chain.doFilter(request, response);
     }
-    
+
+    protected String getSessionIdFromRequest(HttpServletRequest req) {
+
+        String sessionId = "";
+
+        if (requestContainsSessionParam(req)) {
+            sessionId = req.getParameter(PARAM_SESSION);
+        } else {
+            sessionId = req.getHeader(HEADER_SESSION_NAME);
+        }
+
+        return sessionId;
+    }
+
+    private boolean requestContainsSessionParam(HttpServletRequest req) {
+        return req.getParameter(PARAM_SESSION) != null;
+    }
+
     public void setResolver(SecurityTokenResolver resolver) {
         this.resolver = resolver;
     }
