977e50e3bfb651dc9e4638354020093fedd44e3e
James Short <jshort@wgen.net>
2012-01-04 13:29:57 -0500

    Refactor association file structure before final delete

--- sli/acceptance-tests/test/features/api/association/teacher_section_association/step_definitions/teacher_section_association_steps.rb
+++ sli/acceptance-tests/test/features/api/association/teacher_section_association/step_definitions/teacher_section_association_steps.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'builder'
 require 'rexml/document'
 include REXML
-require_relative '../../../utils/sli_utils.rb'
+require_relative '../../../../utils/sli_utils.rb'
 
 Transform /([^\/]*)\/<([^>]*)>$/ do |arg1, arg2|
   uri = arg1+"\/eb424dcc-6cff-a69b-c1b3-2b1fc86b2c94" if arg2 == "Ms. Jones' ID"
@@ -16,6 +16,66 @@ Transform /([^\/]*)\/<([^>]*)>$/ do |arg1, arg2|
   uri = arg1+"" if arg2 == ""
   uri = arg1+arg2 if uri == nil
   uri
+end
+
+Transform /^href ends with "([^"]*)<([^"]*)>"$/ do |arg1, arg2|
+  uri = arg1+"122a340e-e237-4766-98e3-4d2d67786572" if arg2 == "Alfonso at Apple Alternative Elementary School ID"
+  uri = arg1+"53ec02fe-570b-4f05-a351-f8206b6d552a" if arg2 == "Gil at Apple Alternative Elementary School ID"
+  uri = arg1+"4ef1498f-5dfc-4604-83c3-95e81146b59a" if arg2 == "Sybill at Apple Alternative Elementary School ID"
+  uri = arg1+"6de7d3b6-54d7-48f0-92ad-0914fe229016" if arg2 == "Alfonso at Yellow Middle School ID"
+  uri = arg1+"714c1304-8a04-4e23-b043-4ad80eb60992" if arg2 == "Alfonso's ID"
+  uri = arg1+"eb3b8c35-f582-df23-e406-6947249a19f2" if arg2 == "Apple Alternative Elementary School ID"
+  uri = arg1+"eb424dcc-6cff-a69b-c1b3-2b1fc86b2c94" if arg2 == "Ms. Jones' ID"
+  uri = arg1+"58c9ef19-c172-4798-8e6e-c73e68ffb5a3" if arg2 == "Algebra II ID"
+  uri = arg1+"12f25c0f-75d7-4e45-8f36-af1bcc342871" if arg2 == "Association between Ms. Jones and Algebra II ID"
+  uri = arg1+"" if arg2 == ""
+  uri = arg1+arg2 if uri == nil
+  uri
+end
+
+Given /^I am logged in using "([^"]*)" "([^"]*)"$/ do |arg1, arg2|
+  @user = arg1
+  @passwd = arg2
+end
+
+Given /^that I have admin access$/ do
+  idpLogin(@user,@passwd)
+  assert(@cookie != nil, "Cookie retrieved was nil")
+end
+
+Given /^format "([^"]*)"$/ do |fmt|
+  @format = fmt
+end
+
+Given /^"([^"]*)" is "([^"]*)"$/ do |key, value|
+  if !defined? @fields
+    @fields = {}
+  end
+  @fields[key] = value
+end
+
+Then /^I should receive a return code of (\d+)$/ do |code|
+  assert(@res.code == Integer(code), "Return code was not expected: #{@res.code.to_s} but expected #{code}\nbody was #{@res}")
+end
+
+Then /^I should receive a link where rel is "([^"]*)" and (href ends with "[^"]*")$/ do |rel, href|
+  assert(@data != nil, "Response contains no data")
+  assert(@data.is_a?(Hash), "Response contains #{@data.class}, expected Hash")
+  assert(@data.has_key?("links"), "Response contains no links")
+  found = false
+  @data["links"].each do |link|
+    if link["rel"] == rel && link["href"] =~ /#{Regexp.escape(href)}$/
+      found = true
+    end
+  end
+  assert(found, "Link not found rel=#{rel}, href ends with=#{href}")
+end
+
+Then /^"([^"]*)" should equal "([^"]*)"$/ do |key, value|
+  assert(@data != nil, "Response contains no data")
+  assert(@data.is_a?(Hash), "Response contains #{@data.class}, expected Hash")
+  assert(@data.has_key?(key), "Response does not contain key #{key}")
+  assert(@data[key].to_s == value, "Expected #{key} to equal #{value}, received #{@data[key]}")
 end
 
 When /^I navigate to POST "([^"]*)"$/ do |uri|
