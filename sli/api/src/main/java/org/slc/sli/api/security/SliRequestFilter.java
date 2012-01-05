aa6f8b0689d65955e0c5d01c2203e50178800025
Danil Kornishev <dkornishev@wgen.net>
2012-01-05 15:12:07 -0500

    fix for empty string as sessionId

--- sli/api/src/main/java/org/slc/sli/api/security/SliRequestFilter.java
+++ sli/api/src/main/java/org/slc/sli/api/security/SliRequestFilter.java
@@ -18,22 +18,22 @@ import java.io.IOException;
 
 /**
  * A security filter responsible for checking SLI session
- *
+ * 
  * @author dkornishev
  */
 @Component
 public class SliRequestFilter extends GenericFilterBean {
-
-    private static final Logger LOG = LoggerFactory.getLogger(SliRequestFilter.class);
-    private static final String PARAM_SESSION = "sessionId";
-    private static final String HEADER_SESSION_NAME = "sessionId";
-
+    
+    private static final Logger   LOG                 = LoggerFactory.getLogger(SliRequestFilter.class);
+    private static final String   PARAM_SESSION       = "sessionId";
+    private static final String   HEADER_SESSION_NAME = "sessionId";
+    
     @Resource(name = "openamRestTokenResolver")
     private SecurityTokenResolver resolver;
-
+    
     @Value("${sli.security.noSession.landing.url}")
-    private String realmSelectionUrl;
-
+    private String                realmSelectionUrl;
+    
     /**
      * Intercepter method called by spring
      * Checks cookies to see if SLI session id exists
@@ -41,44 +41,42 @@ public class SliRequestFilter extends GenericFilterBean {
      */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+        
         String sessionId = getSessionIdFromRequest((HttpServletRequest) request);
         
-        if (sessionId != null) {
+        if (sessionId != null && !"".equals(sessionId)) {
             SecurityContextHolder.getContext().setAuthentication(resolver.resolve(sessionId));
-        }
-        else {
+        } else {
             LOG.warn("Unauthorized access");
             if (!(((HttpServletRequest) request).getRequestURL().toString().contains("system/session/check"))) {
-
-                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-                ((HttpServletResponse)response).setHeader("WWW-Authenticate", this.realmSelectionUrl);
+                
+                ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                ((HttpServletResponse) response).setHeader("WWW-Authenticate", this.realmSelectionUrl);
                 return;
             }
-
+            
         }
-
+        
         chain.doFilter(request, response);
     }
-
+    
     private String getSessionIdFromRequest(HttpServletRequest req) {
-
+        
         String sessionId = "";
-
+        
         if (requestContainsSessionParam(req)) {
             sessionId = req.getParameter(PARAM_SESSION);
         } else {
             sessionId = req.getHeader(HEADER_SESSION_NAME);
         }
-
+        
         return sessionId;
     }
-
+    
     private boolean requestContainsSessionParam(HttpServletRequest req) {
         return req.getParameter(PARAM_SESSION) != null;
     }
-
+    
     public void setResolver(SecurityTokenResolver resolver) {
         this.resolver = resolver;
     }
