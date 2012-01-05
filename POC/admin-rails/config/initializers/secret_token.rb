d9f253855e0d0371e196ccb299aca0000a2b0ffb
Ryan Latta <rlatta@wgen.net>
2012-01-05 13:29:06 -0500

    Adding RoR concept for admin app.

---
+++ POC/admin-rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SLIAdmin::Application.config.secret_token = '96940e00cba2e4ffd82e53172d03b6d1c2208fa9d055d66e2acc7c608b76e57b200d015ae2d3335dad457b61ad7bcc6d8f69280fb8dac375a8defcaecb5b3842'
