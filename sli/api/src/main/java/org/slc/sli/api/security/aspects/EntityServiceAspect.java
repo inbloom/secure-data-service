fd5657f31264924ec51ff8dce068f12603af07ea
Matthew Lane <lanemt@gmail.com>
2012-01-06 00:31:08 -0500

    authorizing read and writes on CoreEntitiyService

--- sli/api/src/main/java/org/slc/sli/api/security/aspects/EntityServiceAspect.java
+++ sli/api/src/main/java/org/slc/sli/api/security/aspects/EntityServiceAspect.java
@@ -1,20 +1,11 @@
 package org.slc.sli.api.security.aspects;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.avro.Schema;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slc.sli.api.security.SLIPrincipal;
 import org.slc.sli.api.security.enums.Right;
-import org.slc.sli.api.service.EntityService;
 import org.slc.sli.domain.Entity;
 import org.slc.sli.validation.EntitySchemaRegistry;
 import org.slf4j.Logger;
@@ -23,8 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import java.util.Iterator;
+import java.util.Set;
 
 // add this import if we move to CoreEntityService paradigm
 // import org.slc.sli.api.service.CoreEntityService;
@@ -43,16 +34,22 @@ public class EntityServiceAspect {
     @Autowired
     private EntitySchemaRegistry mySchemaRegistry;
 
-    private static List<String> methodsAlwaysAllow = Arrays.asList("getEntityDefinition");
-    private static Map<String, Right> neededRights = new HashMap<String, Right>();
+    /**
+     * Controls access to functions in the EntityService class.
+     *
+     * @param pjp Method invoked if principal has required rights.
+     * @return Entity returned from invoked method (if method is entered).
+     * @throws Throwable AccessDeniedException (HTTP 403).
+     */
+    @Around("call(* org.slc.sli.api.service.CoreEntityService.create(..)) || " +
+            "call(* org.slc.sli.api.service.CoreEntityService.create(..)) || " +
+            "call(* org.slc.sli.api.service.CoreEntityService.delete(..))")
+    public Object authorizeWrite(ProceedingJoinPoint pjp) throws Throwable {
+        if (!isPublicContext() && !getGrantedRights().contains(Right.WRITE_GENERAL)) {
+            throwAccessDeniedException();
+        }
 
-    static {
-        neededRights.put("get", Right.READ_GENERAL);
-        neededRights.put("list", Right.READ_GENERAL);
-        neededRights.put("exists", Right.READ_GENERAL);
-        neededRights.put("create", Right.WRITE_GENERAL);
-        neededRights.put("update", Right.WRITE_GENERAL);
-        neededRights.put("delete", Right.WRITE_GENERAL);
+        return pjp.proceed();
     }
 
     /**
@@ -62,59 +59,14 @@ public class EntityServiceAspect {
      * @return Entity returned from invoked method (if method is entered).
      * @throws Throwable AccessDeniedException (HTTP 403).
      */
-    @Around("call(* EntityService.*(..)) && !within(EntityServiceAspect) && !call(* EntityService.getEntityDefinition(..))")
-    public Object controlAccess(ProceedingJoinPoint pjp) throws Throwable {
-        boolean hasAccess = false;
-        Right neededRight = null;
-        Signature entitySignature = pjp.getSignature();
-        String entityFunctionName = entitySignature.getName();
-
-        EntityService service = (EntityService) pjp.getTarget();
-        String entityDefinitionType = service.getEntityDefinition().getType();
-
-        if (isWhiteListed(entityDefinitionType, entityFunctionName)) {
-            LOG.debug("granting access to user for entity");
-            hasAccess = true;
-        } else {
-            neededRight = neededRights.get(entityFunctionName);
-
-            LOG.debug("attempted access of {} function", entitySignature.toString());
-
-            Set<Right> myRights = getGrantedRights();
-            LOG.debug("user rights: {}", myRights.toString());
-
-            for (Right currentRight : myRights) {
-                if (currentRight.equals(neededRight)) {
-                    LOG.debug("granting access to user for entity");
-                    hasAccess = true;
-                    break;
-                }
-            }
-        }
-
-        if (hasAccess) {
-            LOG.debug("entering {} function [using Around]", entitySignature.toString());
-            Object entityReturned = pjp.proceed();
-            LOG.debug("exiting {} function [using Around]", entitySignature.toString());
-            return entityReturned;
-        } else {
-            LOG.debug("user was denied access due to insufficient permissions.");
-            throw new AccessDeniedException("User does not have authority to access entity.");
-        }
-    }
-
-    private boolean isWhiteListed(String entityDefinitionType, String entityFunctionName) {
-        boolean isPub = SecurityContextHolder.getContext().getAuthentication() == null;
-
-        return isPub || methodsAlwaysAllow.contains(entityFunctionName);
-    }
-
     @Around("call(* org.slc.sli.api.service.CoreEntityService.get(..))")
     public Entity filterEntityRead(ProceedingJoinPoint pjp) throws Throwable {
-        LOG.debug("[ASPECT] filtering read");
+        if (!isPublicContext() && !getGrantedRights().contains(Right.READ_GENERAL)) {
+            throwAccessDeniedException();
+        }
         Entity entity = (Entity) pjp.proceed();
 
-        if (entity != null && !isWhiteListed(entity.getType(), pjp.getSignature().getName())) {
+        if (entity != null && !isPublicContext()) {
             Set<Right> grantedRights = getGrantedRights();
             LOG.debug("Rights {}", grantedRights);
 
@@ -129,6 +81,15 @@ public class EntityServiceAspect {
         }
 
         return entity;
+    }
+
+    private void throwAccessDeniedException() {
+        LOG.debug("user was denied access due to insufficient permissions.");
+        throw new AccessDeniedException("User does not have authority to access entity.");
+    }
+
+    private boolean isPublicContext() {
+        return SecurityContextHolder.getContext().getAuthentication() == null;
     }
 
     private void filterReadGeneral(Entity entity) {
