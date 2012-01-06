0daaa9e2e0ac281d38bff2c0917aa047bbedf827
James Short <jshort@wgen.net>
2012-01-06 10:38:11 -0500

    more debug for jenkins

--- sli/acceptance-tests/test/features/apiSelenium/step_definitions/selenium_common.rb
+++ sli/acceptance-tests/test/features/apiSelenium/step_definitions/selenium_common.rb
@@ -2,6 +2,7 @@ Given /^I have an open web browser$/ do
   @driver = Selenium::WebDriver.for :firefox
 end
 
-After do |scenario|
+After do |scenario| 
+  puts "Running the After hook"
   @driver.quit if @driver
 end
