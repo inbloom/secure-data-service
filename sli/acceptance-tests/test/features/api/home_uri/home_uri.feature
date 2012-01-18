6645bb48f888660219af9cf7957c709c48d40636
William Scott <wscott@wgen.net>
2012-01-18 13:07:15 -0500

    Update Home URI acceptance tests.

--- sli/acceptance-tests/test/features/api/home_uri/home_uri.feature
+++ sli/acceptance-tests/test/features/api/home_uri/home_uri.feature
@@ -1,26 +1,47 @@
-@wip
 Feature: In order to provide base information
 	As a client application using SLI
 	I want to know what links are available to a user based on their user type.
 	This means all associations should be returned as links when accessing the HOME URI.
 
 Background: Logged in as a super-user and using the small data set
-	Given I am logged in using "demo" "demo1234"
 
 Scenario: MOCK Home URI returns a valid response
-	Given format "application/json"
-		And mock student ID <mock ID>
-	When I navigate to GET <home URI>
+  Given I am logged in using "demo" "demo1234"
+    And I have access to all teachers
+    And format "application/json"
+    And mock student ID <'mock' ID>
+	When I navigate to GET "/home"
 	Then I should receive a return code of 200
 		And I should receive a link where rel is "self" and href ends with "/students/" and appropriate ID
 		And I should receive a link where rel is "getStudentSchoolAssociations" and href ends with "/student-school-associations/" and appropriate ID
 
 Scenario: Previous functionality which is now shared logic continues to work as expected
 	Given format "application/json"
-		And mock student ID <mock ID>
+		And mock student ID <'mock' ID>
 	When I navigate to GET <student by ID>
 	Then I should receive a return code of 200
 		And I should receive a link where rel is "self" and href ends with "/students/" and appropriate ID
 		And I should receive a link where rel is "getStudentSchoolAssociations" and href ends with "/student-school-associations/" and appropriate ID
+
+Scenario: HomeURI returns valid links for an Administrator
+  Given I am logged in using "administrator" "administrator1234"
+    And I have access to all teachers
+    And format "application/json"
+  When I navigate to GET "/home"
+  Then I should receive a return code of 200
+    And I should receive a link named "self" with URI "/teachers/<'administrator' ID>"
+    And I should receive a link named "getTeacherSectionAssociations" with URI "/teacher-section-associations/<'administrator' ID>"
+
+
+Scenario: HomeURI returns valid links for a Teacher
+  Given I am logged in using "educator" "educator1234"
+    And I have access to all teachers
+    And format "application/json"
+  When I navigate to GET "/home"
+  Then I should receive a return code of 200
+    And I should receive a link named self with URI "/teachers/<'educator' ID>"
+
+
+
 
 #this is a test
--- sli/acceptance-tests/test/features/api/home_uri/step_definitions/home_uri.rb
+++ sli/acceptance-tests/test/features/api/home_uri/step_definitions/home_uri.rb
@@ -5,27 +5,45 @@ require 'rexml/document'
 include REXML
 require_relative '../../../utils/sli_utils.rb'
 
-Given /^I am logged in using "([^"]*)" "([^"]*)"$/ do |usr, pass|
-  idpLogin(usr, pass)
-  assert(@sessionId != nil, "Session returned was nil")
+
+# transform <Place Holder Id>
+Transform /^<.+>$/ do |template|
+  id = template
+  id = "eb4d7e1b-7bed-6dd7-d9b4-1d729a37fd2d" if template == "<'administrator' ID>";
+  id = "714c1304-8a04-4e23-b043-4ad80eb60992" if template == "<'mock' ID>"
+  id = "fa45033c-5517-b14b-1d39-c9442ba95782" if template == "<'Macey' ID>";
+  id = "738543275"                            if template == "<'Macey Home State' ID>"
+  id = "344cf68d-50fd-8dd7-e8d6-ed9df76c219c" if template == "<'Belle' ID>"
+  id = "11111111-1111-1111-1111-111111111111" if template == "<Unknown>"
+  id = "824643f7-174b-4a50-9383-c9a6f762c49d" if template == "<'Christian' ID>"
+  id = "a249d5d9-f149-d348-9b10-b26d68e7cb9c" if template == "<'Illiana' ID>"
+  id
 end
 
-Given /^format "([^"]*)"$/ do |fmt|
-  @format = fmt
+# transform /path/<Place Holder Id>
+Transform /^(\/[\w-]+\/)(<.+>)$/ do |uri, template|
+  uri + Transform(template)
 end
+
+# transform /path/<Place Holder Id>/targets
+Transform /^(\/[\w-]+\/)(<.+>)\/targets$/ do |uri, template|
+  Transform(uri + template) + "/targets"
+end
+
 
 Given /^mock student ID (<[^"]*>)$/ do |mock_id|
   @mock_id = mock_id
 end
 
-Transform /^<([^"]*)>$/ do |step_arg|
-  id = "714c1304-8a04-4e23-b043-4ad80eb60992"  if step_arg == "mock ID"
-  id = "/home"                                 if step_arg == "home URI"
-  id = "/students/"+@mock_id                   if step_arg == "student by ID"
-  id
-end
+#Transform /^<([^"]*)>$/ do |step_arg|
+#  id = "714c1304-8a04-4e23-b043-4ad80eb60992"  if step_arg == "mock ID"
+#  id = "/home"                                 if step_arg == "home URI"
+#  id = "/students/"+@mock_id                   if step_arg == "student by ID"
+#  id
+#end
 
 When /^I navigate to GET (<[^"]*>)$/ do |uri|
+  print uri
   restHttpGet(uri)
   assert(@res != nil, "Response from rest-client GET is nil")
   if @format == "application/json"
@@ -41,10 +59,6 @@ When /^I navigate to GET (<[^"]*>)$/ do |uri|
   end
 end
 
-Then /^I should receive a return code of (\d+)$/ do |code|
-  assert(@res.code == Integer(code), "Return code was not expected: #{@res.code.to_s} but expected #{code}")
-end
-
 Then /^I should receive a link where rel is "([^"]*)" and href ends with "([^"]*)" and appropriate ID$/ do |rel, href|
   assert(@data != nil, "Response contains no data")
   assert(@data.is_a?(Hash), "Response contains #{@data.class}, expected Hash")
@@ -57,3 +71,40 @@ Then /^I should receive a link where rel is "([^"]*)" and href ends with "([^"]*
   end
   assert(found, "Link not found rel=#{rel}, href ends with=#{href}")
 end
+
+When /^I navigate to GET "([^\"]+)"$/ do |url|
+  restHttpGet(url)
+  assert(@res != nil, "Response from rest-client GET is nil")
+  assert(@res.body != nil, "Response body is nil")
+  contentType = contentType(@res)
+  if /application\/json/.match contentType
+    @result = JSON.parse(@res.body)
+  elsif /application\/xml/.match contentType
+    doc = Document.new @res.body
+    @result = doc.root
+    puts @result
+  else
+    @result = {}
+  end
+end
+
+Then /^I should receive a link named "([^\"]*)" with URI "([^\"]*)"$/ do |rel, href|
+  @result["links"].should_not == nil
+  found = false
+  @result["links"].each do |link|;
+    if link["rel"] == rel && link["href"] =~ /#{Regexp.escape(href)}$/
+      found = true
+      break
+    end
+  end
+  assert(found, "Did not find a link rel=#{rel} href=#{href}")
+end
+
+
+def contentType(response)
+  headers = @res.raw_headers
+  assert(headers != nil, "Headers are nil")
+  assert(headers['content-type'] != nil, "There is no content-type set in the response")
+  headers['content-type'][0]
+end
+
