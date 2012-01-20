eef6447aee7bc8a729b7bd7ad8649f9b3386c79c
James Short <jshort@wgen.net>
2012-01-20 14:05:57 -0500

    Update for camelcase assoc names

--- sli/acceptance-tests/test/data/demoPI2_data/demo_import_data.sh
+++ sli/acceptance-tests/test/data/demoPI2_data/demo_import_data.sh
@@ -12,7 +12,7 @@ echo $DIR
 
 $MONGO_HOME/mongoimport --drop -d $DB_HOST -c teacher -h $DB_HOST --file "$DIR/teacher_data.json"
 $MONGO_HOME/mongoimport --drop -d $DB_HOST -c section -h $DB_HOST --file "$DIR/section_data.json"
-$MONGO_HOME/mongoimport --drop -d $DB_HOST -c teacherschoolassociation -h $DB_HOST --file "$DIR/teacherSchoolAssociation_data.json"
-$MONGO_HOME/mongoimport --drop -d $DB_HOST -c teachersectionassociation -h $DB_HOST --file "$DIR/teacherSectionAssociation_data.json"
-$MONGO_HOME/mongoimport --drop -d $DB_HOST -c studentsectionassociation -h $DB_HOST --file "$DIR/studentSectionAssociation_data.json"
+$MONGO_HOME/mongoimport --drop -d $DB_HOST -c teacherSchoolAssociation -h $DB_HOST --file "$DIR/teacherSchoolAssociation_data.json"
+$MONGO_HOME/mongoimport --drop -d $DB_HOST -c teacherSectionAssociation -h $DB_HOST --file "$DIR/teacherSectionAssociation_data.json"
+$MONGO_HOME/mongoimport --drop -d $DB_HOST -c studentSectionAssociation -h $DB_HOST --file "$DIR/studentSectionAssociation_data.json"
 
