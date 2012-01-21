0784d361c015de35b9dc98c6cd1ae2b4a2157965
Igor Faybyshev <ifaybyshev@wgen.net>
2012-01-20 20:03:28 -0500

    ingestion test fix

--- sli/acceptance-tests/test/features/ingestion/sample_test/step_definitions/sample_test_crud.rb
+++ sli/acceptance-tests/test/features/ingestion/sample_test/step_definitions/sample_test_crud.rb
@@ -78,7 +78,7 @@ When /^zip file is scp to ingestion landing zone$/ do
 
   if (INGESTION_MODE == 'remote')
     # copy file from external path to landing zone
-    ingestion_server_string = "ingestion@" + INGESTION_SERVER_URL + ":" + @destination_path
+    ingestion_server_string = "ingestion@" + INGESTION_SERVER_URL + ":" + @landing_zone_path
     local_source_path = @source_path
     
     puts "Will Execute sh: " + "scp #{local_source_path} #{ingestion_server_string}"
