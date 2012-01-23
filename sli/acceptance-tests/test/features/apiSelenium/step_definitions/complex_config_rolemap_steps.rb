11ed2f1818c41153b258faa79cadfbc323fafc13
Mark Abernathy <mabernathy@wgen.net>
2012-01-23 17:22:39 -0500

    Added stepdefs for complex configurable rolemapping

---
+++ sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
@@ -0,0 +1,45 @@
+require "selenium-webdriver"
+
+require_relative '../../utils/sli_utils.rb'
+
+
+When /^I navigate to the SLI Role Mapping Admin Page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I choose the realm "([^"]*)" from the list of realms$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I map the default SLI role "([^"]*)" to the custom role "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see that "([^"]*)" is now mapped to the "([^"]*)" role$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a "([^"]*)" can now log in to SLI as a "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a "([^"]*)" has been mapped to the "([^"]*)" default role$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I remove the map from the default SLI role "([^"]*)" to the custom role "
+([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see that "([^"]*)" is no longer mapped to the "([^"]*)" role$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a "([^"]*)" can no longer access SLI as a "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not see the mapping from the other realm$/ do
+  pending # express the regexp above with the code you wish you had
+end
