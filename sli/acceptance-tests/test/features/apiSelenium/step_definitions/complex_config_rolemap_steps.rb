aae8fe1b765b87eb816bf790d1bae603dd712ba5
Mark Abernathy <mabernathy@wgen.net>
2012-01-27 09:25:24 -0500

    Updated complex configurable role mapping Gherkin

--- sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
+++ sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
@@ -2,13 +2,17 @@ require "selenium-webdriver"
 
 require_relative '../../utils/sli_utils.rb'
 
+Given /^I am from realm "([^"]*)"$/ do |arg1|
+  #No code needed, this is done as configuration
+end
 
-When /^I navigate to the SLI Role Mapping Admin Page$/ do
+Given /^another realm's admin mapped "([^"]*)" to the "([^"]*)" default SLI role$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
 
-When /^I choose the realm "([^"]*)" from the list of realms$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I navigate to the SLI Role Mapping Admin Page$/ do
+  url = PropLoader.getProps['admintools_server_url']+"/admin/role-mapping"
+  @driver.get url
 end
 
 When /^I map the default SLI role "([^"]*)" to the custom role "([^"]*)"$/ do |arg1, arg2|
@@ -19,7 +23,7 @@ Then /^I see that "([^"]*)" is now mapped to the "([^"]*)" role$/ do |arg1, arg2
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^a "([^"]*)" can now log in to SLI as a "([^"]*)"$/ do |arg1, arg2|
+Then /^a "([^"]*)" can now log in to SLI as a "([^"]*)" from my realm "([^"]*)"$/ do |arg1, arg2, arg3|
   pending # express the regexp above with the code you wish you had
 end
 
@@ -27,8 +31,7 @@ Given /^a "([^"]*)" has been mapped to the "([^"]*)" default role$/ do |arg1, ar
   pending # express the regexp above with the code you wish you had
 end
 
-When /^I remove the map from the default SLI role "([^"]*)" to the custom role "
-([^"]*)"$/ do |arg1, arg2|
+When /^I remove the map from the default SLI role "([^"]*)" to the custom role "([^"]*)"$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
 
@@ -36,7 +39,7 @@ Then /^I see that "([^"]*)" is no longer mapped to the "([^"]*)" role$/ do |arg1
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^a "([^"]*)" can no longer access SLI as a "([^"]*)"$/ do |arg1, arg2|
+Then /^a "([^"]*)" can not access SLI as a "([^"]*)" from my realm "([^"]*)"$/ do |arg1, arg2, arg3|
   pending # express the regexp above with the code you wish you had
 end
 
