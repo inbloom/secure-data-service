6e3ba085fb8db9badf2e37a164d68bdd902e0068
Ryan Latta <rlatta@wgen.net>
2012-01-30 17:08:04 -0500

    Working on updating the mapping resrouce.

--- sli/api/src/main/java/org/slc/sli/api/resources/security/ClientRoleManagerResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/security/ClientRoleManagerResource.java
@@ -1,21 +1,26 @@
 package org.slc.sli.api.resources.security;
 
-import javax.ws.rs.DELETE;
+import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.slc.sli.api.config.EntityDefinition;
+import org.slc.sli.api.config.EntityDefinitionStore;
+import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.service.EntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import org.slc.sli.api.resources.Resource;
-import org.slc.sli.api.security.resolve.ClientRoleManager;
-import org.slc.sli.api.security.resolve.RealmRoleMappingException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Realm role mapping API. Allows a user to define mappings between SLI roles
@@ -25,47 +30,64 @@ import org.slc.sli.api.security.resolve.RealmRoleMappingException;
  * 
  */
 @Component
-@Path("/pub/roles/mappings")
+@Path("/realms")
 @Scope("request")
 @Produces({ Resource.JSON_MEDIA_TYPE })
 public class ClientRoleManagerResource {
 
     @Autowired
-    private ClientRoleManager roleManager;
+    private EntityDefinitionStore store;
+    
+    private EntityService service;
 
-    @POST
-    public Response addClientRole(@QueryParam("realmId") String realmId,
-            @QueryParam("sliRole") String sliRoleName,
-            @QueryParam("clientRole") String clientRoleName) {
-        try {
-            if (roleManager.addClientRole(realmId, sliRoleName, clientRoleName)) {
-                return Response.ok().build();
-            } else {
-                return Response.status(Status.NOT_FOUND).build();
-            }
-        } catch (RealmRoleMappingException e) {
-            return Response.status(Status.FORBIDDEN).entity(e.getMessage())
-                    .build();
-        }
+    @PostConstruct
+    private void init() {
+        EntityDefinition def = store.lookupByResourceName("roles");
+        setService(def.getService());
     }
 
-    @DELETE
-    public Response deleteClientRole(@QueryParam("realmId") String realmId,
-            @QueryParam("clientRole") String clientRoleName) {
-        try {
-            if (roleManager.deleteClientRole(realmId, clientRoleName)) {
-                return Response.ok().build();
-            }
-        } catch (RealmRoleMappingException e) {
-            return Response.status(Status.FORBIDDEN).entity(e.getMessage())
-                    .build();
-        }
-        return Response.status(Status.NOT_FOUND).build();
+    //Injector
+    public void setStore(EntityDefinitionStore store) {
+        this.store = store;
     }
+
+    //Injector
+    public void setService(EntityService service) {
+        this.service = service;
+    }
+
+    @PUT
+    @RequestMapping("/realms/{realmId}")
+    public boolean addClientRole(@PathVariable("realmId") String realmId, String id, EntityBody updatedRealm) {
+        return service.update(id, updatedRealm);
+    }
+
+//    @DELETE
+//    @RequestMapping("/realms/{realmId}")
+//    public Response deleteClientRole(@PathVariable("realmId") String realmId,
+//            String clientRoleName, String sliRoleName) {
+//        try {
+//            if (roleManager.deleteClientRole(realmId, clientRoleName)) {
+//                return Response.ok().build();
+//            }
+//        } catch (RealmRoleMappingException e) {
+//            return Response.status(Status.FORBIDDEN).entity(e.getMessage())
+//                    .build();
+//        }
+//        return Response.status(Status.NOT_FOUND).build();
+//    }
 
     @GET
-    public Object getMappings(@QueryParam("realmId") String realmId,
-            @QueryParam("sliRole") String sliRoleName) {
-        return roleManager.getMappings(realmId, sliRoleName);
+    @RequestMapping("/realms/{realmId}")
+    public Object getMappings(@PathVariable("realmId") String realmId) {
+        //TODO This should be a query.
+        Iterable<String> ids = service.list(0, 100);
+        Iterable<EntityBody> mappings = service.get(ids);
+        for (EntityBody body : mappings) {
+            if (body.get("realm_name").equals(realmId)) {
+                return mappings;
+            }
+        }
+        return null;
     }
 }
---
+++ sli/api/src/test/java/org/slc/sli/api/resources/security/ClientRoleManagerResourceTest.java
@@ -0,0 +1,80 @@
+package org.slc.sli.api.resources.security;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.resources.SecurityContextInjector;
+import org.slc.sli.api.service.EntityService;
+import org.slc.sli.api.test.WebContextTestExecutionListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Simple test for ClientRoleManagerResource
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+@TestExecutionListeners({ WebContextTestExecutionListener.class, DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class })
+public class ClientRoleManagerResourceTest {
+    @Autowired
+    private ClientRoleManagerResource resource;
+
+    @Autowired
+    private SecurityContextInjector injector;
+    
+    private EntityService service;
+    private EntityBody mapping;
+
+    @Before
+    public void setUp() throws Exception {
+
+        injector.setAdminContext();
+        List<String> ids = new ArrayList<String>();
+        ids.add("1234");
+        ids.add("-1");
+        
+        mapping = new EntityBody();
+        mapping.put("id", "123567324");
+        mapping.put("realm_name", "Waffles");
+        
+        List<EntityBody> maps = new ArrayList<EntityBody>();
+        maps.add(mapping);
+
+        service = mock(EntityService.class);
+        when(service.update("-1", mapping)).thenReturn(true);
+        when(service.update("1234", new EntityBody())).thenReturn(true);
+        when(service.list(0, 100)).thenReturn(ids);
+        when(service.get(ids)).thenReturn(maps);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        service = null;
+    }
+
+    @Test
+    public void testAddClientRole() throws Exception {
+        assertFalse(resource.addClientRole("Peanuts", "-1", null));
+        assertTrue(resource.addClientRole("Waffles", "1234", mapping));
+    }
+
+    @Test
+    public void testGetMappings() throws Exception {
+        assertNotNull(resource.getMappings("Waffles"));
+        assertNull(resource.getMappings("Invalid"));
+    }
+}
