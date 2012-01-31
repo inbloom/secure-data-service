7e978f8006472359af3814efcc30cfb0ae6a3e2d
Simon Yau <syau@wgen.net>
2012-01-31 12:50:58 -0500

    Add a wait step for dashboard acceptance test for local authentication.

--- sli/acceptance-tests/test/features/dashboard/step_definitions/dashboard_login_steps.rb
+++ sli/acceptance-tests/test/features/dashboard/step_definitions/dashboard_login_steps.rb
@@ -23,6 +23,7 @@ Then /^I should be redirected to the Realm page$/ do
 end
 
 Given /^I am authenticated to SLI as "([^"]*)" password "([^"]*)"$/ do |username, password|
+  sleep 1
   localLogin(username, password)
 end
 
