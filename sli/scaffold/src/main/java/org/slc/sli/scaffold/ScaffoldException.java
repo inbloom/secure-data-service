05ab9174725617f14b5fca837cab9ec13d43a08c
Suneth Rupasinghe <srupasinghe@wgen.net>
2012-01-31 10:56:23 -0500

    initial commit of scaffold project

---
+++ sli/scaffold/src/main/java/org/slc/sli/scaffold/ScaffoldException.java
@@ -0,0 +1,13 @@
+package org.slc.sli.scaffold;
+
+/**
+ * Base exception for scaffold work
+ * @author srupasinghe
+ *
+ */
+public class ScaffoldException extends Exception {
+    
+    public ScaffoldException(Throwable e) {
+        super(e);
+    }
+}
