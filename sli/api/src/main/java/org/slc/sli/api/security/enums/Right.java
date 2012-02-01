d18969e6156936ed3ffaf6e3b28bfe6d01809fe5
Danil Kornishev <dkornishev@wgen.net>
2012-02-01 16:32:34 -0500

    changed to thread local storage for cached auth.  Added test for elevation/de-elevation of priviliges

--- sli/api/src/main/java/org/slc/sli/api/security/enums/Right.java
+++ sli/api/src/main/java/org/slc/sli/api/security/enums/Right.java
@@ -4,7 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 /**
  * A simple enum describing our basic rights that are required.
- * 
+ * ADMIN_ACCESS -> allows operations on entities in Admin Sphere
+ * FULL_ACCESS -> allows operations on all entities everywhere without regard for associations
  */
 public enum Right implements GrantedAuthority {
     ANONYMOUS_ACCESS, READ_GENERAL, WRITE_GENERAL, READ_RESTRICTED, WRITE_RESTRICTED, AGGREGATE_READ, AGGREGATE_WRITE, ADMIN_ACCESS, FULL_ACCESS;
---
+++ sli/api/src/test/java/org/slc/sli/api/security/SecurityUtilTest.java
@@ -0,0 +1,35 @@
+package org.slc.sli.api.security;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.slc.sli.api.security.enums.Right;
+import org.slc.sli.api.util.SecurityUtil;
+import org.slc.sli.api.util.SecurityUtil.SecurityTask;
+
+/**
+ * Tests sudo runs
+ * Validates that while running the process has elevated privilege
+ * and that once done, original privileges are restored
+ * 
+ * @author dkornishev
+ * 
+ */
+public class SecurityUtilTest {
+    
+    @Test
+    public void testSudo() {
+        Assert.assertNull(SecurityContextHolder.getContext().getAuthentication());
+        SecurityUtil.sudoRun(new SecurityTask<String>() {
+            @Override
+            public String execute() {
+                Assert.assertTrue(SecurityContextHolder.getContext().getAuthentication().getAuthorities().contains(Right.FULL_ACCESS));
+                return "";
+            }
+        });
+        
+        Assert.assertNull(SecurityContextHolder.getContext().getAuthentication());
+    }
+}
