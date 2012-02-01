758851918e07b0b72d07a863c54d9604b91f7907
Ryan Farris <rfarris@wgen.net>
2012-02-01 13:20:16 -0500

    added additional tests for xsd->neutral

--- sli/common/domain/src/test/java/org/slc/sli/validation/utils/XsdToNeutralSchemaTest.java
+++ sli/common/domain/src/test/java/org/slc/sli/validation/utils/XsdToNeutralSchemaTest.java
@@ -13,7 +13,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.slc.sli.validation.NeutralSchemaFactory;
+import org.slc.sli.validation.NeutralSchemaType;
+import org.slc.sli.validation.schema.ComplexSchema;
+import org.slc.sli.validation.schema.DateSchema;
+import org.slc.sli.validation.schema.IntegerSchema;
 import org.slc.sli.validation.schema.NeutralSchema;
+import org.slc.sli.validation.schema.Restriction;
+import org.slc.sli.validation.schema.StringSchema;
 
 /**
  * JUnit for XsdToNeturalSchema
@@ -30,18 +36,56 @@ public class XsdToNeutralSchemaTest {
     @Test
     public void testSchema() throws IOException {
         XsdToNeutralSchemaRepo repo = new XsdToNeutralSchemaRepo("classpath:testSchemas", new NeutralSchemaFactory());
+        
         NeutralSchema schema = repo.getSchema("TestComplexType");
         assertNotNull(schema);
         assertEquals("TestComplexType", schema.getType());
+        assertEquals(ComplexSchema.class.getCanonicalName(), schema.getValidatorClass());
+        
         NeutralSchema testStringSchema = (NeutralSchema) schema.getFields().get("testString");
         assertNotNull(testStringSchema);
-        assertEquals("string", testStringSchema.getType());
+        assertEquals(NeutralSchemaType.STRING.getName(), testStringSchema.getType());
+        assertEquals(StringSchema.class.getCanonicalName(), testStringSchema.getValidatorClass());
+        
         NeutralSchema testSimpleReferenceSchema = (NeutralSchema) schema.getFields().get("testSimpleReference");
         assertNotNull(testSimpleReferenceSchema);
         assertEquals("BaseSimpleType", testSimpleReferenceSchema.getType());
+        assertEquals(StringSchema.class.getCanonicalName(), testSimpleReferenceSchema.getValidatorClass());
+        assertEquals("1", testSimpleReferenceSchema.getProperties().get(Restriction.MIN_LENGTH.getValue()));
+        assertEquals("30", testSimpleReferenceSchema.getProperties().get(Restriction.MAX_LENGTH.getValue()));
+        
         NeutralSchema testDateSchema = (NeutralSchema) schema.getFields().get("testDate");
         assertNotNull(testDateSchema);
         assertEquals("date", testDateSchema.getType());
+        assertEquals(DateSchema.class.getCanonicalName(), testDateSchema.getValidatorClass());
+        
+        NeutralSchema anonSchema = (NeutralSchema) schema.getFields().get("testAnonymousSimpleType");
+        assertNotNull(anonSchema);
+        assertEquals("testAnonymousSimpleType", anonSchema.getType());
+        assertEquals(IntegerSchema.class.getCanonicalName(), anonSchema.getValidatorClass());
+        assertEquals("1", anonSchema.getProperties().get(Restriction.MIN_INCLUSIVE.getValue()));
+        assertEquals("2", anonSchema.getProperties().get(Restriction.MAX_INCLUSIVE.getValue()));
+        
+        NeutralSchema extSchema = (NeutralSchema) schema.getFields().get("testComplexContentExtension");
+        assertNotNull(extSchema);
+        assertEquals("testComplexContentExtension", extSchema.getType());
+        assertEquals(ComplexSchema.class.getCanonicalName(), extSchema.getValidatorClass());
+        NeutralSchema extField1 = (NeutralSchema) extSchema.getFields().get("stringElement");
+        assertNotNull(extField1);
+        assertEquals("string", extField1.getType());
+        assertEquals(StringSchema.class.getCanonicalName(), extField1.getValidatorClass());
+        NeutralSchema extField2 = (NeutralSchema) extSchema.getFields().get("extension1");
+        assertNotNull(extField2);
+        assertEquals("complexType2", extField2.getType());
+        assertEquals(ComplexSchema.class.getCanonicalName(), extField2.getValidatorClass());
+        assertNotNull(extField2.getFields().get("intElement"));
+        assertEquals(IntegerSchema.class.getCanonicalName(),
+                ((NeutralSchema) extField2.getFields().get("intElement")).getValidatorClass());
+        NeutralSchema extField3 = (NeutralSchema) extSchema.getFields().get("extension2");
+        assertNotNull(extField3);
+        assertEquals("BaseSimpleType", extField3.getType());
+        assertEquals(StringSchema.class.getCanonicalName(), extField3.getValidatorClass());
+        
     }
     
     @Test
