6857d842e367d4be526c4cdc870bddc09d3f4125
Jonathan Nanney <jnanney@wgen.net>
2012-02-01 11:15:07 -0500

    Removed mappings from the Role.

--- sli/api/src/main/java/org/slc/sli/api/security/resolve/impl/DefaultClientRoleManager.java
+++ sli/api/src/main/java/org/slc/sli/api/security/resolve/impl/DefaultClientRoleManager.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.slc.sli.api.config.EntityDefinition;
+import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.security.resolve.ClientRoleManager;
-import org.slc.sli.api.security.resolve.RealmRoleMappingException;
-import org.slc.sli.api.security.roles.Role;
 import org.slc.sli.api.security.roles.RoleRightAccess;
+import org.slc.sli.api.service.EntityService;
 
 /**
  * Default converter for client roles to sli roles
@@ -25,6 +28,27 @@ public class DefaultClientRoleManager implements ClientRoleManager {
     @Autowired
     private RoleRightAccess roleRightAccess;
     
+    @Autowired
+    private EntityDefinitionStore store;
+    
+    private EntityService service;
+
+    @PostConstruct
+    public void init() {
+        EntityDefinition def = store.lookupByResourceName("realm");
+        setService(def.getService());
+    }
+   
+    //Injector
+    public void setStore(EntityDefinitionStore store) {
+        this.store = store;
+    }
+
+    //Injector
+    public void setService(EntityService service) {
+        this.service = service;
+    }
+
     @Override
     /**
      */
@@ -34,78 +58,17 @@ public class DefaultClientRoleManager implements ClientRoleManager {
             if (roleRightAccess.findRoleByName(clientRoleName) != null) {
                 result.add(clientRoleName);
             } else {
-                String sliRoleName = getSliRoleName(realmId, clientRoleName);
-                if (sliRoleName != null) {
-                    result.add(sliRoleName);
+                @SuppressWarnings("unchecked")
+                Map<String, List<String>> mappings = (Map<String, List<String>>) service.get(realmId).get("mappings");
+                for (String key : mappings.keySet()) {
+                    if (mappings.get(key).contains(clientRoleName)) {
+                        result.add(clientRoleName);
+                    }
                 }
+                
             }
         }
         return result;
-    }
-    
-    @Override
-    public boolean addClientRole(String realmId, String sliRoleName, String clientRoleName) throws RealmRoleMappingException {
-        Role sliRole = roleRightAccess.findRoleByName(sliRoleName);
-        Map<String, List<String>> mappings = sliRole.getRealmRoleMappings();
-        List<String> listMappingsForRealm = mappings.get(realmId);
-        if (listMappingsForRealm == null) {
-            listMappingsForRealm = new ArrayList<String>();
-        }
-        
-        if (!listMappingsForRealm.contains(clientRoleName)) {
-            listMappingsForRealm.add(clientRoleName);
-        } else {
-            throw new RealmRoleMappingException("Role mapping already exists");
-        }
-        mappings.put(realmId, listMappingsForRealm);
-        sliRole.setRealmRoleMappings(mappings);
-        return roleRightAccess.updateRole(sliRole);
-    }
-    
-    @Override
-    public String getSliRoleName(String realmId, String clientRoleName) {
-        List<Role> allSliRoles = roleRightAccess.fetchAllRoles();
-        for (Role role : allSliRoles) {
-            List<String> clientRoles = role.getRealmRoleMappings().get(realmId);
-            if (clientRoles != null) {
-                for (String clientRole : clientRoles) {
-                    if (clientRole.equals(clientRoleName)) {
-                        return role.getName();
-                    }
-                }
-            }
-        }
-        return null;
-    }
-    
-    public void setRoleRightAccess(RoleRightAccess roleRightAccess) {
-        this.roleRightAccess = roleRightAccess;
-    }
-    
-    @Override
-    public List<String> getMappings(String realmId, String sliRoleName) {
-        Role role = roleRightAccess.findRoleByName(sliRoleName);
-        if (role != null) {
-            Map<String, List<String>> mappings = role.getRealmRoleMappings();
-            return mappings.get(realmId);
-        }
-        return null;
-    }
-    
-    @Override
-    public boolean deleteClientRole(String realmId, String clientRoleName) {
-        List<Role> allSliRoles = roleRightAccess.fetchAllRoles();
-        for (Role role : allSliRoles) {
-            Map<String, List<String>> mappings = role.getRealmRoleMappings();
-            List<String> clientRoles = mappings.get(realmId);
-            if (clientRoles != null && clientRoles.contains(clientRoleName)) {
-                clientRoles.remove(clientRoleName);
-                mappings.put(realmId, clientRoles);
-                role.setRealmRoleMappings(mappings);
-                return roleRightAccess.updateRole(role);
-            }
-        }
-        return false;
     }
     
 }
