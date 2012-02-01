1bc2ab24d2996c410da1c4b8ac7c791a346de62d
kmyers <kmyers@wgen.net>
2012-02-01 18:55:53 -0500

    Support for course entity and associated tests

--- sli/api/src/main/java/org/slc/sli/api/config/BasicDefinitionStore.java
+++ sli/api/src/main/java/org/slc/sli/api/config/BasicDefinitionStore.java
@@ -52,6 +52,7 @@ public class BasicDefinitionStore implements EntityDefinitionStore {
         this.makeExposeAndAddEntityDefinition("aggregation");
         this.makeExposeAndAddEntityDefinition("aggregationDefinition");
         EntityDefinition assessment = this.makeExposeAndAddEntityDefinition("assessment");
+        this.makeExposeAndAddEntityDefinition("course");
         EntityDefinition school = this.makeExposeAndAddEntityDefinition("school");
         EntityDefinition section = this.makeExposeAndAddEntityDefinition("section");
         EntityDefinition session = this.makeExposeAndAddEntityDefinition("session");
--- sli/api/src/test/java/org/slc/sli/api/service/MockRepo.java
+++ sli/api/src/test/java/org/slc/sli/api/service/MockRepo.java
@@ -33,6 +33,7 @@ public class MockRepo implements EntityRepository {
     private AtomicInteger                    counter           = new AtomicInteger();
     
     public MockRepo() {
+        repo.put("course", new LinkedHashMap<String, Entity>());
         repo.put("student", new LinkedHashMap<String, Entity>());
         repo.put("school", new LinkedHashMap<String, Entity>());
         repo.put("roles", new LinkedHashMap<String, Entity>());
