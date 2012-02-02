f300dea83fc709d79b461ea2162f14d84d4c9e17
Ryan Farris <rfarris@wgen.net>
2012-02-02 15:16:40 -0500

    added mock validator to DAL.

---
+++ sli/data-access/dal/src/test/java/org/slc/sli/validation/MockValidator.java
@@ -0,0 +1,22 @@
+package org.slc.sli.validation;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+import org.slc.sli.domain.Entity;
+
+/**
+ * Mock validator for the dal
+ */
+@Component
+public class MockValidator implements EntityValidator {
+    
+    @Override
+    public boolean validate(Entity entity) throws EntityValidationException {
+        if (entity.getBody().containsKey("bad-entity")) {
+            throw new EntityValidationException(entity.getEntityId(), entity.getType(), new ArrayList<ValidationError>());
+        }
+        return true;
+    }
+}
