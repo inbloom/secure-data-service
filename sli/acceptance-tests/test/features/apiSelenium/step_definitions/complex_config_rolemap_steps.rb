d7d40591a54be45466e76d4e009a2b8ca7d2ea9a
Mark Abernathy <mabernathy@wgen.net>
2012-02-03 14:31:45 -0500

    Added some implimentation for Complex Configurable Role mapping tests

--- sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
+++ sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
@@ -3,34 +3,6 @@ require 'json'
 
 require_relative '../../utils/sli_utils.rb'
 
-Given /^I am from realm "([^"]*)"$/ do |arg1|
-  #No code needed, this is done as configuration
-end
-
-Given /^another realm's admin mapped "([^"]*)" to the "([^"]*)" default SLI role in their realm$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^I navigate to the SLI Role Mapping Admin Page$/ do
-  url = PropLoader.getProps['admintools_server_url']+"/admin/role-mapping"
-  @driver.get url
-end
-
-When /^I map the default SLI role "([^"]*)" to the custom role "([^"]*)"$/ do |arg1, arg2|
-  @driver.find_element(:name, arg1).click
-  @driver.find_element(:id, "roletext").send_keys arg2
-  @driver.find_element(:id, "addrole").click
-  
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I see that "([^"]*)" is now mapped to the "([^"]*)" role$/ do |arg1, arg2|
-  @driver.find_element(:name, arg1).click
-  @driver.find_element(:name, arg2)
-  
-  pending # express the regexp above with the code you wish you had
-end
-
 Then /^The user "([^"]*)" who is a "([^"]*)" can now log in to SLI as a "([^"]*)" from my realm "([^"]*)"$/ do |arg1, arg2, arg3, arg4|
   # Login and get a session ID
   realm = "sli" if arg3 == "SLI"
@@ -53,28 +25,6 @@ Then /^The user "([^"]*)" who is a "([^"]*)" can now log in to SLI as a "([^"]*)
   assert(result["authorities"].include?("WRITE_RESTRICTED"), "User "+user+" was not granted WRITE_RESTRICTED permissions") if ["IT Administrator"].include?(arg3)
 end
 
-Given /^a "([^"]*)" has been mapped to the "([^"]*)" default role$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-When /^I remove the map from the default SLI role "([^"]*)" to the custom role "([^"]*)"$/ do |arg1, arg2|
-  @driver.find_element(:name, arg1).click
-  @driver.find_element(:name, arg2).click
-  @driver.find_element(:id, "removerole").click
-  
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I see that "([^"]*)" is no longer mapped to the "([^"]*)" role$/ do |arg1, arg2|
-  begin
-    @driver.find_element(:id, "mapping")
-    assert(false, "Should not get this far")
-  rescue
-  end
-
-  pending # express the regexp above with the code you wish you had
-end
-
 Then /^The user "([^"]*)" who is a "([^"]*)" can not access SLI as a "([^"]*)" from my realm "([^"]*)"$/ do |arg1, arg2, arg3, arg4|
   # Login and get a session ID
   realm = "sli" if arg3 == "SLI"
@@ -93,12 +43,104 @@ Then /^The user "([^"]*)" who is a "([^"]*)" can not access SLI as a "([^"]*)" f
   assert(result["authorities"].size == 0, "User "+arg1+" was granted permissions when they should have none: "+result["authorities"])
 end
 
-Then /^I should not see the mapping from the other realm$/ do
-  begin
-    @driver.find_element(:id, "mapping")
-    assert(false, "Should not get this far")
-  rescue
-  end
-  
+When /^I navigate to the Complex\-Configurable Role Mapping Page$/ do
+  @driver.get PropLoader.getProps['admintools_server_url']+"/realms"
+end
+
+Given /^I am authenticated to "([^"]*)" IDP$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
+
+Given /^I am not a Super Administrator$/ do
+  #No code needed, this is done as configuration
+end
+
+Then /^I should get a message that I am not authorized to access the page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am a Super Administrator for "([^"]*)"$/ do |arg1|
+  #No code needed, this is done as configuration
+end
+
+Then /^I should be redirected to the  Complex\-Configurable Role Mapping Page for "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have tried to access the Complex\-Configurable Role Mapping Page$/ do
+  @driver.get PropLoader.getProps['admintools_server_url']+"/realms"
+end
+
+Then /^I am redirected to the Complex\-Configurable Role Mapping Page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have navigated to my Complex\-Configurable Role Mapping Page$/ do
+  @driver.get PropLoader.getProps['admintools_server_url']+"/realms"
+  @driver.find_element(:id, "Edit").click
+end
+
+When /^I click on the Reset Mapping button$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the Leader, Educator, Aggregate Viewer and IT Administrator roles are now mapped to themselves$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^no other mappings exist for this realm$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click on the role "([^"]*)" radio button$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I enter "([^"]*)" in the text field$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click the add button$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the custom role "([^"]*)" is mapped to the default role "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click on the remove button between role "([^"]*)" and custom role "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the custom role "([^"]*)" is no longer mapped to the default role "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click on the role <First Role> radio button$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get a message that I cannot map the same custom role to multiple SLI Default roles$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get a message that I already have this role mapped to a SLI Default role$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see a message that tells me that I can put only alphanumeric values as a custom role$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mapping is not added between "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I see pre\-existing mappings$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I no longer see the pre\-existing mappings$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
