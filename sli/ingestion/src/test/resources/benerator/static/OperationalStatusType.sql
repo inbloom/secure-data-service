ab8cd196dbca7b30225bd55c3a7a1b7409b20175
Yuan Zhang <yzhang@wgen.net>
2012-02-06 09:40:36 -0500

    add school, teacher, and association generation code

---
+++ sli/ingestion/src/test/resources/benerator/static/OperationalStatusType.sql
@@ -0,0 +1,67 @@
+DELETE FROM [data].[dbo].[OperationalStatusType]
+
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (1,'Active','Active')
+GO
+
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (2,'Added','Added')
+GO
+
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (3,'Changed Agency','Changed Agency')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (4,'Closed','Closed')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (5,'Continuing','Continuing')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (6,'Future','Future')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (7,'Inactive','Inactive')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (8,'New','New')
+GO
+INSERT INTO [data].[dbo].[OperationalStatusType]
+           ([OperationalStatusTypeId]
+           ,[CodeValue]
+           ,[Description])
+     VALUES
+           (9,'Reopened','Reopened')
+GO
