9e70e579c386195b3011b0e77dfd934a19633ad2
Patrick Wolf <pwolf@wgen.net>
2012-02-07 15:45:57 -0500

    Adding method content for step def forbidden access

--- sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
+++ sli/acceptance-tests/test/features/apiSelenium/step_definitions/complex_config_rolemap_steps.rb
@@ -56,7 +56,7 @@ Given /^I am not a Super Administrator$/ do
 end
 
 Then /^I should get a message that I am not authorized to access the page$/ do
-  pending # express the regexp above with the code you wish you had
+  assertWithWait("Failed to find forbidden message")  {@driver.page_source.index("Forbidden") != nil}
 end
 
 Given /^I am a Super Administrator for "([^"]*)"$/ do |arg1|
