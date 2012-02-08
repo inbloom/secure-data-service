41860ffcc1ac4250e87477de103a13e89315910c
Matthew Lane <lanemt@gmail.com>
2012-02-08 11:52:39 -0500

    sessionId passed as header instead of param

--- POC/db-rails/app/models/session_resource.rb
+++ POC/db-rails/app/models/session_resource.rb
@@ -4,10 +4,15 @@ class SessionResource < ActiveResource::Base
   Rails.logger.debug { "Entity Type: #{@url_type}" }
   
   class << self
+    
+    def headers
+      @headers ||= {"sessionId" => self.auth_id}
+    end
+    
     ## Remove format from the url.
      def element_path(id, prefix_options = {}, query_options = nil)
        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-       something = "#{prefix(prefix_options)}#{self.url_type}/#{id}#{query_string(query_options)}?sessionId=#{self.auth_id}"
+       something = "#{prefix(prefix_options)}#{self.url_type}/#{id}#{query_string(query_options)}"
        Rails.logger.debug { "element_path: #{something}" }
        something
      end
@@ -15,7 +20,7 @@ class SessionResource < ActiveResource::Base
      ## Remove format from the url.
      def collection_path(prefix_options = {}, query_options = nil)
        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-       something = "#{prefix(prefix_options)}#{self.url_type}#{query_string(query_options)}?sessionId=#{self.auth_id}"
+       something = "#{prefix(prefix_options)}#{self.url_type}#{query_string(query_options)}"
        Rails.logger.debug { "collection_path: #{something}" }
        something
      end
@@ -23,7 +28,7 @@ class SessionResource < ActiveResource::Base
       ## Remove format from the url.
       def custom_method_collection_url(method_name, options = {})
         prefix_options, query_options = split_options(options)
-        something = "#{prefix(prefix_options)}#{self.url_type}/#{method_name}#{query_string(query_options)}?sessionId=#{self.auth_id}"
+        something = "#{prefix(prefix_options)}#{self.url_type}/#{method_name}#{query_string(query_options)}"
         Rails.logger.debug {"custom_method_collection_url: #{something}"}
         something
       end
