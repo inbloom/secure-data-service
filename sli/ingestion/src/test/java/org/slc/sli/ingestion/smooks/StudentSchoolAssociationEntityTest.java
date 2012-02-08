5e94e99611ebe3c893c8fc47ddef73886455ff88
Danilo Duran <dduran@wgen.net>
2012-02-07 19:20:50 -0500

    remove tabs

--- sli/ingestion/src/test/java/org/slc/sli/ingestion/smooks/StudentSchoolAssociationEntityTest.java
+++ sli/ingestion/src/test/java/org/slc/sli/ingestion/smooks/StudentSchoolAssociationEntityTest.java
@@ -1,6 +1,5 @@
 package org.slc.sli.ingestion.smooks;
 
-
 import java.io.ByteArrayInputStream;
 import java.util.Map;
 
@@ -23,35 +22,31 @@ import org.slc.sli.validation.EntityValidator;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
-
 public class StudentSchoolAssociationEntityTest {
-
 
     @Autowired
     private EntityValidator validator;
 
-    String xmlTestData =	"<InterchangeStudentEnrollment xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-StudentEnrollment.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
-          + "<StudentSchoolAssociation>"
-          + " <StudentReference>"
-          + "  <StudentIdentity>"
-          + "      <StudentUniqueStateId>900000001</StudentUniqueStateId>"
-          + "  </StudentIdentity>"
-          + " </StudentReference>"
-          + " <SchoolReference>"
-          + "  <EducationalOrgIdentity>"
-          + "      <StateOrganizationId>990000001</StateOrganizationId>"
-          + "  </EducationalOrgIdentity>"
-          + " </SchoolReference>"
-          + " <EntryDate>2012-01-17</EntryDate>"
-          + " <EntryGradeLevel>Eighth grade</EntryGradeLevel>"
-          + " <EntryType>Next year school</EntryType>"
-          + " <GraduationPlan>Distinguished</GraduationPlan>"
-          + " <RepeatGradeIndicator>false</RepeatGradeIndicator>"
-          + " <EducationalPlans>"
-          + "   <EducationalPlan>Full Time Employment</EducationalPlan>"
-          + " </EducationalPlans>"
-          + "</StudentSchoolAssociation>"
-          + "</InterchangeStudentEnrollment>";
+    String xmlTestData = "<InterchangeStudentEnrollment xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-StudentEnrollment.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
+            + "<StudentSchoolAssociation>"
+            + " <StudentReference>"
+            + "  <StudentIdentity>"
+            + "      <StudentUniqueStateId>900000001</StudentUniqueStateId>"
+            + "  </StudentIdentity>"
+            + " </StudentReference>"
+            + " <SchoolReference>"
+            + "  <EducationalOrgIdentity>"
+            + "      <StateOrganizationId>990000001</StateOrganizationId>"
+            + "  </EducationalOrgIdentity>"
+            + " </SchoolReference>"
+            + " <EntryDate>2012-01-17</EntryDate>"
+            + " <EntryGradeLevel>Eighth grade</EntryGradeLevel>"
+            + " <EntryType>Next year school</EntryType>"
+            + " <GraduationPlan>Distinguished</GraduationPlan>"
+            + " <RepeatGradeIndicator>false</RepeatGradeIndicator>"
+            + " <EducationalPlans>"
+            + "   <EducationalPlan>Full Time Employment</EducationalPlan>"
+            + " </EducationalPlans>" + "</StudentSchoolAssociation>" + "</InterchangeStudentEnrollment>";
 
     private void checkValidSSANeutralRecord(NeutralRecord record) {
         Map<String, Object> entity = record.getAttributes();
@@ -60,7 +55,7 @@ public class StudentSchoolAssociationEntityTest {
         Assert.assertEquals("Eighth grade", entity.get("entryGradeLevel"));
         Assert.assertEquals("2012-01-17", entity.get("entryDate"));
         Assert.assertEquals("Next year school", entity.get("entryType"));
-        //Assert.assertEquals("Full Time Employment", entity.get("educationPlans"));
+        // Assert.assertEquals("Full Time Employment", entity.get("educationPlans"));
         Assert.assertEquals("false", entity.get("repeatGradeIndicator").toString());
     }
 
@@ -77,7 +72,7 @@ public class StudentSchoolAssociationEntityTest {
         try {
             nrfr = EntityTestUtils.getNeutralRecords(testInput, smooksConfig, targetSelector);
 
-            //Tests that the NeutralRecord was created
+            // Tests that the NeutralRecord was created
             Assert.assertTrue(nrfr.hasNext());
 
             NeutralRecord record = nrfr.next();
@@ -87,31 +82,27 @@ public class StudentSchoolAssociationEntityTest {
         }
     }
 
-
     @Test
-	public void testValidatorStudentSchoolAssociation() throws Exception {
-		String smooksConfig = "smooks_conf/smooks-all-xml.xml";
-		String targetSelector = "InterchangeStudentEnrollment/StudentSchoolAssociation";
+    public void testValidatorStudentSchoolAssociation() throws Exception {
+        String smooksConfig = "smooks_conf/smooks-all-xml.xml";
+        String targetSelector = "InterchangeStudentEnrollment/StudentSchoolAssociation";
 
-		ByteArrayInputStream testInput = new ByteArrayInputStream(
-				xmlTestData.getBytes());
-		NeutralRecordFileReader nrfr = null;
-		try {
-			nrfr = EntityTestUtils.getNeutralRecords(testInput, smooksConfig,
-					targetSelector);
+        ByteArrayInputStream testInput = new ByteArrayInputStream(xmlTestData.getBytes());
+        NeutralRecordFileReader nrfr = null;
+        try {
+            nrfr = EntityTestUtils.getNeutralRecords(testInput, smooksConfig, targetSelector);
 
-			// Tests that the NeutralRecord was created
-			Assert.assertTrue(nrfr.hasNext());
+            // Tests that the NeutralRecord was created
+            Assert.assertTrue(nrfr.hasNext());
 
-			NeutralRecord record = nrfr.next();
-			EntityTestUtils.mapValidation(record.getAttributes(),
-					"studentSchoolAssociation", validator);
+            NeutralRecord record = nrfr.next();
+            EntityTestUtils.mapValidation(record.getAttributes(), "studentSchoolAssociation", validator);
 
-		} finally {
-			nrfr.close();
-		}
+        } finally {
+            nrfr.close();
+        }
 
-	}
+    }
 
     @Test
     public void testValidStudentSchoolAssociationXML() throws Exception {
@@ -123,16 +114,14 @@ public class StudentSchoolAssociationEntityTest {
         NeutralRecordFileReader nrfr = null;
         try {
             nrfr = EntityTestUtils.getNeutralRecords(testInput, smooksConfig, targetSelector);
-            //Tests that the NeutralRecords were created
+            // Tests that the NeutralRecords were created
             Assert.assertTrue(nrfr.hasNext());
             NeutralRecord record = nrfr.next();
             checkValidSSANeutralRecord(record);
         } finally {
-           nrfr.close();
+            nrfr.close();
         }
 
     }
 
-
 }
-
