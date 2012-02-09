25f88a6287fb18cfbf2c8faffc38131806d19f04
Jonathan Nanney <jnanney@wgen.net>
2012-02-09 09:37:09 -0500

    Trying to get the token to be used for testing the students controller

--- POC/db-rails/test/functional/students_controller_test.rb
+++ POC/db-rails/test/functional/students_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
+
+  def setup
+    print("About to start test\n")
+    res = RestClient.get("https://devopenam1.slidev.org/idp1/identity/authenticate?username=linda.kim&password=linda.kim1234")
+    token = res.split("=")[1]
+    print("The token being used for testing is #{token}")
+    SessionResource.auth_id = token
+    #mock_rest_client = Test::Unit::MockObject(RestClient).new
+    #print("The client is #{mock_rest_client.class}\n")
+  end
+
   test "should get index" do
     get :index
     assert_response :success
