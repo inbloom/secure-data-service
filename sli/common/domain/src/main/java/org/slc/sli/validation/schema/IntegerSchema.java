387a75b587ee4b01c9f72053c8dd403c1c0799d9
Ryan Farris <rfarris@wgen.net>
2012-02-10 12:27:54 -0500

    Fixed bug in number validation.  Removed support for TotalDigits, FractionalDigits xml schema facets

--- sli/common/domain/src/main/java/org/slc/sli/validation/schema/IntegerSchema.java
+++ sli/common/domain/src/main/java/org/slc/sli/validation/schema/IntegerSchema.java
@@ -12,37 +12,37 @@ import org.slc.sli.validation.ValidationError;
 import org.slc.sli.validation.ValidationError.ErrorType;
 
 /**
- *
+ * 
  * SLI Integer Schema which validates integer entities
- *
+ * 
  * @author Robert Bloh <rbloh@wgen.net>
- *
+ * 
  */
 @Scope("prototype")
 @Component
 public class IntegerSchema extends NeutralSchema {
-
+    
     // Constructors
     public IntegerSchema() {
         this(NeutralSchemaType.INTEGER.getName());
     }
-
+    
     public IntegerSchema(String xsdType) {
         super(xsdType);
     }
-
+    
     // Methods
-
+    
     @Override
     public NeutralSchemaType getSchemaType() {
         return NeutralSchemaType.INTEGER;
     }
-
+    
     /**
      * Validates the given entity
      * Returns true if the validation was successful or a ValidationException if the validation was
      * unsuccessful.
-     *
+     * 
      * @param fieldName
      *            name of entity field being validated
      * @param entity
@@ -50,15 +50,16 @@ public class IntegerSchema extends NeutralSchema {
      * @param errors
      *            list of current errors
      * @param repo
-     *            reference to the entity repository           
+     *            reference to the entity repository
      * @return true if valid
      */
-    protected boolean validate(String fieldName, Object entity, List<ValidationError> errors, EntityValidationRepository repo) {
+    protected boolean validate(String fieldName, Object entity, List<ValidationError> errors,
+            EntityValidationRepository repo) {
         Integer data = NumberUtils.toInteger(entity);
         if (!addError(data != null, fieldName, entity, "Integer", ErrorType.INVALID_DATATYPE, errors)) {
             return false;
         }
-
+        
         if (this.getProperties() != null) {
             for (Entry<String, Object> entry : this.getProperties().entrySet()) {
                 if (Restriction.isRestriction(entry.getKey())) {
@@ -85,12 +86,6 @@ public class IntegerSchema extends NeutralSchema {
                     case MAX_EXCLUSIVE:
                         if (!addError(data.compareTo(restrictionValue) < 0, fieldName, entity, "max-exclusive="
                                 + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
-                    case TOTAL_DIGITS:
-                        if (!addError(NumberUtils.totalDigits(data) <= restrictionValue, fieldName, entity,
-                                "total-digits=" + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
                             return false;
                         }
                         break;
--- sli/common/domain/src/main/java/org/slc/sli/validation/schema/LongSchema.java
+++ sli/common/domain/src/main/java/org/slc/sli/validation/schema/LongSchema.java
@@ -12,37 +12,37 @@ import org.slc.sli.validation.ValidationError;
 import org.slc.sli.validation.ValidationError.ErrorType;
 
 /**
- *
+ * 
  * SLI Long Schema which validates long entities
- *
+ * 
  * @author Robert Bloh <rbloh@wgen.net>
- *
+ * 
  */
 @Scope("prototype")
 @Component
 public class LongSchema extends NeutralSchema {
-
+    
     // Constructors
     public LongSchema() {
         this(NeutralSchemaType.LONG.getName());
     }
-
+    
     public LongSchema(String xsdType) {
         super(xsdType);
     }
-
+    
     // Methods
-
+    
     @Override
     public NeutralSchemaType getSchemaType() {
         return NeutralSchemaType.LONG;
     }
-
+    
     /**
      * Validates the given entity
      * Returns true if the validation was successful or a ValidationException if the validation was
      * unsuccessful.
-     *
+     * 
      * @param fieldName
      *            name of entity field being validated
      * @param entity
@@ -50,15 +50,16 @@ public class LongSchema extends NeutralSchema {
      * @param errors
      *            list of current errors
      * @param repo
-     *            reference to the entity repository           
+     *            reference to the entity repository
      * @return true if valid
      */
-    protected boolean validate(String fieldName, Object entity, List<ValidationError> errors, EntityValidationRepository repo) {
+    protected boolean validate(String fieldName, Object entity, List<ValidationError> errors,
+            EntityValidationRepository repo) {
         Long data = NumberUtils.toLong(entity);
         if (!addError(data != null, fieldName, entity, "Long", ErrorType.INVALID_DATATYPE, errors)) {
             return false;
         }
-
+        
         if (this.getProperties() != null) {
             for (Entry<String, Object> entry : this.getProperties().entrySet()) {
                 if (Restriction.isRestriction(entry.getKey())) {
@@ -88,17 +89,11 @@ public class LongSchema extends NeutralSchema {
                             return false;
                         }
                         break;
-                    case TOTAL_DIGITS:
-                        if (!addError(NumberUtils.totalDigits(data) <= restrictionValue, fieldName, entity,
-                                "total-digits=" + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
                     }
                 }
             }
         }
         return true;
     }
-
+    
 }
