9e05fc1534f2d5f7abea116953d5c4301fb982d3
Dwayne Williams <dwilliams@wgen.net>
2012-02-10 17:36:19 -0500

    checkstyle fixes

--- sli/common/common-test-data/src/main/java/org/slc/sli/util/transform/AssessmentBody.java
+++ sli/common/common-test-data/src/main/java/org/slc/sli/util/transform/AssessmentBody.java
@@ -2,69 +2,65 @@ package org.slc.sli.util.transform;
 
 import java.util.ArrayList;
 
-public class AssessmentBody implements MongoDataEmitter, Cloneable
-{
-	private ArrayList<AssessmentCode> assessmentIdentificationCodes = null;
-	private String academicSubject = null;
-	private String assessmentCategory = null;
-	private String assessmentTitle = null;
-	private String gradeLevelAssessed = null;
-	private String contentStandard = null;
-	private String version = null;
-	private Assessment.Period thePeriod = null;
-
-
-	public AssessmentBody(String title, ArrayList<AssessmentCode> codes, String subject, String category, String grade, String standard, String version)
-	{
-		this.assessmentTitle = title;
-		this.assessmentIdentificationCodes = codes;
-		this.academicSubject = subject;
-		this.assessmentCategory = category;
-		this.gradeLevelAssessed = grade;
-		this.contentStandard = standard;
-		this.version = version;
-	}
-	
-	public AssessmentBody clone()
-	{
-		AssessmentBody clone = new AssessmentBody(assessmentTitle, assessmentIdentificationCodes, academicSubject, assessmentCategory, gradeLevelAssessed, 
-				contentStandard, version);
-		return clone;
-	}
-	
-	public void setTitle(String title)
-	{
-		this.assessmentTitle = title;
-	}
-	
-	@Override
-	public String emit()
-	{
-		//	"body":{"assessmentTitle":"Mathematics Achievement Assessment Test","assessmentIdentificationCode":[{"identificationSystem":"School","id":"01234B"}],
-		//	"academicSubject":"MATHEMATICS","assessmentCategory":"ACHIEVEMENT_TEST","gradeLevelAssessed":"ADULT_EDUCATION",
-		//	"contentStandard":"LEA_STANDARD","version":"1.2"}
-		StringBuffer answer = new StringBuffer();
-		answer.append("\"body\":{\"assessmentTitle\":\"").append(assessmentTitle).append("\",\"assessmentIdentificationCode\":[");
-		boolean firstAssessmentIdCode = true;
-		for(AssessmentCode ac : assessmentIdentificationCodes){
-			if(!firstAssessmentIdCode){
-				answer.append(",");
-			}
-			answer.append(ac.emit());
-			firstAssessmentIdCode = false;
-		}
-		answer.append("],\"academicSubject\":\"").append(academicSubject).append("\",\"assessmentCategory\":\"").append(assessmentCategory).
-			append("\",\"gradeLevelAssessed\":\"").append(gradeLevelAssessed).append("\",\"contentStandard\":\"").append(contentStandard).append("\",");
-		if(thePeriod != null){
-			answer.append("\"period\":").append(thePeriod.emit()).append(",");
-		}
-		answer.append("\"version\":\"").append(version).append("\"}");
-		
-		return answer.toString();
-	}
-
-	public void setPeriod(Assessment.Period p)
-	{
-		thePeriod = p;
-	}
+public class AssessmentBody implements MongoDataEmitter, Cloneable {
+    private ArrayList<AssessmentCode> assessmentIdentificationCodes = null;
+    private String academicSubject = null;
+    private String assessmentCategory = null;
+    private String assessmentTitle = null;
+    private String gradeLevelAssessed = null;
+    private String contentStandard = null;
+    private String version = null;
+    private Assessment.Period thePeriod = null;
+    
+    public AssessmentBody(String title, ArrayList<AssessmentCode> codes, String subject, String category, String grade,
+            String standard, String version) {
+        this.assessmentTitle = title;
+        this.assessmentIdentificationCodes = codes;
+        this.academicSubject = subject;
+        this.assessmentCategory = category;
+        this.gradeLevelAssessed = grade;
+        this.contentStandard = standard;
+        this.version = version;
+    }
+    
+    public AssessmentBody clone() {
+        AssessmentBody clone = new AssessmentBody(assessmentTitle, assessmentIdentificationCodes, academicSubject,
+                assessmentCategory, gradeLevelAssessed, contentStandard, version);
+        return clone;
+    }
+    
+    public void setTitle(String title) {
+        this.assessmentTitle = title;
+    }
+    
+    @Override
+    public String emit() {
+        // "body":{"assessmentTitle":"Mathematics Achievement Assessment Test","assessmentIdentificationCode":[{"identificationSystem":"School","id":"01234B"}],
+        // "academicSubject":"MATHEMATICS","assessmentCategory":"ACHIEVEMENT_TEST","gradeLevelAssessed":"ADULT_EDUCATION",
+        // "contentStandard":"LEA_STANDARD","version":"1.2"}
+        StringBuffer answer = new StringBuffer();
+        answer.append("\"body\":{\"assessmentTitle\":\"").append(assessmentTitle)
+                .append("\",\"assessmentIdentificationCode\":[");
+        boolean firstAssessmentIdCode = true;
+        for (AssessmentCode ac : assessmentIdentificationCodes) {
+            if (!firstAssessmentIdCode) {
+                answer.append(",");
+            }
+            answer.append(ac.emit());
+            firstAssessmentIdCode = false;
+        }
+        answer.append("],\"academicSubject\":\"").append(academicSubject).append("\",\"assessmentCategory\":\"")
+                .append(assessmentCategory).append("\",\"gradeLevelAssessed\":\"").append(gradeLevelAssessed)
+                .append("\",\"contentStandard\":\"").append(contentStandard).append("\",");
+        if (thePeriod != null) {
+            answer.append("\"period\":").append(thePeriod.emit()).append(",");
+        }
+        answer.append("\"version\":\"").append(version).append("\"}");
+        
+        return answer.toString();
+    }
+    
+    public void setPeriod(Assessment.Period p) {
+        thePeriod = p;
+    }
 }
--- sli/common/common-test-data/src/main/java/org/slc/sli/util/transform/Session.java
+++ sli/common/common-test-data/src/main/java/org/slc/sli/util/transform/Session.java
@@ -1,41 +1,40 @@
 package org.slc.sli.util.transform;
 
-public class Session implements MongoDataEmitter
-{
-	private String schoolYear = null;
-	private String semester = null;
-	private String beginDate = null;
-	private String endDate = null;
-	private int days = 0;
-	
-	private String generatedUuid = null;
-	
-	public Session(String schoolYear, String semester, String beginDate, String endDate, int days)
-	{
-		this.schoolYear = schoolYear;
-		this.semester = semester;
-		this.beginDate = beginDate;
-		this.endDate = endDate;
-		this.days = days;
-		
-		generatedUuid = Base64.nextUuid("aacb");
-	}
-
-	@Override
-	public String emit() 
-	{
-		// {"_id":{"$binary":"zk4WoMB66rtWHSX8sUcbuw==","$type":"03"},"type":"session","body":{"schoolYear":"Year_2010_2011","sessionName":"Spring 2011",
-		// "term":"Spring_Semester","endDate":"2011-06-31","beginDate":"2011-01-01","totalInstructionalDays":90}, "tenantId":"Zork"}
-		StringBuffer answer = new StringBuffer();
-		answer.append("{\"_id\":{\"$binary\":\"").append(Base64.toBase64(generatedUuid)).append("\",\"$type\":\"03\"},\"type\":\"session\",\"body\":{\"schoolYear\":\"").
-			append(schoolYear).append("\",\"term\":\"").append(semester).append("\",\"endDate\":\"").append(endDate).append("\",\"beginDate\":\"").append(beginDate).
-			append("\",\"totalInstructionalDays\":").append(days).append("},\"tenantId\":\"Zork\"}\n");
-		
-		return answer.toString();
-	}
-	
-	public String getUuid()
-	{
-		return generatedUuid;
-	}
+public class Session implements MongoDataEmitter {
+    private String schoolYear = null;
+    private String semester = null;
+    private String beginDate = null;
+    private String endDate = null;
+    private int days = 0;
+    
+    private String generatedUuid = null;
+    
+    public Session(String schoolYear, String semester, String beginDate, String endDate, int days) {
+        this.schoolYear = schoolYear;
+        this.semester = semester;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.days = days;
+        
+        generatedUuid = Base64.nextUuid("aacb");
+    }
+    
+    @Override
+    public String emit() {
+        // {"_id":{"$binary":"zk4WoMB66rtWHSX8sUcbuw==","$type":"03"},"type":"session","body":{"schoolYear":"Year_2010_2011","sessionName":"Spring 2011",
+        // "term":"Spring_Semester","endDate":"2011-06-31","beginDate":"2011-01-01","totalInstructionalDays":90},
+        // "tenantId":"Zork"}
+        StringBuffer answer = new StringBuffer();
+        answer.append("{\"_id\":{\"$binary\":\"").append(Base64.toBase64(generatedUuid))
+                .append("\",\"$type\":\"03\"},\"type\":\"session\",\"body\":{\"schoolYear\":\"").append(schoolYear)
+                .append("\",\"term\":\"").append(semester).append("\",\"endDate\":\"").append(endDate)
+                .append("\",\"beginDate\":\"").append(beginDate).append("\",\"totalInstructionalDays\":").append(days)
+                .append("},\"tenantId\":\"Zork\"}\n");
+        
+        return answer.toString();
+    }
+    
+    public String getUuid() {
+        return generatedUuid;
+    }
 }
