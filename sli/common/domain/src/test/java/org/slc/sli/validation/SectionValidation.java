1fa6a92c904c3334c6a782b848b84f6a59f70857
Nicholas Brown <nbrown@wgen.net>
2012-02-10 12:37:33 -0500

    test for good section

--- sli/common/domain/src/test/java/org/slc/sli/validation/SectionValidation.java
+++ sli/common/domain/src/test/java/org/slc/sli/validation/SectionValidation.java
@@ -6,9 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.slc.sli.validation.schema.NeutralSchema;
 
@@ -18,6 +21,8 @@ import org.slc.sli.validation.schema.NeutralSchema;
  * @author nbrown
  * 
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class SectionValidation {
     private static final Logger LOG = LoggerFactory.getLogger(SectionValidation.class);
     
@@ -27,7 +32,18 @@ public class SectionValidation {
     @Test
     public void testSectionValidation() {
         NeutralSchema schema = repo.getSchema("section");
+        assertNotNull(schema);
         Map<String, Object> goodSection = new HashMap<String, Object>();
+        goodSection.put("uniqueSectionCode", "Math101");
+        goodSection.put("sequenceOfCourse", 1);
+        goodSection.put("educationalEnvironment", "Classroom");
+        goodSection.put("mediumOfInstruction", "Face-to-face instruction");
+        goodSection.put("populationServed", "Regular Students");
+        Map<String, Object> credit = new HashMap<String, Object>();
+        credit.put("credit", 1.5);
+        credit.put("creditType", "Semester hour credit");
+        credit.put("creditConversion", 2.5);
+        goodSection.put("credits", credit);
         schema.validate(goodSection);
     }
 }
