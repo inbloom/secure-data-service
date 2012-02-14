7b113c37e7d952d1a5cecdaab26df4a62475ae9c
Ryan Latta <rlatta@wgen.net>
2012-02-14 10:15:45 -0500

    Adjusting unit tests.

--- sli/databrowser/test/unit/entity_test.rb
+++ sli/databrowser/test/unit/entity_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class EntityTest < ActiveSupport::TestCase
-  
-  
+
   test "simple value generator with nil" do
     v = Entity.value_for_simple_view(VIEW_CONFIG['teacher'].first, nil)
     assert_nil(v)
@@ -16,7 +15,7 @@ class EntityTest < ActiveSupport::TestCase
   test "simple value generator with valid" do
     v = Entity.value_for_simple_view(VIEW_CONFIG['teacher'].first, @teacher_fixtures['one'])
     assert_not_nil(v)
-    assert_equal(v, "Female")
+    assert_equal(v, 1)
   end
 
   test "simple value generator with nested type" do
@@ -54,12 +53,14 @@ class EntityTest < ActiveSupport::TestCase
   
   test "get students" do
     Entity.url_type = "students"
+    Entity._format = ActiveResource::Formats::JsonFormat
     students = Entity.all
     assert_equal(students.size, @student_fixtures.size)
   end
   
   test "get teachers" do
     Entity.url_type = "teachers"
+    Entity._format = ActiveResource::Formats::JsonFormat
     students = Entity.all
     assert_equal(students.size, @teacher_fixtures.size)
   end
