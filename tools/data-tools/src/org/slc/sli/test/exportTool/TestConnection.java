4a478dd8b4872e2ee4d748a9c4d780a1a8d05a66
Yuan Zhang <yzhang@wgen.net>
2012-02-16 11:20:47 -0500

    initial checkin for test data exporting tools

---
+++ tools/data-tools/src/org/slc/sli/test/exportTool/TestConnection.java
@@ -0,0 +1,37 @@
+package org.slc.sli.test.exportTool;
+
+import java.sql.*;
+
+public class TestConnection
+{
+    public static void main(String[] args)
+    {
+        DB db = new DB();
+        db.dbConnect();
+    }
+}
+
+class DB
+{
+    public DB() {}
+
+    public void dbConnect()
+    {
+        try
+        {
+            Connection conn = DBUtil.getConnection();
+            System.out.println("connected");
+
+            Statement st = conn.createStatement();
+            ResultSet rec = st.executeQuery("SELECT StudentUSI,PersonalTitlePrefixTypeId,FirstName,MiddleName,LastSurname,GenerationCodeSuffixTypeId,MaidenName,PersonalInformationVerificationTypeId,SexTypeId,BirthDate,CityOfBirth,StateOfBirthAbbreviationTypeId,CountryOfBirthCodeTypeId,DateEnteredUS,MultipleBirthStatus,ProfileThumbnail,HispanicLatinoEthnicity,OldEthnicityTypeId,EconomicDisadvantaged,SchoolFoodServicesEligibilityTypeId,LimitedEnglishProficiencyTypeId,DisplacementStatusType,LoginId FROM Student");
+            while (rec.next()) {
+            	System.out.println(rec.getString("StudentUSI"));
+            }
+            st.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+};
