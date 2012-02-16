92b06436fa89a09490f0ebc7ede2d79fab1fc93b
William Scott <wscott@wgen.net>
2012-02-16 13:11:49 -0500

    Fix checkstyle errors.

--- sli/api/src/main/java/org/slc/sli/api/resources/HomeResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/HomeResource.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.Response.Status;
 
 import org.slc.sli.api.config.EntityDefinition;
 import org.slc.sli.api.config.EntityDefinitionStore;
@@ -36,18 +35,18 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("request")
 @Produces({ Resource.JSON_MEDIA_TYPE, Resource.XML_MEDIA_TYPE, Resource.SLC_XML_MEDIA_TYPE,
-        Resource.SLC_JSON_MEDIA_TYPE })
+    Resource.SLC_JSON_MEDIA_TYPE })
 public class HomeResource {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(HomeResource.class);
-    
+
     final EntityDefinitionStore entityDefs;
-    
+
     @Autowired
     HomeResource(EntityDefinitionStore entityDefs) {
         this.entityDefs = entityDefs;
     }
-    
+
     /**
      * Returns the initial information when a user logs in.
      * This includes a self link for the user's info, i.e. /staff/{GUID}.
@@ -62,37 +61,37 @@ public class HomeResource {
      */
     @GET
     public Response getHomeUri(@Context final UriInfo uriInfo) {
-        
+
         Home home = null;
-        
+
         // get the entity ID and EntityDefinition for user
         Pair<String, EntityDefinition> pair = this.getEntityInfoForUser();
         if (pair != null) {
             String userId = pair.getLeft();
             EntityDefinition defn = pair.getRight();
-            
+
             // prepare a list of links with the self link
             List<EmbeddedLink> links = ResourceUtil.getSelfLink(uriInfo, userId, defn);
-            
+
             // add links for all of the entity's associations for this ID
             links.addAll(ResourceUtil.getAssociationsLinks(this.entityDefs, defn, userId, uriInfo));
-            
+
             // add the aggregation link
             links.addAll(ResourceUtil.getAggregateLink(uriInfo));
-            
+
             // create a final map of links to relevant links
             HashMap<String, Object> linksMap = new HashMap<String, Object>();
             linksMap.put(ResourceConstants.LINKS, links);
-            
+
             // return as browser response
             home = new Home(defn.getStoredCollectionName(), linksMap);
         } else {
             throw new InsufficientAuthenticationException("No entity mapping found for user");
         }
-        
+
         return Response.ok(home).build();
     }
-    
+
     /**
      * Analyzes security context to get ID and EntityDefinition for user.
      * 
@@ -100,14 +99,14 @@ public class HomeResource {
      */
     private Pair<String, EntityDefinition> getEntityInfoForUser() {
         Pair<String, EntityDefinition> pair = null;
-        
+
         // get the Entity for the logged in user
         Entity entity = ResourceUtil.getSLIPrincipalFromSecurityContext().getEntity();
         if (entity != null) {
             EntityDefinition entityDefinition = this.entityDefs.lookupByEntityType(entity.getType());
             pair = Pair.of(entity.getEntityId(), entityDefinition);
         }
-        
+
         return pair;
     }
 }
--- sli/api/src/main/java/org/slc/sli/api/resources/v1/StudentResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/v1/StudentResource.java
@@ -23,21 +23,24 @@ import org.springframework.stereotype.Component;
 import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.representation.EntityBody;
 
+/**
+ * 
+ */
 @Path(PathConstants.V1 + "/" + PathConstants.STUDENTS)
 @Component
 @Scope("request")
 @Produces({ MediaType.APPLICATION_JSON, HypermediaType.VENDOR_SLC_JSON })
 public class StudentResource implements CrudEndpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentResource.class);
-    
+
     private static final String RESOURCE_NAME_STUDENTS = "students";
     private final CrudEndpoint crudDelegate;
-    
+
     @Autowired
     public StudentResource(final EntityDefinitionStore entityDefs) {
         crudDelegate = new DefaultCrudEndpoint(entityDefs, RESOURCE_NAME_STUDENTS, LOGGER);
     }
-    
+
     /**
      * Returns all Student entities.
      * 
@@ -49,7 +52,7 @@ public class StudentResource implements CrudEndpoint {
             @QueryParam(ParameterConstants.LIMIT) @DefaultValue("50") final int limit, @Context final UriInfo uriInfo) {
         return crudDelegate.readAll(offset, limit, uriInfo);
     }
-    
+
     /**
      * Get a single student entity
      * 
@@ -75,7 +78,7 @@ public class StudentResource implements CrudEndpoint {
             @Context final UriInfo uriInfo) {
         return crudDelegate.read(idList, fullEntities, uriInfo);
     }
-    
+
     /**
      * Returns all the student-school-associations in the context of the specified student.
      * 
@@ -132,7 +135,7 @@ public class StudentResource implements CrudEndpoint {
          * });
          */
     }
-    
+
     /**
      * Create a new student resource.
      * 
@@ -148,7 +151,7 @@ public class StudentResource implements CrudEndpoint {
     public Response create(final EntityBody newEntityBody, @Context final UriInfo uriInfo) {
         return crudDelegate.create(newEntityBody, uriInfo);
     }
-    
+
     /**
      * Delete a student resource.
      * 
@@ -164,7 +167,7 @@ public class StudentResource implements CrudEndpoint {
     public Response delete(@PathParam(ParameterConstants.STUDENT_ID) final String id) {
         return crudDelegate.delete(id);
     }
-    
+
     /**
      * Update an existing student resource.
      * 
