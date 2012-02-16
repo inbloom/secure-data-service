d62d7ddbe47e45a32faa4f82ed7bdbf9295e6caf
Nicholas Brown <nbrown@wgen.net>
2012-02-16 13:10:49 -0500

    have entity definition store use improved builder

--- sli/api/src/main/java/org/slc/sli/api/config/DefinitionFactory.java
+++ sli/api/src/main/java/org/slc/sli/api/config/DefinitionFactory.java
@@ -16,71 +16,86 @@ import org.slc.sli.domain.EntityRepository;
 
 /**
  * Factory class for building Entity and Association definition objects.
- *
+ * 
  * The quantity of fluent builder code is large enough to warrant capturing it in a factory class,
  * yet breaking this into two factories (entity and association) seemed unneccesarily complex.
- *
+ * 
  * @author Ryan Farris <rfarris@wgen.net>
- *
+ * 
  */
 @Component
 public class DefinitionFactory {
-
+    
     @Autowired
-    private EntityRepository   defaultRepo;
-
+    private EntityRepository defaultRepo;
+    
     @Autowired
-    private IdTreatment        idTreatment;
-
+    private IdTreatment idTreatment;
+    
     @Autowired
-    private TypeTreatment      typeTreatment;
-
+    private TypeTreatment typeTreatment;
+    
     @Autowired
     private ApplicationContext beanFactory;
-
+    
     public EntityBuilder makeEntity(String type) {
-        return new EntityBuilder(type);
+        return new EntityBuilder(type, type);
     }
-
+    
+    public EntityBuilder makeEntity(String type, String resourceName) {
+        return new EntityBuilder(type, resourceName);
+    }
+    
     public AssocBuilder makeAssoc(String type) {
         return new AssocBuilder(type);
     }
-
+    
     /**
      * Fluent builder for EntityDefinitions.
      * This class is not static; it requires fields from it's parent factory to build the EntityDef.
-     *
+     * 
      * @author Nick Brown <nbrown@wgen.net>
      * @author Ryan Farris <rfarris@wgen.net>
-     *
+     * 
      */
     public class EntityBuilder {
-        protected String           type;
-        protected String           collectionName;
-        protected String           resourceName;
-        protected List<Treatment>  treatments = new ArrayList<Treatment>();
+        protected String type;
+        protected String collectionName;
+        protected String resourceName;
+        protected List<Treatment> treatments = new ArrayList<Treatment>();
         protected EntityRepository repo;
-
+        
         /**
-         * Create a builder for an entity definition
-         *
-         * @param collectionName
-         *            the name of the entity in the db
-         * @param resourceName
-         *            the name of the entity in the ReST uri
+         * Create a builder for an entity definition. The collection name and resource name will
+         * default to the type
+         * 
+         * @param type
+         *            the type of the entity.
          */
         EntityBuilder(String type) {
+            this(type, type);
+        }
+        
+        /**
+         * Create a builder for an entity definition. The collection name will default to the type
+         * 
+         * @param type
+         *            the type of the entity.
+         * @param resourceName
+         *            the name of the resource the entity will be exposed as
+         */
+        EntityBuilder(String type, String resourceName) {
             this.type = type;
             this.collectionName = type;
-            this.resourceName = type;
+            this.resourceName = resourceName;
             this.repo = DefinitionFactory.this.defaultRepo;
             this.treatments.add(DefinitionFactory.this.idTreatment);
             this.treatments.add(DefinitionFactory.this.typeTreatment);
         }
-
+        
         /**
          * Add a list of treatments to the definition
-         *
+         * 
          * @param treatments
          *            the list of treatments
          * @return the builder
@@ -89,10 +104,10 @@ public class DefinitionFactory {
             this.treatments.addAll(Arrays.asList(treatments));
             return this;
         }
-
+        
         /**
          * Sets the collection to store the entities in
-         *
+         * 
          * @param collectionName
          *            the name of the collection to store the entities in, if different from the
          *            entity name
@@ -102,10 +117,10 @@ public class DefinitionFactory {
             this.collectionName = collectionName;
             return this;
         }
-
+        
         /**
          * Set the repository the service layer will use to get/update/delete/query entities
-         *
+         * 
          * @param repo
          * @return
          */
@@ -113,11 +128,11 @@ public class DefinitionFactory {
             this.repo = repo;
             return this;
         }
-
+        
         /**
          * Sets the name of the ReSTful resource to expose the resource as, if different from the
          * entity name
-         *
+         * 
          * @param resourceName
          *            the name of the ReSTful resource to expose the resource as
          * @return the builder
@@ -126,16 +141,17 @@ public class DefinitionFactory {
             this.resourceName = resourceName;
             return this;
         }
-
+        
         /**
          * Create the actual entity definition
-         *
+         * 
          * @return the entity definition
          */
         public EntityDefinition build() {
-
-            BasicService entityService = (BasicService) DefinitionFactory.this.beanFactory.getBean("basicService", collectionName, treatments);
-
+            
+            BasicService entityService = (BasicService) DefinitionFactory.this.beanFactory.getBean("basicService",
+                    collectionName, treatments);
+            
             EntityDefinition entityDefinition = new EntityDefinition(type, resourceName, collectionName, entityService);
             entityService.setDefn(entityDefinition);
             return entityDefinition;
@@ -143,65 +159,67 @@ public class DefinitionFactory {
         
         /**
          * Create the actual entity definition
-         *
+         * 
          * @return the entity definition
          */
         public EntityDefinition buildAndRegister(BasicDefinitionStore store) {
-
+            
             EntityDefinition entityDefinition = this.build();
             store.addDefinition(entityDefinition);
             return entityDefinition;
         }
     }
-
+    
     /**
      * Fluent builder for AssocBuilder.
      */
     public class AssocBuilder extends EntityBuilder {
         private EntityInfo source;
         private EntityInfo target;
-        private String     relNameFromSource;
-        private String     relNameFromTarget;
-
+        private String relNameFromSource;
+        private String relNameFromTarget;
+        
         /**
          * Create a builder for an association definition
-         *
+         * 
          * @param collectionName
          *            the name of the association in the db
          * @param resourceName
          *            the name of the association in the ReST uri
          */
         AssocBuilder(String type) {
-            super(type);
+            super(type, type);
         }
-
+        
         /**
          * Sets the source definition. The link will get name get{type} and the id will be {type}Id
-         *
+         * 
          * @param source
          *            the source of the association
          * @return the builder
          */
         public AssocBuilder from(EntityDefinition source) {
-            this.source = new EntityInfo(source, "get" + StringUtils.capitalize(source.getType()), source.getType() + "Id");
+            this.source = new EntityInfo(source, "get" + StringUtils.capitalize(source.getType()), source.getType()
+                    + "Id");
             return this;
         }
-
+        
         /**
          * Sets the target definition. The link will get name get{Type} and the id will be {type}Id
-         *
+         * 
          * @param target
          *            the target definition
          * @return the builder
          */
         public AssocBuilder to(EntityDefinition target) {
-            this.target = new EntityInfo(target, "get" + StringUtils.capitalize(target.getType()), target.getType() + "Id");
+            this.target = new EntityInfo(target, "get" + StringUtils.capitalize(target.getType()), target.getType()
+                    + "Id");
             return this;
         }
-
+        
         /**
          * Sets the source definition
-         *
+         * 
          * @param source
          *            the source of the association
          * @param sourceLink
@@ -214,10 +232,10 @@ public class DefinitionFactory {
             this.source = new EntityInfo(source, sourceLink, hopLink, source.getType() + "Id");
             return this;
         }
-
+        
         /**
          * Sets the source definition
-         *
+         * 
          * @param source
          *            the source of the association
          * @param sourceLink
@@ -232,10 +250,10 @@ public class DefinitionFactory {
             this.source = new EntityInfo(source, sourceLink, hopLink, sourceKey);
             return this;
         }
-
+        
         /**
          * Sets the target definition
-         *
+         * 
          * @param target
          *            the target definition
          * @param targetLink
@@ -248,10 +266,10 @@ public class DefinitionFactory {
             this.target = new EntityInfo(target, targetLink, hopLink, target.getType() + "Id");
             return this;
         }
-
+        
         /**
          * Sets the target definition
-         *
+         * 
          * @param target
          *            the target definition
          * @param targetLink
@@ -266,10 +284,10 @@ public class DefinitionFactory {
             this.target = new EntityInfo(target, targetLink, hopLink, targetKey);
             return this;
         }
-
+        
         /**
          * The name of the link from either entity to the relationship
-         *
+         * 
          * @param relName
          * @return
          */
@@ -278,10 +296,10 @@ public class DefinitionFactory {
             this.relNameFromTarget = relName;
             return this;
         }
-
+        
         /**
          * The name of the link on the source entity to the relationship
-         *
+         * 
          * @param relName
          * @return
          */
@@ -289,10 +307,10 @@ public class DefinitionFactory {
             this.relNameFromSource = relName;
             return this;
         }
-
+        
         /**
          * The name of the link of the target entity of the relationship
-         *
+         * 
          * @param relName
          * @return
          */
@@ -300,59 +318,62 @@ public class DefinitionFactory {
             this.relNameFromTarget = relName;
             return this;
         }
-
+        
         @Override
         public AssocBuilder withTreatments(Treatment... treatments) {
             super.withTreatments(treatments);
             return this;
         }
-
+        
         @Override
         public AssocBuilder storeAs(String collectionName) {
             super.storeAs(collectionName);
             return this;
         }
-
+        
         @Override
         public AssocBuilder storeIn(EntityRepository repo) {
             super.storeIn(repo);
             return this;
         }
-
+        
         @Override
         public AssocBuilder exposeAs(String resourceName) {
             super.exposeAs(resourceName);
             return this;
         }
-
+        
         @Override
         public AssociationDefinition build() {
-
-            BasicAssocService service = (BasicAssocService) DefinitionFactory.this.beanFactory.getBean("basicAssociationService", collectionName, treatments, source.getDefn(), source.getKey(), target.getDefn(), target.getKey());
-
+            
+            BasicAssocService service = (BasicAssocService) DefinitionFactory.this.beanFactory.getBean(
+                    "basicAssociationService", collectionName, treatments, source.getDefn(), source.getKey(),
+                    target.getDefn(), target.getKey());
+            
             source.setLinkToAssociation(this.relNameFromSource);
             target.setLinkToAssociation(this.relNameFromTarget);
-            AssociationDefinition associationDefinition = new AssociationDefinition(super.type, super.resourceName, super.collectionName, service, source, target);
+            AssociationDefinition associationDefinition = new AssociationDefinition(super.type, super.resourceName,
+                    super.collectionName, service, source, target);
             service.setDefn(associationDefinition);
             return associationDefinition;
         }
     }
-
+    
     /**
      * Holder class for entity information
      * Mostly so checkstyle doesn't whine about private association definition methods having too
      * many parameters
-     *
+     * 
      * @author nbrown
-     *
+     * 
      */
     public static class EntityInfo {
         private final EntityDefinition defn;
-        private final String           linkName;
-        private final String           key;
-        private final String           hopLinkName;
-        private String                 linkToAssociation;
-
+        private final String linkName;
+        private final String key;
+        private final String hopLinkName;
+        private String linkToAssociation;
+        
         public EntityInfo(EntityDefinition defn, String linkName, String hopLinkName, String key) {
             super();
             this.defn = defn;
@@ -360,7 +381,7 @@ public class DefinitionFactory {
             this.hopLinkName = hopLinkName;
             this.key = key;
         }
-
+        
         public EntityInfo(EntityDefinition defn, String linkName, String key) {
             super();
             this.defn = defn;
@@ -368,30 +389,30 @@ public class DefinitionFactory {
             this.hopLinkName = linkName;
             this.key = key;
         }
-
+        
         public EntityDefinition getDefn() {
             return defn;
         }
-
+        
         public String getLinkName() {
             return linkName;
         }
-
+        
         public String getHopLinkName() {
             return hopLinkName;
         }
-
+        
         public String getKey() {
             return key;
         }
-
+        
         public void setLinkToAssociation(String linkToAssociation) {
             this.linkToAssociation = linkToAssociation;
         }
-
+        
         public String getLinkToAssociation() {
             return linkToAssociation;
         }
-
+        
     }
 }
