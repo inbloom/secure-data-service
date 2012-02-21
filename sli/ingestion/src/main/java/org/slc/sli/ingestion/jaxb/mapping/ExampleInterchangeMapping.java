d31c495a3a8dd41de6ce235e6e2dc18bd57cad2a
Danilo Duran <dduran@wgen.net>
2012-02-21 01:40:03 -0500

    update example after map generator changes

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/jaxb/mapping/ExampleInterchangeMapping.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/jaxb/mapping/ExampleInterchangeMapping.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import org.junit.Test;
-
 import org.slc.sli.ingestion.jaxb.util.MappingUtils;
-
 
 /**
  * Mapping EdFi InterchangeAssessmentMetaData to SLI model
@@ -18,31 +15,34 @@ import org.slc.sli.ingestion.jaxb.util.MappingUtils;
  */
 public class ExampleInterchangeMapping {
 
-    @Test
     public void testUnmarshal() throws JAXBException, IOException {
         String assessmentInterchangeFile = "edfiXsd/assessment1.xml";
-        Unmarshaller u = MappingUtils.createEdFiUnmarshaller();
+        Unmarshaller u = MappingUtils.createUnmarshallerForPackage("org.slc.sli.ingestion.jaxb.domain.edfi");
 
         // unmarshal file
         /*
-        InterchangeAssessmentMetadata interchangeAssessmentMetadata = (InterchangeAssessmentMetadata) u
-                .unmarshal(JaxbTest.class.getClassLoader().getResourceAsStream(assessmentInterchangeFile));
-
-        for (org.slc.sli.domain.edfi.ComplexObjectType assessmentComplexObject : interchangeAssessmentMetadata
-                .getAssessmentFamilyOrAssessmentOrAssessmentPeriodDescriptor()) {
-
-            if (assessmentComplexObject instanceof Assessment) {
-
-                org.slc.sli.domain.sli.Assessment sliAssessment = AssessmentMapping
-                        .mapAssessment((Assessment) assessmentComplexObject);
-                System.out.println(SerializationUtils.writeJson(sliAssessment));
-            } else if (assessmentComplexObject instanceof StudentAssessment) {
-
-                org.slc.sli.domain.sli.StudentAssessmentAssociation sliAssessment = StudentAssessmentAssociationMapping
-                        .mapStudentAssessmentAssociation((StudentAssessment) assessmentComplexObject);
-                System.out.println(SerializationUtils.writeJson(sliAssessment));
-            }
-        }
-        */
+         * InterchangeAssessmentMetadata interchangeAssessmentMetadata =
+         * (InterchangeAssessmentMetadata) u
+         * .unmarshal(JaxbTest.class.getClassLoader().getResourceAsStream(assessmentInterchangeFile))
+         * ;
+         *
+         * for (org.slc.sli.domain.edfi.ComplexObjectType assessmentComplexObject :
+         * interchangeAssessmentMetadata
+         * .getAssessmentFamilyOrAssessmentOrAssessmentPeriodDescriptor()) {
+         *
+         * if (assessmentComplexObject instanceof Assessment) {
+         *
+         * org.slc.sli.domain.sli.Assessment sliAssessment = AssessmentMapping
+         * .mapAssessment((Assessment) assessmentComplexObject);
+         * System.out.println(SerializationUtils.writeJson(sliAssessment));
+         * } else if (assessmentComplexObject instanceof StudentAssessment) {
+         *
+         * org.slc.sli.domain.sli.StudentAssessmentAssociation sliAssessment =
+         * StudentAssessmentAssociationMapping
+         * .mapStudentAssessmentAssociation((StudentAssessment) assessmentComplexObject);
+         * System.out.println(SerializationUtils.writeJson(sliAssessment));
+         * }
+         * }
+         */
     }
 }
