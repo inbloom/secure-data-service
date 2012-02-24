65ebd08aa25a8afe553b5067c3a4182de73140f1
Aaron Saarela <asaarela@wgen.net>
2012-02-24 14:09:30 -0500

    First pass checkpoint.

---
+++ sli/SDK/src/main/java/org/slc/sli/api/client/URLBuilder.java
@@ -0,0 +1,129 @@
+package org.slc.sli.api.client;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+
+/**
+ * Builder for creating a URL suitable for use with the SLI API ReSTful web service.
+ */
+public final class URLBuilder {
+    private static final String ENCODING = "UTF-8";
+    
+    private final StringBuffer url = new StringBuffer();
+    
+    // Private constructor, use 'create' to start the build process.
+    private URLBuilder(final String base) {
+        url.append(base);
+    }
+    
+    /**
+     * Start building a new URL with the provided base location.
+     * 
+     * @param baseUrl
+     *            - base URL of the ReSTful API.
+     * @return URLBuilder instance
+     */
+    public static URLBuilder create(final String baseUrl) {
+        return new URLBuilder(baseUrl);
+    }
+    
+    /**
+     * Append a path fragment to the current URL path.
+     * 
+     * @param path
+     *            URL fragment to add.
+     * @return Updated URLBuilder instance.
+     */
+    public URLBuilder addPath(final String path) {
+        addPathSeparaterIfNeeded();
+        url.append(path);
+        return this;
+    }
+    
+    /**
+     * Append a path element for accessing the provided entity type.
+     * 
+     * @param type
+     *            Entity type of interest.
+     * @return Updated URLBuilder instance.
+     */
+    public URLBuilder entityType(final EntityType type) {
+        addPath(type.getURL());
+        return this;
+    }
+    
+    /**
+     * Append an entity id to the path
+     * 
+     * @Param id Entity ID
+     * @return Updated URLBuilder instance.
+     */
+    public URLBuilder id(final String id) {
+        addPath(id);
+        return this;
+    }
+    
+    /**
+     * Apply the given query to the URL.
+     * 
+     * @param query
+     * @return Updated URLBuilder instance.
+     */
+    public URLBuilder query(final Query query) {
+        
+        Map<String, Object> params = query.getParameters();
+        for (Map.Entry<String, Object> entry : params.entrySet()) {
+            addQueryParameter(entry.getKey(), entry.getValue());
+        }
+        
+        return this;
+    }
+    
+    /**
+     * Builds the URL.
+     * 
+     * @return URL represented by the values set in this builder.
+     * @throws MalformedURLException
+     *             if the URL is not valid.
+     */
+    public URL build() throws MalformedURLException {
+        return new URL(url.toString());
+    }
+    
+    /**
+     * Add a URL Query parameter to the URL.
+     * 
+     * @param key
+     *            query parameter name
+     * @param value
+     *            query parameter value
+     * @return Updated URLBuilder instance.
+     */
+    private URLBuilder addQueryParameter(final String key, final Object value) {
+        if (url.charAt(url.length() - 1) == '/') {
+            url.deleteCharAt(url.length() - 1);
+        }
+        if (url.indexOf("?") < 0) {
+            url.append("?");
+        } else if (url.charAt(url.length() - 1) != '&') {
+            url.append("&");
+        }
+        url.append(key).append("=");
+        try {
+            url.append(URLEncoder.encode(value.toString(), ENCODING));
+        } catch (UnsupportedEncodingException e) {
+            url.append(value);
+        }
+        return this;
+    }
+    
+    private URLBuilder addPathSeparaterIfNeeded() {
+        if (url.charAt(url.length() - 1) != '/') {
+            url.append("/");
+        }
+        return this;
+    }
+}
---
+++ sli/SDK/src/main/java/org/slc/sli/api/client/impl/RESTClient.java
@@ -0,0 +1,179 @@
+package org.slc.sli.api.client.impl;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.slc.sli.api.client.Constants;
+import org.slc.sli.api.client.URLBuilder;
+
+/**
+ * Generic REST client. Provides the ability to connect to a ReSTful web service and make
+ * requests.
+ */
+public class RESTClient {
+    
+    /** Request parameter key used to pass sessionId to API **/
+    private static final String API_SESSION_KEY = "sessionId";
+    
+    private static Logger logger = LoggerFactory.getLogger(RESTClient.class);
+    
+    private String sessionToken = null;
+    
+    private String apiServerUri;
+    
+    private Client client = null;
+    
+    /**
+     * Construct a new RESTClient instance, using the JSON message converter.
+     */
+    protected RESTClient() {
+        
+        ClientConfig config = new DefaultClientConfig();
+        config.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        
+        // TODO - implement SSL configuration here...
+        
+        client = Client.create(config);
+    }
+    
+    /**
+     * Call the session/check API
+     *
+     * @return JsonOject as described by API documentation
+     * @throws NoSessionException
+     */
+    public JsonObject sessionCheck() {
+        try {
+            String response = getRequest(Constants.SESSION_CHECK_PATH);
+            JsonParser parser = new JsonParser();
+            return parser.parse(response).getAsJsonObject();
+            
+        } catch (MalformedURLException e2) {
+            logger.error("Failed to check the user session with the API server: " + e2.getLocalizedMessage());
+            
+        } catch (URISyntaxException e3) {
+            logger.error("Failed to check the user session with the API server: " + e3.getLocalizedMessage());
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Make a request to a REST service and convert the result to JSON
+     * 
+     * @param path
+     *            the unique portion of the requested REST service URL
+     * @return valid JSON if the request is successful, otherwise null.
+     * @throws MalformedURLException
+     *             URISyntaxException
+     */
+    public String getRequest(final String path) throws MalformedURLException,
+    URISyntaxException {
+        
+        URLBuilder url = URLBuilder.create(apiServerUri);
+        url.addPath(path);
+        return getRequest(url.build());
+    }
+    
+    /**
+     * Make a request to a REST service and convert the result to JSON
+     * 
+     * @param url
+     *            full URL to the request.
+     * @return valid JSON if the request is successful, otherwise null.
+     * @throws MalformedURLException
+     *             URISyntaxException
+     */
+    public String getRequest(final URL url) throws MalformedURLException, URISyntaxException {
+        
+        return getRequestWithHeaders(url, null);
+    }
+    
+    /**
+     * Make a request to a REST service and convert the result to JSON. The request
+     * includes additional header information.
+     * 
+     * @param path
+     *            the unique portion of the requested REST service URL
+     * @param headers
+     *            key / value pairs of the headers to attach to the request. A key can map
+     *            to multiple values.
+     * @return valid JSON if the request is successful, otherwise null.
+     */
+    public String getRequestWithHeaders(final URL url, final Map<String, List<String>> headers)
+            throws URISyntaxException {
+        
+        if (sessionToken != null) {
+            ClientRequest.Builder builder = new ClientRequest.Builder();
+            builder.header(API_SESSION_KEY, sessionToken);
+            
+            if (headers != null) {
+                for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+                    builder.header(entry.getKey(), entry.getValue());
+                }
+            }
+            
+            builder.build(url.toURI(), "GET");
+            ClientRequest request = builder.build(url.toURI(), HttpMethod.GET);
+            
+            WebResource resource = client.resource(url.toURI());
+            
+            ClientResponse response = resource.accept(MediaType.APPLICATION_JSON).post(ClientResponse.class, request);
+            
+            return response.toString();
+        }
+        
+        logger.error("Token is null in call to RESTClient for url" + url);
+        return null;
+    }
+    
+    /**
+     * @param host
+     *            Host running the SLI API ReSTful service.
+     * @param port
+     *            Port for the service.
+     * @param user
+     *            SLI authorized username.
+     * @param password
+     *            user password.
+     * @param realm
+     *            IDP authentication realm.
+     */
+    public void openSession(final String host, final int port, final String user, final String password,
+            final String realm) {
+        
+        apiServerUri = new String("https://" + host + ":" + port + "/" + Constants.API_SERVER_PATH);
+        
+        // TODO -- Log into the IDP and get a Session Token. Waiting on ReST call from LuckyStrike.
+        sessionToken = "TODO";
+        
+    }
+    
+    /**
+     * Get the base URL for all SLI API ReSTful service calls.
+     * 
+     * @return
+     */
+    public String getBaseURL() {
+        return apiServerUri;
+    }
+}
