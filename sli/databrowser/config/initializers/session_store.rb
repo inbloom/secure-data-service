a6caf1cef004bbdd91017c9aad0d143ec446d109
Ryan Latta <rlatta@wgen.net>
2012-02-28 16:12:02 -0500

    cookies are http only for rails apps.

--- sli/databrowser/config/initializers/session_store.rb
+++ sli/databrowser/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DbRails::Application.config.session_store :cookie_store, key: '_data_prowler'
+DbRails::Application.config.session_store :cookie_store, key: '_data_prowler', :httponly => true
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
