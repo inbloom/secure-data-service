9c6512b8fc806a9eb35550d58c7f1f49ec693465
William Scott <wscott@wgen.net>
2012-02-28 12:30:47 -0500

    Add negative testing for Section and StudentSectionAssociation.

--- sli/common/domain/src/test/java/org/slc/sli/validation/StudentSectionAssociationTest.java
+++ sli/common/domain/src/test/java/org/slc/sli/validation/StudentSectionAssociationTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.ExpectedException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,28 +19,27 @@ import org.slc.sli.domain.Entity;
 
 /**
  * Test for validation of Student Section associations
- * 
+ *
  * @author nbrown
- * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+@ContextConfiguration(locations = {"/spring/applicationContext-test.xml"})
 public class StudentSectionAssociationTest {
-    
+
     @Autowired
     private EntityValidator validator;
-    
+
     @Autowired
     private DummyEntityRepository repo;
-    
+
     @Before
     public void init() {
         repo.clean();
         repo.addEntity("student", "Calvin", makeDummyEntity("student", "Calvin"));
         repo.addEntity("section", "Math Class", makeDummyEntity("section", "Math Class"));
     }
-    
-    private Entity goodAssociation() throws ParseException {
+
+    private Entity goodAssociation() {
         final Map<String, Object> goodSection = new HashMap<String, Object>();
         goodSection.put("studentId", "Calvin");
         goodSection.put("sectionId", "Math Class");
@@ -48,33 +48,42 @@ public class StudentSectionAssociationTest {
         goodSection.put("homeroomIndicator", true);
         goodSection.put("repeatIdentifier", "Repeated, not counted in grade point average");
         return new Entity() {
-            
+
             @Override
             public String getType() {
                 return "studentSectionAssociation";
             }
-            
+
             @Override
             public String getEntityId() {
                 return "id";
             }
-            
+
             @Override
             public Map<String, Object> getBody() {
                 return goodSection;
             }
-            
+
             @Override
             public Map<String, Object> getMetaData() {
                 return new HashMap<String, Object>();
             }
         };
     }
-    
+
     @Test
-    public void testGoodAssociation() throws ParseException {
+    public void testGoodAssociation() {
         Entity goodAssociation = goodAssociation();
         assertTrue(validator.validate(goodAssociation));
     }
-    
+
+    @Test
+    @ExpectedException(value = EntityValidationException.class)
+    public void testInvalidStudentSectionAssociation() {
+        Entity goodAssociation = goodAssociation();
+        goodAssociation.getBody().put("studentId", "INVALID");
+        goodAssociation.getBody().put("sectionId", "INVALID");
+        validator.validate(goodAssociation);
+    }
+
 }
