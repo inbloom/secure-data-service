7896666c1a5278ad0739ffc0acb15336e0008679
tshewchuk <tshewchuk@wgen.net>
2012-03-01 18:09:10 -0500

    US1226 - Pull2

---
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/init/MongoIndexLoader.java
@@ -0,0 +1,45 @@
+package org.slc.sli.dal.init;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+import org.springframework.data.mongodb.core.index.IndexDefinition;
+import org.springframework.data.mongodb.core.query.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Creates compound indices on mongo database collections.
+ * 
+ * @author shalka
+ * 
+ */
+@Component
+public class MongoIndexLoader implements InitializingBean {
+    private static final Logger LOG = LoggerFactory.getLogger(MongoIndexLoader.class);
+    private static final Order ASCENDING = Order.ASCENDING;
+    private static final String ATTENDANCE_EVENT_COLLECTION = "attendanceEvent";
+    
+    @Autowired(required = false)
+    private MongoTemplate mongoTemplate;
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (this.mongoTemplate != null) {
+            
+            LOG.debug("Creating compound index on attendanceEvent collection");
+            IndexDefinition indexDefinition = new Index().on("body.eventDate", ASCENDING)
+                    .on("body.studentId", ASCENDING).on("body.attendanceEventCategory", ASCENDING).unique();
+            try {
+                mongoTemplate.ensureIndex(indexDefinition, ATTENDANCE_EVENT_COLLECTION);
+                LOG.debug("Successfully created compound index on attendanceEvent collection");
+            } catch(Exception e) {
+                LOG.warn("There was an error creating index");
+            }            
+        }
+    }
+}
