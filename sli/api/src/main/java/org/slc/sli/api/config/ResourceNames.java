7c4952fae9b62c052d83d00457fc96f258b8ed5a
kmyers <kmyers@wgen.net>
2012-03-02 14:52:31 -0500

    Direct entity reference support

--- sli/api/src/main/java/org/slc/sli/api/config/ResourceNames.java
+++ sli/api/src/main/java/org/slc/sli/api/config/ResourceNames.java
@@ -1,5 +1,9 @@
 package org.slc.sli.api.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * Defines for resource names exposed by API.
  * 
@@ -28,7 +32,6 @@ public final class ResourceNames {
     public static final String STUDENTS = "students";
     public static final String TEACHERS = "teachers";
     
-    public static final String EDUCATION_ORGANIZATION_SCHOOL_ASSOCIATIONS = "educationOrganization-school-associations";
     public static final String SCHOOL_SESSION_ASSOCIATIONS = "school-session-associations";
     public static final String SECTION_ASSESSMENT_ASSOCIATIONS = "section-assessment-associations";
     public static final String SESSION_COURSE_ASSOCIATIONS = "session-course-associations";
@@ -41,4 +44,56 @@ public final class ResourceNames {
     public static final String EDUCATION_ORGANIZATION_ASSOCIATIONS = "educationOrganization-associations";
     public static final String COURSE_SECTION_ASSOCIATIONS = "course-section-associations";
     
+    
+    public static final Map<String, String> ENTITY_RESOURCE_NAME_MAPPING = new HashMap<String, String>();
+    
+
+    /*
+     * This map indicates how to display a link to a particular resource. This map assumes
+     * that the destination is a single entity and not a collection.
+     */
+    public static final Map<String, String> SINGULAR_LINK_NAMES = new HashMap<String, String>();
+    static {
+        SINGULAR_LINK_NAMES.put(ResourceNames.STUDENT_SCHOOL_ASSOCIATIONS, "getStudentSchoolAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.TEACHER_SCHOOL_ASSOCIATIONS, "getTeacherSchoolAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.TEACHER_SECTION_ASSOCIATIONS, "getTeacherSectionAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SCHOOL_SESSION_ASSOCIATIONS, "getSchoolSessionAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SECTION_ASSESSMENT_ASSOCIATIONS, "getSectionAssessmentAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SESSION_COURSE_ASSOCIATIONS, "getSessionCourseAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.STUDENT_ASSESSMENT_ASSOCIATIONS, "getStudentAssessmentAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.STUDENT_SECTION_ASSOCIATIONS, "getStudentSectionAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.STAFF_EDUCATION_ORGANIZATION_ASSOCIATIONS, "getStaffEducationOrganizationAssociation");
+        SINGULAR_LINK_NAMES.put(ResourceNames.EDUCATION_ORGANIZATION_ASSOCIATIONS, "getEducationOrganization");
+        SINGULAR_LINK_NAMES.put(ResourceNames.COURSE_SECTION_ASSOCIATIONS, "getCourseSectionAssociation");
+        
+        SINGULAR_LINK_NAMES.put(ResourceNames.ASSESSMENTS, "getAssessment");
+        SINGULAR_LINK_NAMES.put(ResourceNames.BELL_SCHEDULES, "getBellSchedule");
+        SINGULAR_LINK_NAMES.put(ResourceNames.COHORTS, "getCohort");
+        SINGULAR_LINK_NAMES.put(ResourceNames.COURSES, "getCourse");
+        SINGULAR_LINK_NAMES.put(ResourceNames.DISCIPLINE_INCIDENTS, "getDisciplineIncident");
+        SINGULAR_LINK_NAMES.put(ResourceNames.EDUCATION_ORGANIZATIONS, "getEducationOrganization");
+        SINGULAR_LINK_NAMES.put(ResourceNames.PARENTS, "getParent");
+        SINGULAR_LINK_NAMES.put(ResourceNames.PROGRAMS, "getProgram");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SECTIONS, "getSection");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SESSIONS, "getSession");
+        SINGULAR_LINK_NAMES.put(ResourceNames.SCHOOLS, "getSchool");
+        SINGULAR_LINK_NAMES.put(ResourceNames.STUDENTS, "getStudent");
+        SINGULAR_LINK_NAMES.put(ResourceNames.TEACHERS, "getTeacher");
+        SINGULAR_LINK_NAMES.put(ResourceNames.STAFF, "getStaff");
+    }
+    
+    /*
+     * This map indicates how to display a link to a particular resource. This map assumes
+     * that the destination can be multiple entities.
+     */
+    public static final Map<String, String> PLURAL_LINK_NAMES = new HashMap<String, String>();
+    static {
+        for (Entry<String, String> e : SINGULAR_LINK_NAMES.entrySet()) {
+            if (e.getKey().equals(ResourceNames.STAFF)) {
+                PLURAL_LINK_NAMES.put(e.getKey(), e.getValue());
+            } else {
+                PLURAL_LINK_NAMES.put(e.getKey(), e.getValue() + "s");
+            }
+        }
+    }
 }
