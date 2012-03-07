63cc9723fe82595bf919420d232776c1ee85bf44
Dong Liu <dliu@wgen.net>
2012-03-07 02:59:17 -0500

    fix apiclient in dashboard

--- sli/dashboard/src/main/java/org/slc/sli/view/AttendanceAbsenceResolver.java
+++ sli/dashboard/src/main/java/org/slc/sli/view/AttendanceAbsenceResolver.java
@@ -1,21 +1,19 @@
 package org.slc.sli.view;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slc.sli.config.Field;
 import org.slc.sli.entity.GenericEntity;
 import org.slc.sli.util.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Map;
-
-
 /**
  * Class to handle the oddities of getting absences from attendance objects.
  */
 public class AttendanceAbsenceResolver implements AggregateResolver {
     private static Logger logger = LoggerFactory.getLogger(AttendanceAbsenceResolver.class);
-
 
     private GenericEntity student;
     
@@ -35,14 +33,17 @@ public class AttendanceAbsenceResolver implements AggregateResolver {
 
     @Override
     public int getCountForPath(Field configField) {
-        //TODO: This should be a lot more generic.
+        // TODO: This should be a lot more generic.
         List<Map> attendances = student.getList(Constants.ATTR_STUDENT_ATTENDANCES);
         int count = 0;
-        for (Map attendance : attendances) {
-            logger.debug("Attendance: " + attendance);
-            String value = (String) attendance.get(CATEGORY);
-            if (value.contains(COMPARE_VALUE)) { ++count; }
-        }
+        if (attendances != null)
+            for (Map attendance : attendances) {
+                logger.debug("Attendance: " + attendance);
+                String value = (String) attendance.get(CATEGORY);
+                if (value.contains(COMPARE_VALUE)) {
+                    ++count;
+                }
+            }
         return count;
     }
 }
