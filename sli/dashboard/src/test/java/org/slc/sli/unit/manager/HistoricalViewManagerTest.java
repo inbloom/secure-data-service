d83c62d996c66c84c7a52d373ec586f33726cd6f
Nicholas Brown <nbrown@wgen.net>
2012-03-08 13:21:35 -0500

    fixed checkstyle

--- sli/dashboard/src/test/java/org/slc/sli/unit/manager/HistoricalViewManagerTest.java
+++ sli/dashboard/src/test/java/org/slc/sli/unit/manager/HistoricalViewManagerTest.java
@@ -1,22 +1,23 @@
 package org.slc.sli.unit.manager;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.slc.sli.config.ViewConfig;
-import org.slc.sli.manager.HistoricalViewManager;
-import org.slc.sli.view.HistoricalDataResolver;
-import org.slf4j.Logger;
-import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.mockito.Mockito.*;
+import org.slc.sli.config.ViewConfig;
+import org.slc.sli.manager.HistoricalViewManager;
+import org.slc.sli.view.HistoricalDataResolver;
 
 /**
  * Test class for HistoricalViewManager
