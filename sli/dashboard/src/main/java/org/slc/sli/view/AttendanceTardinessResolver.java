9e7a1c0783fca536eb36de485e8201696fd2c82a
Patrick Wolf <pwolf@wgen.net>
2012-03-09 15:08:17 -0500

    Initial pass at tardy data in dashboard

---
+++ sli/dashboard/src/main/java/org/slc/sli/view/AttendanceTardinessResolver.java
@@ -0,0 +1,60 @@
+package org.slc.sli.view;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slc.sli.config.Field;
+import org.slc.sli.entity.GenericEntity;
+import org.slc.sli.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AttendanceTardinessResolver implements AggregateRatioResolver {
+
+    private static Logger logger = LoggerFactory.getLogger(AttendanceAbsenceResolver.class);
+
+    private GenericEntity student;
+
+    public static final String CATEGORY = "attendanceEventCategory";
+
+    public AttendanceTardinessResolver(GenericEntity student) {
+        this.student = student;
+    }
+
+    public AttendanceTardinessResolver() {
+    }
+
+    public void setStudent(GenericEntity student) {
+        this.student = student;
+    }
+
+    @Override
+    public int getTotalCountForPath(Field configField) {
+        // TODO: This should be a lot more generic.
+        List<Map> attendances = student.getList(Constants.ATTR_STUDENT_ATTENDANCES);
+        int count = 0;
+        if (attendances != null)
+            for (Map attendance : attendances) {
+                String value = (String) attendance.get(CATEGORY);
+                if (!value.contains("Absence")) {
+                    ++count;
+                }
+            }
+        return count;
+    }
+
+    @Override
+    public int getSubCountForPath(Field configField) {
+        List<Map> attendances = student.getList(Constants.ATTR_STUDENT_ATTENDANCES);
+        int count = 0;
+        if (attendances != null)
+            for (Map attendance : attendances) {
+                String value = (String) attendance.get(CATEGORY);
+                if (value.contains("Tardy")) {
+                    ++count;
+                }
+            }
+        return count;
+    }
+
+}
