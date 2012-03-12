6cdd5de445be06be78fd1b69743c9a2fc42566b8
Doris Ip <dip@wgen.net>
2012-03-12 09:27:25 -0400

    add dashboard tests

--- sli/acceptance-tests/test/features/dashboard/dash/step_definitions/student_profile_steps.rb
+++ sli/acceptance-tests/test/features/dashboard/dash/step_definitions/student_profile_steps.rb
@@ -1,14 +1,79 @@
 require 'selenium-webdriver'
-require_relative '../../../utils/sli_utils.rb'
 
-When /^I click on student, their name shows up on student profile$/ do 
-  studentTable = @driver.find_element(:id, "studentList");
-  student_cells = studentTable.find_elements(:xpath, "//td[@class='name_w_link']")
+When /^I click on student "([^"]*)"$/ do |name|
+  clickOnStudent(name)
+end
+
+When /^I click on student at index "([^"]*)"$/ do |studentIndex|
+  clickOnStudentAtIndex(studentIndex)
+end
+
+When /^I view its student profile$/ do
+ csiContent = @driver.find_element(:id, "CSIcontent")
+ table_cells = csiContent.find_elements(:xpath, "//td")
   
-  x = student_cells[0].find_elements(:tag_name, "a")
-  student_name = x[0].text
-  x[0].click()
-  z = @driver.find_element(:id, "InnerStudentProfile")
+  @info = Hash.new
+  sName = csiContent.find_element(:tag_name, "b") 
   
-  assert(z.text == student_name)
+  @info["Name"] = sName.text
+  
+for i in 0..table_cells.length-1
+  if table_cells[i].text.length > 0 
+      if i % 2 == 0
+        key = table_cells[i].text
+      else
+        @info[key]= table_cells[i].text
+      end
+   end
+ end
+end
+
+Then /^their name shown in profile is "([^"]*)"$/ do |expectedStudentName|
+   assert(@info["Name"] == expectedStudentName)
+end
+
+Then /^their id shown in proflie is "([^"]*)"$/ do |studentId|
+  assert(@info["ID"] == studentId)
+end
+
+Then /^their grade is "([^"]*)"$/ do |studentGrade|
+ assert(@info["Grade"] == studentGrade)
+end
+
+Then /^the lozenges include "([^"]*)"$/ do |lozenge|
+  csiContent = @driver.find_element(:id, "CSIcontent")
+  labelFound = false
+  
+  all_lozengeSpans = csiContent.find_elements(:tag_name, "span")
+  all_lozengeSpans.each do |lozengeSpan|
+    if lozengeSpan.attribute("innerHTML").to_s.include?(lozenge)
+      labelFound = true
+    end
   end
+  
+  assert(labelFound == true)
+end
+
+Then /^the teacher is "([^"]*)"$/ do |teacherName|
+  assert(@info["Teacher"] == teacherName)
+end
+
+
+def clickOnStudent(name)
+  studentTable = @driver.find_element(:id, "studentList");
+  all_tds = studentTable.find_elements(:xpath, "//td[@class='name_w_link']")
+  
+  @driver.find_element(:link, name).click
+  
+  
+  #studentNames = all_tds[studentIndex.to_i].find_elements(:tag_name, "a")
+  #studentNames[0].click()
+end
+
+def clickOnStudentAtIndex(studentIndex)
+  studentTable = @driver.find_element(:id, "studentList");
+  all_tds = studentTable.find_elements(:xpath, "//td[@class='name_w_link']")
+  studentNames = all_tds[studentIndex.to_i].find_elements(:tag_name, "a")
+  studentNames[0].click()
+end
+
