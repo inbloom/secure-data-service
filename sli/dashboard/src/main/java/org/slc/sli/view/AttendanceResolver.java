e0b0758f6487aea3d7ba555aa4e409ea6d0710b0
Jonathan Nanney <jnanney@wgen.net>
2012-03-12 09:27:52 -0400

    Added the tardy count to the AttendanceAbsenceResolver class.

--- sli/dashboard/src/main/java/org/slc/sli/view/AttendanceResolver.java
+++ sli/dashboard/src/main/java/org/slc/sli/view/AttendanceResolver.java
@@ -12,13 +12,9 @@ public class AttendanceResolver {
     public AttendanceResolver() {
     }
 
-    public AggregateResolver getAbscenceCountResolverForStudent(Map student) {
+    public AggregateResolver getAttendanceCountResolverForStudent(Map student) {
         GenericEntity geStudent = new GenericEntity(student);
         return new AttendanceAbsenceResolver(geStudent);
     }
     
-    public AggregateResolver getTardyCountResolverForStudent(Map student) {
-        GenericEntity geStudent = new GenericEntity(student);
-        return new AttendanceAbsenceResolver(geStudent);
-    }
 }
