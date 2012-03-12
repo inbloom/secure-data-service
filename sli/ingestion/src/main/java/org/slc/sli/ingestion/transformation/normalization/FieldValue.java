1af6b87667296a95aaa47820085636858f4d8436
Nupur Pandey <npandey@wgen.net>
2012-03-12 17:00:00 -0400

    Reading Json

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/FieldValue.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/FieldValue.java
@@ -3,4 +3,18 @@ package org.slc.sli.ingestion.transformation.normalization;
 public class FieldValue {
     String sourceValue;
     Ref ref;
+
+    public String getSourceValue() {
+        return sourceValue;
+    }
+    public void setSourceValue(String sourceValue) {
+        this.sourceValue = sourceValue;
+    }
+    public Ref getRef() {
+        return ref;
+    }
+    public void setRef(Ref ref) {
+        this.ref = ref;
+    }
+
 }
--- sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/Ref.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/Ref.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 
 public class Ref {
-	String	collectionName;
-	List<Field>	fields;
+	private String	collectionName;
+	private List<List<Field>> choiceOfFields;
+
+	Ref() {}
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+    public List<List<Field>> getChoiceOfFields() {
+        return choiceOfFields;
+    }
+    public void setChoiceOfFields(List<List<Field>> choiceOfFields) {
+        this.choiceOfFields = choiceOfFields;
+    }
+
 
 }
