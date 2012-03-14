62fb9e2f863e9c71495a4e3cf7deb0288597f31e
Oleg Krook <okrook@wgen.net>
2012-03-14 11:57:04 -0400

    Moved json parsing into EntityConfig

--- sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/EntityConfig.java
+++ sli/ingestion/src/main/java/org/slc/sli/ingestion/transformation/normalization/EntityConfig.java
@@ -1,6 +1,10 @@
 package org.slc.sli.ingestion.transformation.normalization;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * Configuration of keys and references for an entity.
@@ -26,5 +30,10 @@ public class EntityConfig {
 
     public void setReferences(List<RefDef> references) {
         this.references = references;
+    }
+
+    public static EntityConfig parse(File entityConfig) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(entityConfig, EntityConfig.class);
     }
 }
--- sli/ingestion/src/test/java/org/slc/sli/ingestion/transformation/normalization/ReadJson.java
+++ sli/ingestion/src/test/java/org/slc/sli/ingestion/transformation/normalization/ReadJson.java
@@ -3,9 +3,7 @@ package org.slc.sli.ingestion.transformation.normalization;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.util.List;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 /**
@@ -18,8 +16,8 @@ public class ReadJson {
     @Test
     public void testTeacherSectionAssociation() throws Exception {
         File jsonFile = new File("src/test/resources/TeacherSectionAssociation.json");
-        ObjectMapper mapper = new ObjectMapper();
-        EntityConfig teacherSectionAssociation = mapper.readValue(jsonFile, EntityConfig.class);
+        EntityConfig teacherSectionAssociation = EntityConfig.parse(jsonFile);
+
         assertEquals("metadata.externalId", teacherSectionAssociation.getReferences().get(0).getRef().getChoiceOfFields().get(0).get(0).getValues().get(0).getValueSource());
         assertEquals("Section" , teacherSectionAssociation.getReferences().get(1).getRef().getCollectionName());
     }
