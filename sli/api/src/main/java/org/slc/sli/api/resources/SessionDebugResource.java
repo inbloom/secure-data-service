83d3c3b2a0138529b8eb82f6d4de9586ef309b13
Danil Kornishev <dkornishev@wgen.net>
2012-03-19 15:50:21 -0400

    adjusted session debug

--- sli/api/src/main/java/org/slc/sli/api/resources/SessionDebugResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/SessionDebugResource.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
 
 import org.slc.sli.api.security.SLIPrincipal;
@@ -32,65 +34,72 @@ import org.slc.sli.api.util.SecurityUtil.SecurityTask;
 @Scope("request")
 @Produces("application/json")
 public class SessionDebugResource {
-
+    
     @Autowired
     private RoleRightAccess roleAccessor;
-
+    
     @Value("${sli.security.noSession.landing.url}")
     private String realmPage;
-
+    
     /**
      * Method processing HTTP GET requests, producing "application/json" MIME media
      * type.
-     *
+     * 
      * @return SecurityContext that will be send back as a response of type "application/json".
      */
     @GET
     @Path("debug")
     public SecurityContext getSecurityContext() {
-        if (SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) {
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (auth == null) {
+            throw new InsufficientAuthenticationException("User must be logged in");
+        } else if (auth instanceof OAuth2Authentication) {
+            if (((OAuth2Authentication) auth).getUserAuthentication() instanceof AnonymousAuthenticationToken) {
+                throw new InsufficientAuthenticationException("User must be logged in");
+            }
+        } else if (auth instanceof AnonymousAuthenticationToken) {
             throw new InsufficientAuthenticationException("User must be logged in");
         }
+        
         return SecurityContextHolder.getContext();
     }
-
+    
     @GET
     @Path("check")
     public Object sessionCheck() {
-
+        
         final Map<String, Object> sessionDetails = new TreeMap<String, Object>();
-
+        
         if (isAuthenticated(SecurityContextHolder.getContext())) {
             sessionDetails.put("authenticated", true);
             sessionDetails.put("sessionId", SecurityContextHolder.getContext().getAuthentication().getCredentials());
-
-            SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication()
-                    .getPrincipal();
+            
+            SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             sessionDetails.put("user_id", principal.getId());
             sessionDetails.put("full_name", principal.getName());
             sessionDetails.put("granted_authorities", principal.getRoles());
             sessionDetails.put("realm", principal.getRealm());
-
+            
             List<Role> allRoles = SecurityUtil.sudoRun(new SecurityTask<List<Role>>() {
                 @Override
                 public List<Role> execute() {
                     return roleAccessor.fetchAllRoles();
                 }
             });
-
+            
             sessionDetails.put("all_roles", allRoles);
-
+            
         } else {
             sessionDetails.put("authenticated", false);
             sessionDetails.put("redirect_user", realmPage);
         }
-
+        
         return sessionDetails;
     }
-
+    
     private boolean isAuthenticated(SecurityContext securityContext) {
-        return !(securityContext == null || securityContext.getAuthentication() == null
-                || securityContext.getAuthentication().getCredentials() == null || securityContext.getAuthentication()
-                .getCredentials().equals(""));
+        return !(securityContext == null || securityContext.getAuthentication() == null || securityContext.getAuthentication().getCredentials() == null || securityContext.getAuthentication().getCredentials().equals(""));
     }
 }
