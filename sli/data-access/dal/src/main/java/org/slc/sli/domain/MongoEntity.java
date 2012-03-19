2c7963bc29915862f8fde6c28efe12a97b3a87b5
Patrick Wolf <pwolf@wgen.net>
2012-03-19 10:29:01 -0400

    reducing allocations in fromDBObject

--- sli/data-access/dal/src/main/java/org/slc/sli/domain/MongoEntity.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/domain/MongoEntity.java
@@ -127,16 +127,9 @@ public class MongoEntity implements Entity, Serializable {
         UUID uuid = (UUID) dbObj.get("_id");
         String id = uuid.toString();
         
-        Map<?, ?> map = dbObj.toMap();
-        
-        Map<String, Object> body = new HashMap<String, Object>();
-        if (map.containsKey("body")) {
-            body.putAll((Map<String, ?>) map.get("body"));
-        }
-        Map<String, Object> metaData = new HashMap<String, Object>();
-        if (map.containsKey("metaData")) {
-            metaData.putAll((Map<String, ?>) map.get("metaData"));
-        }
+        BasicBSONObject metaData = (BasicBSONObject) dbObj.get("metaData");
+        BasicBSONObject body = (BasicBSONObject) dbObj.get("body");
+
         return new MongoEntity(type, id, body, metaData);
     }
     
