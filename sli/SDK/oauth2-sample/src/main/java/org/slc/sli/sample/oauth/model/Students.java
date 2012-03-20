c54cc236c6f248ca9702e7cd4a19aeb232dc9207
Nicholas Brown <nbrown@wgen.net>
2012-03-20 19:06:10 -0400

    have demo app run against real api

--- sli/SDK/oauth2-sample/src/main/java/org/slc/sli/sample/oauth/model/Students.java
+++ sli/SDK/oauth2-sample/src/main/java/org/slc/sli/sample/oauth/model/Students.java
@@ -1,24 +1,38 @@
 package org.slc.sli.sample.oauth.model;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import org.slc.sli.api.client.Entity;
+import org.slc.sli.api.client.EntityCollection;
+import org.slc.sli.api.client.EntityType;
+import org.slc.sli.api.client.Query;
 import org.slc.sli.api.client.impl.BasicClient;
+import org.slc.sli.api.client.impl.BasicQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Students {
+    private static final Logger LOG = LoggerFactory.getLogger(Students.class);
 
+    @SuppressWarnings("unchecked")
     public static List<String> getNames(BasicClient client) throws IOException {
+        EntityCollection collection = new EntityCollection();
+        try {
+            client.read(collection, EntityType.STUDENTS, BasicQuery.EMPTY_QUERY);
+        } catch (URISyntaxException e) {
+            LOG.error("Exception occurred",e);
+        }
         ArrayList<String> toReturn = new ArrayList<String>();
-        toReturn.add("Bob Marley");
-        toReturn.add("Willy Wonka");
-        toReturn.add("Homer Simpson");
-        toReturn.add("Juan Valdez");
-        toReturn.add("Gary Coleman");
-        toReturn.add("Mick Jagger");
-        toReturn.add("Austin Powers");
-        toReturn.add("Peter Pan");
+        for(Entity student: collection){
+            String firstName = (String) ((Map<String, Object>) student.getData().get("name")).get("firstName");
+            String lastName = (String) ((Map<String, Object>) student.getData().get("name")).get("lastSurname");
+            toReturn.add(student.getData().get("name").toString());
+        }
         return toReturn;
     }
     
