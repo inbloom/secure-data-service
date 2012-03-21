3eb9897b4119910edbc93ec8259dfe566ab09353
Stan Halka <shalka@wgen.net>
2012-03-21 21:27:37 +0100

    fixed checkstyle

--- sli/api/src/test/java/org/slc/sli/api/security/saml2/XmlSignatureHelperTest.java
+++ sli/api/src/test/java/org/slc/sli/api/security/saml2/XmlSignatureHelperTest.java
@@ -17,7 +17,6 @@ import junit.framework.Assert;
 
 import org.jdom.JDOMException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
