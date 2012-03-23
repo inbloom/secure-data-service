b63ed9978c0afa8f27a62523510eb1fedb059f28
Matthew Lane <lanemt@gmail.com>
2012-03-23 15:04:48 -0400

    updates matching of fields in tests

--- sli/acceptance-tests/test/features/databrowser/step_definitions/databrowser_simpe_detail_steps.rb
+++ sli/acceptance-tests/test/features/databrowser/step_definitions/databrowser_simpe_detail_steps.rb
@@ -87,8 +87,8 @@ Then /^I see a table displaying the associations in a list$/ do
 end
 
 Then /^those names include the IDs of both "([^"]*)" and "([^"]*)" in the association$/ do |arg1, arg2|
-  assertWithWait("Failed to find Coulumn title of "+arg1)  {@driver.find_element(:xpath, "//th/span[text()='#{arg1}']")}
-  assertWithWait("Failed to find Coulumn title of "+arg2)  {@driver.find_element(:xpath, "//th/span[text()='#{arg2}']")}
+  assertWithWait("Failed to find Coulumn title of "+arg1)  {@driver.find_element(:xpath, "//th/span[contains(text(),'#{arg1}')]")}
+  assertWithWait("Failed to find Coulumn title of "+arg2)  {@driver.find_element(:xpath, "//th/span[contains(text(),'#{arg2}')]")}
 end
 
 When /^I click on the row containing "([^"]*)"$/ do |arg1|
@@ -98,7 +98,7 @@ When /^I click on the row containing "([^"]*)"$/ do |arg1|
 end
 
 Then /^the row expands below listing the rest of the attributes for the item$/ do
-  assertWithWait("Failed to find expanded row")  {@driver.find_element(:xpath, "//tr[contains(@style,'display: table-row')]//dd[text()='Independent study']")}
+  assertWithWait("Failed to find expanded row")  {@driver.find_element(:xpath, "//tr[contains(@style,'display: table-row')]//dd[contains(text(),'Independent study')]")}
 end
 
 When /^I click on the expanded row$/ do
