10bb95b1b9a462886f1face7205e2f8307451667
Girish Rao <grao@wgen.net>
2012-03-26 19:22:47 +0530

    fix the step definition according to the previous patches using selenium-webdriver and also gherkin using features file

--- sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
+++ sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
@@ -1,24 +1,39 @@
+require 'rubygems'
+require 'selenium-webdriver'#run the file ruby selenium.rb 'Name of your selection releam' 'username' 'password'
+require_relative '../../utils/sli_utils.rb'
+require_relative '../../utils/selenium_common.rb'
+#driver = Selenium::WebDriver.for :firefox
+
 
 Given /^an admin user Demo exists with "([^\"]*)" and "([^\"]*)"$/ do |username,password|
   
-#  User.new(:username => username, :password => password, :password_confirmation => password).save! unless User.exists?(:username => username)
-#  User.find_by_username('demo').update_attribute(:admin, 1)
 
-User.exists?(:username => username)
 end
 
 Given /^a normal user Educator exists with "([^\"]*)" and "([^\"]*)"$/ do |username,password|
   
   #User.new(:username => username, :password => password, :password_confirmation => password).save! unless User.exists?(:username => username)
-  User.exists?(:username => username)
+ # User.exists?(:username => username)
 end
 
 Then /^I am on the Realm selection page$/ do
-  visit "https://devlr2.slidev.org"
+  @driver.navigate.to "https://devlr2.slidev.org"
 end
 
 Then /^I select "([^\"]*)"$/ do |text|
-  select(text, :from => 'realmId') 
+
+a=@driver.find_element(:name,'realmId') #realmId should be the html tag name of select tag
+options=a.find_elements(:tag_name=>"option") # all the options of that select tag will be selected
+options.each do |g|
+  if g.text == text
+    g.click
+    break
+  end
+end
+ele=driver.find_element(:id, "go")
+ele.click
+
+  #select(text, :from => 'realmId') 
 end
 
 Then /^I click "([^\"]*)"$/ do |btn_text|
@@ -30,22 +45,34 @@ Given /^EULA has been accepted$/ do
 end
 
 When /^I go to the login page$/ do
-  visit "https://devlr2.slidev.org"
-  select('Shared Learning Infrastructure', :from => 'realmId')
-  click_button('Go')
+@driver.navigate.to "https://devlr2.slidev.org"
+a=@driver.find_element(:name,'realmId') #realmId should be the html tag name of select tag
+options=a.find_elements(:tag_name=>"option") # all the options of that select tag will be selected
+options.each do |g|
+  if g.text == 'Shared Learning Infrastructure'
+    g.click
+    break
+  end
+end
+ele=driver.find_element(:id, "go")
+ele.click
+
+
+  #visit "https://devlr2.slidev.org"
+ # select('Shared Learning Infrastructure', :from => 'realmId')
+  #click_button('Go')
 end
 
 
 
 
 Then /^I should logged out$/ do
-  click_link('Logout')
+  @driver.find_element(:link, 'Logout').click
+  #click_link('Logout')
 end
 
 Then /^I should be on the home page$/ do
-  
-  #      response.should contain('Logout')
-  page.should have_content('Logout')
+  @driver.find_element(:link, 'Logout').displayed? ||   @driver.find_element(:link, 'Sign out').displayed?
 
 end
 
@@ -63,12 +90,16 @@ end
 
 When /^I login with "([^\"]*)" and "([^\"]*)"$/ do |username, password|
   
-  fill_in "IDToken1", :with=>username
-  fill_in "IDToken2", :with=>password
-  click_button "Log In"
+  element = driver.find_element(:id, 'IDToken1') #the username field id is IDToken1
+  element.send_keys username
+
+  element = driver.find_element(:id, 'IDToken2') #the username field id is IDToken2
+  element.send_keys password
+  element=driver.find_element(:class, "Btn1Def")
+  element.click
 end
 Then /^I should be on the authentication failed page$/ do
- visit "https://devopenam1.slidev.org:80/idp2/UI/Login"
+ @driver.navigate.to "https://devopenam1.slidev.org:80/idp2/UI/Login"
 end
 
 #When /^I login as Normal with "([^\"]*)" and "([^\"]*)"$/ do |username, password|
@@ -81,19 +112,32 @@ end
 
 
 Then /^(?:|I )should see "([^\"]*)"$/ do |text|
-  page.should have_content(text)
+  begin
+   link=@driver.find_element(:link, text).displayed? || @driver.find_element(:name, text).displayed? 
+   link=true
+  rescue
+   link=false
+  end
+  link 
+  #page.should have_content(text)
 end
 
 Then /^(?:|I )should not see "([^\"]*)"$/ do |text|
-  page.should_not have_content(text)
+  begin
+   link=@driver.find_element(:link, text).displayed? || @driver.find_element(:name, text).displayed? 
+   link=true
+  rescue
+   link=false
+  end 
+  link
+
+#  page.should_not have_content(text)
 end
 When /^(?:|I )follow "([^\"]*)"$/ do |link|
-  click_link(link)
+  @driver.find_element(:link, link).click
+  #click_link(link)
 end
 
-When /^(?:|I )follow "([^\"]*)"$/ do |link|
-  click_link(link)
-end
 
 #Then /^(?:|I )should be on (.+)$/ do |page_name|
 #  current_path = URI.parse(current_url).select(:path, :query).compact.join('?')
