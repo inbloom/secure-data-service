bc36e8d363e88355be69144a6eb8f39e86d9522e
Ryan Latta <rlatta@wgen.net>
2012-03-27 15:45:04 -0400

    Working on the deploy script for databrowser.

--- sli/databrowser/config/deploy.rb
+++ sli/databrowser/config/deploy.rb
@@ -25,7 +25,10 @@ namespace :deploy do
   desc "Start the Thin processes"
   task :start do
     run  <<-CMD
-      cd #{deploy_to}/current/#{working_dir}; bundle exec thin start -C config/thin.yml -e #{rails_env}
+      cd #{deploy_to}/current/sli
+      sh profile_swap.sh #{configuration[:subdomain]} if configuration[:subdomain]
+      cd #{deploy_to}/current/#{working_dir}
+      bundle exec thin start -C config/thin.yml -e #{rails_env}
     CMD
   end
 
