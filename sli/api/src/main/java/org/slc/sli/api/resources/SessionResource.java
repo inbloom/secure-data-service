5fb82e89b05f67b548359bcdea3b1bc26d57842b
Matthew Lane <lanemt@gmail.com>
2012-03-27 10:20:54 -0400

    renames SessionResource class

--- sli/api/src/main/java/org/slc/sli/api/resources/SessionResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/SessionResource.java
@@ -1,13 +1,10 @@
 package org.slc.sli.api.resources;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
+import org.slc.sli.api.security.SLIPrincipal;
+import org.slc.sli.api.security.roles.Role;
+import org.slc.sli.api.security.roles.RoleRightAccess;
+import org.slc.sli.api.util.SecurityUtil;
+import org.slc.sli.api.util.SecurityUtil.SecurityTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
@@ -19,11 +16,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
 
-import org.slc.sli.api.security.SLIPrincipal;
-import org.slc.sli.api.security.roles.Role;
-import org.slc.sli.api.security.roles.RoleRightAccess;
-import org.slc.sli.api.util.SecurityUtil;
-import org.slc.sli.api.util.SecurityUtil.SecurityTask;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * System resource class for security session context.
@@ -33,26 +31,26 @@ import org.slc.sli.api.util.SecurityUtil.SecurityTask;
 @Component
 @Scope("request")
 @Produces("application/json")
-public class SessionDebugResource {
-    
+public class SessionResource {
+
     @Autowired
-    private RoleRightAccess roleAccessor;
-    
+    private RoleRightAccess roleRightAccess;
+
     @Value("${sli.security.noSession.landing.url}")
     private String realmPage;
-    
+
     /**
      * Method processing HTTP GET requests, producing "application/json" MIME media
      * type.
-     * 
+     *
      * @return SecurityContext that will be send back as a response of type "application/json".
      */
     @GET
     @Path("debug")
     public SecurityContext getSecurityContext() {
-        
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        
+
         if (auth == null) {
             throw new InsufficientAuthenticationException("User must be logged in");
         } else if (auth instanceof OAuth2Authentication) {
@@ -62,44 +60,44 @@ public class SessionDebugResource {
         } else if (auth instanceof AnonymousAuthenticationToken) {
             throw new InsufficientAuthenticationException("User must be logged in");
         }
-        
+
         return SecurityContextHolder.getContext();
     }
-    
+
     @GET
     @Path("check")
     public Object sessionCheck() {
-        
+
         final Map<String, Object> sessionDetails = new TreeMap<String, Object>();
-        
+
         if (isAuthenticated(SecurityContextHolder.getContext())) {
             sessionDetails.put("authenticated", true);
             sessionDetails.put("sessionId", SecurityContextHolder.getContext().getAuthentication().getCredentials());
-            
+
             SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             sessionDetails.put("user_id", principal.getId());
             sessionDetails.put("full_name", principal.getName());
             sessionDetails.put("granted_authorities", principal.getRoles());
             sessionDetails.put("realm", principal.getRealm());
             sessionDetails.put("adminRealm", principal.getAdminRealm());
-            
+
             List<Role> allRoles = SecurityUtil.sudoRun(new SecurityTask<List<Role>>() {
                 @Override
                 public List<Role> execute() {
-                    return roleAccessor.fetchAllRoles();
+                    return roleRightAccess.fetchAllRoles();
                 }
             });
-            
+
             sessionDetails.put("all_roles", allRoles);
-            
+
         } else {
             sessionDetails.put("authenticated", false);
             sessionDetails.put("redirect_user", realmPage);
         }
-        
+
         return sessionDetails;
     }
-    
+
     private boolean isAuthenticated(SecurityContext securityContext) {
         return !(securityContext == null || securityContext.getAuthentication() == null || securityContext.getAuthentication().getCredentials() == null || securityContext.getAuthentication().getCredentials().equals(""));
     }
