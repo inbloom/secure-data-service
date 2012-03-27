91c1bcf50d6d041011b68c34b307f8f283606409
Ryan Latta <rlatta@wgen.net>
2012-03-27 11:05:10 -0400

    More adjustments to the script.

--- sli/profile_swap.sh
+++ sli/profile_swap.sh
@@ -2,17 +2,17 @@
 
 #Take care of build profiles for Spring
 echo "Altering dashboard/api/ingetion projects..."
-grep -lR "sli.dev.subdomain=" * | xargs -L 1 sed -n -e "s/sli\.dev\.subdomain=.*/sli.dev.subdomain=$1/p"
+grep -lR "sli.dev.subdomain=" sli-configuration/* | xargs -L 1 sed -i'' -e "s/sli\.dev\.subdomain=.*/sli.dev.subdomain=$1/g" 
 
 #Take care of fixture data for applications
 echo "Altering fixture data for applications to match..."
-grep -lR "https://ci.slidev.org" acceptance-tests/test/data/team_application_fixtures.json | xargs -L 1 sed -n -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/p"
+sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" acceptance-tests/test/data/team_application_fixtures.json
 
 #Take care of rails projects
 echo "Altering rails applications to match..."
-grep -lR "https://ci.slidev.org" admin-tools/admin-rails/config/config.yml | xargs -L 1 sed -n -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/p"
-grep -lR "https://ci.slidev.org" databrowser/config/config.yml | xargs -L 1 sed -n -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/p"
-grep -lR "https://ci.slidev.org" admin-tools/admin-rails/config/deploy/team.rb | xargs -L 1 sed -n -e "s/ci.slidev.org/$1.slidev.org/p"
-grep -lR "https://ci.slidev.org" databrowser/config/deploy/team.rb | xargs -L 1 sed -n -e "s/ci.slidev.org/$1.slidev.org/p"
+grep -lR "https://ci.slidev.org" admin-tools/admin-rails/config/config.yml | xargs -L 1 sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" 
+grep -lR "https://ci.slidev.org" databrowser/config/config.yml | xargs -L 1 sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$1.slidev.org/g" 
+sed -i'' -e "s/ci.slidev.org/$1.slidev.org/g" admin-tools/admin-rails/config/deploy/team.rb 
+sed -i'' -e "s/ci.slidev.org/$1.slidev.org/g" databrowser/config/deploy/team.rb 
 
 echo "Done.. ready to build and deploy!"
