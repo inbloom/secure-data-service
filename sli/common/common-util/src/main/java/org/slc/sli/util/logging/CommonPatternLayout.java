fff2dc738d2d6a9d5e0b2c61b09d81b405e2377c
smelody@wgen.net <smelody@wgen.net>
2012-03-27 13:23:01 -0400

    Fixed tabs issues / formatting issues that were throwing checkstyle warnings Fixed logback.xml in api to reference renamed layout.

--- sli/common/common-util/src/main/java/org/slc/sli/util/logging/CommonPatternLayout.java
+++ sli/common/common-util/src/main/java/org/slc/sli/util/logging/CommonPatternLayout.java
@@ -10,17 +10,16 @@ import ch.qos.logback.classic.PatternLayout;
  * 
  */
 public class CommonPatternLayout extends PatternLayout {
-	// TODO - shouldn't this be in our config files in 1 place?
-	private static final String PATTERN = "%date{dd MMM yyyy HH:mm:ss.SSSZ} %-5level [%thread] %logger{10} [%user] - %msg%n";
-
-	// Registers custom converters
-	static {
-		PatternLayout.defaultConverterMap.put("user",
-				UserConverter.class.getName());
-	}
-
-	public CommonPatternLayout() {
-		setPattern(PATTERN);
-	}
-
+    // TODO - shouldn't this be in our config files in 1 place?
+    private static final String PATTERN = "%date{dd MMM yyyy HH:mm:ss.SSSZ} %-5level [%thread] %logger{10} [%user] - %msg%n";
+    
+    // Registers custom converters
+    static {
+        PatternLayout.defaultConverterMap.put("user", UserConverter.class.getName());
+    }
+    
+    public CommonPatternLayout() {
+        setPattern(PATTERN);
+    }
+    
 }
