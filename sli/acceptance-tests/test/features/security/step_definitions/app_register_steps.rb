f3a79d8a01c378c38dd0d3b7f917d4b9062bb9bc
Mark Abernathy <mabernathy@wgen.net>
2012-03-27 16:18:09 -0400

    Added application update tests for new update method

--- sli/acceptance-tests/test/features/security/step_definitions/app_register_steps.rb
+++ sli/acceptance-tests/test/features/security/step_definitions/app_register_steps.rb
@@ -1,7 +1,7 @@
 require_relative '../../utils/sli_utils.rb'
 
 Transform /^([^"]*)<([^"]*)>$/ do |arg1, arg2|
-  id = arg1+"fm67sH6vZZ" if arg2 == "Testing App"
+  id = arg1+"deb9a9d2-771d-40a1-bb9c-7f93b44e51df" if arg2 == "Testing App"
   id = arg1+@newId       if arg2 == "New App ID"
   #id = step_arg if id == nil
   id
@@ -18,15 +18,18 @@ When /^I navigate to POST "([^"]*)"$/ do |arg1|
 end
 
 Then /^I should receive the data for the specified application entry$/ do
-  assert(@res.code == 200, "Return code was not expected: "+@res.code.to_s+" but expected 200")
   result = JSON.parse(@res.body)
   assert(result != nil, "Result of JSON parsing is nil")
+  @client_secret = result["client_secret"]
+  @client_id = result["client_id"]
 end
 
 When /^I navigate to PUT "([^"]*)"$/ do |arg1|
   @format = "application/json"
   dataObj = DataProvider.getValidAppData()
   dataObj["description"] = "New and Improved"
+  dataObj["client_secret"] = @client_secret
+  dataObj["client_id"] = @client_id
   data = prepareData("application/json", dataObj)
 
   restHttpPut(arg1, data)
@@ -64,6 +67,25 @@ When /^I POST an application specifying the auto\-generated field "([^"]*)"$/ do
   assert(@res != nil, "Response from POST operation was null")
 end
 
-When /^I PUT an application specifying the auto\-generated field "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I PUT an application updating the auto\-generated field "([^"]*)"$/ do |arg1|
+  @format = "application/json"
+  uri = "/apps/deb9a9d2-771d-40a1-bb9c-7f93b44e51df"
+  restHttpGet(uri)
+  result = JSON.parse(@res.body)
+  assert(result != nil, "Result of JSON parsing is nil")
+  
+  prev_client_secret = result["client_secret"]
+  prev_client_id = result["client_id"] 
+
+  dataObj = DataProvider.getValidAppData()
+  dataObj["client_secret"] = prev_client_secret 
+  dataObj["client_id"] = prev_client_id 
+  dataObj["client_secret"] = "thisIsAReallyBadLongClientSecretYarightorlyyarly" if arg1 == "client_secret"
+  dataObj["client_id"] = "badclientId" if arg1 == "client_id"
+
+  data = prepareData("application/json", dataObj)
+
+  restHttpPut(uri, data)
+
+  assert(@res != nil, "Response from PUT operation was null")
 end
