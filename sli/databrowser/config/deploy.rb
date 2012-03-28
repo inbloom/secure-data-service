14d5e751706ab379532b06080f7a25d32282694f
Ryan Latta <rlatta@wgen.net>
2012-03-28 09:38:45 -0400

    Working on deployment script.

--- sli/databrowser/config/deploy.rb
+++ sli/databrowser/config/deploy.rb
@@ -26,7 +26,7 @@ namespace :deploy do
   task :start do
     run  <<-CMD
       cd #{deploy_to}/current/sli
-      sh profile_swap.sh #{configuration[:subdomain]} if configuration[:subdomain]
+      sh profile_swap.sh #{subdomain} if subdomain
       cd #{deploy_to}/current/#{working_dir}
       bundle exec thin start -C config/thin.yml -e #{rails_env}
     CMD
