899db0b13e7d56c0df15098fb306517e749c2ed8
Aaron Saarela <asaarela@wgen.net>
2012-03-29 09:38:06 -0400

    Attempt to fix broken portal acceptance tests.

--- sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
+++ sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
@@ -90,12 +90,12 @@ end
 
 
 Then /^I should logged out$/ do
-  @driver.find_element(:link, 'Logout').click
-  #click_link('Logout')
+  @driver.find_element(:link, 'logout').click
+  #click_link('logout')
 end
 
 Then /^I should be on the home page$/ do
-  @driver.find_element(:link, 'Logout').displayed? ||   @driver.find_element(:link, 'Sign out').displayed?
+  @driver.find_element(:link, 'logout').displayed? ||   @driver.find_element(:link, 'Sign out').displayed?
 
 end
 
@@ -124,7 +124,7 @@ When /^I login with "([^\"]*)" and "([^\"]*)"$/ do |username, password|
   element=@driver.find_element(:class, "Btn1Def")
   element.click
   #wait = Selenium::WebDriver::Wait.new(:timeout => 100) # seconds
- # wait.until { driver.find_element(:link => "Logout") }
+ # wait.until { driver.find_element(:link => "logout") }
 end
 Then /^I should be on the authentication failed page$/ do
  @driver.navigate.to "https://devopenam1.slidev.org:80/idp2/UI/Login"
