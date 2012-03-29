900471a086b15fc391ca3e2d32da42aec4f0038f
James Short <jshort@wgen.net>
2012-03-29 09:51:49 -0400

    Checkstyle

--- sli/dashboard/src/main/java/org/slc/sli/view/GradebookEntryResolver.java
+++ sli/dashboard/src/main/java/org/slc/sli/view/GradebookEntryResolver.java
@@ -4,8 +4,6 @@ import org.slc.sli.entity.GenericEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 
