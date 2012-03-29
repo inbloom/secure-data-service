ae6a73746b8877e33acf897524055948b47ecf56
Steve Lee <slee@wgen.net>
2012-03-29 10:02:34 -0400

    modified:   sli/common/domain/src/test/java/org/slc/sli/validation/NeutralSchemaValidationTest.java

--- sli/common/domain/src/test/java/org/slc/sli/validation/NeutralSchemaValidationTest.java
+++ sli/common/domain/src/test/java/org/slc/sli/validation/NeutralSchemaValidationTest.java
@@ -317,6 +317,7 @@ public class NeutralSchemaValidationTest {
         this.addDummyEntity("student", "714c1304-8a04-4e23-b043-4ad80eb60992");
         this.addDummyEntity("student", "7a86a6a7-1f80-4581-b037-4a9328b9b650");
         this.addDummyEntity("student", "e0e99028-6360-4247-ae48-d3bb3ecb606a");
+        this.addDummyEntity("educationOrganization", "eb3b8c35-f582-df23-e406-6947249a19f2");
 
         readAndValidateFixtureData("src/test/resources/student_disciplineIncident_association_fixture_neutral.json", "studentDisciplineIncidentAssociation");
     }
