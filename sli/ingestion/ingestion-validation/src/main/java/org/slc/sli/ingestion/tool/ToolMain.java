5fa7e2fa224799a7eafbe8875d58938159e3c166
Alexander Blum <ablum@wgen.net>
2012-03-30 17:44:17 -0400

    Fixed main class to use Spring

--- sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
+++ sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
@@ -1,19 +1,37 @@
 package org.slc.sli.ingestion.tool;
 
-import java.io.IOException;
+import java.util.List;
+
+import org.slc.sli.ingestion.validation.XsdValidator;
+import org.slc.sli.ingestion.validation.spring.SimpleValidatorSpring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ToolMain{
 
     public static void main(String [] args){
-        Validation validation = new Validation();
-        try {
-            validation.validateControlFile("/Users/tke/Documents/Projects/SLI/sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/test/", "MainControlFile.ctl");
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println("fine");
+    	ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring/applicationContext.xml");
+
+    	ToolMain main = context.getBean(ToolMain.class);
+    	main.start(args);
+
     }
+
+    @Autowired
+	private List<SimpleValidatorSpring> validators;
+	private void start(String[] args){
+		XsdValidator xsd = (XsdValidator) validators.get(0);
+	}
+
+	public void setValidators(List<SimpleValidatorSpring> validators){
+		this.validators = validators;
+	}
+
+	public List<SimpleValidatorSpring> getValidators(){
+		return validators;
+	}
 
 }
 
