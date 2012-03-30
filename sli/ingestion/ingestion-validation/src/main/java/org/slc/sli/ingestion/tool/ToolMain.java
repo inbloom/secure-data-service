6b9578e8a45be574b982ae610cc2e70be0a1ea03
Tao Ke <tke@wgen.net>
2012-03-30 16:57:11 -0400

    initial checkin of working control file validation

--- sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
+++ sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
@@ -1,11 +1,20 @@
 package org.slc.sli.ingestion.tool;
 
+import java.io.IOException;
+
 public class ToolMain{
 
-    public static void main(String [] args) {
-
-      
+    public static void main(String [] args){
+        Validation validation = new Validation();
+        try {
+            validation.validateControlFile("/Users/tke/Documents/Projects/SLI/sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/test/", "MainControlFile.ctl");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("fine");
     }
+
 }
 
 
