9398928c9dc3acf13c94e4e3b91de9428412642f
Tao Ke <tke@wgen.net>
2012-04-02 16:52:10 -0400

    Fix autowiring of controlFileValidation

--- sli/ingestion/ingestion-validation/src/test/java/org/slc/sli/ingestion/tool/ControlFileValidationTest.java
+++ sli/ingestion/ingestion-validation/src/test/java/org/slc/sli/ingestion/tool/ControlFileValidationTest.java
@@ -11,29 +11,37 @@ import org.slc.sli.ingestion.BatchJob;
 import org.slc.sli.ingestion.landingzone.ControlFile;
 import org.slc.sli.ingestion.landingzone.ControlFileDescriptor;
 import org.slc.sli.ingestion.landingzone.LocalFileSystemLandingZone;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/validatorContext.xml" })
 public class ControlFileValidationTest {
-
+    @Autowired
+    ControlFileValidation cfValidation;
     @Test
     public void testValidateControlFileDescriptorBatchJob() {
-        String dir = "test";
-        String ctrFile = "MainControl.ctl";
+        Resource ctrFile = new ClassPathResource("test/MainControlFile.ctl");
+        String dir = "resources/test";
         LocalFileSystemLandingZone lz = new LocalFileSystemLandingZone();
         lz.setDirectory(new File(dir));
         ControlFile cf = null;
         try {
-            cf = ControlFile.parse(new File(dir + ctrFile));
+            cf = ControlFile.parse(ctrFile.getFile());
         } catch (IOException e) {
             fail("Failed to parse control file");
         }
         ControlFileDescriptor fileDesc = new ControlFileDescriptor(cf, lz);
 
-        ControlFileValidation cfValidation = new ControlFileValidation();
-        BatchJob job = BatchJob.createDefault("MainControl.ctl");
+        BatchJob job = null;
+        try {
+            job = BatchJob.createDefault(ctrFile.getFile().getPath());
+        } catch (IOException e) {
+            fail("Failed to create job");
+        }
         cfValidation.validate(fileDesc, job);
     }
 
