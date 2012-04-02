04e6795047e9ae907d5b7d8bfb05f360dc602f94
Nupur Pandey <npandey@wgen.net>
2012-04-02 13:32:01 -0400

    ZipValidation Update

--- sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ZipValidation.java
+++ sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ZipValidation.java
@@ -3,13 +3,13 @@ package org.slc.sli.ingestion.tool;
 import java.io.File;
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-
 import org.slc.sli.ingestion.BatchJob;
 import org.slc.sli.ingestion.FaultsReport;
 import org.slc.sli.ingestion.landingzone.ZipFileUtil;
 import org.slc.sli.ingestion.landingzone.validation.ZipFileValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
 
 /**
  * Zip Validation and extraction class.
@@ -37,16 +37,15 @@ public class ZipValidation implements MessageSourceAware {
     public File validate(File zipFile, BatchJob job) {
 
         File ctlFile = null;
+        FaultsReport fr = job.getFaultsReport();
         try {
-            FaultsReport fr = job.getFaultsReport();
-
             if (validator.isValid(zipFile, fr)) {
                 File dir = ZipFileUtil.extract(zipFile);
                 ctlFile = ZipFileUtil.findCtlFile(dir);
                 }
         } catch (IOException ex) {
             fr.error(messageSource.getMessage("SL_ERR_MSG4", new Object[] { zipFile.getName() }, null), this);
-        } catch (Exception exception) {
+        } catch (Exception ex) {
             fr.error(ex.getMessage(), this);
         } finally {
             return ctlFile;
