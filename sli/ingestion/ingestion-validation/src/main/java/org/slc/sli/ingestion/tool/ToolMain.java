3d82b83816cc8ce3c17f146759e5fbb0e42c7c4e
Tao Ke <tke@wgen.net>
2012-04-02 12:41:55 -0400

    Added simple argument parsing

--- sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
+++ sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
@@ -1,6 +1,7 @@
 package org.slc.sli.ingestion.tool;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,8 +19,20 @@ public class ToolMain{
     }
 
     private ValidationController controller;
-    private void start(String[] args){
-        controller.doValidation()
+    //Name of the validation tool
+    final String appName;
+
+    private void start(Map<String,String> map_args){
+
+        if( (args.length != n_args + 1) ){
+            System.out.println(appName + ":Illegal options");
+            System.out.println("Usage: " + appName + "[directory]");
+            return ;
+        }
+
+        String landing_zone = args[1];
+
+        controller.doValidation(landing_zone);
     }
 
     public void setValidationController(ValidationController controller){
@@ -29,7 +42,6 @@ public class ToolMain{
     public ValidationController getValidation(){
         return controller;
     }
-
 }
 
 
