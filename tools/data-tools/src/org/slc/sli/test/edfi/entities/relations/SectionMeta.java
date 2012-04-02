2a5884594254421920619609ff04ccb212bf3957
Danilo Duran <dduran@wgen.net>
2012-04-02 17:59:43 -0400

    meta objects to contain only ids and references for entities

---
+++ tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/SectionMeta.java
@@ -0,0 +1,26 @@
+package org.slc.sli.test.edfi.entities.relations;
+
+public class SectionMeta {
+    public final String id;
+    public final String schoolId;
+    public final String courseId;
+    public final String sessionId;
+
+    public final String simpleId;
+
+    public SectionMeta(String id, SchoolMeta schoolMeta, CourseMeta courseMeta, SessionMeta sessionMeta) {
+        this.id = schoolMeta.id + "-" + courseMeta.simpleId + "-" + sessionMeta.simpleId + "-" + id;
+        this.schoolId = schoolMeta.id;
+        this.courseId = courseMeta.id;
+        this.sessionId = sessionMeta.id;
+
+        this.simpleId = id;
+    }
+
+    @Override
+    public String toString() {
+        return "SectionMeta [id=" + id + ", schoolId=" + schoolId + ", courseId=" + courseId + ", sessionId="
+                + sessionId + "]";
+    }
+
+}
