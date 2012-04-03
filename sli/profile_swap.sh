11f1cf7be1d16fd730be8c877d6daf16b887796f
Ryan Latta <rlatta@wgen.net>
2012-04-03 12:42:07 -0400

    Removing unwanted scripting for CI.

--- sli/profile_swap.sh
+++ sli/profile_swap.sh
@@ -8,11 +8,11 @@ fi
 
 #Take care of build profiles for Spring
 echo "Altering dashboard/api/ingetion projects..."
-grep -lR "sli.dev.subdomain=" sli-configuration/* | xargs -L 1 sed -i'' -e "s/sli\.dev\.subdomain=.*/sli.dev.subdomain=$hostname/g"
+# grep -lR "sli.dev.subdomain=" sli-configuration/* | xargs -L 1 sed -i'' -e "s/sli\.dev\.subdomain=.*/sli.dev.subdomain=$hostname/g"
 
 #Take care of fixture data for applications
 echo "Altering fixture data for applications to match..."
-sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$hostname.slidev.org/g" acceptance-tests/test/data/team_application_fixtures.json
+# sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$hostname.slidev.org/g" acceptance-tests/test/data/team_application_fixtures.json
 sed -i'' -e "s/https:\/\/ci.slidev.org/https:\/\/$hostname.slidev.org/g" acceptance-tests/test/data/application_fixture.json
 
 #Take care of rails projects
