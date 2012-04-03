16381d9add338736c113ddeec05bcd41c9c1d1c5
ifaybyshev <ifaybyshev@wgen.net>
2012-04-03 09:29:56 -0400

    changes to the tool

--- sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
+++ sli/ingestion/ingestion-validation/src/main/java/org/slc/sli/ingestion/tool/ToolMain.java
@@ -5,60 +5,64 @@ import java.io.IOException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
-public class ToolMain{
-
-    public static void main(String [] args) throws IOException{
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("spring/validatorContext.xml");
-
-    	ToolMain main = context.getBean(ToolMain.class);
-    main.start(args);
-
+/**
+ * 
+ * Driver/Controller of the Offline Validation Tool
+ * 
+ * @author tke
+ *
+ */
+public class ToolMain {
+    
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/validatorContext.xml");
+        
+        ToolMain main = context.getBean(ToolMain.class);
+        main.start(args);
+        
     }
-
+    
     private ValidationController controller;
-    //Name of the validation tool
+    
+    // Name of the validation tool
     String appName;
-    //Number of arguments
-    int n_args;
-
-    private void start(String [] args){
-
-        if( (args.length != n_args) ){
+    
+    // Number of arguments
+    int inputArgumentCount;
+    
+    private void start(String[] args) {
+        if ((args.length != inputArgumentCount)) {
             System.out.println(appName + ":Illegal options");
             System.out.println("Usage: " + appName + "[directory]");
-            return ;
+            return;
         }
-
+        
         String landing_zone = args[0];
-
+        
         controller.doValidation(landing_zone);
     }
-
-    public void setController(ValidationController controller){
+    
+    public void setController(ValidationController controller) {
         this.controller = controller;
     }
-
-    public ValidationController getControler(){
+    
+    public ValidationController getControler() {
         return controller;
     }
-
-    public void setappName(String name){
+    
+    public void setappName(String name) {
         this.appName = name;
     }
-
-    public String getappName(){
+    
+    public String getappName() {
         return appName;
     }
-
-    public void setn_args(int n){
-        this.n_args = n;
+    
+    public void setInputArgumentCount(int n) {
+        this.inputArgumentCount = n;
     }
-
-    public int getn_args(){
-        return n_args;
+    
+    public int getInputArgumentCount() {
+        return inputArgumentCount;
     }
 }
-
-
