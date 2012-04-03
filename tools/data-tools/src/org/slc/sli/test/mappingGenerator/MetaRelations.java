9ca43cd4f3fa7b1073bb480d3a7cfeb21fe4665d
Danilo Duran <dduran@wgen.net>
2012-04-03 10:19:42 -0400

    add comments

--- tools/data-tools/src/org/slc/sli/test/mappingGenerator/MetaRelations.java
+++ tools/data-tools/src/org/slc/sli/test/mappingGenerator/MetaRelations.java
@@ -105,6 +105,8 @@ public class MetaRelations {
 
             CourseMeta courseMeta = new CourseMeta("course" + idNum, schoolMeta);
 
+            // it's useful to return the objects created JUST for this school
+            // add to both maps here to avoid loop in map.putAll if we merged maps later
             coursesForSchool.put(courseMeta.id, courseMeta);
             courseMap.put(courseMeta.id, courseMeta);
         }
@@ -120,6 +122,8 @@ public class MetaRelations {
 
             SessionMeta sessionMeta = new SessionMeta("session" + idNum, schoolMeta);
 
+            // it's useful to return the objects created JUST for this school
+            // add to both maps here to avoid loop in map.putAll if we merged maps later
             sessionsForSchool.put(sessionMeta.id, sessionMeta);
             sessionMap.put(sessionMeta.id, sessionMeta);
         }
@@ -140,6 +144,8 @@ public class MetaRelations {
 
                     SectionMeta sectionMeta = new SectionMeta("section" + idNum, schoolMeta, courseMeta, sessionMeta);
 
+                    // it's useful to return the objects created JUST for this school
+                    // add to both maps here to avoid loop in map.putAll if we merged maps later
                     sectionMapForSchool.put(sectionMeta.id, sectionMeta);
                     sectionMap.put(sectionMeta.id, sectionMeta);
                 }
@@ -156,6 +162,8 @@ public class MetaRelations {
 
             TeacherMeta teacherMeta = new TeacherMeta("teacher" + idNum, schoolMeta);
 
+            // it's useful to return the objects created JUST for this school
+            // add to both maps here to avoid loop in map.putAll if we merged maps later
             teachersInSchoolMap.put(teacherMeta.id, teacherMeta);
             teacherMap.put(teacherMeta.id, teacherMeta);
         }
