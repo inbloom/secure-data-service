e126424625f0b8828de5f52dba5950e637864c5f
smelody@wgen.net <smelody@wgen.net>
2012-04-04 10:33:20 -0400

    Changed data prowler logger to log in JSon, including the logging gem, and using the logging middleware to add the remote IP to the log statement.

--- sli/databrowser/app/middleware/logging_middleware.rb
+++ sli/databrowser/app/middleware/logging_middleware.rb
@@ -1,38 +1,15 @@
 # Adds thread local variables that we can use for the log messages
 class LoggingMiddleware
-   def initialize(app)
+  def initialize(app)
     @app = app
   end
-  
- 
-   
-    def call(env)
-   # Thread.current[:remote] =  env["REMOTE_ADDR"]
-    Thread.current[:env] = env
-    Thread.current[:awesome] = 'Sean'
-      Thread.current[:remote] =  env["REMOTE_ADDR"]
-    puts "remote"
-  puts  Thread.current[:remote] 
-  puts "end remote"
-  puts   env["ENV_SESSION_KEY"]
-    puts "Session option #{env['ENV_SESSION_KEY']}#"
-    sessionId = env[:_session_id];
-    puts "Session id: #{sessionId}"
-    puts "the prowler lives #{env}\n"
 
-   
+  def call(env)
+    Thread.current[:remote] =  env["REMOTE_ADDR"]
+
+    # Conceptually, we should be able to fetch more variables from env and stash them in the Thread local space for later access
     @app.call(env)
-    
-  end
 
-  
-  
-  def threads( env )
-
-    Thread.current[:session_id] = session_id
-    Thread.current[:remote_ip] =  env["action_dispatch.remote_ip"]
-    Thread.current[:env] = env
-    puts "the prowler lives #{env}\n"
   end
 
 end
--- sli/databrowser/config/initializers/super_logger.rb
+++ sli/databrowser/config/initializers/super_logger.rb
@@ -1,26 +1,20 @@
 require 'active_support/core_ext/object/blank'
 
+# An extension of logger, trying to get access to more thread local variables.
+# We're currently using the logging Gem, so this is here for historic reason. :)
 class Logger
   def format_message(severity, timestamp, progname, msg)
     
     
-    puts     Thread.current[:awesome]
-    puts "logger remote: "
-     puts  Thread.current[:remote] 
-     puts "end logger remote"
+   
     output = { :serverity => severity, 
       :remote_ip => Thread.current[:remote],
-      #:user => Thread.current[:env],
- :awesome =>      Thread.current[:awesome],
- :request => Thread.current[:request],
-      :thread => Thread.current,
-      :session => Thread.current[:session_id] , :timestamp => timestamp, :progname => progname, :msg => msg }
-   # puts "Request #{request}"
-    #puts "UUID #{uuid}"
-     #puts "remote ip #{remote_ip}"
+      :user => Thread.current[:env],
+      :session => Thread.current[:session] , 
+      :timestamp => timestamp, :progname => progname, :msg => msg }
+   
    
     "#{output.to_json} \n"
-   # "FORMAT #{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n" 
-  end 
+   end 
 end
 
