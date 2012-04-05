2b6d2ea25783b977f9be93a86cf8b106ed79a247
Simon Yau <syau@wgen.net>
2012-04-05 12:58:37 -0400

    Add meta relation generation for program entities.

---
+++ tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/ProgramMeta.java
@@ -0,0 +1,16 @@
+package org.slc.sli.test.edfi.entities.relations;
+
+public class ProgramMeta {
+
+    public final String id;
+
+    public ProgramMeta(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ProgramMeta [id=" + id + "]";
+    }
+
+}
