0a11bcd3c17a4fde2efdce6feb8dbe69613b9403
Simon Yau <syau@wgen.net>
2012-04-05 14:39:05 -0400

    Add staff meta relations to sandbox data generator.

--- tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/ProgramMeta.java
+++ tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/ProgramMeta.java
@@ -1,16 +1,24 @@
 package org.slc.sli.test.edfi.entities.relations;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class ProgramMeta {
 
+    public Set<String> staffIds;
+    public Set<String> studentIds;
+    
     public final String id;
 
     public ProgramMeta(String id) {
         this.id = id;
+        staffIds = new HashSet<String>();
+        studentIds = new HashSet<String>();
     }
 
     @Override
     public String toString() {
-        return "ProgramMeta [id=" + id + "]";
+        return "ProgramMeta [id=" + id + ", staffIds=" + staffIds + ",studentIds=" + studentIds + "]";
     }
 
 }
--- tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/TeacherMeta.java
+++ tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/TeacherMeta.java
@@ -2,14 +2,11 @@ package org.slc.sli.test.edfi.entities.relations;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashSet;
-import java.util.Set;
 
 public class TeacherMeta {
     public final String id;
     public List<String> schoolIds;
     public List<String> sectionIds;
-    public Set<String> programIds;
 
     public final String simpleId;
 
@@ -21,14 +18,12 @@ public class TeacherMeta {
 
         this.sectionIds = new ArrayList<String>();
 
-        this.programIds = new HashSet<String>();
-
         this.simpleId = id;
     }
 
     @Override
     public String toString() {
-        return "TeacherMeta [id=" + id + ", schoolIds=" + schoolIds + ", sectionIds=" + sectionIds + ", programIds=" + programIds + "]";
+        return "TeacherMeta [id=" + id + ", schoolIds=" + schoolIds + ", sectionIds=" + sectionIds + "]";
     }
 
 }
