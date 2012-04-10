cd3d1f716875811ac18e7687034f43151b83b0a1
Danilo Duran <dduran@wgen.net>
2012-04-10 10:02:23 -0400

    add meta classes for Interchange-AssessmentMetadata

--- tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/AssessmentMeta.java
+++ tools/data-tools/src/org/slc/sli/test/edfi/entities/relations/AssessmentMeta.java
@@ -1,16 +1,22 @@
 package org.slc.sli.test.edfi.entities.relations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class AssessmentMeta {
 
     public final String id;
-    public String objectiveAssessmentId;
-    public String assessmentItemId;
-    public String assessmentPerformanceLevelId;
+    public final List<String> objectiveAssessmentIds;
+    public final List<String> assessmentItemIds;
+    public final List<String> performanceLevelDescriptorIds;
     public String assessmentFamilyId;
-    public String assessmentPeriodId;
+    public String assessmentPeriodDescriptorId;
 
     private AssessmentMeta(String id) {
         this.id = id;
+        this.objectiveAssessmentIds = new ArrayList<String>();
+        this.assessmentItemIds = new ArrayList<String>();
+        this.performanceLevelDescriptorIds = new ArrayList<String>();
     }
 
     public static AssessmentMeta create(String id) {
@@ -19,9 +25,10 @@ public final class AssessmentMeta {
 
     @Override
     public String toString() {
-        return "AssessmentMeta [id=" + id + ", objectiveAssessmentId=" + objectiveAssessmentId + ", assessmentItemId="
-                + assessmentItemId + ", assessmentPerformanceLevelId=" + assessmentPerformanceLevelId
-                + ", assessmentFamilyId=" + assessmentFamilyId + ", assessmentPeriodId=" + assessmentPeriodId + "]";
+        return "AssessmentMeta [id=" + id + ", objectiveAssessmentIds=" + objectiveAssessmentIds
+                + ", assessmentItemIds=" + assessmentItemIds + ", performanceLevelDescriptorIds="
+                + performanceLevelDescriptorIds + ", assessmentFamilyId=" + assessmentFamilyId
+                + ", assessmentPeriodDescriptorId=" + assessmentPeriodDescriptorId + "]";
     }
 
 }
