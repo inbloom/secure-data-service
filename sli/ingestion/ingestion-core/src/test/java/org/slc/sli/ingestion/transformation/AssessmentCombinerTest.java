03b98c676334a9e65cb4fad54aaeb77c5d8547ab
Nicholas Brown <nbrown@wgen.net>
2012-04-12 12:29:49 -0400

    fix checkstyle

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/AssessmentCombinerTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/AssessmentCombinerTest.java
@@ -9,22 +9,19 @@ import junitx.util.PrivateAccessor;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.slc.sli.ingestion.NeutralRecord;
+import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
+import org.slc.sli.ingestion.dal.NeutralRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import org.slc.sli.domain.NeutralQuery;
-import org.slc.sli.ingestion.NeutralRecord;
-import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
-import org.slc.sli.ingestion.dal.NeutralRecordRepository;
 
 /**
  * Unit Test for AssessmentCombiner
