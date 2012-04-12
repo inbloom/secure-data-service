cbe31973d5ad7413caaebc40c7f9b0e21b4a1c5b
Stan Halka <shalka@wgen.net>
2012-04-12 21:11:14 +0100

    fixed checkstyle errors - unused imports

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
@@ -1,9 +1,7 @@
 package org.slc.sli.ingestion.transformation;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
