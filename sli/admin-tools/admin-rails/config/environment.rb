ae822acc8d9d2ff6fddd490f356837beeebf5f37
Patrick Wolf <pwolf@wgen.net>
2012-04-13 16:28:04 -0400

    Fixing session errors in rails apps

--- sli/admin-tools/admin-rails/config/environment.rb
+++ sli/admin-tools/admin-rails/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Put this here until we fix storing OAuth object directly in session
+require "oauth_helper"
+
 # Initialize the rails application
 SLIAdmin::Application.initialize!
