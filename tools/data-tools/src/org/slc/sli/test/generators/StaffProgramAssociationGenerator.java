0368c7bad06c5927cd41584ab161652f50cf53d6
Simon Yau <syau@wgen.net>
2012-04-16 16:59:49 -0400

    Sandbox data generation for StaffProgramAssocation entities; not turned on pending descision from EdFi as to where (and whether)to put it in one of the interchanges.

---
+++ tools/data-tools/src/org/slc/sli/test/generators/StaffProgramAssociationGenerator.java
@@ -0,0 +1,87 @@
+package org.slc.sli.test.generators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.slc.sli.test.edfi.entities.ProgramIdentityType;
+import org.slc.sli.test.edfi.entities.ProgramReferenceType;
+import org.slc.sli.test.edfi.entities.StaffIdentityType;
+import org.slc.sli.test.edfi.entities.StaffProgramAssociation;
+import org.slc.sli.test.edfi.entities.StaffReferenceType;
+import org.slc.sli.test.edfi.entities.meta.ProgramMeta;
+
+
+/**
+ * Generates StudentProgramAssociation from ProgramMeta
+ * or
+ * String studentId, String programId, String schoolId
+ *
+ * @author slee
+ *
+ */
+public class StaffProgramAssociationGenerator {
+
+    private static String beginDate = "2011-03-04";
+    private static String endDate = "2012-03-04";
+    private static Boolean HAS_STUDENT_RECORD_ACCESS = true;
+    
+    /**
+     * Generates a list of StaffProgramAssociation.
+     *
+     * @param programMeta
+     * 
+     * @return <code>List<StaffProgramAssociation></code>
+     */
+    public static List<StaffProgramAssociation> generateLowFi(ProgramMeta programMeta) {
+        Set<String> staffIds = programMeta.staffIds;
+        String programId = programMeta.id;
+        String schoolId = programMeta.schoolId;
+        
+        List<StaffProgramAssociation> list = new ArrayList<StaffProgramAssociation>(staffIds.size());
+        
+        for (String staffId : staffIds) {
+            list.add(generateLowFi(staffId, programId, schoolId));
+        }
+        return list;
+    }
+    /**
+     * Generates a StudentProgramAssociation.
+     *
+     * @param studentId
+     * @param programId
+     * @param schoolId
+     * 
+     * @return <code>StudentProgramAssociation</code>
+     */
+    public static StaffProgramAssociation generateLowFi(String staffId, String programId, String schoolId) {
+
+        StaffProgramAssociation staffProgram = new StaffProgramAssociation();
+        
+        // construct and add the staff reference
+        StaffIdentityType sit = new StaffIdentityType();
+        sit.setStaffUniqueStateId(staffId);
+        StaffReferenceType srt = new StaffReferenceType();
+        srt.setStaffIdentity(sit);
+        List<StaffReferenceType> staffReferences = staffProgram.getStaffReference();
+        staffReferences.add(srt);
+
+        // construct and add the program reference       
+        ProgramIdentityType pi = new ProgramIdentityType();
+        pi.setProgramId(programId);
+        pi.getStateOrganizationIdOrEducationOrgIdentificationCode().add(schoolId);
+        ProgramReferenceType prt = new ProgramReferenceType();
+        prt.setProgramIdentity(pi);
+        List<ProgramReferenceType> programReferences = staffProgram.getProgramReference();
+        programReferences.add(prt);
+
+        //set begin and end dates
+        staffProgram.setBeginDate(beginDate);
+        staffProgram.setEndDate(endDate);
+        
+        //set has program access
+        staffProgram.setStudentRecordAccess(HAS_STUDENT_RECORD_ACCESS);
+        
+        return staffProgram;
+    }
+}
