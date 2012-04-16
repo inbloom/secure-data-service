1ba3507ad7fbbe785084ab6d66b89c60acf26f21
Nicholas Brown <nbrown@wgen.net>
2012-04-16 11:25:01 -0400

    wrong logging levels used

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoRepository.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoRepository.java
@@ -28,58 +28,60 @@ import org.slc.sli.util.threadutil.ThreadLocalStorage;
 /**
  * mongodb implementation of the repository interface that provides basic
  * CRUD and field query methods for all object classes.
- *
+ * 
  * @author Thomas Shewchuk tshewchuk@wgen.net 3/2/2012 (PI3 US1226)
- *
+ * 
  */
 
 public abstract class MongoRepository<T> implements Repository<T> {
     protected static final Logger LOG = LoggerFactory.getLogger(MongoRepository.class);
     
     protected ThreadLocalStorage threadStore = new ThreadLocalStorage("" + UUID.randomUUID().toString());
-
+    
     protected MongoTemplate template;
-
+    
     protected IdConverter idConverter;
-
+    
     @Autowired
     protected MongoQueryConverter queryConverter;
-
+    
     public void setTemplate(MongoTemplate template) {
         this.template = template;
     }
-
+    
     public MongoTemplate getTemplate() {
         return template;
     }
-
+    
     public void setidConverter(IdConverter idConverter) {
         this.idConverter = idConverter;
     }
-
+    
     @Override
     public T create(String type, Map<String, Object> body) {
         return create(type, body, type);
     }
-
+    
     @Override
     public T create(String type, Map<String, Object> body, String collectionName) {
         return create(type, body, new HashMap<String, Object>(), collectionName);
     }
-
+    
     @Override
     public abstract T create(String type, Map<String, Object> body, Map<String, Object> metaData, String collectionName);
-
+    
     public T create(T record, String collectionName) {
         template.save(record, getComposedCollectionName(collectionName));
-        LOG.info(" create a record in collection {} with id {}", new Object[] { getComposedCollectionName(collectionName), getRecordId(record) });
+        LOG.debug(" create a record in collection {} with id {}", new Object[] {
+                getComposedCollectionName(collectionName), getRecordId(record) });
         return record;
     }
-
+    
     @Override
     public T findById(String collectionName, String id) {
         Object databaseId = idConverter.toDatabaseId(id);
-        LOG.debug("find a record in collection {} with id {}", new Object[] { getComposedCollectionName(collectionName), id });
+        LOG.debug("find a record in collection {} with id {}", new Object[] {
+                getComposedCollectionName(collectionName), id });
         try {
             return template.findById(databaseId, getRecordClass(), getComposedCollectionName(collectionName));
         } catch (Exception e) {
@@ -87,82 +89,82 @@ public abstract class MongoRepository<T> implements Repository<T> {
             return null;
         }
     }
-
+    
     @Override
     public T findOne(String collectionName, NeutralQuery neutralQuery) {
-
-        //convert the neutral query into a mongo query
+        
+        // convert the neutral query into a mongo query
         Query mongoQuery = this.queryConverter.convert(getComposedCollectionName(collectionName), neutralQuery);
-
+        
         // find and return an entity
         return template.findOne(mongoQuery, getRecordClass(), getComposedCollectionName(collectionName));
     }
-
+    
     @Override
     public Iterable<T> findAll(String collectionName) {
         return findAll(collectionName, new NeutralQuery());
     }
-
+    
     @Override
     public Iterable<T> findAll(String collectionName, NeutralQuery neutralQuery) {
-
-        //convert the neutral query into a mongo query
+        
+        // convert the neutral query into a mongo query
         Query mongoQuery = this.queryConverter.convert(getComposedCollectionName(collectionName), neutralQuery);
-
+        
         // find and return an instance
         return template.find(mongoQuery, getRecordClass(), getComposedCollectionName(collectionName));
     }
-
+    
     @Override
     public Iterable<String> findAllIds(String collectionName, NeutralQuery neutralQuery) {
         if (neutralQuery == null) {
             neutralQuery = new NeutralQuery();
         }
         neutralQuery.setIncludeFields("_id");
-
+        
         List<String> ids = new ArrayList<String>();
         for (T t : findAll(collectionName, neutralQuery)) {
             ids.add(this.getRecordId(t));
         }
         return ids;
     }
-
-
+    
     @Override
     public Iterable<T> findAllByPaths(String collectionName, Map<String, String> paths, NeutralQuery neutralQuery) {
         Query mongoQuery = this.queryConverter.convert(getComposedCollectionName(collectionName), neutralQuery);
-
+        
         for (Map.Entry<String, String> field : paths.entrySet()) {
             mongoQuery.addCriteria(Criteria.where(field.getKey()).is(field.getValue()));
         }
-
+        
         // find and return an entity
         return template.find(mongoQuery, getRecordClass(), getComposedCollectionName(collectionName));
     }
-
+    
     @Override
     public long count(String collectionName, NeutralQuery neutralQuery) {
         DBCollection collection = template.getCollection(getComposedCollectionName(collectionName));
         if (collection == null) {
             return 0;
         }
-        return collection.count(this.queryConverter.convert(getComposedCollectionName(collectionName), neutralQuery).getQueryObject());
+        return collection.count(this.queryConverter.convert(getComposedCollectionName(collectionName), neutralQuery)
+                .getQueryObject());
     }
-
+    
     @Override
     public DBCollection getCollection(String collectionName) {
         return template.getCollection(getComposedCollectionName(collectionName));
     }
-
+    
     @Override
     public abstract boolean update(String collection, T record);
-
+    
     public boolean update(String collection, T record, Map<String, Object> body) {
         Assert.notNull(record, "The given record must not be null!");
         String id = getRecordId(record);
         if (id.equals(""))
             return false;
-
+        
         T found = template.findOne(new Query(Criteria.where("_id").is(idConverter.toDatabaseId(id))), getRecordClass(),
                 collection);
         if (found != null) {
@@ -170,35 +172,34 @@ public abstract class MongoRepository<T> implements Repository<T> {
         }
         WriteResult result = template.updateFirst(new Query(Criteria.where("_id").is(idConverter.toDatabaseId(id))),
                 new Update().set("body", body), collection);
-        LOG.info("update a record in collection {} with id {}", new Object[] { collection, id });
+        LOG.debug("update a record in collection {} with id {}", new Object[] { collection, id });
         return result.getN() == 1;
     }
-
+    
     @Override
     public CommandResult execute(DBObject command) {
         return template.executeCommand(command);
     }
-
-
+    
     @Override
     public boolean delete(String collectionName, String id) {
         if (id.equals(""))
             return false;
         T deleted = template.findAndRemove(new Query(Criteria.where("_id").is(idConverter.toDatabaseId(id))),
                 getRecordClass(), getComposedCollectionName(collectionName));
-        LOG.info("delete a entity in collection {} with id {}", new Object[] { getComposedCollectionName(collectionName), id });
+        LOG.debug("delete a entity in collection {} with id {}", new Object[] {
+                getComposedCollectionName(collectionName), id });
         return deleted != null;
     }
-
+    
     @Override
     public void deleteAll(String collectionName) {
         for (T t : this.findAll(collectionName)) {
             this.delete(collectionName, getRecordId(t));
         }
-        LOG.info("delete all objects in collection {}", getComposedCollectionName(collectionName));
+        LOG.debug("delete all objects in collection {}", getComposedCollectionName(collectionName));
     }
-
-
+    
     protected void logResults(String collectioName, List<T> results) {
         if (results == null) {
             LOG.debug("find objects in collection {} with total numbers is {}", new Object[] { collectioName, 0 });
@@ -207,9 +208,10 @@ public abstract class MongoRepository<T> implements Repository<T> {
                     new Object[] { collectioName, results.size() });
         }
     }
-
+    
     /**
-     * returns collection name with a append of collectionGroupingIdentifier (if collectionGroupingFlag is set to true)
+     * returns collection name with a append of collectionGroupingIdentifier (if
+     * collectionGroupingFlag is set to true)
      * 
      * @param collectionName
      * @return
@@ -222,7 +224,6 @@ public abstract class MongoRepository<T> implements Repository<T> {
         return collectionName;
     }
     
-    
     public boolean isCollectionGrouping() {
         if (threadStore.get("collectionGroupingFlag") != null) {
             return (Boolean) threadStore.get("collectionGroupingFlag");
@@ -230,7 +231,7 @@ public abstract class MongoRepository<T> implements Repository<T> {
         
         return false;
     }
-
+    
     public void setCollectionGrouping(boolean collectionGrouping) {
         threadStore.put("collectionGroupingFlag", collectionGrouping);
     }
@@ -238,48 +239,48 @@ public abstract class MongoRepository<T> implements Repository<T> {
     public String getCollectionGroupingIdentifier() {
         return (String) threadStore.get("collectionGroupingIdentifier");
     }
-
+    
     public void setCollectionGroupingIdentifier(String collectionGroupingIdentifier) {
         threadStore.put("collectionGroupingIdentifier", collectionGroupingIdentifier);
     }
     
     protected abstract String getRecordId(T record);
-
+    
     protected abstract Class<T> getRecordClass();
-
+    
     @Override
     @Deprecated
     public Iterable<T> findByPaths(String collectionName, Map<String, String> paths) {
         Query query = new Query();
-
+        
         return findByQuery(collectionName, addSearchPathsToQuery(query, paths));
     }
-
+    
     @Deprecated
     protected Iterable<T> findByQuery(String collectionName, Query query) {
         List<T> results = template.find(query, getRecordClass(), getComposedCollectionName(collectionName));
         logResults(getComposedCollectionName(collectionName), results);
         return results;
     }
-
+    
     @Override
     @Deprecated
     public Iterable<T> findByQuery(String collectionName, Query query, int skip, int max) {
         if (query == null)
             query = new Query();
-
+        
         query.skip(skip).limit(max);
-
+        
         return findByQuery(collectionName, query);
     }
-
+    
     @Deprecated
     private Query addSearchPathsToQuery(Query query, Map<String, String> searchPaths) {
         for (Map.Entry<String, String> field : searchPaths.entrySet()) {
             Criteria criteria = Criteria.where(field.getKey()).is(field.getValue());
             query.addCriteria(criteria);
         }
-
+        
         return query;
     }
 }
