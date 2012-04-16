0d63ccfeed5dc142c94d96d5fd30a896ee48c040
Suneth Rupasinghe <srupasinghe@wgen.net>
2012-04-16 09:37:41 -0400

    Defect DE180

--- sli/data-access/dal/src/test/java/org/slc/sli/dal/repository/MongoQueryConverterTest.java
+++ sli/data-access/dal/src/test/java/org/slc/sli/dal/repository/MongoQueryConverterTest.java
@@ -1,13 +1,21 @@
 package org.slc.sli.dal.repository;
 
-import static org.junit.Assert.assertEquals;
-
+import com.mongodb.DBObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.slc.sli.domain.NeutralCriteria;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 /**
  * JUnits for DAL
@@ -15,6 +23,12 @@ import org.slc.sli.domain.NeutralCriteria;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class MongoQueryConverterTest {
+    private MongoQueryConverter mongoQueryConverter;
+
+    @Before
+    public void setup() {
+        mongoQueryConverter = new MongoQueryConverter();
+    }
     
     @Test
     public void testKeyPrefixing() {
@@ -29,5 +43,45 @@ public class MongoQueryConverterTest {
         assertEquals(MongoQueryConverter.prefixKey(neutralCriteria3), "_id");
         assertEquals(MongoQueryConverter.prefixKey(neutralCriteria4), "body.metadata._id");
     }
+
+    @Test
+    public void testAddCriteria() {
+        Query query = new Query();
+        NeutralCriteria neutralCriteria1 = new NeutralCriteria("eventDate", ">=", "2011-09-08");
+        NeutralCriteria neutralCriteria2 = new NeutralCriteria("eventDate", "<=", "2012-04-08");
+
+        List<NeutralCriteria> list = new ArrayList<NeutralCriteria>();
+        list.add(neutralCriteria1);
+        list.add(neutralCriteria2);
+
+        Map<String, List<NeutralCriteria>> map = new HashMap<String, List<NeutralCriteria>>();
+        map.put("eventDate", list);
+
+        query = mongoQueryConverter.addCriteria(query, map);
+
+        assertNotNull("Should not be null", query);
+        DBObject obj = query.getQueryObject();
+        assertTrue("Should not be null", obj.containsField("body.eventDate"));
+
+        DBObject criteria = (DBObject) obj.get("body.eventDate");
+        assertNotNull("Should not be null", criteria.get("$gte"));
+        assertNotNull("Should not be null", criteria.get("$lte"));
+    }
+
+    @Test
+    public void testNullAddCriteria() {
+        Query query = new Query();
+
+        query = mongoQueryConverter.addCriteria(query, null);
+        assertNotNull("Should not be null", query);
+        DBObject obj = query.getQueryObject();
+        assertEquals("Should match", 0, obj.keySet().size());
+
+        assertNull("Should be null", mongoQueryConverter.addCriteria(null, null));
+        assertNull("Should be null", mongoQueryConverter.addCriteria(null,
+                new HashMap<String, List<NeutralCriteria>>()));
+
+    }
+
 
 }
