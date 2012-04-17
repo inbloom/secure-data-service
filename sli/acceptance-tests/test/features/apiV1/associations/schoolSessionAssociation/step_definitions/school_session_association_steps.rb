130b327e54ca46793a1ba8dffbc4202f5295f43f
Matthew Lane <lanemt@gmail.com>
2012-04-17 14:58:31 -0400

    adds dailyAttendance to create & read test

--- sli/acceptance-tests/test/features/apiV1/associations/schoolSessionAssociation/step_definitions/school_session_association_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/associations/schoolSessionAssociation/step_definitions/school_session_association_steps.rb
@@ -24,34 +24,34 @@ Transform /^<([^"]*)>$/ do |human_readable_id|
   id = "getSchoolSessionAssociations"           if human_readable_id == "ASSOCIATION LINK NAME"
   id = "schoolSessionAssociation"               if human_readable_id == "ASSOCIATION TYPE"
   id = "schoolSessionAssociations"              if human_readable_id == "ASSOCIATION URI"
-  
+
   #school related data
   id = "schoolId"                               if human_readable_id == "ENDPOINT1 FIELD"
   id = "67ce204b-9999-4a11-aaab-000000000008"   if human_readable_id == "ENDPOINT1 ID"          or human_readable_id == "ENDPOINT1 FIELD EXPECTED VALUE"
-  id = "getSchool"                              if human_readable_id == "ENDPOINT1 LINK NAME" 
-  id = "getSchools"                             if human_readable_id == "ENDPOINT1 RESOLUTION LINK NAME" 
-  id = "school"                                 if human_readable_id == "ENDPOINT1 TYPE" 
-  id = "schools"                                if human_readable_id == "ENDPOINT1 URI" 
-  
+  id = "getSchool"                              if human_readable_id == "ENDPOINT1 LINK NAME"
+  id = "getSchools"                             if human_readable_id == "ENDPOINT1 RESOLUTION LINK NAME"
+  id = "school"                                 if human_readable_id == "ENDPOINT1 TYPE"
+  id = "schools"                                if human_readable_id == "ENDPOINT1 URI"
+
   #session related data
   id = "sessionId"                              if human_readable_id == "ENDPOINT2 FIELD"
   id = "67ce204b-9999-4a11-aacb-000000000000"   if human_readable_id == "ENDPOINT2 ID"          or human_readable_id == "ENDPOINT2 FIELD EXPECTED VALUE"
-  id = "getSession"                             if human_readable_id == "ENDPOINT2 LINK NAME" 
-  id = "getSessions"                            if human_readable_id == "ENDPOINT2 RESOLUTION LINK NAME" 
-  id = "session"                                if human_readable_id == "ENDPOINT2 TYPE" 
-  id = "sessions"                               if human_readable_id == "ENDPOINT2 URI" 
-  
+  id = "getSession"                             if human_readable_id == "ENDPOINT2 LINK NAME"
+  id = "getSessions"                            if human_readable_id == "ENDPOINT2 RESOLUTION LINK NAME"
+  id = "session"                                if human_readable_id == "ENDPOINT2 TYPE"
+  id = "sessions"                               if human_readable_id == "ENDPOINT2 URI"
+
   #update related field data
   id = "sessionId"                              if human_readable_id == "UPDATE FIELD"
-  id = "67ce204b-9999-4a11-aacb-000000000000"   if human_readable_id == "UPDATE FIELD EXPECTED VALUE" 
-  id = "67ce204b-9999-4a11-aacb-000000000001"   if human_readable_id == "UPDATE FIELD NEW VALID VALUE" 
-  
+  id = "67ce204b-9999-4a11-aacb-000000000000"   if human_readable_id == "UPDATE FIELD EXPECTED VALUE"
+  id = "67ce204b-9999-4a11-aacb-000000000001"   if human_readable_id == "UPDATE FIELD NEW VALID VALUE"
+
   #general
   id = "11111111-1111-1111-1111-111111111111"   if human_readable_id == "INVALID REFERENCE"
-  id = "self"                                   if human_readable_id == "SELF LINK NAME" 
+  id = "self"                                   if human_readable_id == "SELF LINK NAME"
   id = @newId                                   if human_readable_id == "NEWLY CREATED ASSOCIATION ID"
   id = "Validation failed"                      if human_readable_id == "VALIDATION"
-  
+
   #return the translated value
   id
 end
@@ -70,6 +70,31 @@ Given /^a valid association json document for a "([^"]*)"$/ do |arg1|
       "beginDate" => "2011-09-01",
       "endDate" => "2011-10-31",
       "totalInstructionalDays" =>  45
+    },
+    "dailyAttendance" => {
+        "schoolYearAttendance" =>
+            [{
+                 "schoolYear" => "2011-2012",
+                 "attendanceEvent" => [{
+                                           "event" => "In Attendance",
+                                           "date" => "2011-11-01"
+                                       },
+                                       {
+                                           "event" => "In Attendance",
+                                           "date" => "2011-11-02"
+                                       }]
+             },
+             {
+                "schoolYear" => "2010-2011",
+                "attendanceEvent" => [{
+                                          "event" => "In Attendance",
+                                          "date" => "2010-11-01"
+                                      },
+                                      {
+                                          "event" => "Tardy",
+                                          "date" => "2010-11-02"
+                                      }]
+            }]
     }
   }
 end
