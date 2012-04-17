7d6c607e5c3ca7637d87c622dbf606e8d9faab62
Vijay Ummalaneni <vummalaneni@gmail.com>
2012-04-17 18:15:02 -0400

    Changing the Background Color to Pixel Perfect Color

--- sli/dashboard/src/main/webapp/static/js/fuelGaugeWidget.js
+++ sli/dashboard/src/main/webapp/static/js/fuelGaugeWidget.js
@@ -18,7 +18,7 @@ function FuelGaugeWidget (element, score, cutpoints) {
 
     // constants
     this.PADDING = 2; // paddings between levels, in pixel
-    this.BACKGROUNDCOLOUR = "#CCC"; // background colour of rectangles
+    this.BACKGROUNDCOLOUR = "#CCCCCC"; // background colour of rectangles
 
     this.element = element;
     this.score = score;
