20718269540871174b9ab0b1febe599453c53cd8
Nicholas Brown <nbrown@wgen.net>
2012-04-17 18:09:34 -0400

    student assessments going through combiner

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/assessment/StudentAssessmentCombiner.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/assessment/StudentAssessmentCombiner.java
@@ -1,20 +1,28 @@
 package org.slc.sli.ingestion.transformation.assessment;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
+import org.slc.sli.domain.Repository;
+import org.slc.sli.ingestion.FileType;
+import org.slc.sli.ingestion.NeutralRecord;
+import org.slc.sli.ingestion.NeutralRecordFileWriter;
+import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
+import org.slc.sli.ingestion.landingzone.IngestionFileEntry;
+import org.slc.sli.ingestion.transformation.AbstractTransformationStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import org.slc.sli.domain.Repository;
-import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
-import org.slc.sli.ingestion.transformation.AbstractTransformationStrategy;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 
 /**
  * Transformer for StudentAssessment entities
@@ -29,27 +37,64 @@ public class StudentAssessmentCombiner extends AbstractTransformationStrategy {
     public static final String SOA_EDFI_COLLECTION_NAME = "studentObjectiveAssessment";
     private static final String STUDENT_ASSESSMENT_REFERENCE = "sTAReference";
     private static final String OBJECTIVE_ASSESSMENT_REFERENCE = "oAReference";
+    private static final String XML_ID_FIELD = "xmlId";
     
+    @SuppressWarnings("unchecked")
     @Override
     protected void performTransformation() {
         NeutralRecordMongoAccess db = getNeutralRecordMongoAccess();
-        Repository<?> repo = db.getRecordRepository();
-        DBCollection coll = repo.getCollection(SOA_EDFI_COLLECTION_NAME);
-        DBCursor cursor = coll.find(new BasicDBObject(BATCH_JOB_ID_KEY, getBatchJobId()));
-        while (cursor.hasNext()) {
-            DBObject n = cursor.next();
-            LOG.debug("Transforming student objective assessment {}", n);
-            @SuppressWarnings("unchecked")
-            Map<String, Object> body = (Map<String, Object>) n.get("body");
-            Object studentAssessmentRef = body.get(STUDENT_ASSESSMENT_REFERENCE);
-            Object objectiveAssessmentRef = body.get(OBJECTIVE_ASSESSMENT_REFERENCE);
-            body.remove(STUDENT_ASSESSMENT_REFERENCE);
-            body.remove(OBJECTIVE_ASSESSMENT_REFERENCE);
-            LOG.debug("body is {}", body);
-            LOG.debug("references student-assessment {} and objective assessment {}", new Object[] {
-                    studentAssessmentRef, objectiveAssessmentRef });
-            // look up related student objective assessments
+        Repository<NeutralRecord> repo = db.getRecordRepository();
+        DBCollection studentAssessments = repo.getCollection(SA_EDFI_COLLECTION_NAME);
+        for (IngestionFileEntry fe : getFEs()) {
+            BasicDBObject query = new BasicDBObject(BATCH_JOB_ID_KEY, getBatchJobId());
+            query.append("sourceFile", fe.getFileName());
+            DBCursor cursor = studentAssessments.find(query);
+            try {
+                NeutralRecordFileWriter writer = new NeutralRecordFileWriter(fe.getNeutralRecordFile());
+                try {
+                    while (cursor.hasNext()) {
+                        DBObject studentAssessment = cursor.next();
+                        LOG.debug("Transforming student assessment {}", studentAssessment);
+                        @SuppressWarnings("unchecked")
+                        Map<String, Object> body = ((DBObject) studentAssessment.get("body")).toMap();
+                        String id = (String) body.get(XML_ID_FIELD);
+                        body.remove(XML_ID_FIELD);
+                        Iterable<NeutralRecord> studentObjectAssessmentsRefs = repo.findAll(SOA_EDFI_COLLECTION_NAME,
+                                new NeutralQuery(new NeutralCriteria(STUDENT_ASSESSMENT_REFERENCE, "=", id)));
+                        LOG.debug("related SOAs are {}", studentObjectAssessmentsRefs);
+                        List<Map<String, Object>> soas = new ArrayList<Map<String, Object>>();
+                        for (NeutralRecord ref : studentObjectAssessmentsRefs) {
+                            soas.add(resolveSoa(ref));
+                        }
+                        body.put("studentObjectiveAssessments", soas);
+                        NeutralRecord transformed = new NeutralRecord();
+                        transformed.setRecordType(SA_EDFI_COLLECTION_NAME);
+                        transformed.setAttributes((Map<String, Object>) body);
+                        transformed.setLocalParentIds((Map<String, Object>) studentAssessment.get("localParentIds"));
+                        writer.writeRecord(transformed);
+                    }
+                } finally {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                LOG.error("Exception occurred while writing transformed student assessments", e);
+            }
         }
     }
     
+    private Map<String, Object> resolveSoa(NeutralRecord ref) {
+        // TODO this needs to be implemented to get the actual SOA
+        return ref.getAttributes();
+    }
+    
+    private List<IngestionFileEntry> getFEs() {
+        List<IngestionFileEntry> all = getJob().getFiles();
+        List<IngestionFileEntry> studentAssessmentFiles = new ArrayList<IngestionFileEntry>();
+        for (IngestionFileEntry fe : all) {
+            if (fe.getFileType().equals(FileType.XML_STUDENT_ASSESSMENT)) {
+                studentAssessmentFiles.add(fe);
+            }
+        }
+        return studentAssessmentFiles;
+    }
 }
