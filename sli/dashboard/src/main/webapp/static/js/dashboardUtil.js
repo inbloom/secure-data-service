8dc15396390e34a4bf30cccabb47370bcc2e2710
Dewayne Walker <dwalker@dwalker-mac.wgenhq.net>
2012-04-18 10:17:39 -0400

    Hiding mocked data

--- sli/dashboard/src/main/webapp/static/js/dashboardUtil.js
+++ sli/dashboard/src/main/webapp/static/js/dashboardUtil.js
@@ -231,7 +231,7 @@ DashboardUtil.Grid.Formatters = {
 DashboardUtil.Grid.Sorters = {
 		Enum: function(params) {
 			var enumHash = {};
-			params.sortEnum.sort(DashboardUtil.comparator);
+			params.sortEnum.sort(DashboardUtil.numbersFirstComparator);
 			for (var i in params.sortEnum) {
 				enumHash[params.sortEnum[i]] = i;
 			}
@@ -243,40 +243,41 @@ DashboardUtil.Grid.Sorters = {
 		}
 }
 
-DashboardUtil.comparator =function(a,b){
-	var aIsNull = a === null;
-	var bIsNull = b === null;
-	
-	if(aIsNull && bIsNull) {
-		return 0;
-	} else if (aIsNull) {
-		return 1;
-	} else if (bIsNull) {
-		return -1;
-	}
-	
-	var aIsNumber = a.match(/^\d+$/);
-	var bIsNumber = b.match(/^\d+$/);
-	
-	//Need to do numerical comparison. With lexicographical compare,
-	//the statement '15' < '2' evaluates to true.
-	if(aIsNumber && bIsNumber) {	
-		return parseInt(a) - parseInt(b);
+DashboardUtil.numbersFirstComparator = function(a,b){
+    var aIsNull = a === null;
+    var bIsNull = b === null;
+    
+    if(aIsNull && bIsNull) {
+        return 0;
+    } else if (aIsNull) {
+        return 1;
+    } else if (bIsNull) {
+        return -1;
     }
-	
-	//A lexicographical comparison is sufficient for two strings
-	if(!aIsNumber && !bIsNumber) {
-		return a < b ? -1 : (a == b ? 0 : 1);
-	}
-	
-	//Since we know that one of the values is a number and one is not 
-	//and we want numbers to precede strings.
-	if(aIsNumber) {
-		return -1;
-	} else {
-		return 1;
-	}
+    
+    var aIsNumber = a.match(/^\d+$/);
+    var bIsNumber = b.match(/^\d+$/);
+    
+    //Need to do numerical comparison. With lexicographical compare,
+    //the statement '15' < '2' evaluates to true.
+    if(aIsNumber && bIsNumber) {    
+        return parseInt(a) - parseInt(b);
+    }
+    
+    //A lexicographical comparison is sufficient for two strings
+    if(!aIsNumber && !bIsNumber) {
+        return a < b ? -1 : (a == b ? 0 : 1);
+    }
+    
+    //Since we know that one of the values is a number and one is not 
+    //and we want numbers to precede strings.
+    if(aIsNumber) {
+        return -1;
+    } else {
+        return 1;
+    }
 }
+
 compareInt = function compare(a,b){return a-b;}
 compareIntReverse = function compare(a,b){return b-a;}
 
