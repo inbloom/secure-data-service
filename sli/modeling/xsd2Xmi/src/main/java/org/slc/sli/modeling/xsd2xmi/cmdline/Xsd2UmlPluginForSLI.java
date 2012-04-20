76ecd0105c797a1b82b034f5cb7d40718c8fea26
David Holmes <dholmes@wgen.net>
2012-04-19 23:05:09 -0400

    Add ReferenceType for EventBellSceduleAssociation and EducationOrgAssociation.

--- sli/modeling/xsd2Xmi/src/main/java/org/slc/sli/modeling/xsd2xmi/cmdline/Xsd2UmlPluginForSLI.java
+++ sli/modeling/xsd2Xmi/src/main/java/org/slc/sli/modeling/xsd2xmi/cmdline/Xsd2UmlPluginForSLI.java
@@ -26,207 +26,195 @@ import org.slc.sli.modeling.xsd2xmi.core.Xsd2UmlPluginHost;
 
 public final class Xsd2UmlPluginForSLI implements Xsd2UmlPlugin {
 
-	private static final String NAMESPACE_SLI = "http://slc-sli/ed-org/0.1";
+    private static final String NAMESPACE_SLI = "http://slc-sli/ed-org/0.1";
 
-	private static final QName SLI_PII = new QName(NAMESPACE_SLI,
-			"PersonallyIdentifiableInfo");
-	private static final QName SLI_READ_ENFORCEMENT = new QName(NAMESPACE_SLI,
-			"ReadEnforcement");
-	private static final QName SLI_REFERENCE_TYPE = new QName(NAMESPACE_SLI,
-			"ReferenceType");
-	private static final QName SLI_WRITE_ENFORCEMENT = new QName(NAMESPACE_SLI,
-			"WriteEnforcement");
-	private static final String TAG_DEFINITION_NAME_COLLECTION = "collection";
-	private static final String TAG_DEFINITION_NAME_PII = camelCase(SLI_PII
-			.getLocalPart());
-	private static final String TAG_DEFINITION_NAME_READ = camelCase(SLI_READ_ENFORCEMENT
-			.getLocalPart());
-	private static final String TAG_DEFINITION_NAME_REF = camelCase(SLI_REFERENCE_TYPE
-			.getLocalPart());
-	private static final String TAG_DEFINITION_NAME_RESOURCE = "resource";
-	private static final String TAG_DEFINITION_NAME_WRITE = camelCase(SLI_WRITE_ENFORCEMENT
-			.getLocalPart());
+    private static final QName SLI_PII = new QName(NAMESPACE_SLI, "PersonallyIdentifiableInfo");
+    private static final QName SLI_READ_ENFORCEMENT = new QName(NAMESPACE_SLI, "ReadEnforcement");
+    private static final QName SLI_REFERENCE_TYPE = new QName(NAMESPACE_SLI, "ReferenceType");
+    private static final QName SLI_WRITE_ENFORCEMENT = new QName(NAMESPACE_SLI, "WriteEnforcement");
+    private static final String TAG_DEFINITION_NAME_COLLECTION = "collection";
+    private static final String TAG_DEFINITION_NAME_PII = camelCase(SLI_PII.getLocalPart());
+    private static final String TAG_DEFINITION_NAME_READ = camelCase(SLI_READ_ENFORCEMENT.getLocalPart());
+    private static final String TAG_DEFINITION_NAME_REFERENCE = camelCase(SLI_REFERENCE_TYPE.getLocalPart());
+    private static final String TAG_DEFINITION_NAME_RESOURCE = "resource";
+    private static final String TAG_DEFINITION_NAME_WRITE = camelCase(SLI_WRITE_ENFORCEMENT.getLocalPart());
 
-	private static final String camelCase(final String text) {
-		return text.substring(0, 1).toLowerCase().concat(text.substring(1));
-	}
+    // This approach is a bit hit-and-miss. It's hard to be sure you really have a reference.
+    // It is safer just to fix the W3C XML Schema.
+    private static final String NAME_ENDING_REFERENCE = "Reference";
+    @SuppressWarnings("unused")
+    // The example that broke this is InstitutionId.
+    private static final String NAME_ENDING_ID = "Id";
 
-	private static final TagDefinition makeTagDefinition(final String name,
-			final Occurs lower, final Occurs upper, final Xsd2UmlPluginHost host) {
-		final Range range = new Range(lower, upper);
-		final Multiplicity multiplicity = new Multiplicity(range);
-		final Identifier id = host.ensureTagDefinitionId(name);
-		return new TagDefinition(id, name, multiplicity);
-	}
+    private static final String camelCase(final String text) {
+        return text.substring(0, 1).toLowerCase().concat(text.substring(1));
+    }
 
-	private static final String stringValue(final NodeList markup) {
-		final StringBuilder sb = new StringBuilder();
-		final int length = markup.getLength();
-		for (int i = 0; i < length; i++) {
-			final Node node = markup.item(i);
-			sb.append(node.getTextContent());
-		}
-		return sb.toString();
-	}
+    private static final TagDefinition makeTagDefinition(final String name, final Occurs lower, final Occurs upper,
+            final Xsd2UmlPluginHost host) {
+        final Range range = new Range(lower, upper);
+        final Multiplicity multiplicity = new Multiplicity(range);
+        final Identifier id = host.ensureTagDefinitionId(name);
+        return new TagDefinition(id, name, multiplicity);
+    }
 
-	private static final String titleCase(final String text) {
-		return text.substring(0, 1).toUpperCase().concat(text.substring(1));
-	}
+    private static final String stringValue(final NodeList markup) {
+        final StringBuilder sb = new StringBuilder();
+        final int length = markup.getLength();
+        for (int i = 0; i < length; i++) {
+            final Node node = markup.item(i);
+            sb.append(node.getTextContent());
+        }
+        return sb.toString();
+    }
 
-	@Override
-	public List<TagDefinition> declareTagDefinitions(
-			final Xsd2UmlPluginHost config) {
-		final List<TagDefinition> tagDefinitions = new LinkedList<TagDefinition>();
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_REF,
-				Occurs.ZERO, Occurs.ONE, config));
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_PII,
-				Occurs.ZERO, Occurs.ONE, config));
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_READ,
-				Occurs.ZERO, Occurs.ONE, config));
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_WRITE,
-				Occurs.ZERO, Occurs.ONE, config));
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_RESOURCE,
-				Occurs.ZERO, Occurs.UNBOUNDED, config));
-		tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_COLLECTION,
-				Occurs.ZERO, Occurs.ONE, config));
-		return Collections.unmodifiableList(tagDefinitions);
-	}
+    private static final String titleCase(final String text) {
+        return text.substring(0, 1).toUpperCase().concat(text.substring(1));
+    }
 
-	@Override
-	public String getAssociationEndTypeName(final ClassType classType,
-			final Attribute attribute, final Xsd2UmlPluginHost host) {
-		final List<TaggedValue> taggedValues = attribute.getTaggedValues();
-		for (final TaggedValue taggedValue : taggedValues) {
-			final TagDefinition tagDefinition = host
-					.getTagDefinition(taggedValue.getTagDefinition());
-			if (tagDefinition.getName().equals(TAG_DEFINITION_NAME_REF)) {
-				return nameFromTypeName(new QName(taggedValue.getValue()));
-			}
-		}
-		return null;
-	}
+    @Override
+    public List<TagDefinition> declareTagDefinitions(final Xsd2UmlPluginHost config) {
+        final List<TagDefinition> tagDefinitions = new LinkedList<TagDefinition>();
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_REFERENCE, Occurs.ZERO, Occurs.ONE, config));
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_PII, Occurs.ZERO, Occurs.ONE, config));
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_READ, Occurs.ZERO, Occurs.ONE, config));
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_WRITE, Occurs.ZERO, Occurs.ONE, config));
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_RESOURCE, Occurs.ZERO, Occurs.UNBOUNDED, config));
+        tagDefinitions.add(makeTagDefinition(TAG_DEFINITION_NAME_COLLECTION, Occurs.ZERO, Occurs.ONE, config));
+        return Collections.unmodifiableList(tagDefinitions);
+    }
 
-	@Override
-	public boolean isAssociationEnd(final ClassType classType,
-			final Attribute attribute, final Xsd2UmlPluginHost host) {
-		final List<TaggedValue> taggedValues = attribute.getTaggedValues();
-		for (final TaggedValue taggedValue : taggedValues) {
-			final TagDefinition tagDefinition = host
-					.getTagDefinition(taggedValue.getTagDefinition());
-			if (tagDefinition.getName().equals(TAG_DEFINITION_NAME_REF)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public String getAssociationEndTypeName(final ClassType classType, final Attribute attribute,
+            final Xsd2UmlPluginHost host) {
+        // Look for the reference tag.
+        final List<TaggedValue> taggedValues = attribute.getTaggedValues();
+        for (final TaggedValue taggedValue : taggedValues) {
+            final TagDefinition tagDefinition = host.getTagDefinition(taggedValue.getTagDefinition());
+            if (tagDefinition.getName().equals(TAG_DEFINITION_NAME_REFERENCE)) {
+                return nameFromTypeName(new QName(taggedValue.getValue()));
+            }
+        }
+        // Look at the attribute name.
+        final String name = attribute.getName();
+        if (name.endsWith(NAME_ENDING_REFERENCE)) {
+            return titleCase(name.substring(0, name.length() - NAME_ENDING_REFERENCE.length()));
+        }
+        // else if (name.endsWith(NAME_ENDING_ID)) {
+        // return titleCase(name.substring(0, name.length() - NAME_ENDING_ID.length()));
+        // }
+        else {
+            return null;
+        }
+    }
 
-	@Override
-	public String nameAssociation(final AssociationEnd lhs,
-			final AssociationEnd rhs, final Xsd2UmlPluginHost host) {
-		return host.nameAssociation(lhs, rhs, host);
-	}
+    @Override
+    /**
+     * The most reliable way to determine whether the attribute should be an association is to look for the tag that indicates
+     * that the attribute should be treated as a reference. If there is no tag then we look for clues in the name of the attribute.
+     */
+    public boolean isAssociationEnd(final ClassType classType, final Attribute attribute, final Xsd2UmlPluginHost host) {
+        // Look for the reference tag.
+        final List<TaggedValue> taggedValues = attribute.getTaggedValues();
+        for (final TaggedValue taggedValue : taggedValues) {
+            final TagDefinition tagDefinition = host.getTagDefinition(taggedValue.getTagDefinition());
+            if (tagDefinition.getName().equals(TAG_DEFINITION_NAME_REFERENCE)) {
+                return true;
+            }
+        }
+        // Look at the attribute name.
+        final String name = attribute.getName();
+        if (name.endsWith(NAME_ENDING_REFERENCE)) {
+            return true;
+        }
+        // else if (name.endsWith(NAME_ENDING_ID)) {
+        // return true;
+        // }
+        else {
+            return false;
+        }
+    }
 
-	@Override
-	public String nameFromComplexTypeExtension(final QName complexType,
-			final QName base) {
-		return nameFromTypeName(complexType).concat(" extends ").concat(
-				nameFromTypeName(base));
-	}
+    @Override
+    public String nameAssociation(final AssociationEnd lhs, final AssociationEnd rhs, final Xsd2UmlPluginHost host) {
+        return host.nameAssociation(lhs, rhs, host);
+    }
 
-	@Override
-	public String nameFromElementName(final QName name) {
-		return name.getLocalPart();
-	}
+    @Override
+    public String nameFromComplexTypeExtension(final QName complexType, final QName base) {
+        return nameFromTypeName(complexType).concat(" extends ").concat(nameFromTypeName(base));
+    }
 
-	@Override
-	public String nameFromSimpleTypeRestriction(final QName simpleType,
-			final QName base) {
-		return nameFromTypeName(simpleType).concat(" restricts ").concat(
-				nameFromTypeName(base));
-	}
+    @Override
+    public String nameFromElementName(final QName name) {
+        return name.getLocalPart();
+    }
 
-	@Override
-	public String nameFromTypeName(final QName name) {
-		return titleCase(name.getLocalPart());
-	}
+    @Override
+    public String nameFromSimpleTypeRestriction(final QName simpleType, final QName base) {
+        return nameFromTypeName(simpleType).concat(" restricts ").concat(nameFromTypeName(base));
+    }
 
-	@Override
-	public List<TaggedValue> tagsFromAppInfo(final XmlSchemaAppInfo appInfo,
-			final Xsd2UmlPluginHost host) {
-		final List<TaggedValue> taggedValues = new LinkedList<TaggedValue>();
-		final NodeList markup = appInfo.getMarkup();
-		for (int i = 0; i < markup.getLength(); i++) {
-			final Node node = markup.item(i);
-			if (Node.ELEMENT_NODE == node.getNodeType()) {
-				final Element element = (Element) node;
+    @Override
+    public String nameFromTypeName(final QName name) {
+        return titleCase(name.getLocalPart());
+    }
 
-				final String namespace = element.getNamespaceURI();
-				final String localName = element.getLocalName();
-				final QName name = new QName(namespace, localName);
-				if (SLI_REFERENCE_TYPE.equals(name)) {
-					final Identifier tagDefinition = host
-							.ensureTagDefinitionId(TAG_DEFINITION_NAME_REF);
-					final String refereceType = nameFromTypeName(new QName(
-							XmlTools.collapseWhitespace(stringValue(element
-									.getChildNodes()))));
-					taggedValues.add(new TaggedValue(refereceType,
-							tagDefinition));
-				} else if (SLI_PII.equals(name)) {
-					final Identifier tagDefinition = host
-							.ensureTagDefinitionId(TAG_DEFINITION_NAME_PII);
-					final Boolean isPII = Boolean.valueOf(XmlTools
-							.collapseWhitespace(stringValue(element
-									.getChildNodes())));
-					taggedValues.add(new TaggedValue(isPII.toString(),
-							tagDefinition));
-				} else if (SLI_READ_ENFORCEMENT.equals(name)) {
-					final Identifier tagDefinition = host
-							.ensureTagDefinitionId(TAG_DEFINITION_NAME_READ);
-					final String text = XmlTools
-							.collapseWhitespace(stringValue(element
-									.getChildNodes()));
-					if ("READ_RESTRICTED".equals(text)) {
-						taggedValues.add(new TaggedValue(text, tagDefinition));
-					} else {
-						throw new AssertionError(
-								"Unexpected value for appinfo: " + name
-										+ " => " + text);
-					}
-				} else if (SLI_WRITE_ENFORCEMENT.equals(name)) {
-					final Identifier tagDefinition = host
-							.ensureTagDefinitionId(TAG_DEFINITION_NAME_WRITE);
-					final String text = XmlTools
-							.collapseWhitespace(stringValue(element
-									.getChildNodes()));
-					if ("WRITE_RESTRICTED".equals(text)) {
-						taggedValues.add(new TaggedValue(text, tagDefinition));
-					} else {
-						throw new AssertionError(
-								"Unexpected value for appinfo: " + name
-										+ " => " + text);
-					}
-				} else {
-					throw new AssertionError("Unexpected element in appinfo: "
-							+ name);
-				}
-			}
-		}
-		return taggedValues;
-	}
+    @Override
+    public List<TaggedValue> tagsFromAppInfo(final XmlSchemaAppInfo appInfo, final Xsd2UmlPluginHost host) {
+        final List<TaggedValue> taggedValues = new LinkedList<TaggedValue>();
+        final NodeList markup = appInfo.getMarkup();
+        for (int i = 0; i < markup.getLength(); i++) {
+            final Node node = markup.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                final Element element = (Element) node;
 
-	@Override
-	public List<TaggedValue> tagsFromTopLevelElement(final QName name,
-			final Xsd2UmlPluginHost host) {
-		final List<TaggedValue> tags = new LinkedList<TaggedValue>();
-		// This is a poor-man's pluralization!
-		final Identifier resourceTag = host
-				.ensureTagDefinitionId(TAG_DEFINITION_NAME_RESOURCE);
-		final TaggedValue resource = new TaggedValue(camelCase(
-				name.getLocalPart()).concat("s"), resourceTag);
-		tags.add(resource);
-		final Identifier collectionTag = host
-				.ensureTagDefinitionId(TAG_DEFINITION_NAME_COLLECTION);
-		tags.add(new TaggedValue(camelCase(name.getLocalPart()), collectionTag));
-		return Collections.unmodifiableList(tags);
-	}
+                final String namespace = element.getNamespaceURI();
+                final String localName = element.getLocalName();
+                final QName name = new QName(namespace, localName);
+                if (SLI_REFERENCE_TYPE.equals(name)) {
+                    final Identifier tagDefinition = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_REFERENCE);
+                    final String refereceType = nameFromTypeName(new QName(
+                            XmlTools.collapseWhitespace(stringValue(element.getChildNodes()))));
+                    taggedValues.add(new TaggedValue(refereceType, tagDefinition));
+                } else if (SLI_PII.equals(name)) {
+                    final Identifier tagDefinition = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_PII);
+                    final Boolean isPII = Boolean.valueOf(XmlTools.collapseWhitespace(stringValue(element
+                            .getChildNodes())));
+                    taggedValues.add(new TaggedValue(isPII.toString(), tagDefinition));
+                } else if (SLI_READ_ENFORCEMENT.equals(name)) {
+                    final Identifier tagDefinition = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_READ);
+                    final String text = XmlTools.collapseWhitespace(stringValue(element.getChildNodes()));
+                    if ("READ_RESTRICTED".equals(text)) {
+                        taggedValues.add(new TaggedValue(text, tagDefinition));
+                    } else {
+                        throw new AssertionError("Unexpected value for appinfo: " + name + " => " + text);
+                    }
+                } else if (SLI_WRITE_ENFORCEMENT.equals(name)) {
+                    final Identifier tagDefinition = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_WRITE);
+                    final String text = XmlTools.collapseWhitespace(stringValue(element.getChildNodes()));
+                    if ("WRITE_RESTRICTED".equals(text)) {
+                        taggedValues.add(new TaggedValue(text, tagDefinition));
+                    } else {
+                        throw new AssertionError("Unexpected value for appinfo: " + name + " => " + text);
+                    }
+                } else {
+                    throw new AssertionError("Unexpected element in appinfo: " + name);
+                }
+            }
+        }
+        return taggedValues;
+    }
+
+    @Override
+    public List<TaggedValue> tagsFromTopLevelElement(final QName name, final Xsd2UmlPluginHost host) {
+        final List<TaggedValue> tags = new LinkedList<TaggedValue>();
+        // This is a poor-man's pluralization!
+        final Identifier resourceTag = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_RESOURCE);
+        final TaggedValue resource = new TaggedValue(camelCase(name.getLocalPart()).concat("s"), resourceTag);
+        tags.add(resource);
+        final Identifier collectionTag = host.ensureTagDefinitionId(TAG_DEFINITION_NAME_COLLECTION);
+        tags.add(new TaggedValue(camelCase(name.getLocalPart()), collectionTag));
+        return Collections.unmodifiableList(tags);
+    }
 }
