f06a9d95c7f7ad6586761bd933134a9c34942753
Alexander Blum <ablum@wgen.net>
2012-04-24 15:23:01 -0400

    fixed IndexResourcePatternResolverTest.java to work in Jenkins

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/dal/IndexResourcePatternResolverTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/dal/IndexResourcePatternResolverTest.java
@@ -26,8 +26,7 @@ public class IndexResourcePatternResolverTest {
         String indexRootDir = "classpath:mongoIndexes/**/*.json";
         List<MongoIndexConfig> indexes = irpr.findAllIndexes(indexRootDir);
 
-        Assert.assertEquals(2, indexes.size());
-        Assert.assertEquals("student", indexes.get(1).getCollection());
+        Assert.assertEquals(1, indexes.size());
         Assert.assertEquals("student", indexes.get(0).getCollection());
 
         List<Map<String, String>> fields1 = indexes.get(0).getIndexFields();
@@ -35,13 +34,6 @@ public class IndexResourcePatternResolverTest {
         Assert.assertEquals("body.name", fields1.get(0).get("name"));
         Assert.assertEquals("body.sex", fields1.get(1).get("name"));
         Assert.assertEquals("body.birthDate", fields1.get(2).get("name"));
-
-
-        List<Map<String, String>> fields2 = indexes.get(1).getIndexFields();
-        Assert.assertEquals(1, fields2.size());
-        Assert.assertEquals("metaData.tenantId", fields2.get(0).get("name"));
-
-
     }
 
 }
