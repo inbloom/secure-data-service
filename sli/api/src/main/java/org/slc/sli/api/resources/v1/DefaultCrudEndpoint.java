530ccdb3b40c183ff3e2e66fb7c30f236cd15e18
Suneth Rupasinghe <srupasinghe@wgen.net>
2012-04-25 09:39:58 -0400

    checkstyle fix

--- sli/api/src/main/java/org/slc/sli/api/resources/v1/DefaultCrudEndpoint.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/v1/DefaultCrudEndpoint.java
@@ -7,7 +7,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
