f40857a4abcd58b94580f5afee98e0c9a57468dd
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-04-26 18:43:55 -0400

    Revert "[US2033] checkstyle fixes"
    This reverts commit 6e063baec1055113d4d7d21350595e37c4f91bf2.

--- sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/handler/MatchDiffHandler.java
+++ sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/handler/MatchDiffHandler.java
@@ -9,6 +9,7 @@ import org.slc.sli.ingestion.landingzone.LocalFileSystemLandingZone;
 import org.slc.sli.ingestion.validation.ErrorReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
