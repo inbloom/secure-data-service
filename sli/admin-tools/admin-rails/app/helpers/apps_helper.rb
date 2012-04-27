ddc039ad42dececa5773f73b4ebb1a0f37e8bcaa
Jonathan Nanney <jnanney@wgen.net>
2012-04-27 16:54:23 -0400

    Changed the way that I get the ed org hierarchy.

--- sli/admin-tools/admin-rails/app/helpers/apps_helper.rb
+++ sli/admin-tools/admin-rails/app/helpers/apps_helper.rb
@@ -5,38 +5,24 @@ module AppsHelper
     link_to title, {:sort => value, :direction => direction}, {:class => direction}
   end
 
-  #def get_district_hierarchy
-  #foo = EducationOrganization.find(:all, :params => {"shortNameOfInstitution" => "NYPDSS"})
-  #
-  #  ed_orgs = EducationOrganization.all
-  #  result = {}
-  #
-  #  ed_orgs.each do |ed_org|
-  #
-  #  end
-  #end
-
   def get_district_hierarchy
-    result = {}
-    ed_orgs = EducationOrganization.all
-    ed_org_assocs = EducationOrganizationAssociations.all
-    ed_org_assocs.each do |assoc|
-      parent_id = assoc.educationOrganizationParentId
-      child_id = assoc.educationOrganizationChildId
-      current_parent = {"id" => parent_id, "name" => get_ed_org_name_from_id(ed_orgs, parent_id)}
-      current_child = {"id" => child_id, "name" => get_ed_org_name_from_id(ed_orgs, child_id)}
+    state_ed_orgs = EducationOrganization.all
 
-      if result.keys.include?(current_parent)
-        result[current_parent].push(current_child)
-      else
-        result[current_parent] = [current_child]
+    result = {}
+
+    state_ed_orgs.each do |ed_org|
+      current_parent = {"id" => ed_org.id, "name" => ed_org.nameOfInstitution}
+      child_ed_orgs = EducationOrganization.find(:all, :params => {"parentEducationAgencyReference" => ed_org.id})
+      child_ed_orgs.each do |child_ed_org|
+        current_child = {"id" => child_ed_org.id, "name" => child_ed_org.nameOfInstitution}
+        if result.keys.include?(current_parent)
+          result[current_parent].push(current_child)
+        else
+          result[current_parent] = [current_child]
+        end
       end
     end
-    return result
-  end
-
-  def get_ed_org_name_from_id(ed_orgs, id)
-    ed_orgs.select{|ed_org| ed_org.id == id}[0].nameOfInstitution
+    result
   end
 
   # For client_id and client_secret, return 'Pending' if app isn't yet registered
