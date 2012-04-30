6f6aeb10f0deeb31f2de01d18c8b783da54ae8e8
Dong Liu <dliu@wgen.net>
2012-04-29 20:36:46 -0400

    fix unit tests

--- sli/admin-tools/admin-rails/app/controllers/account_managements_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/account_managements_controller.rb
@@ -46,30 +46,30 @@ class AccountManagementsController < ApplicationController
 
   # GET /account_managements/1
   # GET /account_managements/1.json
-  def show
-    @account_management = AccountManagement.find(params[:id])
+#  def show
+ #   @account_management = AccountManagement.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @account_management }
-    end
-  end
+ #   respond_to do |format|
+ #     format.html # show.html.erb
+ #     format.json { render json: @account_management }
+ #   end
+ # end
 
   # GET /account_managements/new
   # GET /account_managements/new.json
-  def new
-    @account_management = AccountManagement.new
+#  def new
+ #   @account_management = AccountManagement.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @account_management }
-    end
-  end
+  #  respond_to do |format|
+  #    format.html # new.html.erb
+  #    format.json { render json: @account_management }
+   # end
+  #end
 
   # GET /account_managements/1/edit
-  def edit
-    @account_management = AccountManagement.find(params[:id])
-  end
+ # def edit
+ #   @account_management = AccountManagement.find(params[:id])
+ # end
 
   # POST /account_managements
   # POST /account_managements.json
@@ -111,31 +111,31 @@ class AccountManagementsController < ApplicationController
 
   # PUT /account_managements/1
   # PUT /account_managements/1.json
-  def update
-    @account_management = AccountManagement.find(params[:id])
+#  def update
+#    @account_management = AccountManagement.find(params[:id])
 
-    respond_to do |format|
-      if @account_management.update_attributes(params[:account_management])
-        format.html { redirect_to @account_management, notice: 'Account management was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @account_management.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ #   respond_to do |format|
+ #     if @account_management.update_attributes(params[:account_management])
+ #       format.html { redirect_to @account_management, notice: 'Account management was successfully updated.' }
+ #       format.json { head :ok }
+ #     else
+ #       format.html { render action: "edit" }
+  #      format.json { render json: @account_management.errors, status: :unprocessable_entity }
+#      end
+ #   end
+ # end
 
   # DELETE /account_managements/1
   # DELETE /account_managements/1.json
-  def destroy
-    @account_management = AccountManagement.find(params[:id])
-    @account_management.destroy
+#  def destroy
+ #   @account_management = AccountManagement.find(params[:id])
+ #   @account_management.destroy
 
-    respond_to do |format|
-      format.html { redirect_to account_managements_url }
-      format.json { head :ok }
-    end
-  end
+  #  respond_to do |format|
+  #    format.html { redirect_to account_managements_url }
+  #    format.json { head :ok }
+  #  end
+  #end
   
   def sort(account_managements)
      pending_account_managements,approved_account_managements,rejected_account_managements,disabled_account_managements=Array.new(),Array.new(),Array.new(),Array.new()
