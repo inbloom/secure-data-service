0dc51c1e051958fcf5a34f6e92771dafbaf9baad
Stephan Altmueller <saltmueller@wgen.net>
2012-05-01 17:24:40 -0400

    added ldap storage

--- sli/admin-tools/approval/lib/ldapstorage.rb
+++ sli/admin-tools/approval/lib/ldapstorage.rb
@@ -4,7 +4,7 @@ require 'net/ldap'
 class LDAPStorage 
 	# set the objectClasses 
 	OBJECT_CLASS = ["inetOrgPerson", "top"]
-	TARGET_TREE  = "ou=people,ou=DevTest,dc=slidev,dc=org"
+	#TARGET_TREE  = "ou=people,ou=DevTest,dc=slidev,dc=org"
 
 	LDAP_ATTR_MAPPING = {
 		:givenname            => :first,
@@ -28,6 +28,7 @@ class LDAPStorage
            		:password => password
      		}
      	}
+     	@base = base
      	@ldap = Net::LDAP.new @ldap_conf
      	raise "Could not bind to ldap server." if !@ldap.bind 
 	end
@@ -43,8 +44,8 @@ class LDAPStorage
 	#     :status  ... "submitted"
 	# }
 	def create_user(user_info)
-		cn = "#{user_info[:first]} #{user_info[:last]}"
-		dn = "cn=#{cn},#{TARGET_TREE}"
+		cn = user_info[:email]
+		dn = get_DN(user_info[:email])
 		puts "cn: #{cn}\ndn: #{dn}\npassword: #{user_info[:password]}"
 		puts user_info[:vendor]
 		attr = {
@@ -58,14 +59,15 @@ class LDAPStorage
 
 	# returns extended user_info
 	def read_user(email_address)
-		filter = Net::LDAP::Filter.eq( "uid", email_address)
-		return map_fields(@ldap.search(:filter => filter))[0]
+		filter = Net::LDAP::Filter.eq( "cn", email_address)
+		return map_fields(@ldap.search(:filter => filter), 1)[0]
 	end
 
 	# updates the user status from an extended user_info 
 	def update_status(user)
 		if user_exists?(user[:email])
-			
+			dn = get_DN(user[:email])
+			@ldap.replace_attribute(dn, ENTITY_ATTR_MAPPING[:status], user[:status])
 		end
 	end
 
@@ -84,12 +86,15 @@ class LDAPStorage
 
 	# returns extended user_info for the given emailtoken (see create_user) or nil 
 	def read_user_emailtoken(emailtoken)
-
+		filter = Net::LDAP::Filter.eq(ENTITY_ATTR_MAPPING[:emailtoken].to_s, emailtoken)
+		return map_fields(@ldap.search(:filter => filter), 1)[0]		
 	end
 
 	# returns array of extended user_info for all users or all users with given status 
 	# use constants in approval.rb 
 	def read_users(status=nil)
+		filter = Net::LDAP::Filter.eq(ENTITY_ATTR_MAPPING[:status].to_s, status ? status : "*")
+		return map_fields(@ldap.search(:filter => filter))		
 	end
 
 	# updates the user_info except for the user status 
@@ -99,11 +104,22 @@ class LDAPStorage
 
 	# deletes the user entirely 
 	def delete_user(email_address)
+		@ldap.delete(:dn => get_DN(email_address))
 	end 
 
+	# returns the LDAP DN
+	def get_DN(email_address)
+		return "cn=#{email_address},#{@base}"
+	end
+
 	# extract the user from the ldap record
-	def map_fields(search_result, max_recs)
-		return search_result.to_a()[0..(max_recs-1)].map do |entry|
+	def map_fields(search_result, max_recs=nil)
+		arr = search_result.to_a()
+		if !max_recs
+			max_recs = arr.length
+		end
+
+		return arr[0..(max_recs-1)].map do |entry|
 			user_rec = {}
 			LDAP_ATTR_MAPPING.each { |ldap_k, rec_k| user_rec[rec_k] = entry[ldap_k]}
 			user_rec
--- sli/admin-tools/approval/lib/test_ldap.rb
+++ sli/admin-tools/approval/lib/test_ldap.rb
@@ -1,19 +1,28 @@
 require './ldapstorage'
 
-ldap = LDAPStorage.new("ldap.slidev.org", 389, "ou=DevTest,dc=slidev,dc=org", "cn=DevLDAP User, ou=People,dc=slidev,dc=org", "Y;Gtf@w{")
+ldap = LDAPStorage.new("ldap.slidev.org", 389, "ou=people,ou=DevTest,dc=slidev,dc=org", "cn=DevLDAP User, ou=People,dc=slidev,dc=org", "Y;Gtf@w{")
+jd_email = "jdoe@example.com"
+jd_emailtoken = "0102030405060708090A0B0C0D0E0F"
 user_info = {
 	:first      => "John",
 	:last       => "Doe", 
-	:email      => "jdoe@example.com",
+	:email      => jd_email,
 	:password   => "secret", 
 	:vendor     => "Acme Inc.",
-	:emailtoken => "0102030405060708090A0B0C0D0E0F",
+	:emailtoken => jd_emailtoken,
 	:status     => "submitted"	
 }
+
+td_email = "tdoe@example.com"
+td_user_info = user_info.clone 
+td_user_info[:first] = "Tom"
+td_user_info[:email] = td_email 
 
 result = ldap.create_user(user_info)
 puts "#{result}"
 found_user = ldap.read_user("jdoe@example.com")
+
+ldap.create_user(td_user_info)
 
 puts "DN: #{found_user}"
 found_user.each do |attribute, values|
@@ -22,3 +31,42 @@ found_user.each do |attribute, values|
 		puts "      --->#{value}"
 	end
 end
+
+any_email = "anything@example.com" 
+puts "User #{jd_email} exists: #{ldap.user_exists?(jd_email)}"
+puts "User #{any_email} exists: #{ldap.user_exists?(any_email)}"
+
+new_user = user_info.clone 
+new_user[:status] = "pending"
+ldap.update_status(new_user)
+found_user = ldap.read_user(jd_email)
+puts "Changed status: #{found_user[:status]}"
+
+found_user = ldap.read_user_emailtoken(jd_emailtoken)
+puts "#{found_user}"
+
+found_user = ldap.read_user_emailtoken("nothing")
+if !found_user
+	puts "Did not find arbitrary emailtoken."
+end
+
+
+puts "Fetching all users:"
+all_users = ldap.read_users
+all_users.each do |e|
+	puts e[:email]
+end 
+
+puts "-----------------------------\nFetching pending users:"
+pending_users = ldap.read_users("pending")
+pending_users.each do |e|
+	puts e[:email]
+end
+
+found_user = true 
+ldap.delete_user(jd_email)
+found_user = ldap.read_user_emailtoken("nothing")
+if !found_user
+	puts "Did not find delete user."
+end
+ldap.delete_user(td_email)
