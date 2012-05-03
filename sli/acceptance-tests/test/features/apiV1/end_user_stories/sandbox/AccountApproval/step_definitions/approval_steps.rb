85c1e2fd8bee83d90fd1c706990bc46e92055c8f
James Short <jshort@wgen.net>
2012-05-03 17:04:54 -0400

    Added rumbster updates to step defs

--- sli/acceptance-tests/test/features/apiV1/end_user_stories/sandbox/AccountApproval/step_definitions/approval_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/end_user_stories/sandbox/AccountApproval/step_definitions/approval_steps.rb
@@ -1,5 +1,41 @@
 require 'approval'
+require 'rumbster'
+require 'message_observers'
 require_relative '../../../../../utils/sli_utils.rb'
+
+Before do 
+  
+end
+
+After do |scenario|
+  puts "in after hook, yay"
+  @rumbster.stop if @rumbster
+end
+
+Given /^I have a "([^"]*)" SMTP\/Email server configured$/ do |live_or_mock|
+  sender_email_address = "admin@SLC.org"
+  email_name = "SLC Admin"
+  test_port = 2525
+  
+  if live_or_mock == "live"
+    @email_conf = {
+      :host => 'mon.slidev.org',
+      :sender_name => email_name,
+      :sender_email_addr => sender_email_address
+    }
+  else
+    @rumbster = Rumbster.new(test_port)
+    @message_observer = MailMessageObserver.new
+    @rumbster.add_observer @message_observer
+    @rumbster.start
+    @email_conf = {
+      :host => '127.0.0.1',
+      :port => test_port,
+      :sender_name => email_name,
+      :sender_email_addr => sender_email_address
+    }
+  end
+end
 
 Given /^a production account request for vendor "([^"]*)"$/ do |arg1|
   if defined? @userinfo
@@ -37,9 +73,12 @@ end
 When /^I approve the account request$/ do
   ApprovalEngine.change_user_status(@userinfo[:email], "approve")
   
-  puts @ldap.read_user(@userinfo[:email])
+  email = @message_observer.messages.first
+  assert(email != nil, "email was not received")
+  assert(email.to[0] == @userinfo[:email], "email address was incorrect")
+  assert(email.subject == "Your account has been approved.", "email did not have correct subject")
   
-  assert(@ldap.read_user(@userinfo[:email])[:status] == "approved", "User #{arg1} is not in pending state")
+  assert(@ldap.read_user(@userinfo[:email])[:status] == "approved", "User #{@userinfo[:email]} is not in pending state")
 end
 
 Then /^a new account is created in production LDAP with login name "([^"]*)" and the role is "([^"]*)"$/ do |arg1, arg2|
@@ -95,13 +134,9 @@ Then /^sandbox LDAP account with login name "([^"]*)" is set as inactive$/ do |a
 end
 
 #### Common methods ##############
-def intializaApprovalEngineAndLDAP(sandbox=false,sender_email_address = "devldapuser@slidev.org", email_name = @userinfo[:first] + " " + @userinfo[:last])
+def intializaApprovalEngineAndLDAP(email_conf = @email_conf, sandbox=false)
   @ldap = LDAPStorage.new(PropLoader.getProps['ldap.hostname'], 389, "ou=DevTest,dc=slidev,dc=org", "cn=DevLDAP User, ou=People,dc=slidev,dc=org", "Y;Gtf@w{")
-  email_conf = {
-    :host => 'mon.slidev.org',
-    :sender_name => email_name,
-    :sender_email_addr => sender_email_address
-  }
+  
   email = Emailer.new email_conf
   ApprovalEngine.init(@ldap, email, sandbox)
   
