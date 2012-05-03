e8827802a3115ea39d2d650c295c7d6cf874ff73
Ryan Farris <rfarris@wgen.net>
2012-05-03 16:10:10 -0400

    fix form not having auth token

--- sli/admin-tools/admin-rails/app/controllers/landing_zone_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/landing_zone_controller.rb
@@ -1,10 +1,12 @@
 class LandingZoneController < ApplicationController
   def provision
-  end
-  
-  def success
+    params.each do |key, value|
+      logger.fatal "XXXXX    #{key}  #{value}"
+    end
+    redirect_to '/'
   end
 
   def index
+    @edOrgs = ["High-Level Ed-Org from Sample Dataset 1", "High-Level Ed-Org from Sample Dataset 2"]
   end
 end
