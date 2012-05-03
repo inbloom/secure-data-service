761a495cc7729cc0a477f0d284b8d05747a6d5eb
Ryan Farris <rfarris@wgen.net>
2012-05-03 18:55:10 -0400

    fix form post url, create new edorg and post to api.

---
+++ sli/admin-tools/admin-rails/app/models/landing_zone.rb
@@ -0,0 +1,21 @@
+class LandingZone
+  
+  def self.possible_edorgs
+    ["High-Level Ed-Org from Sample Dataset 1", "High-Level Ed-Org from Sample Dataset 2"]
+  end
+  
+  def self.provision(edorg_id)
+    edOrg = EducationOrganization.new(:stateUniqueId => edorg_id, 
+                              :nameOfInstitution => 'TEMPORARY_PLACEHOLDER', 
+                              :organizationCategories => 'State Education Agency', 
+                              :address => { 
+                                  :streetNumberName => 'TEMPORARY_PLACEHOLDER', 
+                                  :city => 'TEMPORARY_PLACEHOLDER', 
+                                  :stateAbbreviation => 'TX', 
+                                  :postalCode => 'TEMPORARY_PLACEHOLDER'})
+    saved = edOrg.save()
+    Rails.logger.info "Provisioning Request: #{edorg_id}, successful? #{saved}"
+    return saved
+  end
+  
+end
