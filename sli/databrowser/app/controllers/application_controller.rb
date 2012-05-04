a2d824bbe6e0e6802a13abaca65d39024c40fb74
Ryan Latta <rlatta@wgen.net>
2012-05-04 13:45:28 -0400

    Removing some sensitive logging.

--- sli/databrowser/app/controllers/application_controller.rb
+++ sli/databrowser/app/controllers/application_controller.rb
@@ -78,15 +78,12 @@ class ApplicationController < ActionController::Base
     end
     if oauth.enabled?
       if oauth.token != nil
-        logger.info { "OAuth access token is #{oauth.token}"}
         SessionResource.access_token = oauth.token
         Check.url_type = "check"
         session[:full_name] ||= Check.get("")["full_name"]    
       elsif params[:code] && !oauth.has_code
-        logger.info { "Requesting access token for  #{params[:code]}"}
         SessionResource.access_token = oauth.get_token(params[:code])
       else
-        logger.info { "Redirecting to oauth auth URL:  #{oauth.authorize_url}"}
         redirect_to oauth.authorize_url + "&state=" + CGI::escape(form_authenticity_token)
       end
     else
