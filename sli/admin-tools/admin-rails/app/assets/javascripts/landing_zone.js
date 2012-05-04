2a5c92c72a15badad1144ec2b82d83f10f31862a
Nick Brown <nbrown@wgen.net>
2012-05-04 16:19:54 -0400

    provision can only be clicked when something is chosen

--- sli/admin-tools/admin-rails/app/assets/javascripts/landing_zone.js
+++ sli/admin-tools/admin-rails/app/assets/javascripts/landing_zone.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  $("input[type=radio][id!=custom]").click(function() {
-   $("#custom_ed_org").attr("disabled","disabled")});
-  $("#custom").click(function() {
-   $("#custom_ed_org").removeAttr("disabled")});
+  var buttonEnableForCustom = function() {
+    if($("#custom_ed_org").val().length == 0) {
+      $("#provisionButton").attr("disabled","disabled")
+    }
+    else {
+      $("#provisionButton").removeAttr("disabled")
+    }
   }
-)
+  $("input[type=radio][id!=custom]").click(function() {
+   $("#custom_ed_org").attr("disabled","disabled");
+   $("#provisionButton").removeAttr("disabled")});
+  $("#custom").click(function() {
+   $("#custom_ed_org").removeAttr("disabled");
+   buttonEnableForCustom()});
+  $("#custom_ed_org").keyup(buttonEnableForCustom); })
