9403a2f443f9ebb9a2c207f73ed3a541b85538a0
Ryan Farris <rfarris@wgen.net>
2012-05-04 16:34:38 -0400

    added todo

--- sli/admin-tools/admin-rails/app/controllers/landing_zone_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/landing_zone_controller.rb
@@ -4,6 +4,7 @@ class LandingZoneController < ApplicationController
   rescue_from ProvisioningError, :with => :handle_error
   
   def provision
+    # TODO check the user has the correct role to be doing this.
     if (params[:cancel] == "Cancel")
       redirect_to "/"
       return
@@ -19,7 +20,7 @@ class LandingZoneController < ApplicationController
   end
 
   def index
-    session = Check.get("")
+    # TODO check the user has the correct role to be doing this.
     @edOrgs = LandingZone.possible_edorgs
   end
   
