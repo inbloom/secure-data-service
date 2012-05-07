eee3d346cc8e289f49ce8bc86c8a4649a81d612a
Shawn Marie Richards <srichards@wgen.net>
2012-05-07 13:43:35 -0400

    [US2170] basic flesh on TenantResource stubs

--- sli/api/src/main/java/org/slc/sli/api/resources/tenant/TenantResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/tenant/TenantResource.java
@@ -1,5 +1,7 @@
 package org.slc.sli.api.resources.tenant;
 
+import java.util.Collection;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -18,6 +20,8 @@ import javax.ws.rs.core.UriInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import org.slc.sli.api.config.EntityDefinitionStore;
@@ -26,16 +30,17 @@ import org.slc.sli.api.resources.Resource;
 import org.slc.sli.api.resources.v1.DefaultCrudEndpoint;
 import org.slc.sli.api.service.EntityService;
 import org.slc.sli.common.constants.v1.ParameterConstants;
+import org.slc.sli.domain.enums.Right;
 
 /**
  *
- * Provides CRUD operations on registered application through the /apps path.
+ * Provides CRUD operations on registered application through the /tenants path.
  *
  * @author
  */
 @Component
 @Scope("request")
-@Path("tenant")
+@Path("tenants")
 @Produces({ Resource.JSON_MEDIA_TYPE })
 public class TenantResource extends DefaultCrudEndpoint {
 
@@ -48,14 +53,15 @@ public class TenantResource extends DefaultCrudEndpoint {
 
     private EntityService service;
 
-    private static final int TENANT_ID_LENGTH = 10; //TODO: Find true max length
-    private static final int EDORG_ID_LENGTH = 48; //TODO: Find true max length
-    private static final int USERNAME_LENGTH = 24; //TODO: Find true max length
+    //TODO: validate LandingZone data lengths, etc?
     public static final String RESOURCE_NAME = "tenant";
     public static final String TENANT_ID = "tenantId";
-    public static final String EDORG_ID = "educationOrganizationId";
-    public static final String USERNAME = "userName";
-    public static final String UUID = "uuid";
+    public static final String LZ_EDUCATION_ORGANIZATION = "educationOrganization";
+    public static final String LZ_INGESTION_SERVER = "ingestionServer";
+    public static final String LZ_PATH = "path";
+    public static final String LZ_USER_NAME = "userName";
+    public static final String LZ_DESC = "desc";
+    //TODO: validate LandingZone data lengths, etc?
 
     public void setMountDir(boolean mountDir) {
         this.mountDir = mountDir;
@@ -68,216 +74,90 @@ public class TenantResource extends DefaultCrudEndpoint {
         service = store.lookupByResourceName(RESOURCE_NAME).getService();
     }
 
-    @POST
-    public Response createTenant(EntityBody newApp, @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
-
-        if (newApp.containsKey(TENANT_ID)
-                && newApp.containsKey(EDORG_ID)
-                && newApp.containsKey(USERNAME)) {
-            EntityBody body = new EntityBody();
-            body.put("message", "Auto-generated attribute (id|client_secret|client_id) specified in POST.  "
-                    + "Remove attribute and try again.");
-            return Response.status(Status.BAD_REQUEST).entity(body).build();
-        }
-        /*
-        if (!hasRight(Right.APP_CREATION)) {
-            EntityBody body = new EntityBody();
-            body.put("message", "You are not authorized to create new applications.");
-            return Response.status(Status.BAD_REQUEST).entity(body).build();
-        }
-        String clientId = TokenGenerator.generateToken(CLIENT_ID_LENGTH);
-        while (isDuplicateToken(clientId)) {
-            clientId = TokenGenerator.generateToken(CLIENT_ID_LENGTH);
-        }
-
-        newApp.put(CLIENT_ID, clientId);
-        SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        newApp.put(CREATED_BY, principal.getExternalId());
-
-        Map<String, Object> registration = new HashMap<String, Object>();
-        registration.put(STATUS, "PENDING");
-        if (autoRegister) {
-            registration.put(APPROVAL_DATE, System.currentTimeMillis());
-            registration.put(STATUS, "APPROVED");
-            registration.put(APPROVAL_DATE, System.currentTimeMillis());
-        }
-        registration.put(REQUEST_DATE, System.currentTimeMillis());
-        newApp.put(REGISTRATION, registration);
-
-        String clientSecret = TokenGenerator.generateToken(CLIENT_SECRET_LENGTH);
-        newApp.put(CLIENT_SECRET, clientSecret);
-        return super.create(newApp, headers, uriInfo);
-        */
-    	return null;
+    private boolean hasRight(Right required) {
+        Collection<GrantedAuthority> rights = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return rights.contains(required);
     }
 
-    @SuppressWarnings("rawtypes")
+    @POST
+    public Response createTenant(EntityBody newTenant, @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
+
+        if (!hasRight(Right.ADMIN_ACCESS)) {
+            EntityBody body = new EntityBody();
+            body.put("message", "You are not authorized to create new tenants.");
+            return Response.status(Status.BAD_REQUEST).entity(body).build();
+        }
+
+        //TODO: look up the tenantId; if it exists, add new LZs here to the existing list; otherwise, create
+
+        return super.create(newTenant, headers, uriInfo);
+    }
+
     @GET
-    public Response getApplications(@QueryParam(ParameterConstants.OFFSET) @DefaultValue(ParameterConstants.DEFAULT_OFFSET) final int offset,
+    public Response getTenants(@QueryParam(ParameterConstants.OFFSET) @DefaultValue(ParameterConstants.DEFAULT_OFFSET) final int offset,
             @QueryParam(ParameterConstants.LIMIT) @DefaultValue(ParameterConstants.DEFAULT_LIMIT) final int limit,
             @Context
             HttpHeaders headers, @Context final UriInfo uriInfo) {
-    	/*
-        SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (hasRight(Right.APP_CREATION)) {
-            extraCriteria = new NeutralCriteria(CREATED_BY, NeutralCriteria.OPERATOR_EQUAL, principal.getExternalId());
-        } else {
-            debug("ED-ORG of operator/admin {}", principal.getEdOrg());
-            extraCriteria = new NeutralCriteria(AUTHORIZED_ED_ORGS, NeutralCriteria.OPERATOR_EQUAL,
-                    principal.getEdOrg());
+
+        if (!hasRight(Right.ADMIN_ACCESS)) {
+            EntityBody body = new EntityBody();
+            body.put("message", "You are not authorized to view tenants.");
+            return Response.status(Status.BAD_REQUEST).entity(body).build();
         }
-        Response resp = super.readAll(offset, limit, headers, uriInfo);
-        filterSensitiveData((Map) resp.getEntity());
-        return resp;
-        */
-    	return null;
+
+        return super.readAll(offset, limit, headers, uriInfo);
     }
 
     /**
      * Looks up a specific application based on client ID, ie.
-     * /api/rest/apps/<uuid>
+     * /api/rest/tenants/<tenantId>
      *
-     * @param uuid
+     * @param tenantId
      *            the client ID, not the "id"
      * @return the JSON data of the application, otherwise 404 if not found
      */
-    @SuppressWarnings("rawtypes")
     @GET
-    @Path("{" + UUID + "}")
-    public Response getApplication(@PathParam(UUID) String uuid,
+    @Path("{" + TENANT_ID + "}")
+    public Response getTenant(@PathParam(TENANT_ID) String tenantId,
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
-        Response resp = null;
-        /*
-        SLIPrincipal principal = (SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (hasRight(Right.APP_CREATION)) {
-            extraCriteria = new NeutralCriteria(CREATED_BY, NeutralCriteria.OPERATOR_EQUAL, principal.getExternalId());
-        } else {
-            extraCriteria = new NeutralCriteria(AUTHORIZED_ED_ORGS, NeutralCriteria.OPERATOR_EQUAL,
-                    principal.getEdOrg());
+
+        if (!hasRight(Right.ADMIN_ACCESS)) {
+            EntityBody body = new EntityBody();
+            body.put("message", "You are not authorized to view tenants.");
+            return Response.status(Status.BAD_REQUEST).entity(body).build();
         }
-        resp = super.read(uuid, headers, uriInfo);
-        filterSensitiveData((Map) resp.getEntity());
-        */
-        return resp;
+
+        return super.read(tenantId, headers, uriInfo);
     }
 
 
     @DELETE
-    @Path("{" + UUID + "}")
-    public Response deleteApplication(@PathParam(UUID) String uuid,
+    @Path("{" + TENANT_ID + "}")
+    public Response deleteTenant(@PathParam(TENANT_ID) String tenantId,
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
-    	/*
 
-        if (hasRight(Right.APP_CREATION)) {
-            return super.delete(uuid, headers, uriInfo);
-        } else {
+        if (!hasRight(Right.ADMIN_ACCESS)) {
             EntityBody body = new EntityBody();
-            body.put("message", "You cannot delete this application");
+            body.put("message", "You cannot delete this tenant.");
             return Response.status(Status.BAD_REQUEST).entity(body).build();
         }
-        */
-    	return null;
+
+        return super.delete(tenantId, headers, uriInfo);
     }
 
     @SuppressWarnings("unchecked")
     @PUT
-    @Path("{" + UUID + "}")
-    public Response updateApplication(@PathParam(UUID) String uuid, EntityBody app,
+    @Path("{" + TENANT_ID + "}")
+    public Response updateTenant(@PathParam(TENANT_ID) String tenantId, EntityBody tenant,
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
-    	/*
 
-        EntityBody oldApp = service.get(uuid);
-
-        //The client id and secret could be null if they were filtered from the client
-        String clientSecret = (String) app.get(CLIENT_SECRET);
-        if (clientSecret == null)
-            app.put(CLIENT_SECRET, oldApp.get(CLIENT_SECRET));
-        String clientId = (String) app.get(CLIENT_ID);
-        if (clientId == null)
-            app.put(CLIENT_ID, oldApp.get(CLIENT_ID));
-
-        String id = (String) app.get("id");
-        Map<String, Object> oldReg = ((Map<String, Object>) oldApp.get(REGISTRATION));
-        Map<String, Object> newReg = ((Map<String, Object>) app.get(REGISTRATION));
-        String newRegStatus = (String) newReg.get(STATUS);
-        String oldRegStatus = (String) oldReg.get(STATUS);
-        List<String> changedKeys = new ArrayList<String>();
-
-        for (Map.Entry<String, Object> entry : app.entrySet()) {
-            if (oldApp.containsKey(entry.getKey()) && !oldApp.get(entry.getKey()).equals(entry.getValue())) {
-                changedKeys.add(entry.getKey());
-            }
-        }
-
-        if ((clientSecret != null && !clientSecret.equals(oldApp.get(CLIENT_SECRET)))
-                || (clientId != null && !clientId.equals(oldApp.get(CLIENT_ID)))
-                || (id != null && !id.equals(oldApp.get("id")))
-                || (!registrationDatesMatch(oldReg, newReg, APPROVAL_DATE))
-                || (!registrationDatesMatch(oldReg, newReg, REQUEST_DATE))) {
+        if (!hasRight(Right.ADMIN_ACCESS)) {
             EntityBody body = new EntityBody();
-            body.put("message", "Cannot modify attribute (id|client_secret|client_id|request_date|approval_date) specified in PUT.  "
-                    + "Remove attribute and try again.");
+            body.put("message", "You cannot delete this tenant.");
             return Response.status(Status.BAD_REQUEST).entity(body).build();
         }
 
-        changedKeys.remove("registration");
-        changedKeys.remove("developer_info");   //TODO: developer_info is a pain since it's nested--need to validate this hasn't changed
-        changedKeys.remove("metaData");
-
-        //Operator - can only change registration status
-        if (hasRight(Right.APP_REGISTER)) {
-            if (changedKeys.size() > 0) {
-                EntityBody body = new EntityBody();
-                body.put("message", "You are not authorized to alter applications.");
-                return Response.status(Status.BAD_REQUEST).entity(body).build();
-            }
-
-            if (newRegStatus.equals("APPROVED") && oldRegStatus.equals("PENDING")) {
-                debug("App approved");
-                newReg.put(STATUS, "APPROVED");
-                newReg.put(APPROVAL_DATE, System.currentTimeMillis());
-            } else if (newRegStatus.equals("DENIED") && oldRegStatus.equals("PENDING")) {
-                debug("App denied");
-            } else if (newRegStatus.equals("UNREGISTERED") && oldRegStatus.equals("APPROVED")) {
-                debug("App unregistered");
-                newReg.remove(APPROVAL_DATE);
-                newReg.remove(REQUEST_DATE);
-            } else {
-                EntityBody body = new EntityBody();
-                body.put("message", "Invalid state change: " + oldRegStatus + " to " + newRegStatus);
-                return Response.status(Status.BAD_REQUEST).entity(body).build();
-            }
-
-        } else if (hasRight(Right.APP_CREATION)) {  //App Developer
-            if (!oldRegStatus.endsWith(newRegStatus)) {
-                EntityBody body = new EntityBody();
-                body.put("message", "You are not authorized to register applications.");
-                return Response.status(Status.BAD_REQUEST).entity(body).build();
-            }
-
-            if (oldRegStatus.equals("PENDING")) {
-                EntityBody body = new EntityBody();
-                body.put("message", "Application cannot be modified while approval request is in Pending state.");
-                return Response.status(Status.BAD_REQUEST).entity(body).build();
-            }
-
-            //when a denied or unreg'ed app is altered, it goes back into pending
-            if (oldRegStatus.equals("DENIED") || oldRegStatus.equals("UNREGISTERED")) {
-
-                //TODO: If auto approval is on, approve instead
-                newReg.put(STATUS, "PENDING");
-                newReg.put(REQUEST_DATE, System.currentTimeMillis());
-            }
-        } else {
-            EntityBody body = new EntityBody();
-            body.put("message", "You are not authorized to update application.");
-            return Response.status(Status.BAD_REQUEST).entity(body).build();
-        }
-
-        return super.update(uuid, app, headers, uriInfo);
-        */
-    	return null;
+        return super.update(tenantId, tenant, headers, uriInfo);
     }
-
 
 }
