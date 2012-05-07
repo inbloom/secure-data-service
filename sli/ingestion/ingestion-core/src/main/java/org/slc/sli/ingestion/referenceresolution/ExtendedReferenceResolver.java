7530f6c36f365619f4b3b03ebebafc5d090cc131
Mahesh Patel <mpatel@wgen.net>
2012-05-07 12:24:28 -0400

    Removed tabs.

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/ExtendedReferenceResolver.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/ExtendedReferenceResolver.java
@@ -3,9 +3,9 @@ package org.slc.sli.ingestion.referenceresolution;
 import java.io.File;
 
 public class ExtendedReferenceResolver implements ReferenceResolutionStrategy {
-	@Override
-	public void resolveReference(String referenceName, String referenceId, String enclosingEntityName, File xmlFile, String interchangeName) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void resolveReference(String referenceName, String referenceId, String enclosingEntityName, File xmlFile, String interchangeName) {
+        // TODO Auto-generated method stub
+    }
 
 }
--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/ReferenceResolutionStrategy.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/ReferenceResolutionStrategy.java
@@ -3,5 +3,5 @@ package org.slc.sli.ingestion.referenceresolution;
 import java.io.File;
 
 public interface ReferenceResolutionStrategy {
-	void resolveReference(String referenceName, String referenceId, String enclosingEntityName, File xmlFile, String interchangeName);
+    void resolveReference(String referenceName, String referenceId, String enclosingEntityName, File xmlFile, String interchangeName);
 }
