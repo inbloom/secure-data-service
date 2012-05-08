aa7ce62460b3ba3ae76a5594dd7b5cc8a40da00c
Simon Yau <syau@wgen.net>
2012-05-08 13:22:17 -0400

    WIP: API for tenant resource.

--- sli/api/src/main/java/org/slc/sli/api/resources/tenant/TenantResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/tenant/TenantResource.java
@@ -1,5 +1,11 @@
 package org.slc.sli.api.resources.tenant;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -22,9 +28,13 @@ import org.springframework.stereotype.Component;
 import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.representation.EntityBody;
 import org.slc.sli.api.resources.Resource;
+import org.slc.sli.api.resources.util.ResourceUtil;
 import org.slc.sli.api.resources.v1.DefaultCrudEndpoint;
+import org.slc.sli.api.service.EntityService;
 import org.slc.sli.api.util.SecurityUtil;
 import org.slc.sli.common.constants.v1.ParameterConstants;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.domain.enums.Right;
 
 /**
@@ -44,6 +54,7 @@ public class TenantResource extends DefaultCrudEndpoint {
 
     public static final String RESOURCE_NAME = "tenant";
     public static final String TENANT_ID = "tenantId";
+    public static final String LZ = "landingZone";
     public static final String LZ_EDUCATION_ORGANIZATION = "educationOrganization";
     public static final String LZ_INGESTION_SERVER = "ingestionServer";
     public static final String LZ_PATH = "path";
@@ -55,10 +66,10 @@ public class TenantResource extends DefaultCrudEndpoint {
     public TenantResource(EntityDefinitionStore entityDefs) {
         super(entityDefs, RESOURCE_NAME);
         store = entityDefs;
-        store.lookupByResourceName(RESOURCE_NAME).getService();
     }
 
     @POST
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public Response createTenant(EntityBody newTenant, @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
 
         if (!SecurityUtil.hasRight(Right.ADMIN_ACCESS)) {
@@ -67,9 +78,66 @@ public class TenantResource extends DefaultCrudEndpoint {
             return Response.status(Status.FORBIDDEN).entity(body).build();
         }
 
-        //TODO: look up the tenantId; if it exists, add new LZs here to the existing list; otherwise, create
+        EntityService tenantService = store.lookupByResourceName(RESOURCE_NAME).getService();
 
-        return super.create(newTenant, headers, uriInfo);
+        // look up the tenantId; if it exists, add new LZs here to the existing list; otherwise, create
+
+        // Create the query
+        if (!newTenant.containsKey(LZ) || !newTenant.containsKey(TENANT_ID)) {
+            EntityBody body = new EntityBody();
+            body.put("message", "Required attributes (" + LZ + "," + TENANT_ID + ") not specified in POST.  "
+                    + "add attribute and try again.");
+            return Response.status(Status.BAD_REQUEST).entity(body).build();
+        }
+        String tenantId = (String) newTenant.get(TENANT_ID);
+        NeutralQuery query = new NeutralQuery();
+        query.addCriteria(new NeutralCriteria(TENANT_ID, "=", tenantId));
+
+        // look up ids of existing tenant entries
+        List<String> existingIds = new ArrayList<String>();
+        for (String id : tenantService.listIds(query)) {
+            existingIds.add(id);
+        }
+        // If no tenant already exist, create
+        if (existingIds.size() == 0) {
+            String id = tenantService.create(newTenant);
+            String uri = ResourceUtil.getURI(uriInfo, "tenants", id).toString();
+            return Response.status(Status.CREATED).header("Location", uri).build();
+        }
+        // If more than exists, something is wrong
+        if (existingIds.size() > 1) {
+            throw new RuntimeException("Internal error: multiple tenant entry with identical IDs");
+        }
+
+        String existingTenantId = existingIds.get(0);
+        // combine lzs from existing tenant and new tenant entry, overwriting with values of new tenant entry if there is conflict.
+        TreeSet allLandingZones = new TreeSet(new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Map<String, Object> lz1 = (Map<String, Object>) o1;
+                Map<String, Object> lz2 = (Map<String, Object>) o2;
+                if (!lz1.containsKey(LZ_EDUCATION_ORGANIZATION) || !(lz1.get(LZ_EDUCATION_ORGANIZATION) instanceof String)) {
+                    throw new RuntimeException("Badly formed tenant entry: " + lz1.toString());
+                }
+                if (!lz2.containsKey(LZ_EDUCATION_ORGANIZATION) || !(lz2.get(LZ_EDUCATION_ORGANIZATION) instanceof String)) {
+                    throw new RuntimeException("Badly formed tenant entry: " + lz2.toString());
+                }
+                return ((String) lz1.get(LZ_EDUCATION_ORGANIZATION)).compareTo((String) lz2.get(LZ_EDUCATION_ORGANIZATION));
+            }
+        });
+
+        EntityBody existingBody = tenantService.get(existingTenantId);
+        List existingLandingZones = (List) existingBody.get(LZ);
+        allLandingZones.addAll(existingLandingZones);
+        List newLandingZones = (List) newTenant.get(LZ);
+        allLandingZones.addAll(newLandingZones);
+
+        existingBody.put(LZ, new ArrayList(allLandingZones));
+        tenantService.update(existingTenantId, existingBody);
+
+        // Construct the response
+        String uri = ResourceUtil.getURI(uriInfo, "tenants", existingTenantId).toString();
+        return Response.status(Status.CREATED).header("Location", uri).build();
     }
 
     @GET
