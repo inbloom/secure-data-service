7ab8a3e139e7115c1dc9c3241c945b8ce2fa7942
Tao Ke <tke@wgen.net>
2012-05-08 12:35:33 -0400

    Initial implementation of IdRef resolution configuration

---
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/ReferenceFactory.java
@@ -0,0 +1,42 @@
+package org.slc.sli.ingestion.referenceresolution;
+
+import java.util.Map;
+
+import org.milyn.Smooks;
+import org.milyn.payload.StringResult;
+import org.milyn.payload.StringSource;
+
+/**
+ *
+ * @author tke
+ *
+ */
+public class ReferenceFactory {
+    private Map<String, Smooks> idRefConfigs;
+    public Map<String, Smooks> getIdRefConfigs() {
+        return idRefConfigs;
+    }
+
+    public void setIdRefConfigs(Map<String, Smooks> idRefConfigs) {
+        this.idRefConfigs = idRefConfigs;
+    }
+
+    /**
+     * resolve the reference
+     *
+     * @param interchange : Name of interchnage
+     * @param element  : name of element
+     * @param reference : name of the reference
+     * @param content : the content of the referenced element in XML format
+     * @return : the resolved content in XML format
+     */
+    public String resolve(String interchange, String element, String reference, String content) {
+        String key = interchange + element + reference;
+        Smooks smooks = idRefConfigs.get(key);
+        StringResult result = new StringResult();
+        StringSource source = new StringSource(content);
+        smooks.filterSource(source, result);
+
+        return result.toString();
+    }
+}
---
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/referenceresolution/ReferenceFactoryTest.java
@@ -0,0 +1,65 @@
+package org.slc.sli.ingestion.referenceresolution;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author tke
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+public class ReferenceFactoryTest {
+
+    @Autowired
+    ReferenceFactory referenceFactory;
+    @Test
+    public void test() {
+        String testXML = "<StudentSectionAssociation>"
+                            + "<StudentReference>"
+                                + "<StudentIdentity>"
+                                    + "<StudentUniqueStateId>"
+                                        + "testing"
+                                  + "</StudentUniqueStateId>"
+                                  + "</StudentIdentity>"
+                                + "</StudentReference>"
+                                + "<SectionReference>"
+                                    + "<SectionIdentity>"
+                                        + "<UniqueSectionCode>"
+                                            + "testing"
+                                        + "</UniqueSectionCode>"
+                                     + "</SectionIdentity>"
+                                + "</SectionReference>"
+                                + "</StudentSectionAssociation>";
+
+        String resultXML = "<StudentSectionAssociationReference>"
+                            + "<StudentSectionAssociationIdentity>"
+                                + "<StudentIdentity>"
+                                    + "<StudentUniqueStateId>"
+                                        + "testing"
+                                    + "</StudentUniqueStateId>"
+                                + "</StudentIdentity>"
+                                + "<SectionIdentity>"
+                                    + "<UniqueSectionCode>"
+                                        + "testing"
+                                    + "</UniqueSectionCode>"
+                                + "</SectionIdentity>"
+                             + "</StudentSectionAssociationIdentity>"
+                          + "</StudentSectionAssociationReference>";
+
+
+        String result = referenceFactory.resolve("InterchangeStudentGrade", "StudentGradebookEntry", "StudentSectionAssociationReference", testXML);
+
+        result = result.replaceAll("\\n", "");
+        result = result.replaceAll("\\s+", "");
+
+        Assert.assertEquals(resultXML, result);
+    }
+
+}
