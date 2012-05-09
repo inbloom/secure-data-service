614557d2a07da6e576d6b810046b8fe16dd0f88f
Tao Ke <tke@wgen.net>
2012-05-09 09:38:41 -0400

    Checked if the reference is supported to avoid null pointer exception

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/SmooksExtendedReferenceResolver.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/referenceresolution/SmooksExtendedReferenceResolver.java
@@ -30,8 +30,12 @@ public class SmooksExtendedReferenceResolver extends ExtendedReferenceResolver {
      * @param content : the content of the referenced element in XML format
      * @return : the resolved content in XML format
      */
+    @Override
     public String resolve(String interchange, String element, String reference, String content) {
         String key = interchange + element + reference;
+        if (!idRefConfigs.containsKey(key)) {
+            return null;
+        }
         Smooks smooks = idRefConfigs.get(key);
         StringResult result = new StringResult();
         StringSource source = new StringSource(content);
