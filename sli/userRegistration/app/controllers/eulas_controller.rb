342d6ee397381d09205abc8c5b626e6bdbf719fb
jcolewgen <jcole@wgen.net>
2012-05-09 15:25:33 -0400

    updates to page navigations

--- sli/userRegistration/app/controllers/eulas_controller.rb
+++ sli/userRegistration/app/controllers/eulas_controller.rb
@@ -1,6 +1,5 @@
 class EulasController < ApplicationController
-  before_filter :cancel_redirect, :only => [:new, :create]
-  before_filter :ok, :only => [:new, :create]
+  before_filter :redirect, :only => [:new, :create]
 
   # GET /eulas
   # GET /eulas.json
@@ -16,7 +15,7 @@ class EulasController < ApplicationController
   # GET /eulas/1
   # GET /eulas/1.json
   def show
-    @user_account_registration = UserAccountRegistration.new(params[:id])
+    @eula = Eula.new
 
           respond_to do |format|
             format.html # new.html.erb
@@ -35,12 +34,6 @@ class EulasController < ApplicationController
     end
   end
 
-
-  # GET /eulas/1/edit
-  def edit
-    @eula = Eula.find(params[:id])
-  end
-
   # POST /eulas
   # POST /eulas.json
   def create
@@ -52,43 +45,15 @@ class EulasController < ApplicationController
     end
   end
 
-  # PUT /eulas/1
-  # PUT /eulas/1.json
-  def update
-    @eula = Eula.find(params[:id])
-
-    respond_to do |format|
-      if @eula.update_attributes(params[:eula])
-        format.html { redirect_to @eula, notice: 'Eula was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @eula.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /eulas/1
-  # DELETE /eulas/1.json
-  def destroy
-    @eula = Eula.find(params[:id])
-    @eula.destroy
-
-    respond_to do |format|
-      format.html { redirect_to eulas_url }
-      format.json { head :no_content }
-    end
-  end
-
 private
 
-  def cancel_redirect
+  def redirect
     if params[:commit] == "Reject"
       redirect_to APP_CONFIG['redirect_slc_url']
     end
-  end
-
-  def ok
+    if params[:commit] == "Accept"
+      redirect_to "/eulas/show"
+    end
     if params[:commit] == "Ok"
       redirect_to APP_CONFIG['redirect_slc_url']
     end
