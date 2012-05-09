3a2c6a8de63ab018414b53307664695286de0833
Jeffrey Stokes <jstokes@wgen.net>
2012-05-09 16:56:50 -0400

    eula should be a static page, not a full blown resource

--- sli/userRegistration/app/models/eula.rb
+++ sli/userRegistration/app/models/eula.rb
@@ -1,20 +1,2 @@
 class Eula
-    include ActiveModel::Validations
-    include ActiveModel::Conversion
-    extend ActiveModel::Naming
-
-   attr_accessor :termsOfService, :eula
-
-   validates_presence_of :termsOfService,:eula
-
-   def initialize(attributes = {})
-       attributes.each do |name, value|
-         send("#{name}=", value)
-       end
-     end
-
-     def persisted?
-       false
-     end
 end
-
