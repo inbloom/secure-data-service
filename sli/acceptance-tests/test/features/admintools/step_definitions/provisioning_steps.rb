93a97cb1fe2624a64326f7ea88d1aabd94f48977
Nick Brown <nbrown@wgen.net>
2012-05-09 14:20:24 -0400

    add test for duplicate provision

--- sli/acceptance-tests/test/features/admintools/step_definitions/provisioning_steps.rb
+++ sli/acceptance-tests/test/features/admintools/step_definitions/provisioning_steps.rb
@@ -13,18 +13,6 @@ Given /^there is a production account in ldap for vendor "([^"]*)"$/ do |vendor|
   @sandboxMode=false
 end
 
-When /^I provision$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^a high\-level ed\-org is created in Mongo$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^Landing\-Zone provisioining API is invoked with the high\-level ed\-org information$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
 When /^I go to the provisioning application$/ do
   url = PropLoader.getProps['admintools_server_url']+"/landing_zone"
   @driver.get(url)
@@ -60,4 +48,8 @@ Then /^I can select between the the high level ed\-org of the sample data sets o
   assertWithWait("Sample data choice does not exist") {@driver.find_element(:id, SAMPLE_DATA_SET1_CHOICE) != nil}
   assertWithWait("Sample data choice does not exist") {@driver.find_element(:id, SAMPLE_DATA_SET2_CHOICE) != nil}
   assertWithWait("Custom data choice does not exist") {@driver.find_element(:id, CUSTOM_DATA_SET_CHOICE) != nil}
+end
+
+Then /^I get a conflict error message$/ do
+  assertWithWait("No conflict error message") {@driver.find_element(:id, "conflictMessage") != nil}
 end
