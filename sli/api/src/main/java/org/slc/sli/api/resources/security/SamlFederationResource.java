47359c86f716abf3f98c099ffdf97e97850f2201
Yuan Zhang <yzhang@wgen.net>
2012-05-10 12:12:43 -0400

    security log to capture SAML validation failures.

--- sli/api/src/main/java/org/slc/sli/api/resources/security/SamlFederationResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/security/SamlFederationResource.java
@@ -7,25 +7,18 @@ import java.net.URI;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jdom.Document;
-import org.slc.sli.api.security.OauthSessionManager;
-import org.slc.sli.api.security.SLIPrincipal;
-import org.slc.sli.api.security.resolve.UserLocator;
-import org.slc.sli.api.security.saml.SamlAttributeTransformer;
-import org.slc.sli.api.security.saml.SamlHelper;
-import org.slc.sli.domain.Entity;
-import org.slc.sli.domain.NeutralCriteria;
-import org.slc.sli.domain.NeutralQuery;
-import org.slc.sli.domain.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,40 +27,61 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 
+import org.slc.sli.api.security.OauthSessionManager;
+import org.slc.sli.api.security.SLIPrincipal;
+import org.slc.sli.api.security.resolve.UserLocator;
+import org.slc.sli.api.security.saml.SamlAttributeTransformer;
+import org.slc.sli.api.security.saml.SamlHelper;
+import org.slc.sli.common.util.logging.LogLevelType;
+import org.slc.sli.common.util.logging.SecurityEvent;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
+import org.slc.sli.domain.Repository;
+
 /**
  * Process SAML assertions
- * 
+ *
  * @author dkornishev
  */
 @Component
 @Path("saml")
 public class SamlFederationResource {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(SamlFederationResource.class);
-    
+
     @Autowired
     private SamlHelper saml;
-    
+
     @Autowired
     private Repository<Entity> repo;
-    
+
     @Autowired
     private UserLocator users;
-    
+
     @Autowired
     private SamlAttributeTransformer transformer;
-    
+
     @Autowired
     private OauthSessionManager sessionManager;
-    
+
     @Value("${sli.security.sp.issuerName}")
     private String metadataSpIssuerName;
-    
+
     @Value("classpath:saml/samlMetadata.xml.template")
     private Resource metadataTemplateResource;
-    
+
+//    @Context
+//    private UriInfo uriInfo;
+
+//    @Context
+//    private HttpHeaders httpHeaders;
+
+    @Context
+    private HttpServletRequest httpServletRequest;
+
     private String metadata;
-    
+
     @SuppressWarnings("unused")
     @PostConstruct
     private void processMetadata() throws IOException {
@@ -75,36 +89,60 @@ public class SamlFederationResource {
         StringWriter writer = new StringWriter();
         IOUtils.copy(is, writer);
         is.close();
-        
+
         metadata = writer.toString();
         metadata = metadata.replaceAll("\\$\\{sli\\.security\\.sp.issuerName\\}", metadataSpIssuerName);
     }
-    
+
     @POST
     @Path("sso/post")
     @SuppressWarnings("unchecked")
     public Response consume(@FormParam("SAMLResponse") String postData) throws Exception {
-        
+
         LOG.info("Received a SAML post for SSO...");
-        
-        Document doc = saml.decodeSamlPost(postData);
-        
+
+        Document doc = null;
+
+        try {
+            doc = saml.decodeSamlPost(postData);
+        } catch (Exception e) {
+            SecurityEvent event = new SecurityEvent();
+
+            event.setClassName(this.getClass().toString());
+
+            if (this.httpServletRequest != null) {
+                event.setUserOrigin(httpServletRequest.getRemoteHost());
+                event.setAppId(httpServletRequest.getHeader("User-Agent"));
+                event.setActionUri(httpServletRequest.getRequestURI());
+                event.setUser(httpServletRequest.getRemoteUser());
+                event.setLogMessage("SAML message received from " + httpServletRequest.getHeader("Origin") + " is invalid!");
+                event.setLogLevel(LogLevelType.TYPE_WARN);
+            } else {
+                event.setLogMessage("This should never happen!!");
+                event.setLogLevel(LogLevelType.TYPE_ERROR);
+            }
+
+            audit(event);
+
+            throw e;
+        }
+
         String inResponseTo = doc.getRootElement().getAttributeValue("InResponseTo");
         String issuer = doc.getRootElement().getChildText("Issuer", SamlHelper.SAML_NS);
-        
+
         NeutralQuery neutralQuery = new NeutralQuery();
         neutralQuery.setOffset(0);
         neutralQuery.setLimit(1);
         neutralQuery.addCriteria(new NeutralCriteria("idp.id", "=", issuer));
         Entity realm = fetchOne("realm", neutralQuery);
-        
+
         if (realm == null) {
             throw new IllegalStateException("Failed to locate realm: " + issuer);
         }
-        
+
         org.jdom.Element stmt = doc.getRootElement().getChild("Assertion", SamlHelper.SAML_NS).getChild("AttributeStatement", SamlHelper.SAML_NS);
         List<org.jdom.Element> attributeNodes = stmt.getChildren("Attribute", SamlHelper.SAML_NS);
-        
+
         LinkedMultiValueMap<String, String> attributes = new LinkedMultiValueMap<String, String>();
         for (org.jdom.Element attributeNode : attributeNodes) {
             String samlAttributeName = attributeNode.getAttributeValue("Name");
@@ -113,47 +151,47 @@ public class SamlFederationResource {
                 attributes.add(samlAttributeName, valueNode.getText());
             }
         }
-        
+
         // Apply transforms
         attributes = transformer.apply(realm, attributes);
-        
+
         SLIPrincipal principal = users.locate((String) realm.getBody().get("tenantId"), attributes.getFirst("userId"));
         principal.setName(attributes.getFirst("userName"));
         principal.setRoles(attributes.get("roles"));
         principal.setRealm(realm.getEntityId());
         principal.setAdminRealm(attributes.getFirst("adminRealm"));
         principal.setEdOrg(attributes.getFirst("edOrg"));
-        
+
         // {sessionId,redirectURI}
         Pair<String, URI> tuple = this.sessionManager.composeRedirect(inResponseTo, principal);
-        
+
         return Response.temporaryRedirect(tuple.getRight()).cookie(new NewCookie("_tla", tuple.getLeft(), "/", ".slidev.org", "", 300, false)).build();
     }
-    
+
     private Entity fetchOne(String collection, NeutralQuery neutralQuery) {
         Iterable<Entity> results = repo.findAll(collection, neutralQuery);
-        
+
         if (!results.iterator().hasNext()) {
             throw new RuntimeException("Not found");
         }
-        
+
         return results.iterator().next();
     }
-    
+
     /**
      * Get metadata describing saml federation.
      * This is an unsecured (public) resource.
-     * 
+     *
      * @return Response containing saml metadata
      */
     @GET
     @Path("metadata")
     public Response getMetadata() {
-        
+
         if (!metadata.isEmpty()) {
             return Response.ok(metadata).build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
-        
+
     }
 }
