6898a147cd0bfce34b2baf5059a4a9adf377d3dc
Chen Cheng <ccheng@wgen.net>
2012-05-10 17:23:47 -0400

    fixing checkstyle error

--- sli/dashboard/src/main/java/org/slc/sli/manager/component/impl/CustomizationAssemblyFactoryImpl.java
+++ sli/dashboard/src/main/java/org/slc/sli/manager/component/impl/CustomizationAssemblyFactoryImpl.java
@@ -76,7 +76,7 @@ public class CustomizationAssemblyFactoryImpl implements CustomizationAssemblyFa
 
     protected Config getConfig(String componentId) {
         EdOrgKey edOrg = userEdOrgManager.getUserEdOrg(getTokenId());
-        if(edOrg == null) {
+        if (edOrg == null) {
             throw new DashboardException("No data is available for you to view. Please contact your IT administrator");
         }
         return configManager.getComponentConfig(getTokenId(), edOrg, componentId);
