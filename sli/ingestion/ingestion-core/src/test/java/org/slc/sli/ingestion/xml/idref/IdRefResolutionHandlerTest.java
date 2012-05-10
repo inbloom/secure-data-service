49d339e39bed5b1c04e9356b54f0facaa4880276
Oleg Krook <okrook@wgen.net>
2012-05-10 14:45:04 -0400

    Fixed resource leakage in unit tests

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/xml/idref/IdRefResolutionHandlerTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/xml/idref/IdRefResolutionHandlerTest.java
@@ -1,9 +1,10 @@
 package org.slc.sli.ingestion.xml.idref;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
@@ -38,65 +39,124 @@ import org.slc.sli.ingestion.validation.ErrorReport;
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class IdRefResolutionHandlerTest {
 
+    /**
+     * TestExecutor interface
+     *
+     * @author okrook
+     *
+     */
+    public interface TestExecutor {
+        void execute(File file) throws Throwable;
+    }
+
     @Autowired
     private IdRefResolutionHandler idRefResolutionHandler;
 
     @Test
     public void testFindIDRefsToResolve() throws Throwable {
         File inputFile = IngestionTest.getFile("ReferenceResolution/InterchangeStudentGrade.xml");
-        File tempInputFile = new File(inputFile.getPath().substring(0, inputFile.getPath().lastIndexOf(".xml")) + "_TEMP.xml");
 
-        IOUtils.copy(new FileInputStream(inputFile), new FileOutputStream(tempInputFile));
+        TestExecutor findIdRefsToResolve = new TestExecutor() {
 
-        @SuppressWarnings("unchecked")
-        Set<String> result = (Set<String>) PrivateAccessor.invoke(idRefResolutionHandler, "findIDRefsToResolve", new Class[]{File.class}, new Object[]{tempInputFile});
+            @Override
+            public void execute(File file) throws Throwable {
+                @SuppressWarnings("unchecked")
+                Set<String> result = (Set<String>) PrivateAccessor.invoke(idRefResolutionHandler, "findIDRefsToResolve", new Class[]{File.class}, new Object[]{file});
 
-        Assert.assertNotNull(result);
-        Assert.assertEquals(1, result.size());
-        Assert.assertTrue(result.contains("GBE-8th Grade English-3"));
+                Assert.assertNotNull(result);
+                Assert.assertEquals(1, result.size());
+                Assert.assertTrue(result.contains("GBE-8th Grade English-3"));
+            }
+        };
+
+        performTestOnFile(inputFile, findIdRefsToResolve);
     }
 
     @Test
     public void testFindMatchingEntities() throws Throwable {
         File inputFile = IngestionTest.getFile("ReferenceResolution/InterchangeStudentGrade.xml");
-        File tempInputFile = new File(inputFile.getPath().substring(0, inputFile.getPath().lastIndexOf(".xml")) + "_TEMP.xml");
 
-        IOUtils.copy(new FileInputStream(inputFile), new FileOutputStream(tempInputFile));
+        TestExecutor findMatchingEntities = new TestExecutor() {
 
+            @SuppressWarnings("unchecked")
+            @Override
+            public void execute(File file) throws Throwable {
+                Map<String, File> result = null;
 
-        Set<String> refs = (Set<String>) PrivateAccessor.invoke(idRefResolutionHandler, "findIDRefsToResolve", new Class[]{File.class}, new Object[]{tempInputFile});
-        Map<String, String> result = (Map<String, String>) PrivateAccessor.invoke(idRefResolutionHandler, "findMatchingEntities", new Class[]{File.class, Set.class}, new Object[]{tempInputFile, refs});
+                try {
+                    Set<String> refs = (Set<String>) PrivateAccessor.invoke(idRefResolutionHandler, "findIDRefsToResolve", new Class[]{File.class}, new Object[]{file});
 
-        Assert.assertNotNull(result);
-        Assert.assertEquals(1, result.size());
-        Assert.assertTrue(result.containsKey("GBE-8th Grade English-3"));
+                    result = (Map<String, File>) PrivateAccessor.invoke(idRefResolutionHandler, "findMatchingEntities", new Class[]{File.class, Set.class}, new Object[]{file, refs});
+
+                    Assert.assertNotNull(result);
+                    Assert.assertEquals(1, result.size());
+                    Assert.assertTrue(result.containsKey("GBE-8th Grade English-3"));
+                } finally {
+                    if (result != null) {
+                        for (File f : result.values()) {
+                            deleteQuietly(f);
+                        }
+                    }
+                }
+            }
+        };
+
+        performTestOnFile(inputFile, findMatchingEntities);
     }
 
     @Test
-    public void testProcess() throws IOException {
-        File inputFile = IngestionTest.getFile("ReferenceResolution/InterchangeStudentGrade.xml");
-        File tempInputFile = new File(inputFile.getPath().substring(0, inputFile.getPath().lastIndexOf(".xml")) + "_TEMP.xml");
+    public void testProcess() throws Throwable {
+        final File inputFile = IngestionTest.getFile("ReferenceResolution/InterchangeStudentGrade.xml");
+
+        TestExecutor process = new TestExecutor() {
+
+            @Override
+            public void execute(File file) throws Throwable {
+                ErrorReport errorReport = Mockito.mock(ErrorReport.class);
+                FileProcessStatus fileProcessStatus = Mockito.mock(FileProcessStatus.class);
+                String beforeHash = MD5.calculate(inputFile);
+                IngestionFileEntry inputFileEntry = new IngestionFileEntry(FileFormat.EDFI_XML,
+                        FileType.XML_STUDENT_GRADES, file.getName(), beforeHash);
+                inputFileEntry.setFile(file);
+
+                idRefResolutionHandler.doHandling(inputFileEntry, errorReport, fileProcessStatus);
+
+                String afterHash = MD5.calculate(file);
+
+                Assert.assertFalse(beforeHash.equals(afterHash));
+            }
+        };
+
+        performTestOnFile(inputFile, process);
+    }
 
 
-        InputStream inputFileStream = new FileInputStream(inputFile);
-        OutputStream tempInputFileStream = new FileOutputStream(tempInputFile);
-        IOUtils.copy(inputFileStream , tempInputFileStream);
+    private static void performTestOnFile(File inputFile, TestExecutor test) throws Throwable {
+        File tempInputFile = null;
+        InputStream input = null;
+        OutputStream output = null;
 
-        inputFileStream.close();
-        tempInputFileStream.close();
+        try {
+            tempInputFile = File.createTempFile("unit", ".test");
 
-        ErrorReport errorReport = Mockito.mock(ErrorReport.class);
-        FileProcessStatus fileProcessStatus = Mockito.mock(FileProcessStatus.class);
-        String beforeHash = MD5.calculate(inputFile);
-        IngestionFileEntry inputFileEntry = new IngestionFileEntry(FileFormat.EDFI_XML,
-                FileType.XML_STUDENT_GRADES, tempInputFile.getName(), beforeHash);
-        inputFileEntry.setFile(tempInputFile);
+            input = new BufferedInputStream(new FileInputStream(inputFile));
+            output = new BufferedOutputStream(new FileOutputStream(tempInputFile));
 
-        idRefResolutionHandler.doHandling(inputFileEntry, errorReport, fileProcessStatus);
+            IOUtils.copy(input, output);
 
-        String afterHash = MD5.calculate(tempInputFile);
+            output.flush();
 
-        Assert.assertFalse(beforeHash.equals(afterHash));
+            test.execute(tempInputFile);
+        } finally {
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(output);
+            deleteQuietly(tempInputFile);
+        }
+    }
 
+    private static void deleteQuietly(File file) {
+        if (file != null) {
+            file.delete();
+        }
     }
 }
