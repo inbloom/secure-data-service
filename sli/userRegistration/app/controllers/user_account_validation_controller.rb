57882b44469c7135f0b8b4b65d47c28314789f24
kmyers <kmyers@wgen.net>
2012-05-10 14:04:12 -0400

    Refactor validation controller

--- sli/userRegistration/app/controllers/user_account_validation_controller.rb
+++ sli/userRegistration/app/controllers/user_account_validation_controller.rb
@@ -1,64 +1,10 @@
 
-require 'rest-client'
-require 'json'
-
 class UserAccountValidationController < ApplicationController
-  
-  # success message
-  ACCOUNT_VERIFICATION_COMPLETE = {
-    "status" => "Registration Complete!",
-    "message" => "An administrator will email you when your account is ready."
-  }
-  
-  INVALID_VERIFICATION_CODE = {
-    "status" => "Account validation failed!",
-    "message" => "Invalid account verification code."
-  }
-  
-  # error condition for attemptint to re-verify verified account
-  ACCOUNT_PREVIOUSLY_VERIFIED = {
-    "status" => "Account validation failed!",
-    "message" => "Account previously verified."
-  }
-  
-  # error condition for attemptint to re-verify verified account
-  UNEXPECTED_VERIFICATION_ERROR = {
-    "status" => "Account validation failed!",
-    "message" => "Unexpected verification error. Try again later."
-  }
-  
-  REST_HEADER = {
-    "Content-Type" => "application/json",
-    "content-type" => "application/json",
-    "accept" => "application/json"
-  }
   
   # GET /user_account_registrations/validate/1
   # GET /user_account_registrations/validate/1.json
   def show
-    
-    url = APP_CONFIG['api_base'] + "/" + params[:id]
-    
-    res = RestClient.get(url, REST_HEADER){|response, request, result| response }
-    
-    if (res.code == 200)
-      parsedJsonHash = JSON.parse(res.body)
-      if (parsedJsonHash["validated"] == true)
-        @validation_result = ACCOUNT_PREVIOUSLY_VERIFIED
-      else
-        parsedJsonHash["validated"] = true
-        putRes = RestClient.put(url, parsedJsonHash.to_json, REST_HEADER){|response, request, result| response }
-        if (putRes.code == 204)
-          @validation_result = ACCOUNT_VERIFICATION_COMPLETE
-        else
-          @validation_result = UNEXPECTED_VERIFICATION_ERROR
-        end
-      end
-    elsif (res.code == 404)
-      @validation_result = INVALID_VERIFICATION_CODE
-    else
-      @validation_result = UNEXPECTED_VERIFICATION_ERROR
-    end 
-
+    @validation_result = UserAccountValidation.validate_account params[:id]
+    render :show
   end # end def show
 end
