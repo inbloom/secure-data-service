d0f3ad1dd43d67f08471e1bcc3cf663cf84d655c
Oleg Krook <okrook@wgen.net>
2012-05-11 15:53:29 -0400

    FreeMarker based ReferenceResolver

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/referenceresolution/SmooksExtendedReferenceResolverTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/referenceresolution/SmooksExtendedReferenceResolverTest.java
@@ -3,17 +3,17 @@ package org.slc.sli.ingestion.referenceresolution;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.Assert;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.milyn.Smooks;
+import org.xml.sax.SAXException;
 
 import org.slc.sli.ingestion.IngestionTest;
 
@@ -22,12 +22,8 @@ import org.slc.sli.ingestion.IngestionTest;
  * @author tke
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class SmooksExtendedReferenceResolverTest {
-
-    @Autowired
-    SmooksExtendedReferenceResolver referenceFactory;
+    SmooksExtendedReferenceResolver referenceFactory = new SmooksExtendedReferenceResolver();
 
     private void test(File content, File expected, String xpath) throws IOException {
         File result = null;
@@ -65,9 +61,15 @@ public class SmooksExtendedReferenceResolverTest {
     }
 
     @Test
-    public void testResolution() throws IOException {
+    public void testResolution() throws IOException, SAXException {
         File input = IngestionTest.getFile("idRefResolutionData/InterchangeAssessmentMetadataAssessmentAssessmentFamilyReference_input.xml");
         File expected = IngestionTest.getFile("idRefResolutionData/InterchangeAssessmentMetadataAssessmentAssessmentFamilyReference_expected.xml");
+
+        Map<String, Smooks> config = new HashMap<String, Smooks>();
+        config.put("/InterchangeStudentAssessment/StudentAssessment/AssessmentReference", new Smooks("idRefResolution/InterchangeStudentAssessment/StudentAssessment/AssessmentReference.xml"));
+
+        referenceFactory.setIdRefConfigs(config);
+
         test(input, expected, "/InterchangeStudentAssessment/StudentAssessment/AssessmentReference");
     }
 }
