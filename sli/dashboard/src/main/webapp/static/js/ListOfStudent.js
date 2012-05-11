cbbd86fd960b1d06c5e5cb0ac34a1078daecdcf6
Vijay Ummalaneni <vummalaneni@wgen.net>
2012-05-11 10:40:45 -0400

    Population widget skinning Changes

--- sli/dashboard/src/main/webapp/static/js/ListOfStudent.js
+++ sli/dashboard/src/main/webapp/static/js/ListOfStudent.js
@@ -1,4 +1,77 @@
 var listOfStudents = 'listOfStudents';
+var selectedPopulation=dataModel['userEdOrg']['selectedPopulation'];
+var edOrgIndex = -1
+var schoolIndex = -1;
+var courseIndex = -1;
+var sectionIndex = -1;
+
+if(selectedPopulation != undefined && selectedPopulation != null) {
+
+    $(document).ready( function() {
+    	
+        var edOrgLength = instHierarchy.length;
+        edOrgIndex = edOrgLength == 1 ?  0 : -1;
+        if (edOrgLength != 1) {
+            for(var i=0; i < edOrgLength; i++) {
+                if(instHierarchy[i].name == selectedPopulation.name) {
+                    DashboardUtil.selectDropDownOption("edOrg", i, true);
+                    edOrgIndex = i;
+                    break;
+                }
+            }
+        } 
+        
+        var schoolsLength = instHierarchy[edOrgIndex].schools.length;
+        schoolIndex = schoolsLength == 1 ? 0 : -1;
+        if (edOrgIndex > -1 && schoolsLength > 1) {
+           for(var i=0; i < schoolsLength; i++) {
+                if(instHierarchy[edOrgIndex].schools[i].id == selectedPopulation.section.schoolId) {
+                    DashboardUtil.selectDropDownOption("school", i, true);
+                    schoolIndex = i;
+                    break;
+                }
+            } 
+        }
+        
+        var coursesLength =  instHierarchy[edOrgIndex].schools[schoolIndex].courses.length;
+        courseIndex = coursesLength == 1 ? 0 : -1;
+        if (edOrgIndex > -1 && schoolIndex > -1 && coursesLength > 1) {
+            for(var i=0; i < coursesLength; i++) {
+                if(instHierarchy[edOrgIndex].schools[schoolIndex].courses[i].id == selectedPopulation.section.courseId) {
+                    DashboardUtil.selectDropDownOption("course", i, true);
+                    courseIndex = i;
+                    break;
+                }
+            } 
+        }
+        
+        var sectionsLength = instHierarchy[edOrgIndex].schools[schoolIndex].courses[courseIndex].sections.length;
+        sectionIndex = sectionsLength == 1 ? 0 : -1;
+        if (edOrgIndex > -1 && schoolIndex > -1 && courseIndex > -1 && sectionsLength > 0) { // No autoselection for section
+            for(var i=0; i < sectionsLength; i++) {
+                if(instHierarchy[edOrgIndex].schools[schoolIndex].courses[courseIndex].sections[i].id == selectedPopulation.section.id) {
+                    DashboardUtil.selectDropDownOption("section", i, false);
+                    sectionIndex = i;
+                    break;
+                } 
+            }
+        }
+        
+        if (edOrgIndex > -1 && schoolIndex > -1 && courseIndex > -1 && sectionIndex > -1) {
+            DashboardProxy.load("listOfStudents", selectedPopulation.section.id, function(panel) {
+                populateView(panel.viewConfig.items);
+                if (panel.viewConfig.items.length > 0) {
+                    DashboardUtil.selectDropDownOption("view", 0, false);
+                } else {
+                    $("#viewSelect").val(-1);
+                }
+                populateFilter();
+                DashboardUtil.selectDropDownOption("filter", -1, false);
+                printStudentList();
+            });
+        }
+    });
+}
 
 function getStudentListData() 
   {
@@ -10,28 +83,20 @@ function getStudentListData()
       location.href = contextRootPath + "/service/layout/listOfStudentsPage/" + instHierarchy[edorgIndex].schools[schoolIndex].courses[courseIndex].sections[selectionIndex].id;
   }
   
-  function populateView(panelConfig) 
+  function populateView(panelConfigItems) 
   {
-    var select = "<select id='viewSelect' onChange='clearStudentList();printStudentList()'>";
-    var index=0;
-    if(panelConfig.items.length == 0) {
-	    select += "<option value='-1'></option>";
-    }
-    else {
-	    for(index=0;index<panelConfig.items.length;index++) {
-	        select += "<option value='"+index+"'>"+panelConfig.items[index].name+"</option>";
-	    }
-    }
-    select += "</selection>";
-    document.getElementById("viewDiv").innerHTML = select;
+	  $("#viewSelection").show();
+	  
+	  DashboardUtil.setDropDownOptions("view", null, panelConfigItems, "name", "", false, function() {
+		  printStudentList();
+	  });
   } 
   
   function printStudentList()
   {
       var tableId = getTableId();
       var panelConfig = DashboardProxy.getConfig(listOfStudents);
-      var viewSelect=document.getElementById("viewSelect");
-      var viewIndex=viewSelect.options[viewSelect.selectedIndex].value;
+      var viewIndex=$("#viewSelect").val();
       var options={};
       if(viewIndex != -1) {
 	      jQuery.extend(options, panelConfig, {items:panelConfig.items[viewIndex].items});
@@ -44,18 +109,45 @@ function getStudentListData()
         $('#'+getTableId()).jqGrid("GridUnload");
     }
     
+    function clearSelections(currentSelect) {
+    	var selects = { "edOrg":1, "school":2, "course":3, "section":4 };
+    	switch(selects[currentSelect]) {
+    		case 1: $("#schoolSelect").val(-1);
+    				$("#schoolSelectMenu .optionText").html("");
+    				$("#schoolSelectMenu .dropdown-toggle").addClass("disabled");
+    				$("#schoolSelectMenu .dropdown-menu").html("");
+    		case 2: $("#courseSelect").val(-1);
+    				$("#courseSelectMenu .optionText").html("");
+    				$("#courseSelectMenu .dropdown-toggle").addClass("disabled"); 
+    				$("#courseSelectMenu .dropdown-menu").html("");
+    		case 3: $("#sectionSelect").val(-1);
+    				$("#sectionSelectMenu .optionText").html("");
+    				$("#sectionSelectMenu .dropdown-toggle").addClass("disabled");
+    				$("#sectionSelectMenu .dropdown-menu").html("");
+    		case 4: $("#filterSelect").val(-1);
+    				$("#filterSelectMenu .optionText").html("");
+    				$("#filterSelectMenu .dropdown-toggle").addClass("disabled");
+    				$("#filterSelectMenu .dropdown-menu").html("");
+    				$("#viewSelect").val(-1);
+    				$("#viewSelectMenu .optionText").html("");
+    				$("#viewSelectMenu .dropdown-toggle").addClass("disabled");
+    				$("#viewSelectMenu .dropdown-menu").html("");
+    				$("#viewSelection").hide();
+    				clearStudentList();
+    	}
+    }
+    
     function filterStudentList(filterBy)
     {
       var panelConfig = DashboardProxy.getConfig(listOfStudents);
       var options={};
-      var viewSelect=document.getElementById("viewSelect");
-      var viewIndex=viewSelect.options[viewSelect.selectedIndex].value;      
+      var viewIndex = $("#viewSelect").val();
       if(viewIndex != -1) {
 	      jQuery.extend(options, panelConfig, {items:panelConfig.items[viewIndex].items});
 	      
 	      if (filterBy == undefined) {
 	        DashboardUtil.makeGrid(getTableId(), options, DashboardProxy.getData(listOfStudents), {});
-	      }else {
+	      } else {
 	         
 	          var filteredData = jQuery.extend({}, DashboardProxy.getData(listOfStudents));
 	          filteredStudents = filteredData.students;
@@ -72,117 +164,52 @@ function getStudentListData()
 	      }
       }
     }
-function populateInstHierarchy(){
-    var y = "<select id='edOrgSelect' onChange='clearStudentList();populateSchoolMenu()'>";
-    y += "<option value='-1'></option>";
-    var i = 0;
-    for(i = 0;i<instHierarchy.length;i++){
-	y += "<option value='" +i +"'>"+ instHierarchy[i].name + "</option>";
-    }
-    y += "</select>";
-    document.getElementById("edorgDiv").innerHTML = y;
-    
-    if(instHierarchy.length == 1) {
-    	$("#edOrgSelect").val(0);
-    	populateSchoolMenu();
-    }
+function populateInstHierarchy() {
+	
+	$("#viewSelection").hide();
+	clearSelections("edOrg");
+	DashboardUtil.setDropDownOptions("edOrg", null,  instHierarchy, "name", "", true, function() {
+		populateSchoolMenu();
+	});
 }
 
-function populateSchoolMenu(){
-    var edorgSelect = document.getElementById("edOrgSelect");
-    var edorgIndex = edorgSelect.options[edorgSelect.selectedIndex].value;
-    var y = '';
-    document.getElementById("courseDiv").innerHTML = '';
-    document.getElementById("sectionDiv").innerHTML = '';
-    if( edorgIndex > -1 ) {
-        var temp = instHierarchy[edorgIndex].schools;
-    
-        y = "<select id='schoolSelect' onChange='clearStudentList();populateCourseMenu()'>";
-        y += "<option value='-1'></option>"   ;
-        var i = 0;
-        for(i = 0;i<temp.length;i++) {
-        	y += "<option value='" +i +"'>"+ temp[i].nameOfInstitution + "</option>";
-        }
-        y += "</select>";
-    }
-    document.getElementById("schoolDiv").innerHTML = y;
-    
-    if (instHierarchy[edorgIndex].schools.length == 1) {
-    	$("#schoolSelect").val(0);
-    	populateCourseMenu();
-    } 
+function populateSchoolMenu() {
+	var edOrgIndex = $("#edOrgSelect").val();
+	clearSelections("school");
+	DashboardUtil.setDropDownOptions("school", null, instHierarchy[edOrgIndex].schools, "nameOfInstitution", "", true, function() {
+		populateCourseMenu();
+	});
 }
 
 function populateCourseMenu(){
-    var edorgSelect = document.getElementById("edOrgSelect");
-    var edorgIndex = edorgSelect.options[edorgSelect.selectedIndex].value;
-    var schoolSelect = document.getElementById("schoolSelect");
-    var schoolIndex = schoolSelect.options[schoolSelect.selectedIndex].value;
-    var y = '';
-
-    document.getElementById("sectionDiv").innerHTML = '';
-    if( schoolIndex > -1) {
-        var temp = instHierarchy[edorgIndex].schools[schoolIndex].courses;
-     
-        y = "<select id='courseSelect' onChange='clearStudentList();populateSectionMenu()'>";
-        y += "<option value='-1'></option>";
-        var j = 0;
-        for(j = 0;j < temp.length;j++){
-        	y += "<option value='" +j +"'>"+ temp[j].courseTitle + "</option>";
-        }
-    }
-    document.getElementById("courseDiv").innerHTML = y;
-    
-    if (instHierarchy[edorgIndex].schools[schoolIndex].courses.length == 1) {
-    	$("#courseSelect").val(0);
-    	populateSectionMenu();
-    }
+	var edOrgIndex = $("#edOrgSelect").val();
+	var schoolIndex = $("#schoolSelect").val();
+	clearSelections("course");
+	DashboardUtil.setDropDownOptions("course", null, instHierarchy[edOrgIndex].schools[schoolIndex].courses, "courseTitle", "", true, function() {
+		populateSectionMenu();
+	});
 }
 
 function populateSectionMenu(){
-    var edorgSelect = document.getElementById("edOrgSelect");
-    var schoolSelect = document.getElementById("schoolSelect");
-    var courseSelect = document.getElementById("courseSelect");
-    var edorgIndex = edorgSelect.options[edorgSelect.selectedIndex].value;
-    var schoolIndex = schoolSelect.options[schoolSelect.selectedIndex].value;
-    var courseIndex = courseSelect.options[courseSelect.selectedIndex].value;
-    var y = '';
-
-    if( courseIndex > -1) {
-        var temp = instHierarchy[edorgIndex].schools[schoolIndex].courses[courseIndex].sections;
-        
-        y = "<select id='sectionSelect' onChange='clearStudentList();getStudentListData()'>";
-        y += "<option value='-1'></option>";
-        var i = 0;
-        for(;i < temp.length;i++){
-        	y += "<option value='" +i +"'>"+ temp[i].sectionName + "</option>";
-        }
-        y += "</select>";
-    }
-    document.getElementById("sectionDiv").innerHTML = y;
-    
-    /* Commenting out as there is no go button hence, no auto selection for section
-    if (instHierarchy[edorgIndex].schools[schoolIndex].courses[courseIndex].sections.length == 1) {
-    	$("#sectionSelect").val(0);
-    	getStudentListData();
-    }*/
+	
+	var edOrgIndex = $("#edOrgSelect").val();
+	var schoolIndex = $("#schoolSelect").val();
+	var courseIndex = $("#courseSelect").val();
+	clearSelections("section");
+	DashboardUtil.setDropDownOptions("section", null, instHierarchy[edOrgIndex].schools[schoolIndex].courses[courseIndex].sections, "sectionName", "", false, function() {
+		getStudentListData();
+	});
 }
 
 function populateFilter() {
-    
-    var select = "<select id='filterSelect' onChange='clearStudentList();filterStudents()'>";
-    var index=0;
-    select += "<option value='-1'>No Filter</option>";
-    var lozengeConfig = DashboardProxy.widgetConfig.lozenge;
-    for(index=0;index<lozengeConfig.items.length; index++) {
-        select += "<option value='"+index+"'>"+lozengeConfig.items[index].description+"</option>";
-    }
-    select += "</select>";
-    document.getElementById("filterDiv").innerHTML = select;
-    
+    var defaultOptions = {"-1": "No Filter"};
+	DashboardUtil.setDropDownOptions("filter", defaultOptions, DashboardProxy.widgetConfig.lozenge.items, "description", "", true, function() {
+		clearStudentList();
+		filterStudents();
+	});
 }
 
 function filterStudents() {
-    var filterSelect = document.getElementById("filterSelect");
-    filterStudentList(DashboardProxy.widgetConfig.lozenge.items[filterSelect.selectedIndex-1]); 
+    var filterSelect = $("#filterSelect").val();
+    filterStudentList(DashboardProxy.widgetConfig.lozenge.items[filterSelect]); 
 }
