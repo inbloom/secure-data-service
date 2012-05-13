ebba8de402777116272539ffeba8ef4c60529e5e
asaarela <asaarela@wgen.net>
2012-05-13 15:26:44 -0400

    Updating Unix version of runTestsLocally to reflect changes made on the win32 side.

--- sli/acceptance-tests/runTestsLocally.sh
+++ sli/acceptance-tests/runTestsLocally.sh
@@ -8,24 +8,16 @@ if [ -z "$SLI_ROOT" ]; then
 	exit 1
 fi
 
-echo "Shutting down databrowser and admin-rails..."
-if [ -f ${SLI_ROOT}/databrowser/tmp/pids/server.pid ]; then
-	kill -9 < ${SLI_ROOT}/databrowser/tmp/pids/server.pid
-	rm -f ${SLI_ROOT}/databrowser/tmp/pids/server.pid
-fi
-
-if [ -f ${SLI_ROOT}/admin-tools/admin-rails/tmp/pids/server.pid ]; then
-	kill -9 < ${SLI_ROOT}/admin-tools/admin-rails/tmp/pids/server.pid
-	rm -f ${SLI_ROOT}/admin-tools/admin-rails/tmp/pids/server.pid
-fi
-
 echo "Generating properties file..."
 ${SLI_ROOT}/config/scripts/webapp-provision.rb ${SLI_ROOT}/config/config.in/canonical_config.yml local-acceptance-tests ${SLI_ROOT}/config/properties/sli.properties
 echo "Done."
 
 cd ${SLI_ROOT}/acceptance-tests
 
-export MAVEN_OPTS="-XX:PermSize=256m -XX:MaxPermSize=1024m -Dsli.conf=${SLI_ROOT}/config/properties/sli.properties -Dsli.env=local -Dsli.encryption.keyStore=${SLI_ROOT}/data-access/dal/keyStore/localKeyStore.jks"
+export ADMIN_RAILS_DIR="${SLI_ROOT}/admin-tools/admin-rails"
+export DATABROWSER_DIR="${SLI_ROOT}/databrowser"
+
+export MAVEN_OPTS="-XX:PermSize=256m -XX:MaxPermSize=1024m -DADMIN_RAILS_DIR=${ADMIN_RAILS_DIR} -DDATABROWSER_DIR=${DATABROWSER_DIR} -Dsli.conf=${SLI_ROOT}/config/properties/sli.properties -Dsli.env=local -Dsli.encryption.keyStore=${SLI_ROOT}/data-access/dal/keyStore/localKeyStore.jks"
 
 export BUNDLE_GEMFILE=Gemfile
 
@@ -37,9 +29,22 @@ if [ -z ${1} ]; then
 else
    export TESTS_TO_RUN=${1}
 fi
-echo "Running test: ${TESTS_TO_RUN}"
+
+echo "Starting rails apps..."
+cd ${ADMIN_RAILS_DIR}
+bundle install
+bundle exec rails server -d -p 2000 -e local-integration-tests
+
+cd ${DATABROWSER_DIR}
+bundle install
+bundle exec rails server -d -p 2001 -e local-integration-tests
+
+cd ${SLI_ROOT}/acceptance-tests
+
+echo "Running maven tests"
 mvn integration-test
 
+echo "Stopping rails apps..."
 echo "Shutting down databrowser and admin-rails..."
 if [ -f ${SLI_ROOT}/databrowser/tmp/pids/server.pid ]; then
 	kill -9 < ${SLI_ROOT}/databrowser/tmp/pids/server.pid
