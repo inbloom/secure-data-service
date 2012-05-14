9c4d71c1c9731b897b3c2d1b4732b9f3ab6b975a
kmyers <kmyers@wgen.net>
2012-05-14 13:34:34 -0400

    Unit tests

--- sli/userRegistration/test/functional/user_account_validation_controller_test.rb
+++ sli/userRegistration/test/functional/user_account_validation_controller_test.rb
@@ -2,13 +2,16 @@ require 'test_helper'
 require 'mocha'
 
 class UserAccountValidationControllerTest < ActionController::TestCase
- 
+  
+  # success message
+  ACCOUNT_VERIFICATION_COMPLETE = {
+    "status" => "Registration Complete!",
+    "message" => "An administrator will email you when your account is ready."
+  }
+  
   test "should present success/failure screen" do
+    UserAccountValidation.stubs(:validate_account).returns(ACCOUNT_VERIFICATION_COMPLETE)
     get :show
-    assert_response :success
-  end
-
-  test "should show eula" do
     assert_response :success
   end
 end
