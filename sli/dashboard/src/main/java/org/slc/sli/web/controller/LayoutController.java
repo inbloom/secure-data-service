65f16508d8ccb076e81989c8c7172e33864c5c05
Anna Grebneva <agrebneva@agrebneva-mac.wgenhq.net>
2012-05-15 11:41:47 -0400

    CR changes

--- sli/dashboard/src/main/java/org/slc/sli/web/controller/LayoutController.java
+++ sli/dashboard/src/main/java/org/slc/sli/web/controller/LayoutController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import org.slc.sli.web.util.SafeUUID;
+import org.slc.sli.web.entity.SafeUUID;
 
 /**
  * Layout controller for all types of requests.
