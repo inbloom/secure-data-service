8ef4c8b57ed06ca942aede428b5968d2c81dc2fb
Ryan Farris <rfarris@wgen.net>
2012-05-15 11:25:24 -0400

    Initialize constants.

--- sli/admin-tools/admin-rails/app/models/developer_approval.rb
+++ sli/admin-tools/admin-rails/app/models/developer_approval.rb
@@ -1,4 +1,9 @@
 class DeveloperApproval < Ldap
+  IS_SANDBOX           = APP_CONFIG["is_sandbox"]
+  EMAIL_HOST           = APP_CONFIG["email_host"]
+  EMAIL_PORT           = APP_CONFIG["email_port"]
+  
+  
   @@emailer=Emailer.new({:host=>EMAIL_HOST,:port=>EMAIL_PORT})
   @@j = ActiveSupport::JSON
 
