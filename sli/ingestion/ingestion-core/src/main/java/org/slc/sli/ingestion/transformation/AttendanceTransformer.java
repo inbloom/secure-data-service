5935b1e301425713325ac933a310b4b2c3a4b87e
Stan Halka <shalka@wgen.net>
2012-05-15 03:33:05 +0100

    checkstyle fixes

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
@@ -17,6 +17,7 @@ import org.slc.sli.ingestion.NeutralRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -26,6 +27,7 @@ import org.springframework.stereotype.Component;
  * 
  * @author shalka
  */
+@Scope("prototype")
 @Component("attendanceTransformationStrategy")
 public class AttendanceTransformer extends AbstractTransformationStrategy {
     private static final Logger LOG = LoggerFactory.getLogger(AttendanceTransformer.class);
