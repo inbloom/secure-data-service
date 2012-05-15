7716f195bb7a7478ccf83c373ccb55a60b2106d8
Matthew Lane <lanemt@gmail.com>
2012-05-15 17:49:37 -0400

    adds unit tests and resource null check

--- sli/api/src/test/java/org/slc/sli/api/resources/security/AdminDelegationResourceTest.java
+++ sli/api/src/test/java/org/slc/sli/api/resources/security/AdminDelegationResourceTest.java
@@ -1,21 +1,23 @@
 package org.slc.sli.api.resources.security;
 
+import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.slc.sli.api.representation.EntityBody;
 import org.slc.sli.api.resources.SecurityContextInjector;
-import org.slc.sli.api.service.EntityService;
+import org.slc.sli.api.security.SLIPrincipal;
 import org.slc.sli.api.test.WebContextTestExecutionListener;
-import org.slc.sli.domain.Entity;
-import org.slc.sli.domain.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import javax.ws.rs.core.Response;
 
 /**
  * AdminDelegationResource tests
@@ -34,18 +36,58 @@ public class AdminDelegationResourceTest {
     @Autowired
     private SecurityContextInjector securityContextInjector;
 
+    @Test(expected = InsufficientAuthenticationException.class)
+    public void testGetDelegationsNoEdOrg() throws Exception {
+
+        securityContextInjector.setLeaAdminContext();
+        resource.getDelegations();
+
+    }
+
     @Test
-    public void testGetDelegations() throws Exception {
-        //securityContextInjector.setLeaAdminContext();
-        //resource.getDelegations();
-        //securityContextInjector.setSeaAdminContext();
-        //resource.getDelegations();
-        //securityContextInjector.setOperatorContext();
-        //resource.getDelegations();
+    public void testGetDelegationsBadRole() throws Exception {
+
+        securityContextInjector.setEducatorContext();
+        Assert.assertEquals(resource.getDelegations().getStatus(), Response.Status.FORBIDDEN.getStatusCode());
+
+    }
+
+    @Test
+    public void testGetSingleDelegation() throws Exception {
+
+        securityContextInjector.setLeaAdminContext();
+        ((SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setEdOrg("1234");
+
+        Assert.assertEquals(resource.getSingleDelegation().getStatus(), Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    public void testSetLocalDelegationNoEdOrg() throws Exception {
+
+        securityContextInjector.setLeaAdminContext();
+        Assert.assertEquals(resource.setLocalDelegation(null).getStatus(), Response.Status.BAD_REQUEST.getStatusCode());
+
+    }
+
+    @Test
+    public void testSetLocalDelegationBadRole() throws Exception {
+
+        securityContextInjector.setEducatorContext();
+        Assert.assertEquals(resource.setLocalDelegation(null).getStatus(), Response.Status.FORBIDDEN.getStatusCode());
+
     }
 
     @Test
     public void testSetLocalDelegation() throws Exception {
+
+        securityContextInjector.setLeaAdminContext();
+
+        ((SLIPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).setEdOrg("1234");
+
+        EntityBody body = new EntityBody();
+        body.put(resource.LEA_ID, "1234");
+
+        Assert.assertEquals(resource.setLocalDelegation(body).getStatus(), Response.Status.NO_CONTENT.getStatusCode());
 
     }
 }
