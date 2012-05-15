c2f78d03174fc41314792020ff2042e295bebf45
Jonathan Nanney <jnanney@wgen.net>
2012-05-15 13:21:41 -0400

    Finished test for delegate privileges admin page.

--- sli/acceptance-tests/test/features/admintools/delegate_privileges.feature
+++ sli/acceptance-tests/test/features/admintools/delegate_privileges.feature
@@ -7,10 +7,9 @@ Scenario: District Super Administrator delegates app approval permission to stat
     And I was redirected to the "Simple" IDP Login page
     And I submit the credentials "sunsetadmin" "sunsetadmin1234" for the "Simple" login page
     And I am redirected to the delegation page for my district
-    And Application Authorization is un-checked
-    And I check the Application Authorization
-    And click Save
-    And I log out 
-    And I am authenticated to SLI IDP as user "" with pass "1234"
-    Then I am able to view the applications authorizations page as a state administrator
+    And "Application Authorization" is unchecked
+    And I check the "Application Authorization"
+    And I should click the "Save" button
+    Then I am redirected to the delegation page for my district
+    And "Application Authorization" is checked
   
--- sli/acceptance-tests/test/features/admintools/step_definitions/delegate_privileges_steps.rb
+++ sli/acceptance-tests/test/features/admintools/step_definitions/delegate_privileges_steps.rb
@@ -2,30 +2,36 @@ When /^I hit the delegation url$/ do
   @driver.get(PropLoader.getProps['admintools_server_url'] + "/admin_delegations")
 end
 
-When /^I authenticate with username "([^"]*)" and password "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+When /^I am redirected to the delegation page for my district$/ do
+  assertWithWait("Expected to be on district delegation page") do
+    @driver.page_source.index("Delegate District Privileges") != nil
+  end
 end
 
-When /^I am shown the Delegation configuration for my district$/ do
-  pending # express the regexp above with the code you wish you had
+When /^"([^"]*)" is unchecked$/ do |feature|
+  checkbox = getCheckbox(feature)
+  assert(!checkbox.attribute("checked"), "Expected #{feature} checkbox to be unchecked")
 end
 
-When /^Application Authorization is un\-checked$/ do
-  pending # express the regexp above with the code you wish you had
+When /^I check the "([^"]*)"$/ do |feature|
+  checkbox = getCheckbox(feature)
+  checkbox.click
 end
 
-When /^I check the Application Authorization$/ do
-  pending # express the regexp above with the code you wish you had
+
+Then /^"([^"]*)" is checked$/ do |feature|
+  checkbox = getCheckbox(feature)
+  assert(checkbox.attribute("checked"), "Expected #{feature} checkbox to be checked")
 end
 
-When /^click Save$/ do
-  pending # express the regexp above with the code you wish you had
+
+def getCheckbox(feature)
+    if feature == "Application Authorization"
+    id = "admin_delegation_appApprovalEnabled"
+  else
+    assert(false, "Could not find the ID for #{feature} checkbox")
+  end
+  checkbox = @driver.find_element(:id, id)
+  return checkbox
 end
 
-When /^I am authenticated to SLI IDP as user "([^"]*)" with pass "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I am able to view the applications authorizations page as a state administrator$/ do
-  pending # express the regexp above with the code you wish you had
-end
