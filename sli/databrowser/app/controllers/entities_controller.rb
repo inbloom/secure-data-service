4da3dbfd1b0a7baaf054dd0393abb777fdf10ba5
Ryan Latta <rlatta@wgen.net>
2012-05-17 13:18:20 -0400

    Searching is done, tests, and gem file tweaks.

--- sli/databrowser/app/controllers/entities_controller.rb
+++ sli/databrowser/app/controllers/entities_controller.rb
@@ -4,14 +4,35 @@ class EntitiesController < ApplicationController
   before_filter :set_url
 
   def set_url
+    @search_field = nil
+    case params[:search_type]
+    when /teachers/
+      @search_field = "teacherUniqueStateId"
+    when /students/
+      @search_field = "studentUniqueStateId"
+    when /staff/
+      @search_field = "staffUniqueStateId"
+    when /parents/
+      @search_field = "parentUniqueStateId"
+    when /educationOrganizations/
+      @search_field = "stateOrganizationId"
+    end
+    params[:other] = params[:search_type] if @search_field
     Entity.url_type = params[:other]
+    params.delete(:search_type)
+    params.delete(:utf8)
     Entity.format = ActiveResource::Formats::JsonLinkFormat
   end
 
   # GET /entities/1
   # GET /entities/1.json
   def show
-    @entities = Entity.get("", request.query_parameters)
+    if params[:search_id] && @search_field
+      @entities = Entity.get("", @search_field => params[:search_id]) if params[:search_id]
+      return
+    else
+      @entities = Entity.get("")
+    end
     if @entities.is_a?(Hash)
       tmp = Array.new()
       tmp.push(@entities)
