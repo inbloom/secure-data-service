a4f41fad11cf004532de9bd54080ff2686e28951
Yuan Zhang <yzhang@wgen.net>
2012-05-18 16:46:59 -0400

    add entity filter to the SecurityEventResource

--- sli/api/src/main/java/org/slc/sli/api/resources/security/SecurityEventResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/security/SecurityEventResource.java
@@ -1,5 +1,11 @@
 package org.slc.sli.api.resources.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -17,13 +23,25 @@ import javax.ws.rs.core.UriInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import org.slc.sli.api.config.EntityDefinition;
 import org.slc.sli.api.config.EntityDefinitionStore;
+import org.slc.sli.api.init.RoleInitializer;
 import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.representation.EntityResponse;
+import org.slc.sli.api.representation.ErrorResponse;
 import org.slc.sli.api.resources.Resource;
+import org.slc.sli.api.resources.util.ResourceUtil;
 import org.slc.sli.api.resources.v1.DefaultCrudEndpoint;
+import org.slc.sli.api.security.SLIPrincipal;
+import org.slc.sli.api.service.query.ApiQuery;
+import org.slc.sli.common.constants.ResourceConstants;
 import org.slc.sli.common.constants.v1.ParameterConstants;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
 
 /**
  *
@@ -39,28 +57,116 @@ public class SecurityEventResource extends DefaultCrudEndpoint {
 
     public static final String UUID = "uuid";
     public static final String RESOURCE_NAME = "securityEvent";
+    public static final String[] allowedRoles = {RoleInitializer.SLC_OPERATOR,
+                                                 RoleInitializer.SEA_ADMINISTRATOR,
+                                                 RoleInitializer.LEA_ADMINISTRATOR};
 
+    /* Access to entity definitions */
+    private final EntityDefinitionStore entityDefs;
+
+//    @Autowired
+//    private ClientRoleResolver roleResolver;
 
     @Autowired
     public SecurityEventResource(EntityDefinitionStore entityDefs) {
         super(entityDefs, RESOURCE_NAME);
+        this.entityDefs = entityDefs;
     }
 
     @POST
     public Response createSecurityEvent(EntityBody newSecurityEvent, @Context HttpHeaders headers,
             @Context final UriInfo uriInfo) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            SLIPrincipal principal = (SLIPrincipal) auth.getPrincipal();
+            if (principal != null) {
+                System.out.println("tenant: " + principal.getTenantId());
+                System.out.println("realm: " + principal.getRealm());
+                System.out.println("AdminRealm: " + principal.getAdminRealm());
+                for (String role : principal.getRoles()) {
+                    System.out.println("role: " + role);
+                }
+
+                Set<String> sliRoles = new HashSet<String>(principal.getSliRoles());
+
+                for (String role : sliRoles) {
+                    System.out.println("sliRole: " + role);
+                }
+            }
+        }
         return super.create(newSecurityEvent, headers, uriInfo);
     }
 
     @SuppressWarnings("rawtypes")
     @GET
-    public Response getSecurityEvents(@QueryParam(ParameterConstants.OFFSET)
-            @DefaultValue(ParameterConstants.DEFAULT_OFFSET) final int offset,
+    public Response getSecurityEvents(
+            @QueryParam(ParameterConstants.OFFSET) @DefaultValue(ParameterConstants.DEFAULT_OFFSET) final int offset,
             @QueryParam(ParameterConstants.LIMIT) @DefaultValue(ParameterConstants.DEFAULT_LIMIT) final int limit,
-            @Context
-            HttpHeaders headers, @Context final UriInfo uriInfo) {
-        Response resp = super.readAll(offset, limit, headers, uriInfo);
-        return resp;
+            @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            SLIPrincipal principal = (SLIPrincipal) auth.getPrincipal();
+            if (principal != null) {
+
+                Set<String> sliRoles = new HashSet<String>(principal.getSliRoles());
+
+                if (sliRoles.contains(RoleInitializer.SLC_OPERATOR)) {
+                    return getEntities(offset, limit, uriInfo, principal, true);
+                } else if (sliRoles.contains(RoleInitializer.SEA_ADMINISTRATOR)
+                        || sliRoles.contains(RoleInitializer.LEA_ADMINISTRATOR)) {
+                    return getEntities(offset, limit, uriInfo, principal, false);
+                }
+            }
+        }
+
+        Status errorStatus = Status.FORBIDDEN;
+        return Response
+                .status(errorStatus)
+                .entity(new ErrorResponse(errorStatus.getStatusCode(), Status.FORBIDDEN.getReasonPhrase(),
+                        "You have no permission to see it!")).build();
+    }
+
+    private Response getEntities(final int offset, final int limit, final UriInfo uriInfo, SLIPrincipal principal,
+            boolean isSLCOperator) {
+        EntityDefinition entityDef = entityDefs.lookupByResourceName(RESOURCE_NAME);
+        if (entityDef == null) {
+            return Response
+                    .status(Status.NOT_FOUND)
+                    .entity(new ErrorResponse(Status.NOT_FOUND.getStatusCode(), Status.NOT_FOUND.getReasonPhrase(),
+                            "Invalid resource path: " + RESOURCE_NAME)).build();
+        }
+
+        List<String> targetEdOrgs = Arrays.asList(principal.getEdOrg().split(","));
+        NeutralQuery neutralQuery = new ApiQuery(uriInfo);
+        neutralQuery = addTypeCriteria(entityDef, neutralQuery);
+        neutralQuery.setLimit(limit);
+        neutralQuery.setOffset(offset);
+        if (!isSLCOperator) {
+            neutralQuery.addCriteria(new NeutralCriteria("targetEdOrg", "in", targetEdOrgs));
+        }
+
+        // a new list to store results
+        List<EntityBody> results = new ArrayList<EntityBody>();
+
+        // list all entities matching query parameters and iterate over results
+        for (EntityBody entityBody : entityDef.getService().list(neutralQuery)) {
+            entityBody.put(ResourceConstants.LINKS,
+                    ResourceUtil.getLinks(entityDefs, entityDef, entityBody, uriInfo));
+
+            // add entity to resulting response
+            results.add(entityBody);
+        }
+
+        if (results.isEmpty()) {
+            Status errorStatus = Status.NOT_FOUND;
+            return Response
+                    .status(errorStatus)
+                    .entity(new ErrorResponse(errorStatus.getStatusCode(), Status.NOT_FOUND
+                            .getReasonPhrase(), "Entity not found")).build();
+        } else {
+            return Response.ok(new EntityResponse(entityDef.getType(), results)).build();
+        }
     }
 
     @SuppressWarnings("rawtypes")
