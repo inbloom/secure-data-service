84217fd3d3a3a597b8fd8580af8e7c6b9e51211b
Takashi Osako <tosako@wgen.net>
2012-05-21 12:00:18 -0400

    merge from master to DashbaordDev

--- sli/acceptance-tests/test/features/apiV1/associations/studentDisciplineIncidentAssociation/step_definitions/student_discipline_incident_association_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/associations/studentDisciplineIncidentAssociation/step_definitions/student_discipline_incident_association_steps.rb
@@ -14,14 +14,14 @@ Transform /^<([^"]*)>$/ do |val|
 
   case val
     #student parent association data
-  when "ASSOCIATION COUNT" then 7
+  when "ASSOCIATION COUNT" then 4
   when "ASSOCIATION COUNT FOR ENDPOINT 1" then 1
   when "ASSOCIATION COUNT FOR ENDPOINT 2" then 2
   when "RESOLUTION COUNT FOR ENDPOINT 1" then 1
   when "RESOLUTION COUNT FOR ENDPOINT 2" then 2
   when "ASSOCIATION ID" then "0e26de6c-225b-9f67-9625-5113ad50a03b"
   when "ASSOCIATION ID FOR UPDATE" then "0e26de6c-225b-9f67-9625-5113ad50a03b"
-  when "ASSOCIATION ID FOR DELETE" then "0e26de6c-225b-9f67-9625-5113ad50a03b"
+  when "ASSOCIATION ID FOR DELETE" then $createdId
   when "ASSOCIATION LINK NAME" then "getStudentDisciplineIncidentAssociations"
   when "ASSOCIATION TYPE" then "studentDisciplineIncidentAssociation"
   when "ASSOCIATION URI" then "studentDisciplineIncidentAssociations"
@@ -43,14 +43,14 @@ Transform /^<([^"]*)>$/ do |val|
   when "ENDPOINT2 URI" then "disciplineIncidents"
 
     #update related field data
-  when "UPDATE FIELD" then "studentId"
-  when "UPDATE FIELD EXPECTED VALUE" then "0fb8e0b4-8f84-48a4-b3f0-9ba7b0513dba"
-  when "UPDATE FIELD NEW VALID VALUE" then "e1af7127-743a-4437-ab15-5b0dacd1bde0"
+  when "UPDATE FIELD" then "studentParticipationCode"
+  when "UPDATE FIELD EXPECTED VALUE" then "Reporter"
+  when "UPDATE FIELD NEW VALID VALUE" then "Witness"
 
     #general
   when "INVALID REFERENCE" then "11111111-1111-1111-1111-111111111111"
   when "SELF LINK NAME" then "self"
-  when "NEWLY CREATED ASSOCIATION ID" then @newId
+  when "NEWLY CREATED ASSOCIATION ID" then $createdId = @newId
   when "VALIDATION" then "Validation failed"
   end
 end
