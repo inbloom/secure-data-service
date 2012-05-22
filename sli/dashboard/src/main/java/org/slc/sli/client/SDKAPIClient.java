244033b27e3846cb55b46b9408b5257b350acd58
rbloh <rbloh@wgen.net>
2012-05-22 09:59:40 -0400

    Resolving merge conflicts with SDK integration

--- sli/dashboard/src/main/java/org/slc/sli/client/SDKAPIClient.java
+++ sli/dashboard/src/main/java/org/slc/sli/client/SDKAPIClient.java
@@ -5,11 +5,9 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.slc.sli.api.client.Entity;
 import org.slc.sli.api.client.EntityCollection;
@@ -19,6 +17,8 @@ import org.slc.sli.api.client.impl.BasicQuery;
 import org.slc.sli.entity.ConfigMap;
 import org.slc.sli.entity.GenericEntity;
 import org.slc.sli.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This client will use the SDK client to communicate with the SLI API.
@@ -91,14 +91,33 @@ public class SDKAPIClient implements APIClient{
 
     @Override
     public ConfigMap getEdOrgCustomData(String token, String id) {
-        // TODO Auto-generated method stub
-        return liveApiClient.getEdOrgCustomData(token, id);
+        ConfigMap configMap = null;
+        try {
+            List entityList = new ArrayList();
+            sdkClient.read(entityList, SDK_EDORGS_URL + id + CUSTOM_DATA, ConfigMap.class);
+            if (entityList.size() > 0) {
+                configMap = (ConfigMap) entityList.get(0);
+            }
+        } catch (URISyntaxException e) {
+            LOGGER.error("Exception occurred", e);
+        } catch (MalformedURLException e) {
+            LOGGER.error("Exception occurred", e);
+        }
+        return configMap;
     }
-
+    
     @Override
     public void putEdOrgCustomData(String token, String id, ConfigMap configMap) {
-        // TODO Auto-generated method stub
-        liveApiClient.putEdOrgCustomData(token, id, configMap);
+        try {
+            Map<String, Object> entityMap = new HashMap<String, Object>();
+            entityMap.put("config", configMap.getConfig());
+            Entity configMapEntity = new GenericEntity(entityMap);
+            sdkClient.create(SDK_EDORGS_URL + id + CUSTOM_DATA, configMapEntity);
+        } catch (URISyntaxException e) {
+            LOGGER.error("Exception occurred", e);
+        } catch (MalformedURLException e) {
+            LOGGER.error("Exception occurred", e);
+        }
     }
 
     @Override
