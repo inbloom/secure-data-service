5d091cce169c19890409fa4b724eed0cfe5b42df
Yuan Zhang <yzhang@wgen.net>
2012-05-22 09:33:27 -0400

    populate sliRoles in SAMLFederationResource

--- sli/api/src/main/java/org/slc/sli/api/resources/security/SamlFederationResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/security/SamlFederationResource.java
@@ -34,6 +34,7 @@ import org.springframework.util.LinkedMultiValueMap;
 
 import org.slc.sli.api.security.OauthSessionManager;
 import org.slc.sli.api.security.SLIPrincipal;
+import org.slc.sli.api.security.resolve.ClientRoleResolver;
 import org.slc.sli.api.security.resolve.UserLocator;
 import org.slc.sli.api.security.saml.SamlAttributeTransformer;
 import org.slc.sli.api.security.saml.SamlHelper;
@@ -70,15 +71,18 @@ public class SamlFederationResource {
     @Autowired
     private OauthSessionManager sessionManager;
 
+    @Autowired
+    private ClientRoleResolver roleResolver;
+
     @Value("${sli.security.sp.issuerName}")
     private String metadataSpIssuerName;
 
     @Value("classpath:saml/samlMetadata.xml.template")
     private Resource metadataTemplateResource;
-    
+
     @Value("${sli.api.cookieDomain}")
     private String apiCookieDomain;
-    
+
     @Context
     private HttpServletRequest httpServletRequest;
 
@@ -188,12 +192,13 @@ public class SamlFederationResource {
         } else {
             principal.setName(attributes.getFirst("userName"));
         }
-        
+
         principal.setRoles(attributes.get("roles"));
         principal.setRealm(realm.getEntityId());
         principal.setEdOrg(attributes.getFirst("edOrg"));
         principal.setAdminRealm(attributes.getFirst("edOrg"));
-        
+        principal.setSliRoles(roleResolver.resolveRoles(principal.getRealm(), principal.getRoles()));
+
 
         // {sessionId,redirectURI}
         Pair<String, URI> tuple = this.sessionManager.composeRedirect(inResponseTo, principal);
