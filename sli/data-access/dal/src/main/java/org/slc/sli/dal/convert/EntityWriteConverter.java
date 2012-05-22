f3cac8068c9bf74fa3c632a44cce85dc5c98f55a
Steve Ashton <sashton@wgen.net>
2012-05-22 13:17:08 -0400

    Revert "[US1604] Added prototype scope to encryption beans"
    This reverts commit 3b0b5a8eeeed8f94736466730c2e9504a5ab3c74.

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/EntityWriteConverter.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/EntityWriteConverter.java
@@ -2,42 +2,42 @@ package org.slc.sli.dal.convert;
 
 import com.mongodb.DBObject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+
 import org.slc.sli.common.util.uuid.UUIDGeneratorStrategy;
 import org.slc.sli.dal.encrypt.EntityEncryption;
 import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.MongoEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.convert.converter.Converter;
 
 /**
  * Spring converter registered in the Mongo configuration to convert MongoEntity objects into
  * DBObjects.
- * 
+ *
  */
-
 public class EntityWriteConverter implements Converter<Entity, DBObject> {
-    
+
+    @Autowired(required = false)
+    @Qualifier("entityEncryption")
+    EntityEncryption encrypt;
+
     @Autowired
     UUIDGeneratorStrategy uuidGeneratorStrategy;
-    
-    @Autowired
-    ApplicationContext ctx;
-    
+
     @Override
     public DBObject convert(Entity e) {
         MongoEntity me;
-        
+
         if (e instanceof MongoEntity) {
             me = (MongoEntity) e;
         } else {
             me = new MongoEntity(e.getType(), e.getEntityId(), e.getBody(), e.getMetaData());
         }
-        EntityEncryption encrypt = ctx.getBean("entityEncryption", EntityEncryption.class);
         if (encrypt != null) {
             me.encrypt(encrypt);
         }
         return me.toDBObject(uuidGeneratorStrategy);
     }
-    
+
 }
