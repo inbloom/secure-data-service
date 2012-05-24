9f4dc79d5fa48724359f9a67e98c0ad36afa2ddc
Ryan Latta <rlatta@wgen.net>
2012-05-23 20:53:15 -0400

    Working on UX

--- sli/admin-tools/admin-rails/app/assets/javascripts/apps.js
+++
@@ -1,7 +0,0 @@
-
-function changeCheck(checkboxName, checkVal) {
-    checkboxes = document.getElementsByName(checkboxName);
-    for(var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = checkVal;
-    }
-}
