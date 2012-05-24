85c75cab365b4cccd171ae6ac4e4cd8177893a68
Jon Tully <jtully@wgen.net>
2012-05-24 14:35:39 -0400

    [DE580] Changed ingestion tenant poller to not wait for LZ dir to be created before adding a LZ route.
    This will mean that ingestion/camel will make the LZ directories as routes are added.

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/TenantProcessor.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/TenantProcessor.java
@@ -1,6 +1,5 @@
 package org.slc.sli.ingestion.processors;
 
-import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -13,13 +12,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.Route;
 import org.apache.camel.builder.RouteBuilder;
+import org.slc.sli.ingestion.routes.LandingZoneRouteBuilder;
+import org.slc.sli.ingestion.tenant.TenantDA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import org.slc.sli.ingestion.routes.LandingZoneRouteBuilder;
-import org.slc.sli.ingestion.tenant.TenantDA;
 
 /**
  * Processor for tenant collection polling
@@ -81,7 +79,7 @@ public class TenantProcessor implements Processor {
         for (String lzPath : newLzPaths) {
             if (oldLzPaths.contains(lzPath)) {
                 oldLzPaths.remove(lzPath);
-            } else if (new File(lzPath).exists()) {
+            } else {
                 routesToAdd.add(lzPath);
             }
         }
