464f91fd689d252c5454863f96390d9d3eadf33a
Oleg Krook <okrook@wgen.net>
2012-05-25 12:34:16 -0400

    Fixed a broken unit test

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/referenceresolution/FreeMarkerExtendedReferenceResolverTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/referenceresolution/FreeMarkerExtendedReferenceResolverTest.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.ws.Holder;
+
 import junit.framework.Assert;
 
 import org.apache.commons.io.IOUtils;
@@ -28,6 +30,8 @@ public class FreeMarkerExtendedReferenceResolverTest {
         File result = null;
         try {
             result = referenceFactory.resolve(xpath, content);
+
+            Assert.assertNotNull(result);
 
             String expectedXML = readFromFile(expected);
             String actualXML = readFromFile(result);
@@ -71,20 +75,23 @@ public class FreeMarkerExtendedReferenceResolverTest {
 
         test(input, expected, "/InterchangeStudentParent/StudentParentAssociation/StudentReference");
 
+        final Holder<Boolean> exceptionThrown = new Holder<Boolean>(Boolean.FALSE);
+
         Runnable run = new Runnable() {
             @Override
             public void run() {
                 try {
-                    test(input, expected, "/InterchangeStudentAssessment/StudentAssessment/AssessmentReference");
-                } catch (IOException e) {
-                    new RuntimeException(e);
+                    test(input, expected, "/InterchangeStudentParent/StudentParentAssociation/StudentReference2");
+                } catch (Throwable t) {
+                    exceptionThrown.value = Boolean.TRUE;
+                    throw new RuntimeException(t);
                 }
             }
         };
 
-        Thread th1 = new Thread(run);
+        Thread th1 = new Thread(run, "Thread1");
 
-        Thread th2 = new Thread(run);
+        Thread th2 = new Thread(run, "Thread2");
 
         th1.start();
         th2.start();
@@ -93,13 +100,17 @@ public class FreeMarkerExtendedReferenceResolverTest {
             th1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Assert.fail();
         }
 
         try {
             th2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Assert.fail();
         }
+
+        Assert.assertFalse(exceptionThrown.value);
     }
 
     @Test
