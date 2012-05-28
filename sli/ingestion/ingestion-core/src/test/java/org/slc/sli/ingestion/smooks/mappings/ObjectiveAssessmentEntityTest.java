b8ab94f0da16d8114262c549bc199ad2b4a7257e
Stan Halka <shalka@wgen.net>
2012-05-28 14:12:52 +0100

    fixed objective assessment mapping and unit test

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/ObjectiveAssessmentEntityTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/ObjectiveAssessmentEntityTest.java
@@ -103,22 +103,19 @@ public class ObjectiveAssessmentEntityTest {
         Assert.assertEquals("50", entity.get("percentOfAssessment").toString());
         Assert.assertEquals("nomenclature", entity.get("nomenclature"));
 
-        List<?> assessmentPerformanceLevelList = (List<?>) entity.get("assessmentPerformanceLevel");
+        @SuppressWarnings("unchecked")
+        List<Map<String, Object>> assessmentPerformanceLevelList = (List<Map<String, Object>>) entity.get("assessmentPerformanceLevel");
         Assert.assertTrue(assessmentPerformanceLevelList != null);
-        Map<?, ?> assessmentPerformanceLevel = (Map<?, ?>) assessmentPerformanceLevelList.get(0);
+        Map<String, Object> assessmentPerformanceLevel = assessmentPerformanceLevelList.get(0);
         Assert.assertTrue(assessmentPerformanceLevel != null);
         Assert.assertEquals("ACT score", assessmentPerformanceLevel.get("assessmentReportingMethod"));
         Assert.assertEquals("1", assessmentPerformanceLevel.get("minimumScore").toString());
         Assert.assertEquals("20", assessmentPerformanceLevel.get("maximumScore").toString());
 
-        //Map<?, ?> performanceLevel = (Map<?, ?>) assessmentPerformanceLevel.get("performanceLevel");
-        List<?> performanceLevelList = (List<?>) assessmentPerformanceLevel.get("performanceLevelDescriptor");
-        Assert.assertTrue(performanceLevelList != null);
-        Map<?, ?> performanceLevel = (Map<?, ?>) performanceLevelList.get(0);
+        @SuppressWarnings("unchecked")
+        Map<String, Object> performanceLevel = (Map<String, Object>) assessmentPerformanceLevel.get("performanceLevelDescriptor");
         Assert.assertTrue(performanceLevel != null);
         Assert.assertEquals("description", performanceLevel.get("description"));
-        performanceLevel = (Map<?,?>) performanceLevelList.get(1);
-        Assert.assertTrue(performanceLevel != null);
         Assert.assertEquals("codevalue", performanceLevel.get("codeValue"));
 
         List<?> subObjectiveAssessments = (List<?>) entity.get(ObjectiveAssessmentBuilder.SUB_OBJECTIVE_REFS);
