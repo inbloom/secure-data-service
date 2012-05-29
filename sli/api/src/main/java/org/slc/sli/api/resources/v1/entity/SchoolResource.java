7dcc400a4eb38fde72a2a7860b11def082b4e97b
Stan Halka <shalka@wgen.net>
2012-05-29 15:40:16 +0100

    reverting previous deletion of school session association resource

--- sli/api/src/main/java/org/slc/sli/api/resources/v1/entity/SchoolResource.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/v1/entity/SchoolResource.java
@@ -20,23 +20,23 @@ import org.springframework.stereotype.Component;
 /**
  * This entity represents an educational organization that includes staff and students who
  * participate in classes and educational activity groups.
- * 
+ *
  * For more information, see the schema for $$School$$ resources.
- * 
+ *
  * @author srupasinghe
  * @author kmyers
- * 
+ *
  */
 @Path(PathConstants.V1 + "/" + PathConstants.SCHOOLS)
 @Component
 @Scope("request")
 public class SchoolResource extends DefaultCrudResource {
-    
+
     @Autowired
     public SchoolResource(EntityDefinitionStore entityDefs) {
         super(entityDefs, ResourceNames.SCHOOLS);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
@@ -47,7 +47,7 @@ public class SchoolResource extends DefaultCrudResource {
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
         return super.read(ResourceNames.TEACHER_SCHOOL_ASSOCIATIONS, "schoolId", schoolId, headers, uriInfo);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
@@ -60,7 +60,7 @@ public class SchoolResource extends DefaultCrudResource {
         return super.read(ResourceNames.TEACHER_SCHOOL_ASSOCIATIONS, "schoolId", schoolId, "teacherId",
                 ResourceNames.TEACHERS, headers, uriInfo);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
@@ -71,7 +71,7 @@ public class SchoolResource extends DefaultCrudResource {
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
         return super.read(ResourceNames.STUDENT_SCHOOL_ASSOCIATIONS, "schoolId", schoolId, headers, uriInfo);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
@@ -84,7 +84,7 @@ public class SchoolResource extends DefaultCrudResource {
         return super.read(ResourceNames.STUDENT_SCHOOL_ASSOCIATIONS, "schoolId", schoolId, "studentId",
                 ResourceNames.STUDENTS, headers, uriInfo);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
@@ -95,15 +95,29 @@ public class SchoolResource extends DefaultCrudResource {
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
         return super.read(ResourceNames.SECTIONS, "schoolId", schoolId, headers, uriInfo);
     }
-    
+
     /**
      * Returns the requested collection of resources that are associated with the specified
      * resource.
      */
     @GET
-    @Path("{" + ParameterConstants.SCHOOL_ID + "}" + "/" + PathConstants.SESSIONS)
-    public Response getSessionsForSchool(@PathParam(ParameterConstants.SCHOOL_ID) final String schoolId,
+    @Path("{" + ParameterConstants.SCHOOL_ID + "}" + "/" + PathConstants.SCHOOL_SESSION_ASSOCIATIONS)
+    public Response getSchoolSessionAssociations(@PathParam(ParameterConstants.SCHOOL_ID) final String schoolId,
             @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
-        return super.read(ResourceNames.SESSIONS, "schoolId", schoolId, headers, uriInfo);
+        return super.read(ResourceNames.SCHOOL_SESSION_ASSOCIATIONS, "schoolId", schoolId, headers, uriInfo);
     }
+
+    /**
+     * Returns the requested collection of resources that are associated with the specified
+     * resource.
+     */
+    @GET
+    @Path("{" + ParameterConstants.SCHOOL_ID + "}" + "/" + PathConstants.SCHOOL_SESSION_ASSOCIATIONS + "/"
+            + PathConstants.SESSIONS)
+    public Response getSchoolSessionAssociationSessions(@PathParam(ParameterConstants.SCHOOL_ID) final String schoolId,
+            @Context HttpHeaders headers, @Context final UriInfo uriInfo) {
+        return super.read(ResourceNames.SCHOOL_SESSION_ASSOCIATIONS, "schoolId", schoolId, "sessionId",
+                ResourceNames.SESSIONS, headers, uriInfo);
+    }
+
 }
--- sli/api/src/test/java/org/slc/sli/api/resources/v1/entity/SchoolResourceTest.java
+++ sli/api/src/test/java/org/slc/sli/api/resources/v1/entity/SchoolResourceTest.java
@@ -12,17 +12,11 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slc.sli.api.representation.EntityBody;
-import org.slc.sli.api.resources.SecurityContextInjector;
-import org.slc.sli.api.resources.util.ResourceTestUtil;
-import org.slc.sli.api.resources.v1.HypermediaType;
-import org.slc.sli.api.resources.v1.association.StudentSchoolAssociationResource;
-import org.slc.sli.api.resources.v1.association.TeacherSchoolAssociationResource;
-import org.slc.sli.api.test.WebContextTestExecutionListener;
-import org.slc.sli.common.constants.v1.ParameterConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -30,7 +24,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
-import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.resources.SecurityContextInjector;
+import org.slc.sli.api.resources.util.ResourceTestUtil;
+import org.slc.sli.api.resources.v1.HypermediaType;
+import org.slc.sli.api.resources.v1.association.SchoolSessionAssociationResource;
+import org.slc.sli.api.resources.v1.association.StudentSchoolAssociationResource;
+import org.slc.sli.api.resources.v1.association.TeacherSchoolAssociationResource;
+import org.slc.sli.api.test.WebContextTestExecutionListener;
+import org.slc.sli.common.constants.v1.ParameterConstants;
 
 /**
  * JUnit for school resources
@@ -50,6 +52,7 @@ public class SchoolResourceTest {
     private final String studentResourceName = "StudentResource";
     private final String sectionResourceName = "SectionResource";
     private final String teacherSchoolAssociationResourceName = "TeacherSchoolAssociationResource";
+    private final String schoolSessionAssociationResourceName = "SchoolSessionAssociationResource";
     private final String studentSchoolAssociationResourceName = "StudentSchoolAssociationResource";
 
     @Autowired
@@ -66,6 +69,8 @@ public class SchoolResourceTest {
     private SectionResource sectionResource;
     @Autowired
     private TeacherSchoolAssociationResource teacherSchoolAssociationResource;
+    @Autowired
+    private SchoolSessionAssociationResource schoolSessionAssociationResource;
     @Autowired
     private StudentSchoolAssociationResource studentSchoolAssociationResource;
 
@@ -122,18 +127,37 @@ public class SchoolResourceTest {
     }
 
     @Test
-    public void testGetSchoolSessions() {
+    public void testGetSchoolSessionAssociations() {
         Response createResponse = schoolResource.create(new EntityBody(
                 ResourceTestUtil.createTestEntity(schoolResourceName)), httpHeaders, uriInfo);
         String schoolId = ResourceTestUtil.parseIdFromLocation(createResponse);
-        
-        Map<String, Object> map = ResourceTestUtil.createTestEntity(sessionResourceName);
-        map.put(ParameterConstants.SCHOOL_ID, schoolId);
-        sessionResource.create(new EntityBody(map), httpHeaders, uriInfo); 
-        
-        Response response = schoolResource.getSessionsForSchool(schoolId, httpHeaders, uriInfo);        
-        EntityBody body = ResourceTestUtil.assertions(response);
-        assertEquals("School IDs should equal", schoolId, body.get(ParameterConstants.SCHOOL_ID));
+        createResponse = sessionResource.create(new EntityBody(
+                ResourceTestUtil.createTestEntity(sessionResourceName)), httpHeaders, uriInfo);
+        String sessionId = ResourceTestUtil.parseIdFromLocation(createResponse);
+
+        Map<String, Object> map = ResourceTestUtil.createTestAssociationEntity(
+                schoolSessionAssociationResourceName, schoolResourceName, schoolId, sessionResourceName, sessionId);
+        schoolSessionAssociationResource.create(new EntityBody(map), httpHeaders, uriInfo);
+
+        Response response = schoolResource.getSchoolSessionAssociations(schoolId, httpHeaders, uriInfo);
+        ResourceTestUtil.assertions(response);
+    }
+
+    @Test
+    public void testGetSchoolSessionAssociationSessions() {
+        Response createResponse = schoolResource.create(new EntityBody(
+                ResourceTestUtil.createTestEntity(schoolResourceName)), httpHeaders, uriInfo);
+        String schoolId = ResourceTestUtil.parseIdFromLocation(createResponse);
+        createResponse = sessionResource.create(new EntityBody(
+                ResourceTestUtil.createTestEntity(sessionResourceName)), httpHeaders, uriInfo);
+        String sessionId = ResourceTestUtil.parseIdFromLocation(createResponse);
+
+        Map<String, Object> map = ResourceTestUtil.createTestAssociationEntity(
+                schoolSessionAssociationResourceName, schoolResourceName, schoolId, sessionResourceName, sessionId);
+        schoolSessionAssociationResource.create(new EntityBody(map), httpHeaders, uriInfo);
+
+        Response response = schoolResource.getSchoolSessionAssociationSessions(schoolId, httpHeaders, uriInfo);
+        ResourceTestUtil.assertions(response);
     }
 
     @Test
