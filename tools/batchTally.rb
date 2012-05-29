559bcce7acdd22f4b79c37e7b67b9dfb4ac23ea7
Danilo Duran <dduran@wgen.net>
2012-05-29 15:38:48 -0400

    improve execution stats aggregation. code cleanup.

--- tools/batchTally.rb
+++ tools/batchTally.rb
@@ -20,89 +20,130 @@ rec=coll.find("_id"=>id)
 
 job = rec.to_a[0]
 
-# earliest time on a chunk
-earliest=9999999999
+# pitProcessingStartTime time on a chunk
+pitProcessingStartTime=9999999999
 
 # Time the job ended
-endTime=0
+jobProcessingEndTime=0
 
 #times elapsed
-out = {}
+pitElapsedPerResource = {}
 
 # Record Counts
-rc = {}
+rcPerResource = {}
 
 #total job time
 jobStart = job["jobStartTimestamp"]
 jobEnd = job["jobStopTimestamp" ]
 if ! jobEnd.nil? && !jobStart.nil?  
-	jobTime = jobEnd-jobStart
+	totalJobTime = jobEnd-jobStart
 end
 # Record Counts for stage
-rcStage={"TransformationProcessor" => 0,  "PersistenceProcessor"=>0 }
+rcStage={
+  "ZipFileProcessor" => 0,  "ControlFilePreProcessor"=>0, "ControlFileProcessor" => 0,  "XmlFileProcessor"=>0,
+  "EdFiProcessor" => 0, "TransformationProcessor" => 0,  "PersistenceProcessor"=>0
+}
 
+cpuProcessingTime=0
 job["stages"].each do |stage|
   if stage["stageName"] == "TransformationProcessor" or stage["stageName"] == "PersistenceProcessor"
+    # Pit nodes
     stage["chunks"].each do |chunk|
-      earliest=chunk["startTimestamp"].to_i unless chunk["startTimestamp"].to_i>earliest
+      pitProcessingStartTime=chunk["startTimestamp"].to_i unless chunk["startTimestamp"].to_i>pitProcessingStartTime
       chunk["metrics"].each do |metric|
 
-        out[metric["resourceId"]]=0 unless out[metric["resourceId"]]
-        out[metric["resourceId"]]+=chunk["elapsedTime"] unless chunk.nil?
+        pitElapsedPerResource[metric["resourceId"]]=0 unless pitElapsedPerResource[metric["resourceId"]]
+        pitElapsedPerResource[metric["resourceId"]]+=chunk["elapsedTime"] unless chunk.nil?
 
-        rc[metric["resourceId"]]=0 unless rc[metric["resourceId"]]
-        rc[metric["resourceId"]]+=metric["recordCount"] unless metric.nil? or metric["recordCount"].nil?
+        rcPerResource[metric["resourceId"]]=0 unless rcPerResource[metric["resourceId"]]
+        rcPerResource[metric["resourceId"]]+=metric["recordCount"] unless metric.nil? or metric["recordCount"].nil?
 
         rcStage[stage["stageName"]]+=metric["recordCount"]
-
       end
-    end
+    end 
   elsif stage["stageName"]=="JobReportingProcessor"
-    endTime = stage["chunks"][0]["stopTimestamp"].to_i
-  end
-end
+    # Job reporting
+    jobProcessingEndTime = stage["chunks"][0]["stopTimestamp"].to_i
+  else
+    # Maestro processing nodes - currently single threaded so add all stage time
+    cpuProcessingTime += stage["chunks"][0]["elapsedTime"].to_i
 
-sum=0
-out.each do |key,value|
-  rps = "N/A"
-  rps = rc[key] / (value / 1000.0) unless value == 0
-  puts "[\e[31m#{rc[key]}\e[0m] #{key} => \e[32m#{value}\e[0m ms (\e[35m#{rps.round() unless rps=="N/A"}\e[0m rps)"
-  sum+=value
-end
-
-maestroMongoTime=0
-maestroMongoCalls=0
-mongoCalls=0
-mongoTime=0
-if !job["executionStats"].nil? 
-  job["executionStats"].each do |hostName,value|
-    value.each do |functionName,innerValue|
-      if hostName=="nxmaestro"
-        maestroMongoCalls+=innerValue["left"]
-        maestroMongoTime+=innerValue["right"]
+    # Record counts for Maestro processing nodes
+    stage["chunks"].each do |chunk|
+      chunk["metrics"].each do |metric|
+        rcStage[stage["stageName"]]+=metric["recordCount"]
       end
-      mongoCalls+=innerValue["left"]
-      mongoTime+=innerValue["right"]
     end
   end
 end
 
-transformed = rcStage["TransformationProcessor"]
-wallClock = (endTime-earliest)
+pitElapsedPerResource.each do |key,value|
+  rps = "N/A"
+  rps = rcPerResource[key] / (value / 1000.0) unless value == 0
+  puts "[\e[31m#{rcPerResource[key]}\e[0m] #{key} => \e[32m#{value}\e[0m ms (\e[35m#{rps.round() unless rps=="N/A"}\e[0m rps)"
+  cpuProcessingTime+=value
+end
+
+executionStats = {}
+
+if !job["executionStats"].nil? 
+  job["executionStats"].each do |hostName,value| 
+    nodeType = "pit"
+    if hostName=="nxmaestro"
+      nodeType = "maestro"
+    end
+
+    if executionStats[nodeType].nil?
+      executionStats[nodeType] = {}
+    end
+
+    value.each do |functionName,innerValue|
+      if executionStats[nodeType][functionName].nil?
+        executionStats[nodeType][functionName] = {}
+      end
+
+      executionStats[nodeType][functionName]["calls"] = 0 unless executionStats[nodeType][functionName]["calls"]
+      executionStats[nodeType][functionName]["calls"] += innerValue["left"]
+
+      executionStats[nodeType][functionName]["time"] = 0 unless executionStats[nodeType][functionName]["time"]
+      executionStats[nodeType][functionName]["time"] += innerValue["right"]
+    end
+  end
+end
+
+transformedRecordCount = rcStage["TransformationProcessor"]
+persistedRecordCount = rcStage["PersistenceProcessor"]
+edfiRecordCount = rcStage["EdFiProcessor"]
+wallClockForPits = (jobProcessingEndTime-pitProcessingStartTime)
+
 puts "---------------------------"
-puts "Total records for Transformation: #{rcStage["TransformationProcessor"]}"
-puts "Total records for Persistence: #{rcStage["PersistenceProcessor"]}"
-puts "Total wall-clock time: #{wallClock}sec"
-puts "Total time spent (on all nodes): #{sum/1000} sec"
-puts "Transformed and Persist RPS (transformed per total time)  #{(transformed / wallClock )}"
-puts "Mongo calls (ALL): #{mongoCalls} took #{mongoTime/1000} secs"
-puts "Mongo calls (MAESTRO): #{maestroMongoCalls} took #{maestroMongoTime/1000} secs"
+puts "Total records for Transformation: #{transformedRecordCount}"
+puts "Total records for Persistence: #{persistedRecordCount}"
+puts "Total wall-clock time: #{wallClockForPits}sec"
+
+puts ""
+puts "Total time spent (on all nodes): #{cpuProcessingTime/1000} sec"
+puts "Transformed and Persist RPS (transformed per total time)  #{(transformedRecordCount / wallClockForPits )}"
+
+puts ""
+executionStats.each do |nodeType,functions|
+  functions.each do |functionName,stats|
+    callStats = stats["calls"]
+    timeStats = stats["time"]
+    puts "(#{nodeType}) Mongo #{functionName} calls: #{callStats} took #{timeStats/1000} secs"
+  end
+end
+
+#puts "Mongo calls (ALL): #{mongoCalls} took #{mongoTime/1000} secs"
+
+puts ""
 puts "Job started: #{jobStart.getlocal}"
 if ! jobEnd.nil? 
 puts "Job ended: #{jobEnd.getlocal}"
 end 
-if  !jobTime.nil?  
-	puts "Job time #{jobTime} sec"
-	puts "Job RPS #{transformed / jobTime}" 
+if  !totalJobTime.nil?  
+	puts "Total Job time #{totalJobTime} sec"
+	puts "Job RPS #{edfiRecordCount / totalJobTime}" 
 end
+
 puts "ALL DONE"
