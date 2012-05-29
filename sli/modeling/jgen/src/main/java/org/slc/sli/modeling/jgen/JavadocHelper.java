311344f3d8fddf5111094e391d64c898c1e89602
Takashi Osako <tosako@wgen.net>
2012-05-29 15:11:29 -0400

    Merege from master to DashboardDev

---
+++ sli/modeling/jgen/src/main/java/org/slc/sli/modeling/jgen/JavadocHelper.java
@@ -0,0 +1,22 @@
+package org.slc.sli.modeling.jgen;
+
+import java.io.IOException;
+
+import org.slc.sli.modeling.uml.ModelElement;
+import org.slc.sli.modeling.uml.TagDefinition;
+import org.slc.sli.modeling.uml.TaggedValue;
+import org.slc.sli.modeling.uml.index.ModelIndex;
+
+public final class JavadocHelper {
+
+    public static final void writeJavadoc(final ModelElement element, final ModelIndex model, final JavaStreamWriter jsw)
+            throws IOException {
+        for (final TaggedValue taggedValue : element.getTaggedValues()) {
+            final TagDefinition tagDefinition = model.getTagDefinition(taggedValue.getTagDefinition());
+            if (tagDefinition.getName().equals("documentation")) {
+                jsw.writeComment(taggedValue.getValue());
+            }
+        }
+    }
+
+}
