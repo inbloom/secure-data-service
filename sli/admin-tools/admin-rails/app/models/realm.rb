2e2c01ae604126fc0e2344afd414d8a121d1a4bf
Ryan Latta <rlatta@wgen.net>
2012-05-31 14:42:30 -0400

    Realm management validation and tests
    Conflicts:
    
    	sli/api/src/main/java/org/slc/sli/api/resources/security/RealmRoleManagerResource.java

--- sli/admin-tools/admin-rails/app/models/realm.rb
+++ sli/admin-tools/admin-rails/app/models/realm.rb
@@ -5,11 +5,21 @@ class Realm < SessionResource
   validates_length_of :uniqueIdentifier, :minimum => 5, :message => "must be at least 5 characters long"
   validates_presence_of :name, :message => "can't be blank"
   validate :idp_and_redirect_cannot_be_blank
+  validate :defaults_on_save
+
+  def defaults_on_save
+    if mappings.nil?
+      self.mappings = {}
+      self.mappings["role"] = [{"sliRoleName" => "IT Administrator", "clientRoleName" => [ "IT Administrator" ] }, {"sliRoleName" => "Educator", "clientRoleName" => [ "Educator" ] }, {"sliRoleName" => "Aggregate Viewer", "clientRoleName" => [ "Aggregate Viewer" ] }, {"sliRoleName" => "Leader", "clientRoleName" => [ "Leader" ] }]
+    end
+    self.admin = false
+    #Default saml mapping
+    self.saml =  { "field" => [ { "clientName" => "roles", "sliName" => "roles", "transform" => "(.+)" }, { "clientName" => "userId", "sliName" => "userId", "transform" => "(.+)" }, { "clientName" => "userName", "sliName" => "userName", "transform" => "(.+)" } ] } if saml.nil?
+  end
    
   def idp_and_redirect_cannot_be_blank
     idp.valid?
   end
-  
   schema do
     string "uniqueIdentifier", "name", "edOrg"
     string "saml", "mappings"
