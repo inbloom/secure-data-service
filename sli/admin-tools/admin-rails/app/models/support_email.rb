75df037d1e9bc7bfa8058980a4226e52301393d0
Ryan Latta <rlatta@wgen.net>
2012-06-01 16:55:04 -0400

    Unit testing for mail is done

--- sli/admin-tools/admin-rails/app/models/support_email.rb
+++ sli/admin-tools/admin-rails/app/models/support_email.rb
@@ -1,4 +1,4 @@
 class SupportEmail < SessionResource
   self.collection_name = "email"
-  self.site = "#{APP_CONFIG['api_base']}/v1/support/"
+  self.site = "#{APP_CONFIG['api_base']}/v1/system/support/"
 end
