f7e592179d78eafbdceea044105acb47582a69c5
Nick Brown <nbrown@wgen.net>
2012-06-01 08:07:55 -0400

    concurrency bug in MongoTrackingAspect causing NPEs

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/aspect/MongoTrackingAspect.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/aspect/MongoTrackingAspect.java
@@ -11,20 +11,20 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
- * 
+ *
  * @author dkornishev
- * 
+ *
  */
 @Aspect
 public class MongoTrackingAspect {
-    
+
     private Map<String, Pair<AtomicLong, AtomicLong>> stats = new HashMap<String, Pair<AtomicLong, AtomicLong>>();
-    
+
     @Around("call(* org.springframework.data.mongodb.core.MongoTemplate.*(..)) && !this(MongoTrackingAspect) && !within(org..*Test)")
     public Object track(ProceedingJoinPoint pjp) throws Throwable {
-        
+
         MongoTemplate mt = (MongoTemplate) pjp.getTarget();
-        
+
         String collection = "UNKNOWN";
         Object[] args = pjp.getArgs();
         if (args.length > 0 && args[0] instanceof String) {
@@ -34,34 +34,35 @@ public class MongoTrackingAspect {
         } else if (args.length > 2 && args[2] instanceof String) {
             collection = (String) args[2];
         }
-        
+
         if (collection.lastIndexOf("_") > -1) {
             collection = collection.substring(0, collection.lastIndexOf("_"));
         }
-        
+
         String key = String.format("%s#%s#%s", mt.getDb().getName(), pjp.getSignature().getName(), collection);
-        
+
         if (stats.get(key) == null) {
             stats.put(key, Pair.of(new AtomicLong(0), new AtomicLong(0)));
         }
-        
+
         long start = System.currentTimeMillis();
         Object result = pjp.proceed();
         long elapsed = System.currentTimeMillis() - start;
-        
+
         Pair<AtomicLong, AtomicLong> pair = stats.get(key);
-        pair.getLeft().incrementAndGet();
-        pair.getRight().addAndGet(elapsed);
-        
+        if (pair != null) {
+            pair.getLeft().incrementAndGet();
+            pair.getRight().addAndGet(elapsed);
+        }
         return result;
     }
-    
+
     public Map<String, Pair<AtomicLong, AtomicLong>> getStats() {
         return this.stats;
     }
-    
+
     public void reset() {
         this.stats = new HashMap<String, Pair<AtomicLong, AtomicLong>>();
     }
-    
+
 }
