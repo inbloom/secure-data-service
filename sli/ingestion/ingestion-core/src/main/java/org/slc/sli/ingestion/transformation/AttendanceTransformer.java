e1ac4b90ab953b68818a35ffc960b8591b10b5db
Nupur Pandey <npandey@wgen.net>
2012-06-04 13:41:02 -0400

    Modified Attendance transformer to uniquely stamp each record in the collection.

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/AttendanceTransformer.java
@@ -249,6 +249,7 @@ public class AttendanceTransformer extends AbstractTransformationStrategy {
         // --> augment transformer so that the file of each attendance event is
         // carried through to this stage?
         record.setSourceFile(collections.get(EntityNames.ATTENDANCE).values().iterator().next().getSourceFile());
+        record.setLocationInSourceFile(collections.get(EntityNames.ATTENDANCE).values().iterator().next().getLocationInSourceFile());
         return record;
     }
 
