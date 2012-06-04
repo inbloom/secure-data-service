8230e62e19fe874c3f9c614c389324c9e3e40fcc
smelody@wgen.net <smelody@wgen.net>
2012-06-04 11:21:41 -0400

    4 shards, uses config for directory

--- sli/config/scripts/sharding/start-shard.sh
+++ sli/config/scripts/sharding/start-shard.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
+
+M=/storage/mongo-shard
+
 rm -rf data logs
-mkdir -p data/db/a
-mkdir -p data/db/b
-mkdir -p data/db/config
-mkdir -p logs
+mkdir -p $M/data/db/a
+mkdir -p $M/data/db/b
+mkdir -p $M/data/db/c
+mkdir -p $M/data/db/d
+mkdir -p $M/data/db/config
+mkdir -p $M/logs
 
 if [ -z $1 ]; then
     mongos_port=27017
@@ -27,19 +32,30 @@ wait_for_mongo() {
 }
 
 echo "Starting up shard servers..."
-mongod --shardsvr --dbpath data/db/a --port 10001 > logs/sharda.log &
+mongod --shardsvr --dbpath $M/data/db/a --port 10001 > $M/logs/sharda.log &
 echo $! >> pids
-mongod --shardsvr --dbpath data/db/b --port 10002 > logs/shardb.log &
+mongod --shardsvr --dbpath $M/data/db/b --port 10002 > $M/logs/shardb.log &
 echo $! >> pids
+
+mongod --shardsvr --dbpath $M/data/db/c --port 10003 > $M/logs/shardc.log &
+echo $! >> pids
+mongod --shardsvr --dbpath $M/data/db/d --port 10004 > $M/logs/shardd.log &
+echo $! >> pids
+
 wait_for_mongo 10001
 wait_for_mongo 10002
+wait_for_mongo 10003
+wait_for_mongo 10004
 
 echo "Setting chunk size on shard servers..."
 mongo config --port 10001 --eval 'db.settings.save({"_id":"chunksize", "value":1});'
 mongo config --port 10002 --eval 'db.settings.save({"_id":"chunksize", "value":1});'
+mongo config --port 10003 --eval 'db.settings.save({"_id":"chunksize", "value":1});'
+mongo config --port 10004 --eval 'db.settings.save({"_id":"chunksize", "value":1});'
+
 
 echo "Starting up config server..."
-mongod --configsvr --dbpath data/db/config --port 20000 > logs/configdb.log &
+mongod --configsvr --dbpath $M/data/db/config --port 20000 > $M/logs/configdb.log &
 echo $! >> pids
 wait_for_mongo 20000
 
@@ -47,12 +63,15 @@ echo "Setting chunk size on conig server..."
 mongo config --port 20000 --eval 'db.settings.save({"_id":"chunksize", "value":1});'
 
 echo "Starting up mongos router..."
-mongos --configdb localhost:20000 --port $mongos_port> logs/mongos.log &
+mongos --configdb localhost:20000 --port $mongos_port> $M/logs/mongos.log &
 echo $! >> pids
 wait_for_mongo $mongos_port
 
 echo "Adding shards to cluster..."
 mongo admin --port $mongos_port --eval 'db.runCommand( { addshard : "localhost:10001" } )'
 mongo admin --port $mongos_port --eval 'db.runCommand( { addshard : "localhost:10002" } )'
+mongo admin --port $mongos_port --eval 'db.runCommand( { addshard : "localhost:10003" } )'
+mongo admin --port $mongos_port --eval 'db.runCommand( { addshard : "localhost:10004" } )'
+
 
 echo "mongos is running on port $mongos_port"
