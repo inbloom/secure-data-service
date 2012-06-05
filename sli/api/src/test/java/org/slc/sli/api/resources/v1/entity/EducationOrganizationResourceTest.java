37f23bdaed008470511a6da962695f213dda8c6d
Mark Abernathy <mabernathy@wgen.net>
2012-06-05 09:58:40 -0400

    Fixed checkstyle

--- sli/api/src/test/java/org/slc/sli/api/resources/v1/entity/EducationOrganizationResourceTest.java
+++ sli/api/src/test/java/org/slc/sli/api/resources/v1/entity/EducationOrganizationResourceTest.java
@@ -22,10 +22,6 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slc.sli.api.client.constants.ResourceConstants;
-import org.slc.sli.api.client.constants.ResourceNames;
-import org.slc.sli.api.client.constants.v1.ParameterConstants;
-import org.slc.sli.api.representation.EntityResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -33,7 +29,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
+import org.slc.sli.api.client.constants.ResourceConstants;
+import org.slc.sli.api.client.constants.ResourceNames;
+import org.slc.sli.api.client.constants.v1.ParameterConstants;
 import org.slc.sli.api.representation.EntityBody;
+import org.slc.sli.api.representation.EntityResponse;
 import org.slc.sli.api.resources.SecurityContextInjector;
 import org.slc.sli.api.resources.util.ResourceTestUtil;
 import org.slc.sli.api.resources.v1.HypermediaType;
@@ -160,7 +160,7 @@ public class EducationOrganizationResourceTest {
         }
     }
 
-    @Test
+    @Test(expected = EntityNotFoundException.class)
     public void testDelete() {
         //create one entity
         Response createResponse = edOrgResource.create(new EntityBody(createTestEntity()), httpHeaders, uriInfo);
@@ -170,15 +170,8 @@ public class EducationOrganizationResourceTest {
         Response response = edOrgResource.delete(id, httpHeaders, uriInfo);
         assertEquals("Status code should be NO_CONTENT", Status.NO_CONTENT.getStatusCode(), response.getStatus());
 
-        try {
-            @SuppressWarnings("unused")
-            Response getResponse = edOrgResource.read(id, httpHeaders, uriInfo);
-            fail("should have thrown EntityNotFoundException");
-        } catch (EntityNotFoundException e) {
-            return;
-        } catch (Exception e) {
-            fail("threw wrong exception: " + e);
-        }
+        @SuppressWarnings("unused")
+        Response getResponse = edOrgResource.read(id, httpHeaders, uriInfo);
     }
 
     @Test
