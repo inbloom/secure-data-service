c6e85c9d4f46805a47d5903c03f758ebdb2bfd39
Joe Chung <joechung@wgen.net>
2012-06-05 14:25:59 -0400

    Merge branch 'emailer' into UX-improvement

---
+++ sli/admin-tools/admin-rails/config/initializers/mailer_setup.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => APP_CONFIG['email_host'],
+  :port                 => APP_CONFIG['email_port'],
+  :domain               => APP_CONFIG['email_replace_uri'],
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = APP_CONFIG["email_replace_uri"].gsub(/http[s]*:\/\//, '')
+# Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
