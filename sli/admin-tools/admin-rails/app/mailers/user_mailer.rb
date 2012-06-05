0075aa86aa2a213b12e07b18b8f0f125fea66d20
Joe Chung <joechung@wgen.net>
2012-06-05 15:44:45 -0400

    emailer refactoring and fixing registration form

--- sli/admin-tools/admin-rails/app/mailers/user_mailer.rb
+++ sli/admin-tools/admin-rails/app/mailers/user_mailer.rb
@@ -3,47 +3,13 @@ require 'approval'
 class UserMailer < ActionMailer::Base
   default from: APP_CONFIG["email_sender_address"]
 
-EMAIL_SUBJECT_PROD = "SLC Developer Account - Email Confirmation"
-EMAIL_SUBJECT_SANDBOX = "SLC Sandbox Developer Account - Email Confirmation"
+  EMAIL_SUBJECT_PROD = "Welcome to the SLC Developer Program"
+  EMAIL_SUBJECT_SANDBOX = "Welcome to the SLC Developer Sandbox"
 
   def welcome_email(user)
     @firstName = user[:first]
-    @landing_zone_link = "__URI__/landing_zone"
-    @portal_link = "__PORTAL__"
-    Rails.logger.debug "User = #{user}\n\n\n"
+    @landing_zone_link = "#{APP_CONFIG['email_replace_uri']}/landing_zone"
+    @portal_link = APP_CONFIG["portal_url"]
     mail(:to => user[:emailAddress], :subject => (APP_CONFIG["is_sandbox"]?EMAIL_SUBJECT_SANDBOX : EMAIL_SUBJECT_PROD))
-  end
-
-  def self.get_email_token(email_address)
-    user_info= ApprovalEngine.get_user(email_address)
-    if (user_info.nil?)
-      return nil
-    else
-      return user_info[:emailtoken]
-    end
-  end
-
-
-  # Returns a map containing values for email_address, first_name, and last_name.
-  #
-  # Input Parameters:
-  #   - guid : identifier of record containing an email address
-  #
-  # Returns : first, last, and user name on associated record
-  #
-  def get_email_info(guid)
-    url = API_BASE + "/" + guid
-    res = RestClient.get(url, REST_HEADER){|response, request, result| response }
-
-    if (res.code==200)
-      jsonDocument = JSON.parse(res.body)
-      return {
-          "email_address" => jsonDocument["userName"],
-          "first_name" => jsonDocument["firstName"],
-          "last_name" => jsonDocument["lastName"],
-      }
-    else
-      return UNKNOWN_EMAIL
-    end
   end
 end
