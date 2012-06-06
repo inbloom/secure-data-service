0a9f59d4ad4cf10cac122536e6868caebb1c2291
Dong Liu <dliu@wgen.net>
2012-06-06 15:17:48 -0400

    udpate email text

--- sli/admin-tools/admin-rails/app/mailers/application_mailer.rb
+++ sli/admin-tools/admin-rails/app/mailers/application_mailer.rb
@@ -8,6 +8,7 @@ class ApplicationMailer < ActionMailer::Base
     @firstName = user[:first]
     @landing_zone_link = "#{APP_CONFIG['email_replace_uri']}/landing_zone"
     @portal_link = APP_CONFIG["portal_url"]
+    @apps_link = "#{APP_CONFIG['email_replace_uri']}/apps"
     mail(:to => user[:emailAddress], :subject => (APP_CONFIG["is_sandbox"]?EMAIL_SUBJECT_SANDBOX : EMAIL_SUBJECT_PROD))
   end
 
