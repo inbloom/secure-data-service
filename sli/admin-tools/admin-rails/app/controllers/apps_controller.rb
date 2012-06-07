fa53f145e0deaa77335ba5be780bf781373750b6
Patrick Wolf <pwolf@wgen.net>
2012-06-07 12:55:07 -0400

    [DE502] filtering edorgs in sandbox based on tenancy

--- sli/admin-tools/admin-rails/app/controllers/apps_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/apps_controller.rb
@@ -175,11 +175,14 @@ class AppsController < ApplicationController
   
   def get_district_hierarchy
     state_ed_orgs = EducationOrganization.all
-
     result = {}
-
+    user_tenant = get_tenant
     state_ed_orgs.each do |ed_org|
-      next if ed_org.organizationCategories == nil or ed_org.organizationCategories.index("State Education Agency") == nil
+
+      # In sandbox mode, only show edorgs for the current user's tenant
+      filter_tenant = APP_CONFIG["is_sandbox"] && (!ed_org.metaData.attributes.has_key?("tenantId") || ed_org.metaData.tenantId != user_tenant)
+
+      next if ed_org.organizationCategories == nil or ed_org.organizationCategories.index("State Education Agency") == nil or filter_tenant
       current_parent = {"id" => ed_org.id, "name" => ed_org.nameOfInstitution, "stateOrganizationId" => ed_org.stateOrganizationId}
       child_ed_orgs = EducationOrganization.find(:all, :params => {"parentEducationAgencyReference" => ed_org.id})
       child_ed_orgs.each do |child_ed_org|
@@ -193,5 +196,5 @@ class AppsController < ApplicationController
     end
     result
   end
-
+ 
 end
