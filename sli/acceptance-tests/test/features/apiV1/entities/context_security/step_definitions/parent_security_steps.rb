e05b6cf85f2ba440d4f9b6d7ff5604d7e01b8434
Mark Abernathy <mabernathy@wgen.net>
2012-06-08 09:35:50 -0400

    Moved context resolver tests for entities into same folder

--- sli/acceptance-tests/test/features/apiV1/entities/context_security/step_definitions/parent_security_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/entities/context_security/step_definitions/parent_security_steps.rb
@@ -19,45 +19,8 @@ Transform /the parent "([^"]*)"/ do |arg1|
   id
 end
 
-Given /^I am user "([^"]*)" in IDP "([^"]*)"$/ do |arg1, arg2|
-  user = arg1
-  pass = arg1+"1234"
-  realm = arg2
-  
-  idpRealmLogin(user, pass, realm)
-  assert(@sessionId != nil, "Session returned was nil")
-end
-
-Given /^I am assigned the Educator role in my IDP$/ do
-  # No code needed, this is done as configuration
-end
-
-Given /^I teach (the student "[^"]*")$/ do |arg1|
-  # No code needed, this is done as configuration
-end
-
-When /^I make an API call to get (the student "[^"]*")$/ do |arg1|
-  @format = "application/vnd.slc+json"
-  restHttpGet("/v1/students/"+arg1)
-  assert(@res != nil, "Response from rest-client GET is nil")
-end
-
-Then /^I receive a JSON response$/ do
-  assert(@res.code == 200, "Response code not expected: expected 200 but received "+@res.code.to_s)
-  result = JSON.parse(@res.body)
-  assert(result != nil, "Result of JSON parsing is nil")
-end
-
 Then /^I should see a link "([^"]*)"/ do |arg1|
   assert(@res.body.include?(arg1), "Did not find text in the body including the text: "+arg1)
-end
-
-Given /^I do not teach (the student "[^"]*")$/ do |arg1|
-  # No code needed, this is done as configuration
-end
-
-Then /^I get a message that I am not authorized$/ do
-  assert(@res.code == 403, "Response code not expected: expected 403 but received "+@res.code.to_s)
 end
 
 Then /^I should receive a JSON object of the parent$/ do
