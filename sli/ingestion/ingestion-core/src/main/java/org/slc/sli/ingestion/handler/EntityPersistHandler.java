7f600c2c41d8eee05974501078bae34fb09b385e
Stan Halka <shalka@wgen.net>
2012-06-08 20:44:43 +0100

    constrain the focus of the entity persist handler to education organization re-routing (for SEAs and LEAs)

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/handler/EntityPersistHandler.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/handler/EntityPersistHandler.java
@@ -19,32 +19,31 @@ import org.slc.sli.validation.ValidationError;
 
 /**
  * Handles the persisting of Entity objects
- *
+ * 
  * @author dduran
  *         Modified by Thomas Shewchuk (PI3 US811)
  *         - 2/1/2010 Added record DB lookup and update capabilities, and support for association
  *         entities.
- *
+ * 
  */
 public class EntityPersistHandler extends AbstractIngestionHandler<SimpleEntity, Entity> implements InitializingBean {
-
+    
     private Repository<Entity> entityRepository;
-
+    
     private MessageSource messageSource;
-
+    
     @Value("${sli.ingestion.mongotemplate.writeConcern}")
     private String writeConcern;
-
+    
     @Override
     public void afterPropertiesSet() throws Exception {
-       entityRepository.setWriteConcern(writeConcern);
+        entityRepository.setWriteConcern(writeConcern);
     }
-
-
+    
     Entity doHandling(SimpleEntity entity, ErrorReport errorReport) {
         return doHandling(entity, errorReport, null);
     }
-
+    
     @Override
     protected Entity doHandling(SimpleEntity entity, ErrorReport errorReport, FileProcessStatus fileProcessStatus) {
         try {
@@ -56,10 +55,10 @@ public class EntityPersistHandler extends AbstractIngestionHandler<SimpleEntity,
         }
         return null;
     }
-
+    
     /**
      * Persist entity in the data store.
-     *
+     * 
      * @param entity
      *            Entity to be persisted
      * @return Persisted entity
@@ -67,43 +66,46 @@ public class EntityPersistHandler extends AbstractIngestionHandler<SimpleEntity,
      *             Validation Exception
      */
     private Entity persist(SimpleEntity entity) throws EntityValidationException {
-
-        String collectionName = entity.getType();
+        
+        String oldCollectionName = entity.getType();
+        String newCollectionName = oldCollectionName;
         if ((String) entity.getBody().get("collectionName") != null) {
-            collectionName = (String) entity.getBody().get("collectionName");
+            newCollectionName = (String) entity.getBody().get("collectionName");
             entity.getBody().remove("collectionName");
-            entity.setType(collectionName);
+            
+            if (oldCollectionName.equals("stateEducationAgency") || oldCollectionName.equals("localEducationAgency")) {
+                entity.setType(newCollectionName);
+            }
         }
-
+        
         if (entity.getEntityId() != null) {
-
-            if (!entityRepository.update(collectionName, entity)) {
+            
+            if (!entityRepository.update(newCollectionName, entity)) {
                 // TODO: exception should be replace with some logic.
                 throw new RuntimeException("Record was not updated properly.");
             }
-
+            
             return entity;
         } else {
-            return entityRepository.create(entity.getType(), entity.getBody(), entity.getMetaData(), collectionName);
+            return entityRepository.create(entity.getType(), entity.getBody(), entity.getMetaData(), newCollectionName);
         }
     }
-
+    
     private void reportErrors(List<ValidationError> errors, SimpleEntity entity, ErrorReport errorReport) {
         for (ValidationError err : errors) {
-
+            
             String message = "ERROR: There has been a data validation error when saving an entity" + "\n"
-                           + "       Error      " + err.getType().name() + "\n"
-                           + "       Entity     " + entity.getType() + "\n"
-                           + "       Field      " + err.getFieldName() + "\n"
-                           + "       Value      " + err.getFieldValue() + "\n"
-                           + "       Expected   " + Arrays.toString(err.getExpectedTypes()) + "\n";
+                    + "       Error      " + err.getType().name() + "\n" + "       Entity     " + entity.getType()
+                    + "\n" + "       Field      " + err.getFieldName() + "\n" + "       Value      "
+                    + err.getFieldValue() + "\n" + "       Expected   " + Arrays.toString(err.getExpectedTypes())
+                    + "\n";
             errorReport.error(message, this);
         }
     }
-
+    
     /**
      * Generic error reporting function.
-     *
+     * 
      * @param errorMessage
      *            Error message reported by entity.
      * @param entity
@@ -115,19 +117,19 @@ public class EntityPersistHandler extends AbstractIngestionHandler<SimpleEntity,
         String assembledMessage = "Entity (" + entity.getType() + ") reports failure: " + errorMessage;
         errorReport.error(assembledMessage, this);
     }
-
+    
     protected String getFailureMessage(String code, Object... args) {
         return MessageSourceHelper.getMessage(messageSource, code, args);
     }
-
+    
     public void setEntityRepository(Repository<Entity> entityRepository) {
         this.entityRepository = entityRepository;
     }
-
+    
     public MessageSource getMessageSource() {
         return messageSource;
     }
-
+    
     public void setMessageSource(MessageSource messageSource) {
         this.messageSource = messageSource;
     }
