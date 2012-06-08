544c1999feeb1f07c67617164af089a9cb47e712
Steve Lee <slee@wgen.net>
2012-06-08 17:25:56 -0400

    [DE811] Ingestion begins processing valid zip files prematurely

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/routes/LandingZoneRouteBuilder.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/routes/LandingZoneRouteBuilder.java
@@ -54,7 +54,7 @@ public class LandingZoneRouteBuilder extends RouteBuilder {
                     "file:" + inboundDir + "?include=^(.*)\\." + FileFormat.CONTROL_FILE.getExtension() + "$"
                             + "&move=" + inboundDir + "/.done/${file:onlyname}.${date:now:yyyyMMddHHmmssSSS}"
                             + "&moveFailed=" + inboundDir + "/.error/${file:onlyname}.${date:now:yyyyMMddHHmmssSSS}"
-                            + "&readLock=changed")
+                            + "&readLock=rename")
                     .routeId(CTRL_POLLER_PREFIX + inboundDir)
                     .log(LoggingLevel.INFO, "Job.PerformanceMonitor", "- ${id} - ${file:name} - Processing file.")
                     .process(controlFilePreProcessor)
@@ -62,10 +62,10 @@ public class LandingZoneRouteBuilder extends RouteBuilder {
 
             // routeId: zipFilePoller-inboundDir
             from(
-                    "file:" + inboundDir + "?include=^(.*)\\." + FileFormat.ZIP_FILE.getExtension() + "$&preMove="
+                    "file:" + inboundDir + "?include=^(.*)\\." + FileFormat.ZIP_FILE.getExtension() + "$&exclude=\\.in\\.*&preMove="
                             + inboundDir + "/.done&moveFailed=" + inboundDir
                             + "/.error"
-                            + "&readLock=changed")
+                            + "&readLock=rename")
                     .routeId(ZIP_POLLER_PREFIX + inboundDir)
                     .log(LoggingLevel.INFO, "Job.PerformanceMonitor", "- ${id} - ${file:name} - Processing zip file.")
                     .process(zipFileProcessor)
