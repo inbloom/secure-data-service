47880f39e52371d6bc4b1c9c6c0280651d0fb49d
Vijay Ummalaneni <vummalaneni@wgen.net>
2012-06-09 14:12:27 -0400

    DE720 changes

--- sli/admin-tools/admin-rails/app/models/change_password.rb
+++ sli/admin-tools/admin-rails/app/models/change_password.rb
@@ -5,10 +5,10 @@ class ChangePassword < SessionResource
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :old, :new, :confirmation
+  attr_accessor :old_pass, :new_pass, :confirmation
    
-  validates_presence_of :old, :new, :confirmation
-  validates :new, :confirmation => true #password_confirmation attr
+  validates_presence_of :old_pass, :new_pass, :confirmation
+  validates :new_pass, :confirmation => true #password_confirmation attr
   validate :confirm_new 
   
   def initialize(attributes = {})
@@ -22,19 +22,11 @@ class ChangePassword < SessionResource
   end
     
   def confirm_new
-    if self.new != self.confirmation
+    if self.new_pass != self.confirmation
       errors[:base] << "New password and Confirmation password should match"
     end
-    if self.new == self.old
+    if self.new_pass == self.old_pass
       errors[:base] <<"Old password and New password should be different"
-    end
-    
-    check = Check.get("")
-    email = check["external_id"]
-    cn  = check["cn"]
-    temp = check.map{|k,v| "#{k}=#{v}"}.join('&')
-    if !APP_LDAP_CLIENT.authenticate(email, self.old) && !APP_LDAP_CLIENT.authenticate_secure(email, self.old)
-      errors[:base] <<"Please enter your old password" + email + self.old  + ":" + temp
     end
   end
 end
