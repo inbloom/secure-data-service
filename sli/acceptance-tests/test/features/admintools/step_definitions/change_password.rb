d41c559e3129a032cd4c528a69dd53e868d8ce83
Vijay Ummalaneni <vummalaneni@wgen.net>
2012-06-10 21:13:58 -0400

    DE720 Code Review Changes.

---
+++ sli/acceptance-tests/test/features/admintools/step_definitions/change_password.rb
@@ -0,0 +1,47 @@
+require "selenium-webdriver"
+require 'json'
+require 'net/imap'
+
+require_relative '../../utils/sli_utils.rb'
+require_relative '../../utils/selenium_common.rb'
+require 'date'
+
+Given /^I am a SLI Developer "([^"]*)" from the "([^"]*)" hosted directory$/ do |arg1, arg2|
+  # No code needed, done as configuration
+end
+
+Given /^I am a SLC Operator "([^"]*)" from the "([^"]*)" hosted directory$/ do |arg1, arg2|
+  # No code needed, done as configuration
+end
+
+When /^I hit the Change Password URL$/ do
+  @driver.get(PropLoader.getProps['change_password_url'])
+end
+
+Then /^I am redirected to the Change Password page$/ do
+  assertWithWait("Failed to navigate to the Change Password page")  {@driver.page_source.index("Change Password") != nil}
+end
+
+Then /^I see the input boxes to change my password$/ do
+  assertWithWait("Failed to find the input boxes to change my password") {@driver.find_element(:id, "new_change_password")}
+end
+
+When /^I fill out the input field "([^\"]*)" as "([^\"]*)"$/ do |field, value|
+  trimmed = field.sub(" ", "")
+  @driver.find_element(:xpath, "//input[contains(
+    translate(@id, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '#{trimmed.downcase}')
+    ]").send_keys(value)
+end
+
+Then /^I click on "(.*?)"$/ do |arg1|
+  @driver.find_element(:xpath, "//input[contains(@id, '#{arg1}')]").click
+end
+
+Then /^I see an error message "(.*?)"$/ do |errorMsg|
+  assert(@driver.find_element(:xpath, "//h2[contains(@id, 'errorCountNotifier' and contains(text(), '#{errorMsg}')]").size != 0,
+         "Cannot find error message \"#{errorMsg}\"")
+end
+
+Then /^I check for message  "(.*?)"$/ do |arg1|
+  assertText(arg1)
+end
