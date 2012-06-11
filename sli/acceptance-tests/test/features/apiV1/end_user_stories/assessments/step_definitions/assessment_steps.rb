c4de04843ef1fb8278442e179f867192cfb84267
William Scott <wscott@wgen.net>
2012-06-11 17:47:33 -0400

    Update test for tenantId changes.

--- sli/acceptance-tests/test/features/apiV1/end_user_stories/assessments/step_definitions/assessment_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/end_user_stories/assessments/step_definitions/assessment_steps.rb
@@ -57,7 +57,7 @@ Transform /^<([^"]*)>$/ do |human_readable_id|
   #assessments
   id = "67ce204b-9999-4a11-bfea-000000004682"       if human_readable_id == "'Math Assessment' ID"
   id = "dd916592-7d7e-5d27-a87d-dfc7fcb757f6"       if human_readable_id == "'SAT' ID"
-  id = "e85b5aa7-465a-6dd3-8ffb-d02461ed79f8"       if human_readable_id == "'Most Recent SAT Student Assessment Association' ID"
+  id = "87fb8da5-e1aa-a6d9-efc7-b0eb091cd695"       if human_readable_id == "'Most Recent SAT Student Assessment Association' ID"
   id = "e5e13e61-01aa-066b-efe0-710f7a0e8755"       if human_readable_id == "'Most Recent Math Student Assessment Association' ID"
   id = "dd9165f2-65fe-4e27-a8ac-bec5f4b757f6"       if human_readable_id == "'Grade 2 BOY DIBELS' ID"
   id = "dd916592-7dfe-4e27-a8ac-bec5f4b757b7"       if human_readable_id == "'Grade 2 MOY READ2' ID"
