0de49b1980a01a85db4388185d8be7a28ef255dd
Jeffrey Stokes <jstokes@wgen.net>
2012-06-11 14:07:47 -0400

    adding unit test for Level0Client

--- sli/modeling/shtick/src/main/test/org/slc/sli/shtick/StandardLevel0ClientTest.java
+++ sli/modeling/shtick/src/main/test/org/slc/sli/shtick/StandardLevel0ClientTest.java
@@ -3,11 +3,15 @@ package org.slc.sli.shtick;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.client.InvocationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 /**
@@ -15,10 +19,12 @@ import static org.junit.Assert.fail;
  */
 public class StandardLevel0ClientTest {
 
-    Level0Client client;
+    private Level0Client client; // class under test
 
     // RRogers - IT Admin testing token
     private static final String TESTING_TOKEN = "cacd9227-5b14-4685-babe-31230476cf3b";
+
+    // TODO: Should be pulling this out of a config file
     private static final String BASE_URL = "http://local.slidev.org:8080/api/rest/v1";
 
     @Before
@@ -29,10 +35,15 @@ public class StandardLevel0ClientTest {
     @Test
     public void testGetRequest() {
         try {
-            Response actualResponse = client.getRequest(TESTING_TOKEN, new URL(BASE_URL + "/students"));
+            Response actualResponse = client.getRequest(TESTING_TOKEN,
+                    new URL(BASE_URL + "/students"), MediaType.APPLICATION_JSON);
+            assertNotNull(actualResponse);
+            assertEquals(Response.ok().build().getStatus(), actualResponse.getStatus());
         } catch (URISyntaxException e) {
             fail(e.getMessage());
         } catch (MalformedURLException e) {
+            fail(e.getMessage());
+        } catch (InvocationException e) {
             fail(e.getMessage());
         }
     }
