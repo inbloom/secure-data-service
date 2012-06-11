e1fcda8a5081566475426e1ee5f1d5eb38de5ee6
Ryan Farris <rfarris@wgen.net>
2012-06-11 13:41:49 -0400

    DE821: user_account_validation unit tests

--- sli/admin-tools/admin-rails/app/models/user_account_validation.rb
+++ sli/admin-tools/admin-rails/app/models/user_account_validation.rb
@@ -1,9 +1,5 @@
-
-require 'rest-client'
-require 'json'
-
 class UserAccountValidation
-    
+  
   # success message
   ACCOUNT_VERIFICATION_COMPLETE = {
     "status" => "Registration Complete!",
@@ -15,22 +11,16 @@ class UserAccountValidation
     "message" => "Invalid account verification code."
   }
   
-  # error condition for attemptint to re-verify verified account
+  # error condition for attempt to re-verify verified account
   ACCOUNT_PREVIOUSLY_VERIFIED = {
     "status" => "Account validation failed!",
     "message" => "This account was previously verified. For questions please contact #{APP_CONFIG['support_email']}"
   }
   
-  # error condition for attemptint to re-verify verified account
+  # error condition for unhandled exceptions
   UNEXPECTED_VERIFICATION_ERROR = {
     "status" => "Account validation failed!",
     "message" => "Unexpected verification error. Try again later."
-  }
-  
-  REST_HEADER = {
-    "Content-Type" => "application/json",
-    "content-type" => "application/json",
-    "accept" => "application/json"
   }
     
   def self.validate_account(emailToken)
@@ -48,33 +38,5 @@ class UserAccountValidation
         return UNEXPECTED_VERIFICATION_ERROR
       end
     end
-    
-    # BULLSHIT BELOW THIS LINE
-    # url = APP_CONFIG['api_base']+"/v1/userAccounts?userName=" + user_info[:email]+"&environment="+ environment
-    # 
-    # res = RestClient.get(url, REST_HEADER){|response, request, result| response }
-    # 
-    # if (res.code == 200)
-    #   parsedJsonHash = JSON.parse(res.body)
-    #   guid=parsedJsonHash[0]["id"]
-    #   if (parsedJsonHash[0]["validated"] == true)
-    #     return ACCOUNT_PREVIOUSLY_VERIFIED
-    #   else
-    #     parsedJsonHash[0]["validated"] = true
-    #     url =APP_CONFIG['api_base']+"/v1/userAccounts/"+guid
-    # 
-    #     putRes = RestClient.put(url, parsedJsonHash[0].to_json, REST_HEADER){|response, request, result| response }
-    #     if (putRes.code == 204)
-    #       ApplicationHelper.verify_email(emailToken)
-    #       return ACCOUNT_VERIFICATION_COMPLETE
-    #     else
-    #       return UNEXPECTED_VERIFICATION_ERROR
-    #     end
-    #   end
-    # elsif (res.code == 404)
-    #   return INVALID_VERIFICATION_CODE
-    # else
-    #   return UNEXPECTED_VERIFICATION_ERROR
-    # end 
   end
 end
