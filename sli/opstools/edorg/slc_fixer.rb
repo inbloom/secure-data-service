51960bc8d6e79172bf886057458e89b2330970e5
Ryan Latta <rlatta@wgen.net>
2012-06-13 18:47:00 -0400

    Batching stamps

--- sli/opstools/edorg/slc_fixer.rb
+++ sli/opstools/edorg/slc_fixer.rb
@@ -11,6 +11,7 @@ class SLCFixer
     @students = @db['student']
     @student_hash = {}
     @count = 0
+    @basic_options = {:timeout => false, :batch_size => 100}
   end
 
   def start
@@ -42,7 +43,7 @@ class SLCFixer
 
 
   def fix_students
-    @db['studentSchoolAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentSchoolAssociation'].find({}, @basic_options) do |cur|
       cur.each do |student|
         edorgs = []
         old = old_edorgs(@students, student['body']['studentId'])
@@ -50,8 +51,8 @@ class SLCFixer
         edorgs << old unless old.empty?
         edorgs = edorgs.flatten.uniq.sort
         stamp_id(@db['studentSchoolAssociation'], student['_id'], student['body']['schoolId'])
+        @student_hash[student['body']['studentId']] = edorgs
         if !edorgs.eql? old
-          @student_hash[student['body']['studentId']] = edorgs
           stamp_id(@students, student['body']['studentId'], edorgs)
         end
       end
@@ -59,29 +60,29 @@ class SLCFixer
   end
 
   def fix_sections
-    @db['section'].find({}, :timeout => false) do |cur|
+    @db['section'].find({}, @basic_options) do |cur|
       cur.each do |section|
         edorgs = section['body']['schoolId']
         stamp_id(@db['section'], section['_id'], edorgs)
-        @db['teacherSectionAssociation'].find({"body.sectionId"    => section['_id']}, :timeout => false) do |scur| 
+        @db['teacherSectionAssociation'].find({"body.sectionId"    => section['_id']}, @basic_options) do |scur| 
           scur.each {|assoc| stamp_id(@db['teacherSectionAssociation'], assoc['_id'], edorgs)}
         end
             
-        @db['sectionAssessmentAssociation'].find({"body.sectionId" => section['_id']}, :timeout => false) do |scur| 
+        @db['sectionAssessmentAssociation'].find({"body.sectionId" => section['_id']}, @basic_options) do |scur| 
           scur.each {|assoc| stamp_id(@db['sectionAssessmentAssociation'], assoc['_id'], edorgs) }
         end
-        @db['studentSectionAssociation'].find({'body.sectionId' => section['_id']}, :timeout => false) do |scur|
+        @db['studentSectionAssociation'].find({'body.sectionId' => section['_id']}, @basic_options) do |scur|
           scur.each { |assoc| stamp_id(@db['studentSectionAssociation'], assoc['_id'], ([] << edorgs << student_edorgs(assoc['body']['studentId'])).flatten.uniq) }
         end
       end
     end
-    @db['sectionSchoolAssociation'].find(:timeout => false) do |cur|
+    @db['sectionSchoolAssociation'].find({}, @basic_options) do |cur|
       cur.each { |assoc| stamp_id(@db['sectionSchoolAssociation'], assoc['_id'], assoc['body']['schoolId']) }
     end
   end
 
   def fix_attendance
-    @db['attendance'].find({}, :timeout => false) do |cur|
+    @db['attendance'].find({}, @basic_options) do |cur|
       cur.find.each do |attendance|
         edOrg = student_edorgs(attendance['body']['studentId'])
         stamp_id(@db['attendance'], attendance['_id'], edOrg)
@@ -90,7 +91,7 @@ class SLCFixer
   end
 
   def fix_assessments
-    @db['studentAssessmentAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentAssessmentAssociation'].find({}, @basic_options) do |cur|
       cur.each do |assessment|
         edOrg = []
         student_edorg = student_edorgs(assessment['body']['studentId'])
@@ -101,7 +102,7 @@ class SLCFixer
         stamp_id(@db['assessment'], assessment['body']['assessmentId'], edOrg)
       end
     end
-    @db['sectionAssessmentAssociation'].find({}, :timeout => false) do |cur|
+    @db['sectionAssessmentAssociation'].find({}, @basic_options) do |cur|
       cur.each do |assessment|
         edorgs = []
         assessment_edOrg = old_edorgs(@db['assessment'], assessment['body']['assessmentId'])
@@ -140,7 +141,7 @@ class SLCFixer
   end
 
   def fix_parents
-    @db['studentParentAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentParentAssociation'].find({}, @basic_options) do |cur|
       cur.each do |parent|
         edorg = student_edorgs(parent['body']['studentId'])
         stamp_id(@db['studentParentAssociation'], parent['_id'], edorg)
@@ -150,7 +151,7 @@ class SLCFixer
   end
 
   def fix_report_card
-    @db['reportCard'].find({}, :timeout => false) do |cur|
+    @db['reportCard'].find({}, @basic_options) do |cur|
       cur.each do |card|
         edorg = student_edorgs(card['body']['studentId'])
         stamp_id(@db['reportCard'], card['_id'], edorg)
@@ -159,14 +160,14 @@ class SLCFixer
   end
 
   def fix_programs
-    @db['studentProgramAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentProgramAssociation'].find({}, @basic_options) do |cur|
       cur.each do |program|
         edorg = student_edorgs(program['body']['studentId'])
         stamp_id(@db['studentProgramAssociation'], program['_id'], program['body']['educationOrganizationId'])
         stamp_id(@db['program'], program['body']['programId'], program['body']['educationOrganizationId'])
       end
     end
-    @db['staffProgramAssociation'].find({}, :timeout => false) do |cur|
+    @db['staffProgramAssociation'].find({}, @basic_options) do |cur|
       cur.each do |program|
         edorg = []
         program_edorg = old_edorgs(@db['program'], program['body']['programId'])
@@ -180,18 +181,18 @@ class SLCFixer
   end
 
   def fix_cohorts
-    @db['cohort'].find({}, :timeout => false) do |cur|
+    @db['cohort'].find({}, @basic_options) do |cur|
       cur.each do |cohort|
         stamp_id(@db['cohort'], cohort['_id'], cohort['body']['educationOrgId'])
       end
     end
-    @db['studentCohortAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentCohortAssociation'].find({}, @basic_options) do |cur|
       cur.each do |cohort|
         edorg = old_edorgs(@db['student'], cohort['body']['studentId'])
         stamp_id(@db['studentCohortAssociation'], cohort['_id'], edorg)
       end
     end
-    @db['staffCohortAssociation'].find({}, :timeout => false) do |cur|
+    @db['staffCohortAssociation'].find({}, @basic_options) do |cur|
       cur.each do |cohort|
         edorg = []
         edorg << old_edorgs(@db['cohort'], cohort['body']['cohortId'])
@@ -204,17 +205,17 @@ class SLCFixer
   end
 
   def fix_sessions
-    @db['session'].find({}, :timeout => false) do |cur|
+    @db['session'].find({}, @basic_options) do |cur|
       cur.each do |session|
         edorg = []
-        @db['section'].find({"body.sessionId" => session["_id"]}, :timeout => false) do |scur| 
+        @db['section'].find({"body.sessionId" => session["_id"]}, @basic_options) do |scur| 
           scur.each do |sec|
             edorg << sec['metaData']['edOrgs'] unless sec['metaData'].nil? 
           end
         end
         edorg = edorg.flatten.uniq
         stamp_id(@db['session'], session['_id'], edorg)
-        @db['schoolSessionAssociation'].find({"body.sessionId" => session['_id']}, :timeout => false) do |scur| 
+        @db['schoolSessionAssociation'].find({"body.sessionId" => session['_id']}, @basic_options) do |scur| 
           scur.each do |assoc|
             stamp_id(@db['schoolSessionAssociation'], assoc['_id'], edorg)
           end
@@ -232,7 +233,7 @@ class SLCFixer
   end
 
   def fix_staff
-    @db['staffEducationOrganizationAssociation'].find({}, :timeout => false) do |cur|
+    @db['staffEducationOrganizationAssociation'].find({}, @basic_options) do |cur|
       cur.each do |staff|
         old = old_edorgs(@db['staff'], staff['body']['staffReference'])
         edorg = staff['body']['educationOrganizationReference']
@@ -241,7 +242,7 @@ class SLCFixer
       end
     end
     #This needed?
-    @db['teacherSchoolAssociation'].find({}, :timeout => false) do |cur|
+    @db['teacherSchoolAssociation'].find({}, @basic_options) do |cur|
       cur.each do |teacher|
         old = old_edorgs(@db['staff'], teacher['body']['teacherId'])
         stamp_id(@db['teacherSchoolAssociation'], teacher['_id'], teacher['body']['schoolId'])
@@ -251,14 +252,14 @@ class SLCFixer
   end
 
   def fix_grades
-    @db['gradebookEntry'].find({}, :timeout => false) do |cur|
+    @db['gradebookEntry'].find({}, @basic_options) do |cur|
       cur.each do |grade|
         edorg = old_edorgs(@db['section'], grade['body']['sectionId'])
         stamp_id(@db['gradebookEntry'], grade['_id'], edorg)
       end
     end
     #Grades and grade period
-    @db['grade'].find({}, :timeout => false) do |cur|
+    @db['grade'].find({}, @basic_options) do |cur|
       cur.each do |grade|
         edorg = old_edorgs(@db['studentSectionAssociation'], grade['body']['studentSectionAssociationId'])
         stamp_id(@db['grade'], grade['_id'], edorg)
@@ -268,13 +269,13 @@ class SLCFixer
   end
 
   def fix_courses
-    @db['section'].find({}, :timeout => false) do |cur|
+    @db['section'].find({}, @basic_options) do |cur|
       cur.each do |section|
         edorg = section['metaData']['edOrgs']
         stamp_id(@db['course'], section['body']['courseId'], edorg)
       end
     end
-    @db['courseOffering'].find({}, :timeout => false) do |cur|
+    @db['courseOffering'].find({}, @basic_options) do |cur|
       cur.each do |course|
         edorgs = []
         edorgs << old_edorgs(@db['course'], course['body']['courseId'])
@@ -287,13 +288,13 @@ class SLCFixer
 
   def fix_miscellany
     #StudentTranscriptAssociation
-    @db['studentTranscriptAssociation'].find({}, :timeout => false) do |cur|
+    @db['studentTranscriptAssociation'].find({}, @basic_options) do |cur|
       cur.each do |trans|
         edorg = []
         edorg << old_edorgs(@db['studentTranscriptAssociation'], trans['_id'])	  
         edorg << student_edorgs(trans['body']['studentId'])
 
-        @db['studentAcademicRecord'].find({"_id" => trans['body']['studentAcademicRecordId']}, :timeout => false) do |scur|
+        @db['studentAcademicRecord'].find({"_id" => trans['body']['studentAcademicRecordId']}, @basic_options) do |scur|
           scur.each do |sar|
             studentId = sar['body']['studentId']
             edorg << student_edorgs(studentId)        
@@ -305,7 +306,7 @@ class SLCFixer
     end
 
     #StudentSectionGradeBook
-    @db['studentSectionGradebookEntry'].find({}, :timeout => false) do |cur|
+    @db['studentSectionGradebookEntry'].find({}, @basic_options) do |cur|
       cur.each do |trans|
         edorg = student_edorgs(trans['body']['studentId'])
         stamp_id(@db['studentSectionGradebookEntry'], trans['_id'], edorg)
@@ -313,7 +314,7 @@ class SLCFixer
     end
 
     #Student Compentency
-    @db['studentCompetency'].find({}, :timeout => false) do |cur|
+    @db['studentCompetency'].find({}, @basic_options) do |cur|
       cur.each do |student|
         edorg = old_edorgs(@db['studentSectionAssociation'], student['body']['studentSectionAssociationId'])
         stamp_id(@db['studentCompetency'], student['_id'], edorg)
@@ -321,7 +322,7 @@ class SLCFixer
     end
 
     #Student Academic Record
-    @db['studentAcademicRecord'].find({}, :timeout => false) do |cur|
+    @db['studentAcademicRecord'].find({}, @basic_options) do |cur|
       cur.each do |student|
         edorg = student_edorgs(student['body']['studentId'])
         stamp_id(@db['studentAcademicRecord'], student['_id'], edorg)
