32a5dc6099caf0064ea37e390f188e1693325908
David Holmes <dholmes@wgen.net>
2012-06-13 15:16:53 -0400

    Testing of multiple-id GET requests.

--- sli/modeling/shtick/src/test/java/org/slc/sli/shtick/StandardLevel2ClientTest.java
+++ sli/modeling/shtick/src/test/java/org/slc/sli/shtick/StandardLevel2ClientTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +36,7 @@ public class StandardLevel2ClientTest {
 
     private void doGetStudentsById(final Level1Client inner) {
         final Level2Client client = new StandardLevel2Client(BASE_URL, inner);
+        // One identifier.
         try {
             final List<Entity> students = client.getStudentsById(TestingConstants.TESTING_TOKEN,
                     TestingConstants.TEST_STUDENT_ID, EMPTY_QUERY_ARGS);
@@ -50,6 +52,57 @@ public class StandardLevel2ClientTest {
             assertEquals("Male", data.get("sex"));
             assertEquals(Boolean.FALSE, data.get("economicDisadvantaged"));
             assertEquals("100000005", data.get("studentUniqueStateId"));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        } catch (final SLIDataStoreException e) {
+            fail(e.getMessage());
+        }
+        // Two identifiers.
+        try {
+            final List<Entity> students = client.getStudentsById(TestingConstants.TESTING_TOKEN,
+                    TestingConstants.TEST_STUDENT_ID + "," + TestingConstants.TEST_STUDENT_ID_ONE + ","
+                            + TestingConstants.TEST_STUDENT_ID_TWO, EMPTY_QUERY_ARGS);
+
+            assertNotNull(students);
+            assertEquals(3, students.size());
+            // We have observed that multiple-id GET does not preserve ordering, so we use a map.
+            final Map<String, Entity> studentMap = new HashMap<String, Entity>();
+            for (final Entity student : students) {
+                studentMap.put(student.getId(), student);
+            }
+            {
+                final Entity student = studentMap.get(TestingConstants.TEST_STUDENT_ID);
+                assertNotNull(student);
+                assertEquals(TestingConstants.TEST_STUDENT_ID, student.getId());
+                assertEquals("student", student.getEntityType());
+                final Map<String, Object> data = student.getData();
+                assertNotNull(data);
+                assertEquals("Male", data.get("sex"));
+                assertEquals(Boolean.FALSE, data.get("economicDisadvantaged"));
+                assertEquals("100000005", data.get("studentUniqueStateId"));
+            }
+            {
+                final Entity student = studentMap.get(TestingConstants.TEST_STUDENT_ID_ONE);
+                assertNotNull(student);
+                assertEquals(TestingConstants.TEST_STUDENT_ID_ONE, student.getId());
+                assertEquals("student", student.getEntityType());
+                final Map<String, Object> data = student.getData();
+                assertNotNull(data);
+                assertEquals("Female", data.get("sex"));
+                assertEquals(null, data.get("economicDisadvantaged"));
+                assertEquals("900000011", data.get("studentUniqueStateId"));
+            }
+            {
+                final Entity student = studentMap.get(TestingConstants.TEST_STUDENT_ID_TWO);
+                assertNotNull(student);
+                assertEquals(TestingConstants.TEST_STUDENT_ID_TWO, student.getId());
+                assertEquals("student", student.getEntityType());
+                final Map<String, Object> data = student.getData();
+                assertNotNull(data);
+                assertEquals("Female", data.get("sex"));
+                assertEquals(null, data.get("economicDisadvantaged"));
+                assertEquals("900000011", data.get("studentUniqueStateId"));
+            }
         } catch (final IOException e) {
             throw new RuntimeException(e);
         } catch (final SLIDataStoreException e) {
--- sli/modeling/shtick/src/test/java/org/slc/sli/shtick/TestingConstants.java
+++ sli/modeling/shtick/src/test/java/org/slc/sli/shtick/TestingConstants.java
@@ -9,6 +9,7 @@ public class TestingConstants {
     public static final String TESTING_TOKEN = "cacd9227-5b14-4685-babe-31230476cf3b";
     public static final String BASE_URL = "http://local.slidev.org:8080/api/rest/v1";
     public static final String TEST_STUDENT_ID = "d2462231-4f6c-452e-9b29-4a63ad92138e";
+    public static final String TEST_STUDENT_ID_ONE = "2012au-b950394c-b57c-11e1-b7c8-3c0754560b5a";
+    public static final String TEST_STUDENT_ID_TWO = "2012cc-fd0109b4-b56a-11e1-a12b-3c0754560b5a";
     public static final String TEST_STUDENT_DELETE_ID = "0f0d9bac-0081-4900-af7c-d17915e02378";
-
 }
