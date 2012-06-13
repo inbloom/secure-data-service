cf26be9f139495d81fefb166960a94e5e6f7d467
Yuan Zhang <yzhang@wgen.net>
2012-06-13 17:05:14 -0400

    add id to staff, update generators to ingest attendance events.

--- tools/data-tools/src/org/slc/sli/test/generators/AttendanceEventGenerator.java
+++ tools/data-tools/src/org/slc/sli/test/generators/AttendanceEventGenerator.java
@@ -14,13 +14,15 @@ import org.slc.sli.test.edfi.entities.SectionReferenceType;
 
 public class AttendanceEventGenerator {
 
-    private static final boolean INCLUDE_OPTIONAL_DATA = false;
+    private static final boolean INCLUDE_OPTIONAL_DATA = true;
 
     private static final Random RANDOM = new Random();
     private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
-    private static Calendar calendar = new GregorianCalendar(2012, 0, 1);
-    
-    private static String[] attendance = {"In Attendance","Excused Absence","Unexcused Absence", "Tardy", "Early departure"};
+    private static Calendar calendar;
+
+    static {
+        resetCalendar();
+    }
 
     public static AttendanceEvent generateMidFi(String studentID, String schoolID, String sectionCode) {
         AttendanceEvent ae = new AttendanceEvent();
@@ -46,13 +48,13 @@ public class AttendanceEventGenerator {
 
         return ae;
     }
-    
+
     public static AttendanceEvent generateLowFi(String studentID, String schoolID, String sectionCode) {
         AttendanceEvent ae = new AttendanceEvent();
 
         calendar.add(Calendar.DAY_OF_YEAR, 1);
         ae.setEventDate(DATE_FORMATTER.format(calendar.getTime()));
-        ae.setAttendanceEventCategory(AttendanceEventCategoryType.fromValue(attendance[0]));
+        ae.setAttendanceEventCategory(AttendanceEventCategoryType.values()[0]);
         ae.setStudentReference(StudentGenerator.getStudentReferenceType(studentID));
         ae.setSchoolReference(SchoolGenerator.getEducationalOrgReferenceType(schoolID));
 
@@ -77,64 +79,38 @@ public class AttendanceEventGenerator {
     }
 
     public static AttendanceEventCategoryType getAttendanceEventCategoryType() {
-    	int index = RANDOM.nextInt(AttendanceEventCategoryType.values().length);
-    	if (index == 1)
-    		index = index + 1;
-    	
+        int index = RANDOM.nextInt(AttendanceEventCategoryType.values().length);
+
+        // TODO remove after DE769
+        if (index == 1) {
+            index++;
+        }
+
         return AttendanceEventCategoryType.values()[index];
     }
 
-   // ["In Attendance", "Absence". "Excused Absence","Unexcused Absence", "Tardy", "Early departure"]
-   // ["In Attendance", "Excused Absence","Unexcused Absence", "Tardy", "Early departure"]
-
-    
     public static AttendanceEventCategoryType getAttendanceEventCategoryTypeMedFi() {
-    	
-    	int roll = RANDOM.nextInt(100);
-		
-		switch (roll) {
-		case 1 :
-			return AttendanceEventCategoryType.fromValue(attendance[1]);		
-		case 2 :
-			return AttendanceEventCategoryType.fromValue(attendance[2]);
-		case 3 : 
-			return AttendanceEventCategoryType.fromValue(attendance[1]);
-		case 4 :
-			return AttendanceEventCategoryType.fromValue(attendance[2]);
-		case 5 : 
-			return AttendanceEventCategoryType.fromValue(attendance[1]);
-		case 6:
-			return AttendanceEventCategoryType.fromValue(attendance[3]);
-		case 7:
-			return AttendanceEventCategoryType.fromValue(attendance[4]);
-		default : 
-			return AttendanceEventCategoryType.fromValue(attendance[0]);
-					
-		}
+        return getAttendanceEventCategoryType();
     }
-		
-    
+
     public static EducationalEnvironmentType getEducationalEnvironmentType() {
         return EducationalEnvironmentType.values()[RANDOM.nextInt(EducationalEnvironmentType.values().length)];
     }
 
     public static void resetCalendar() {
-        calendar = new GregorianCalendar(2012, 0, 1);
+        calendar = new GregorianCalendar(2010, 0, 1);
     }
 
-    public static void main(String args[]) throws Exception{
-    
-    	
-    	AttendanceEventGenerator test = new AttendanceEventGenerator();
-    	test.getAttendanceEventCategoryTypeMedFi();
-    	String studentID = "sid";
-    	String schoolID = "schid";
-    	String sectionCode = "secode";
-    	test.generateLowFi(studentID, schoolID, sectionCode);
-    	test.generateLowFi(studentID, schoolID, sectionCode);
-    	test.generateLowFi(studentID, schoolID, sectionCode);
-    	
+    public static void main(String args[]) throws Exception {
+
+        AttendanceEventGenerator test = new AttendanceEventGenerator();
+        test.getAttendanceEventCategoryTypeMedFi();
+        String studentID = "sid";
+        String schoolID = "schid";
+        String sectionCode = "secode";
+        test.generateLowFi(studentID, schoolID, sectionCode);
+        test.generateLowFi(studentID, schoolID, sectionCode);
+        test.generateLowFi(studentID, schoolID, sectionCode);
+
     }
 }
-
-
