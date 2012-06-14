06b6784096e6f6f6c0cfadf1f6ee9c999bcfcdc2
Stephan Altmueller <saltmueller@wgen.net>
2012-06-14 15:11:43 -0400

    fixes

--- sli/admin-tools/admin-rails/config/initializers/mailer_setup.rb
+++ sli/admin-tools/admin-rails/config/initializers/mailer_setup.rb
@@ -1,7 +1,7 @@
-smpt_settings = {
+smtp_settings = {
   :address              => APP_CONFIG['email_host'],
   :port                 => APP_CONFIG['email_port'],
-  :enable_starttls_auto => APP_CONFIG['email_tls'] == "true"],
+  :enable_starttls_auto => (APP_CONFIG['email_tls'] == "true")
 }
 
 username = APP_CONFIG["email_username"]
@@ -12,6 +12,6 @@ if username && password && (username.strip != "") && (password.strip != "")
     smtp_settings[:authentication] = :plain
 end 
 
-ActionMailer::Base.smtp_settings = smpt_settings
+ActionMailer::Base.smtp_settings = smtp_settings
 ActionMailer::Base.default_url_options[:host] = APP_CONFIG["email_replace_uri"].gsub(/http[s]*:\/\//, '')
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
