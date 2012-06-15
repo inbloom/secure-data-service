1afa95c97019fb8d09b6231ca37584216a928580
Stan Halka <shalka@wgen.net>
2012-06-15 22:22:12 +0100

    use string builder and secure random in shard type 1 uuid generator strategy

--- sli/common/sli-common/src/main/java/org/slc/sli/common/util/uuid/ShardType1UUIDGeneratorStrategy.java
+++ sli/common/sli-common/src/main/java/org/slc/sli/common/util/uuid/ShardType1UUIDGeneratorStrategy.java
@@ -1,13 +1,14 @@
 package org.slc.sli.common.util.uuid;
 
-import java.util.Random;
+import java.security.SecureRandom;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.uuid.EthernetAddress;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedGenerator;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
 /**
  * Generates Type 1 (time-based) UUIDs, preceeded by a shard key 'YYYYRR-'.
@@ -17,18 +18,23 @@ import org.springframework.stereotype.Component;
 @Component
 @Qualifier("shardType1UUIDGeneratorStrategy")
 public class ShardType1UUIDGeneratorStrategy implements UUIDGeneratorStrategy {
-
+    
     private TimeBasedGenerator generator = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
-    Random r = new Random();
+    SecureRandom r = new SecureRandom();
 
     /**
      * Generate a shardable type 1 random UUID .
      */
     @Override
     public String randomUUID() {
+        StringBuilder builder = new StringBuilder();
         char c1 = (char) (r.nextInt(26) + 'a');
         char c2 = (char) (r.nextInt(26) + 'a');
-        return "" + new DateTime().getYear() + c1 + c2 + "-" + generator.generate();
+        builder.append(new DateTime().getYear());
+        builder.append(c1);
+        builder.append(c2);
+        builder.append(generator.generate().toString());
+        String uuid = builder.toString();
+        return uuid;
     }
-
 }
