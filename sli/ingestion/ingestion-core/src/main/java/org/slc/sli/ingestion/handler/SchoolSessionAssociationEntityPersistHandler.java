6e2b7b0c72c0202a0ca1ff006198a308ef7118be
Stan Halka <shalka@wgen.net>
2012-06-18 21:30:31 +0100

    change the handling of persistence for sessions and school session associations

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/handler/SchoolSessionAssociationEntityPersistHandler.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/handler/SchoolSessionAssociationEntityPersistHandler.java
@@ -9,25 +9,23 @@ import org.slf4j.LoggerFactory;
 
 /**
  * A class used to persist Session and SchoolSessionAssociation in a certain order.
- *
+ * 
  * @author ablum
- *
+ * 
  */
 public class SchoolSessionAssociationEntityPersistHandler extends EntityPersistHandler {
-
+    
     private static final Logger LOG = LoggerFactory.getLogger(SchoolSessionAssociationEntityPersistHandler.class);
     
     @Override
     protected Entity doHandling(SimpleEntity entity, ErrorReport errorReport, FileProcessStatus fileProcessStatus) {
-        SimpleEntity session = (SimpleEntity) entity.getBody().remove("session");
-        LOG.debug("session peeled off from school session association: {}", session);
+        SimpleEntity schoolSessionAssociation = (SimpleEntity) entity.getBody().remove("schoolSessionAssociation");
         
-        Entity mongoSession = super.doHandling(session, errorReport, fileProcessStatus);
+        Entity mongoSession = super.doHandling(entity, errorReport, fileProcessStatus);
         LOG.debug("id of created session: {}", mongoSession.getEntityId());
-        entity.getBody().put("sessionId", mongoSession.getEntityId());
-
-        Entity createdSchoolSessionAssociation = super.doHandling(entity, errorReport, fileProcessStatus);
-        LOG.debug("created school session association: {}", createdSchoolSessionAssociation);
-        return createdSchoolSessionAssociation;
+        schoolSessionAssociation.getBody().put("sessionId", mongoSession.getEntityId());
+        
+        super.doHandling(schoolSessionAssociation, errorReport, fileProcessStatus);
+        return entity;
     }
 }
