d7935df768df42f9493fee7576c3e68f87137b26
Simon Yau <syau@wgen.net>
2012-06-18 17:03:39 -0400

    DE608: First cut: Add functionality in IdNormalizer to resolve references specified with array of complex objects.

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/IdNormalizer.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/IdNormalizer.java
@@ -33,6 +33,67 @@ public class IdNormalizer {
 
     private Repository<Entity> entityRepository;
 
+    /**
+     * Resolves a reference represented by an array of complex objects, which
+     *
+     * @param entity - the referer entity
+     * @param tenantId - tenant's id
+     * @param valueSource - xpath to the complex object array in the referer entity
+     * @param fieldPath - xpath to the field in the referer entity where the resolved id will be written into
+     * @param targetCollection - referenced entity
+     * @param path - xpath to the complex object array in the referenced entity
+     * @param complexFieldNames - names of fields in the complex object
+     * @param errorReport - error reporter
+     */
+    public void resolveReferenceWithComplexArray(Entity entity, String tenantId,
+                                                 String valueSource, String fieldPath,
+                                                 String collectionName, String path,
+                                                 List<String> complexFieldNames,
+                                                 ErrorReport errorReport) {
+
+        try {
+            List<?> refValues = (List<?>) PropertyUtils.getProperty(entity, valueSource);
+
+            // For each element in the referer's array, create a mongo query; store all matching results into a set
+            Set<String> foundIds = new HashSet<String>();
+            for (int refIndex = 0; refIndex < refValues.size(); refIndex++) {
+                String valueSourcePath = valueSource + ".[" + Integer.toString(refIndex) + "]";
+                Query query = new Query();
+                Criteria criteria = Criteria.where(METADATA_BLOCK + "." + EntityMetadataKey.TENANT_ID.getKey()).is(tenantId);
+
+                // For each field in the complex object, add a criteria to the the query
+                for (String fieldName : complexFieldNames) {
+                    Object fieldValue = PropertyUtils.getProperty(entity, valueSourcePath + "." + fieldName);
+                    criteria = criteria.and(path + "." + fieldName).is(fieldValue);
+                }
+                query.addCriteria(criteria);
+
+                // execute query and record results
+                @SuppressWarnings("deprecation")
+                Iterable<Entity> foundRecords = entityRepository.findByQuery(collectionName, query, 0, 0);
+                for(Entity record : foundRecords) {
+                    foundIds.add(record.getEntityId());
+                }
+
+            }
+
+            // resolution fails if not exactly one resolved object is found.
+            if (foundIds.size() != 1) {
+                throw new RuntimeException("Number of resolved ids in resolve complex reference is not 1, but is " + foundIds.size());
+            } else {
+                PropertyUtils.setProperty(entity, fieldPath, foundIds.iterator().next());
+            }
+
+        } catch (Exception e) {
+            LogUtil.error(LOG, "Error resolving reference to " + collectionName + " in " + entity.getType(), e);
+            String errorMessage = "ERROR: Failed to resolve a reference" + "\n" + "       Entity " + entity.getType()
+                    + ": Reference to " + collectionName + " cannot be resolved" + "\n";
+            // @@@ here you are, Simon. Add more debug information.
+            errorReport.error(errorMessage, this);
+        }
+
+    }
+
     public void resolveInternalIds(Entity entity, String tenantId, EntityConfig entityConfig, ErrorReport errorReport) {
 
         if (entityConfig.getReferences() == null) {
