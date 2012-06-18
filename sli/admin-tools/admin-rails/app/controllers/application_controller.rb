075340f6008e09c3ade275cb09875c83abf60a60
Ryan Latta <rlatta@wgen.net>
2012-06-18 17:51:52 -0400

    [DE966] Aggressive session check in admin

--- sli/admin-tools/admin-rails/app/controllers/application_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/application_controller.rb
@@ -52,17 +52,10 @@ class ApplicationController < ActionController::Base
       if oauth.token != nil
         SessionResource.access_token = oauth.token
         logger.debug "TOKEN = #{oauth.token}"
+        set_session
       elsif params[:code] && !oauth.has_code
         SessionResource.access_token = oauth.get_token(params[:code])
-        check = Check.get("")
-        email = SupportEmail.get("")
-        logger.debug { "Email #{email}"}
-        session[:support_email] = email
-        session[:full_name] ||= check["full_name"]   
-        session[:adminRealm] = check["adminRealm"]
-        session[:roles] = check["sliRoles"]
-        session[:edOrg] = check["edOrg"]
-        session[:external_id] = check["external_id"]
+        set_session
       else
         admin_realm = "#{APP_CONFIG['admin_realm']}"
         redirect_to oauth.authorize_url + "&Realm=" + CGI::escape(admin_realm) + "&state=" + CGI::escape(form_authenticity_token)
@@ -129,6 +122,21 @@ class ApplicationController < ActionController::Base
 
   def not_found
   	  raise ActionController::RoutingError.new('Not Found')
+  end
+  
+  def set_session
+    check = Check.get("")
+    if check['authenticated'] == false 
+      raise ActiveResource::UnauthorizedAccess, caller
+    end
+    email = SupportEmail.get("")
+    logger.debug { "Email #{email}"}
+    session[:support_email] = email
+    session[:full_name] ||= check["full_name"]   
+    session[:adminRealm] = check["adminRealm"]
+    session[:roles] = check["sliRoles"]
+    session[:edOrg] = check["edOrg"]
+    session[:external_id] = check["external_id"]
   end
 
 end
