4cd7e7d736281443d695e208e02e357fa3085a0c
Ryan Latta <rlatta@wgen.net>
2012-06-19 17:37:42 -0400

    Teacher tests

--- sli/opstools/edorg/spec/teacher_fixer_spec.rb
+++ sli/opstools/edorg/spec/teacher_fixer_spec.rb
@@ -1,19 +1,29 @@
 require File.dirname(__FILE__) + '/../teacher_fixer'
 
 describe TeacherFixer do
-  before(:each) do
+  def insert_to_collection(collection, hash)
+    new_hash = {:body => hash}
+    @db[collection].insert(new_hash)
+  end
+  before(:all) do
     conn = Mongo::Connection.new('localhost', 27017)
     @db = conn['test']
     Object.send(:db, db)
   end
   describe "#fixing_students" do
+    after(:each) do
+      @db['student'].remove
+      @db['staff'].remove
+      @db['teacherSectionAssociation'].remove
+      @db['studentSectionAssociation'].remove
+    end
     it "should stamp students with section associations" do
-      teacher_id = @db['staff'].insert({:name => "Teacher"})
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
       5.times do |i|
-        student_id = @db['student'].insert({:name => "Student #{i}"})
-        section_id = @db['section'].insert({:name => "Section #{id}"})
-        @db['teacherSectionAssociation'].insert({:sectionId => section_id, :teacherId => teacher_id})
-        @db['studentSectionAssociation'].insert({:studentId => id, :sectionId => section_id})
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        section_id = insert_to_collection @db['section'], {:name => "Section #{id}"}
+        insert_to_collection @db['teacherSectionAssociation'], {:sectionId => section_id, :teacherId => teacher_id}
+        insert_to_collection @db['studentSectionAssociation'], {:studentId => id, :sectionId => section_id}
       end
       Object.send(:fix_students)
       
@@ -24,12 +34,100 @@ describe TeacherFixer do
       end
     end
     it "should stamp students with program assocations" do
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        program_id = insert_to_collection @db['program'], {:name => "program #{id}"}
+        insert_to_collection @db['staffProgramAssociation'], {:programId => [program_id], :staffId => [teacher_id]}
+        insert_to_collection @db['studentProgramAssociation'], {:programId => [program_id], :studentId => [teacher_id]}
+      end
+      Object.send(:fix_students)
       
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should include 'teacherId'
+        student['metaData']['teacherId'].should eql teacher_id
+      end
     end
     it "should should stamp students with cohort associations" do
+      teacher_id = insert_to_collection @db['staff'], {:body =>{:name => "Teacher"}}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        program_id = insert_to_collection @db['cohort'], {:name => "cohort #{id}"}
+        insert_to_collection @db['staffCohortAssociation'], {:cohortId => [program_id], :staffId => [teacher_id]}
+        insert_to_collection @db['studentCohortAssociation'], {:cohortId => [program_id], :studentId => [teacher_id]}
+      end
+      Object.send(:fix_students)
       
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should include 'teacherId'
+        student['metaData']['teacherId'].should eql teacher_id
+      end
     end
-    it "should respect end dates for associations" do
+    it "should respect end dates for student section associations" do
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        section_id = insert_to_collection @db['section'], {:name => "Section #{id}"}
+        insert_to_collection @db['teacherSectionAssociation'], {:sectionId => section_id, :teacherId => teacher_id}
+        insert_to_collection @db['studentSectionAssociation'], {:studentId => id, :sectionId => section_id, :endDate => Date.today - 2001}
+      end
+      Object.send(:fix_students)
+      
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should_not include 'teacherId'
+        student['metaData']['teacherId'].should_not eql teacher_id
+      end
+    end
+    it "should respect end dates for teacher section associations" do
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        section_id = insert_to_collection @db['section'], {:name => "Section #{id}"}
+        insert_to_collection @db['teacherSectionAssociation'], {:sectionId => section_id, :teacherId => teacher_id, :endDate => Date.today - 2001}
+        insert_to_collection @db['studentSectionAssociation'], {:studentId => id, :sectionId => section_id}
+      end
+      Object.send(:fix_students)
+      
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should_not include 'teacherId'
+        student['metaData']['teacherId'].should_not eql teacher_id
+      end
+    end
+    it "should respect end dates for student program associations" do
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        program_id = insert_to_collection @db['program'], {:name => "program #{id}"}
+        insert_to_collection @db['staffProgramAssociation'], {:programId => [program_id], :staffId => [teacher_id]}
+        insert_to_collection @db['studentProgramAssociation'], {:programId => [program_id], :studentId => [teacher_id], :endDate => Date.today - 2001}
+      end
+      Object.send(:fix_students)
+      
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should_not include 'teacherId'
+        student['metaData']['teacherId'].should_not eql teacher_id
+      end
+    end
+    it "should respect end dates for staff program associations" do
+      teacher_id = insert_to_collection @db['staff'], {:name => "Teacher"}
+      5.times do |i|
+        student_id = insert_to_collection @db['student'], {:name => "Student #{i}"}
+        program_id = insert_to_collection @db['program'], {:name => "program #{id}"}
+        insert_to_collection @db['staffProgramAssociation'], {:programId => [program_id], :staffId => [teacher_id], :endDate => Date.today - 2001}
+        insert_to_collection @db['studentProgramAssociation'], {:programId => [program_id], :studentId => [teacher_id]}
+      end
+      Object.send(:fix_students)
+      
+      @db['student'].find.each do |student| 
+        student.should include 'metaData'
+        student['metaData'].should_not include 'teacherId'
+        student['metaData']['teacherId'].should_not eql teacher_id
+      end
     end
   end
 end
