6498c19f1d54ee596abc75a181dafd64da544e11
Simon Yau <syau@wgen.net>
2012-06-21 17:23:54 -0400

    US2086: Clear RSA key area when showing/hiding. Improve indentation.

--- sli/admin-tools/admin-rails/app/assets/javascripts/landing_zone.js
+++ sli/admin-tools/admin-rails/app/assets/javascripts/landing_zone.js
@@ -3,30 +3,32 @@ $(document).ready(function() {
   $("#addKeyBtn").unbind("click");
   $("#addKeyBtn").click(function() {
     $("#rsaKeyField").toggle(250);
-    $("#rsaKeyField").val(""); // this doesn't work, want to clear the text whenever shown/hidden
+    $("#rsaKeyField").find(':input').val('');
     $("#key_arrow").toggleClass("icon-chevron-left, icon-chevron-down");
   });
 
-	if (is_sandbox) {
-  var buttonEnableForCustom = function() {
-    if($("#custom_ed_org").val().length == 0) {
-      $("#provisionButton").attr("disabled","disabled")
+  if (is_sandbox) {
+    var buttonEnableForCustom = function() {
+      if($("#custom_ed_org").val().length == 0) {
+        $("#provisionButton").attr("disabled","disabled")
+      }
+      else {
+        $("#provisionButton").removeAttr("disabled")
+      }
     }
-    else {
-      $("#provisionButton").removeAttr("disabled")
+    $("input[type=radio][id!=custom]").click(function() {
+	$("#custom_ed_org").attr("disabled","disabled");
+	$("#provisionButton").removeAttr("disabled")
+    });
+    $("#custom").click(function() {
+      $("#custom_ed_org").removeAttr("disabled");
+      buttonEnableForCustom()
+    });
+    $("#custom_ed_org").bind('input propertychange', buttonEnableForCustom); 
+    if($("#custom").attr("type") == "radio"){
+      $("#custom_ed_org").attr("disabled","disabled")
     }
+    $("#provisionButton").attr("disabled", "disabled")
   }
-  
-  $("input[type=radio][id!=custom]").click(function() {
-   $("#custom_ed_org").attr("disabled","disabled");
-   $("#provisionButton").removeAttr("disabled")});
-  $("#custom").click(function() {
-   $("#custom_ed_org").removeAttr("disabled");
-   buttonEnableForCustom()});
-  $("#custom_ed_org").bind('input propertychange', buttonEnableForCustom); 
-  if($("#custom").attr("type") == "radio"){
-    $("#custom_ed_org").attr("disabled","disabled")
-  }
-  $("#provisionButton").attr("disabled", "disabled")
-  }
+
 })
