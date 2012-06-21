1efe104de34c4f1970c119fb1eeb1dc9200fd147
Ryan Latta <rlatta@wgen.net>
2012-06-21 13:28:24 -0400

    Fixing a bad route.

--- sli/admin-tools/admin-rails/config/routes.rb
+++ sli/admin-tools/admin-rails/config/routes.rb
@@ -11,7 +11,7 @@ SLIAdmin::Application.routes.draw do
 
   resources :realm_management
   post "landing_zone/provision", :to => 'landing_zone#provision'
-  get "landing_zone/provision", :to => 'landing_zone#success'
+  get "landing_zone/provision", :to => 'landing_zone#provision'
   get "landing_zone", :to => 'landing_zone#index'
 
 
