28068ed81efd97e9dd692e59d91fbd5e578ed598
kmyers <kmyers@wgen.net>
2012-06-21 11:13:10 -0400

    DE188 fix

--- sli/domain/src/main/java/org/slc/sli/validation/schema/DoubleSchema.java
+++ sli/domain/src/main/java/org/slc/sli/validation/schema/DoubleSchema.java
@@ -1,16 +1,9 @@
 package org.slc.sli.validation.schema;
 
-import java.util.List;
-import java.util.Map.Entry;
-
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import org.slc.sli.domain.Repository;
-import org.slc.sli.domain.Entity;
 import org.slc.sli.validation.NeutralSchemaType;
-import org.slc.sli.validation.ValidationError;
-import org.slc.sli.validation.ValidationError.ErrorType;
 
 /**
  *
@@ -21,90 +14,33 @@ import org.slc.sli.validation.ValidationError.ErrorType;
  */
 @Scope("prototype")
 @Component
-public class DoubleSchema extends NeutralSchema {
+public class DoubleSchema extends PrimitiveNumericSchema<Double> {
 
     // Constructors
     public DoubleSchema() {
-        this(NeutralSchemaType.DOUBLE.getName());
+        super(NeutralSchemaType.DOUBLE);
     }
-
-    public DoubleSchema(String xsdType) {
-        super(xsdType);
-    }
-
-    // Methods
-
-    @Override
-    public NeutralSchemaType getSchemaType() {
-        return NeutralSchemaType.DOUBLE;
-    }
+    
 
     @Override
     public Object convert(Object value) {
-        return NumberUtils.converterHelper(value, new NumberUtils.Converter() {
-            @Override
-            public Object convert(Object value) {
+        if (value instanceof Integer) {
+            return ((Integer) value).doubleValue();
+        } else if (value instanceof Long) {
+            return ((Long) value).doubleValue();
+        } else if (value instanceof Float) {
+            return ((Float) value).doubleValue();
+        } else if (value instanceof Double) {
+            return (Double) value;
+        } else if (value instanceof String) {
+            try {
                 return Double.parseDouble((String) value);
-            }
-        });
-    }
-    
-    /**
-     * Validates the given entity
-     * Returns true if the validation was successful or a ValidationException if the validation was
-     * unsuccessful.
-     *
-     * @param fieldName
-     *            name of entity field being validated
-     * @param entity
-     *            being validated using this SLI Schema
-     * @param errors
-     *            list of current errors
-     * @param repo
-     *            reference to the entity repository
-     * @return true if valid
-     */
-    protected boolean validate(String fieldName, Object entity, List<ValidationError> errors, Repository<Entity> repo) {
-        entity = convert(entity);
-        Double data = NumberUtils.toDouble(entity);
-        if (!addError(data != null, fieldName, entity, "Double", ErrorType.INVALID_DATATYPE, errors)) {
-            return false;
-        }
-
-        if (this.getProperties() != null) {
-            for (Entry<String, Object> entry : this.getProperties().entrySet()) {
-                if (Restriction.isRestriction(entry.getKey())) {
-                    double restrictionValue = Double.parseDouble(entry.getValue().toString());
-                    switch (Restriction.fromValue(entry.getKey())) {
-                    case MIN_INCLUSIVE:
-                        if (!addError(data.compareTo(restrictionValue) >= 0, fieldName, entity, "min="
-                                + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
-                    case MAX_INCLUSIVE:
-                        if (!addError(data.compareTo(restrictionValue) <= 0, fieldName, entity, "max="
-                                + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
-                    case MIN_EXCLUSIVE:
-                        if (!addError(data.compareTo(restrictionValue) > 0, fieldName, entity, "min-exclusive="
-                                + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
-                    case MAX_EXCLUSIVE:
-                        if (!addError(data.compareTo(restrictionValue) < 0, fieldName, entity, "max-exclusive="
-                                + restrictionValue, ErrorType.INVALID_VALUE, errors)) {
-                            return false;
-                        }
-                        break;
-                    }
-                }
+            } catch (NumberFormatException nfe) {
+                throw new IllegalArgumentException(value + " cannot be parsed to a double");
             }
         }
-        return true;
+        
+        throw new IllegalArgumentException(value + " is not a double");
     }
 
 }
