dfb7f0bf20d0f67d2c896d826ed0236d4029c1be
Ryan Latta <rlatta@wgen.net>
2012-06-21 17:49:49 -0400

    [DE1063] Versions have new and improved validation.
    Conflicts:
    
    	sli/admin-tools/admin-rails/app/models/app.rb
    	sli/domain/src/main/resources/sliXsd/Application.xsd

--- sli/admin-tools/admin-rails/app/models/app.rb
+++ sli/admin-tools/admin-rails/app/models/app.rb
@@ -20,6 +20,7 @@ limitations under the License.
 class App < SessionResource
   self.format = ActiveResource::Formats::JsonFormat
   validates_presence_of [:description, :application_url, :name, :redirect_uri, :vendor], :message => "must not be blank"
+  validates_format_of :version, :with => /^[A-Za-z0-9\.]{1,25}$/, :message => "must contain only alphanumeric characters and periods and be less than 25 characters long"
 
   def self.all_but_admin
     apps = App.all
