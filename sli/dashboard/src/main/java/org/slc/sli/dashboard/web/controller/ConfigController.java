0752a44abc27f1682d4c11f8e760118591c1d7c7
Igor Ivanisevic <iivanisevic@wgen.net>
2012-06-22 15:31:41 -0400

    DE618, moving the dashboard code to its own package

---
+++ sli/dashboard/src/main/java/org/slc/sli/dashboard/web/controller/ConfigController.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.slc.sli.dashboard.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import com.google.gson.GsonBuilder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import org.slc.sli.dashboard.entity.ConfigMap;
+import org.slc.sli.dashboard.entity.GenericEntity;
+import org.slc.sli.dashboard.manager.ConfigManager;
+import org.slc.sli.dashboard.manager.UserEdOrgManager;
+import org.slc.sli.dashboard.util.Constants;
+import org.slc.sli.dashboard.util.SecurityUtil;
+
+/**
+ * 
+ * DashboardConfigController
+ * This controller handles the dashboard config pages which are only accessible
+ * by IT Admins of a District.
+ * 
+ */
+@Controller
+public class ConfigController extends GenericLayoutController {
+    private static final String DASHBOARD_CONFIG_FTL = "dashboard_config.ftl";
+    private static final String CONFIG_URL = "/service/config";
+    private static final String CONFIG_SAVE_URL = "/service/config/ajaxSave";
+    
+    private UserEdOrgManager userEdOrgManager;
+    private ConfigManager configManager;
+    
+    @Autowired
+    public void setConfigManager(ConfigManager configManager) {
+        this.configManager = configManager;
+    }
+    
+    @Autowired
+    public void setUserEdOrgManager(UserEdOrgManager userEdOrgManager) {
+        this.userEdOrgManager = userEdOrgManager;
+    }
+    
+    /**
+     * Generic layout handler
+     * 
+     * @param id
+     * @param request
+     * @return
+     * @throws IllegalAccessException
+     */
+    @RequestMapping(value = CONFIG_URL, method = RequestMethod.GET)
+    public ModelAndView getConfig(HttpServletRequest request) throws IllegalAccessException {
+        ModelMap model = new ModelMap();
+        
+        String token = SecurityUtil.getToken();
+        GenericEntity staffEntity = userEdOrgManager.getStaffInfo(token);
+        
+        Boolean isAdmin = SecurityUtil.isAdmin();
+        if (isAdmin != null && isAdmin.booleanValue()) {
+            Boolean localEducationAgency = (Boolean) staffEntity.get(Constants.LOCAL_EDUCATION_AGENCY);
+            
+            if (localEducationAgency != null && localEducationAgency.booleanValue()) {
+                ConfigMap configMap = configManager.getCustomConfig(token, userEdOrgManager.getUserEdOrg(token));
+                
+                if (configMap != null) {
+                    model.addAttribute("configJSON", new GsonBuilder().create().toJson(configMap));
+                } else {
+                    model.addAttribute("configJSON", "");
+                }
+            } else {
+                model.addAttribute("configJSON", "nonLocalEducationAgency");
+                
+            }
+            
+            addCommonData(model, request);
+            model.addAttribute(Constants.PAGE_TO_INCLUDE, DASHBOARD_CONFIG_FTL);
+            return new ModelAndView(Constants.OVERALL_CONTAINER_PAGE, model);
+        }
+        throw new IllegalAccessException("Access Denied");
+    }
+    
+    @RequestMapping(value = CONFIG_SAVE_URL, method = RequestMethod.POST)
+    @ResponseBody
+    public String saveConfig(@RequestBody @Valid ConfigMap configMap) {
+        try {
+            putCustomConfig(configMap);
+        } catch (RuntimeException re) {
+            logger.error("Error saving config", re);
+            return "Permission Denied";
+        }
+        return "Success";
+    }
+    
+    public void putCustomConfig(ConfigMap configMap) {
+        String token = SecurityUtil.getToken();
+        configManager.putCustomConfig(token, userEdOrgManager.getUserEdOrg(token), configMap);
+    }
+}
---
+++ sli/dashboard/src/test/java/org/slc/sli/dashboard/unit/controller/DataControllerTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.slc.sli.dashboard.unit.controller;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.slc.sli.dashboard.entity.GenericEntity;
+import org.slc.sli.dashboard.manager.component.impl.CustomizationAssemblyFactoryImpl;
+import org.slc.sli.dashboard.web.controller.DataController;
+import org.slc.sli.dashboard.web.entity.SafeUUID;
+
+/**
+ * Testing data controller
+ *
+ * @author ccheng
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/application-context.xml" })
+public class DataControllerTest {
+
+    private final String componentId = "abc01";
+    private final String rawId = "abcd-abcd-abcd-abcd-abcd";
+
+    private MockHttpServletRequest request;
+    private DataController dataController;
+    private SafeUUID id;
+
+    // partially mock CustomizationAssemblyFactoryImpl
+    CustomizationAssemblyFactoryImpl dataFactory = new CustomizationAssemblyFactoryImpl() {
+
+        @Override
+        public GenericEntity getDataComponent(String componentId, Object entityKey) {
+            GenericEntity simpleEntity = new GenericEntity();
+            simpleEntity.put("id", componentId);
+            return simpleEntity;
+        }
+    };
+
+    @Before
+    public void setup() {
+        request = new MockHttpServletRequest();
+        dataController = new DataController();
+        id = new SafeUUID(rawId);
+    }
+
+    @Test
+    public void testHandleValidId() throws Exception {
+
+        dataController.setCustomizedDataFactory(dataFactory);
+
+        GenericEntity res = dataController.handle(componentId, id, request);
+        Assert.assertEquals(componentId, res.getId());
+    }
+}
