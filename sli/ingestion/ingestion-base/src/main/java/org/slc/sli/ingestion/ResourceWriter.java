0c8227fd751bc93630143a885486cc2804fbbf2b
Stan Halka <shalka@wgen.net>
2012-06-22 19:36:12 +0100

    add insertResources() method for batch insert functionality to data store

--- sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/ResourceWriter.java
+++ sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/ResourceWriter.java
@@ -1,5 +1,7 @@
 package org.slc.sli.ingestion;
 
+import java.util.List;
+
 /**
  * Generic interface for writing resources.
  *
@@ -8,7 +10,28 @@ package org.slc.sli.ingestion;
  */
 public interface ResourceWriter<T> {
 
+    /**
+     * Write an entity to the data store (using 'upsert').
+     * 
+     * @param t Entity to be written.
+     * @param jobId Current batch job id.
+     */
     void writeResource(T t, String jobId);
 
+    /**
+     * Inserts an entity to the data store (using 'insert').
+     * 
+     * @param t Entity to be written.
+     * @param jobId Current batch job id.
+     */
     void insertResource(T t, String jobId);
+
+    /**
+     * Inserts multiple entities to the data store (using 'insert').
+     * 
+     * @param t Entities to be written.
+     * @param collectionName Name of collection to write entities to.
+     * @param jobId Current batch job id.
+     */
+    void insertResources(List<T> neutralRecords, String collectionName, String jobId);
 }
