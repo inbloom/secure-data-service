45e71ac642516d9da2ad91656620c36f2902a220
jtully <jtully@wgen.net>
2012-07-05 15:07:55 -0400

    [US2806] Added rsa key copy unit test

--- sli/admin-tools/admin-rails/test/unit/helpers/landing_zone_helper_test.rb
+++ sli/admin-tools/admin-rails/test/unit/helpers/landing_zone_helper_test.rb
@@ -58,5 +58,16 @@ Nb28W0C5u7TvZrWgWTPbR6sZ2lK1dsmaXa+dsPWwHnvBPEGImThe/nyqIvpyIGXvlMd+4I
       "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6JVegtFBWdLKDE1gQplnHA8PZ+ceAnoduBTnJj+XjBLi4To5DSEFLzF1D6fLorIK3F2GIe+3yGT+wmdhXRFXEtpU+p8MD1ys/w6qR+s57kkV9/tpa3Ako7DwAL2YOIM50dEcWkvNGZbIOSBgjxM/dI6x5YEQZXsRc4wFydcJxQ8K6sN5t0fke8Nb28W0C5u7TvZrWgWTPbR6sZ2lK1dsmaXa+dsPWwHnvBPEGImThe/nyqIvpyIGXvlMd+4I3wx3PoRceyJD/PtsJBYI7NBYiflVnqiLhM1/rRKMOBNqmQat/vwsY6VVxbNkC4a12GM2ipOxzd/1/ZKeKmQ5j5R63Q== srichards@wgen.net\n"
     )
   end
+  
+  test "Key should be copied to specified directory" do
+    keyFilePath = File.join(APP_CONFIG['rsa_key_dir'], "test_key")
+    
+  	LandingZoneHelper.create_key("the_key", "test_key")
+  	assert_equal true, File.file?(keyFilePath)
+  	#check keyFile contents
+  	keyFile = File.open(keyFilePath, 'rb')
+	contents = keyFile.read
+  	assert_equal "the_key", contents
+  end
 
 end
