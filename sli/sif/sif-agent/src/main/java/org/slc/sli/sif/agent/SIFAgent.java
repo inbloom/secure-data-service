f3b4cfc8c07a619320a3b7de1af9cc4b2a110e14
Steve Ashton <sashton@wgen.net>
2012-07-05 11:50:45 -0400

    [US2949] Fixing SIFAgent: part II

---
+++ sli/sif/sif-agent/src/main/java/org/slc/sli/sif/agent/SIFAgent.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.slc.sli.sif.agent;
+
+import java.io.File;
+
+import openadk.library.ADK;
+import openadk.library.ADKException;
+import openadk.library.ADKFlags;
+import openadk.library.Agent;
+import openadk.library.AgentMessagingMode;
+import openadk.library.AgentProperties;
+import openadk.library.SIFVersion;
+import openadk.library.Zone;
+import openadk.library.student.StudentDTD;
+import openadk.library.tools.cfg.AgentConfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import org.slc.sli.sif.subscriber.SifSubscriber;
+
+@Component
+public class SIFAgent extends Agent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SIFAgent.class);
+
+
+    AgentConfig fCfg;
+
+    @Value("classpath:/sif/agent-config.xml")
+    Resource configFile;
+
+    public SIFAgent() {
+        this("SIFAgent");
+    }
+
+    public SIFAgent(String id){
+        super(id);
+        setName(id);
+    }
+
+    public void startAgent() throws Exception {
+
+        File file = configFile.getFile();
+        String configPath = file.getAbsolutePath();
+
+        LOG.info("Using config file: " + configPath);
+
+        // Read the configuration file
+        fCfg = new AgentConfig();
+        fCfg.read(file.getAbsolutePath(), false);
+
+        // Override the SourceId passed to the constructor with the SourceId
+        // specified in the configuration file
+        setId(fCfg.getSourceId());
+
+        // Inform the ADK of the version of SIF specified in the sifVersion=
+        // attribute of the <agent> element
+        SIFVersion version = fCfg.getVersion();
+        LOG.info("Using SIF version: " + version);
+        ADK.setVersion(version);
+
+        // initialize once the configuration file has been read
+        super.initialize();
+
+        fCfg.apply(this, true);
+
+        // Connect to each zone specified in the configuration file, registering
+        // this agent as the Provider of the SIS objects.
+
+        Zone[] allZones = getZoneFactory().getAllZones();
+        for (Zone zone : allZones) {
+            try {
+                // Connect to this zone
+
+                LOG.info("- Connecting to zone \"" + zone.getZoneId() + "\" at " + zone.getZoneUrl());
+
+                zone.setSubscriber(new SifSubscriber(), StudentDTD.SCHOOLINFO);
+                zone.connect(ADKFlags.PROV_REGISTER);
+
+            } catch (ADKException ex) {
+                LOG.error("  " + ex.getMessage(), ex);
+            }
+        }
+    }
+
+    public Zone addZone(String id, String url) throws ADKException {
+        Zone zone = getZoneFactory().getInstance(id, url);
+
+        AgentProperties zoneProps = zone.getProperties();
+        zoneProps.setMessagingMode(AgentMessagingMode.PUSH);
+        zoneProps.setEncryptionLevel(3);
+        zoneProps.setAuthenticationLevel(3);
+
+        zone.connect(ADKFlags.PROV_REGISTER);
+
+        return zone;
+    }
+
+}
+
+//
+// public class SIFAgent extends Agent {
+//
+// // TODO move this configuration to spring
+// private static Map<String, String> zoneIdentifierMap = new HashMap<String, String>();
+// static {
+// zoneIdentifierMap.put("DistrictZone", "http://localhost:1337/DistrictZone");
+// zoneIdentifierMap.put("SchoolZone", "http://localhost:1337/SchoolZone");
+// }
+//
+// public SIFAgent() {
+// this("default");
+// }
+//
+// public SIFAgent(String agentId) {
+// super(agentId);
+// }
+//
+// public void initialize() throws Exception {
+// super.initialize();
+// // Set default properties
+// AgentProperties props = getProperties();
+// props.setMessagingMode( AgentMessagingMode.PUSH );
+//
+// // Set transport protocol properties
+// // TODO
+//
+// for (Entry<String, String> entry : zoneIdentifierMap.entrySet()) {
+// String id = entry.getKey();
+// String url = entry.getValue();
+// addZone(id, url);
+// }
+// }
+//
+// public Zone addZone(String id, String url) throws ADKException {
+// Zone zone = getZoneFactory().getInstance(id, url);
+//
+// AgentProperties zoneProps = zone.getProperties();
+// zoneProps.setMessagingMode(AgentMessagingMode.PUSH);
+// zoneProps.setEncryptionLevel(3);
+// zoneProps.setAuthenticationLevel(3);
+//
+// zone.connect(ADKFlags.PROV_REGISTER);
+//
+// return zone;
+// }
+// }
