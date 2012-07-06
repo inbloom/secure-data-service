1179a3f362ee816ac366934e9e7f6ce0fa41f210
Stan Halka <shalka@wgen.net>
2012-07-06 21:53:46 +0100

    keep all indexes for ingestion staging db in one file

--- sli/config/indexes/is_indexes.js
+++ sli/config/indexes/is_indexes.js
@@ -73,6 +73,62 @@ db["teacher"].ensureIndex({"creationTime":1});
 db["teacherSchoolAssociation"].ensureIndex({"creationTime":1});
 db["teacherSectionAssociation"].ensureIndex({"creationTime":1});
 
+//
+// put indexes on shard keys (both in sharded and un-sharded configurations)
+//
+db["assessment"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["assessment_transformed"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["attendance"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["attendance_transformed"].ensureIndex({"batchJobId" : 1, "_id" : 1, "studentId" : 1, "schoolId" : 1});
+db["calendarDate"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["cohort"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["competencyLevelDescriptor"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["course"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["courseOffering"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["disciplineAction"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["disciplineIncident"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["educationOrganization"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["educationOrganizationAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["educationOrganizationSchoolAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["grade"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["gradebookEntry"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["gradingPeriod"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["graduationPlan"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["learningObjective"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["learningObjective_transformed"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["learningStandard"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["parent"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["program"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["reportCard"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["school"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["section"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["sectionAssessmentAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["sectionSchoolAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["session"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["sessionCourseAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["staff"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["staffCohortAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["staffEducationOrganizationAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["staffProgramAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["student"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentAcademicRecord"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentAssessmentAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentAssessmentAssociation_transformed"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentCohortAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentCompetency"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentCompetencyObjective"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentDisciplineIncidentAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentParentAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentProgramAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentSectionAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentSectionGradebookEntry"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentSchoolAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentTranscriptAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["studentTranscriptAssociation_transformed"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["teacher"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["teacherSchoolAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+db["teacherSectionAssociation"].ensureIndex({"batchJobId" : 1, "_id" : 1});
+
 // 
 // other indexes required for responsiveness of staging database during transformations
 //
