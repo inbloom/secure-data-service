c6eb4644fa38fd82cd052607ebdd1aaacdc0202b
Steve Lee <slee@wgen.net>
2012-07-09 15:55:55 -0400

    Support SchoolInfo mapping from SIF to SLI

--- sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/slientity/SchoolEntity.java
+++ sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/slientity/SchoolEntity.java
@@ -16,6 +16,8 @@
 
 package org.slc.sli.sif.domain.slientity;
 
+import java.util.List;
+
 /**
  * An SLI Entity corresponding to an SLI School.
  *
@@ -24,5 +26,57 @@ package org.slc.sli.sif.domain.slientity;
  */
 public class SchoolEntity extends GenericEntity
 {
+    /*
+     * SLI domain                               SIF domain
+     * ------------------------------mappingg----------------------
+     * stateOrganizationId                      StateProvinceId
+     * nameOfInstitution                        SchoolName
+     * organizationCategories [1..*]
+     * address [1..*]                           AddressList
+     * schoolType [0..1]                        SchoolFocusList
+     * charterStatus [0..1]
+     * titleIPartASchoolDesignation [0..1]
+     * magnetSpecialProgramEmphasisSchool [0..1]
+     * administrativeFundingControl [0..1]
+     * shortNameOfInstitution [0..1]
+     * webSite [0..1]                           SchoolURL
+     * operationalStatus [0..1]                 OperationalStatus
+     * agencyHierarchyName [0..1]
+     * parentEducationAgencyReference [0..1]
+     * gradesOffered [0..*]                     GradeLevels
+     * schoolCategories [0..*]                  SchoolType
+     * educationOrgIdentificationCode [0..*]
+     * telephone [0..*]                         PhoneNumberList
+     * accountabilityRatings [0..*]
+     * programReference [0..*]
+     *
+     */
+    private String stateOrganizationId;
+    private String nameOfInstitution;
+    private List<String> organizationCategories;
+    private String address;
+    private String schoolType;
+    private String charterStatus;
+    private String titleIPartASchoolDesignation;
+    private String magnetSpecialProgramEmphasisSchool;
+    private String shortNameOfInstitution;
+    private String webSite;
+    private String operationalStatus;
+    private String agencyHierarchyName;
+    private String parentEducationAgencyReference;
+    private List<String> gradesOffered;
+    private List<String> schoolCategories;
+    private List<String> educationOrgIdentificationCode;
+    private List<String> telephone;
+    private List<String> accountabilityRatings;
+    private List<String> programReference;
+
+    /**
+     *  Constructor
+     */
+    public SchoolEntity() {
+        super();
+    }
+
 
 }
