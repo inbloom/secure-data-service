a6ee86f1d815242d4d0914c9f77ab7c06c8aa768
Joe Chung <joechung@wgen.net>
2012-07-09 17:28:44 -0400

    added gherkins and stubbed step definitions for user admin acceptance tests

---
+++ sli/acceptance-tests/test/features/apiV1/user_admin/step_definitions/user_admin_crud.rb
@@ -0,0 +1,103 @@
+Given /^I have a SLC_Operator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am authenticated on Shared Learning Collaborative$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I navigate to GET <EdOrg_URI>$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the "(.*?)" is IL$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should receive a return code of (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should receive a list of Ed\-Org and  is in the list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a SEA_Super_Administrator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am authenticated on IL$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should receive a list of Ed\-Org and IL is in the list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a LEA_Super_Administrator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am authenticated on IL\-DAYBREAK$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should receive a list of Ed\-Org and IL\-DAYBREAK is in the list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a Realm_Administrator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a Ingestion_Administrator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I navigate to GET <ADMIN_ACCOUT_URI>$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^then I should receive a list (\d+) of SLC_Operator$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^each account has SLC_Operator_(\d+), <User_name>, SLC_Operator@test\.com, "(.*?)" and "(.*?)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^then I should receive a list (\d+) of SEA_Super_Administrator$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^each account has SEA_Admin, <User_name>, SeaAdmin@test\.com, "(.*?)" and "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^then I should receive a list (\d+) of LEA_Super_Administrator$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^each account has LEA_Admin, <User_name>, LeaAdmin@test\.com, "(.*?)" and "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^then I should receive a list (\d+) of Realm_Administrator$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^each account has Realm_Admin, <User_name>, RealmAdmin@test\.com, "(.*?)" and "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^then I should receive a list (\d+) of Ingestion_Administrator$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^each account has Ingest_Admin, <User_name>, IngestAdmin@test\.com, "(.*?)" and "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am authenticated on IL\-SUNSET$/ do
+  pending # express the regexp above with the code you wish you had
+end
