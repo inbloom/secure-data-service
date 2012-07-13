1c1a94cac3953e57e57a5bf78b5becf8b4d966f6
Suneth Rupasinghe <srupasinghe@wgen.net>
2012-07-12 23:13:38 -0400

    moving natural key validation to apientityvalidator

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoEntityRepository.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoEntityRepository.java
@@ -17,15 +17,10 @@
 
 package org.slc.sli.dal.repository;
 
-import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Date;
 
-import org.slc.sli.validation.EntityValidationException;
-import org.slc.sli.validation.NaturalKeyValidationException;
-import org.slc.sli.validation.ValidationError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -43,8 +38,6 @@ import org.slc.sli.dal.encrypt.EntityEncryption;
 import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.EntityMetadataKey;
 import org.slc.sli.domain.MongoEntity;
-import org.slc.sli.domain.NeutralCriteria;
-import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.validation.EntityValidator;
 
 /**
@@ -57,7 +50,6 @@ import org.slc.sli.validation.EntityValidator;
 
 public class MongoEntityRepository extends MongoRepository<Entity> implements InitializingBean {
     protected static final Logger LOG = LoggerFactory.getLogger(MongoEntityRepository.class);
-    private static final String TENANT_ID = "tenantId";
 
     private static final int PADDING = 300;
 
@@ -110,9 +102,6 @@ public class MongoEntityRepository extends MongoRepository<Entity> implements In
         Entity entity = new MongoEntity(type, null, body, metaData, PADDING);
         validator.validate(entity);
 
-        // natural fields are only applicable for API crud operations
-        validateNaturalKeys(entity, collectionName);
-
         this.addTimestamps(entity);
         return super.create(entity, collectionName);
     }
@@ -144,9 +133,6 @@ public class MongoEntityRepository extends MongoRepository<Entity> implements In
     public boolean update(String collection, Entity entity) {
         validator.validate(entity);
 
-        // natural fields can only be applicable for API crud operations
-        validateNaturalKeys(entity, collection);
-
         this.updateTimestamp(entity);
 
         // Map<String, Object> body = entity.getBody();
@@ -170,79 +156,6 @@ public class MongoEntityRepository extends MongoRepository<Entity> implements In
     public void updateTimestamp(Entity entity) {
         Date now = DateTimeUtil.getNowInUTC();
         entity.getMetaData().put(EntityMetadataKey.UPDATED.getKey(), now);
-    }
-
-    /**
-     *
-     * @param entity
-     * @param collectionName
-     * @return
-     */
-    private void validateNaturalKeys(final Entity entity, String collectionName) {
-
-        boolean recordMatchingKeyFields = false;
-        List<String> naturalKeyList = validator.getNaturalKeyFields(entity);
-
-        if (naturalKeyList != null && naturalKeyList.size() != 0) {
-
-            Map<String, Object> newEntityBody = entity.getBody();
-            boolean possibleMatch = true;
-            String entityId = entity.getEntityId();
-
-            // if we have an existing entityId, then we're doing an update. Check to
-            // make sure that there is no existing entity with the new key fields of the entity
-            if (entityId != null && !entityId.isEmpty()) {
-                possibleMatch = false;
-                NeutralQuery neutralQuery = new NeutralQuery();
-                neutralQuery.addCriteria(new NeutralCriteria("_id", "=", entity.getEntityId()));
-                neutralQuery.addCriteria(new NeutralCriteria("metaData.tenantId", NeutralCriteria.OPERATOR_EQUAL, entity.getMetaData().get(TENANT_ID), false));
-
-                Entity existingEntity = super.findOne(collectionName, neutralQuery);
-                if (existingEntity != null) {
-                    for (String key : naturalKeyList) {
-
-                        Map<String, Object> existingBody = existingEntity.getBody();
-                        Object value = existingBody.get(key);
-                        if (!newEntityBody.containsKey(key)) {
-                            newEntityBody.put(key, value);
-                        } else {
-                            String existingValueString = value.toString();
-                            String newValueString = newEntityBody.get(key).toString();
-
-                            // if all values are equal, then we're ok - as we're just trying to catch
-                            // the case where a key field has been changed, and check the new target
-                            if (!existingValueString.equals(newValueString)) {
-                                possibleMatch = true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // At this point we either have a possible match on the entity to create or the new key fields of the entity we're updating
-            if (possibleMatch) {
-                NeutralQuery neutralQuery = new NeutralQuery();
-                for (String key : naturalKeyList) {
-                    neutralQuery.addCriteria(new NeutralCriteria(key, NeutralCriteria.OPERATOR_EQUAL, newEntityBody.get(key)));
-                }
-                neutralQuery.addCriteria(new NeutralCriteria("metaData.tenantId", NeutralCriteria.OPERATOR_EQUAL, entity.getMetaData().get(TENANT_ID), false));
-
-                Entity existingEntity = super.findOne(collectionName, neutralQuery);
-                if (existingEntity != null) {
-                    recordMatchingKeyFields = true;
-                }
-            }
-        }
-
-        if (recordMatchingKeyFields) {
-            if (entity.getEntityId() != null && !entity.getEntityId().isEmpty()) {
-                List<ValidationError> errors = new ArrayList<ValidationError>();
-                throw new EntityValidationException(entity.getEntityId(), entity.getType(), errors);
-            } else {
-                throw new NaturalKeyValidationException(entity.getType());
-            }
-        }
-
     }
 
 }
