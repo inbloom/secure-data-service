2a9b23014cdf6ac183aa1d3669843df4834fad81
Jerry Hung <chung@wgen.net>
2012-07-13 11:12:45 -0400

    Moved old association tests to a sub folder. Keeping them for now...

---
+++ sli/acceptance-tests/test/features/apiV1/associations/legacy_tests/teacherSectionAssociation/step_definitions/teacher_section_association_steps.rb
@@ -0,0 +1,92 @@
+=begin
+
+Copyright 2012 Shared Learning Collaborative, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=end
+
+
+require 'rest-client'
+require 'json'
+require 'builder'
+require 'rexml/document'
+include REXML
+require_relative '../../../../../utils/sli_utils.rb'
+require_relative '../../../../utils/api_utils.rb'
+
+###############################################################################
+# TRANSFORM TRANSFORM TRANSFORM TRANSFORM TRANSFORM TRANSFORM TRANSFORM
+###############################################################################
+
+Transform /^<([^"]*)>$/ do |human_readable_id|
+
+  #teacher section association data
+  id = 9                                        if human_readable_id == "ASSOCIATION COUNT"
+  id = 2                                        if human_readable_id == "ASSOCIATION COUNT FOR ENDPOINT 1"
+  id = 1                                        if human_readable_id == "ASSOCIATION COUNT FOR ENDPOINT 2"
+  id = 2                                        if human_readable_id == "RESOLUTION COUNT FOR ENDPOINT 1"
+  id = 1                                        if human_readable_id == "RESOLUTION COUNT FOR ENDPOINT 2"
+  id = "32b86a2a-e55c-4689-aedf-4b676f3da3fc"   if human_readable_id == "ASSOCIATION ID"
+  id = "32b86a2a-e55c-4689-aedf-4b676f3da3fc"   if human_readable_id == "ASSOCIATION ID FOR UPDATE"
+  id = "32b86a2a-e55c-4689-aedf-4b676f3da3fc"   if human_readable_id == "ASSOCIATION ID FOR DELETE"
+  id = "getTeacherSectionAssociations"  		if human_readable_id == "ASSOCIATION LINK NAME"
+  id = "teacherSectionAssociation"     			if human_readable_id == "ASSOCIATION TYPE"
+  id = "teacherSectionAssociations"     		if human_readable_id == "ASSOCIATION URI"
+  
+  #teacher related data
+  id = "teacherId"                         		if human_readable_id == "ENDPOINT1 FIELD"
+  id = "e9ca4497-e1e5-4fc4-ac7b-24bad1f2998b"   if human_readable_id == "ENDPOINT1 ID"          or human_readable_id == "ENDPOINT1 FIELD EXPECTED VALUE"
+  id = "getTeacher"                             if human_readable_id == "ENDPOINT1 LINK NAME" 
+  id = "getTeachers"                            if human_readable_id == "ENDPOINT1 RESOLUTION LINK NAME" 
+  id = "teacher"                                if human_readable_id == "ENDPOINT1 TYPE" 
+  id = "teachers"                               if human_readable_id == "ENDPOINT1 URI" 
+  
+  #educationOrganization related data
+  id = "sectionId"         						if human_readable_id == "ENDPOINT2 FIELD"
+  id = "15ab6363-5509-470c-8b59-4f289c224107"   if human_readable_id == "ENDPOINT2 ID"          or human_readable_id == "ENDPOINT2 FIELD EXPECTED VALUE"
+  id = "getSection"               				if human_readable_id == "ENDPOINT2 LINK NAME" 
+  id = "getSections"              				if human_readable_id == "ENDPOINT2 RESOLUTION LINK NAME" 
+  id = "section"                  				if human_readable_id == "ENDPOINT2 TYPE" 
+  id = "sections"                 				if human_readable_id == "ENDPOINT2 URI" 
+  
+  #update related field data
+  id = "sectionId"                         		if human_readable_id == "UPDATE FIELD"
+  id = "15ab6363-5509-470c-8b59-4f289c224107"   if human_readable_id == "UPDATE FIELD EXPECTED VALUE" 
+  id = "706ee3be-0dae-4e98-9525-f564e05aa388"   if human_readable_id == "UPDATE FIELD NEW VALID VALUE" 
+  
+  #general
+  id = "11111111-1111-1111-1111-111111111111"   if human_readable_id == "INVALID REFERENCE"
+  id = "04f708bc-928b-420d-a440-f1592a5d1073"   if human_readable_id == "INACCESSIBLE REFERENCE 1"
+  id = "a47eb9aa-1c97-4c8e-9d0a-45689a66d4f8"   if human_readable_id == "INACCESSIBLE REFERENCE 2"
+  id = "self"                                   if human_readable_id == "SELF LINK NAME" 
+  id = @newId                                   if human_readable_id == "NEWLY CREATED ASSOCIATION ID"
+  id = "Validation failed"                      if human_readable_id == "VALIDATION"
+  id = "Invalid reference. No association to referenced entity." if human_readable_id == "BAD REFERENCE"
+
+  #return the translated value
+  id
+end
+
+
+###############################################################################
+# GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN GIVEN
+###############################################################################
+
+Given /^a valid association json document for a "([^"]*)"$/ do |arg1|
+  @fields = {
+    "teacherId" => "edce823c-ee28-4840-ae3d-74d9e9976dc5",
+    "sectionId" => "7295e51e-cd51-4901-ae67-fa33966478c7",
+    "classroomPosition" => "Teacher of Record"
+}
+end
