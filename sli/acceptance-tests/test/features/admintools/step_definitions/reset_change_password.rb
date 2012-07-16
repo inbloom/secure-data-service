a24bc72e9a9303fa15c1d6fa54fabc0e2b9323ec
Vijay Ummalaneni <vummalaneni@wgen.net>
2012-07-16 09:50:33 -0400

    US2121 Acceptance tests for Reset Password

---
+++ sli/acceptance-tests/test/features/admintools/step_definitions/reset_change_password.rb
@@ -0,0 +1,97 @@
+=begin
+
+Copyright 2012 Shared Learning Collaborative, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=end
+
+
+require "selenium-webdriver"
+require 'json'
+require 'net/imap'
+require 'ldapstorage'
+
+require_relative '../../utils/sli_utils.rb'
+require_relative '../../utils/selenium_common.rb'
+require 'date'
+
+Given /^I am a SLI Developer "([^"]*)" from the "([^"]*)" hosted directory$/ do |arg1, arg2|
+  # No code needed, done as configuration
+end
+
+Given /^I am a SLC Operator "([^"]*)" from the "([^"]*)" hosted directory$/ do |arg1, arg2|
+  # No code needed, done as configuration
+end
+
+When /^I hit the Change Password URL$/ do
+  @driver.get(PropLoader.getProps['admintools_server_url'] + "/change_passwords/new")
+end
+
+When /^I hit the Admin URL$/ do
+  @driver.get(PropLoader.getProps['admintools_server_url'])
+end
+
+When /^I visit the link sent to "(.*?)"$/ do |arg1|
+  @ldap = LDAPStorage.new(PropLoader.getProps['ldap_hostname'], PropLoader.getProps['ldap_port'], 
+                          PropLoader.getProps['ldap_base'], PropLoader.getProps['ldap_admin_user'], 
+                          PropLoader.getProps['ldap_admin_pass'])
+  user = @ldap.read_user(arg1)
+  resetKey = user[:resetKey].split("@")[0]
+  @driver.get(PropLoader.getProps['admintools_server_url'] + "/resetPassword?key=" + resetKey)
+end
+
+Then /^I am redirected to the Change Password page$/ do
+  assertWithWait("Failed to navigate to the Change Password page")  {@driver.page_source.index("Change Password") != nil}
+end
+
+Then /^I am redirected to the Forgot Password page$/ do
+  assertWithWait("Failed to navigate to the Forgot Password page")  {@driver.page_source.index("Forgot Password") != nil}
+end
+
+Then /^I am redirected to the Reset Password page$/ do
+  assertWithWait("Failed to navigate to the Reset Password page")  {@driver.page_source.index("Reset Password") != nil}
+end
+
+Then /^I see the input boxes to change my password$/ do
+  assertWithWait("Failed to find the input boxes to change my password") {@driver.find_element(:id, "new_change_password")}
+end
+
+Then /^I see the input box to enter user id$/ do
+  assertWithWait("Failed to find the input boxes to change my password") {@driver.find_element(:id, "user_id")}
+end
+
+When /^I fill out the input field "([^\"]*)" as "([^\"]*)"$/ do |field, value|
+  trimmed = field.sub(" ", "")
+  @driver.find_element(:xpath, "//input[contains(
+    translate(@id, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '#{trimmed.downcase}')
+    ]").send_keys(value)
+end
+
+Then /^I click on "(.*?)"$/ do |arg1|
+  @driver.find_element(:xpath, "//input[contains(@id, '#{arg1}')]").click
+end
+
+Then /^I click the "(.*?)" link$/ do |arg1| 
+  @driver.find_element(:xpath, "//a[contains(@id, '#{arg1}')]").click
+end
+
+Then /^I see an error message "(.*?)"$/ do |errorMsg|
+  assert(@driver.find_element(:xpath, "//h2[contains(@id, 'errorCountNotifier' and contains(text(), '#{errorMsg}')]").size != 0,
+         "Cannot find error message \"#{errorMsg}\"")
+end
+
+Then /^I check for message  "(.*?)"$/ do |arg1|
+  assertText(arg1)
+end
+
