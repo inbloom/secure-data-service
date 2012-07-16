35b1b5fafd5e9c78b5a8f410a2d295d09385a61c
Vijay Ummalaneni <vummalaneni@wgen.net>
2012-07-16 16:31:10 -0400

    US2121 Unit Tests

--- sli/admin-tools/admin-rails/test/functional/forgot_passwords_controller_test.rb
+++ sli/admin-tools/admin-rails/test/functional/forgot_passwords_controller_test.rb
@@ -1,49 +1,40 @@
+=begin
+
+Copyright 2012 Shared Learning Collaborative, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=end
+
 require 'test_helper'
 
 class ForgotPasswordsControllerTest < ActionController::TestCase
   setup do
-    @forgot_password = forgot_passwords(:one)
+     @forgot_password = ForgotPassword.new(
+        :token => 's9a8qwiwdd9ww23e223e22e2e2wdqw==',
+        :new_pass => 'testabcd',
+        :confirmation => 'testabcd'
+    )
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:forgot_passwords)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
   end
 
   test "should create forgot_password" do
-    assert_difference('ForgotPassword.count') do
-      post :create, forgot_password: @forgot_password.attributes
-    end
-
-    assert_redirected_to forgot_password_path(assigns(:forgot_password))
-  end
-
-  test "should show forgot_password" do
-    get :show, id: @forgot_password.to_param
+    post :create, forgot_password: { token: @forgot_password.token, new_pass: @forgot_password.new_pass, confirmation: @forgot_password.confirmation }
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @forgot_password.to_param
-    assert_response :success
-  end
-
-  test "should update forgot_password" do
-    put :update, id: @forgot_password.to_param, forgot_password: @forgot_password.attributes
-    assert_redirected_to forgot_password_path(assigns(:forgot_password))
-  end
-
-  test "should destroy forgot_password" do
-    assert_difference('ForgotPassword.count', -1) do
-      delete :destroy, id: @forgot_password.to_param
-    end
-
-    assert_redirected_to forgot_passwords_path
-  end
 end
