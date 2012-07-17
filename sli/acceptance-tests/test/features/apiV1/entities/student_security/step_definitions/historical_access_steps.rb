24f4da8623d9b7101963839c9b9d050e4cb1b646
Mark Abernathy <mabernathy@wgen.net>
2012-07-17 14:54:32 -0400

    Added reportCard to historical access tests

--- sli/acceptance-tests/test/features/apiV1/entities/student_security/step_definitions/historical_access_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/entities/student_security/step_definitions/historical_access_steps.rb
@@ -21,7 +21,7 @@ require_relative '../../../../utils/sli_utils.rb'
 
 Then /^I should be able to access data about (the student "[^"]*")$/ do |arg1|
   @format = "application/vnd.slc+json"
-  ["attendances", "studentAssessments"].each do |endpoint|
+  ["attendances", "studentAssessments", "reportCards"].each do |endpoint|
     restHttpGet("/v1/#{endpoint}?studentId=#{arg1}")
     assert(@res != nil, "Response from rest-client GET is nil")
     assert(@res.code == 200, "Get on endpoint #{endpoint}, expected code: 200 but actual code was #{@res.code}")
@@ -32,7 +32,7 @@ end
 
 Then /^I should not be able to access data about (the student "[^"]*")$/ do |arg1|
   @format = "application/vnd.slc+json"
-  ["attendances", "studentAssessments"].each do |endpoint|
+  ["attendances", "studentAssessments", "reportCards"].each do |endpoint|
     restHttpGet("/v1/#{endpoint}?studentId=#{arg1}")
     assert(@res != nil, "Response from rest-client PUT is nil")
     assert(@res.code == 403, "Get on endpoint #{endpoint}, expected code: 200 but actual code was #{@res.code}")
