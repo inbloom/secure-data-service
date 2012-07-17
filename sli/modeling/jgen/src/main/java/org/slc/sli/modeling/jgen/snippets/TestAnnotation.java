bcb7cd4f366f2a0a92270f58cc20b02b91ade8bc
Jeffrey Stokes <jstokes@wgen.net>
2012-07-17 15:00:10 -0400

    new annotation java snippets

---
+++ sli/modeling/jgen/src/main/java/org/slc/sli/modeling/jgen/snippets/TestAnnotation.java
@@ -0,0 +1,10 @@
+package org.slc.sli.modeling.jgen.snippets;
+
+/**
+ * @author jstokes
+ */
+public class TestAnnotation extends Annotation {
+    public TestAnnotation() {
+        super("Test");
+    }
+}
