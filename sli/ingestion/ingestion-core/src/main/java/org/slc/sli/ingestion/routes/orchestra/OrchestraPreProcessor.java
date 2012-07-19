e1c1dff7e359412432c31bdf5531261aa68fa96f
Danilo Duran <dduran@wgen.net>
2012-07-19 11:28:02 -0400

    cleanup unused method

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/routes/orchestra/OrchestraPreProcessor.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/routes/orchestra/OrchestraPreProcessor.java
@@ -16,34 +16,32 @@
 
 package org.slc.sli.ingestion.routes.orchestra;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.slc.sli.ingestion.EdfiEntity;
-import org.slc.sli.ingestion.IngestionStagedEntity;
-import org.slc.sli.ingestion.NeutralRecord;
-import org.slc.sli.ingestion.WorkNote;
-import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
-import org.slc.sli.ingestion.model.da.BatchJobDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import org.slc.sli.ingestion.EdfiEntity;
+import org.slc.sli.ingestion.IngestionStagedEntity;
+import org.slc.sli.ingestion.WorkNote;
+import org.slc.sli.ingestion.dal.NeutralRecordMongoAccess;
+import org.slc.sli.ingestion.model.da.BatchJobDAO;
+
 /**
- * 
+ *
  * @author dduran
- * 
+ *
  */
 @Component
 public class OrchestraPreProcessor implements Processor {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(OrchestraPreProcessor.class);
-    
+
     @Autowired
     private BatchJobDAO batchJobDAO;
 
@@ -57,16 +55,11 @@ public class OrchestraPreProcessor implements Processor {
 
         String jobId = workNote.getBatchJobId();
         exchange.getIn().setHeader("jobId", jobId);
-        
+
         LOG.info("Looking up staged entities for batch job: {}", jobId);
-        
+
         Set<String> stagedCollectionNames = neutralRecordMongoAccess.getRecordRepository().getStagedCollectionsForJob(
                 jobId);
-
-        // ******
-        // Ensure ordering
-        // EdOrgs can only be processes if SEA->LEA->School
-        // rearrangeData(jobId);
 
         Set<IngestionStagedEntity> stagedEntities = constructStagedEntities(stagedCollectionNames);
 
@@ -96,41 +89,6 @@ public class OrchestraPreProcessor implements Processor {
         }
         LOG.debug("staged entities for job: {}", stagedEntities);
         return stagedEntities;
-    }
-
-    @SuppressWarnings({ "unchecked", "unused" })
-    private void rearrangeData(String jobId) {
-        Iterable<NeutralRecord> edorgs = neutralRecordMongoAccess.getRecordRepository().findAll(
-                EdfiEntity.EDUCATION_ORGANIZATION.getEntityName());
-        List<List<NeutralRecord>> recs = new ArrayList<List<NeutralRecord>>();
-
-        recs.add(new ArrayList<NeutralRecord>());   // SEA
-        recs.add(new ArrayList<NeutralRecord>());   // LEA
-        recs.add(new ArrayList<NeutralRecord>());   // School
-
-        for (NeutralRecord nr : edorgs) {
-            List<String> categories = (List<String>) nr.getAttributes().get("organizationCategories");
-            if (categories != null) {
-                int index = -1;
-                if (categories.contains("State Education Agency")) {
-                    index = 0;
-                } else if (categories.contains("Local Education Agency")) {
-                    index = 1;
-                } else if (categories.contains("School")) {
-                    index = 2;
-                }
-
-                recs.get(index).add(nr);
-            }
-        }
-
-        neutralRecordMongoAccess.getRecordRepository().deleteAll(EdfiEntity.EDUCATION_ORGANIZATION.getEntityName());
-
-        for (List<NeutralRecord> list : recs) {
-            for (NeutralRecord nr : list) {
-                neutralRecordMongoAccess.getRecordRepository().createForJob(nr, jobId);
-            }
-        }
     }
 
 }
