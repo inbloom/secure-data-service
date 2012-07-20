a5cca77f6977e4d84bde46f6cf6cf716ff52dbfb
asaarela <asaarela@wgen.net>
2012-07-20 15:58:59 -0400

    Aggregate to the school

--- POC/aggregation/simple-script/math-aggregation.rb
+++ POC/aggregation/simple-script/math-aggregation.rb
@@ -47,6 +47,7 @@ time = Benchmark.realtime do
                           @emitCount = @emitCount + 1
                           @cv.signal
                           Thread.pass
+                          @cv.wait(@mutex)
                         end
                       end
                     }
@@ -73,11 +74,13 @@ time = Benchmark.realtime do
             @vals[id] = val
           end
         end
+        @cv.signal
       end
     end
   }
 
   m1.join()
+  r.kill()
 
   # print(@vals)
 
@@ -86,8 +89,88 @@ time = Benchmark.realtime do
     student = studentColl.find_one('_id' => key)
     studentColl.update({"_id" => key}, {"$set" => { "aggregations.assessments." + @assmtIDCode + ".HighestEver" => val.to_s()}})
   }
+
+
+  # roll up to the school level.
+  def emit_student_scores
+    coll = @db['student']
+    assessment = nil
+      coll.find.each { |row|
+      if row['aggregations'].nil? == false then
+        aggregations = row['aggregations']
+        if aggregations['assessments'].nil? == false then
+          assessments = aggregations['assessments']
+          if assessments[@assmtIDCode].nil? == false then
+            assessment = assessments[@assmtIDCode]
+          end
+        end
+      end
+
+      @mutex.synchronize do
+        if assessment.nil? then
+          @emit = [row['_id'], nil]
+        else
+          @emit = [row['_id'], assessment['HighestEver'].to_f()]
+        end
+        @cv.signal
+        Thread.pass
+        @cv.wait(@mutex)
+      end
+      @emitCount = @emitCount + 1
+    }
+    @cv.signal
+  end
 end
+
+@rvals = Hash.new
+r = Thread.new {
+  loop do
+    @mutex.synchronize do
+      @cv.wait(@mutex)
+      if @emit.nil? == false then
+
+        schools = @db['studentSchoolAssociation']
+        school = schools.find_one('body.studentId' => @emit[0])
+        schoolId = school['body']['schoolId']
+        val = @emit[1]
+
+        if @rvals[schoolId].nil? then
+          @rvals[schoolId] = Hash.new
+          @rvals[schoolId]['W'] = 0
+          @rvals[schoolId]['B'] = 0
+          @rvals[schoolId]['S'] = 0
+          @rvals[schoolId]['E'] = 0
+          @rvals[schoolId]['!'] = 0
+          @rvals[schoolId]['-'] = 0
+        end
+
+        if val.nil? then
+          @rvals[schoolId]['-'] = @rvals[schoolId]['-'] + 1
+        elsif val < 6 || val > 33 then
+          @rvals[schoolId]['!'] = @rvals[schoolId]['!'] + 1
+        elsif val >= 6 && val <= 14 then
+          @rvals[schoolId]['W'] = @rvals[schoolId]['W'] + 1
+        elsif val >= 15 && val <= 20 then
+          @rvals[schoolId]['B'] = @rvals[schoolId]['B'] + 1
+        elsif val >=21 && val <= 27 then
+          @rvals[schoolId]['S'] = @rvals[schoolId]['S'] + 1
+        elsif val >= 28 && val <=33 then
+          @rvals[schoolId]['E'] = @rvals[schoolId]['E'] + 1
+        else
+          @rvals[schoolId]['!'] = @rvals[schoolId]['!'] + 1
+        end
+      end
+      @cv.signal
+    end
+  end
+}
+
+t1 = Thread.new { emit_student_scores() }
+
+t1.join()
+
+puts(@rvals)
 
 puts "Time elapsed #{time} seconds."
 puts "Total emit operations: #{@emitCount}"
-puts "Total reduce set: #{@vals.length}"
+puts "Total reduce set: #{@vals.length + @rvals.length}"
