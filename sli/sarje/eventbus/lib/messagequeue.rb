ce09cfd47e32da315637a283130e731305278e95
Joe Chung <joechung@wgen.net>
2012-07-26 16:15:30 -0400

    refactored

--- sli/sarje/eventbus/lib/messagequeue.rb
+++ sli/sarje/eventbus/lib/messagequeue.rb
@@ -20,7 +20,7 @@ module Eventbus
       :parse_timeout => 5,
   }
 
-  class Agent
+  class Publisher
     def initialize(queue_name)
       @queue_name = queue_name
       @client = Stomp::Client.new(HASH)
@@ -31,60 +31,63 @@ module Eventbus
     end
   end
 
-  class Listener
+  class Subscriber
     def initialize(queue_name)
-      @client = Stomp::Client.new(HASH)
-      @client.subscribe queue_name do |message|
-        yield JSON.parse message.body
+      Thread.new do
+        @client = Stomp::Client.new(HASH)
+        @client.subscribe queue_name do |message|
+          yield JSON.parse message.body
+        end
+        @client.join
+        @client.close
       end
-    end
-
-    def merge
-      @client.join
-      @client.close
     end
   end
 end
 
 queue_name = '/queue/oplog'
-agent_queue = Queue.new
 
-agent = Eventbus::Agent.new queue_name
-Thread.new do
-  loop do
-    message = agent_queue.pop
-    puts "publishing message #{message}"
-    agent.publish message
-  end
+subscriber_queue = Queue.new
+subscriber = Eventbus::Subscriber.new queue_name do |message|
+  subscriber_queue << message
 end
 
 Thread.new do
   loop do
-    message = {
-        'event_type' => 'heartbeat',
-        'agent_id' => 'agent 1',
-        'hostname' => Socket.gethostname,
-        'timestamp' => Time.now.to_i.to_s
-    }.to_json
-    agent_queue << message
-    sleep 5
-  end
-end
-
-listener_queue = Queue.new
-listener = Eventbus::Listener.new queue_name do |message|
-  listener_queue << message
-end
-
-Thread.new do
-  loop do
-    message = listener_queue.pop
+    message = subscriber_queue.pop
     if message["event_type"] == "heartbeat"
-      puts "received heartbeat from host = #{message['hostname']}, agent id = #{message['agent_id']}"
+      puts "received heartbeat #{message}"
     else
       puts "received regular message: #{message}"
     end
   end
 end
 
-listener.merge
+sleep 2
+publisher_queue = Queue.new
+publisher = Eventbus::Publisher.new queue_name
+Thread.new do
+  loop do
+    message = publisher_queue.pop
+    puts "publishing message #{message}"
+    publisher.publish message
+  end
+end
+
+Thread.new do
+  counter = 0
+  loop do
+    counter = counter + 1
+    message = {
+        'event_type' => 'heartbeat',
+        'event_id' => counter,
+        'agent_id' => 'agent 1',
+        'hostname' => Socket.gethostname,
+        'timestamp' => Time.now.to_i.to_s
+    }.to_json
+    publisher_queue << message
+    sleep 5
+  end
+end
+
+sleep
