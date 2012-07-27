efdc56494a091b436bca9311b9ed4f74fa9815a4
Joe Chung <joechung@wgen.net>
2012-07-27 17:50:04 -0400

    implemented oplog agent with basic tests

--- sli/sarje/eventbus/lib/oplogagent.rb
+++ sli/sarje/eventbus/lib/oplogagent.rb
@@ -1,10 +1,134 @@
+testdir = File.dirname(__FILE__)
+$LOAD_PATH << testdir + "/../lib"
+
 require 'mongo'
+require 'thread'
+require 'eventbus'
 
 module Eventbus
+  class OpLogReader
+    def read_oplogs
+      @thread ||= Thread.new do
+        connect_to_mongo_oplog
+        loop do
+          while not @cursor.closed?
+            begin
+              if doc = @cursor.next_document
+                # only care about insert, update, delete
+                yield doc if ["i", "u", "d"].include?(doc["op"])
+              end
+            rescue Exception => e
+              puts e
+              connect_to_mongo_oplog
+            end
+          end
+        end
+      end
+    end
 
-    class OpLogAgent
-        # OpLog collection 
-        Mongo_Coll_OpLog = "oplog.rs"
-    end    
+    def connect_to_mongo_oplog
+      begin
+        db = Mongo::Connection.new("localhost", 27017).db("local")
+        coll = db['oplog.rs']
+        @cursor = Mongo::Cursor.new(coll, :timeout => false, :tailable => true)
+      rescue Exception => e
+        puts "exception occurred when connecting to mongo for oplog: #{e}"
+        puts "retrying connection in 5 seconds"
+        sleep 5
+        retry
+      end
+    end
+  end
 
+  class OpLogThrottler
+    def initialize
+      @oplog_queue = Queue.new
+      @collection_filter_lock = Mutex.new
+      set_collection_filter([])
+    end
+
+    def run
+      Thread.new do
+        sleep 5
+        counter = 0
+        begin
+          loop do
+            @oplog_queue.pop(true)
+            counter = counter + 1
+          end
+        rescue
+          # no more oplog in oplog queue
+        end
+        puts "ignore first oplog reading: #{counter} oplogs detected"
+
+        loop do
+          sleep 5
+          collection_changed = Set.new
+          begin
+            loop do
+              message = @oplog_queue.pop(true)
+              collection_changed << message["ns"]
+            end
+          rescue
+            # no more oplog in oplog queue
+          end
+          puts "collection changed = #{collection_changed.to_a}"
+          collection_filter = get_collection_filter
+          collection_changed = collection_filter & collection_changed.to_a
+          if(collection_changed != nil && collection_changed != [])
+            message = {
+                "collections" => collection_changed
+            }
+            yield message
+          end
+        end
+      end
+    end
+
+    def push(oplog)
+      @oplog_queue.push(oplog)
+    end
+
+    def set_collection_filter(collection_filter)
+      @collection_filter_lock.synchronize {
+        @collection_filter = collection_filter
+      }
+    end
+
+    def get_collection_filter()
+      collection_filter = []
+      @collection_filter_lock.synchronize {
+        collection_filter = @collection_filter
+      }
+      collection_filter
+    end
+  end
+
+  class OpLogAgent
+    def initialize
+      oplog_throttler = Eventbus::OpLogThrottler.new
+      oplog_reader = OpLogReader.new
+
+      oplog_reader.read_oplogs do |incoming_oplog_message|
+        oplog_throttler.push(incoming_oplog_message)
+      end
+
+      agent_config = {
+          :node_name => Socket.gethostname,
+          :publish_queue_name => "/queue/listener",
+          :subscribe_queue_name => "/queue/#{Socket.gethostname}/agent"
+      }
+
+      messaging_service = Eventbus::MessagingService.new(agent_config) do |incoming_configuration_message|
+        collection_filter = incoming_configuration_message['collection_filter']
+        if(collection_filter != nil)
+          oplog_throttler.set_collection_filter(collection_filter)
+        end
+      end
+
+      oplog_throttler.run do |message|
+        messaging_service.publish(message)
+      end
+    end
+  end
 end
--- sli/sarje/eventbus/test/test_oplogagent.rb
+++ sli/sarje/eventbus/test/test_oplogagent.rb
@@ -1,12 +1,89 @@
-require 'test/unit'
-require 'eventbus' 
+testdir = File.dirname(__FILE__)
+$LOAD_PATH << testdir + "/../lib"
 
-class TestLdap < Test::Unit::TestCase
-    def setup
+require 'eventbus'
+require 'test/unit'
+
+class TestOpLogAgent < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_oplog_throttler
+    throttler = Eventbus::OpLogThrottler.new
+    doc1 = {"ns" => "initial"}
+    doc2 = {"ns" => "darth.vader"}
+    doc3 = {"ns" => "will.adama"}
+    doc4 = {"ns" => "wario"}
+    doc5 = {"ns" => "optimus.prime"}
+    10.times do
+      throttler.push(doc1)
+    end
+    evil_collections = ["darth.vader", "wario"]
+    good_collections = ["optimus.prime", "power.rangers"]
+    throttler.set_collection_filter(evil_collections)
+
+    collection_received = Set.new
+    throttler.run do |events|
+      events["collections"].each do |collection|
+        collection_received << collection
+      end
+    end
+    sleep 10
+    100.times do
+      throttler.push(doc1)
+      throttler.push(doc2)
+      throttler.push(doc3)
+      throttler.push(doc4)
+      throttler.push(doc5)
+    end
+    sleep 10
+    assert_equal(2, collection_received.size)
+    assert_equal(2, (collection_received & evil_collections).size)
+
+    throttler.set_collection_filter(good_collections)
+    collection_received = Set.new
+    sleep 10
+    100.times do
+      throttler.push(doc1)
+      throttler.push(doc2)
+      throttler.push(doc3)
+      throttler.push(doc4)
+      throttler.push(doc5)
+    end
+    sleep 10
+    assert_equal(1, collection_received.size)
+    assert_equal(1, (collection_received & good_collections).size)
+  end
+
+  def test_oplog_agent
+    Eventbus::OpLogAgent.new
+
+    def connect_to_mongo
+      begin
+        @conn = Mongo::Connection.new
+        @db   = @conn['sample-db']
+        @coll = @db['test']
+        @coll.remove
+      rescue
+        puts "Cannot connect to mongo"
+        sleep 1
+        retry
+      end
     end
 
-    def test_mefirst
-        puts "test_mefirst called !"
-    end 
+    Thread.new do
+      connect_to_mongo
+      100.times do |i|
+        begin
+          @coll.insert({'a' => i+1})
+          sleep 1
+        rescue Exception => e
+          puts e
+          connect_to_mongo
+        end
+      end
+    end
 
+    sleep
+  end
 end
