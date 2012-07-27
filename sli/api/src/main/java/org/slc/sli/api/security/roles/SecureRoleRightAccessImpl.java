8554ff49e7ec65859592dc60786d61e1d9702be6
Stan Halka <shalka@wgen.net>
2012-07-27 21:01:52 +0100

    code clean up in api (checkstyle fixes, unit test fix)

--- sli/api/src/main/java/org/slc/sli/api/security/roles/SecureRoleRightAccessImpl.java
+++ sli/api/src/main/java/org/slc/sli/api/security/roles/SecureRoleRightAccessImpl.java
@@ -24,9 +24,6 @@ import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import org.slc.sli.api.config.EntityDefinition;
 import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.representation.EntityBody;
@@ -38,27 +35,29 @@ import org.slc.sli.domain.NeutralCriteria;
 import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.domain.Repository;
 import org.slc.sli.domain.enums.Right;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * A basic implementation of RoleRightAccess
- *
+ * 
  * @author rlatta
  */
 @Component
 public class SecureRoleRightAccessImpl implements RoleRightAccess {
     @Autowired
     private EntityDefinitionStore store;
-
+    
     private EntityService service;
-
+    
     @Resource(name = "validationRepo")
     private Repository<Entity> repo;
-
+    
     public static final String EDUCATOR = "Educator";
     public static final String LEADER = "Leader";
     public static final String AGGREGATOR = "Aggregate Viewer";
     public static final String IT_ADMINISTRATOR = "IT Administrator";
-
+    
     public static final String LEA_ADMINISTRATOR = "LEA Administrator";
     public static final String SEA_ADMINISTRATOR = "SEA Administrator";
     public static final String APP_DEVELOPER = "Application Developer";
@@ -67,68 +66,108 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
     public static final String INGESTION_USER = "Ingestion User";
     public static final String SANDBOX_SLC_OPERATOR = "Sandbox SLC Operator";
     public static final String SANDBOX_ADMINISTRATOR = "Sandbox Administrator";
-
+    
     private final Map<String, Role> adminRoles = new HashMap<String, Role>();
-
+    
     @PostConstruct
-    private void init() {
+    public void init() {
         EntityDefinition def = store.lookupByResourceName("roles");
         setService(def.getService());
-
-        adminRoles.put(LEA_ADMINISTRATOR, RoleBuilder.makeRole(LEA_ADMINISTRATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.EDORG_APP_AUTHZ, Right.READ_PUBLIC, Right.CRUD_LEA_ADMIN }).setAdmin(true).build());
-        adminRoles.put(SEA_ADMINISTRATOR, RoleBuilder.makeRole(SEA_ADMINISTRATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.EDORG_DELEGATE, Right.READ_PUBLIC, Right.CRUD_SEA_ADMIN, Right.CRUD_LEA_ADMIN }).setAdmin(true).build());
-        adminRoles.put(SANDBOX_SLC_OPERATOR, RoleBuilder.makeRole(SANDBOX_SLC_OPERATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.CRUD_SANDBOX_SLC_OPERATOR, Right.CRUD_SANDBOX_ADMIN }).setAdmin(true).build());
-        adminRoles.put(SANDBOX_ADMINISTRATOR, RoleBuilder.makeRole(SANDBOX_ADMINISTRATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.CRUD_SANDBOX_ADMIN }).setAdmin(true).build());
-        adminRoles.put(REALM_ADMINISTRATOR, RoleBuilder.makeRole(REALM_ADMINISTRATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.READ_GENERAL, Right.CRUD_REALM_ROLES, Right.READ_PUBLIC, Right.CRUD_ROLE }).setAdmin(true).build());
-        adminRoles.put(APP_DEVELOPER, RoleBuilder.makeRole(APP_DEVELOPER).addRights(new Right[] { Right.ADMIN_ACCESS, Right.DEV_APP_CRUD, Right.READ_GENERAL, Right.READ_PUBLIC }).setAdmin(true).build());
-        adminRoles.put(INGESTION_USER, RoleBuilder.makeRole(INGESTION_USER).addRights(new Right[] { Right.INGEST_DATA, Right.ADMIN_ACCESS }).setAdmin(true).build());
+        
+        adminRoles.put(
+                LEA_ADMINISTRATOR,
+                RoleBuilder
+                        .makeRole(LEA_ADMINISTRATOR)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.EDORG_APP_AUTHZ, Right.READ_PUBLIC,
+                                        Right.CRUD_LEA_ADMIN }).setAdmin(true).build());
+        adminRoles.put(
+                SEA_ADMINISTRATOR,
+                RoleBuilder
+                        .makeRole(SEA_ADMINISTRATOR)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.EDORG_DELEGATE, Right.READ_PUBLIC,
+                                        Right.CRUD_SEA_ADMIN, Right.CRUD_LEA_ADMIN }).setAdmin(true).build());
+        adminRoles.put(
+                SANDBOX_SLC_OPERATOR,
+                RoleBuilder
+                        .makeRole(SANDBOX_SLC_OPERATOR)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.CRUD_SANDBOX_SLC_OPERATOR,
+                                        Right.CRUD_SANDBOX_ADMIN }).setAdmin(true).build());
+        adminRoles
+                .put(SANDBOX_ADMINISTRATOR,
+                        RoleBuilder.makeRole(SANDBOX_ADMINISTRATOR)
+                                .addRights(new Right[] { Right.ADMIN_ACCESS, Right.CRUD_SANDBOX_ADMIN }).setAdmin(true)
+                                .build());
+        adminRoles.put(
+                REALM_ADMINISTRATOR,
+                RoleBuilder
+                        .makeRole(REALM_ADMINISTRATOR)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.READ_GENERAL, Right.CRUD_REALM_ROLES,
+                                        Right.READ_PUBLIC, Right.CRUD_ROLE }).setAdmin(true).build());
+        adminRoles.put(
+                APP_DEVELOPER,
+                RoleBuilder
+                        .makeRole(APP_DEVELOPER)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.DEV_APP_CRUD, Right.READ_GENERAL,
+                                        Right.READ_PUBLIC }).setAdmin(true).build());
+        adminRoles.put(INGESTION_USER,
+                RoleBuilder.makeRole(INGESTION_USER).addRights(new Right[] { Right.INGEST_DATA, Right.ADMIN_ACCESS })
+                        .setAdmin(true).build());
         adminRoles.put(
                 SLC_OPERATOR,
-                RoleBuilder.makeRole(SLC_OPERATOR).addRights(new Right[] { Right.ADMIN_ACCESS, Right.SLC_APP_APPROVE, Right.READ_GENERAL, Right.READ_PUBLIC, Right.CRUD_SLC_OPERATOR, Right.CRUD_SEA_ADMIN, Right.CRUD_LEA_ADMIN })
-                        .setAdmin(true).build());
-
+                RoleBuilder
+                        .makeRole(SLC_OPERATOR)
+                        .addRights(
+                                new Right[] { Right.ADMIN_ACCESS, Right.SLC_APP_APPROVE, Right.READ_GENERAL,
+                                        Right.READ_PUBLIC, Right.CRUD_SLC_OPERATOR, Right.CRUD_SEA_ADMIN,
+                                        Right.CRUD_LEA_ADMIN }).setAdmin(true).build());
+        
     }
-
+    
     @Override
     public List<Role> findAdminRoles(List<String> roleNames) {
         List<Role> roles = new ArrayList<Role>();
-
+        
         for (String roleName : roleNames) {
             if (adminRoles.containsKey(roleName)) {
                 roles.add(adminRoles.get(roleName));
             }
         }
-
+        
         return roles;
     }
-
+    
     @Override
     @SuppressWarnings("unchecked")
     public List<Role> findRoles(String tenantId, String realmId, List<String> roleNames) {
         List<Role> roles = new ArrayList<Role>();
+        
         if (roleNames != null) {
-
             final NeutralQuery neutralQuery = new NeutralQuery();
             neutralQuery.addCriteria(new NeutralCriteria("tenantId", "=", tenantId));
             neutralQuery.addCriteria(new NeutralCriteria("realmId", "=", realmId));
-
+            
             Entity doc = SecurityUtil.runWithAllTenants(new SecurityTask<Entity>() {
-
+                
                 @Override
                 public Entity execute() {
                     return repo.findOne("customRole", neutralQuery);
                 }
             });
-
+            
             if (doc != null) {
                 Map<String, Object> roleDefs = doc.getBody();
-
+                
                 if (roleDefs != null) {
                     List<Map<String, List<String>>> roleData = (List<Map<String, List<String>>>) roleDefs.get("roles");
-
+                    
                     for (Map<String, List<String>> role : roleData) {
                         List<String> names = role.get("names");
-
+                        
                         for (String roleName : names) {
                             if (roleNames.contains(roleName)) {
                                 List<String> rights = role.get("rights");
@@ -141,39 +180,38 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
         }
         return roles;
     }
-
+    
     @Override
     public Role findRoleByName(String name) {
-
         Role admin = this.adminRoles.get(name);
-
+        
         if (admin != null) {
             return admin;
         }
-
+        
         NeutralQuery neutralQuery = new NeutralQuery();
         neutralQuery.setOffset(0);
         neutralQuery.setLimit(100);
-
+        
         // TODO find a way to "findAll" from entity service
         Iterable<String> ids = service.listIds(neutralQuery);
         for (String id : ids) {
             EntityBody body;
             body = service.get(id); // FIXME massive hack for roles disappearing sporadically
-
+            
             if (body.get("name").equals(name)) {
                 return getRoleWithBodyAndID(id, body);
             }
         }
         return null;
     }
-
+    
     @Override
     public Role findRoleBySpringName(String springName) {
         NeutralQuery neutralQuery = new NeutralQuery();
         neutralQuery.setOffset(0);
         neutralQuery.setLimit(100);
-
+        
         Iterable<String> ids = service.listIds(neutralQuery);
         for (String id : ids) {
             EntityBody body = service.get(id);
@@ -184,17 +222,17 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
         }
         return null;
     }
-
+    
     private Role getRoleWithBodyAndID(String id, EntityBody body) {
         return RoleBuilder.makeRole(body).addId(id).build();
     }
-
+    
     @Override
     public List<Role> fetchAllRoles() {
         NeutralQuery neutralQuery = new NeutralQuery();
         neutralQuery.setOffset(0);
         neutralQuery.setLimit(100);
-
+        
         List<Role> roles = new ArrayList<Role>();
         Iterable<String> ids = service.listIds(neutralQuery);
         for (String id : ids) {
@@ -203,12 +241,12 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
         }
         return roles;
     }
-
+    
     @Override
     public boolean addRole(Role role) {
         return service.create(role.getRoleAsEntityBody()) != null;
     }
-
+    
     @Override
     public boolean deleteRole(Role role) {
         if (role.getId().length() > 0) {
@@ -218,11 +256,11 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
             } catch (Exception e) {
                 return false;
             }
-
+            
         }
         return false;
     }
-
+    
     @Override
     public boolean updateRole(Role role) {
         if (role.getId().length() > 0) {
@@ -235,17 +273,17 @@ public class SecureRoleRightAccessImpl implements RoleRightAccess {
         }
         return false;
     }
-
+    
     // Injection method.
     public void setStore(EntityDefinitionStore store) {
         this.store = store;
     }
-
+    
     // Injection method.
     public void setService(EntityService service) {
         this.service = service;
     }
-
+    
     @Override
     public Role getDefaultRole(String name) {
         return findRoleByName(name);
--- sli/api/src/test/java/org/slc/sli/api/security/roles/RolesToRightsTest.java
+++ sli/api/src/test/java/org/slc/sli/api/security/roles/RolesToRightsTest.java
@@ -14,12 +14,11 @@
  * limitations under the License.
  */
 
-
 package org.slc.sli.api.security.roles;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,8 +31,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.slc.sli.api.security.resolve.impl.DefaultRolesToRightsResolver;
+import org.slc.sli.api.test.WebContextTestExecutionListener;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.MongoEntity;
+import org.slc.sli.domain.Repository;
+import org.slc.sli.domain.enums.Right;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.test.annotation.DirtiesContext;
@@ -43,82 +47,80 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
-import org.slc.sli.api.security.resolve.impl.DefaultRolesToRightsResolver;
-import org.slc.sli.api.test.WebContextTestExecutionListener;
-import org.slc.sli.domain.Entity;
-import org.slc.sli.domain.Repository;
-import org.slc.sli.domain.enums.Right;
-
 /**
  * Tests default role to rights resolution pipeline
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 @TestExecutionListeners({ WebContextTestExecutionListener.class, DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class })
+        DirtiesContextTestExecutionListener.class })
 @DirtiesContext
 public class RolesToRightsTest {
-
-    @Autowired @InjectMocks
-    private DefaultRolesToRightsResolver resolver;
+    
     @Autowired
+    @InjectMocks
+    private DefaultRolesToRightsResolver resolver;
+    
+    @Mock
     private RoleRightAccess mockAccess;
-
+    
     @Mock
     Repository<Entity> repo;
-
+    
     private static final String DEFAULT_TENANT_ID = "huzzah";
     private static final String DEFAULT_REALM_ID = "dc=slidev,dc=net";
     private static final String ADMIN_REALM_ID = "adminRealmId";
-
+    
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        mockAccess = mock(RoleRightAccess.class);
-
+        
+        Map<String, Object> adminRealmBody = new HashMap<String, Object>();
+        adminRealmBody.put("admin", true);
+        Entity adminRealmEntity = new MongoEntity("realm", adminRealmBody);
+        
+        Map<String, Object> userRealmBody = new HashMap<String, Object>();
+        userRealmBody.put("admin", false);
+        Entity userRealmEntity = new MongoEntity("realm", userRealmBody);
+        
+        when(repo.findById("realm", ADMIN_REALM_ID)).thenReturn(adminRealmEntity);
+        when(repo.findById("realm", DEFAULT_REALM_ID)).thenReturn(userRealmEntity);
+        
+        when(
+                mockAccess.findRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID,
+                        Arrays.asList(SecureRoleRightAccessImpl.EDUCATOR, SecureRoleRightAccessImpl.AGGREGATOR)))
+                .thenReturn(Arrays.asList(buildRole()));
+        
+        when(mockAccess.findRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID, Arrays.asList("Pink", "Goo"))).thenReturn(
+                new ArrayList<Role>());
+        
+        when(
+                mockAccess.findRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID, Arrays.asList(
+                        SecureRoleRightAccessImpl.EDUCATOR, SecureRoleRightAccessImpl.AGGREGATOR, "bad", "doggie")))
+                .thenReturn(Arrays.asList(buildRole()));
+        
         resolver.setRoleRightAccess(mockAccess);
-
-        //wire up isAdminRealm
-        Entity adminRealmEnt = Mockito.mock(Entity.class);
-        Map realmData = new HashMap();
-        realmData.put("admin", true);
-        Mockito.when(adminRealmEnt.getBody()).thenReturn(realmData);
-        when(repo.findById("realm", ADMIN_REALM_ID)).thenReturn(adminRealmEnt);
-
-        Entity userRealmEnt = Mockito.mock(Entity.class);
-        realmData = new HashMap();
-        realmData.put("admin", false);
-        Mockito.when(userRealmEnt.getBody()).thenReturn(realmData);
-        when(repo.findById("realm", DEFAULT_REALM_ID)).thenReturn(userRealmEnt);
-
-        when(mockAccess.getDefaultRole(SecureRoleRightAccessImpl.EDUCATOR)).thenReturn(buildRole());
-        when(mockAccess.getDefaultRole(SecureRoleRightAccessImpl.AGGREGATOR)).thenReturn(buildRole());
-        when(mockAccess.getDefaultRole("bad")).thenReturn(null);
-        when(mockAccess.getDefaultRole("doggie")).thenReturn(null);
-        when(mockAccess.getDefaultRole("Pink")).thenReturn(null);
-        when(mockAccess.getDefaultRole("Goo")).thenReturn(null);
     }
-
+    
     private Role buildRole() {
         return RoleBuilder.makeRole(SecureRoleRightAccessImpl.EDUCATOR).addRight(Right.AGGREGATE_READ).build();
     }
-
-
+    
     @Test
     public void testMappedRoles() throws Exception {
-
         Set<GrantedAuthority> rights = resolver.resolveRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID,
                 Arrays.asList(SecureRoleRightAccessImpl.EDUCATOR, SecureRoleRightAccessImpl.AGGREGATOR));
         Assert.assertTrue(rights.size() > 0);
     }
-
+    
     @Test
     public void testBadRoles() throws Exception {
-        Set<GrantedAuthority> authorities = resolver.resolveRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID, Arrays.asList("Pink", "Goo"));
+        Set<GrantedAuthority> authorities = resolver.resolveRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID,
+                Arrays.asList("Pink", "Goo"));
         Assert.assertTrue("Authorities must be empty", authorities.size() == 0);
     }
-
+    
     @Test
     public void testMixedRoles() throws Exception {
         Set<GrantedAuthority> authorities = resolver.resolveRoles(DEFAULT_TENANT_ID, DEFAULT_REALM_ID, Arrays.asList(
