cc7313043e3c171f88eee5ae2a5a9c5d2ad182e7
William Scott <wscott@wgen.net>
2012-08-03 15:15:00 -0400

    Update presplit/shard script to auto-discover shard names.

--- sli/config/shards/sli-shard-presplit.js
+++ sli/config/shards/sli-shard-presplit.js
@@ -16,47 +16,60 @@
 
 var sli_collections = ["assessment","attendance","calendarDate","cohort","competencyLevelDescriptor","course","courseOffering","courseSectionAssociation","disciplineAction","disciplineIncident","educationOrganization","educationOrganizationAssociation","educationOrganizationSchoolAssociation","grade","gradebookEntry","gradingPeriod","graduationPlan","learningObjective","learningStandard","parent","program","reportCard","section","sectionAssessmentAssociation","sectionSchoolAssociation","session","sessionCourseAssociation","staff","staffCohortAssociation","staffEducationOrganizationAssociation","staffProgramAssociation","student","studentAcademicRecord","studentAssessmentAssociation","studentCohortAssociation","studentCompetency","studentCompetencyObjective","studentDisciplineIncidentAssociation","studentParentAssociation","studentProgramAssociation","studentSectionAssociation","studentGradebookEntry","studentSchoolAssociation","studentTranscriptAssociation","teacherSchoolAssociation","teacherSectionAssociation"];
 
-var sli_shards = ["kelvin", "constellation", "hood", "stargazer"]
+//var sli_shards = ["kelvin", "constellation", "hood", "stargazer"]
+var sli_shards = discoverShards();
+
+function discoverShards() {
+    var shards = [];
+    var available_shards = db.runCommand( { listShards : 1 } );
+    var shard_list = available_shards["shards"];
+    for (var shard in shard_list) {
+        shards.push(shard_list[shard]["_id"]);
+    }
+    return shards;
+}
 
 function preSplit(database_name, tenantId){
     //make sure the database is sharded
+    print("Enabling sharding on database:" + database_name);
     db.runCommand( { enablesharding : database_name } );
 
-    var a_code = "a".charCodeAt(0)
-    var char_offset = Math.floor(26 / sli_shards.length)
+    var a_code = "a".charCodeAt(0);
+    var char_offset = Math.floor(26 / sli_shards.length);
 
     for (var col_num in sli_collections) {
         //calculate strings
         var collection = database_name + "." + sli_collections[col_num];
-        var year = new Date().getFullYear()
-        var move_strings = []
+        var year = new Date().getFullYear();
+        var move_strings = [];
 
         //enable sharding on the collection
+        print("Sharding collecion:" + collection);
         db.runCommand({shardcollection:collection,
-                       key:{"metaData.tenantId":1, "_id":1} });
+            key:{"metaData.tenantId":1, "_id":1} });
 
         //calculate splits and add to the moves array
-        move_strings.push(year + "a")
+        move_strings.push(year + "a");
         for (var shard_num = 1; shard_num < sli_shards.length; shard_num++) {
             var split_letter = String.fromCharCode(a_code + (char_offset * shard_num));
             var split_string = year + split_letter;
-            move_strings.push(split_string)
+            move_strings.push(split_string);
 
             //execute db command
             db.adminCommand({split:collection,
-                             middle:{"metaData.tenantId":tenantId, "_id":split_string}
-                            });
+                middle:{"metaData.tenantId":tenantId, "_id":split_string}
+            });
         }
 
         //explicitly move chunks to each shard
-        for (i in move_strings) {
+        for (var i in move_strings) {
             //execute db command
             db.adminCommand({moveChunk:collection,
-                             find:{"metaData.tenantId":tenantId, "_id":move_strings[i]},
-                             to:sli_shards[i]
-                            });
+                find:{"metaData.tenantId":tenantId, "_id":move_strings[i]},
+                to:sli_shards[i]
+            });
         }
     }
 }
 
-preSplit("sli", "Midgar")
+preSplit("sli", "Midgar");
