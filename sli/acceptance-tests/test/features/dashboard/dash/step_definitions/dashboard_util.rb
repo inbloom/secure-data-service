20e3f4acf38860a7a4fd5ad9344a137146f2cc79
Mark Abernathy <mabernathy@wgen.net>
2012-08-03 11:42:48 -0400

    fix for DE1357, unwiped sad path test

--- sli/acceptance-tests/test/features/dashboard/dash/step_definitions/dashboard_util.rb
+++ sli/acceptance-tests/test/features/dashboard/dash/step_definitions/dashboard_util.rb
@@ -15,3 +15,31 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 =end
+
+Given /^the district "([^\"]*)" has dissallowed use of the dashboard$/ do |district|
+  conn = Mongo::Connection.new(PropLoader.getProps['DB_HOST'])
+  db = conn[PropLoader.getProps['api_database_name']]
+  appColl = db.collection("application")
+  dashboardId = appColl.find_one({"body.name" => "SLC Dashboards"})["_id"]
+  puts("The dashboard id is #{dashboardId}") if ENV['DEBUG']
+  
+  appAuthColl = db.collection("applicationAuthorization")
+  edOrgColl = db.collection("educationOrganization")
+
+  edOrg = edOrgColl.find_one({"body.stateOrganizationId" => district})
+  if edOrg != nil
+    puts("Found edOrg: #{edOrg.inspect}") if ENV['DEBUG']
+    edOrgId = edOrg["_id"]
+    edOrgTenant = edOrg["metaData"]["tenantId"]
+    existingAppAuth = appAuthColl.find_one({"body.authId" => edOrgId})
+    if existingAppAuth != nil
+      existingAppAuth["body"]["appIds"].delete(dashboardId)
+      puts("About to update #{existingAppAuth.inspect}") if ENV['DEBUG']
+      appAuthColl.update({"body.authId" => edOrgId}, existingAppAuth)
+    else
+      puts("District #{edOrgId} already denies all apps") if ENV['DEBUG']
+    end
+  else
+    puts("District #{district} not found") if ENV['DEBUG']
+  end
+end
