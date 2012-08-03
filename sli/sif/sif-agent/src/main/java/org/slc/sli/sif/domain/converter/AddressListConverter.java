ceca40a4858e3bab050554da7b0151ad7ec407b3
Steve Ashton <sashton@wgen.net>
2012-08-03 15:59:10 -0400

    [US2949] Removed Dozer

--- sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/AddressListConverter.java
+++ sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/AddressListConverter.java
@@ -21,23 +21,21 @@ import java.util.List;
 
 import openadk.library.common.AddressList;
 
-import org.dozer.DozerConverter;
-
 import org.slc.sli.sif.domain.slientity.Address;
 
 /**
- * A customized Dozer converter to convert SIF AddressList to SLI Address list.
+ * A custom converter to convert SIF AddressList to SLI Address list.
  *
  * @author slee
  *
  */
-public class AddressListConverter extends
-        DozerConverter<AddressList, List<Address>> {
-    public AddressListConverter() {
-        super(AddressList.class, (Class<List<Address>>) new ArrayList<Address>().getClass());
+public class AddressListConverter {
+
+    public List<Address> convertTo(AddressList source) {
+        List<Address> destination = new ArrayList<Address>();
+        return convertTo(source, destination);
     }
 
-    @Override
     public List<Address> convertTo(AddressList source, List<Address> destination) {
         if (source == null) {
             return null;
@@ -46,22 +44,16 @@ public class AddressListConverter extends
         List<Address> list = new ArrayList<Address>(addresses.length);
         for (openadk.library.common.Address address : addresses) {
             Address sliAddr = new Address();
-            sliAddr.setStreetNumberName(address.getStreet().getStreetNumber()
-                    + " " + address.getStreet().getStreetName());
+            sliAddr.setStreetNumberName(address.getStreet().getStreetNumber() + " "
+                    + address.getStreet().getStreetName());
             sliAddr.setCity(address.getCity());
             sliAddr.setCountryCode(address.getCountry());
             sliAddr.setPostalCode(address.getPostalCode());
             sliAddr.setStateAbbreviation(address.getStateProvince());
-            sliAddr.setAddressType(SchoolMappings.toSliAddressType(address
-                    .getType()));
+            sliAddr.setAddressType(SchoolMappings.toSliAddressType(address.getType()));
             list.add(sliAddr);
         }
         return list;
-    }
-
-    @Override
-    public AddressList convertFrom(List<Address> source, AddressList destination) {
-        return null;
     }
 
 }
