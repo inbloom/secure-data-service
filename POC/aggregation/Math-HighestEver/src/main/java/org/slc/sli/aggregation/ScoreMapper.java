1732f1ab7d7040f54a6c168a98a38d316effd48b
asaarela <asaarela@wgen.net>
2012-08-06 17:21:53 -0400

    Removing unnecessary check for the result type -- this is included in the map query. Iterate all results, not just the first match.

--- POC/aggregation/Math-HighestEver/src/main/java/org/slc/sli/aggregation/ScoreMapper.java
+++ POC/aggregation/Math-HighestEver/src/main/java/org/slc/sli/aggregation/ScoreMapper.java
@@ -28,16 +28,11 @@ public class ScoreMapper extends Mapper<String, BSONObject, TenantAndID, DoubleW
         Map<String, Object> body = (Map<String, Object>) studentAssessment.get("body");
         String studentId = (String) body.get("studentId");
 
-        String scoreType = context.getConfiguration().get(SCORE_TYPE);
-
         List<Map<String, Object>> scoreResults = (List<Map<String, Object>>) body.get("scoreResults");
         for (Map<String, Object> result : scoreResults) {
-            if (scoreType == null || scoreType.equals(result.get("assessmentReportingMethod"))) {
-                String scoreString = (String) result.get("result");
-                double score = Double.parseDouble(scoreString);
-                context.write(new TenantAndID(studentId, tenant), new DoubleWritable(score));
-                return;
-            }
+            String scoreString = (String) result.get("result");
+            double score = Double.parseDouble(scoreString);
+            context.write(new TenantAndID(studentId, tenant), new DoubleWritable(score));
         }
     }
 }
