cb0f487127cd98aa53952e9168aed5063c5d1fac
Joe Chung <joechung@wgen.net>
2012-08-06 09:00:22 -0400

    fixed some tests and refactored

--- sli/sarje/eventbus/lib/mongo_helper.rb
+++ sli/sarje/eventbus/lib/mongo_helper.rb
@@ -23,21 +23,12 @@ require 'thread'
 module Eventbus
 
   class MongoHelper
-    JOB_COLLECTION = "jobdefinitions"
-    CONFIG = {
-        :mongo_host           => "127.0.0.1",
-        :mongo_port           => 27017,
-        :mongo_db             => "eventbus",
-        :mongo_job_collection => "jobs",
-        :poll_interval        => 5
-    }
-
     #TODO: move this out to a property
     MONGO_HOME = "/usr/local"
     MONGO_EXEC = "#{MONGO_HOME}/bin/mongo"
     MONGOIMPORT_EXEC = "#{MONGO_HOME}/bin/mongoimport"
 
-    def initialize(config = CONFIG)
+    def initialize(config)
       @active_config = config
     end
 
@@ -48,7 +39,7 @@ module Eventbus
     end
 
     # Load a fixture file into the mongodb
-    def setFixture(collectionName, fixtureFilePath, dropExistingCollection=true)
+    def setFixture(collectionName = @active_config[:mongo_job_collection], fixtureFilePath = @active_config[:fixture_file_path], dropExistingCollection = true)
       dropOption = (dropExistingCollection) ? "--drop":""
       command = "#{MONGOIMPORT_EXEC} #{dropOption} -d #{@active_config[:mongo_db]} -c #{collectionName} -h #{@active_config[:mongo_host]} --file #{fixtureFilePath}"
       status, stdout, stderr = systemu command
--- sli/sarje/eventbus/test/test_jobscheduler.rb
+++ sli/sarje/eventbus/test/test_jobscheduler.rb
@@ -26,21 +26,31 @@ require 'systemu'
 
 class TestJobScheduler < Test::Unit::TestCase
 
-    # the directory where this test lives
-    TEST_DIR = File.dirname(__FILE__) + "/"
+  JOB_COLLECTION = "jobdefinitions"
+  # the directory where this test lives
+  TEST_DIR = File.dirname(__FILE__) + "/"
+
+  CONFIG = {
+      :mongo_host           => "127.0.0.1",
+      :mongo_port           => 27017,
+      :mongo_db             => "eventbus",
+      :mongo_job_collection => "jobs",
+      :poll_interval        => 5,
+      :fixture_file_path    => TEST_DIR + 'data/test.json'
+  }
 
     def setup
-        mongo_helper = Eventbus::MongoHelper.new
+        mongo_helper = Eventbus::MongoHelper.new(CONFIG)
         # Set up the fixtures in Mongo 
         mongo_helper.removeDatabase
-        mongo_helper.setFixture(CONFIG[:mongo_job_collection], TEST_DIR + 'data/test.json')
+        mongo_helper.setFixture
     end
 
     def test_job_reader
       job_reader = Eventbus::JobReader.new(CONFIG)
       jobs, event_job_map = job_reader.get_jobs
-      assert(1, jobs.size)
-      assert(1, event_job_map.size)
+      assert_equal(1, jobs.size)
+      assert_equal(1, event_job_map.size)
     end
 
     def test_scheduler 
@@ -51,20 +61,19 @@ class TestJobScheduler < Test::Unit::TestCase
         # add mock listener that emits events and jobrunner that counts scheduled jobs
         listener = MockMQListener.new(nevents, delay)
         jobrunner = EventCountingJobRunner.new
-        @active_config[:messaging_service] = listener
-        @active_config[:jobrunner] = jobrunner
-        @active_config[:mongo_poll_interval] = 100
-        @scheduler = Eventbus::JobScheduler.new(@active_config)
+        active_config = CONFIG.clone
+        active_config[:messaging_service] = listener
+        active_config[:jobrunner] = jobrunner
+        active_config[:mongo_poll_interval] = 100
+        Eventbus::JobScheduler.new(active_config)
 
         # wait until all events have fired 
         start = Time.now 
         while (!listener.done) && ((Time.now - start) < (nevents * 2 * delay))
             sleep(0.1)
         end
-        assert jobrunner.total == nevents, "Not all events were successfully dispatched. #{jobrunner.total}"
-    end 
-
-    private
+        assert_equal(nevents, jobrunner.total, "Not all events were successfully dispatched. #{jobrunner.total}")
+    end
 end
 
 class MockMQListener 
@@ -87,13 +96,11 @@ class MockMQListener
         end 
     end 
 
-    def subscribe(events)
-        @events = events
+    def subscribe
     end 
 
-    def receive(&block)
-        @receive_block = block 
-    end 
+    def publish(message)
+    end
 end
 
 class EventCountingJobRunner 
