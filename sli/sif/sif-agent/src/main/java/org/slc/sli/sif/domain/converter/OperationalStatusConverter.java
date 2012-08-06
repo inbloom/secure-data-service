b0b06a4d8dd5c03002a0c7def860980cffdf8393
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-08-06 13:25:43 -0400

    [US2949] updated operational status mappings

--- sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/OperationalStatusConverter.java
+++ sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/OperationalStatusConverter.java
@@ -25,25 +25,35 @@ import org.springframework.stereotype.Component;
 
 /**
  * Converter for SIF OperationalStatus to SLI operationStatus
+ *
+ * SLI values:
+ *	Active
+ *	Added
+ *	Changed Agency
+ *	Closed
+ *	Continuing
+ *	Future
+ *	Inactive
+ *	New
+ *	Reopened
  */
-
 @Component
 public class OperationalStatusConverter {
 
     private static final Map<OperationalStatus, String> OPERATIONAL_STATUS_MAP = new HashMap<OperationalStatus, String>();
     static {
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_CHANGED, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_CLOSED, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_FUTURE, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_INACTIVE, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_NEW, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_OPEN, "Not Supported");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.CHANGED_BOUNDARY, "Changed Agency");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_CHANGED, "Changed Agency");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_CLOSED, "Closed");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_FUTURE, "Future");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_INACTIVE, "Inactive");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_NEW, "New");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.AGENCY_OPEN, "Active");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.CHANGED_BOUNDARY, null);
         OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_CLOSED, "Closed");
         OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_FUTURE, "Future");
         OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_INACTIVE, "Inactive");
         OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_NEW, "New");
-        OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_OPEN, "Reopened");
+        OPERATIONAL_STATUS_MAP.put(OperationalStatus.SCHOOL_OPEN, "Active");
     }
 
     /**
@@ -56,11 +66,6 @@ public class OperationalStatusConverter {
             return null;
         }
 
-        return toSliOperationalStatus(OperationalStatus.wrap(operationalStatus.getValue()));
-    }
-
-    private String toSliOperationalStatus(OperationalStatus operationalStatus) {
-        String mapped = OPERATIONAL_STATUS_MAP.get(operationalStatus);
-        return mapped == null ? "Not Supported" : mapped;
+        return OPERATIONAL_STATUS_MAP.get(operationalStatus);
     }
 }
