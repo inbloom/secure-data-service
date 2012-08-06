93978d5afa1be549edbb1370cabb440e6c2836b6
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-08-06 09:35:52 -0400

    [US2949] Address list converter with unit tests

--- sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/AddressListConverter.java
+++ sli/sif/sif-agent/src/main/java/org/slc/sli/sif/domain/converter/AddressListConverter.java
@@ -17,9 +17,13 @@
 package org.slc.sli.sif.domain.converter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import openadk.library.common.AddressList;
+import openadk.library.common.AddressType;
+import openadk.library.common.Street;
 
 import org.slc.sli.sif.domain.slientity.Address;
 
@@ -31,29 +35,60 @@ import org.slc.sli.sif.domain.slientity.Address;
  */
 public class AddressListConverter {
 
-    public List<Address> convertTo(AddressList source) {
-        List<Address> destination = new ArrayList<Address>();
-        return convertTo(source, destination);
+    private static final Map<AddressType, String> ADDRESS_TYPE_MAP = new HashMap<AddressType, String>();
+    static {
+        ADDRESS_TYPE_MAP.put(AddressType._0369_CAMPUS, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_EMPLOYER, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_EMPLOYMENT, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_MAILING, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_ORGANIZATION, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_OTHER, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType._0369_PERMANENT, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.EMPLOYERS, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.EMPLOYMENT, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.MAILING, "Mailing");
+        ADDRESS_TYPE_MAP.put(AddressType.OTHER_HOME, "Home");
+        ADDRESS_TYPE_MAP.put(AddressType.OTHER_ORGANIZATION, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.PHYSICAL_LOCATION, "Physical");
+        ADDRESS_TYPE_MAP.put(AddressType.SHIPPING, "Billing");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_CAMPUS, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_EMPLOYER, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_EMPLOYMENT, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_MAILING, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_ORGANIZATION, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_OTHER, "Other");
+        ADDRESS_TYPE_MAP.put(AddressType.SIF15_PERMANENT, "Other");
     }
 
-    public List<Address> convertTo(AddressList source, List<Address> destination) {
-        if (source == null) {
+    public List<Address> convert(AddressList addressList) {
+        if (addressList == null) {
             return null;
         }
-        openadk.library.common.Address[] addresses = source.getAddresses();
+
+        return toSliAddressList(addressList.getAddresses());
+    }
+
+    private List<Address> toSliAddressList(openadk.library.common.Address[] addresses) {
         List<Address> list = new ArrayList<Address>(addresses.length);
         for (openadk.library.common.Address address : addresses) {
             Address sliAddr = new Address();
-            sliAddr.setStreetNumberName(address.getStreet().getStreetNumber() + " "
-                    + address.getStreet().getStreetName());
+            Street street = address.getStreet();
+            if (street != null) {
+                sliAddr.setStreetNumberName(street.getStreetNumber() + " "
+                    + street.getStreetName());
+            }
             sliAddr.setCity(address.getCity());
             sliAddr.setCountryCode(address.getCountry());
             sliAddr.setPostalCode(address.getPostalCode());
             sliAddr.setStateAbbreviation(address.getStateProvince());
-            sliAddr.setAddressType(SchoolMappings.toSliAddressType(address.getType()));
+            sliAddr.setAddressType(toSliAddressType(AddressType.wrap(address.getType())));
             list.add(sliAddr);
         }
         return list;
     }
 
+    private String toSliAddressType(AddressType addressType) {
+        String mapping = ADDRESS_TYPE_MAP.get(addressType);
+        return mapping == null ? "Other" : mapping;
+    }
 }
---
+++ sli/sif/sif-agent/src/test/java/org/slc/sli/sif/domain/converter/AddressListConverterTest.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.slc.sli.sif.domain.converter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+import openadk.library.common.AddressList;
+import openadk.library.common.AddressType;
+import openadk.library.common.StatePrCode;
+import openadk.library.common.Street;
+
+import org.junit.Test;
+
+import org.slc.sli.sif.ADKTest;
+import org.slc.sli.sif.domain.slientity.Address;
+
+public class AddressListConverterTest extends ADKTest {
+
+    private final AddressListConverter converter = new AddressListConverter();
+    private Map<AddressType, String> map = new HashMap<AddressType, String>();
+    private List<AddressType> addressTypes = new ArrayList<AddressType>();
+
+    @Test
+    public void testNullObject(){
+        List<Address> result = converter.convert(null);
+        Assert.assertNull("Address list should be null", result);
+    }
+
+    @Test
+    public void testEmptyList(){
+        AddressList list = new AddressList();
+        List<Address> result = converter.convert(list);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testEmptyAddress(){
+        AddressList list = new AddressList();
+        openadk.library.common.Address original = new openadk.library.common.Address();
+        list.add(original);
+        List<Address> result = converter.convert(list);
+
+        Assert.assertEquals(1, result.size());
+
+        Address address = result.get(0);
+        Assert.assertEquals("Other", address.getAddressType());
+        Assert.assertNull(address.getCity());
+        Assert.assertNull(address.getCountryCode());
+        Assert.assertNull(address.getPostalCode());
+        Assert.assertNull(address.getStateAbbreviation());
+        Assert.assertNull(address.getStreetNumberName());
+    }
+
+    @Test
+    public void testConversion(){
+        map.clear();
+        map.put(AddressType._0369_CAMPUS, "Other");
+        map.put(AddressType._0369_EMPLOYER, "Other");
+        map.put(AddressType._0369_EMPLOYMENT, "Other");
+        map.put(AddressType._0369_MAILING, "Other");
+        map.put(AddressType._0369_ORGANIZATION, "Other");
+        map.put(AddressType._0369_OTHER, "Other");
+        map.put(AddressType._0369_PERMANENT, "Other");
+        map.put(AddressType.EMPLOYERS, "Other");
+        map.put(AddressType.EMPLOYMENT, "Other");
+        map.put(AddressType.MAILING, "Mailing");
+        map.put(AddressType.OTHER_HOME, "Home");
+        map.put(AddressType.OTHER_ORGANIZATION, "Other");
+        map.put(AddressType.PHYSICAL_LOCATION, "Physical");
+        map.put(AddressType.SHIPPING, "Billing");
+        map.put(AddressType.SIF15_CAMPUS, "Other");
+        map.put(AddressType.SIF15_EMPLOYER, "Other");
+        map.put(AddressType.SIF15_EMPLOYMENT, "Other");
+        map.put(AddressType.SIF15_MAILING, "Other");
+        map.put(AddressType.SIF15_ORGANIZATION, "Other");
+        map.put(AddressType.SIF15_OTHER, "Other");
+        map.put(AddressType.SIF15_PERMANENT, "Other");
+
+        // used to test default
+        map.put(AddressType.wrap("something else"), "Other");
+
+        addressTypes.addAll(map.keySet());
+
+        // Come up with SIF addresses, add to list, convert
+        AddressList originalList = new AddressList();
+        // Add addresses...
+        for (int i = 0; i < 25; i++) {
+            openadk.library.common.Address address = getAddress(i);
+            originalList.add(address);
+        }
+
+        List<Address> convertedList = converter.convert(originalList);
+
+        Assert.assertEquals(originalList.size(), convertedList.size());
+        int i = 0;
+        for (Address converted : convertedList) {
+            openadk.library.common.Address original = originalList.get(i++);
+            testMapping(original, converted);
+        }
+    }
+
+    private openadk.library.common.Address getAddress(int i) {
+        openadk.library.common.Address address = new openadk.library.common.Address();
+
+        String streetNumber = i + "";
+        String streetName = i + " Street";
+        String city = "City " + i;
+        String countryCode = "US" + i;
+        String postalCode = "2770" + (i % 10);
+        StatePrCode stateAbbreviation = StatePrCode.NC;
+
+        int addressTypeIndex = i % addressTypes.size();
+        AddressType addressType = addressTypes.get(addressTypeIndex);
+
+        Street street = new Street();
+        street.setStreetNumber(streetNumber);
+        street.setStreetName(streetName);
+        address.setStreet(street);
+        address.setCity(city);
+        address.setCountry(countryCode);
+        address.setPostalCode(postalCode);
+        address.setStateProvince(stateAbbreviation);
+        address.setType(addressType);
+
+        return address;
+    }
+
+
+    private void testMapping(openadk.library.common.Address original, Address converted){
+         String expectedStreetNumberName = original.getStreet().getStreetNumber() + " "
+                + original.getStreet().getStreetName();
+         Assert.assertEquals(expectedStreetNumberName, converted.getStreetNumberName());
+
+         String expectedCity = original.getCity();
+         Assert.assertEquals(expectedCity, converted.getCity());
+
+         String expectedCountryCode = original.getCountry();
+         Assert.assertEquals(expectedCountryCode, converted.getCountryCode());
+
+         String expectedPostalCode = original.getPostalCode();
+         Assert.assertEquals(expectedPostalCode, converted.getPostalCode());
+
+         String expectedStateAbbreviation = original.getStateProvince();
+         Assert.assertEquals(expectedStateAbbreviation, converted.getStateAbbreviation());
+
+         String expectedAddressType = map.get(AddressType.wrap(original.getType()));
+         expectedAddressType = expectedAddressType == null ? "Other" : expectedAddressType;
+         Assert.assertEquals(expectedAddressType, converted.getAddressType());
+    }
+}
