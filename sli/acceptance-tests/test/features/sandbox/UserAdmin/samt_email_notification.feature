01e556d9b63939dac1b6c2c788b4b12e602a7b01
Dong Liu <dliu@wgen.net>
2012-08-07 16:18:31 -0400

    add samt email notification test

--- sli/acceptance-tests/test/features/sandbox/UserAdmin/samt_email_notification.feature
+++ sli/acceptance-tests/test/features/sandbox/UserAdmin/samt_email_notification.feature
@@ -1,4 +1,3 @@
-@wip
 @US_3455
 Feature: As a new SAMT account holder I get an email when my account is created.
 
@@ -7,21 +6,43 @@ Given I have an open web browser
 And I have a SMTP/Email server configured
 
 @sandbox
-Scenario: An admin creates an account with a valid email, initial notification is sent
-Given the testing user "samt_sandbox@slidev.org" does not already exists in LDAP
+Scenario: An admin creates a sandbox account with a valid email, initial notification is sent
+
+Given the testing user "peacefrog@slidev.org" does not already exists in LDAP
 When I navigate to the sandbox user account management page
 And I submit the credentials "sandboxadministrator" "sandboxadministrator1234" for the "Simple" login page
 Then I am redirected to "Sandbox Account Management" page 
 Then I click on "Add User" button
 And I am redirected to "Add a User" page
 
-And I enter Full Name "firstName lastName" and Email "samt_sandbox@slidev.org" into the required fields
+And I enter Full Name "firstName lastName" and Email "peacefrog@slidev.org" into the required fields
 And I can select "Application Developer" from a choice between "Sandbox Administrator, Application Developer, Ingestion User" Role 
 And I can also check "Ingestion User" Role 
 
 When I click button "Save"
 Then I am redirected to "Sandbox Account Management" page 
 And a "Success! You have added a new user" message is displayed
+And a verify email notification is sent to user
+
+@production
+Scenario: An admin creates a production account with a valid email, initial notification is sent
+
+Given the prod testing user "peacefrog@slidev.org" does not already exists in LDAP
+When I navigate to the User Management Page 
+And I submit the credentials "slcoperator" "slcoperator1234" for the "Simple" login page
+Then I am redirected to "Admin Account Management" page 
+Then I click on "Add User" button
+And I am redirected to "Add a User" page
+
+And I enter Full Name "firstName lastName" and Email "peacefrog@slidev.org" into the required fields
+And I can select "SEA Administrator" from a choice between "SLC Operator, SEA Administrator, LEA Administrator, Ingestion User, Realm Administrator" Role
+And I can also check "Realm Administrator" Role 
+And I can update the "Tenant" field to "IL1212"
+And I can update the "EdOrg" field to "IL-DAYBREAK"
+
+When I click button "Save"
+Then I am redirected to "Admin Account Management" page
+And a "Success" message is displayed 
 And a verify email notification is sent to user
 
 @wip
--- sli/acceptance-tests/test/features/sandbox/UserAdmin/step_definitions/samt_email_notification_steps.rb
+++ sli/acceptance-tests/test/features/sandbox/UserAdmin/step_definitions/samt_email_notification_steps.rb
@@ -24,47 +24,65 @@ require_relative '../../../utils/sli_utils.rb'
 require_relative '../../../utils/selenium_common.rb'
 
 SAMT_EMAIL_NOTIFICATION_SUBJECT_SANDBOX = "SLC Sandbox Account - Email Confirmation"
+SAMT_EMAIL_NOTIFICATION_SUBJECT_PROD = "SLC Developer Account - Email Confirmation"
+TEST_EMAIL = "peacefrog@slidev.org"
+TEST_EMAIL_USER ="peacefrog"
+TEST_EMAIL_PASS ="demouser"
 
 Before do
   @explicitWait = Selenium::WebDriver::Wait.new(:timeout => 60)
 end
 
+After do
+  remove_user(TEST_EMAIL)
+end
+
 Given /^I have a SMTP\/Email server configured$/ do
   @email_sender_name= "SLC Administrator"
-  @email = PropLoader.getProps['email_imap_registration_user_email']
-  defaultUser = PropLoader.getProps['email_imap_registration_user']
-  defaultPassword = PropLoader.getProps['email_imap_registration_pass']
+  @email = TEST_EMAIL
+  defaultUser = TEST_EMAIL_USER
+  defaultPassword = TEST_EMAIL_PASS
   @imap = Net::IMAP.new(PropLoader.getProps['email_imap_host'], PropLoader.getProps['email_imap_port'], true, nil, false)
   @imap.authenticate('LOGIN', defaultUser, defaultPassword)
-  @imap.examine('INBOX')
 
   @last_id = get_last_email_id
-  puts @last_id
+  puts "current email id is: #{@last_id}"
 end
 
 Given /^the testing user "(.*?)" does not already exists in LDAP$/ do |email|
-  idpRealmLogin("sandboxoperator", nil)
-  sessionId = @sessionId
-  format = "application/json"
-  restHttpDelete("/users/"+email, format, sessionId)
+  remove_user(email)
+  @test_env="sandbox"
+end
+
+Given /^the prod testing user "(.*?)" does not already exists in LDAP$/ do |email|
+  remove_user(email)
+  @test_env="prod"
+end
+
+When /^I navigate to the User Management Page$/ do
+  step "I navigate to the sandbox user account management page"
 end
 
 Then /^I enter Full Name "(.*?)" and Email "(.*?)" into the required fields$/ do |fullName, email|
-  pending # express the regexp above with the code you wish you had
+  @driver.find_element(:name, 'user[fullName]').send_keys fullName
+  @driver.find_element(:name, 'user[email]').send_keys email
 end
 
 Then /^a verify email notification is sent to user$/ do
-  pending # express the regexp above with the code you wish you had
+  sleep(3)
+  verify_email
+
 end
 
 def get_last_email_id
+  @imap.examine('INBOX')
   ids = @imap.search(["FROM", @email_sender_name,"TO",@email])
   last_id = ids[-1]
   return last_id
 end
 
 def get_email_subject(id)
-  subject = @imap.fetchimap.fetch(id, "BODY[HEADER.FIELDS (SUBJECT)]")[0].attr["BODY[HEADER.FIELDS (SUBJECT)]"]
+  subject = @imap.fetch(id, "BODY[HEADER.FIELDS (SUBJECT)]")[0].attr["BODY[HEADER.FIELDS (SUBJECT)]"]
   return subject
 end
 
@@ -76,13 +94,32 @@ def verify_email
   current_id = get_last_email_id
   if  (current_id <= @last_id)
   found = false
-    else
+  else
     ids = (@last_id+1)..current_id
     ids.each do |id|
-      if get_email_subject == SAMT_EMAIL_NOTIFICATION_SUBJECT_SANDBOX
-      found = true
+
+      if ((get_email_subject(id).include? SAMT_EMAIL_NOTIFICATION_SUBJECT_SANDBOX) && @test_env =="sandbox")||((get_email_subject(id).include? SAMT_EMAIL_NOTIFICATION_SUBJECT_PROD) && @test_env =="prod")
+        found = true
+        puts "current email id is: #{id}"
+        puts get_email_subject(id)
+        puts get_email_content(id)
       end
     end
   end
+  assert(found,"did not receive the samt notification email in mailbox #{TEST_EMAIL}")
+end
+
+def remove_user(email)
+  # remove user created for sandbox
+  idpRealmLogin("sandboxoperator", nil)
+  sessionId = @sessionId
+  format = "application/json"
+  restHttpDelete("/users/"+email, format, sessionId)
+
+  # remove user created for production
+  idpRealmLogin("operator", nil)
+  sessionId = @sessionId
+  format = "application/json"
+  restHttpDelete("/users/"+email, format, sessionId)
 end
 
