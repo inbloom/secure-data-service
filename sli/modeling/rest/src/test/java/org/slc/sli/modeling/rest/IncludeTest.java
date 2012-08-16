7629dc6b013b47cf6cf9d9987bf85a23c584e5b5
William Scott <wscott@wgen.net>
2012-08-16 14:36:04 -0400

    Add JUnit test for Include class in rest modeling module.

---
+++ sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/IncludeTest.java
@@ -0,0 +1,38 @@
+package org.slc.sli.modeling.rest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test for Include class.
+ *
+ * @author wscott
+ *
+ */
+public class IncludeTest {
+
+    private Include include;
+
+    private static final ArrayList<Documentation> DOC = new ArrayList<Documentation>(0);
+    private static final String HREF = "href";
+
+    @Before
+    public void setup() throws Exception {
+        include = new Include(HREF, DOC);
+    }
+
+    @Test
+    public void testNullHref() {
+        new Include(null, DOC);
+    }
+
+    @Test
+    public void testGetHref() {
+        assertEquals(HREF, include.getHref());
+    }
+
+}
