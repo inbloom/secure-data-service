f09fc886c4a5cb82d38d940f6c8ec59b66f8dad3
William Scott <wscott@wgen.net>
2012-08-16 14:56:18 -0400

    Checkstyle fixes.

--- sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/DocumentationTest.java
+++ sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/DocumentationTest.java
@@ -10,29 +10,29 @@ import org.junit.Test;
 import org.slc.sli.modeling.xdm.DmNode;
 
 /**
- * 
+ *
  * JUnit test for Documentation class.
- * 
+ *
  * @author wscott
- * 
+ *
  */
 public class DocumentationTest {
-    
+
     private Documentation documentation;  //class under test
     private static final String TITLE = "doctest";
     private static final String LANGUAGE = "en";
-    private final List<DmNode> CONTENTS = new ArrayList<DmNode>(0);
-    
+    private static final List<DmNode> CONTENTS = new ArrayList<DmNode>(0);
+
     @Before
     public void setup() {
         documentation = new Documentation(TITLE, LANGUAGE, CONTENTS);
     }
-    
+
     @Test (expected = NullPointerException.class)
     public void testNullContents() {
         new Documentation(TITLE, LANGUAGE, null);
     }
-    
+
     @Test
     public void testGetTitle() {
         assertEquals(TITLE, documentation.getTitle());
@@ -42,12 +42,12 @@ public class DocumentationTest {
     public void testGetLanguage() {
         assertEquals(LANGUAGE, documentation.getLanguage());
     }
-    
+
     @Test
     public void testGetContents() {
         assertEquals(CONTENTS, documentation.getContents());
     }
-    
+
     @Test
     public void testToString() {
         String expected = "{language : " + LANGUAGE + ", title : " + TITLE + ", contents : []}";
