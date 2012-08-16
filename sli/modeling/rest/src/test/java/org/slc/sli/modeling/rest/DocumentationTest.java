3bd46bcdf5886a502cf550cb64b0bb6d2132f103
William Scott <wscott@wgen.net>
2012-08-16 12:15:14 -0400

    Add JUnit test for Documentation class in rest modeling module.

---
+++ sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/DocumentationTest.java
@@ -0,0 +1,57 @@
+package org.slc.sli.modeling.rest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slc.sli.modeling.xdm.DmNode;
+
+/**
+ * 
+ * JUnit test for Documentation class.
+ * 
+ * @author wscott
+ * 
+ */
+public class DocumentationTest {
+    
+    private Documentation documentation;  //class under test
+    private static final String TITLE = "doctest";
+    private static final String LANGUAGE = "en";
+    private final List<DmNode> CONTENTS = new ArrayList<DmNode>(0);
+    
+    @Before
+    public void setup() {
+        documentation = new Documentation(TITLE, LANGUAGE, CONTENTS);
+    }
+    
+    @Test (expected = NullPointerException.class)
+    public void testNullContents() {
+        new Documentation(TITLE, LANGUAGE, null);
+    }
+    
+    @Test
+    public void testGetTitle() {
+        assertEquals(TITLE, documentation.getTitle());
+    }
+
+    @Test
+    public void testGetLanguage() {
+        assertEquals(LANGUAGE, documentation.getLanguage());
+    }
+    
+    @Test
+    public void testGetContents() {
+        assertEquals(CONTENTS, documentation.getContents());
+    }
+    
+    @Test
+    public void testToString() {
+        String expected = "{language : " + LANGUAGE + ", title : " + TITLE + ", contents : []}";
+        assertEquals(expected, documentation.toString());
+    }
+
+}
