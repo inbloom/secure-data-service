a78c328190850e8e5fe2131af48ce632f0685170
William Scott <wscott@wgen.net>
2012-08-16 14:40:01 -0400

    Add JUnit test for Grammars class in rest modeling module.

---
+++ sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/GrammarsTest.java
@@ -0,0 +1,43 @@
+package org.slc.sli.modeling.rest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test for Grammars class.
+ *
+ * @author wscott
+ *
+ */
+public class GrammarsTest {
+
+    private Grammars grammars;
+
+    private static final ArrayList<Include> INCLUDES = new ArrayList<Include>(0);
+    private static final ArrayList<Documentation> DOC = new ArrayList<Documentation>(0);
+
+    @Before
+    public void setup() throws Exception {
+        grammars = new Grammars(DOC, INCLUDES);
+    }
+
+    @Test
+    public void testNullIncludes() {
+        new Grammars(DOC, null);
+    }
+
+    @Test
+    public void testGetIncludes() {
+        assertEquals(INCLUDES, grammars.getIncludes());
+    }
+
+    @Test
+    public void testToString() {
+        assertTrue(!"".equals(grammars.toString()));
+    }
+
+}
