b10f365a65a8f21e4cf7ed495239c6c16c9b473b
William Scott <wscott@wgen.net>
2012-08-16 14:44:53 -0400

    Add JUnit test for Request class in rest modeling module.

---
+++ sli/modeling/rest/src/test/java/org/slc/sli/modeling/rest/RequestTest.java
@@ -0,0 +1,53 @@
+package org.slc.sli.modeling.rest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test for Request class.
+ *
+ * @author wscott
+ *
+ */
+public class RequestTest {
+
+    private Request request;
+
+    private static final ArrayList<Documentation> DOC = new ArrayList<Documentation>(0);
+    private static final ArrayList<Param> PARAMS = new ArrayList<Param>(0);
+    private static final ArrayList<Representation> REPRESENTATIONS = new ArrayList<Representation>(0);
+
+    @Before
+    public void setup() throws Exception {
+        request = new Request(DOC, PARAMS, REPRESENTATIONS);
+    }
+
+    @Test
+    public void testNullParams() {
+        new Request(DOC, null, REPRESENTATIONS);
+    }
+
+    @Test
+    public void testNullRepresentations() {
+        new Request(DOC, PARAMS, null);
+    }
+
+    @Test
+    public void testGetParams() {
+        assertEquals(PARAMS, request.getParams());
+    }
+
+    @Test
+    public void testGetRepresentations() {
+        assertEquals(REPRESENTATIONS, request.getRepresentations());
+    }
+
+    @Test
+    public void testToString() {
+        assertTrue(!"".equals(request.toString()));
+    }
+}
