25a9a8be683bcf8ae9eb54b9bb88458bbd1dd504
tshewchuk <tshewchuk@wgen.net>
2012-08-17 15:16:31 -0400

    Added unit test.

--- sli/data-access/dal/src/test/java/org/slc/sli/dal/RetryMongoCommandTest.java
+++ sli/data-access/dal/src/test/java/org/slc/sli/dal/RetryMongoCommandTest.java
@@ -14,17 +14,27 @@
  * limitations under the License.
  */
 
-
 package org.slc.sli.dal;
 
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.mongodb.MongoException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.slc.sli.domain.MongoEntity;
 
 /**
  * Junit for RetryMongoCommand
@@ -36,27 +46,55 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class RetryMongoCommandTest {
 
+    private RetryMongoCommand retryMongoCommand = null;
+
     private MongoTemplate mockedGoodMongoTemplate;
     private MongoTemplate mockedBadMongoTemplate;
+    private MongoTemplate mockedNotSoBadMongoTemplate;
     private MongoTemplate mockedDuplicateMongoTemplate;
+    private Logger mockedLogger;
+    MongoEntity student;
 
     @Before
     public void setup() {
-        // Setup the mocked Mongo Template.
+        // Setup the mocked Mongo Templates and Logger.
         mockedGoodMongoTemplate = mock(MongoTemplate.class);
         mockedBadMongoTemplate = mock(MongoTemplate.class);
+        mockedNotSoBadMongoTemplate = mock(MongoTemplate.class);
         mockedDuplicateMongoTemplate = mock(MongoTemplate.class);
+        mockedLogger = mock(Logger.class);
+        student = new MongoEntity("student", null);
     }
 
     @Test
     public void testRetryMongoTemplateCommandWithGood() {
+        when(mockedGoodMongoTemplate.findById(any(Object.class), eq(MongoEntity.class), eq("student"))).thenReturn(
+                new MongoEntity("Good!", null));
+        retryMongoCommand = new RetryMongoCommand(mockedGoodMongoTemplate, mockedLogger);
+        assertEquals(1, retryMongoCommand.insertWithRetries(student, "student"));
     }
 
     @Test
     public void testRetryMongoTemplateCommandWithBad() {
+        when(mockedBadMongoTemplate.findById(any(Object.class), eq(MongoEntity.class), eq("student"))).thenThrow(
+                new MongoException("Bad!"));
+        retryMongoCommand = new RetryMongoCommand(mockedBadMongoTemplate, mockedLogger);
+        assertEquals(0, retryMongoCommand.findOneWithRetries(new Query(), MongoEntity.class, "student"));
+    }
+
+    @Test
+    public void testRetryMongoTemplateCommandWithNotSoBad() {
+        when(mockedNotSoBadMongoTemplate.findById(any(Object.class), eq(MongoEntity.class), eq("student"))).thenThrow(
+                new MongoException("Bad!")).thenReturn(new MongoEntity("Good!", null));
+        retryMongoCommand = new RetryMongoCommand(mockedNotSoBadMongoTemplate, mockedLogger);
+        assertEquals(2, retryMongoCommand.findOneWithRetries(new Query(), MongoEntity.class, "student"));
     }
 
     @Test
     public void testRetryMongoTemplateCommandWithDuplicate() {
+        doThrow(new MongoException("Duplicate!")).when(mockedDuplicateMongoTemplate).insert(any(Object.class),
+                eq("student"));
+        retryMongoCommand = new RetryMongoCommand(mockedDuplicateMongoTemplate, mockedLogger);
+        assertEquals(1, retryMongoCommand.insertWithRetries(student, "student"));
     }
 }
