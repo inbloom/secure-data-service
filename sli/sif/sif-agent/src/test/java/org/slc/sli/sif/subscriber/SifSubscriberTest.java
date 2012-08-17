7b5cba5d914ef4725b5519cb88da779b64feb689
Steve Ashton <sashton@wgen.net>
2012-08-17 09:00:13 -0400

    RED: added shouldTranslateEventEntities()

--- sli/sif/sif-agent/src/test/java/org/slc/sli/sif/subscriber/SifSubscriberTest.java
+++ sli/sif/sif-agent/src/test/java/org/slc/sli/sif/subscriber/SifSubscriberTest.java
@@ -13,16 +13,62 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.slc.sli.sif.subscriber;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import openadk.library.ADKException;
+import openadk.library.Event;
+import openadk.library.EventAction;
+import openadk.library.MessageInfo;
+import openadk.library.Zone;
+import openadk.library.student.SchoolInfo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import org.slc.sli.api.client.impl.GenericEntity;
+import org.slc.sli.sif.AdkTest;
+import org.slc.sli.sif.translation.SifTranslationManager;
 
 /**
  * JUnit tests for SifSubscriber
  *
  */
-public class SifSubscriberTest {
+public class SifSubscriberTest extends AdkTest {
 
+    @InjectMocks
+    private SifSubscriber subscriber = new SifSubscriber();
+
+    @Mock
+    private SifTranslationManager translationManager;
+
+    @Before
+    public void setupMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldTranslateEventEntities() throws ADKException {
+
+        SchoolInfo sifData = new SchoolInfo();
+        Event event = new Event(sifData, EventAction.ADD);
+        MessageInfo info = Mockito.mock(MessageInfo.class);
+        Zone zone = Mockito.mock(Zone.class);
+        Mockito.when(zone.getZoneId()).thenReturn("zoneId");
+
+        List<GenericEntity> translatedEntities = new ArrayList<GenericEntity>();
+
+        Mockito.when(translationManager.translate(sifData, "zoneId")).thenReturn(translatedEntities);
+
+        subscriber.onEvent(event, zone, info);
+
+        Mockito.verify(translationManager, Mockito.times(1)).translate(sifData, "zoneId");
+    }
 
 }
