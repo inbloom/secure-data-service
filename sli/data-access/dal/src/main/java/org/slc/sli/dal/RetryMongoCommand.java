204021926ef001f60140d2b3c064049d7bfcbb07
Nupur Pandey <npandey@wgen.net>
2012-08-20 12:21:57 -0400

    Retrial Logic

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/RetryMongoCommand.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/RetryMongoCommand.java
@@ -16,72 +16,31 @@
 
 package org.slc.sli.dal;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
-
+import com.mongodb.MongoException;
 /**
  * Way to retry mongo commands
  *
  * @author tshewchuk
  *
  */
-public class RetryMongoCommand {
-    private final MongoTemplate template;
-    private final Logger LOG;
-
-    public RetryMongoCommand(MongoTemplate template, Logger log) {
-        this.template = template;
-        this.LOG = log;
+public abstract class RetryMongoCommand {
+    public Object executeOperation(int noOfRetries) throws Exception {
+        Object result = null;
+        while (noOfRetries > 0) {
+            try {
+               result = execute();
+                break;
+            } catch (MongoException me) {
+                if (me.getCode() == 11000) {
+                    break;
+                }
+                noOfRetries--;
+            } catch (Exception ex) {
+                throw ex;
+            }
+        }
+        return result;
     }
 
-    /**
-     * Retry inserting a record n times
-     *
-     * @param record
-     *        record to insert
-     * @param collectionName
-     *        name of collection to insert new record
-     *
-     * @return int
-     *        number of insert attempts, or 0 if unsuccessful
-     */
-    public <T> int insertWithRetries(T record, String collectionName) {
-        int tries = 0;
-        return tries;
-    }
-
-    /**
-     * Retry inserting a records batch n times
-     *
-     * @param records
-     *        records batch to insert
-     * @param collectionName
-     *        name of collection to insert new record
-     *
-     * @return int
-     *        number of insert attempts, or 0 if unsuccessful
-     */
-    public <T> int insertWithRetries(List<T> records, String collectionName) {
-        int tries = 0;
-        return tries;
-    }
-
-    /**
-     * Retry finding the first record in a collection n times
-     *
-     * @param record
-     *        record to insert
-     * @param collectionName
-     *        name of collection to insert new record
-     *
-     * @return int
-     *        number of insert attempts, or 0 if unsuccessful
-     */
-    public <T> int findOneWithRetries(Query query, Class<T> entityClass, String collectionName) {
-        int tries = 0;
-        return tries;
-    }
+    public abstract Object execute();
 }
