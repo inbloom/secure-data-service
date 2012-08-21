dd1f48e8c320aae363a56abf0001cd8d43d0ae74
Steve Ashton <sashton@wgen.net>
2012-08-21 11:58:19 -0400

    [US3369] Added date converter

---
+++ sli/sif/sif-agent/src/test/java/org/slc/sli/sif/domain/converter/DateConverterTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.slc.sli.sif.domain.converter;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class DateConverterTest {
+
+    DateConverter converter = new DateConverter();
+
+    @Test
+    public void shouldConvertWithCorrectFormat() {
+
+        Calendar date = new GregorianCalendar(2004, Calendar.FEBRUARY, 29);
+        String result = converter.convert(date);
+
+        Assert.assertEquals("2004-02-29", result);
+    }
+
+    @Test
+    public void shouldHandleNullDate() {
+        String result = converter.convert(null);
+
+        Assert.assertNull(result);
+    }
+
+}
