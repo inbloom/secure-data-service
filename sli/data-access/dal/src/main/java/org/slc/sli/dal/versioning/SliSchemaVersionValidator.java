bd48a82439074c3e4de2b4c3e54602e038756c7d
William Scott <wscott@wgen.net>
2012-08-23 09:44:32 -0400

    Remove unused import.

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/versioning/SliSchemaVersionValidator.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/versioning/SliSchemaVersionValidator.java
@@ -30,7 +30,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.stereotype.Component;
 
 import org.slc.sli.validation.SchemaRepository;
 import org.slc.sli.validation.schema.AppInfo;
