989a1fc0a882e0b1b4bf8b45eed74860dc82aacf
Doris Ip <dip@wgen.net>
2012-08-28 17:37:05 -0400

    catch it in case there are errors

--- sli/acceptance-tests/test/features/dashboard/dash/step_definitions/dashboard_builder.rb
+++ sli/acceptance-tests/test/features/dashboard/dash/step_definitions/dashboard_builder.rb
@@ -154,7 +154,10 @@ def hoverOverPage(pageName, mode = nil)
     #TODO
   elsif (mode == "delete")
     @driver.find_element(:css, "[class*='tab-content']").find_element(:css,"[class*='active']").find_element(:css, "[ng-click='removePage()']").click
-    @driver.switch_to.alert.accept
+    begin
+      @driver.switch_to.alert.accept
+    rescue
+    end
   end  
 end
 
