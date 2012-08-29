956a480e4072572e4b1e5d9672f24417152de879
Ryan Latta <rlatta@wgen.net>
2012-08-29 13:53:42 -0400

    Fixing imports.

--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherToStudentDisciplineIncidentAssociationResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherToStudentDisciplineIncidentAssociationResolver.java
@@ -17,8 +17,8 @@
 
 package org.slc.sli.api.security.context.resolver;
 
-import org.slc.sli.api.client.constants.EntityNames;
-import org.slc.sli.api.client.constants.ResourceNames;
+import org.slc.sli.api.constants.EntityNames;
+import org.slc.sli.api.constants.ResourceNames;
 import org.slc.sli.api.security.context.AssociativeContextHelper;
 import org.slc.sli.domain.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherToTeacherSectionAssociationResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherToTeacherSectionAssociationResolver.java
@@ -17,7 +17,7 @@
 
 package org.slc.sli.api.security.context.resolver;
 
-import org.slc.sli.api.client.constants.EntityNames;
+import org.slc.sli.api.constants.EntityNames;
 import org.slc.sli.api.security.context.AssociativeContextHelper;
 import org.slc.sli.domain.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
