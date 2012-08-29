fd7df2a7512fae7895ddea3ca4d37c25c0454355
Stan Halka <shalka@wgen.net>
2012-08-29 18:33:52 -0400

    fixed previous addToSet error

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoRepository.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/repository/MongoRepository.java
@@ -415,13 +415,16 @@ public abstract class MongoRepository<T> implements Repository<T> {
                 for (Map.Entry<String, Object> fieldValues : operands.entrySet()) {
                     convertedUpdate.push(fieldValues.getKey(), fieldValues.getValue());
                 }
-            } else if (operation.equals("pushAll") || operation.equals("addToSet")) {
+            } else if (operation.equals("pushAll")) {
                 for (Map.Entry<String, Object> fieldValues : operands.entrySet()) {
                     convertedUpdate.pushAll(fieldValues.getKey(), (Object[]) fieldValues.getValue());
                 }
+            } else if (operation.equals("addToSet")) {
+                for (Map.Entry<String, Object> fieldValues : operands.entrySet()) {
+                    convertedUpdate.addToSet(fieldValues.getKey(), fieldValues.getValue());
+                }
             }
         }
-
         return template.updateFirst(convertedQuery, convertedUpdate, collectionName);
     }
 
