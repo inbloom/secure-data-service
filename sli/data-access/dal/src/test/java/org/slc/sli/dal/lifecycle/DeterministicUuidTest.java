622bd71f8818a764c7de7f67acf2b2a0ae63b2ba
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-09-07 12:43:56 -0400

    [US3950] moved lifecycle POC code to SLI, starting unit tests

---
+++ sli/data-access/dal/src/test/java/org/slc/sli/dal/lifecycle/DeterministicUuidTest.java
@@ -0,0 +1,34 @@
+package org.slc.sli.dal.lifecycle;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Tests for DeterministicUuid
+ * @author vmcglaughlin
+ *
+ */
+public class DeterministicUuidTest {
+
+    @Test
+    public void testGenerate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetUUIDModelEntityString() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetUUIDModelEntityStringBoolean() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGenerateUUID() {
+        fail("Not yet implemented");
+    }
+
+}
