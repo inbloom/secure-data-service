c118d93339840de22ba92e78ac385ca830b97728
kmyers <kmyers@wgen.net>
2012-09-10 10:21:28 -0400

    Add test

--- sli/modeling/waudit/src/test/java/org/slc/sli/modeling/waudit/WadlExpertTest.java
+++ sli/modeling/waudit/src/test/java/org/slc/sli/modeling/waudit/WadlExpertTest.java
@@ -5,9 +5,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 import org.junit.Test;
+import org.slc.sli.modeling.rest.Documentation;
+import org.slc.sli.modeling.rest.Method;
+import org.slc.sli.modeling.rest.Param;
+import org.slc.sli.modeling.rest.Resource;
 
 public class WadlExpertTest {
 
@@ -38,11 +44,44 @@ public class WadlExpertTest {
 		assertEquals(expectedResult, WadlExpert.splitBasedOnFwdSlash(string));
 	}
 	
+	private Resource createResource(String path) {
+		String id = "";
+		List<String> type = new ArrayList<String>();
+		String queryType = "";
+		List<Documentation> doc = new ArrayList<Documentation>();
+		List<Param> params = new ArrayList<Param>();
+		List<Method> methods = new ArrayList<Method>();
+		List<Resource> resources = new ArrayList<Resource>();
+		String resourceClass = "";
+		
+		return new Resource(id, type, queryType, path, doc, params, methods, resources, resourceClass);
+	}
+	
 	@Test
 	public void testToSteps() {
 		
+		Stack<Resource> ancestors = new Stack<Resource>();
+		ancestors.push(this.createResource("a/b/c"));
+		ancestors.push(this.createResource("d/e/f"));
+		
+		Resource resource = this.createResource("g/h/i");
+		
+		List<String> receivedResult = WadlExpert.toSteps(resource, ancestors);
+		
+		List<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("a");
+		expectedResult.add("b");
+		expectedResult.add("c");
+		expectedResult.add("d");
+		expectedResult.add("e");
+		expectedResult.add("f");
+		expectedResult.add("g");
+		expectedResult.add("h");
+		expectedResult.add("i");
+		
+		assertEquals(expectedResult, receivedResult);
+		
 	}
-
 
 
 
