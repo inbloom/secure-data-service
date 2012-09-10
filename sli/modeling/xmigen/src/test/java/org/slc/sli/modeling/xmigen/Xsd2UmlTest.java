abc152f70a5d73a264634e2cdc6d4351f83cf2ee
pghosh <pghosh@wgen.net>
2012-09-10 10:18:40 -0400

    [us3865] added unit test

---
+++ sli/modeling/xmigen/src/test/java/org/slc/sli/modeling/xmigen/Xsd2UmlTest.java
@@ -0,0 +1,35 @@
+package org.slc.sli.modeling.xmigen;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Xsd2Uml Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 7, 2012</pre> 
+* @version 1.0 
+*/ 
+public class Xsd2UmlTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: transform(final String name, final XmlSchema schema, final Xsd2UmlPlugin plugin) 
+* 
+*/ 
+@Test
+public void testTransform() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
