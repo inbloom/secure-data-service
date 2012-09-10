446c9c0fc18fb5bbd05e53cbef77b904b67cdb8f
William Scott <wscott@wgen.net>
2012-09-10 15:47:05 -0400

    Make methods non-final to allow mocking.

--- sli/modeling/uml/src/main/java/org/slc/sli/modeling/uml/Feature.java
+++ sli/modeling/uml/src/main/java/org/slc/sli/modeling/uml/Feature.java
@@ -48,12 +48,12 @@ public abstract class Feature extends NamedModelElement implements HasType, HasM
     public abstract boolean isAssociationEnd();
     
     @Override
-    public final Multiplicity getMultiplicity() {
+    public Multiplicity getMultiplicity() {
         return multiplicity;
     }
     
     @Override
-    public final Identifier getType() {
+    public Identifier getType() {
         return type;
     }
     
