9a90a5a99c7e0c557ba89b568a3562a953852ad8
Ryan Latta <rlatta@wgen.net>
2012-09-10 11:10:36 -0400

    Section/session resolvers fixed and some stupid caching

--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherSectionResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherSectionResolver.java
@@ -7,11 +7,16 @@ import java.util.List;
 import java.util.Set;
 
 import org.slc.sli.api.constants.EntityNames;
+import org.slc.sli.api.constants.ParameterConstants;
 import org.slc.sli.api.constants.ResourceNames;
 import org.slc.sli.api.security.context.AssociativeContextHelper;
 import org.slc.sli.api.security.context.traversal.cache.impl.SessionSecurityCache;
 import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
+import org.slc.sli.domain.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -28,6 +33,13 @@ public class TeacherSectionResolver implements EntityContextResolver {
     
     @Autowired
     private SessionSecurityCache securityCache;
+    
+    @Autowired
+    private TeacherStudentResolver studentResolver;
+    
+    @Autowired
+    @Qualifier("validationRepo")
+    private Repository<Entity> repo;
 
     @Override
     public boolean canResolve(String fromEntityType, String toEntityType) {
@@ -37,13 +49,26 @@ public class TeacherSectionResolver implements EntityContextResolver {
 
     @Override
     public List<String> findAccessible(Entity principal) {
+        
+        // We really want the students and the sections that those students are in.
+        List<String> studentIds = new ArrayList<String>();
+        if (!securityCache.contains(EntityNames.STUDENT)) {
+            studentIds = studentResolver.findAccessible(principal);
+        } else {
+            studentIds = new ArrayList<String>(securityCache.retrieve(EntityNames.STUDENT));
+        }
+
         List<String> teacherSectionIds = helper.findAccessible(principal, Arrays.asList(
                 ResourceNames.TEACHER_SECTION_ASSOCIATIONS));
+        List<String> studentSectionIds = new ArrayList<String>();
 
-        List<String> studentSectionIds = helper.findAccessible(principal, Arrays.asList(
-                ResourceNames.TEACHER_SECTION_ASSOCIATIONS,
-                ResourceNames.STUDENT_SECTION_ASSOCIATIONS,
-                ResourceNames.STUDENT_SECTION_ASSOCIATIONS));
+        Iterable<Entity> studentSections = repo.findAll(EntityNames.STUDENT_SECTION_ASSOCIATION, new NeutralQuery(
+                new NeutralCriteria("studentId", NeutralCriteria.CRITERIA_IN, studentIds)));
+        for (Entity studentSection : studentSections) {
+            if (studentSection.getBody().containsKey(ParameterConstants.SECTION_ID)) {
+                studentSectionIds.add((String) studentSection.getBody().get(ParameterConstants.SECTION_ID));
+            }
+        }
 
         Set<String> sectionIds = new HashSet<String>();
         sectionIds.addAll(teacherSectionIds);
