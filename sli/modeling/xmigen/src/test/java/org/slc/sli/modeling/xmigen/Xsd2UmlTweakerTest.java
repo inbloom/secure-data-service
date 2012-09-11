3e082f53ec3bd987e7cde2bdf0b6212eebaf3f6d
pghosh <pghosh@wgen.net>
2012-09-11 11:16:04 -0400

    [us3865] unit test for Xsd2UmlPluginGeneric

---
+++ sli/modeling/xmigen/src/test/java/org/slc/sli/modeling/xmigen/Xsd2UmlTweakerTest.java
@@ -0,0 +1,45 @@
+package org.slc.sli.modeling.xmigen;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.slc.sli.modeling.uml.Identifier;
+import org.slc.sli.modeling.uml.Model;
+
+/** 
+* Xsd2UmlTweaker Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 11, 2012</pre> 
+* @version 1.0 
+*/ 
+public class Xsd2UmlTweakerTest { 
+
+    @Mock
+    Model model;
+
+    @Mock
+    Xsd2UmlPlugin plugin;
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: tweak(final Model model, final Xsd2UmlPlugin plugin) 
+* 
+*/ 
+@Test
+public void testTweak() throws Exception {
+//    Xsd2UmlTweaker.tweak(new Model(Mockito.any(Identifier.class),Mockito.anyString(),Mockito.anyList(),Mockito.anyList()),plugin);
+}
+
+
+} 
