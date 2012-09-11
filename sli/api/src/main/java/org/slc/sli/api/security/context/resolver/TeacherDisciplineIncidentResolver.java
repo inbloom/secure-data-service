4546ba8b306d217850e6fcba64cd4151835b74bf
Ryan Latta <rlatta@wgen.net>
2012-09-11 10:25:51 -0400

    All api tests pass

--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherDisciplineIncidentResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherDisciplineIncidentResolver.java
@@ -17,18 +17,20 @@
 
 package org.slc.sli.api.security.context.resolver;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import org.slc.sli.api.constants.EntityNames;
-import org.slc.sli.api.constants.ResourceNames;
-import org.slc.sli.api.security.context.AssociativeContextHelper;
+import org.slc.sli.api.constants.ParameterConstants;
 import org.slc.sli.api.security.context.traversal.cache.impl.SessionSecurityCache;
 import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
+import org.slc.sli.domain.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
 /**
  * Resolves which DisciplineIncident a given teacher is allowed to see
@@ -40,7 +42,11 @@ import org.slc.sli.domain.Entity;
 public class TeacherDisciplineIncidentResolver implements EntityContextResolver {
 
     @Autowired
-    private AssociativeContextHelper helper;
+    @Qualifier("validationRepo")
+    private Repository<Entity> repo;
+    
+    @Autowired
+    private TeacherToStudentDisciplineIncidentAssociationResolver studentResolver;
     
     @Autowired
     private SessionSecurityCache securityCachingStrategy;
@@ -52,9 +58,21 @@ public class TeacherDisciplineIncidentResolver implements EntityContextResolver 
 
     @Override
     public List<String> findAccessible(Entity principal) {
-        List<String> ids = helper.findAccessible(principal, Arrays.asList(ResourceNames.TEACHER_SECTION_ASSOCIATIONS,
-                                                              ResourceNames.STUDENT_SECTION_ASSOCIATIONS,
-                                                              ResourceNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATIONS));
+        List<String> studentIds = new ArrayList<String>();
+        if (!securityCachingStrategy.contains(EntityNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATION)) {
+            studentIds = studentResolver.findAccessible(principal);
+        } else {
+            studentIds = new ArrayList<String>(
+                    securityCachingStrategy.retrieve(EntityNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATION));
+        }
+        Iterable<Entity> sdias = repo.findAll(EntityNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATION, new NeutralQuery(
+                new NeutralCriteria(ParameterConstants.ID, NeutralCriteria.CRITERIA_IN, studentIds)));
+        List<String> ids = new ArrayList<String>();
+        for (Entity assoc : sdias) {
+            if (assoc.getBody().containsKey(ParameterConstants.DISCIPLINE_INCIDENT_ID)) {
+                ids.add((String) assoc.getBody().get(ParameterConstants.DISCIPLINE_INCIDENT_ID));
+            }
+        }
         securityCachingStrategy.warm(EntityNames.DISCIPLINE_INCIDENT, new HashSet<String>(ids));
         return ids;
     }
