52e2ef190438fc6b391e98b8fb7e9e28a0e80eb1
pghosh <pghosh@wgen.net>
2012-09-11 10:37:54 -0400

    [us3865] unit test for Xsd2UmlPluginHostAdapter

--- sli/modeling/uml/src/main/java/org/slc/sli/modeling/uml/ModelElement.java
+++ sli/modeling/uml/src/main/java/org/slc/sli/modeling/uml/ModelElement.java
@@ -64,7 +64,7 @@ public abstract class ModelElement implements HasIdentity, Taggable, Visitable {
     }
     
     @Override
-    public final boolean equals(final Object obj) {
+    public  boolean equals(final Object obj) {
         if (obj instanceof ModelElement) {
             final ModelElement other = (ModelElement) obj;
             return id.equals(other.id);
