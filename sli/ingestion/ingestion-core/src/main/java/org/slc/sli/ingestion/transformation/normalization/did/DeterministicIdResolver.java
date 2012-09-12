65d7e07f3dd6ef84773e7ed5756bd4b7ef614f39
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-09-12 12:43:50 -0400

    [US3951] early changes for DID resolution

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolver.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolver.java
@@ -16,12 +16,48 @@
 
 package org.slc.sli.ingestion.transformation.normalization.did;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import org.slc.sli.common.domain.NaturalKeyDescriptor;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.ingestion.transformation.normalization.IdNormalizer;
+import org.slc.sli.ingestion.validation.ErrorReport;
+
 /**
  * Resolver for deterministic Id resolution.
  *
  * @author jtully
  *
  */
+@Component
 public class DeterministicIdResolver {
 
+//    @Autowired
+//    private UUIDGeneratorStrategy uuidGeneratorStrategy;
+
+    private static final Logger LOG = LoggerFactory.getLogger(IdNormalizer.class);
+
+    public void resolveInternalIds(Entity entity, String tenantId, DidEntityConfig entityConfig, DidRefConfig refConfig,
+            ErrorReport errorReport) {
+
+        // for each reference in entityConfig
+
+        // pull out reference type map from entityConfig
+        // if a list, resolve to list of ids (iterate over list)
+        // if a single object, resolve to single id
+
+        // resolve reference type map to single id
+
+        // function which, given reference type map (source object) and refConfig, return a did
+
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+
+        NaturalKeyDescriptor naturalKeyDescriptor = new NaturalKeyDescriptor(naturalKeys, tenantId, entity.getType());
+//        String uuid = uuidGeneratorStrategy.generateId(naturalKeyDescriptor);
+    }
 }
