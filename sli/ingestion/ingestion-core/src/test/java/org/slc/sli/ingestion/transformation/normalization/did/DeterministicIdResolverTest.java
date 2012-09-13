36d7360cf4a3bd3eb8f5c04509465abe806f0107
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-09-13 15:22:52 -0400

    [US3951] robustness and unit tests for DeterministicIdResolver

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolverTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolverTest.java
@@ -17,6 +17,7 @@
 package org.slc.sli.ingestion.transformation.normalization.did;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,11 +39,13 @@ import org.slc.sli.ingestion.NeutralRecord;
 import org.slc.sli.ingestion.NeutralRecordEntity;
 import org.slc.sli.ingestion.landingzone.validation.TestErrorReport;
 import org.slc.sli.ingestion.validation.ErrorReport;
+import org.slc.sli.validation.SchemaRepository;
 
 /**
  * Unit tests for DeterministicIdResolver
  *
  * @author jtully
+ * @author vmcglaughlin
  *
  */
 public class DeterministicIdResolverTest {
@@ -53,11 +56,15 @@ public class DeterministicIdResolverTest {
     private UUIDGeneratorStrategy didGenerator;
 
     @Mock
+    private SchemaRepository schemaRepository;
+
+    @Mock
     private DidEntityConfigFactory didEntityConfigs;
 
     @Mock
     private DidRefConfigFactory didRefConfigs;
 
+    private static final String TENANT = "tenant";
     private static final String ENTITY_TYPE = "entity_type";
     private static final String SRC_KEY_FIELD = "key_field";
     private static final String SRC_KEY_VALUE = "key_value";
@@ -65,6 +72,7 @@ public class DeterministicIdResolverTest {
     private static final String DID_TARGET_FIELD = "id_field";
     private static final String SRC_REF_FIELD = "ref_field";
 
+    private static final String NON_DID_ENTITY_TYPE = "non_did_entity_type";
 
     @Before
     public void setup() {
@@ -82,12 +90,19 @@ public class DeterministicIdResolverTest {
 
         Mockito.when(didRefConfigs.getDidRefConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(refConfig);
         Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
-        //maybe we should implementent an equals method for NaturalKeyDescriptor to allow a better test
-        Mockito.when(didGenerator.generateId(Mockito.any(NaturalKeyDescriptor.class))).thenReturn(DID_VALUE);
+        Mockito.when(schemaRepository.getSchema(ENTITY_TYPE)).thenReturn(null);
+
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+        naturalKeys.put(SRC_KEY_FIELD, SRC_KEY_VALUE);
+        String entityType = ENTITY_TYPE;
+        String tenantId = TENANT;
+        NaturalKeyDescriptor ndk = new NaturalKeyDescriptor(naturalKeys, tenantId, entityType);
+
+        Mockito.when(didGenerator.generateId(ndk)).thenReturn(DID_VALUE);
 
         ErrorReport errorReport = new TestErrorReport();
 
-        didResolver.resolveInternalIds(entity, "TENANT", errorReport);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
 
         Assert.assertEquals(DID_VALUE, entity.getBody().get(DID_TARGET_FIELD));
         Assert.assertFalse("no errors should be reported from reference resolution ", errorReport.hasErrors());
@@ -95,17 +110,160 @@ public class DeterministicIdResolverTest {
 
     @Test
     public void shouldFailFastForNonDidEntities() {
+        Entity entity = createSourceEntity();
+        ErrorReport errorReport = new TestErrorReport();
+
+        // test null entity config
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(NON_DID_ENTITY_TYPE))).thenReturn(null);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+        Mockito.verifyZeroInteractions(didRefConfigs);
+
+        // test null ref config list
+        DidEntityConfig entityConfig = Mockito.mock(DidEntityConfig.class);
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
+        Mockito.when(entityConfig.getReferenceSources()).thenReturn(null);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+        Mockito.verifyZeroInteractions(didRefConfigs);
+
+        // test empty ref config list
+        Mockito.when(entityConfig.getReferenceSources()).thenReturn(new ArrayList<DidRefSource>());
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+        Mockito.verifyZeroInteractions(didRefConfigs);
+    }
+
+    @Test
+    public void shouldResolveNestedDid() throws IOException {
+
+        // TODO get this test working correctly
+
+        DidRefConfig refConfig = createRefConfig("test_DID_ref_config.json");
+        DidEntityConfig entityConfig = createEntityConfig("test_DID_entity_config.json");
+        ErrorReport errorReport = new TestErrorReport();
+        Entity entity = createSourceEntity();
+        Mockito.when(didRefConfigs.getDidRefConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(refConfig);
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
+        Mockito.when(schemaRepository.getSchema(ENTITY_TYPE)).thenReturn(null);
+        Mockito.when(didGenerator.generateId(Mockito.any(NaturalKeyDescriptor.class))).thenReturn(DID_VALUE);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
 
     }
 
     @Test
-    public void shouldResolveNestedDid() {
+    public void testErrorReportingOnRefConfigEntityTypeEmpty() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_entityType_empty.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
 
+        testGenericErrorReporting(refConfig, entityConfig);
     }
 
     @Test
-    public void shouldGenerateErrorReportWithBadData() {
+    public void testErrorReportingOnRefConfigEntityTypeMissing() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_entityType_missing.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
 
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigKeyFieldNameEmpty() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_keyFieldName_empty.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigKeyFieldNameMissing() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_keyFieldName_missing.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigKeyFieldsEmpty() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_keyFields_empty.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigKeyFieldsMissing() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_keyFields_missing.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigValueSourceEmpty() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_valueSource_empty.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnRefConfigValueSourceMissing() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config_valueSource_missing.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    @Test
+    public void testErrorReportingOnEntityConfigDidFieldPathEmpty() throws IOException {
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config_didFieldPath_empty.json");
+
+        testGenericErrorReporting(refConfig, entityConfig);
+    }
+
+    private void testGenericErrorReporting(DidRefConfig refConfig, DidEntityConfig entityConfig) {
+        ErrorReport errorReport = new TestErrorReport();
+        Entity entity = createSourceEntity();
+        Mockito.when(didRefConfigs.getDidRefConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(refConfig);
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
+        Mockito.when(schemaRepository.getSchema(ENTITY_TYPE)).thenReturn(null);
+        Mockito.when(didGenerator.generateId(Mockito.any(NaturalKeyDescriptor.class))).thenReturn(DID_VALUE);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+
+        Assert.assertNull("Id should not have been resolved", entity.getBody().get(DID_TARGET_FIELD));
+        Assert.assertTrue("Errors should be reported from reference resolution ", errorReport.hasErrors());
+    }
+
+    @Test
+    public void testErrorReportingOnEntityRefFieldMissing() throws IOException {
+        // entity doesn't have property to get at sourceRefPath
+        ErrorReport errorReport = new TestErrorReport();
+        Entity entity = createSourceEntityMissingRefField();
+        DidRefConfig refConfig = createRefConfig("Simple_DID_ref_config.json");
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        Mockito.when(didRefConfigs.getDidRefConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(refConfig);
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
+        Mockito.when(schemaRepository.getSchema(ENTITY_TYPE)).thenReturn(null);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+
+        Assert.assertNull("Id should not have been resolved", entity.getBody().get(DID_TARGET_FIELD));
+        Assert.assertTrue("Errors should be reported from reference resolution ", errorReport.hasErrors());
+    }
+
+    @Test
+    public void testErrorReportingOnFactoryMissingRefConfigForEntityType() throws IOException {
+        ErrorReport errorReport = new TestErrorReport();
+        Entity entity = createSourceEntityMissingRefField();
+        DidEntityConfig entityConfig = createEntityConfig("Simple_DID_entity_config.json");
+
+        Mockito.when(didRefConfigs.getDidRefConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(null);
+        Mockito.when(didEntityConfigs.getDidEntityConfiguration(Mockito.eq(ENTITY_TYPE))).thenReturn(entityConfig);
+        Mockito.when(schemaRepository.getSchema(ENTITY_TYPE)).thenReturn(null);
+        didResolver.resolveInternalIds(entity, TENANT, errorReport);
+
+        Assert.assertNull("Id should not have been resolved", entity.getBody().get(DID_TARGET_FIELD));
+        Assert.assertFalse("No errors should be reported from reference resolution ", errorReport.hasErrors());
+        Mockito.verifyZeroInteractions(schemaRepository);
     }
 
     private DidEntityConfig createEntityConfig(String fileName) throws IOException {
@@ -139,5 +297,19 @@ public class DeterministicIdResolverTest {
 
         return entity;
     }
+
+    private Entity createSourceEntityMissingRefField() {
+
+        Map<String, Object> attributes = new HashMap<String, Object>();
+
+        NeutralRecord nr = new NeutralRecord();
+        nr.setAttributes(attributes);
+        nr.setRecordType(ENTITY_TYPE);
+
+        Entity entity = new NeutralRecordEntity(nr);
+
+        return entity;
+    }
+
 
 }
