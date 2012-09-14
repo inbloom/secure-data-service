7935b66cb126a30857c266480272ff6f9e8f174f
kmyers <kmyers@wgen.net>
2012-09-14 15:19:07 -0400

    remove unused import

--- sli/modeling/xmi/src/test/java/org/slc/sli/modeling/xmi/reader/XmiReaderTest.java
+++ sli/modeling/xmi/src/test/java/org/slc/sli/modeling/xmi/reader/XmiReaderTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
