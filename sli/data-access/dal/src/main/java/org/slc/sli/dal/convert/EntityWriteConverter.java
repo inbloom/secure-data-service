1f7162a17a0d5c972ae1f2759cf1c8889524a0f8
Steve Ashton <sashton@wgen.net>
2012-09-17 15:07:45 -0400

    Revert "Revert "[US3950] Use extracted natural keys from xsd during id generation""
    This reverts commit f5afed598dd64063846e55f01ad902fc738759d2.

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/EntityWriteConverter.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/EntityWriteConverter.java
@@ -14,48 +14,52 @@
  * limitations under the License.
  */
 
-
 package org.slc.sli.dal.convert;
 
 import com.mongodb.DBObject;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.convert.converter.Converter;
 
 import org.slc.sli.common.util.uuid.UUIDGeneratorStrategy;
 import org.slc.sli.dal.encrypt.EntityEncryption;
 import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.MongoEntity;
+import org.slc.sli.validation.schema.NaturalKeyExtractor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
 
 /**
  * Spring converter registered in the Mongo configuration to convert MongoEntity objects into
  * DBObjects.
- *
+ * 
  */
 public class EntityWriteConverter implements Converter<Entity, DBObject> {
-
+    
     @Autowired(required = false)
     @Qualifier("entityEncryption")
     EntityEncryption encrypt;
-
+    
     @Autowired
+    // @Qualifier("deterministicUUIDGeneratorStrategy")
     @Qualifier("shardType1UUIDGeneratorStrategy")
     UUIDGeneratorStrategy uuidGeneratorStrategy;
-
+    
+    @Autowired
+    NaturalKeyExtractor naturalKeyExtractor;
+    
     @Override
     public DBObject convert(Entity e) {
         MongoEntity me;
-
+        
         if (e instanceof MongoEntity) {
             me = (MongoEntity) e;
         } else {
-            me = new MongoEntity(e.getType(), e.getEntityId(), e.getBody(), e.getMetaData(), e.getCalculatedValues(), e.getAggregates());
+            me = new MongoEntity(e.getType(), e.getEntityId(), e.getBody(), e.getMetaData(), e.getCalculatedValues(),
+                    e.getAggregates());
         }
         if (encrypt != null) {
             me.encrypt(encrypt);
         }
-        return me.toDBObject(uuidGeneratorStrategy);
+        return me.toDBObject(uuidGeneratorStrategy, naturalKeyExtractor);
     }
-
+    
 }
