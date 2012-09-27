8b770fd9aa998e8b23d7e727999af74286c8e796
Brandon Suzuki <bsuzuki@wgen.net>
2012-09-27 15:17:40 -0400

    Remove the zip file size change check since we are retrying IOException.

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/ZipFileProcessor.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/ZipFileProcessor.java
@@ -111,8 +111,6 @@ public class ZipFileProcessor implements Processor, MessageSourceAware {
 
             FaultsReport errorReport = new FaultsReport();
 
-            waitForZipfile(zipFile);  // DE1618 gluster does not atomic move so we need to check for file size changes
-
             File ctlFile = zipFileHandler.handle(zipFile, errorReport);
 
             BatchJobUtils.writeErrorsWithDAO(batchJobId, zipFile.getName(), BATCH_JOB_STAGE, errorReport, batchJobDAO);
