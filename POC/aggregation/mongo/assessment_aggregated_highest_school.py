f52c77b5184d1027db314280be9b81af36023b52
Stephan Altmueller <saltmueller@wgen.net>
2012-09-28 11:40:54 -0400

    parallelized mongodb based aggregates

--- POC/aggregation/mongo/assessment_aggregated_highest_school.py
+++ POC/aggregation/mongo/assessment_aggregated_highest_school.py
@@ -1,4 +1,7 @@
 from aggregatedriver import run_pipeline, write_result, cond_from_bands 
+from instrumentation import instrument
+from multiprocessing import Process, Queue 
+from Queue import Empty 
 
 import pymongo
 import sys 
@@ -12,50 +15,75 @@ bands = [
 ]
 by_rank = dict([(x["rank"], x) for x in bands])
 
+inst = instrument() 
+
 def do_work(assessment_id, db):
     edorg_collection = db["educationOrganization"]
     src_var = "calculatedValues.assessments.%s.HighestEver.ScaleScore" % assessment_id
     target_var = "aggregates.assessments.%s.cut_points" % assessment_id
 
+    def _worker(q):
+        try:
+          while True:
+              print "Queue size: ", q.qsize() 
+              school_id = q.get(True, 10) 
+
+              # get all students in the school 
+              ssa_collection = db["studentSchoolAssociation"]
+              students = [s["body"]["studentId"] 
+                          for s  in ssa_collection.find({ "body.schoolId" : school_id }, {"_id" : 0, "body.studentId" : 1})
+                          if s.get("body", {}).get("studentId",None)]
+
+              print "School:   %s" % school_id
+              print "Students: %s" % len(students)
+
+              if students:
+                  # assemble a pipeline 
+                  cond_stmt = cond_from_bands(src_var, bands)
+                  pipeline = [
+                    { "$match" : { 
+                          "_id" : { "$in" : students }
+                      }
+                    },
+                    { 
+                      "$project" : {
+                          "band" : cond_stmt
+                      }
+                    },
+                    {
+                      "$group" : {
+                        "_id" : "$band",
+                        "count" : {
+                          "$sum" : 1
+                        },
+                      }
+                    }
+                  ]
+
+                  result = dict([(by_rank[x["_id"]]["abbreviation"], x["count"]) for x in run_pipeline(pipeline, "student", db)])
+                  edorg_collection.update({"_id" : school_id}, {"$set" : {target_var : result }})
+                  print "Written: ", result 
+
+        except Empty, e:
+            print "Process finished."      
+
+
     # run a query over schools 
-    for school in edorg_collection.find({ "type" : "school" }): 
-        school_id = school["_id"]
+    N_WORKERS = 10 
+    q = Queue() 
+    for school in edorg_collection.find({ "type" : "school" }):
+        q.put(school["_id"])
 
-        # get all students in the school 
-        ssa_collection = db["studentSchoolAssociation"]
-        students = [s["body"]["studentId"] 
-                    for s  in ssa_collection.find({ "body.schoolId" : school_id }, {"_id" : 0, "body.studentId" : 1})
-                    if s.get("body", {}).get("studentId",None)]
+    all_processes = [] 
+    for i in xrange(N_WORKERS):
+      t = Process(target=_worker, args=(q,), name="Thread_%s" % i)
+      t.daemon = True
+      t.start() 
+      all_processes.append(t)
 
-        print "School:   %s" % school_id
-        print "Students: %s" % len(students)
-
-        if students:
-            # assemble a pipeline 
-            cond_stmt = cond_from_bands(src_var, bands)
-            pipeline = [
-              { "$match" : { 
-                    "_id" : { "$in" : students }
-                }
-              },
-              { 
-                "$project" : {
-                    "band" : cond_stmt
-                }
-              },
-              {
-                "$group" : {
-                  "_id" : "$band",
-                  "count" : {
-                    "$sum" : 1
-                  },
-                }
-              }
-            ]
-
-            result = dict([(by_rank[x["_id"]]["abbreviation"], x["count"]) for x in run_pipeline(pipeline, "student", db)])
-            edorg_collection.update({"_id" : school_id}, {"$set" : {target_var : result }})
-            print "Written: ", result 
+    # wait until everything is processed
+    with inst.lap(id="one_school_aggregation"):
+      [p.join() for p in all_processes]
 
 def main():
     hostname = "localhost" if len(sys.argv) < 2 else sys.argv[1]
@@ -63,15 +91,17 @@ def main():
     con = pymongo.Connection(hostname)
     db = con.sli
 
+    inst.start() 
     if len(sys.argv) < 3:
-        result = set()
-        result = set((x["body"]["assessmentId"] for x in db["studentAssessmentAssociation"].find({}, ["body.assessmentId"]) if x.get("body", {}).get("assessmentId", None)))
+        result = db["studentAssessmentAssociation"].distinct("body.assessmentId")
         print "Available Assessment IDs:"
         for e in result:
           print e
     else:
         assessment_id = sys.argv[2]
         do_work(assessment_id, db)
+    inst.stop()
+    inst.print_results() 
 
     # close the connection 
     con.close()
--- POC/aggregation/mongo/assessment_calculated_highest_student.py
+++ POC/aggregation/mongo/assessment_calculated_highest_student.py
@@ -1,94 +1,124 @@
 from aggregatedriver import run_pipeline, write_result
+from instrumentation import instrument 
+from multiprocessing import Process, Queue 
+from Queue import Empty 
 
 import pymongo
 import sys 
 
+inst = instrument()
+
 def do_work(assessment_id, db): 
+    @inst.lap
     def find_max_score(entry):
         all_scores = [float(x) for x in entry["scores"]]
         entry["highestScore"] = max(all_scores)
         del entry["scores"]
         return entry
 
-    temp_collection = "temp_calculated_highest_score"
+    def _worker(q):
+        try:
+          while True:
+              print "Queue size: ", q.qsize() 
+              student_batch = q.get(True, 10) 
+              print "GOT batch of size %s" % len(student_batch)
+              PIPELINE = [
+                {
+                  "$match" : { 
+                    "body.assessmentId" : assessment_id, 
+                    "body.scoreResults.assessmentReportingMethod" : "Scale score",
+                    "body.studentId" : { "$in" : student_batch }
+                  }
+                },
+                { 
+                  "$project" : {
+                    "tenantId"  : "$metaData.tenantId",
+                    "studentId" : "$body.studentId",
+                    "result" : "$body.scoreResults.result"
+                  }
+                },
+                {
+                  "$unwind" : "$result",
+                },
+                {
+                  "$group" : {
+                    "_id" : "$studentId",
+                    "scores" : { "$push" : "$result" }
+                  }
+                }
+              ]
+
+              results = [find_max_score(x) 
+                         for x in db.command("aggregate", input_collection, pipeline=PIPELINE)["result"]]
+              col = db[target_collection]
+              print "Values read !"
+              for entry in results:
+                col.update({"_id" : entry["_id"]}, {"$set" : { target_var : entry[src_field]}})
+              print "%s students processed." % (batch * BATCH_SIZE)
+        except Empty, e:
+            print "Process finished."
+
     input_collection = "studentAssessmentAssociation"
+    target_collection = "student"
     target_query = {"_id" : "%(_id)s"}
     target_var = "calculatedValues.assessments.%s.HighestEver.ScaleScore" % assessment_id
     src_field = "highestScore"
 
-    cursor = db[input_collection].find({"body.assessmentId" : assessment_id}, 
-                                       {"_id" : 0, "body.studentId" : 1})
-    student_ids = list(set((x["body"]["studentId"] for x in cursor if x.get("body", {}).get("studentId", None))))
-    print "Found %s students." % len(student_ids)
+    with inst.lap(id="read_students"):
+      cursor = db[input_collection].find({"body.assessmentId" : assessment_id}, 
+                                         {"_id" : 0, "body.studentId" : 1})
+      print "Queried students. Found %s documents" % cursor.count()
+      student_ids = list(set((x["body"]["studentId"] for x in cursor if x.get("body", {}).get("studentId", None))))
+      print "Found %s students." % len(student_ids)
 
-    BATCH_SIZE = 5000
+    BATCH_SIZE = 50000
+    N_WORKERS = 10
+
+    # fill a queue and start the workers 
+    q = Queue() 
     batch = 0
     while (batch * BATCH_SIZE) < len(student_ids):
         student_batch = student_ids[batch * BATCH_SIZE:batch * BATCH_SIZE + BATCH_SIZE]
-        PIPELINE = [
-          {
-            "$match" : { 
-              "body.assessmentId" : assessment_id, 
-              "body.scoreResults.assessmentReportingMethod" : "Scale score",
-              "body.studentId" : { "$in" : student_batch }
-            }
-          },
-          { 
-            "$project" : {
-              "tenantId"  : "$metaData.tenantId",
-              "studentId" : "$body.studentId",
-              "result" : "$body.scoreResults.result"
-            }
-          },
-          {
-            "$unwind" : "$result",
-          },
-          {
-            "$group" : {
-              "_id" : "$studentId",
-              "scores" : { "$push" : "$result" }
-            }
-          }
-        ]
-
-        # # run the pipeline, end if there are no more records 
-        # temp_result = [find_max_score(x) for x in db.command("aggregate", input_collection, pipeline=PIPELINE)["result"]]
-        # if not temp_result: break
-        # db[temp_collection].insert(temp_result)
-
-        # print "Wrote %s records to %s" % (len(temp_result), temp_collection)
-        # batch += 1
-
-        target_collection = "student"
-        results = [find_max_score(x) 
-                   for x in db.command("aggregate", input_collection, pipeline=PIPELINE)["result"]]
-        col = db[target_collection]
-        for entry in results:
-          col.update({"_id" : entry["_id"]}, {"$set" : { target_var : entry[src_field]}})
+        print "Batch read !"
+        q.put(student_batch)
         batch += 1
-        print "%s students processed." % (batch * BATCH_SIZE)
 
+    all_processes = [] 
+    for i in xrange(N_WORKERS):
+      t = Process(target=_worker, args=(q,), name="Thread_%s" % i)
+      t.daemon = True
+      t.start() 
+      all_processes.append(t)
+
+    # wait until everything is processed
+    with inst.lap(id="process_student_batch"):
+      [p.join() for p in all_processes]
+
+    col = db[target_collection]
     total_count = col.find({}).count()
     written_docs = col.find({ target_var  : { "$exists" : True } }, 
                             { "_id" : True, target_var : True })
     found_count = written_docs.count()
     print "%s of %s records in %s contain values" % (found_count, total_count, target_collection)
 
-
 def main():
     hostname = "localhost" if len(sys.argv) < 2 else sys.argv[1]
     con = pymongo.Connection(hostname, 27017)
     db = con.sli
 
+    inst.start()
     if len(sys.argv) < 3:
-        result = set()
-        result = set((x["body"]["assessmentId"] for x in db["studentAssessmentAssociation"].find({}, ["body.assessmentId"]) if x.get("body", {}).get("assessmentId", None)))
+
+        #result = set((x["body"]["assessmentId"] for x in db["studentAssessmentAssociation"].find({}, ["body.assessmentId"]) if x.get("body", {}).get("assessmentId", None)))
+        result = db["studentAssessmentAssociation"].distinct("body.assessmentId")
         print "Available Assessment IDs:"
         for e in result:
           print e
     else:
         assessment_id = sys.argv[2]
         do_work(assessment_id, db)
+    inst.stop()
+    inst.print_results() 
 
     # close the connection 
     con.close()
