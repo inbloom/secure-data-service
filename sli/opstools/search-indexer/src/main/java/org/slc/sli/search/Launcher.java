fdeb51398911c6d99a9810a8bda719edce6ea5b7
Anna Grebneva <agrebneva@wgen.net>
2012-10-04 11:38:59 -0400

    US4006: app lock

--- sli/opstools/search-indexer/src/main/java/org/slc/sli/search/Launcher.java
+++ sli/opstools/search-indexer/src/main/java/org/slc/sli/search/Launcher.java
@@ -2,10 +2,12 @@ package org.slc.sli.search;
 
 import java.io.IOException;
 
+import org.slc.sli.search.util.AppLock;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Launcher {
     public static final void main(String[] args) throws IOException {
+        new AppLock("data/indexer.lock");
         new ClassPathXmlApplicationContext("application-context.xml");
     }
 }
