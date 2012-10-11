b9f9c5e8f8c35a357762563b0d2a0b65493e0f40
tshewchuk <tshewchuk@wgen.net>
2012-10-11 14:45:39 -0400

    US4245 Replacing Camel polling with Active MQ - Removed LandingZoneRouteBuilder; added LandingZoneProcessorTest.

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/processors/LandingZoneProcessorTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/processors/LandingZoneProcessorTest.java
@@ -17,20 +17,16 @@
 package org.slc.sli.ingestion.processors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import com.google.common.io.Files;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,19 +44,19 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.slc.sli.ingestion.tenant.TenantDA;
 
 /**
- * Tests for TenantProcessor
+ * Tests for LandingZoneProcessor
  *
- * @author jtully
+ * @author tshewchuk
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class })
-public class TenantProcessorTest {
+public class LandingZoneProcessorTest {
 
     @InjectMocks
     @Autowired
-    private TenantProcessor tenantProcessor;
+    private LandingZoneProcessor landingZoneProcessor;
 
     @Mock
     private TenantDA mockedTenantDA;
@@ -72,46 +68,60 @@ public class TenantProcessorTest {
     }
 
     /**
-     * Test to check that a single lz that is added to the
-     * tenant collection is added by processor.
+     * Test to check that an lz submitted to LandingZoneProcessor is valid.
      *
      * @throws Exception
      */
     @Test
-    public void shouldAddNewLz() throws Exception {
+    public void testValidLz() {
 
+        File validLzPathFile = new File("/test/lz/inbound/TEST-LZ/testFile.zip");
+        String validLzPathname = validLzPathFile.getParent();
         List<String> testLzPaths = new ArrayList<String>();
-        testLzPaths.add("."); // this must be a path that exists on all platforms
+        testLzPaths.add(validLzPathname);
         when(mockedTenantDA.getLzPaths(Mockito.any(String.class))).thenReturn(testLzPaths);
 
-        // get a test tenantRecord
+        // Submit a valid landing zone.
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
-        tenantProcessor.process(exchange);
+        exchange.getIn().setBody(validLzPathFile.getPath(), String.class);
+        try {
+            landingZoneProcessor.process(exchange);
+        } catch (Exception e) {
+            fail();
+        }
 
-        // check there is no error on the received message
-        assertEquals("Header on exchange should indicate success", TenantProcessor.TENANT_POLL_SUCCESS, exchange
-                .getIn().getHeader(TenantProcessor.TENANT_POLL_HEADER));
+        // Check there is no error in the exchange.
+        assertEquals("Header on exchange should indicate success", false,
+                exchange.getIn().getHeader("hasErrors"));
     }
 
+    /**
+     * Test to check that an lz submitted to LandingZoneProcessor is invalid.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testPreLoad() {
-        File landingZone = Files.createTempDir();
-        try {
-            assertTrue(tenantProcessor.preLoad(landingZone.getAbsolutePath(), Arrays.asList("small")));
-            assertTrue(landingZone.list(new WildcardFileFilter("preload-*.zip")).length == 1);
-        } finally {
-            landingZone.delete();
-        }
-    }
+    public void testInvalidLz() {
 
-    @Test
-    public void testPreLoadBadDataSet() {
-        File landingZone = Files.createTempDir();
+        File validLzPathFile = new File("/test/lz/inbound/TEST-LZ/testFile.zip");
+        String validLzPathname = validLzPathFile.getParent();
+        List<String> testLzPaths = new ArrayList<String>();
+        testLzPaths.add(validLzPathname);
+        when(mockedTenantDA.getLzPaths(Mockito.any(String.class))).thenReturn(testLzPaths);
+
+        // Submit an invalid landing zone.
+        File inValidLzPathFile = new File("/test/lz/inbound/BAD-TEST-LZ/testFile.zip");
+        Exchange exchange = new DefaultExchange(new DefaultCamelContext());
+        exchange.getIn().setBody(inValidLzPathFile.getPath(), String.class);
         try {
-            assertTrue(!tenantProcessor.preLoad(landingZone.getAbsolutePath(), Arrays.asList("smallish")));
-        } finally {
-            landingZone.delete();
+            landingZoneProcessor.process(exchange);
+        } catch (Exception e) {
+            fail();
         }
+
+        // Check there is an error in the exchange.
+        assertEquals("Header on exchange should indicate failure", true,
+                exchange.getIn().getHeader("hasErrors"));
     }
 
 }
