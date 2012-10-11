ee92b276a26ccda6169cd1cc3cc68ab373ff6534
Yang Cao <ycao@wgen.net>
2012-10-11 10:52:10 -0400

    collapse studentSectionAssociation

--- sli/common/sli-common/src/main/java/org/slc/sli/common/domain/EmbedDocumentRelations.java
+++ sli/common/sli-common/src/main/java/org/slc/sli/common/domain/EmbedDocumentRelations.java
@@ -33,8 +33,8 @@ public class EmbedDocumentRelations {
 
     static {
         Map<String, Parent> map = new HashMap<String, Parent>();
-        //map.put("studentSectionAssociation", new Parent("section", "sectionId"));
-        map.put("studentAssessmentAssociation", new Parent("student", "studentId"));
+        map.put("studentSectionAssociation", new Parent("section", "sectionId"));
+        //map.put("studentAssessmentAssociation", new Parent("student", "studentId"));
         subDocParent = Collections.unmodifiableMap(map);
     };
 
