423e826b423420d71325853a34688be1b27226a2
ecole <ecole@wgen.net>
2012-10-11 16:35:00 -0400

    [US4239] Chaned exception handling for natural key validation

--- sli/domain/src/main/java/org/slc/sli/validation/NaturalKeyValidationException.java
+++ sli/domain/src/main/java/org/slc/sli/validation/NaturalKeyValidationException.java
@@ -1,5 +1,6 @@
 package org.slc.sli.validation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,11 +10,34 @@ import java.util.List;
  */
 public class NaturalKeyValidationException extends RuntimeException {
 
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     final String entityType;
     final List<String> naturalKeys;
 
+    public NaturalKeyValidationException(Exception ex) {
+        super(ex);
+        this.naturalKeys = null;
+        this.entityType = null;
+    }
+
+    public NaturalKeyValidationException(String msg) {
+        super(msg);
+        this.naturalKeys = null;
+        this.entityType = null;
+    }
+
     public NaturalKeyValidationException(String entityType, List<String> naturalKeys) {
         super(entityType + " is missing the following required key fields: " + naturalKeys);
+        this.entityType = entityType;
+        this.naturalKeys = naturalKeys;
+    }
+
+    public NaturalKeyValidationException(NoNaturalKeysDefinedException e,
+            String entityType, ArrayList<String> naturalKeys) {
+        super(e);
         this.entityType = entityType;
         this.naturalKeys = naturalKeys;
     }
