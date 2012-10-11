353b530d0eebff8e590a30f4143972b7d194e4da
Steve Ashton <sashton@wgen.net>
2012-10-11 15:34:15 -0400

    [US3951] Fixed target fieldPath for studentSectionAssociation.section

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSectionAssociationTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSectionAssociationTest.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package org.slc.sli.ingestion.smooks.mappings;
 
 import java.util.Map;
@@ -24,43 +23,41 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.slc.sli.ingestion.NeutralRecord;
+import org.slc.sli.ingestion.util.EntityTestUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import org.slc.sli.ingestion.NeutralRecord;
-import org.slc.sli.ingestion.util.EntityTestUtils;
-
 /**
- *
+ * 
  * @author mpatel
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class StudentSectionAssociationTest {
-
+    
     String xmlTestData = "<InterchangeStudentEnrollment xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-StudentEnrollment.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
             + "<StudentSectionAssociation>"
-
+            
             + "<SectionReference>"
             + "<SectionIdentity>"
             + "<UniqueSectionCode>MT100</UniqueSectionCode>"
             + "</SectionIdentity>"
             + "</SectionReference>"
-
+            
             + "<StudentReference>"
             + "<StudentIdentity>"
             + "<StudentUniqueStateId>111220001</StudentUniqueStateId>"
             + "</StudentIdentity>"
             + "</StudentReference>"
-
+            
             + " <BeginDate>2009-09-15</BeginDate>"
             + " <EndDate>2010-06-02</EndDate>"
-            + " <HomeroomIndicator>false</HomeroomIndicator>"
-            + " <RepeatIdentifier>Not repeated</RepeatIdentifier>"
-
+            + " <HomeroomIndicator>false</HomeroomIndicator>" + " <RepeatIdentifier>Not repeated</RepeatIdentifier>"
+            
             + "</StudentSectionAssociation></InterchangeStudentEnrollment>";
-
+    
     String invalidXMLTestData = "<InterchangeStudentEnrollment xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-StudentEnrollment.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
             + "<StudentSectionAssociation>"
             + "<SectionReference>"
@@ -71,47 +68,50 @@ public class StudentSectionAssociationTest {
             + " <BeginDate>2009-09-15</BeginDate>"
             + " <HomeroomIndicator>false</HomeroomIndicator>"
             + "</StudentSectionAssociation></InterchangeStudentEnrollment>";
-
+    
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
-
+    
     @Test
     public void testInValidSectionXML() throws Exception {
         String smooksConfig = "smooks_conf/smooks-all-xml.xml";
         String targetSelector = "InterchangeStudentEnrollment/StudentSectionAssociation";
-
+        
         NeutralRecord record = EntityTestUtils.smooksGetSingleNeutralRecord(smooksConfig, targetSelector,
                 invalidXMLTestData);
         checkInValidSectionNeutralRecord(record);
     }
-
+    
     @Test
     public void testValidSectionXML() throws Exception {
         String smooksConfig = "smooks_conf/smooks-all-xml.xml";
         String targetSelector = "InterchangeStudentEnrollment/StudentSectionAssociation";
-
+        
         NeutralRecord record = EntityTestUtils.smooksGetSingleNeutralRecord(smooksConfig, targetSelector, xmlTestData);
         checkValidSectionNeutralRecord(record);
     }
-
+    
     @SuppressWarnings("unchecked")
     private void checkValidSectionNeutralRecord(NeutralRecord record) {
         Map<String, Object> entity = record.getAttributes();
-        Assert.assertEquals("111220001", ((Map<String, Object>) ((Map<String, Object>) entity.get("StudentReference")).get("StudentIdentity")).get("StudentUniqueStateId"));
-        Assert.assertEquals("MT100", ((Map<String, Object>) ((Map<String, Object>) entity.get("sectionReference")).get("sectionIdentity")).get("uniqueSectionCode"));
+        Assert.assertEquals("111220001", ((Map<String, Object>) ((Map<String, Object>) entity.get("StudentReference"))
+                .get("StudentIdentity")).get("StudentUniqueStateId"));
+        Assert.assertEquals("MT100", ((Map<String, Object>) ((Map<String, Object>) entity.get("SectionReference"))
+                .get("sectionIdentity")).get("uniqueSectionCode"));
         Assert.assertEquals("2009-09-15", entity.get("beginDate"));
         Assert.assertEquals("2010-06-02", entity.get("endDate"));
         Assert.assertEquals("false", entity.get("homeroomIndicator").toString());
         Assert.assertEquals("Not repeated", entity.get("repeatIdentifier"));
     }
-
+    
     @SuppressWarnings("unchecked")
     private void checkInValidSectionNeutralRecord(NeutralRecord record) {
         Map<String, Object> entity = record.getAttributes();
         Assert.assertEquals(null, entity.get("StudentReference"));
-        Assert.assertEquals("MT100", ((Map<String, Object>) ((Map<String, Object>) entity.get("sectionReference")).get("sectionIdentity")).get("uniqueSectionCode"));
+        Assert.assertEquals("MT100", ((Map<String, Object>) ((Map<String, Object>) entity.get("SectionReference"))
+                .get("sectionIdentity")).get("uniqueSectionCode"));
         Assert.assertEquals("2009-09-15", entity.get("beginDate"));
         Assert.assertEquals(null, entity.get("endDate"));
         Assert.assertEquals("false", entity.get("homeroomIndicator").toString());
