f51ee2c6bdaaa6ffbb2c843491e81ec3b882b0fc
jtully <jtully@wgen.net>
2012-10-16 18:18:38 -0400

    [US4349] unit test fix for new GraduationPlan ref

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSchoolAssociationEntityTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSchoolAssociationEntityTest.java
@@ -21,23 +21,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import org.slc.sli.domain.Entity;
-import org.slc.sli.domain.Repository;
 import org.slc.sli.ingestion.NeutralRecord;
 import org.slc.sli.ingestion.util.EntityTestUtils;
-import org.slc.sli.validation.EntityValidator;
 
 /**
  *
@@ -47,13 +37,6 @@ import org.slc.sli.validation.EntityValidator;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class StudentSchoolAssociationEntityTest {
-
-    @InjectMocks
-    @Autowired
-    private EntityValidator validator;
-
-    @Mock
-    private Repository<Entity> mockRepository;
 
     String xmlTestData = "<InterchangeStudentEnrollment xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"Interchange-StudentEnrollment.xsd\" xmlns=\"http://ed-fi.org/0100RFC062811\">"
             + "<StudentSchoolAssociation>"
@@ -77,11 +60,6 @@ public class StudentSchoolAssociationEntityTest {
             + " <ExitWithdrawType>End of school year</ExitWithdrawType>"
             + "</StudentSchoolAssociation>" + "</InterchangeStudentEnrollment>";
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @SuppressWarnings("unchecked")
     private void checkValidSSANeutralRecord(NeutralRecord record) {
         Map<String, Object> entity = record.getAttributes();
@@ -98,7 +76,6 @@ public class StudentSchoolAssociationEntityTest {
         Assert.assertNotNull(schoolIdentity);
         Assert.assertEquals("990000001", schoolIdentity.get("StateOrganizationId"));
 
-        Assert.assertEquals("990000001", entity.get("schoolId"));
         Assert.assertEquals("Eighth grade", entity.get("EntryGradeLevel"));
         Assert.assertEquals("2012-01-17", entity.get("EntryDate"));
         Assert.assertEquals("Next year school", entity.get("EntryType"));
@@ -111,33 +88,6 @@ public class StudentSchoolAssociationEntityTest {
 
         Assert.assertEquals("Full Time Employment", educationalPlans.get(0));
 
-    }
-
-    @Ignore
-    @Test
-    public void testValidStudentSchoolAssociationCSV() throws Exception {
-
-        String smooksConfig = "smooks_conf/smooks-studentSchoolAssociation-csv.xml";
-        String targetSelector = "csv-record";
-
-        String testData = ",,,900000001,,,,,,,,,,,,,,,,,,,,,,,990000001,,,2012-01-17,Eighth grade,Next year school,false,true,2011-09-12,End of school year,true,Full Time Employment,Full";
-
-        NeutralRecord record = EntityTestUtils.smooksGetSingleNeutralRecord(smooksConfig, targetSelector, testData);
-        checkValidSSANeutralRecord(record);
-    }
-
-    @Test
-    public void testValidatorStudentSchoolAssociation() throws Exception {
-        String smooksConfig = "smooks_conf/smooks-all-xml.xml";
-        String targetSelector = "InterchangeStudentEnrollment/StudentSchoolAssociation";
-
-        NeutralRecord record = EntityTestUtils.smooksGetSingleNeutralRecord(smooksConfig, targetSelector, xmlTestData);
-
-        // mock repository will simulate "finding" the references
-        Mockito.when(mockRepository.exists("educationOrganization", "990000001")).thenReturn(true);
-        Mockito.when(mockRepository.exists("student", "900000001")).thenReturn(true);
-
-        EntityTestUtils.mapValidation(record.getAttributes(), "studentSchoolAssociation", validator);
     }
 
     @Test
