5707f4d63611ec03b566bfc51914bfb205f1d7e4
Danil Kornishev <dkornishev@wgen.net>
2012-10-16 09:40:55 -0400

    switch added for seprate xml vs json logic

--- sli/api/src/main/java/org/slc/sli/api/representation/ContextInferrenceFailedExceptionHandler.java
+++ sli/api/src/main/java/org/slc/sli/api/representation/ContextInferrenceFailedExceptionHandler.java
@@ -1,5 +1,10 @@
 package org.slc.sli.api.representation;
 
+import java.util.Collections;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -10,21 +15,29 @@ import org.springframework.stereotype.Component;
 
 /**
  * Handles empty contexts
+ * 
  * @author dkornishev
- *
+ * 
  */
 @Provider
 @Component
-public class ContextInferrenceFailedExceptionHandler implements ExceptionMapper<ContextInferrenceFailedException>{
+public class ContextInferrenceFailedExceptionHandler implements ExceptionMapper<ContextInferrenceFailedException> {
+
+	@Context
+	private HttpHeaders headers;
 
 	@Override
 	public Response toResponse(ContextInferrenceFailedException exception) {
 		warn("Failed Context Inferrence");
-		return Response.ok(new EmptyResponse()).header("TotalCount", 0).build();
+		Object entity = Collections.EMPTY_LIST;
+		if (headers.getAcceptableMediaTypes().contains(MediaType.APPLICATION_XML_TYPE)) {
+			entity=new EmptyResponse();
+		}
+
+		return Response.ok(entity).header("TotalCount", 0).build();
 	}
 
 	@XmlRootElement(name = "emptyList")
 	public static class EmptyResponse {
-		
 	}
 }
