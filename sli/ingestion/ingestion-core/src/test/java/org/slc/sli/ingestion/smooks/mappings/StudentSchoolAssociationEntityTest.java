776a077e44fb2b3f2f5505d4c2acfb0578f85856
jtully <jtully@wgen.net>
2012-10-16 08:22:46 -0400

    [US4349] Changes to get graduationPlan going through did ref resolver

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSchoolAssociationEntityTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/smooks/mappings/StudentSchoolAssociationEntityTest.java
@@ -29,14 +29,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.Repository;
 import org.slc.sli.ingestion.NeutralRecord;
 import org.slc.sli.ingestion.util.EntityTestUtils;
 import org.slc.sli.validation.EntityValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
@@ -74,9 +75,6 @@ public class StudentSchoolAssociationEntityTest {
             + " <SchoolChoiceTransfer>true</SchoolChoiceTransfer>"
             + " <ExitWithdrawDate>2011-09-12</ExitWithdrawDate>"
             + " <ExitWithdrawType>End of school year</ExitWithdrawType>"
-            + " <EducationalPlans>"
-            + "   <EducationalPlan>Full Time Employment</EducationalPlan>"
-            + " </EducationalPlans>"
             + "</StudentSchoolAssociation>" + "</InterchangeStudentEnrollment>";
 
     @Before
@@ -84,17 +82,30 @@ public class StudentSchoolAssociationEntityTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    @SuppressWarnings("unchecked")
     private void checkValidSSANeutralRecord(NeutralRecord record) {
         Map<String, Object> entity = record.getAttributes();
-        Assert.assertEquals("900000001", entity.get("studentId"));
+
+        Map<String, Object> studentRef = (Map<String, Object>) entity.get("StudentReference");
+        Assert.assertNotNull(studentRef);
+        Map<String, Object> studentIdentity = (Map<String, Object>) studentRef.get("StudentIdentity");
+        Assert.assertNotNull(studentIdentity);
+        Assert.assertEquals("900000001", studentIdentity.get("StudentUniqueStateId"));
+
+        Map<String, Object> schoolRef = (Map<String, Object>) entity.get("SchoolReference");
+        Assert.assertNotNull(schoolRef);
+        Map<String, Object> schoolIdentity = (Map<String, Object>) schoolRef.get("EducationalOrgIdentity");
+        Assert.assertNotNull(schoolIdentity);
+        Assert.assertEquals("990000001", schoolIdentity.get("StateOrganizationId"));
+
         Assert.assertEquals("990000001", entity.get("schoolId"));
-        Assert.assertEquals("Eighth grade", entity.get("entryGradeLevel"));
-        Assert.assertEquals("2012-01-17", entity.get("entryDate"));
-        Assert.assertEquals("Next year school", entity.get("entryType"));
-        Assert.assertEquals("false", entity.get("repeatGradeIndicator").toString());
-        Assert.assertEquals("2011-09-12", entity.get("exitWithdrawDate"));
-        Assert.assertEquals("End of school year", entity.get("exitWithdrawType"));
-        Assert.assertEquals("true", entity.get("schoolChoiceTransfer").toString());
+        Assert.assertEquals("Eighth grade", entity.get("EntryGradeLevel"));
+        Assert.assertEquals("2012-01-17", entity.get("EntryDate"));
+        Assert.assertEquals("Next year school", entity.get("EntryType"));
+        Assert.assertEquals("false", entity.get("RepeatGradeIndicator").toString());
+        Assert.assertEquals("2011-09-12", entity.get("ExitWithdrawDate"));
+        Assert.assertEquals("End of school year", entity.get("ExitWithdrawType"));
+        Assert.assertEquals("true", entity.get("SchoolChoiceTransfer").toString());
         List<?> educationalPlans = (List<?>) record.getAttributes().get("educationalPlans");
         Assert.assertTrue(educationalPlans != null);
 
