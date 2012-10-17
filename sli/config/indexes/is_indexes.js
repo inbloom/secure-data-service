abe46aa7237fdc4a96ab83391d776c5bb04c3630
William Scott <wscott@wgen.net>
2012-10-17 11:28:09 -0400

    Fix indexing mistake -- attendance/assessment mixup.

--- sli/config/indexes/is_indexes.js
+++ sli/config/indexes/is_indexes.js
@@ -18,7 +18,7 @@
 // put 'creationTime' index on all entities (including staged-only entities)
 //
 db["assessment"].ensureIndex({"batchJobId" : 1, "creationTime":1});
-db["attendance_transformed"].ensureIndex({"batchJobId" : 1, "creationTime":1});
+db["assessment_transformed"].ensureIndex({"batchJobId" : 1, "creationTime":1});
 db["assessmentFamily"].ensureIndex({"batchJobId" : 1, "creationTime":1});
 db["assessmentItem"].ensureIndex({"batchJobId" : 1, "creationTime":1});
 db["assessmentPeriodDescriptor"].ensureIndex({"batchJobId" : 1, "creationTime":1});
@@ -80,9 +80,9 @@ db["assessment"].ensureIndex({"batchJobId":1,"_id":1});
 db["assessmentFamily"].ensureIndex({"batchJobId":1,"_id":1});
 db["assessmentItem"].ensureIndex({"batchJobId":1,"_id":1});
 db["assessmentPeriodDescriptor"].ensureIndex({"batchJobId":1,"_id":1});
-db["attendance_transformed"].ensureIndex({"batchJobId" : 1, "body.studentId" : 1, "body.schoolId" : 1}, {unique : true});
+db["assessment_transformed"].ensureIndex({"batchJobId":1,"_id":1});
 db["attendance"].ensureIndex({"batchJobId":1,"_id":1});
-db["attendance_transformed"].ensureIndex({"batchJobId":1,"_id":1});
+db["attendance_transformed"].ensureIndex({"batchJobId" : 1, "body.studentId" : 1, "body.schoolId" : 1}, {unique : true});
 db["calendarDate"].ensureIndex({"batchJobId":1,"_id":1});
 db["cohort"].ensureIndex({"batchJobId":1,"_id":1});
 db["competencyLevelDescriptor"].ensureIndex({"batchJobId":1,"_id":1});
