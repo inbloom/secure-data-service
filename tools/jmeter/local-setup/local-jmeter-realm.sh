c298e0a29b391f01c347139bcf7e398c5407b1c7
Aaron Saarela <asaarela@wgen.net>
2012-10-18 13:55:09 -0400

    Switch to SHA1 hash for tenant name.

--- tools/jmeter/local-setup/local-jmeter-realm.sh
+++ tools/jmeter/local-setup/local-jmeter-realm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mongoimport -d Midgar -c application --drop application.json
-mongoimport -d Midgar -c applicationAuthorization --drop applicationAuthorization.json
-mongoimport -d Midgar -c customRole --drop customRole.json
+mongoimport -d 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a -c application --drop application.json
+mongoimport -d 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a -c applicationAuthorization --drop applicationAuthorization.json
+mongoimport -d 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a -c customRole --drop customRole.json
 mongoimport -d sli -c realm --drop realm.json
