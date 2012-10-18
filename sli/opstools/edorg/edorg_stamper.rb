7f40da1379dcab6fa16d1527793adc06dbe914a9
ablum <ablum@wgen.net>
2012-10-18 09:52:18 -0400

    Modified ed org stamper to stamp the tenant databases

--- sli/opstools/edorg/edorg_stamper.rb
+++ sli/opstools/edorg/edorg_stamper.rb
@@ -23,9 +23,26 @@ is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
 
 trap('HUP') {} unless is_windows
 
-def run_fixer(tenant = nil)
+def run_fixer(tenant = nil, systemDatabaseName)
+
+  # Tenant Databases are identified by field body.tenantId in records located in sli.tenant collection
+  systemDatabaseConnection = Mongo::Connection.new(@hp[0], @hp[1].to_i)
+  @systemDatabase = systemDatabaseConnection[systemDatabaseName]
+  @tenantCollection = @systemDatabase.collection('tenant')
+  
+  tenantDatabaseName = ""
+  @log.info "--- running tenant stamper for " + tenant + " ---"
+  @tenantCollection.find({'body.tenantId' => tenant})  do |row|
+    row.each do |systemTenant|
+        # database name is stored as systemTenant['body']['tenantId'].  It will be stored in a separate attribute later on.
+
+        @log.info "--- Tenant with ID <" + tenant + "> located in database <" + systemTenant['body']['dbName'] + ">"
+        tenantDatabaseName = systemTenant['body']['dbName']
+    end
+  end
+
   connection = Mongo::Connection.new(@hp[0], @hp[1].to_i, :pool_size => 10, :pool_timeout => 25, :safe => {:wtimeout => 500})
-  db = connection[@database]
+  db = connection[tenantDatabaseName]
   @log.info "Creating a new stamper for #{tenant}"
   fixer = SLCFixer.new(db, @terminates, tenant)
   begin
@@ -51,25 +68,27 @@ def launch_fixer
       if !@tenants.include? tenant['body']['tenantId']
         @log.info "Starting stamper for #{tenant['body']['tenantId']}"
         @tenants.add tenant['body']['tenantId'] 
-        @threads << Thread.new {run_fixer(tenant['body']['tenantId'])}
+        @threads << Thread.new {run_fixer(tenant['body']['tenantId'], @database)}
       end
     end
   end
 
   # If there is no tenant connection, we stamp stupidly
   @log.info "No tenant collection, repainting the bridge" if @tenants.empty?
-  @threads << Thread.new {run_fixer(nil)} if @tenants.empty?
+  @threads << Thread.new {run_fixer(nil, @database)} if @tenants.empty?
   @threads.each {|th| th.join } unless @terminates
   connection.close
 end
+
 def convertTenantIdToDbName(tenantId)
   return Digest::SHA1.hexdigest tenantId
 end
+
 if ARGV.count < 1
   puts "Usage: edorg_stamper <dbhost:port> <database> <terminates>"
-  puts "\t dbhost - hostname for mongo"
-  puts "\t port - port mongo is running on (27017 is common)"
-  puts "\t database - the name of the database (Defaults to sli)"
+  puts "\t dbhost - hostname for mongo system SLI database"
+  puts "\t port - port mongo system SLI database is running on (27017 is common)"
+  puts "\t database - the name of the system database (Defaults to sli)"
   puts "\t terminates - if there is any parameter for this, it will run forever."
   puts "*** Note: These parameters must exist in the order they are presented ***"
   exit
@@ -78,7 +97,7 @@ else
   @tenants = Set.new
   @terminates = (ARGV[2].nil? ? false : true)
   #change databasae from 'sli' to 'Security' as a result of DataPartitioning
-  @database = convertTenantIdToDbName((ARGV[1].nil? ? 'Security' : ARGV[1]))
+  @database = (ARGV[1].nil? ? 'sli' : ARGV[1])
   @hp = ARGV[0].split(":")
   @log = Logger.new(STDOUT)
   @log.level = Logger::INFO
