cb52b85af5e8651f6c26a3cac71651ca0c1c9f13
Jeff Stokes <jstokes@wgen.net>
2012-10-30 15:13:50 -0400

    Update to correct definition of an attendance entity

--- sli/acceptance-tests/test/features/apiV1/entities/crud/step_definitions/crud_step.rb
+++ sli/acceptance-tests/test/features/apiV1/entities/crud/step_definitions/crud_step.rb
@@ -78,12 +78,10 @@ Given /^a valid entity json document for a "([^"]*)"$/ do |arg1|
     "entityType" => "attendance",
     "studentId" => "0c2756fd-6a30-4010-af79-488d6ef2735a",
     "schoolId" => "6756e2b9-aba1-4336-80b8-4a5dde3c63fe",
-    "schoolYearAttendance" => [{
-      "schoolYear" => "2011-2012",
-      "attendanceEvent" => [{
-        "date" => "2011-09-16",
-        "event" => "Tardy"
-      }]
+    "schoolYear" => "2011-2012",
+    "attendanceEvent" => [{
+      "date" => "2011-09-16",
+      "event" => "Tardy"
     }]
   },
   "studentAcademicRecord" => {
