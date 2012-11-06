cc021229d0157c0550d44641dffa0ab51df222f0
Vic McGlaughlin <vmcglaughlin@wgen.net>
2012-11-06 12:56:08 -0500

    [US4399] extension for learningStandard reference in course, gradebookEntry, and objectiveAssessment with unit test

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/normalization/did/DidReferenceResolutionTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/normalization/did/DidReferenceResolutionTest.java
@@ -36,43 +36,56 @@ import org.slc.sli.ingestion.validation.ErrorReport;
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
 public class DidReferenceResolutionTest {
 
-	@Autowired
-	DeterministicIdResolver didResolver;
+    @Autowired
+    DeterministicIdResolver didResolver;
 
-	private static final String TENANT_ID = "tenant_id";
+    private static final String TENANT_ID = "tenant_id";
 
-	@Test
-	public void shouldResolveStaffDidCorrectly() throws JsonParseException, JsonMappingException, IOException {
-		Entity entity = loadEntity("didTestEntities/staffReference.json");
-		ErrorReport errorReport = new TestErrorReport();
+    @Test
+    public void shouldResolveStaffDidCorrectly() throws JsonParseException, JsonMappingException, IOException {
+        Entity entity = loadEntity("didTestEntities/staffReference.json");
+        ErrorReport errorReport = new TestErrorReport();
 
-		didResolver.resolveInternalIds(entity, TENANT_ID, errorReport);
+        didResolver.resolveInternalIds(entity, TENANT_ID, errorReport);
 
-		Map<String, String> naturalKeys = new HashMap<String, String>();
-		naturalKeys.put("staffUniqueStateId", "jjackson");
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+        naturalKeys.put("staffUniqueStateId", "jjackson");
 
-		checkId(entity, "StaffReference", naturalKeys, "staff");
-	}
+        checkId(entity, "StaffReference", naturalKeys, "staff");
+    }
 
-	// generate the expected deterministic ids to validate against
-	private String generateExpectedDid(Map<String, String> naturalKeys, String tenantId, String entityType, String parentId) throws JsonParseException, JsonMappingException, IOException {
-		NaturalKeyDescriptor nkd = new NaturalKeyDescriptor(naturalKeys, tenantId, entityType, parentId);
-		return new DeterministicUUIDGeneratorStrategy().generateId(nkd);
-	}
+    @Test
+    public void shouldResolveLearningStandardDidCorrectly() throws JsonParseException, JsonMappingException, IOException {
+        Entity entity = loadEntity("didTestEntities/learningStandardReference.json");
+        ErrorReport errorReport = new TestErrorReport();
 
-	// validate reference resolution
-	private void checkId(Entity entity, String referenceField, Map<String, String> naturalKeys, String collectionName) throws JsonParseException, JsonMappingException, IOException {
-		String expectedDid =  generateExpectedDid(naturalKeys, TENANT_ID, collectionName, null);
-		Map<String, Object> body = entity.getBody();
-		Assert.assertNotNull(body.get(referenceField));
-		Assert.assertEquals(expectedDid, body.get(referenceField));
-	}
+        didResolver.resolveInternalIds(entity, TENANT_ID, errorReport);
 
-	//load a sample NeutralRecordEntity from a json file
-	private Entity loadEntity(String fname) throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		Resource jsonFile = new ClassPathResource(fname);
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+        naturalKeys.put("learningStandardId.identificationCode", "0123456789");
+
+        checkId(entity, "LearningStandardReference", naturalKeys, "learningStandard");
+    }
+
+    // generate the expected deterministic ids to validate against
+    private String generateExpectedDid(Map<String, String> naturalKeys, String tenantId, String entityType, String parentId) throws JsonParseException, JsonMappingException, IOException {
+        NaturalKeyDescriptor nkd = new NaturalKeyDescriptor(naturalKeys, tenantId, entityType, parentId);
+        return new DeterministicUUIDGeneratorStrategy().generateId(nkd);
+    }
+
+    // validate reference resolution
+    private void checkId(Entity entity, String referenceField, Map<String, String> naturalKeys, String collectionName) throws JsonParseException, JsonMappingException, IOException {
+        String expectedDid =  generateExpectedDid(naturalKeys, TENANT_ID, collectionName, null);
+        Map<String, Object> body = entity.getBody();
+        Assert.assertNotNull(body.get(referenceField));
+        Assert.assertEquals(expectedDid, body.get(referenceField));
+    }
+
+    //load a sample NeutralRecordEntity from a json file
+    private Entity loadEntity(String fname) throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Resource jsonFile = new ClassPathResource(fname);
         NeutralRecord nr = mapper.readValue(jsonFile.getInputStream(), NeutralRecord.class);
         return new NeutralRecordEntity(nr);
-	}
+    }
 }
