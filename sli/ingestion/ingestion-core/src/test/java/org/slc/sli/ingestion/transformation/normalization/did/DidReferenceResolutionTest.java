6e14c8f4074a4f9a44acbdfac6bca727f0f31989
Brandon Suzuki <bsuzuki@wgen.net>
2012-11-07 11:00:34 -0500

    Unit tests for EdOrgRef in disciplineIncident and teacherSchoolAssociation

---
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/transformation/normalization/did/DidReferenceResolutionTest.java
@@ -0,0 +1,85 @@
+package org.slc.sli.ingestion.transformation.normalization.did;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.slc.sli.common.domain.NaturalKeyDescriptor;
+import org.slc.sli.common.util.uuid.DeterministicUUIDGeneratorStrategy;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.ingestion.NeutralRecord;
+import org.slc.sli.ingestion.NeutralRecordEntity;
+import org.slc.sli.ingestion.landingzone.validation.TestErrorReport;
+import org.slc.sli.ingestion.validation.ErrorReport;
+
+/**
+ *
+ * Integrated JUnit tests to check to check the schema parser and
+ * DeterministicIdResolver in combination
+ *
+ * @author jtully
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+public class DidReferenceResolutionTest {
+
+	@Autowired
+	DeterministicIdResolver didResolver;
+
+	private static final String TENANT_ID = "tenant_id";
+
+    @Test
+    public void resolvesEdOrgRefDidInDisciplineIncidentCorrectly() throws JsonParseException, JsonMappingException, IOException {
+        Entity entity = loadEntity("didTestEntities/disciplineIncident.json");
+        ErrorReport errorReport = new TestErrorReport();
+        didResolver.resolveInternalIds(entity, TENANT_ID, errorReport);
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+        naturalKeys.put("stateOrganizationId", "testSchoolId");
+        checkId(entity, "SchoolReference", naturalKeys, "educationOrganization");
+    }
+
+    @Test
+    public void resolvesEdOrgRefDidInTeacherSchoolAssociationCorrectly() throws JsonParseException, JsonMappingException, IOException {
+        Entity entity = loadEntity("didTestEntities/teacherSchoolAssociation.json");
+        ErrorReport errorReport = new TestErrorReport();
+        didResolver.resolveInternalIds(entity, TENANT_ID, errorReport);
+        Map<String, String> naturalKeys = new HashMap<String, String>();
+        naturalKeys.put("stateOrganizationId", "testSchoolId");
+        checkId(entity, "SchoolReference", naturalKeys, "educationOrganization");
+    }
+
+	// generate the expected deterministic ids to validate against
+	private String generateExpectedDid(Map<String, String> naturalKeys, String tenantId, String entityType, String parentId) throws JsonParseException, JsonMappingException, IOException {
+		NaturalKeyDescriptor nkd = new NaturalKeyDescriptor(naturalKeys, tenantId, entityType, parentId);
+		return new DeterministicUUIDGeneratorStrategy().generateId(nkd);
+	}
+
+	// validate reference resolution
+	private void checkId(Entity entity, String referenceField, Map<String, String> naturalKeys, String collectionName) throws JsonParseException, JsonMappingException, IOException {
+		String expectedDid =  generateExpectedDid(naturalKeys, TENANT_ID, collectionName, null);
+		Map<String, Object> body = entity.getBody();
+		Assert.assertNotNull(body.get(referenceField));
+		Assert.assertEquals(expectedDid, body.get(referenceField));
+	}
+
+	//load a sample NeutralRecordEntity from a json file
+	private Entity loadEntity(String fname) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Resource jsonFile = new ClassPathResource(fname);
+        NeutralRecord nr = mapper.readValue(jsonFile.getInputStream(), NeutralRecord.class);
+        return new NeutralRecordEntity(nr);
+	}
+}
