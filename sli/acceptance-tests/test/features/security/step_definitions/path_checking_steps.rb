bae78270cc19eacc5d03cee77e853972406112f7
Mark Abernathy <mabernathy@wgen.net>
2012-11-08 08:26:12 -0500

    Implement tests for path mutating, fix minor issues in mutator

--- sli/acceptance-tests/test/features/security/step_definitions/path_checking_steps.rb
+++ sli/acceptance-tests/test/features/security/step_definitions/path_checking_steps.rb
@@ -29,11 +29,12 @@ end
 When /^I call "(.*?)" using ID "(.*?)"$/ do |arg1, arg2|
   @id = arg2
   uri = arg1.gsub("@id", @id)
-  restHttpGet(uri,"application/json")
+  step "I navigate to GET \"/v1#{uri}\""
+  #restHttpGet("/v1#{uri}","application/json")
   assert(@res != nil, "Response from rest-client GET is nil")
 end
 
-Then /^the executed URI should be "(.*?)"$/ do |arg1|
+Then /^the executed URI should be "(.*?)"$/ do |expectedUri|
   version = "v1"
   root = expectedUri.match(/\/(.+?)\/|$/)[1]
   expected = version+expectedUri
@@ -41,6 +42,8 @@ Then /^the executed URI should be "(.*?)"$/ do |arg1|
 
   #First, make sure the paths of the URIs are the same
   expectedPath = expected.gsub("@context", "[^/]+")
+  expectedPath = expectedPath.gsub("@id", @id) if expectedPath.include?("@id")
+  expectedPath = expectedPath.gsub("\?", "\\\?")
   assert(actual.match(expectedPath), "Rewriten URI path didn't match, expected:#{expectedPath}, actual:#{actual}")
 
   #Then, validate the list of ids are the same
@@ -54,12 +57,5 @@ Then /^the executed URI should be "(.*?)"$/ do |arg1|
     expectedIds.each do |id|
       assert(actualIds.include?(id),"Infered Context IDs not equal: expected:#{expectedIds.inspect}, actual:#{actualIds.inspect}")
     end
-  end
-  
-  if expectedPath.include?("?")
-    queryId = actual.match(/?[^=]*=(.+)/)[1]
-    assert(queryId == @id, "Query Parameter did not match, expected #{@id} but was #{queryId}")
-  else
-    assert(!actual.include?("?"), "Expected no query parameters but found one: #{actual}")
   end
 end
--- sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
@@ -119,7 +119,7 @@ public class UriMutator {
                     mutatedParameters = mutuateQueryParameterString("courseId", transitiveEntityId, mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.COURSE_OFFERINGS + ";" + PathConstants.SECTIONS + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
-                    mutatedPath = String.format("/sections/%s",
+                    mutatedPath = String.format("/sections/%s/",
                             StringUtils.join(sectionHelper.getTeachersSections(user), ","));
                     mutatedParameters = mutuateQueryParameterString("courseOfferingId", transitiveEntityId,
                             mutatedParameters);
@@ -131,16 +131,16 @@ public class UriMutator {
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.GRADING_PERIODS + ";" + PathConstants.GRADES + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
-                    mutatedPath = String.format("/sections/%s/studentSectionAssociations/students/grades",
+                    mutatedPath = String.format("/sections/%s/studentSectionAssociations/grades",
                             StringUtils.join(sectionHelper.getTeachersSections(user), ","));
-                    mutatedParameters = mutuateQueryParameterString("gradingPeriod", transitiveEntityId,
+                    mutatedParameters = mutuateQueryParameterString("gradingPeriodId", transitiveEntityId,
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.GRADING_PERIODS + ";" + PathConstants.REPORT_CARDS
                         + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
                     mutatedPath = String.format("/sections/%s/studentSectionAssociations/students/reportCards",
                             StringUtils.join(sectionHelper.getTeachersSections(user), ","));
-                    mutatedParameters = mutuateQueryParameterString("gradingPeriod", transitiveEntityId,
+                    mutatedParameters = mutuateQueryParameterString("gradingPeriodId", transitiveEntityId,
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.LEARNING_OBJECTIVES + ";"
                         + PathConstants.STUDENT_COMPETENCIES + ";")) {
@@ -151,7 +151,7 @@ public class UriMutator {
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.SESSIONS + ";" + PathConstants.SECTIONS + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
-                    mutatedPath = String.format("/sections/%s",
+                    mutatedPath = String.format("/sections/%s/",
                             StringUtils.join(sectionHelper.getTeachersSections(user), ","));
                     mutatedParameters = mutuateQueryParameterString("sessionId", transitiveEntityId, mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.SESSIONS + ";" + PathConstants.STUDENT_ACADEMIC_RECORDS
@@ -292,16 +292,16 @@ public class UriMutator {
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.GRADING_PERIODS + ";" + PathConstants.GRADES + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
-                    mutatedPath = String.format("/schools/%s/studentSchoolAssociations/students/grades",
+                    mutatedPath = String.format("/schools/%s/sections/studentSectionAssociations/grades",
                             StringUtils.join(edOrgHelper.getDirectEdOrgAssociations(user), ","));
-                    mutatedParameters = mutuateQueryParameterString("gradingPeriod", transitiveEntityId,
+                    mutatedParameters = mutuateQueryParameterString("gradingPeriodId", transitiveEntityId,
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.GRADING_PERIODS + ";" + PathConstants.REPORT_CARDS
                         + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
                     mutatedPath = String.format("/schools/%s/studentSchoolAssociations/students/reportCards",
                             StringUtils.join(edOrgHelper.getDirectEdOrgAssociations(user), ","));
-                    mutatedParameters = mutuateQueryParameterString("gradingPeriod", transitiveEntityId,
+                    mutatedParameters = mutuateQueryParameterString("gradingPeriodId", transitiveEntityId,
                             mutatedParameters);
                 } else if (modifiedRequest.equals(PathConstants.SESSIONS + ";" + PathConstants.SECTIONS + ";")) {
                     verifySingleTransitiveId(transitiveEntityId);
@@ -453,7 +453,7 @@ public class UriMutator {
                 result = String.format("/teachers/%s/teacherSectionAssociations/sections", user.getEntityId());
             } else if (ResourceNames.SCHOOLS.equals(resource)) {
                 List<String> ids = edOrgHelper.getDirectSchools(user);
-                result = String.format("/schools/%s", StringUtils.join(edOrgHelper.getDirectSchools(user), ","));
+                result = String.format("/schools/%s/", StringUtils.join(edOrgHelper.getDirectSchools(user), ","));
                 success = !ids.isEmpty();
             } else if (ResourceNames.SESSIONS.equals(resource)) {
                 result = String.format("/educationOrganizations/%s/sessions",
@@ -565,7 +565,7 @@ public class UriMutator {
                 result = String.format("/schools/%s/studentSchoolAssociations/students/reportCards", ids);
             } else if (ResourceNames.SCHOOLS.equals(resource)) {
                 List<String> ids = edOrgHelper.getDirectSchools(user);
-                result = String.format("/schools/%s", StringUtils.join(edOrgHelper.getDirectSchools(user), ","));
+                result = String.format("/schools/%s/", StringUtils.join(edOrgHelper.getDirectSchools(user), ","));
                 success = !ids.isEmpty();
             } else if (ResourceNames.SECTIONS.equals(resource)) {
                 String ids = StringUtils.join(edOrgHelper.getDirectEdOrgAssociations(user), ",");
