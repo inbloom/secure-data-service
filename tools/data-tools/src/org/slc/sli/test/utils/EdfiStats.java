5148f73a5058c680ac515858f29048347af19bf0
John Singh <jsingh@jsingh-mac.local>
2012-11-08 17:22:59 -0500

    Removed studentAssessmentAssociation and studentSectionAssociation from verification mongo scripts as now in super-docs

--- tools/data-tools/src/org/slc/sli/test/utils/EdfiStats.java
+++ tools/data-tools/src/org/slc/sli/test/utils/EdfiStats.java
@@ -154,7 +154,9 @@ public class EdfiStats {
         sliCounts.remove("serviceDescriptor"            );
         sliCounts.remove("studentAssessmentItem"        );
         sliCounts.remove("studentObjectiveAssessment"   );
-        
+        sliCounts.remove("studentAssessmentAssociation" );
+        sliCounts.remove("studentSectionAssociation"    );
+
         PrintWriter js = new PrintWriter(
                 new FileOutputStream(dataDirectory + "/" + "jsExpected.js"));
         js.println("//mongo localhost:27017/sli --eval 'var tenant=\"MegatronIL\";' jsExpected.js");
