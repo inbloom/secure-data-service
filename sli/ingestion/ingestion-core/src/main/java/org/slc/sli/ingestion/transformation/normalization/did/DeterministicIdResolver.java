772f038a6888a8aa06e4720123a20389361b78f9
jtully <jtully@wgen.net>
2012-11-09 12:44:04 -0500

    [US4398] Updating deterministicIdResolver to handle refs in lists of embedded sub entities

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolver.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/normalization/did/DeterministicIdResolver.java
@@ -74,6 +74,9 @@ public class DeterministicIdResolver {
 
     private static final Logger LOG = LoggerFactory.getLogger(DeterministicIdResolver.class);
 
+    private static final String BODY_PATH = "body.";
+    private static final String PATH_SEPARATOR = "\\.";
+
     public void resolveInternalIds(Entity entity, String tenantId, ErrorReport errorReport) {
 
         if (IdNormalizerFlag.useOldNormalization) {
@@ -101,6 +104,7 @@ public class DeterministicIdResolver {
                 referenceEntityType = didRefSource.getEntityType();
 
                 sourceRefPath = didRefSource.getSourceRefPath();
+
                 NeutralSchema schema = schemaRepository.getSchema(referenceEntityType);
                 if (schema != null && schema.getAppInfo() != null) {
                     collectionName = schema.getAppInfo().getCollectionType();
@@ -129,7 +133,7 @@ public class DeterministicIdResolver {
         return didSchemaParser.getRefConfigs().get(refType);
     }
 
-    private void handleDeterministicIdForReference(Entity entity, DidRefSource didRefSource, String collectionName,
+	private void handleDeterministicIdForReference(Entity entity, DidRefSource didRefSource, String collectionName,
             String tenantId) throws IdResolutionException {
 
         String entityType = didRefSource.getEntityType();
@@ -141,7 +145,39 @@ public class DeterministicIdResolver {
             return;
         }
 
-        Object referenceObject = getProperty(entity, sourceRefPath);
+        //handle case of references within embedded lists of objects (for assessments)
+        String elementRefPath = sourceRefPath;
+        //split source ref path and look for lists in embedded objects
+        String strippedRefPath = sourceRefPath.replaceFirst(BODY_PATH, "");
+        String[] pathParts = strippedRefPath.split(PATH_SEPARATOR);
+
+        //is there an embedded list of objects?
+        List<Object> embeddedList = null;
+        if (pathParts.length > 1) {
+        	Object embeddedObject = getProperty(entity.getBody(), pathParts[0]);
+        	if (embeddedObject instanceof List) {
+        		embeddedList = (List<Object>) embeddedObject;
+        	}
+        }
+
+        if (embeddedList != null) {
+        	for (int objIndex = 0; objIndex < embeddedList.size(); objIndex++) {
+        		//construct indexed x-path string
+        		String indexedRefPath = BODY_PATH + pathParts[0] + ".["
+        				+ Integer.toString(objIndex) + "]." +  pathParts[1];
+        		didRefSource.setSourceRefPath(indexedRefPath);
+        		resolveReference(didRefSource, didRefConfig, entity, collectionName, tenantId);
+            }
+        } else {
+        	resolveReference(didRefSource, didRefConfig, entity, collectionName, tenantId);
+        }
+
+    }
+
+    private void resolveReference(DidRefSource didRefSource, DidRefConfig didRefConfig,
+    		Entity entity, String collectionName, String tenantId) throws IdResolutionException {
+    	String sourceRefPath = didRefSource.getSourceRefPath();
+    	Object referenceObject = getProperty(entity, sourceRefPath);
 
         if (referenceObject == null) {
             // ignore an empty reference if it is optional
@@ -294,6 +330,8 @@ public class DeterministicIdResolver {
             // don't add null or empty keys or values to the naturalKeys map
             if (fieldName == null || fieldName.isEmpty() || value == null) {
                 continue;
+            } else {
+            	//
             }
             naturalKeys.put(fieldName, value);
         }
