9930eb281ec749cf8e59751a850bee038c61e392
smelody@wgen.net <smelody@wgen.net>
2012-11-13 11:19:35 -0500

    Refactored to utility class

--- tools/odin/odin.rb
+++ tools/odin/odin.rb
@@ -18,24 +18,10 @@ limitations under the License.
 
 require "rexml/document"
 require 'digest/md5'
-
-################### Should be moved into utilities file #########################################
-def stripInterchange(interchangeName)
-  interchangeName[11..-5]
-end
-
-def genCtlFile(dir='generated')
-  File.open("generated/ControlFile.ctl", 'w') do |f|
-    Dir["#{File.dirname(__FILE__)}/#{dir}/*.xml"].each do |int|
-      # I don't like how I get the ingestion file type below
-      f.write "edfi-xml,#{stripInterchange(int[int.rindex('/')+1..-1])},#{int[int.rindex('/')+1..-1]},#{Digest::MD5.file(int).hexdigest}\n"
-    end
-  end
-end
-
-###########################################################################################################
-
 require 'yaml'
+require_relative 'util.rb'
+
+
 Dir["#{File.dirname(__FILE__)}/interchangeGenerators/*.rb"].each { |f| load(f) }
 
 configYAML = YAML.load_file(File.join(File.dirname(__FILE__),'config.yml'))
