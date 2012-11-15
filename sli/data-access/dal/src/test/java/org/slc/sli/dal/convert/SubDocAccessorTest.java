8fac8714dddc84aafc81df0b5d52e71b18fc19b2
Dong Liu <dliu@wgen.net>
2012-11-15 13:33:22 -0500

    DE2141 api perf regression fix

--- sli/data-access/dal/src/test/java/org/slc/sli/dal/convert/SubDocAccessorTest.java
+++ sli/data-access/dal/src/test/java/org/slc/sli/dal/convert/SubDocAccessorTest.java
@@ -35,22 +35,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.CommandResult;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.WriteConcern;
-import com.mongodb.WriteResult;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
-
 import org.slc.sli.common.domain.NaturalKeyDescriptor;
 import org.slc.sli.common.util.tenantdb.TenantContext;
 import org.slc.sli.common.util.uuid.DeterministicUUIDGeneratorStrategy;
@@ -58,6 +45,18 @@ import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.MongoEntity;
 import org.slc.sli.validation.NoNaturalKeysDefinedException;
 import org.slc.sli.validation.schema.INaturalKeyExtractor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.CommandResult;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.WriteConcern;
+import com.mongodb.WriteResult;
 
 /**
  * Test for sub doc accessor
@@ -159,8 +158,9 @@ public class SubDocAccessorTest {
                 "{$unwind: \"$studentSectionAssociation\"},{$match : { \"studentSectionAssociation._id\" : \"parent_idchild\"}}]}";
         when(template.executeCommand(findByID)).thenReturn(successCR);
 
-        String nonExistIdForDelete = "{aggregate : \"section\", pipeline:[{$project : {\"studentSectionAssociation\":1,\"_id\":0 } }," +
-                "{$unwind: \"$studentSectionAssociation\"},{$match : { \"studentSectionAssociation._id\" : \"nonExistId\"}}]}";
+        String nonExistIdForDelete = "{aggregate : \"section\", pipeline:[{$match : { \"studentSectionAssociation._id\" : \"nonExistId\"}}"
+                + ",{$project : {\"studentSectionAssociation\":1,\"_id\":0 } },"
+                + "{$unwind: \"$studentSectionAssociation\"},{$match : { \"studentSectionAssociation._id\" : \"nonExistId\"}}]}";
 
         when(template.executeCommand(nonExistIdForDelete)).thenReturn(failCR);
 
