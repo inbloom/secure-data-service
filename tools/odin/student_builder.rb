cb8ef4250fccf225af3d4efa636e425609656a89
Nick Brown <nbrown@wgen.net>
2012-11-15 16:32:31 -0500

    get student builder to kinda acutally work

--- tools/odin/student_builder.rb
+++ tools/odin/student_builder.rb
@@ -17,37 +17,39 @@ limitations under the License.
 =end
 
 require_relative 'baseEntityClasses/student.rb'
+require_relative 'baseEntityClasses/studentSchoolAssociation.rb'
+require_relative 'baseEntityClasses/studentSectionAssociation.rb'
 
 class StudentBuilder
 
   def initialize(work_order, files)
-    @id = work_order.id
-    @rand = Random.new(id)
+    @id = work_order[:id]
+    @rand = Random.new(@id)
     @work_order = work_order
-    @student_writer = files.studentParent
-    @enrollment_writer = files.enrollment
+    @student_writer = files[:studentParent]
+    @enrollment_writer = files[:enrollment]
   end
 
   def build
-    s = Student.new(id, work_order.student_demographics, rand)
+    s = Student.new(@id, @rand)
     @student_writer.write(s.render)
-    (0..@work_order.enrollments).each{ |term|
+    @work_order[:enrollments].each{ |term|
       gen_enrollment(term)
     }
   end
 
   def gen_enrollment(term)
-    school_id = term.school
+    school_id = term[:school]
     schoolAssoc = StudentSchoolAssociation.new(@id, school_id, @rand)
     @enrollment_writer.write(schoolAssoc.render)
-    (0..term.sections).each{ |section|
-      gen_section(id, section, writer, rand)
+    term[:sections].each{ |section|
+      gen_section(section)
     }
   end
 
-  def gen_section(id, section, writer, rand)
-    sectionAssoc = StudentSectionAssociation.new(id, section.id)
-    writer.write(sectionAssoc.render)
+  def gen_section(section)
+    sectionAssoc = StudentSectionAssociation.new(@id, section[:id], section[:edOrg], @rand)
+    @enrollment_writer.write(sectionAssoc.render)
   end
 
 end
