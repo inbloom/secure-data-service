0ebc06d332c3e1c7f06a4eabf9648663f7497d87
Nidhi Paroha <nparoha@gmail.com>
2012-11-19 14:10:13 -0500

    debugging failures

--- sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_samt.rb
+++ sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_samt.rb
@@ -75,7 +75,7 @@ end
 Then /^I delete the user "(.*?)" if exists$/ do |fullname|
   begin
     link = @driver.find_element(:xpath, "//a[@id='#{fullname}_delete']")
-    puts "admin account management debug = \n" + link.attribute('innerHTML').to_s
+    puts "admin account management debug = \n" + link.to_s
   rescue
   end
   unless(link.nil?)
