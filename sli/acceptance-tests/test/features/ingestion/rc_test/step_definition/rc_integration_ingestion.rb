4eca1768481172f70d26d7e36e166aa469e06f92
John Singh <jsingh@jsingh-mac.local>
2012-11-19 18:12:53 -0500

    Trying commented out link

--- sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
+++ sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
@@ -26,7 +26,7 @@ require 'net/http'
 require 'rest-client'
 
 require_relative '../../../utils/sli_utils.rb'
-require_relative '../../../cross_app_tests/step_definitions/rc_integration_samt.rb'
+#require_relative '../../../cross_app_tests/step_definitions/rc_integration_samt.rb'
 require_relative '../../features/step_definitions/ingestion_steps.rb'
 
 UPLOAD_FILE_SCRIPT = File.expand_path("../opstools/ingestion_trigger/publish_file_uploaded.rb")
