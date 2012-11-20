75a3b54c679793cf78ea6dc5942bf9f31e6cfc0a
Yang Cao <ycao@ycao-mac.local>
2012-11-20 14:54:28 -0500

    [US4826] remove more TODOs

--- sli/api/src/main/java/org/slc/sli/api/ldap/UserContextMapper.java
+++ sli/api/src/main/java/org/slc/sli/api/ldap/UserContextMapper.java
@@ -53,8 +53,6 @@ public class UserContextMapper implements ContextMapper {
         user.setHomeDir(context.getStringAttribute("homeDirectory"));
         user.setCreateTime(ldapStringToDate(context.getStringAttribute(LdapService.CREATE_TIMESTAMP)));
         user.setModifyTime(ldapStringToDate(context.getStringAttribute(LdapService.MODIFY_TIMESTAMP)));
-        // TODO figure out consistent ways to set user password with either plain text or MD5 hash
-        // user.setPassword(context.getStringAttribute("userPassword"));
         user.setPassword("**********");
         user.setCn(context.getStringAttribute("cn"));
         user.setStatus(User.Status.getFromString(context.getStringAttribute("destinationindicator")));
