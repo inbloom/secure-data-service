5a22d0dc93b73acb7f709dca769ff0ecc487b25b
ablum <ablum@wgen.net>
2012-11-28 12:11:03 -0500

    Removed unneeded require

--- sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
+++ sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
@@ -26,7 +26,6 @@ require 'net/http'
 require 'rest-client'
 
 require_relative '../../../utils/sli_utils.rb'
-require_relative '../../../cross_app_tests/step_definitions/rc_accountRequest.rb'
 require_relative '../../features/step_definitions/ingestion_steps.rb'
 
 UPLOAD_FILE_SCRIPT = File.expand_path("../opstools/ingestion_trigger/publish_file_uploaded.rb")
