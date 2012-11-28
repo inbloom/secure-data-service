cb23ce7966b94a9f81cba360234865482f163d99
John Singh <jsingh@jsingh-mac.local>
2012-11-27 21:21:05 -0500

    Added before hook to initilise RUN_ON_RC

--- sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
+++ sli/acceptance-tests/test/features/ingestion/rc_test/step_definition/rc_integration_ingestion.rb
@@ -37,6 +37,10 @@ end
 
 World(NoLandingZone)
 
+Before do
+  RUN_ON_RC = ENV['RUN_ON_RC'] ? true : false
+end
+
 ############################################################
 # TEST SETUP FUNCTIONS
 ############################################################
