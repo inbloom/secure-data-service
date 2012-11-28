31b57d995c52ad213ee12309bbe2aa6fdf13b6ad
Matthew Lane <lanemt@gmail.com>
2012-11-28 11:11:09 -0500

    code cleanup: removes dead store local variables

--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherAttendanceResolver.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/TeacherAttendanceResolver.java
@@ -16,18 +16,18 @@
 
 package org.slc.sli.api.security.context.resolver;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 import org.slc.sli.api.constants.EntityNames;
+import org.slc.sli.api.security.context.PagingRepositoryDelegate;
 import org.slc.sli.api.security.context.traversal.cache.impl.SessionSecurityCache;
 import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.NeutralCriteria;
 import org.slc.sli.domain.NeutralQuery;
-import org.slc.sli.api.security.context.PagingRepositoryDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Resolves which attendance events any given teacher can access.
@@ -54,7 +54,7 @@ public class TeacherAttendanceResolver implements EntityContextResolver {
     
     @Override
     public List<String> findAccessible(Entity principal) {
-        List<String> studentIds = new ArrayList<String>();
+        List<String> studentIds;
         if (!securityCachingStrategy.contains(EntityNames.STUDENT)) {
             studentIds = studentResolver.findAccessible(principal);
         } else {
