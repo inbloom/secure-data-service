e82d6caa909112a3e747ab8f7ee2c014c81a3859
ablum <ablum@wgen.net>
2012-11-28 14:32:58 -0500

    Fixed require_relative paths

--- sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_ingestion.rb
+++ sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_ingestion.rb
@@ -25,8 +25,8 @@ require 'net/sftp'
 require 'net/http'
 require 'rest-client'
 
-require_relative '../../../utils/sli_utils.rb'
-require_relative '../../../ingestion/features/step_definitions/ingestion_steps.rb'
+require_relative '../../utils/sli_utils.rb'
+require_relative '../../ingestion/features/step_definitions/ingestion_steps.rb'
 
 UPLOAD_FILE_SCRIPT = File.expand_path("../opstools/ingestion_trigger/publish_file_uploaded.rb")
 
