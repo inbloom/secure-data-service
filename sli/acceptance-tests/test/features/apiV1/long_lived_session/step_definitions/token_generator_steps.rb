21af7f3a96e9a8502b69305c8b1300ee7f50d465
Jonathan Nanney <jnanney@wgen.net>
2012-11-29 12:26:38 -0500

    Fixed API test failure.

--- sli/acceptance-tests/test/features/apiV1/long_lived_session/step_definitions/token_generator_steps.rb
+++ sli/acceptance-tests/test/features/apiV1/long_lived_session/step_definitions/token_generator_steps.rb
@@ -2,7 +2,7 @@ require_relative '../../../utils/sli_utils.rb'
 require 'open3'
 
 Given /^I used the long lived session token generator script to create a token for user "(.*?)" with role "(.*?)" in tenant "(.*?)"  for realm "(.*?)" that will expire in "(.*?)" seconds with client_id "(.*?)"$/ do |user, role, tenant, realm, expiration_in_seconds, client_id|
-  out, status = Open3.capture2("ruby /Users/jnanney/Documents/workspace/sli/sli/opstools/token-generator/generator.rb -e #{expiration_in_seconds} -c #{client_id} -u #{user} -r \"#{role}\" -t \"#{tenant}\" -R \"#{realm}\"")
+  out, status = Open3.capture2("ruby ../opstools/token-generator/generator.rb -e #{expiration_in_seconds} -c #{client_id} -u #{user} -r \"#{role}\" -t \"#{tenant}\" -R \"#{realm}\"")
   match = /token is (.*)/.match(out)
   @sessionId = match[1]
   puts("The generated token is #{@sessionId}") if $SLI_DEBUG
