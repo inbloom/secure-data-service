3275fd56a03c7a26d834c43d08e3961d84476e0e
Tao Ke <tke@wgen.net>
2012-11-30 17:44:01 -0500

    Cleaned up

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/util/MongoCommander.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/util/MongoCommander.java
@@ -17,11 +17,9 @@ package org.slc.sli.ingestion.util;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
@@ -47,46 +45,6 @@ public final class MongoCommander {
     private MongoCommander() { }
 
     protected static final Logger LOG = LoggerFactory.getLogger(MongoCommander.class);
-
-    /**
-     * Executes mongo command
-     *
-     * @param db
-     *            the database name in mongo
-     * @param script
-     *            the name of the java script to be executed
-     * @param jsContent
-     *            javascript content to be evaluated before script execution
-     * @return
-     */
-    public static void exec(String db, String script, String jsContent) {
-        try {
-            URL scriptFile = Thread.currentThread().getContextClassLoader().getResource(script);
-            if (scriptFile != null) {
-                String path = (new File(scriptFile.getFile())).getPath();
-                String[] args = { "mongo", db, "--eval", jsContent, path };
-
-                LOG.info("Running process with args: {} {} {} {} {}", args);
-                ProcessBuilder pb = new ProcessBuilder(args);
-                Process pr = pb.start();
-                try {
-                    pr.waitFor();
-                } catch (InterruptedException e) {
-                    LOG.error(e.getMessage());
-                }
-                if (pr.exitValue() != 0) {
-                    LOG.error("Failed to execute the script " + script + " during tenant onboarding");
-                    throw new RuntimeException("Failed to execute the script " + script + " during tenant onboarding");
-                }
-            } else {
-                LOG.error("Failed to locate the script " + script + " during tenant onboarding");
-                throw new RuntimeException("Failed to locate the script " + script + " during tenant onboarding");
-            }
-        } catch (IOException e) {
-            LOG.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
 
     private static boolean validIndex (String line) {
         if (line.startsWith("#")) {
