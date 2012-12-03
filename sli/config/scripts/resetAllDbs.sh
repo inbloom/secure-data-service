250cecbab2a72c1af121dab80383322b1a9e5102
Tao Ke <tke@wgen.net>
2012-12-03 17:05:12 -0500

    Made resetAllDbs.sh to install missing gems so that we don't need jenkins job changes

--- sli/config/scripts/resetAllDbs.sh
+++ sli/config/scripts/resetAllDbs.sh
@@ -22,6 +22,8 @@ then
   });'
 fi
 
+bundle install --deployment
+
 echo "[MONGO] indexing database: sli"; mongo sli ../indexes/sli_indexes.js --quiet 
 echo "[MONGO] indexing database: ingestion_batch_job"; mongo ingestion_batch_job ../indexes/ingestion_batch_job_indexes.js --quiet
 echo "[MONGO] indexing database: midgar"; bundle exec ruby ./indexTenantDb.rb localhost 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a
