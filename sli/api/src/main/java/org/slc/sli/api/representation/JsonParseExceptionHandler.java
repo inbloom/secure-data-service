b62e5d9c64c3793ce91869a82beb00663d940cf2
Stephan Altmueller <saltmueller@wgen.net>
2012-12-05 09:15:51 -0500

    DE2258 - added AT to test upload of large (1+ MB) of custom base64 encoded data and added JSON parse exception handler to turn 500 error into bad request error

---
+++ sli/api/src/main/java/org/slc/sli/api/representation/JsonParseExceptionHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.slc.sli.api.representation;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.codehaus.jackson.JsonParseException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Exception handler for SortingExceptions
+ * 
+ * @author Ryan Farris <rfarris@wgen.net>
+ * 
+ */
+@Provider
+@Component
+public class JsonParseExceptionHandler implements ExceptionMapper<JsonParseException> {
+    
+    @Override
+    public Response toResponse(JsonParseException e) {
+        Response.Status errorStatus = Response.Status.BAD_REQUEST;
+        return Response.status(errorStatus)
+                .entity(new ErrorResponse(errorStatus.getStatusCode(), errorStatus.getReasonPhrase(), e.getMessage()))
+                .build();
+    }
+    
+}
