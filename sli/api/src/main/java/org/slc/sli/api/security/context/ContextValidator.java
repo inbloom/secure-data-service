a86f693cc072b13e521dab636f0a97a455a5e52a
Jonathan Nanney <jnanney@wgen.net>
2012-12-06 16:23:55 -0500

    Implemented more of ContextValidator and unit test.

--- sli/api/src/main/java/org/slc/sli/api/security/context/ContextValidator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/ContextValidator.java
@@ -19,14 +19,18 @@ package org.slc.sli.api.security.context;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.core.PathSegment;
 
 import org.slc.sli.api.config.EntityDefinition;
+import org.slc.sli.api.config.EntityDefinitionStore;
 import org.slc.sli.api.constants.EntityNames;
 import org.slc.sli.api.resources.generic.util.ResourceHelper;
 import org.slc.sli.api.security.SLIPrincipal;
@@ -53,162 +57,214 @@ import com.sun.jersey.spi.container.ContainerRequest;
 @Component
 public class ContextValidator implements ApplicationContextAware {
 
-    private List<IContextValidator> validators;
+	private List<IContextValidator> validators;
 
+	@Autowired
+	private ResourceHelper resourceHelper;
+
+	@Autowired
+	private PagingRepositoryDelegate<Entity> repo;
+	
     @Autowired
-    private ResourceHelper resourceHelper;
+    private EntityDefinitionStore store;
 
-    @Autowired
-    private PagingRepositoryDelegate<Entity> repo;
-    private static final Collection<String> ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION = new HashSet<String>(
-            Arrays.asList(EntityNames.ATTENDANCE, EntityNames.COHORT, EntityNames.COURSE, EntityNames.COURSE_OFFERING,
-                    EntityNames.COURSE_TRANSCRIPT, EntityNames.DISCIPLINE_INCIDENT, EntityNames.DISCIPLINE_ACTION,
-                    EntityNames.GRADEBOOK_ENTRY, EntityNames.GRADUATION_PLAN, EntityNames.PROGRAM, EntityNames.SECTION,
-                    EntityNames.SESSION, EntityNames.STUDENT_COHORT_ASSOCIATION,
-                    EntityNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATION, EntityNames.STUDENT_PROGRAM_ASSOCIATION,
-                    EntityNames.STUDENT_GRADEBOOK_ENTRY, EntityNames.STUDENT_SCHOOL_ASSOCIATION,
-                    EntityNames.STUDENT_SECTION_ASSOCIATION)
-    );
+	private Map<String, String> ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        validators = new ArrayList<IContextValidator>();
-        validators.addAll(applicationContext.getBeansOfType(IContextValidator.class).values());
-    }
+	@PostConstruct
+	private void init() {
+		ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION = new HashMap<String, String>() {{
+			put(EntityNames.ATTENDANCE, "");
+			put(EntityNames.COHORT, "");
+			put(EntityNames.COURSE, "");
+			put(EntityNames.COURSE_OFFERING, "");
+			put(EntityNames.COURSE_TRANSCRIPT, "");
+			put(EntityNames.DISCIPLINE_INCIDENT, "");
+			put(EntityNames.DISCIPLINE_ACTION, "");
+			put(EntityNames.GRADEBOOK_ENTRY, "");
+			put(EntityNames.GRADUATION_PLAN, "");
+			put(EntityNames.PROGRAM, "");
+			put(EntityNames.SECTION, "");
+			put(EntityNames.SESSION, "");
+			put(EntityNames.STUDENT_COHORT_ASSOCIATION, "");
+			put(EntityNames.STUDENT_DISCIPLINE_INCIDENT_ASSOCIATION, "");
+			put(EntityNames.STUDENT_PROGRAM_ASSOCIATION, "");
+			put(EntityNames.STUDENT_GRADEBOOK_ENTRY, "");
+			put(EntityNames.STUDENT_SCHOOL_ASSOCIATION, "");
+			put(EntityNames.STUDENT_SECTION_ASSOCIATION, "");
+		}};
+	}
 
-    public void validateContextToUri(ContainerRequest request, SLIPrincipal principal) {
-        validateUserHasContextToRequestedEntities(request, principal);
-        if (!isValidForEdOrgWrite(request, principal)) {
-            throw new AccessDeniedException("Failed education organization write validation");
-        }
-    }
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		validators = new ArrayList<IContextValidator>();
+		validators.addAll(applicationContext.getBeansOfType(IContextValidator.class).values());
+	}
 
-    private boolean isValidForEdOrgWrite(ContainerRequest request, SLIPrincipal principal) {
-        boolean isValid = true;
+	public void validateContextToUri(ContainerRequest request, SLIPrincipal principal) {
+		validateUserHasContextToRequestedEntities(request, principal);
+		boolean isValid = true;
+		
+		if (request.getMethod() != "GET") {
+			if(request.getMethod() != "POST") {
+				// look if we have ed org write context to already existing entity
+				int RESOURCE_SEGMENT_INDEX = 1;
+				int IDS_SEGMENT_INDEX = 2;
+				if (request.getPathSegments().size() > IDS_SEGMENT_INDEX) {
+					String resourceName = request.getPathSegments().get(RESOURCE_SEGMENT_INDEX).getPath();
+					String id = request.getPathSegments().get(IDS_SEGMENT_INDEX).getPath();
+					Entity entity = repo.findById(store.lookupByResourceName(resourceName).getStoredCollectionName(), id);
+					isValid = isValidForEdOrgWrite(entity, principal);
+				}
+				// check write context of update
+			}
+			Entity entity = request.getEntity(Entity.class);
+			if (ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.get(entity.getType()) != null) {
+				String edOrgId = (String) entity.getBody().get(ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.get(entity.getType()));
+				isValid = principal.getSubEdOrgHierarchy().contains(edOrgId);
+			}
+		}
+		
+		if(!isValid) {
+			throw new AccessDeniedException("Trying to write an entity outside of your education organization hierarchy");
+		}
+	}
+	
 
-        if (request.getMethod() != "GET") {
-            String entityType = getEntityType(request);
-            if (ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.contains(entityType)) {
-                Collection<String> principalsEdOrgs = principal.getSubEdOrgHierarchy(); //TODO initialize the principal hierarchy
-                String entityEdOrg = getEntityEdOrg(request);
-                isValid = principalsEdOrgs.contains(entityEdOrg);
-            }
-        }
-        return isValid;
-    }
+	private boolean isValidForEdOrgWrite(Entity entity , SLIPrincipal principal) {
+		boolean isValid = true;
 
-    private String getEntityEdOrg(ContainerRequest request) {
-        return "";  //TODO replace stub
-    }
+			if (ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.get(entity.getType()) != null) {
+				Collection<String> principalsEdOrgs = principal.getSubEdOrgHierarchy(); //TODO initialize the principal hierarchy
+				String edOrgId = (String) entity.getBody().get(ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.get(entity.getType()));
+				isValid = principalsEdOrgs.contains(edOrgId);
+			}
+		return isValid;
+	}
 
-    private String getEntityType(ContainerRequest request) {
-        return "";  //TODO replace stub
-    }
+	private String getEntityEdOrg(ContainerRequest request, EntityDefinition entityDef) {
+		int IDS_SEGMENT_INDEX = 2;
+		String edOrgId = "";
+		List<PathSegment> pathSegs = request.getPathSegments();
+		
+		if (pathSegs.size() > IDS_SEGMENT_INDEX) {
+			String id = pathSegs.get(IDS_SEGMENT_INDEX).getPath();
+			Entity entity = repo.findById(entityDef.getStoredCollectionName(), id);
+			edOrgId = (String) entity.getBody().get(ENTITIES_NEEDING_ED_ORG_WRITE_VALIDATION.get(entityDef.getType()));
+		}
+		return edOrgId;  
+	}
 
-    private void validateUserHasContextToRequestedEntities(ContainerRequest request, SLIPrincipal principal) {
+	private EntityDefinition getEntityDefinition(ContainerRequest request) {
+		List<PathSegment> pathSegments = request.getPathSegments();
+		String resource = pathSegments.get(1).getPath();
+		EntityDefinition entityDef = store.lookupByResourceName(resource);
+		return entityDef;
+	}
 
-        List<PathSegment> segs = request.getPathSegments();
-        for (Iterator<PathSegment> i = segs.iterator(); i.hasNext(); ) {
-            if (i.next().getPath().isEmpty()) {
-                i.remove();
-            }
-        }
+	private void validateUserHasContextToRequestedEntities(ContainerRequest request, SLIPrincipal principal) {
 
-        if (segs.size() < 3) {
-            return;
-        }
+		List<PathSegment> segs = request.getPathSegments();
+		for (Iterator<PathSegment> i = segs.iterator(); i.hasNext(); ) {
+			if (i.next().getPath().isEmpty()) {
+				i.remove();
+			}
+		}
 
-        String rootEntity = segs.get(1).getPath();
-        EntityDefinition def = resourceHelper.getEntityDefinition(rootEntity);
-        if (def == null) {
-            return;
-        }
+		if (segs.size() < 3) {
+			return;
+		}
 
-        /*
-        * e.g.
-        * !isTransitive - /v1/staff/<ID>/disciplineActions
-        * isTransitive - /v1/staff/<ID>
-        */
-        boolean isTransitive = segs.size() < 4;
+		String rootEntity = segs.get(1).getPath();
+		EntityDefinition def = resourceHelper.getEntityDefinition(rootEntity);
+		if (def == null) {
+			return;
+		}
 
-        /**
-         * If we are v1/entity/id and the entity is "public" don't validate
-         *
-         * Unless of course you're posting/putting/deleting, blah blah blah.
-         */
-        if (segs.size() == 3 || (segs.size() == 4 && segs.get(3).getPath().equals("custom"))) {
-            if (def.getStoredCollectionName().equals(EntityNames.EDUCATION_ORGANIZATION)) {
-                if (!request.getMethod().equals("GET")) {
-                    isTransitive = false;
-                } else {
-                    info("Not validating access to public entity and it's custom data");
-                    return;
-                }
+		/*
+		 * e.g.
+		 * !isTransitive - /v1/staff/<ID>/disciplineActions
+		 * isTransitive - /v1/staff/<ID>
+		 */
+		boolean isTransitive = segs.size() < 4;
 
-            }
-        }
+		/**
+		 * If we are v1/entity/id and the entity is "public" don't validate
+		 *
+		 * Unless of course you're posting/putting/deleting, blah blah blah.
+		 */
+		if (segs.size() == 3 || (segs.size() == 4 && segs.get(3).getPath().equals("custom"))) {
+			if (def.getStoredCollectionName().equals(EntityNames.EDUCATION_ORGANIZATION)) {
+				if (!request.getMethod().equals("GET")) {
+					isTransitive = false;
+				} else {
+					info("Not validating access to public entity and it's custom data");
+					return;
+				}
+
+			}
+		}
 
 
-        String idsString = segs.get(2).getPath();
-        Set<String> ids = new HashSet<String>(Arrays.asList(idsString.split(",")));
-        validateContextToEntities(def, ids, isTransitive);
-    }
+		String idsString = segs.get(2).getPath();
+		Set<String> ids = new HashSet<String>(Arrays.asList(idsString.split(",")));
+		validateContextToEntities(def, ids, isTransitive);
+	}
 
-    public void validateContextToEntities(EntityDefinition def, Collection<String> ids, boolean isTransitive) {
+	public void validateContextToEntities(EntityDefinition def, Collection<String> ids, boolean isTransitive) {
 
-        IContextValidator validator = findValidator(def.getType(), isTransitive);
-        if (validator != null) {
-            Set<String> idsToValidate = new HashSet<String>();
-            NeutralQuery getIdsQuery = new NeutralQuery(new NeutralCriteria("_id", "in", new ArrayList<String>(ids)));
-            int found = 0;
-            for (Entity ent : repo.findAll(def.getStoredCollectionName(), getIdsQuery)) {
-                found++;
-                if (SecurityUtil.principalId().equals(ent.getMetaData().get("createdBy"))
-                        && "true".equals(ent.getMetaData().get("isOrphaned"))) {
-                    debug("Entity is orphaned: id {} of type {}", ent.getEntityId(), ent.getType());
-                } else {
-                    idsToValidate.add(ent.getEntityId());
-                }
-            }
+		IContextValidator validator = findValidator(def.getType(), isTransitive);
+		if (validator != null) {
+			Set<String> idsToValidate = new HashSet<String>();
+			NeutralQuery getIdsQuery = new NeutralQuery(new NeutralCriteria("_id", "in", new ArrayList<String>(ids)));
+			int found = 0;
+			for (Entity ent : repo.findAll(def.getStoredCollectionName(), getIdsQuery)) {
+				found++;
+				if (SecurityUtil.principalId().equals(ent.getMetaData().get("createdBy"))
+						&& "true".equals(ent.getMetaData().get("isOrphaned"))) {
+					debug("Entity is orphaned: id {} of type {}", ent.getEntityId(), ent.getType());
+				} else {
+					idsToValidate.add(ent.getEntityId());
+				}
+			}
 
-            if (found != ids.size()) {
-                debug("Invalid reference, an entity does not exist. collection: {} ids: {}",
-                        def.getStoredCollectionName(), ids);
-                throw new EntityNotFoundException("Could not locate " + def.getType() + " with ids " + ids);
-            }
+			if (found != ids.size()) {
+				debug("Invalid reference, an entity does not exist. collection: {} ids: {}",
+						def.getStoredCollectionName(), ids);
+				throw new EntityNotFoundException("Could not locate " + def.getType() + " with ids " + ids);
+			}
 
-            if (!idsToValidate.isEmpty()) {
-                if (!validator.validate(def.getType(), idsToValidate)) {
-                    throw new AccessDeniedException("Cannot access entities " + ids);
-                }
-            }
-        } else {
-            throw new AccessDeniedException("No validator for " + def.getType() + ", transitive=" + isTransitive);
-        }
-    }
+			if (!idsToValidate.isEmpty()) {
+				if (!validator.validate(def.getType(), idsToValidate)) {
+					throw new AccessDeniedException("Cannot access entities " + ids);
+				}
+			}
+		} else {
+			throw new AccessDeniedException("No validator for " + def.getType() + ", transitive=" + isTransitive);
+		}
+	}
 
-    /**
-     * @param toType
-     * @param isTransitive
-     * @return
-     * @throws IllegalStateException
-     */
-    private IContextValidator findValidator(String toType, boolean isTransitive) throws IllegalStateException {
+	/**
+	 * @param toType
+	 * @param isTransitive
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	private IContextValidator findValidator(String toType, boolean isTransitive) throws IllegalStateException {
 
-        IContextValidator found = null;
-        for (IContextValidator validator : this.validators) {
-            if (validator.canValidate(toType, isTransitive)) {
-                info("Using {} to validate {}", new Object[]{validator.getClass().toString(), toType});
-                found = validator;
-                break;
-            }
-        }
+		IContextValidator found = null;
+		for (IContextValidator validator : this.validators) {
+			if (validator.canValidate(toType, isTransitive)) {
+				info("Using {} to validate {}", new Object[]{validator.getClass().toString(), toType});
+				found = validator;
+				break;
+			}
+		}
 
-        if (found == null) {
-            warn("No {} validator to {}.", isTransitive ? "TRANSITIVE" : "NOT TRANSITIVE", toType);
-        }
+		if (found == null) {
+			warn("No {} validator to {}.", isTransitive ? "TRANSITIVE" : "NOT TRANSITIVE", toType);
+		}
 
-        return found;
-    }
+		return found;
+	}
 
 }
--- sli/api/src/test/java/org/slc/sli/api/security/context/ContextValidatorTest.java
+++ sli/api/src/test/java/org/slc/sli/api/security/context/ContextValidatorTest.java
@@ -17,21 +17,30 @@
 
 package org.slc.sli.api.security.context;
 
-import com.sun.jersey.spi.container.ContainerRequest;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.PathSegment;
+
 import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.slc.sli.api.constants.EntityNames;
 import org.slc.sli.api.security.SLIPrincipal;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.MongoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-
-import static org.mockito.Mockito.when;
+import com.sun.jersey.spi.container.ContainerRequest;
 
 /**
  * Tests for ContextResolver
@@ -53,9 +62,47 @@ public class ContextValidatorTest {
     }
 
     @Test
-    public void testDenyWritingOutsideOfEdOrgHierarchy() throws Exception {
+    public void testDenyWritingOutsideOfEdOrgHierarchyCreate() throws Exception {
+    	
+    	Map<String, Object> entityBody = new HashMap<String, Object>();
+    	entityBody.put("schoolId", "unshared-id");
+    	Entity entity = new MongoEntity(EntityNames.SECTION, entityBody);
+    	
+    	when(containerRequest.getEntity(Entity.class)).thenReturn(entity);
+        when(containerRequest.getMethod()).thenReturn("POST");
+        when(containerRequest.getPathSegments()).thenReturn(Arrays.asList(new PathSegment[]{}));
+        when(principal.getSubEdOrgHierarchy()).thenReturn(Arrays.asList("valid-id", "second-valid-id"));
 
-        when(containerRequest.getMethod()).thenReturn("UPDATE");
+        Method validateEdOrgWrite = contextValidator.getClass().getDeclaredMethod("isValidForEdOrgWrite", Entity.class, SLIPrincipal.class);
+        validateEdOrgWrite.setAccessible(true);
+
+        Boolean isValid = (Boolean) validateEdOrgWrite.invoke(contextValidator, new Object[]{entity, principal});
+
+        Assert.assertFalse("should fail validation", isValid.booleanValue());
+
+    }
+
+    @Test
+    public void testValidWritingInEdOrgHierarchyCreate() throws Exception {
+
+        when(containerRequest.getMethod()).thenReturn("POST");
+        when(principal.getSubEdOrgHierarchy()).thenReturn(Arrays.asList("1234existsOnEntity"));
+        //TODO setup inputs to have a matching ed org
+
+        Method validateEdOrgWrite = contextValidator.getClass().getDeclaredMethod("isValidForEdOrgWrite", ContainerRequest.class, SLIPrincipal.class);
+        validateEdOrgWrite.setAccessible(true);
+
+        Boolean isValid = (Boolean) validateEdOrgWrite.invoke(contextValidator, new Object[]{containerRequest, principal});
+
+        Assert.assertTrue("should pass validation", isValid.booleanValue());
+
+    }
+    
+    @Test
+    public void testDenyWritingOutsideOfEdOrgHierarchyUpdate() throws Exception {
+
+        when(containerRequest.getMethod()).thenReturn("PUT");
+        when(containerRequest.getPathSegments()).thenReturn(Arrays.asList(new PathSegment[]{}));
 
         Method validateEdOrgWrite = contextValidator.getClass().getDeclaredMethod("isValidForEdOrgWrite", ContainerRequest.class, SLIPrincipal.class);
         validateEdOrgWrite.setAccessible(true);
@@ -67,9 +114,8 @@ public class ContextValidatorTest {
     }
 
     @Test
-    public void testValidWritingInEdOrgHierarchy() throws Exception {
-
-        when(containerRequest.getMethod()).thenReturn("UPDATE");
+    public void testValidWritingInEdOrgHierarchyUpdate() throws Exception {
+        when(containerRequest.getMethod()).thenReturn("PUT");
         when(principal.getSubEdOrgHierarchy()).thenReturn(Arrays.asList("1234existsOnEntity"));
         //TODO setup inputs to have a matching ed org
 
@@ -79,7 +125,7 @@ public class ContextValidatorTest {
         Boolean isValid = (Boolean) validateEdOrgWrite.invoke(contextValidator, new Object[]{containerRequest, principal});
 
         Assert.assertTrue("should pass validation", isValid.booleanValue());
-
+    	
     }
 
 
