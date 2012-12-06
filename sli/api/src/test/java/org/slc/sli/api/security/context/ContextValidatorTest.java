02748e6afb452afbc0011bd065f82b52e3916a89
Matthew Lane <lanemt@gmail.com>
2012-12-06 10:46:37 -0500

    stub for context validator tests

---
+++ sli/api/src/test/java/org/slc/sli/api/security/context/ContextValidatorTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.slc.sli.api.security.context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Tests for ContextResolver
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+public class ContextValidatorTest {
+
+    @Autowired
+    private ContextResolverStore contextResolverStore;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
+    @Test
+    public void test() {
+        //TODO
+    }
+
+}
