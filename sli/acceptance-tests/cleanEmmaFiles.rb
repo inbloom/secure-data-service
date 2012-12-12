3aa06b693d4a266f10b00cede6d65955c85d26c8
John Singh <jsingh@jsingh-mac.local>
2012-12-11 19:03:20 -0500

    Delete Emma files script and additional filter added to Emma POM profile

---
+++ sli/acceptance-tests/cleanEmmaFiles.rb
@@ -0,0 +1,10 @@
+begin
+    begin
+        File.delete("../ingestion/ingestion-validation/target/OfflineValidationTool/ingestion-validation/coverage.ec")
+    rescue Exception => e
+    end
+    begin
+        File.delete("../ingestion/ingestion-validation/target/OfflineValidationTool/ingestion-validation/coverage.ec")
+    rescue Exception => e
+    end
+end
