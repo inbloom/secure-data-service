5203b347ab2dec9c65f06820e76014bf6f071b24
Dong Liu <dliu@wgen.net>
2012-12-12 17:57:04 -0500

    add cutoff time to jmeter multi nodes test script

--- tools/jmeter/load_test/jmeter_multi_nodes_runner.rb
+++ tools/jmeter/load_test/jmeter_multi_nodes_runner.rb
@@ -29,9 +29,11 @@ JMETER_PROP = "/Users/dliu/sli_home/sli/tools/jmeter/local.properties"
 JMETER_JMX_DIR = "/Users/dliu/sli_home/sli/tools/jmeter"
 JMETER_JMXS = ["list-attendance.jmx","list-grades.jmx","list-sections.jmx","list-students.jmx","single-student.jmx","update-attendance.jmx","update-gradebooks.jmx"]
 RESULT_DIR = "/Users/dliu/sli_home/sli/tools/jmeter/result"
-REMOTE_SERVERS = ["localhost"]
-RUN_NUMBERS = 10
-CUT_OFF_TIME = 30000
+REMOTE_SERVERS = ["localhost","10.71.1.45"]
+RUN_NUMBERS = 20
+CUT_OFF_TIME = 100000
+LOGIN_THREAD = "Login 1-1"
+REPORT_FILE = "test_report"
 
 def clear_dir
   if File.directory? RESULT_DIR
@@ -44,8 +46,8 @@ def run_jmeter(jmx_file,result_file, threads)
   File.delete(result_fullpath) if File.exist? result_fullpath
 
   jmx_fullpath = File.join(JMETER_JMX_DIR,jmx_file)
-
-  pid = Process.spawn("#{JMETER_EXEC} -n -G #{JMETER_PROP} -t #{jmx_fullpath} -l #{result_fullpath} -Gthreads=#{threads} -Gloops=1 -R #{REMOTE_SERVERS.join(', ')}")
+  puts REMOTE_SERVERS.join(',')
+  pid = Process.spawn("#{JMETER_EXEC} -n -G #{JMETER_PROP} -t #{jmx_fullpath} -l #{result_fullpath} -Gthreads=#{threads} -Gloops=1 -R #{REMOTE_SERVERS.join(',')}")
   Process.wait(pid)
 end
 
@@ -56,10 +58,35 @@ def get_results(filename)
   results = {}
   doc.css('httpSample').each do |sample|
     sample = Sample.new(sample)
-    results[sample.label] = [] if results[sample.label].nil?
-    results[sample.label] << sample
+    results[sample.thread_name] = [] if results[sample.thread_name].nil?
+    results[sample.thread_name] << sample
   end
   results
+end
+
+def get_login_time(samples)
+login_samples = samples[LOGIN_THREAD]
+login_time = 0
+login_samples.each do |sample|
+  login_time += sample.elapsed_time
+end
+login_time
+end
+
+def write_report(results,report_file_name)
+report_fullpath = File.join(RESULT_DIR, report_file_name)
+File.delete(report_fullpath) if File.exist? report_fullpath
+File.open(report_fullpath, "w") do |f|
+results.each do |key,value| 
+f.printf "%20s\n", key
+f.printf "%15s %25s\n", "threads/users", "average resp time(ms)"
+value.each do |threads, resp_time|
+f.printf "%15s %25s\n", threads, resp_time
+end
+end
+end
+
+
 end
 
  clear_dir
@@ -71,15 +98,21 @@ end
   run_jmeter(jmx_file,user_case + threads.to_s, threads)
 
   samples = get_results(user_case + threads.to_s)
+#  login_time = get_login_time(samples)
   results[user_case] = {} if results[user_case].nil?
   total_time = 0
-  samples.each do |label,req_samples|
+  samples.each do |thread,req_samples|
+  if thread != LOGIN_THREAD
   req_samples.each do |sample|
   total_time +=sample.elapsed_time
   end
   end
-  results[user_case] = results[user_case].merge({threads => total_time / threads})
-  if total_time/threads > CUT_OFF_TIME
+  end
+#  average_resp_time = login_time + total_time/threads
+  average_resp_time = 0
+  average_resp_time = total_time/threads/REMOTE_SERVERS.size if REMOTE_SERVERS.size >0
+  results[user_case] = results[user_case].merge({threads => average_resp_time})
+  if average_resp_time > CUT_OFF_TIME || average_resp_time == 0
   break
   end
   #get_results("test#{threads}").each do |label, samples|
@@ -94,3 +127,4 @@ end
  p value
  end
  end
+ write_report(results,REPORT_FILE)
