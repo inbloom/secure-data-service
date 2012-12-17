914ac986205baf4fa17c59e043db4533e4090c31
Mark Abernathy <mabernathy@wgen.net>
2012-12-17 15:16:12 -0500

    Made Test Leaderboard more efficient when filtering by component

--- tools/quantum/app/controllers/features_controller.rb
+++ tools/quantum/app/controllers/features_controller.rb
@@ -4,13 +4,18 @@ class FeaturesController < ApplicationController
 		@features = []
 		start_time = 0 # Catch all... if date filter not specified, grab all events since 1970-01-01
 		start_time = Time.now - params[:time_filter].to_i unless (params[:time_filter] == nil || params[:time_filter] == "0")
-		Failure.all.distinct(:feature).each do |feature|
-			if params[:component_filter] != nil and !params[:component_filter].empty?
-				cur_component = Failure.where(feature: feature)[0][:component]
-				@features.push(:name => feature, :count => Failure.where(feature: feature, :timestamp.gte => start_time).count, :component => params[:component_filter]) if cur_component == params[:component_filter]
-			else
-				@features.push(:name => feature, :count => Failure.where(feature: feature, :timestamp.gte => start_time).count, :component => Failure.where(feature: feature)[0][:component])
+		feature_set = Set.new
+		if params[:component_filter] != nil and !params[:component_filter].empty?
+			@cur_component = params[:component_filter]
+			Failure.only(:feature, :component).where(:component => params[:component_filter]).each do |failure|
+				feature_set.add(failure[:feature])
 			end
+		else
+			feature_set = Failure.all.distinct(:feature)
+		end
+		
+		feature_set.each do |feature|
+			@features.push(:name => feature, :count => Failure.where(feature: feature, :timestamp.gte => start_time).count, :component => (@cur_component!=nil ? @cur_component : Failure.where(feature: feature)[0][:component]))
 		end
 		@features.sort!{|a,b| b[:count] <=> a[:count]} 
 
