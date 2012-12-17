82046e61dbaeb9000f0ab1df3d757f8e58d4f8ac
Mark Abernathy <mabernathy@wgen.net>
2012-12-17 11:32:39 -0500

    added filtering functionality, default landing page, other graphical improvements

--- tools/quantum/app/controllers/components_controller.rb
+++ tools/quantum/app/controllers/components_controller.rb
@@ -1,10 +1,13 @@
 class ComponentsController < ApplicationController
 
 	def index
+		start_time = 0 # Catch all... if date filter not specified, grab all events since 1970-01-01
+		start_time = Time.now - params[:time_filter].to_i unless (params[:time_filter] == nil || params[:time_filter] == "0")
+
 		@components = []
 		Failure.all.distinct(:component).each do |component|
-			totalCount = Failure.where(component: component).count
-			localCount = Failure.where(component: component, hostname: "local").count
+			totalCount = Failure.where(component: component, :timestamp.gte => start_time).count
+			localCount = Failure.where(component: component, :timestamp.gte => start_time, hostname: "local").count
 			@components.push(:name => component, :local => localCount, :ci => totalCount - localCount)
 		end
 		@components.sort!{|a,b| a[:name] <=> b[:name]} 
--- tools/quantum/app/controllers/health_controller.rb
+++ tools/quantum/app/controllers/health_controller.rb
@@ -2,8 +2,10 @@ class HealthController < ApplicationController
 
 	def index
 		@hosts = []
+		start_time = 0 # Catch all... if date filter not specified, grab all events since 1970-01-01
+		start_time = Time.now - params[:time_filter].to_i unless (params[:time_filter] == nil || params[:time_filter] == "0")
 		Failure.all.distinct(:hostname).each do |host|
-			@hosts.push(:name => host, :count => Failure.where(hostname: host).count) unless host == "local"
+			@hosts.push(:name => host, :count => Failure.where(hostname: host, :timestamp.gte => start_time).count) unless host == "local"
 		end
 		@hosts.sort!{|a,b| a[:name] <=> b[:name]} 
 
