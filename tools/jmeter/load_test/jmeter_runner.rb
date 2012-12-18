48cfdb6d9863e31b211b4d2f687f264326773c04
Joe Chung <joechung@wgen.net>
2012-12-18 09:51:53 -0500

    US4960 - refactored

--- tools/jmeter/load_test/jmeter_runner.rb
+++ tools/jmeter/load_test/jmeter_runner.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'json'
 
-API_LOAD_TEST_HOME = File.dirname(File.absolute_path(__FILE__))
 IGNORED_JMX_FILES = ["oauth.jmx"]
 IGNORED_REQUESTS = ["/api/oauth/sso", "simple-idp", "IDP Login HTTP Request", "/api/rest/saml/sso/post", "/api/oauth/token"]
 JTL_FILE_PREFIX = "test"
@@ -36,10 +35,10 @@ module ApiLoadTest
     def initialize(config = {})
       @remote = config[:remote] || false
       @remote_servers = config[:remote_servers]
-      @result_dir = config[:result_dir] || "#{API_LOAD_TEST_HOME}/result"
-      @jmeter_exec = config[:jmeter_exec] || "/opt/apache-jmeter/bin/jmeter"
+      @result_dir = config[:result_dir]
+      @jmeter_exec = config[:jmeter_exec]
       @max_avg_elapsed_time = config[:max_avg_elapsed_time] || 30000
-      @jmeter_prop = config[:jmeter_prop] || "#{API_LOAD_TEST_HOME.split('/')[0..-2].join('/')}/local.properties"
+      @jmeter_prop = config[:jmeter_prop]
     end
 
     def run_jmeter(jmx_file, jtl_file, thread_count)
@@ -137,6 +136,29 @@ module ApiLoadTest
       results
     end
 
+    def aggregate_multinode_test_result
+      aggregated_result = {}
+      Dir.foreach(@result_dir) do |result_folder|
+        unless [".", ".."].include?(result_folder)
+          path = File.join(@result_dir, result_folder)
+          if File.directory? path
+            test_name = path.split('/')[-1]
+            file = File.read(File.join(path, "result.json"))
+            json = JSON.parse(file)
+            json.each do |scenario, result|
+              aggregated_result[scenario] = {} if aggregated_result[scenario].nil?
+              categories = result.keys.sort! {|x,y| x.to_i <=> y.to_i}
+              categories.each do |category|
+                aggregated_result[scenario][category] = {} if aggregated_result[scenario][category].nil?
+                aggregated_result[scenario][category][test_name] = result[category]["total"]
+              end
+            end
+          end
+        end
+      end
+      aggregated_result
+    end
+
     def collect_all_errors(config_dir)
       errors = {}
       Dir.foreach(config_dir) do |file|
@@ -144,6 +166,7 @@ module ApiLoadTest
         if match_index = file =~ /[.]jmx$/
           scenario = file[0..(match_index - 1)]
           full_path = File.join(@result_dir, scenario)
+          #TODO: generalize error collection
           jtl_file = File.join(full_path, "#{JTL_FILE_PREFIX}1.jtl")
           errors[scenario] = get_errors(jtl_file)
         end
@@ -151,16 +174,5 @@ module ApiLoadTest
       errors
     end
 
-    def self.run(config = {})
-      runner = Runner.new(config)
-      config_home = config[:config_home] || "#{API_LOAD_TEST_HOME}/.."
-      result_json_file = config[:result_json_file] || "#{API_LOAD_TEST_HOME}/result/result.json"
-      max_threads = config[:max_threads] || 20
-
-      runner.collect_all_data(config_home, max_threads)
-      File.open(result_json_file, "w") do |f|
-        f.write(runner.aggregate_all_result().to_json)
-      end
-    end
   end
 end
