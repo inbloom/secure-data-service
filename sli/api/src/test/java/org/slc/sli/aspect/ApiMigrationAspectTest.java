66a1bccfbab6eb18af91bfc567b65a1043538bca
ecole <ecole@wgen.net>
2013-01-03 17:59:57 -0500

    [US5091] Added copyright

--- sli/api/src/test/java/org/slc/sli/aspect/ApiMigrationAspectTest.java
+++ sli/api/src/test/java/org/slc/sli/aspect/ApiMigrationAspectTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.slc.sli.aspect;
 
 import static org.junit.Assert.assertTrue;
@@ -20,6 +35,12 @@ import org.slc.sli.api.resources.generic.representation.ServiceResponse;
 import org.slc.sli.api.test.WebContextTestExecutionListener;
 import org.slc.sli.aspect.ApiMigrationAspect.MigratePostedEntity;
 import org.slc.sli.aspect.ApiMigrationAspect.MigrateResponse;
+
+/**
+ * Test for ApiMigrationAspect
+ *
+ * @author ecole
+ */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
