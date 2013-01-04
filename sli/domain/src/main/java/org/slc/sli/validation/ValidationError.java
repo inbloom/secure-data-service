53b785716e0327b4082bc01648853344d6eb3409
ablum <ablum@wgen.net>
2013-01-04 11:49:24 -0500

    Cherry pick DE2381 Add Self Referencing validation

--- sli/domain/src/main/java/org/slc/sli/validation/ValidationError.java
+++ sli/domain/src/main/java/org/slc/sli/validation/ValidationError.java
@@ -21,27 +21,27 @@ import java.util.Arrays;
 
 /**
  * Describes a validation rule violation.
- * 
+ *
  * @author Ryan Farris <rfarris@wgen.net>
- * 
+ *
  */
 public class ValidationError {
-    
+
     /**
      * Indicates the type of validation condition that was violated.
-     * 
+     *
      * @author Ryan Farris <rfarris@wgen.net>
-     * 
+     *
      */
     public enum ErrorType {
-        INVALID_DATE_FORMAT, REQUIRED_FIELD_MISSING, UNKNOWN_FIELD, ENUMERATION_MISMATCH, INVALID_DATATYPE, REFERENTIAL_INFO_MISSING, INVALID_VALUE, INVALID_CHOICE_VALUE, TOO_MANY_CHOICES, TOO_FEW_CHOICES, MISSING_CHOICE_TYPE, MISSING_CHOICE_VALUE, INVALID_CHOICE_TYPE, INVALID_VALUE_FOR_CHOICE, INVALID_TYPE_FOR_CHOICE
+        INVALID_DATE_FORMAT, REQUIRED_FIELD_MISSING, UNKNOWN_FIELD, ENUMERATION_MISMATCH, INVALID_DATATYPE, REFERENTIAL_INFO_MISSING, INVALID_VALUE, INVALID_CHOICE_VALUE, TOO_MANY_CHOICES, TOO_FEW_CHOICES, MISSING_CHOICE_TYPE, MISSING_CHOICE_VALUE, INVALID_CHOICE_TYPE, INVALID_VALUE_FOR_CHOICE, INVALID_TYPE_FOR_CHOICE, SELF_REFERENCING_DATA
     }
-    
+
     private final ErrorType type;
     private final String fieldName;
     private final Object fieldValue;
     private final String[] expectedTypes;
-    
+
     public ValidationError(ErrorType type, String fieldName, Object fieldValue, String[] expectedTypes) {
         this.type = type;
         this.fieldName = fieldName;
@@ -49,27 +49,27 @@ public class ValidationError {
         this.expectedTypes = expectedTypes == null ? new String[] {} : Arrays.copyOf(expectedTypes,
                 expectedTypes.length);
     }
-    
+
     public ErrorType getType() {
         return type;
     }
-    
+
     public String getFieldName() {
         return fieldName;
     }
-    
+
     public Object getFieldValue() {
         return fieldValue;
     }
-    
+
     public String[] getExpectedTypes() {
         return Arrays.copyOf(expectedTypes, expectedTypes.length);
     }
-    
+
     @Override
     public String toString() {
         return "ValidationError [type=" + type + ", fieldName=" + fieldName + ", fieldValue=" + fieldValue
                 + ", expectedTypes=" + Arrays.toString(expectedTypes) + "]";
     }
-    
+
 }
