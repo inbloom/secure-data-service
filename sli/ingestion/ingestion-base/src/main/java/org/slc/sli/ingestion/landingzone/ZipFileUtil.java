f5553b4142de1aaea428aa7ac81d01797f615802
John Hinsdale <jhinsdale@wgen.net>
2013-01-10 17:47:32 -0500

    Merge w/ master

--- sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/landingzone/ZipFileUtil.java
+++ sli/ingestion/ingestion-base/src/main/java/org/slc/sli/ingestion/landingzone/ZipFileUtil.java
@@ -29,9 +29,7 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import org.apache.commons.compress.archivers.ArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
-import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
