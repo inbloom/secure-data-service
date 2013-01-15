9f5b4c8ccae5f756d72c7f03d8727c10763fb317
pghosh <pghosh@wgen.net>
2013-01-15 14:21:46 -0500

    [us4214] added granular access criteria generator framework

---
+++ sli/api/src/main/java/org/slc/sli/api/criteriaGenerator/DateFilterCriteriaGenerator.java
@@ -0,0 +1,81 @@
+/*
+ *
+ *  * Copyright 2012 Shared Learning Collaborative, LLC
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.slc.sli.api.criteriaGenerator;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import org.slc.sli.domain.NeutralCriteria;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pghosh
+ * Date: 1/15/13
+ */
+
+@Component
+public class DateFilterCriteriaGenerator {
+    private ThreadLocal<GranularAccessFilter> granularAccessFilterStore = new ThreadLocal<GranularAccessFilter>();
+    public void generate(ContainerRequest request) {
+        //validate
+        //Extract date range using session
+        //Find appropriate entity to apply filter
+        //Use above two points to build criteria
+
+    }
+    private DateFilterCriteriaBuilder builder() {
+        return new DateFilterCriteriaBuilder();
+    }
+
+    private final class DateFilterCriteriaBuilder {
+        private String entityName;
+        private String beginDate;
+        private String endDate;
+        private String beginDateAttribute;
+        private String endDateAttribute;
+
+        public DateFilterCriteriaBuilder forEntity(String entityName) {
+            this.entityName = entityName;
+            return this;
+        }
+        public DateFilterCriteriaBuilder withAttributes(String beginDateAttribute, String endDateAttribute) {
+            this.beginDateAttribute = beginDateAttribute;
+            this.endDateAttribute = endDateAttribute;
+            return this;
+        }
+        public DateFilterCriteriaBuilder startingFrom(String beginDate) {
+            this.beginDate = beginDate;
+            return this;
+        }
+        public DateFilterCriteriaBuilder endingTo(String endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public void build() {
+            GranularAccessFilter granularAccessFilter = new GranularAccessFilter();
+            NeutralCriteria neutralCriteria = new NeutralCriteria("");
+            granularAccessFilter.setEntityName(entityName);
+            granularAccessFilter.setNeutralCriteria(neutralCriteria);
+            granularAccessFilterStore.set(granularAccessFilter);
+        }
+
+    }
+}
+
+
---
+++ sli/api/src/main/java/org/slc/sli/api/criteriaGenerator/GranularAccessFilter.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  * Copyright 2012 Shared Learning Collaborative, LLC
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.slc.sli.api.criteriaGenerator;
+
+import org.slc.sli.domain.NeutralCriteria;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pghosh
+ * Date: 1/15/13
+ */
+public class GranularAccessFilter {
+    private String entityName;
+    private NeutralCriteria neutralCriteria;
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public NeutralCriteria getNeutralCriteria() {
+        return neutralCriteria;
+    }
+
+    public void setNeutralCriteria(NeutralCriteria neutralCriteria) {
+        this.neutralCriteria = neutralCriteria;
+    }
+}
