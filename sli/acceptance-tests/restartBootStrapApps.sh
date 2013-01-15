a807f6b271d31be05e9840d905235bd6ab08917c
John Singh <jsingh@wgen.net>
2013-01-15 12:14:32 -0500

    US5078 - Fixed typo and added Odin placeholder

--- sli/acceptance-tests/restartBootStrapApps.sh
+++
@@ -1,4 +0,0 @@
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/api
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/ingestion-service
-curl http://tomcat:s3cret@localhost/manager/text/start?path=/api
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/ingestion-service
