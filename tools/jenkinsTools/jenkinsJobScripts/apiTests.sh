a48a5abbfac8244e428dd3ba950467e4594c1726
James Short <jshort@wgen.net>
2013-01-16 16:13:17 -0500

    new changes for jenkins scripts

--- tools/jenkinsTools/jenkinsJobScripts/apiTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/apiTests.sh
@@ -7,19 +7,11 @@ source "$ROOT/utils.sh"
 mongo --eval "db.adminCommand( { setParameter: 1, notablescan: false } )"
 /usr/sbin/cleanup_tomcat
 
-cd $WORKSPACE/sli
-sh profile_swap.sh $NODE_NAME
-cd config/scripts
-sh resetAllDbs.sh
-ruby webapp-provision.rb ../config.in/canonical_config.yml team /opt/tomcat/conf/sli.properties
-cp $WORKSPACE/sli/data-access/dal/keyStore/ci* /opt/tomcat/encryption/ 
-cp $WORKSPACE/sli/common/common-encrypt/trust/* /opt/tomcat/trust/
-cd ../../search-indexer
-scripts/local_search_indexer.sh restart target/search_indexer.tar.gz -Dsli.conf=/opt/tomcat/conf/sli.properties -Dsli.encryption.keyStore=/opt/tomcat/encryption/ciKeyStore.jks -Dlock.dir=data/
+resetDatabases
 
-curlStop api
-curlUndeploy api
-curlDeploy api "$WORKSPACE/sli/api/target/api.war"
+profileSwapAndPropGen
+
+startSearchIndexer
 
 cd $WORKSPACE/sli/acceptance-tests
 export LANG=en_US.UTF-8
