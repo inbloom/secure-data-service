54810e2fbcd83da2599a45e5f30ff99ddd4c7d4c
James Short <jshort@wgen.net>
2013-01-16 21:39:08 -0500

    smoke test script needs abs path

--- tools/jenkinsTools/jenkinsJobScripts/smokeTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/smokeTests.sh
@@ -16,7 +16,7 @@ processApps $APPSTODEPLOY
 
 Xvfb :4 -screen 0 1024x768x24 >/dev/null 2>&1 &
 export DISPLAY=:4.0
-cd sli/acceptance-tests
+cd $WORKSPACE/sli/acceptance-tests
 export LANG=en_US.UTF-8
 bundle install --deployment
 bundle exec rake FORCE_COLOR=true sampleApp_server_address=https://$NODE_NAME.slidev.org/ dashboard_server_address=https://$NODE_NAME.slidev.org dashboard_api_server_uri=https://$NODE_NAME.slidev.org realm_page_url=https://$NODE_NAME.slidev.org/api/oauth/authorize admintools_server_url=https://$NODE_NAME.slidev.org:2001 api_server_url=https://$NODE_NAME.slidev.org databrowser_server_url=https://$NODE_NAME.slidev.org:2000 ingestion_landing_zone=/home/ingestion/lz/inbound sif_zis_address_trigger=http://$NODE_NAME.slidev.org:8080/mock-zis/trigger TOGGLE_TABLESCANS=true smokeTests
