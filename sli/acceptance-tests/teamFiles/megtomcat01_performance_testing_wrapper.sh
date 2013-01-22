ed76cc21764684a41372dee58bc190e6321174f0
John Singh <jsingh@wgen.net>
2013-01-22 11:38:48 -0500

    US5129 - Added done line and removed tail

--- sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
@@ -4,4 +4,3 @@ now=$(date +"%Y_%m_%d")
 filename="/opt/megatron/sli/sli/acceptance-tests/teamFiles/megtomcat01_logs/ingestion_performance_$now.log"
 echo "Starting automated performance testing, logging to $filename"
 nohup sh megtomcat01_ingestion_performance.sh $1 > $filename &
-tail -f $filename
