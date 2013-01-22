5d7e7fd9016977fed7773561413191f19b04593f
John Singh <jsingh@wgen.net>
2013-01-22 12:02:26 -0500

    US5129 - Fixing script for jenkins take 3

--- sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-if [ -z "$1" ]; then
-    echo "Branch defaulted to master"
-    branch="master"
-else
-    branch=$1
-fi
 set -e
-sh checkoutAndBuild.sh $branch
+sh checkoutAndBuild.sh $1
 cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
 sh ingestDataset.sh 1
 sh ingestDataset.sh 2
