bfd6a43a27a6530debb3985b57ddd247c7b92d7a
John Singh <jsingh@wgen.net>
2013-01-22 11:56:08 -0500

    US5129 - Fixing script for jenkins take 2

--- sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
@@ -6,9 +6,11 @@ else
     branch=$1
 fi
 set -e
+cd /opt/megatron/sli/sli/
 git fetch
 git checkout $branch
 git pull
+cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
 now=$(date +"%Y_%m_%d")
 filename="/opt/megatron/sli/sli/acceptance-tests/teamFiles/megtomcat01_logs/ingestion_performance_$now.log"
 echo "Starting automated performance testing, logging to $filename"
