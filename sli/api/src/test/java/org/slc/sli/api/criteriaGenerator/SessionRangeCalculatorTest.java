f22fbabc486030f20ec8144f2b301b8a42ff7e02
Jeff Stokes <jstokes@wgen.net>
2013-01-22 10:24:10 -0500

    [US4214] Checkstyle fixes

--- sli/api/src/test/java/org/slc/sli/api/criteriaGenerator/SessionRangeCalculatorTest.java
+++ sli/api/src/test/java/org/slc/sli/api/criteriaGenerator/SessionRangeCalculatorTest.java
@@ -33,7 +33,12 @@ import org.slc.sli.domain.MongoEntity;
 import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.domain.QueryParseException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author: sashton
@@ -58,26 +63,23 @@ public class SessionRangeCalculatorTest {
     }
 
 
-
-
-
     @Test
     public void shouldCatchInvalidDateRange() {
-        
+
         List<String> invalidRanges = Arrays.asList("123-1234", "1234-123", "12345-1234", "1234-12345", "123A-1234",
                 "1234-123A", "12341234", "1234--1234");
-        
+
         for (String range : invalidRanges) {
             try {
                 initRepo();
                 calc.findDateRange(range);
                 Assert.fail("Did not catch an invalid range: " + range);
-                
+
             } catch (QueryParseException e) {
                 // expected to get here
             }
         }
-        
+
     }
 
     @Test
@@ -102,34 +104,34 @@ public class SessionRangeCalculatorTest {
         Assert.assertEquals("Should match", 0, result.getSessionIds().size());
     }
 
-    private void initRepo(){
+    private void initRepo() {
 
-        List<Pair<String,String>> dates = new ArrayList<Pair<String, String>>();
-        dates.add(new ImmutablePair<String, String>("2006-08-14","2006-12-11"));
-        dates.add(new ImmutablePair<String, String>("2007-01-11","2007-05-21"));
-        dates.add(new ImmutablePair<String, String>("2007-08-15","2007-12-12"));
-        dates.add(new ImmutablePair<String, String>("2009-01-12","2009-05-22"));
+        List<Pair<String, String>> dates = new ArrayList<Pair<String, String>>();
+        dates.add(new ImmutablePair<String, String>("2006-08-14", "2006-12-11"));
+        dates.add(new ImmutablePair<String, String>("2007-01-11", "2007-05-21"));
+        dates.add(new ImmutablePair<String, String>("2007-08-15", "2007-12-12"));
+        dates.add(new ImmutablePair<String, String>("2009-01-12", "2009-05-22"));
 
         initRepo(dates);
     }
 
 
-    private void initRepo(List<Pair<String,String>> sessionDates){
+    private void initRepo(List<Pair<String, String>> sessionDates) {
         List<Entity> sessions = new ArrayList<Entity>();
 
         int id = 1000;
 
-        for(Pair<String,String> pair : sessionDates){
+        for (Pair<String, String> pair : sessionDates) {
             String entityId = id + "";
             id += 1;
             Entity e = new MongoEntity("someEntity", entityId, new HashMap<String, Object>(), null);
-            e.getBody().put("beginDate",pair.getLeft());
-            e.getBody().put("endDate",pair.getRight());
+            e.getBody().put("beginDate", pair.getLeft());
+            e.getBody().put("endDate", pair.getRight());
             sessions.add(e);
         }
 
         Mockito.when(mockRepo.findAll(Mockito.eq(EntityNames.SESSION), Mockito.any(NeutralQuery.class))).thenReturn(sessions);
     }
 
-    
+
 }
