42e21d3e88ce78270ec95e32700287717f5efcdb
John Singh <jsingh@wgen.net>
2013-01-22 09:53:05 -0500

    US5129 - Added fail on exception and tail log

--- sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_performance_testing_wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -e
 now=$(date +"%Y_%m_%d")
 filename="/opt/megatron/sli/sli/acceptance-tests/teamFiles/megtomcat01_logs/ingestion_performance_$now.log"
 echo "Starting automated performance testing, logging to $filename"
 nohup sh megtomcat01_ingestion_performance.sh > $filename &
+tail -f $filename
