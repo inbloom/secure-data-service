a15a4b643a86d2201b8c0cf8985d2f3e16484283
John Singh <jsingh@wgen.net>
2013-01-22 21:18:36 -0500

    US5129 - Added duration script

--- sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
@@ -4,8 +4,10 @@ sh checkoutAndBuild.sh $1
 cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
 sh ingestDataset.sh 1
 sh ingestDataset.sh 2
+sh log_durations.sh
 sh resetEnvironment.sh
 sh ingestDataset.sh 1
 sh ingestDataset.sh 2
+sh log_durations.sh
 echo "Done!"
 
