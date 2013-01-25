95724a173919b819bad46989c8f154696df4c4bc
Lina Chen <lchen@wgen.net>
2013-01-25 14:29:13 -0500

    Hackathon update from email

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/ControlFileProcessor.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/processors/ControlFileProcessor.java
@@ -25,10 +25,9 @@ import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.mail.internet.AddressException;
-import javax.mail.MessagingException;
 import org.slc.sli.common.util.email.SendEmail;
 import org.slc.sli.common.util.tenantdb.TenantContext;
 import org.slc.sli.dal.aspect.MongoTrackingAspect;
@@ -81,6 +80,9 @@ public class ControlFileProcessor implements Processor {
 
     @Autowired
     private AbstractMessageReport databaseMessageReport;
+
+    @Value("${sli.ingestion.notify.fromEmailAddress}")
+    private String fromEmailAddress;
 
     @Override
     public void process(Exchange exchange) throws Exception {
@@ -263,9 +265,6 @@ public class ControlFileProcessor implements Processor {
     private void notifyStart(Exchange exchange, ControlFile cf, boolean goodData) {
     	String distro = (String) exchange.getIn().getHeader(AttributeType.EMAIL_NOTIFY.name());
 		if ( null != distro && !distro.isEmpty()) {
-			// TODO: take this from ingestion-specific property
-			String fromAddr = "ingestion-support@sli-fictitious-contractor.org";
-
 			String subject = "";
 			String body = "";
 
@@ -281,7 +280,7 @@ public class ControlFileProcessor implements Processor {
 	    	LOG.info("SENDING EMAIL to '" + distro + "':\n\nSubject: " + subject + "\n" + body);
 	    	SendEmail se = new SendEmail();
 	    	try {
-	    		se.sendMail(distro, fromAddr, subject, body);
+	    		se.sendMail(distro, fromEmailAddress, subject, body);
 	    	} catch( Exception ex ) {
 	    		LOG.warn("Failed sending Email to '" + distro + "'\n:" + ex.getMessage());
 	    	}
