23674b167fa040a5ba4dd588faa5d957f60a8b21
John Singh <jsingh@wgen.net>
2013-01-28 18:06:21 -0500

    US5129 - Added dataset as argument and to logging

--- sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
-sh checkoutAndBuild.sh $1
+sh checkoutAndBuild.sh $2
 cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
-sh ingestDataset.sh 1
-sh ingestDataset.sh 2
+sh ingestDataset.sh $1 1
+sh ingestDataset.sh $1 2
 sh log_durations.sh
 sh resetEnvironment.sh
-sh ingestDataset.sh 1
-sh ingestDataset.sh 2
-sh log_durations.sh
+sh ingestDataset.sh $1 1
+sh ingestDataset.sh $1 2
+sh log_durations.sh $1
 echo "Done!"
 
