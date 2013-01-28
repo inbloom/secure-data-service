605fd1fcc2e06b0a2439f3e42d45e9d682a8fa40
John Singh <jsingh@wgen.net>
2013-01-28 13:16:56 -0500

    US5129 - Added bundle install for opstools

--- sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 sh checkoutAndBuild.sh $1
+cd /opt/megatron/sli/sli/opstools
+bundle install --deployment
 cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
 sh ingestDataset.sh 1
 sh ingestDataset.sh 2
