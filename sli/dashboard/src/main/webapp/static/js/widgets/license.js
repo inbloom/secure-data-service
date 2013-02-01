d9f77b808fb3443aa5cac266eb123f347f9de6c4
Steve Ashton <sashton@wgen.net>
2013-02-01 12:40:37 -0500

    [us5281] Updated copyright headers

--- sli/dashboard/src/main/webapp/static/js/widgets/license.js
+++ sli/dashboard/src/main/webapp/static/js/widgets/license.js
@@ -1,5 +1,5 @@
 /*!
-* Copyright 2012 Shared Learning Collaborative, LLC
+* Copyright 2012-2013 inBloom, Inc. and its affiliates.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
