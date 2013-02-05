8b42c68a7bd20a8290a5c66a0cd8c94c6110078c
Shan Shan <sshan@wgen.net>
2013-02-05 09:50:10 -0500

    US4121  unit  test IndexValidatorExecutorTest.java

---
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/validation/indexes/IndexValidatorExecutorTest.java
@@ -0,0 +1,45 @@
+package org.slc.sli.ingestion.validation;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.slc.sli.ingestion.reporting.ReportStats;
+import org.slc.sli.ingestion.reporting.Source;
+import org.slc.sli.ingestion.reporting.impl.JobSource;
+import org.slc.sli.ingestion.reporting.impl.LoggingMessageReport;
+import org.slc.sli.ingestion.reporting.impl.SimpleReportStats;
+import org.slc.sli.ingestion.validation.IndexValidatorExecutor;
+import org.slc.sli.ingestion.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring/applicationContext-test.xml" })
+public class IndexValidatorExecutorTest {
+	
+    @Autowired
+    private IndexValidatorExecutor indexValidatorExecutor;
+    
+    @Test
+    public void testInit() throws Exception {
+    	
+        Validator<?> mockedSystemValidator = Mockito.mock(Validator.class);
+        LoggingMessageReport mockedLoggingMessageReport = Mockito.mock(LoggingMessageReport.class);
+        Source source = new JobSource(null);
+		ReportStats reportStats = new SimpleReportStats();
+		indexValidatorExecutor.setLoggingMessageReport(mockedLoggingMessageReport);
+        indexValidatorExecutor.setValidator(mockedSystemValidator);
+        Mockito.when(mockedSystemValidator.isValid(null, mockedLoggingMessageReport, reportStats, source)).thenReturn(false);
+        try{
+        	indexValidatorExecutor.init();
+        	fail("Indexes could not be verified, check the index file configurations are set");
+        } catch(RuntimeException e) {
+        	assertNotNull(e.getMessage());        
+        }
+//        Mockito.verify(mockedSystemValidator, times(1)).isValid(anyObject(), any(AbstractMessageReport.class), any(ReportStats.class), any(Source.class));      
+    }
+
+}
