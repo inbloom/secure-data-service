355bbc45a38bce6d7b249e8a26747dc4abf0bd6e
Riley Miller <rmiller@wgen.net>
2013-02-11 13:34:20 -0500

    Jmeter fixture data update: include mongo router refresh

--- tools/jmeter/odin-slirp/slirp-jmeter-realm.sh
+++ tools/jmeter/odin-slirp/slirp-jmeter-realm.sh
@@ -8,3 +8,6 @@ mongo 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a --quiet --eval "db.customRole.rem
 mongoimport -d 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a -c customRole --file customRole.json
 mongo sli --quiet --eval "db.realm.remove({}); assert.eq(null, db.getLastError());"
 mongoimport -d sli -c realm --file realm.json
+# flush router config
+mongo sli --quiet --eval "db.adminCommand('flushRouterConfig'); assert.eq(null, db.getLastError());"
+mongo 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a --quiet --eval "db.adminCommand('flushRouterConfig'); assert.eq(null, db.getLastError());"
