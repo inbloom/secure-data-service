326a317a7d29669538074735026443853aa3d277
Riley Miller <rmiller@wgen.net>
2013-02-13 16:17:18 -0500

    Uplift Jmeter Regression parameters

--- tools/jenkinsTools/jenkinsJobScripts/jmeterTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/jmeterTests.sh
@@ -39,7 +39,7 @@ bundle exec ruby $WORKSPACE/sli/opstools/migration/70ApplicationAuthorizationMig
 
 bundle exec rake runSearchBulkExtract
 #run the jmeter acceptance test wrapper with ci properties
-bundle exec rake FORCE_COLOR=true apiJMeterTests jmeter_jmx_path=../../tools/jmeter/ jmeter_bin=/opt/apache-jmeter-2.7/bin/jmeter jmeter_properties=ci.properties jmeter_regression_threshold=0.5
+bundle exec rake FORCE_COLOR=true apiJMeterTests jmeter_jmx_path=../../tools/jmeter/ jmeter_bin=/opt/apache-jmeter-2.7/bin/jmeter jmeter_properties=ci.properties jmeter_regression_threshold=0.25
 
 EXITCODE=$?
 
