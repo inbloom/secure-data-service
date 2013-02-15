934caf1534c864a4d6351788f71774eff05c6ee3
pghosh <pghosh@wgen.net>
2013-02-15 12:15:12 -0500

    [us4332] added uuid generatr

--- sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/ContainerDocumentAccessor.java
+++ sli/data-access/dal/src/main/java/org/slc/sli/dal/convert/ContainerDocumentAccessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 inBloom, Inc. and its affiliates.
+ * Copyright 2012-2013 inBloom, Inc. and its affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,16 @@ package org.slc.sli.dal.convert;
 import org.slc.sli.common.domain.ContainerDocument;
 import org.slc.sli.common.domain.ContainerDocumentHolder;
 import org.slc.sli.common.domain.NaturalKeyDescriptor;
+import org.slc.sli.common.util.uuid.DeterministicUUIDGeneratorStrategy;
+import org.slc.sli.common.util.uuid.UUIDGeneratorStrategy;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.MongoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author jstokes
@@ -31,11 +39,26 @@ public class ContainerDocumentAccessor {
     @Autowired
     private ContainerDocumentHolder containerDocumentHolder;
 
+//    @Autowired
+//    @Qualifier("deterministicUUIDGeneratorStrategy")
+//    private UUIDGeneratorStrategy uuidGeneratorStrategy;
+
+    @Autowired
+    DeterministicUUIDGeneratorStrategy deterministicUUIDGeneratorStrategy;
+
     public boolean isContainerDocument(final String entity) {
         return containerDocumentHolder.isContainerDocument(entity);
     }
 
-    public String createParentKey(final ContainerDocument containerDocument) {
-        final NaturalKeyDescriptor naturalKeyDescriptor = new NaturalKeyDescriptor(containerDocument.getParentNaturalKeyMap());
+    public String createParentKey(final  Entity entity) {
+        ContainerDocument containerDocument = containerDocumentHolder.getContainerDocument(entity.getType());
+        Map<String,String> parentKeyMap = containerDocument.getParentNaturalKeyMap();
+        Map<String, String> naturalKeyMap = new HashMap<String, String>();
+        for(String key : parentKeyMap.keySet()) {
+           String value = (String) entity.getBody().get(key);
+            naturalKeyMap.put(key,value);
+        }
+        final NaturalKeyDescriptor naturalKeyDescriptor = new NaturalKeyDescriptor(naturalKeyMap);
+       return deterministicUUIDGeneratorStrategy.generateId(naturalKeyDescriptor);
     }
 }
--- sli/data-access/dal/src/test/java/org/slc/sli/dal/convert/ContainerDocumentAccessorTest.java
+++ sli/data-access/dal/src/test/java/org/slc/sli/dal/convert/ContainerDocumentAccessorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2013 inBloom, Inc. and its affiliates.
+ * Copyright 2012-2013 inBloom, Inc. and its affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,15 +21,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.slc.sli.common.domain.ContainerDocument;
 import org.slc.sli.common.domain.ContainerDocumentHolder;
+import org.slc.sli.domain.MongoEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static org.mockito.Mockito.when;
 
 /**
@@ -61,8 +66,22 @@ public class ContainerDocumentAccessorTest {
 
     @Test
     public void testCreateParentKey() {
+        Map<String,String> parentKeyMap = new HashMap<String, String>();
+         parentKeyMap.put("studentId","studentId");
+        parentKeyMap.put("schoolId","studentId");
+                parentKeyMap.put("schoolYear","studentId");
         final ContainerDocument attendance = ContainerDocument.builder().forCollection("testCollection")
                 .forField("array_field")
-                .withParent(Collections.EMPTY_MAP).build();
+                .withParent(parentKeyMap).build();
+        Map<String, Object> body = new HashMap<String, Object>();
+        body.put("studentId","student1");
+        body.put("schoolId","school1");
+        body.put("schoolYear","schoolyear1");
+        MongoEntity entity = new MongoEntity("attendance", body);
+
+        when(mockHolder.getContainerDocument(Mockito.anyString())).thenReturn(attendance);
+        String parentUUid = testAccessor.createParentKey(entity);
+        System.out.print(parentUUid);
+        assertFalse(parentUUid.isEmpty());
     }
 }
