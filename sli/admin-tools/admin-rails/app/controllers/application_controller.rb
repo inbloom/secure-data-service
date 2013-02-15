76bdfd315c4af7284e166e979931f85cb97aab10
Jessica Wall <jwall@jwall-mac.local>
2013-02-15 09:40:49 -0500

    DE2517 Moved role constants and includes_*group? functions to top-level lib dir

--- sli/admin-tools/admin-rails/app/controllers/application_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/application_controller.rb
@@ -22,14 +22,6 @@ require "oauth_helper"
 
 class ApplicationController < ActionController::Base
 
-  SANDBOX_ADMINISTRATOR = "Sandbox Administrator"
-  APPLICATION_DEVELOPER = "Application Developer"
-  INGESTION_USER = "Ingestion User"
-  INBLOOM_OPERATOR = "SLC Operator" #TODO: inBloom Operator
-  SEA_ADMINISTRATOR = "SEA Administrator"
-  LEA_ADMINISTRATOR = "LEA Administrator"
-  REALM_ADMINISTRATOR = "Realm Administrator"
-  IT_ADMINISTRATOR = "IT Administrator"
 
   protect_from_forgery
   before_filter :handle_oauth
@@ -190,35 +182,4 @@ class ApplicationController < ActionController::Base
     session[:tenant_id] = get_tenant()
   end
 
-  # Check for user groups
-  def includes_sandbox_admin_group?(groups)
-    groups.include? SANDBOX_ADMINISTRATOR
-  end
-
-  def includes_app_developer_group?(groups)
-    groups.include? APPLICATION_DEVELOPER
-  end
-
-  def includes_ingestion_user_group?(groups)
-    groups.include? INGESTION_USER
-  end
-
-  def includes_operator_group?(groups)
-    groups.each do |group|
-      return true if group.include? "Operator"
-    end
-    return false
-  end
-
-  def includes_sea_admin_group?(groups)
-    groups.include? SEA_ADMINISTRATOR
-  end
-
-  def includes_lea_admin_group?(groups)
-    groups.include? LEA_ADMINISTRATOR
-  end
-
-  def includes_realm_admin_group?(groups)
-    groups.include? REALM_ADMINISTRATOR
-  end
 end
