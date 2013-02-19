1dbf09fd20e265c8644e5a9ea0516622b8ba350b
Dong Liu <dliu@wgen.net>
2013-02-19 15:38:51 -0500

    [US5392] revert poc implementation for assessment transformation

--- sli/api/src/main/java/org/slc/sli/api/resources/util/ResourceUtil.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/util/ResourceUtil.java
@@ -33,6 +33,11 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.slc.sli.api.config.AssociationDefinition;
 import org.slc.sli.api.config.EntityDefinition;
 import org.slc.sli.api.config.EntityDefinitionStore;
@@ -45,18 +50,14 @@ import org.slc.sli.api.security.SLIPrincipal;
 import org.slc.sli.validation.schema.ListSchema;
 import org.slc.sli.validation.schema.NeutralSchema;
 import org.slc.sli.validation.schema.ReferenceSchema;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * Performs tasks common to both Resource and HomeResource to eliminate code-duplication. These
  * tasks include creating a list of embedded links, adding links to a list regarding associations,
  * and resolving a new URI based on parameters.
- * 
+ *
  * @author kmyers <kmyers@wgen.net>
- * 
+ *
  */
 public class ResourceUtil {
 
@@ -103,7 +104,7 @@ public class ResourceUtil {
 
     /**
      * Extracts the API version from the provided URI info.
-     * 
+     *
      * @param uriInfo
      *            URI requested by user (version is part of the URI)
      * @return version referenced by UriInfo
@@ -128,7 +129,7 @@ public class ResourceUtil {
     /**
      * Creates a new LinkedList and adds a link for self, then returns that list. When not creating
      * a self link, all other parameters can be null.
-     * 
+     *
      * @param uriInfo
      *            base URI
      * @param userId
@@ -157,7 +158,7 @@ public class ResourceUtil {
 
     /**
      * Create a self link
-     * 
+     *
      * @param uriInfo
      *            base URI
      * @param entityId
@@ -174,7 +175,7 @@ public class ResourceUtil {
 
     /**
      * Create the custom entity link
-     * 
+     *
      * @param uriInfo
      *            base uri
      * @param entityId
@@ -191,7 +192,7 @@ public class ResourceUtil {
     /**
      * Looks up associations for the given entity (definition) and adds embedded links for each
      * association for the given user ID.
-     * 
+     *
      * @param entityDefs
      *            all entity definitions
      * @param defn
@@ -237,7 +238,7 @@ public class ResourceUtil {
     /**
      * Looks up associations for the given entity (definition) and adds embedded links for each
      * association for the given user ID.
-     * 
+     *
      * @param entityDefs
      *            all entity definitions
      * @param defn
@@ -354,11 +355,8 @@ public class ResourceUtil {
             // see what GUID is stored in the reference field
             String key = referenceField.getKey();
             if (key.contains(".")) {
-                List<EmbeddedLink> embeddedLinks = getEmbeddedReferences(key.split("\\."), referenceField.getValue(),
-                        defn, entityBody, uriInfo, defnStore);
-                if (embeddedLinks != null) {
-                    links.addAll(embeddedLinks);
-                }
+                links.addAll(getEmbeddedReferences(key.split("\\."), referenceField.getValue(), defn, entityBody,
+                        uriInfo, defnStore));
             } else {
                 List<String> guidList = entityBody.getValues(key);
                 int count = guidList.size();
@@ -393,12 +391,8 @@ public class ResourceUtil {
 
     private static List<EmbeddedLink> getEmbeddedReferences(String[] keys, ReferenceSchema ref, EntityDefinition defn,
             Map<String, Object> entityBody, UriInfo uri, EntityDefinitionStore defnStore) {
-        EntityDefinition df = defnStore.lookupByEntityType(ref.getEntityType());
-        if (df != null) {
-            return getEmbeddedReferences("", Arrays.asList(keys), defnStore.lookupByEntityType(ref.getEntityType())
-                    .getResourceName(), defn.getSchema(), entityBody, uri);
-        }
-        return null;
+        return getEmbeddedReferences("", Arrays.asList(keys), defnStore.lookupByEntityType(ref.getEntityType())
+                .getResourceName(), defn.getSchema(), entityBody, uri);
     }
 
     private static List<EmbeddedLink> getEmbeddedReferences(String prefix, List<String> keys, String resourceName,
@@ -442,10 +436,7 @@ public class ResourceUtil {
         for (Entry<String, NeutralSchema> fieldEntry : schema.getFields().entrySet()) {
             NeutralSchema field = fieldEntry.getValue();
             if (field.getAppInfo() != null && field.getAppInfo().isNaturalKey()) {
-                Object value = object.get(fieldEntry.getKey());
-                if (value != null) {
-                    return value.toString();
-                }
+                return object.get(fieldEntry.getKey()).toString();
             }
         }
         return "";
@@ -453,7 +444,7 @@ public class ResourceUtil {
 
     /**
      * Returns the URI for aggregations
-     * 
+     *
      * @param uriInfo
      *            The base URI
      * @return A list of links pointing to the base Url for aggregations
@@ -470,7 +461,7 @@ public class ResourceUtil {
     /**
      * Adds the value to a list and then puts the list into the query parameters associated to the
      * given key.
-     * 
+     *
      * @param queryParameters
      *            where to put the value once added to a list
      * @param key
@@ -487,7 +478,7 @@ public class ResourceUtil {
     /**
      * Adds the value to a list and then puts the list into the query parameters associated to the
      * given key.
-     * 
+     *
      * @param queryParameters
      *            where to put the value once added to a list
      * @param key
@@ -501,7 +492,7 @@ public class ResourceUtil {
 
     /**
      * Helper method to convert MultivaluedMap to a Map
-     * 
+     *
      * @param map
      * @return
      */
@@ -519,7 +510,7 @@ public class ResourceUtil {
 
     /**
      * Returns a URI based on the supplied URI with the paths appended to the base URI.
-     * 
+     *
      * @param uriInfo
      *            URI of current actions
      * @param paths
@@ -552,7 +543,7 @@ public class ResourceUtil {
 
     /**
      * Analyzes security context to get SLIPrincipal for user.
-     * 
+     *
      * @return SLIPrincipal from security context
      */
     public static SLIPrincipal getSLIPrincipalFromSecurityContext() {
@@ -570,7 +561,7 @@ public class ResourceUtil {
 
     /**
      * Finds the link name based on the entity type and the reference entity name
-     * 
+     *
      * @param resourceName
      *            Entity name for which the links are generated
      * @param referenceName
