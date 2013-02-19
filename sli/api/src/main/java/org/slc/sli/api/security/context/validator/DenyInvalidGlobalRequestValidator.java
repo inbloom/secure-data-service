f2240b26698b86c70d1abc5cc5754090c8c0cf4f
Mark Abernathy <mabernathy@wgen.net>
2013-02-19 09:03:21 -0500

    Add global context and write validation for course offering

--- sli/api/src/main/java/org/slc/sli/api/security/context/validator/DenyInvalidGlobalRequestValidator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/validator/DenyInvalidGlobalRequestValidator.java
@@ -1,17 +1,30 @@
 package org.slc.sli.api.security.context.validator;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+
+import org.slc.sli.api.constants.EntityNames;
 
 /**
  * This class is to catch any exceptional request that got through the global
- * entity validator filters
+ * entity validator filters. These include such requests that go through
+ * entities with no context (Assessment, Learning Objective, Learning Standard)
+ * that resolve to a non-global entity, or other global entities which currently
+ * do not have endpoints on them that resolve to non-global entities
  * 
  */
 public class DenyInvalidGlobalRequestValidator extends AbstractContextValidator {
 
+	protected static final Set<String> OTHER_INVALID_RESOURCES = new HashSet<String>(
+			Arrays.asList(
+					EntityNames.COURSE,
+					EntityNames.COURSE_OFFERING));
+
 	@Override
 	public boolean canValidate(String entityType, boolean isTransitive) {
-		return !isTransitive && isGlobalWrite(entityType);
+		return !isTransitive
+				&& (isGlobalWrite(entityType) || isOtherInvalidNonTransitiveValidator(entityType));
 	}
 
 	@Override
@@ -23,6 +36,10 @@ public class DenyInvalidGlobalRequestValidator extends AbstractContextValidator 
 		 */
 		throw new IllegalStateException(
 				"The non-transitive validator for Global Write entities should never be called");
+	}
+
+	private boolean isOtherInvalidNonTransitiveValidator(String entityType) {
+		return OTHER_INVALID_RESOURCES.contains(entityType);
 	}
 
 }
---
+++ sli/api/src/main/java/org/slc/sli/api/security/context/validator/GenericToGlobalCourseOfferingWriteValidator.java
@@ -0,0 +1,38 @@
+package org.slc.sli.api.security.context.validator;
+
+import java.util.Set;
+
+import org.slc.sli.api.constants.EntityNames;
+import org.slc.sli.domain.NeutralCriteria;
+import org.slc.sli.domain.NeutralQuery;
+
+public class GenericToGlobalCourseOfferingWriteValidator extends
+		AbstractContextValidator {
+
+	@Override
+	public boolean canValidate(String entityType, boolean isTransitive) {
+		return isTransitive && EntityNames.COURSE_OFFERING.equals(entityType);
+	}
+
+	@Override
+	public boolean validate(String entityType, Set<String> ids)
+			throws IllegalStateException {
+		if (!areParametersValid(EntityNames.COURSE_OFFERING, entityType, ids)) {
+			return false;
+		}
+
+		/*
+		 * Grab all the Courses that are being requested AND contain a reference
+		 * to a school in your edorg hierarchy.  Counts should be equal if
+		 * you can see all the courses you asked for
+		 */
+		Set<String> edOrgLineage = getEdorgLineage(getDirectEdorgs());
+		NeutralQuery query = new NeutralQuery(new NeutralCriteria("_id",
+				NeutralCriteria.CRITERIA_IN, ids, false));
+		query.addCriteria(new NeutralCriteria("schoolId",
+				NeutralCriteria.CRITERIA_IN, edOrgLineage));
+
+		return ids.size() == repo.count(entityType, query);
+	}
+
+}
