a3472321c3e9f96aa3a667d0a7b57153ad1c7078
pghosh <pghosh@wgen.net>
2013-02-21 22:25:35 -0500

    [us4332] added support for updating multiple files in subdirectory

--- sli/acceptance-tests/migration/attendance_interchange_migration.rb
+++ sli/acceptance-tests/migration/attendance_interchange_migration.rb
@@ -5,21 +5,25 @@ include REXML
 if(ARGV[0].nil?)
   dir = Dir.pwd
 else
-dir = ARGV[0]
+  dir = ARGV[0]
 end
-file_name = dir.to_s + "//InterchangeAttendance.xml"
+file_to_search = dir.to_s + "/**/InterchangeAttendance.xml"
+Dir[file_to_search].each do |file_name|
 
-file = File.new(file_name)
-doc = REXML::Document.new file
+  file = File.new(file_name)
+  doc = REXML::Document.new file
 
-subdoc = '<schoolYear>2011-2012</schoolYear>'
-subdoc = REXML::Document.new(p)
-doc.root.each_element('//AttendanceEvent//EventDate') do |e|
-  school_year = Element.new('SchoolYear')
-  date = Date.parse(e.text)
-  school_year_text = date.year.to_s + "-" +date.next_year.year.to_s
-  school_year.add_text(school_year_text)
-  e.previous_sibling = school_year
-end 
-puts doc
+  subdoc = '<schoolYear>2011-2012</schoolYear>'
+  subdoc = REXML::Document.new(p)
+  doc.root.each_element('//AttendanceEvent//EventDate') do |e|
+    school_year = Element.new('SchoolYear')
+    date = Date.parse(e.text)
+    school_year_text = date.year.to_s + "-" +date.next_year.year.to_s
+    school_year.add_text(school_year_text)
+    e.previous_sibling = school_year
+  end 
+  formatter = REXML::Formatters::Pretty.new
+  formatter.compact = true
+  File.open(file_name, 'w') {|f| formatter.write(doc, f)}
+end
 
