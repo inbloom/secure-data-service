616002923b5d36a5bc3b66a94436b13a04c1a331
pghosh <pghosh@wgen.net>
2013-02-22 09:30:44 -0500

    [us4332] calculate school year with slightly betterlogic

--- sli/acceptance-tests/migration/attendance_interchange_migration.rb
+++ sli/acceptance-tests/migration/attendance_interchange_migration.rb
@@ -16,7 +16,12 @@ Dir[file_to_search].each do |file_name|
   doc.root.each_element('//AttendanceEvent//EventDate') do |e|
     school_year = Element.new('SchoolYear')
     date = Date.parse(e.text)
-    school_year_text = date.year.to_s + "-" +date.next_year.year.to_s
+    school_year_text = String.new
+    if (date.month < 9) 
+      school_year_text = date.prev_year.year.to_s + "-" +date.year.to_s
+    else
+      school_year_text = date.year.to_s + "-" +date.next_year.year.to_s
+    end
     school_year.add_text(school_year_text)
     e.previous_sibling = school_year
   end 
