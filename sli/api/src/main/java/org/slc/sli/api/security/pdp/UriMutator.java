3c5af5187da9d9297662aeaf3d8a3a663e107263
pghosh <pghosh@wgen.net>
2013-02-25 09:58:56 -0500

    [de2663] query param should be objectiveId.learningObjectiveId for studentCompetencies

--- sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
@@ -166,7 +166,7 @@ public class UriMutator {
             put(joinPathSegments(PathConstants.SESSIONS, PathConstants.SECTIONS),
                     new MutateInfo("/sections/%s/", "sessionId"));
             put(joinPathSegments(PathConstants.LEARNING_OBJECTIVES, PathConstants.STUDENT_COMPETENCIES),
-                    new MutateInfo("/sections/%s/studentSectionAssociations/studentCompetencies", "learningObjectiveId"));
+                    new MutateInfo("/sections/%s/studentSectionAssociations/studentCompetencies", "objectiveId.learningObjectiveId"));
             put(joinPathSegments(PathConstants.GRADING_PERIODS, PathConstants.REPORT_CARDS),
                     new MutateInfo("/sections/%s/studentSectionAssociations/students/reportCards", "gradingPeriodId"));
             put(joinPathSegments(PathConstants.GRADING_PERIODS, PathConstants.GRADES),
@@ -393,7 +393,7 @@ public class UriMutator {
                 mutated.setPath(String.format(
                         "/schools/%s/sections/studentSectionAssociations/studentCompetencies",
                         StringUtils.join(edOrgHelper.getFilteredDirectEdorgs(user), ",")));
-                mutated.setQueryParameters(mutuateQueryParameterString("learningObjectiveId", transitiveEntityId, mutated.getQueryParameters()));
+                mutated.setQueryParameters(mutuateQueryParameterString("objectiveId.learningObjectiveId", transitiveEntityId, mutated.getQueryParameters()));
             }
         }
         return mutated;
