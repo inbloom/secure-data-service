d3f2a32de1006eb43fe00788fd7255faf47d3315
Jerry Hung <chung@wgen.net>
2013-03-09 11:58:03 -0500

    Revert "Revert US5389 merge and commits"
    This reverts commit 750206066f081886d9355a6f64b9aa9328a689d1.

--- sli/common/sli-common/src/main/java/org/slc/sli/api/constants/PathConstants.java
+++ sli/common/sli-common/src/main/java/org/slc/sli/api/constants/PathConstants.java
@@ -49,6 +49,7 @@ public class PathConstants {
     public static final String PARENT_LEARNING_OBJECTIVES = "parentLearningObjectives";
     public static final String CHILD_LEARNING_OBJECTIVES = "childLearningObjectives";
     public static final String ATTENDANCES = "attendances";
+    public static final String YEARLY_ATTENDANCES = "yearlyAttendances";
     public static final String COHORTS = "cohorts";
     public static final String COMPETENCY_LEVEL_DESCRIPTORS = "competencyLevelDescriptor";
     public static final String COURSES = "courses";
@@ -151,6 +152,7 @@ public class PathConstants {
         TEMP_MAP.put(ResourceNames.TEACHERS, TEACHERS);
         TEMP_MAP.put(ResourceNames.GRADES, GRADES);
         TEMP_MAP.put(ResourceNames.GRADUATION_PLANS, GRADUATION_PLANS);
+        TEMP_MAP.put(ResourceNames.YEARLY_ATTENDANCES, YEARLY_ATTENDANCES);
     }
 
     public static final String V1 = "v1";
