764efcbb5fb4a32b8a65fe7fe6b0729de3840e21
Riley Miller <rmiller@wgen.net>
2013-03-11 07:20:24 -0400

    Remove clean database from apiOdinTests.sh

--- tools/jenkinsTools/jenkinsJobScripts/apiOdinTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/apiOdinTests.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-noTableScanAndCleanTomcat
-
-resetDatabases
-
-profileSwapAndPropGen
-
-startSearchIndexer
-
-processApps $APPSTODEPLOY
-
 # Generate and Ingest Odin data
-cd $WORKSPACE/sli/acceptance-tests
-export LANG=en_US.UTF-8
-bundle install --deployment
 bundle exec rake FORCE_COLOR=true api_server_url=https://$NODE_NAME.slidev.org apiSuperAssessmentTests TOGGLE_TABLESCANS=true
 
 EXITCODE=$?
