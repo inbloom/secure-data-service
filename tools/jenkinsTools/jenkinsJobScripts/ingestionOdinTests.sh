050ec967966fa4ad64124f2f3c850313dc3abb2d
Dong Liu <dliu@wgen.net>
2013-03-12 13:12:13 -0400

    US5221 update api odin test script

--- tools/jenkinsTools/jenkinsJobScripts/ingestionOdinTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/ingestionOdinTests.sh
@@ -16,7 +16,7 @@ export DISPLAY=:6.0
 cd $WORKSPACE/sli/acceptance-tests
 export LANG=en_US.UTF-8
 bundle install --deployment
-bundle exec rake FORCE_COLOR=true ingestion_log_directory=/home/ingestion/logs ingestion_landing_zone=/home/ingestion/lz/inbound ingestion_healthcheck_url=https://$NODE_NAME.slidev.org/ingestion-service/healthcheck apiOdinTests TOGGLE_TABLESCANS=false
+bundle exec rake FORCE_COLOR=true api_server_url=https://$NODE_NAME.slidev.org ingestion_log_directory=/home/ingestion/logs ingestion_landing_zone=/home/ingestion/lz/inbound ingestion_healthcheck_url=https://$NODE_NAME.slidev.org/ingestion-service/healthcheck apiOdinTests TOGGLE_TABLESCANS=false
 
 EXITCODE=$?
 
