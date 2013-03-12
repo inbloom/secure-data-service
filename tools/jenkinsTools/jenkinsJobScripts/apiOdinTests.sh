db6d3bccd87450a9a4e44d4d2294ca586ae1b6b9
Yang Cao <ycao@wgen.net>
2013-03-12 10:01:05 -0400

    Revert "start search indexer before start apiOdin job"
    This reverts commit 92829b4bd07dc4a428f25aa4db43df457c618521.

--- tools/jenkinsTools/jenkinsJobScripts/apiOdinTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/apiOdinTests.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-startSearchIndexer
-
-processApps $APPSTODEPLOY
-
 # Generate and Ingest Odin data
 bundle exec rake FORCE_COLOR=true api_server_url=https://$NODE_NAME.slidev.org apiSuperAssessmentTests TOGGLE_TABLESCANS=true
 
