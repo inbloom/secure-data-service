60f69e1606804f772f403eb9c5618e91b4f4e764
ablum <ablum@wgen.net>
2013-03-14 16:50:29 -0400

    US5525 Added UT, and simplified spring configuration

---
+++ sli/bulk-extract/src/test/java/org/slc/sli/bulk/extract/zip/OutstreamZipFileTest.java
@@ -0,0 +1,57 @@
+package org.slc.sli.bulk.extract.zip;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+public class OutstreamZipFileTest {
+    private static final String TESTPATH = "src/test/resources/zip/";
+    
+    @Test
+    public void testOutstreamZipFile() {
+        File tmpFile = null;
+        try {
+            OutstreamZipFile zip = new OutstreamZipFile(TESTPATH, "test");
+            
+            tmpFile = new File(TESTPATH + "test_tmp.zip");
+            Assert.assertTrue(tmpFile.exists());
+            Assert.assertTrue(!tmpFile.isDirectory());
+            
+        } catch (IOException e) {
+            Assert.fail();
+        } finally {
+            FileUtils.deleteQuietly(tmpFile);
+        }
+    }
+    
+    @Test
+    public void testCreateAndWriteArchiveEntry() {
+        OutstreamZipFile zip = null;
+        File unzipDirectory = null;
+        try {
+            zip = new OutstreamZipFile(TESTPATH, "test");
+            
+            zip.createArchiveEntry("testArchive.json");
+            zip.writeData("testArchive");
+            zip.renameTempZipFile();
+            
+            unzipDirectory = new File(TESTPATH + "unzip/test");
+            if (unzipDirectory.exists()) {
+                FileUtils.deleteQuietly(unzipDirectory);
+            }
+            ZipFileUtil.extract(zip.getZipFile());
+            File archivedFile = new File(TESTPATH + "unzip/test/testArchive.json");
+            Assert.assertTrue(archivedFile.exists());
+        } catch (IOException e) {
+            Assert.fail();
+        } finally {
+            FileUtils.deleteQuietly(zip.getZipFile());
+            FileUtils.deleteQuietly(unzipDirectory);
+        }
+    }
+    
+}
