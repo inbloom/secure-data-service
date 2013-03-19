8a9ca05995ed99fcc0408ef320f1215f206f45f8
Tao Ke <tke@wgen.net>
2013-03-19 13:04:20 -0400

    US5525: changed to better name so that it doesn't get ignored

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/EntityExtractor.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/EntityExtractor.java
@@ -30,7 +30,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.slc.sli.bulk.extract.metadata.MetaData;
+import org.slc.sli.bulk.extract.metadata.DataFile;
 import org.slc.sli.bulk.extract.zip.OutstreamZipFile;
 import org.slc.sli.common.util.tenantdb.TenantContext;
 import org.slc.sli.dal.repository.connection.TenantAwareMongoDbFactory;
@@ -85,7 +85,7 @@ public class EntityExtractor implements Extractor {
 
     private BulkExtractMongoDA bulkExtractMongoDA;
 
-    private MetaData metaData;
+    private DataFile metaData;
 
     @Override
     public void destroy() {
@@ -330,14 +330,14 @@ public class EntityExtractor implements Extractor {
     /**
      * @return the metaData
      */
-    public MetaData getMetaData() {
+    public DataFile getMetaData() {
         return metaData;
     }
 
     /**
      * @param metaData the metaData to set
      */
-    public void setMetaData(MetaData metaData) {
+    public void setMetaData(DataFile metaData) {
         this.metaData = metaData;
     }
 
