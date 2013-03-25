729da3e007614f86dde3676bb42263c1f5ffd7d2
Tony Requist <trequist@wgen.net>
2013-03-25 10:20:58 -0600

    SLIRP scripts

--- sli/config/scripts/ingestion/start_ingest.sh
+++ sli/config/scripts/ingestion/start_ingest.sh
@@ -4,7 +4,7 @@
 #
 #set -x
 
-if [ $# -eq 0 -o $# -gt 5 ] ;
+if [ $# -eq 0 -o $# -gt 7 ] ;
 then
   echo "Usage: start_ingest FILE [FILE2] [FILE3] [FILE4] [FILE5]"
   exit 1
@@ -17,51 +17,26 @@ if [ -z "$PUBSCRIPT" ] ; then
   PUBSCRIPT=/opt/sli/bin/publish_file_uploaded.rb
 fi
 
-echo "******************************************************************************"
-echo "**  Hyrule-NYC - Starting SLIRP ingestion of $FILE_PATH at `date`"
-echo "******************************************************************************"
+TENANTS[1]="Hyrule-NYC"
+TENANTS[2]="Midgar-DAYBREAK"
+TENANTS[3]="Tenant_1-State"
+TENANTS[4]="Tenant_2-State"
+TENANTS[5]="Tenant_3-State"
+TENANTS[6]="Tenant_4-State"
+TENANTS[7]="Tenant_5-State"
 
-FILE_PATH=$1
-FILE=${FILE_PATH##*/}
-cp $FILE_PATH $LZ/inbound/Hyrule-NYC
-$PUBSCRIPT STOR $LZ/inbound/Hyrule-NYC/$FILE localhost
-
-if [ -n "$2" ] ; then
-  echo "******************************************************************************"
-  echo "**  Midgar-DAYBREAK - Starting SLIRP ingestion of $FILE_PATH at `date`"
-  echo "******************************************************************************"
-  FILE_PATH=$2
-  FILE=${FILE_PATH##*/}
-  cp $FILE_PATH $LZ/inbound/Midgar-DAYBREAK
-  $PUBSCRIPT STOR $LZ/inbound/Midgar-DAYBREAK/$FILE localhost
-fi
-
-if [ -n "$3" ] ; then
-  echo "******************************************************************************"
-  echo "**  Tenant_1-State - Starting SLIRP ingestion of $FILE_PATH at `date`"
-  echo "******************************************************************************"
-  FILE_PATH=$3
-  FILE=${FILE_PATH##*/}
-  cp $FILE_PATH $LZ/inbound/Tenant_1-State
-  $PUBSCRIPT STOR $LZ/inbound/Tenant_1-State/$FILE localhost
-fi
-
-if [ -n "$4" ] ; then
-  echo "******************************************************************************"
-  echo "**  Tenant_2-State - Starting SLIRP ingestion of $FILE_PATH at `date`"
-  echo "******************************************************************************"
-  FILE_PATH=$4
-  FILE=${FILE_PATH##*/}
-  cp $FILE_PATH $LZ/inbound/Tenant_2-State
-  $PUBSCRIPT STOR $LZ/inbound/Tenant_2-State/$FILE localhost
-fi
-
-if [ -n "$5" ] ; then
-  echo "******************************************************************************"
-  echo "**  Tenant_3-State - Starting SLIRP ingestion of $FILE_PATH at `date`"
-  echo "******************************************************************************"
-  FILE_PATH=$5
-  FILE=${FILE_PATH##*/}
-  cp $FILE_PATH $LZ/inbound/Tenant_3-State
-  $PUBSCRIPT STOR $LZ/inbound/Tenant_3-State/$FILE localhost
-fi
+NUM=1
+while [ $# -gt 0 ] ; do
+  FILE_PATH=$1
+  if [ "$FILE_PATH" != "null" ] ; then
+    TENANT=${TENANTS[$NUM]}
+    echo "******************************************************************************"
+    echo "**  $TENANT - Starting SLIRP ingestion of $FILE_PATH at `date`"
+    echo "******************************************************************************"
+    FILE=${FILE_PATH##*/}
+    cp $FILE_PATH $LZ/inbound/$TENANT
+    $PUBSCRIPT STOR $LZ/inbound/$TENANT/$FILE localhost
+  fi
+  let "NUM=$NUM+1"
+  shift
+done
