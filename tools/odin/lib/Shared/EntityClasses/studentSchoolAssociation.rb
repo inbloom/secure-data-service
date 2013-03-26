c61f8ad4e815a2f8ee4b67c86de249c55153a663
Aaron Saarela <asaarela@wgen.net>
2013-03-26 10:35:10 -0400

    Skip school year enum that is missing in ed-fi 1.0

--- tools/odin/lib/Shared/EntityClasses/studentSchoolAssociation.rb
+++ tools/odin/lib/Shared/EntityClasses/studentSchoolAssociation.rb
@@ -45,8 +45,8 @@ class StudentSchoolAssociation < BaseEntity
        "2017-2018",
        "2018-2019",
        "2019-2020",
-       "2020-2021",
-       "2021-2022"])
+       "2021-2022",
+       "2022-2023"])
      }
 
     optional {@entryType = choose([
