ad2c0288d48dc22ca7bea86e2b3a81944f626251
Matthew Lane <lanemt@gmail.com>
2013-03-28 13:28:00 -0400

    adds encryptData method

--- sli/api/src/main/java/org/slc/sli/api/resources/BulkExtract.java
+++ sli/api/src/main/java/org/slc/sli/api/resources/BulkExtract.java
@@ -16,28 +16,6 @@
 
 package org.slc.sli.api.resources;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Date;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.StreamingOutput;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-
 import org.slc.sli.api.security.RightsAllowed;
 import org.slc.sli.api.security.SLIPrincipal;
 import org.slc.sli.domain.Entity;
@@ -45,6 +23,33 @@ import org.slc.sli.domain.NeutralCriteria;
 import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.domain.Repository;
 import org.slc.sli.domain.enums.Right;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.Date;
 
 /**
  *
@@ -126,6 +131,28 @@ public class BulkExtract {
         initializePrincipal();
         NeutralQuery tenantQuery = new NeutralQuery(new NeutralCriteria("tenantId", NeutralCriteria.OPERATOR_EQUAL, principal.getTenantId()));
         return mongoEntityRepository.findOne(BULK_EXTRACT_FILES, tenantQuery);
+    }
+
+    private byte[] encryptData(PublicKey publicKey, byte[] rawData) {
+        byte[] encryptedData = null;
+
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            encryptedData = cipher.doFinal(rawData);
+        } catch (NoSuchAlgorithmException e) {
+            LOG.error("Exception: NoSuchAlgorithmException {}", e);
+        } catch (NoSuchPaddingException e) {
+            LOG.error("Exception: NoSuchPaddingException {}", e);
+        } catch (InvalidKeyException e) {
+            LOG.error("Exception: InvalidKeyException {}", e);
+        } catch (BadPaddingException e) {
+            LOG.error("Exception: BadPaddingException {}", e);
+        } catch (IllegalBlockSizeException e) {
+            LOG.error("Exception: IllegalBlockSizeException {}", e);
+        }
+
+        return encryptedData;
     }
 
     /**
