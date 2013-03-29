54ed532c83b3530610b7e4edd3fb86f64e833568
Sabbir Muhit <smuhit@wgen.net>
2013-03-29 14:34:19 -0400

    Added Bulk extract to smoke tests (removed databrowser from smoke)

--- tools/jenkinsTools/jenkinsJobScripts/smokeTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/smokeTests.sh
@@ -8,8 +8,6 @@ profileSwapAndPropGen
 
 deployAdmin
 
-deployDatabrowser
-
 startSearchIndexer
 
 processApps $APPSTODEPLOY
@@ -19,7 +17,7 @@ export DISPLAY=:4.0
 cd $WORKSPACE/sli/acceptance-tests
 export LANG=en_US.UTF-8
 bundle install --deployment
-bundle exec rake FORCE_COLOR=true sampleApp_server_address=https://$NODE_NAME.slidev.org/ dashboard_server_address=https://$NODE_NAME.slidev.org dashboard_api_server_uri=https://$NODE_NAME.slidev.org realm_page_url=https://$NODE_NAME.slidev.org/api/oauth/authorize admintools_server_url=https://$NODE_NAME.slidev.org:2001 api_server_url=https://$NODE_NAME.slidev.org databrowser_server_url=https://$NODE_NAME.slidev.org:2000 ingestion_landing_zone=/home/ingestion/lz/inbound sif_zis_address_trigger=http://$NODE_NAME.slidev.org:8080/mock-zis/trigger TOGGLE_TABLESCANS=true smokeTests
+bundle exec rake FORCE_COLOR=true sampleApp_server_address=https://$NODE_NAME.slidev.org/ dashboard_server_address=https://$NODE_NAME.slidev.org dashboard_api_server_uri=https://$NODE_NAME.slidev.org realm_page_url=https://$NODE_NAME.slidev.org/api/oauth/authorize admintools_server_url=https://$NODE_NAME.slidev.org:2001 api_server_url=https://$NODE_NAME.slidev.org ingestion_landing_zone=/home/ingestion/lz/inbound sif_zis_address_trigger=http://$NODE_NAME.slidev.org:8080/mock-zis/trigger TOGGLE_TABLESCANS=true smokeTests
 
 EXITCODE=$?
 
