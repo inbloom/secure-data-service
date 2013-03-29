167fedaaf61ef7ec9007cf80d5228c0fb2b9ef3f
John Singh <jsingh@wgen.net>
2013-03-29 00:30:04 -0400

    us5180 - creating delete one student from large dataset test

--- sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
+++ sli/acceptance-tests/teamFiles/megtomcat01_ingestion_performance.sh
@@ -5,13 +5,17 @@ cd /opt/megatron/sli/sli/acceptance-tests/teamFiles/
 sh ingestDataset.sh $1 1
 sh ingestDataset.sh $1 2
 sh log_durations.sh $1
-sh ingestDataset.sh purge.zip 3
+sh ingestDataset.sh delete.zip 3
+sh log_delete_duration.sh
+sh ingestDataset.sh purge.zip 4
 sh log_purge_duration.sh
 sh resetEnvironment.sh
 sh ingestDataset.sh $1 1
 sh ingestDataset.sh $1 2
 sh log_durations.sh $1
-sh ingestDataset.sh purge.zip 3
+sh ingestDataset.sh delete.zip 3
+sh log_delete_duration.sh
+sh ingestDataset.sh purge.zip 4
 sh log_purge_duration.sh
 echo "Done!"
 
