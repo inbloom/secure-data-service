b800f98e269332c36312e4ff186d394ad279c759
Thomas Shewchuk <tshewchuk@wgen.net>
2013-04-01 16:25:59 -0400

    US5527: Added extraction of yearlyTranscript container docs.

--- sli/acceptance-tests/test/features/bulk_extract/features/step_definitions/bulk_extract.rb
+++ sli/acceptance-tests/test/features/bulk_extract/features/step_definitions/bulk_extract.rb
@@ -205,8 +205,7 @@ When /^the extract contains a file for each of the following entities:$/ do |tab
 	end
 
   fileList = Dir.entries(@unpackDir)
-# Had to comment this next line out.  Different servers contain different numbers of records.
-#	assert((fileList.size-3)==table.hashes.size, "Expected " + table.hashes.size.to_s + " extract files, Actual:" + (fileList.size-3).to_s)
+	assert((fileList.size-3)==table.hashes.size, "Expected " + table.hashes.size.to_s + " extract files, Actual:" + (fileList.size-3).to_s)
 end
 
 When /^a "(.*?)" extract file exists$/ do |collection|
