1ce8ce723fc939603aca44a44c42c46af6b790d9
Ryan Latta <rlatta@wgen.net>
2013-04-02 16:14:47 -0400

    Removing spam text

--- sli/acceptance-tests/test/features/security/step_definitions/app_register_steps.rb
+++ sli/acceptance-tests/test/features/security/step_definitions/app_register_steps.rb
@@ -149,10 +149,8 @@ end
 
 Then /^the "([^"]*)" bootstrap app should exist$/ do |appName|
   apps = JSON.parse(@res.body)
-  puts apps
   foundApp = false
   apps.each do |app|
-    puts app["name"]
     if app["name"] == appName
         foundApp = true
         assert(app["bootstrap"] == true, "App #{appName} was not marked bootstrap")
