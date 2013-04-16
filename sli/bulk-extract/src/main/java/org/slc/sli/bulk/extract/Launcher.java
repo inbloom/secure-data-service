6ebbe2cfeda96fa70f329a11377972b3cd4a26ce
Ryan Latta <rlatta@wgen.net>
2013-04-16 12:51:22 -0400

    Adding LEA bulk extract helpers.

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/Launcher.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/Launcher.java
@@ -19,14 +19,12 @@ package org.slc.sli.bulk.extract;
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import org.slc.sli.bulk.extract.extractor.TenantExtractor;
 import org.slc.sli.bulk.extract.files.ExtractFile;
 import org.slc.sli.common.util.tenantdb.TenantContext;
@@ -35,6 +33,10 @@ import org.slc.sli.domain.Entity;
 import org.slc.sli.domain.NeutralCriteria;
 import org.slc.sli.domain.NeutralQuery;
 import org.slc.sli.domain.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Bulk extract launcher.
  *
@@ -126,6 +128,40 @@ public class Launcher {
      */
     public void setRepository(Repository<Entity> repository) {
         this.repository = repository;
+    }
+    
+    /**
+     * A helper function to get the list of approved app ids that have bulk extract enabled
+     * 
+     * @return a set of approved bulk extract app ids
+     */
+    public Set<String> getBulkExctractApps() {
+        NeutralQuery appQuery = new NeutralQuery(new NeutralCriteria("isBulkExtract", NeutralCriteria.OPERATOR_EQUAL,
+                true));
+        appQuery.addCriteria(new NeutralCriteria("registration.status", NeutralCriteria.OPERATOR_EQUAL, "APPROVED"));
+        Iterable<Entity> apps = repository.findAll("application", appQuery);
+        Set<String> appIds = new HashSet<String>();
+        for (Entity app : apps) {
+            appIds.add(app.getEntityId());
+        }
+        return appIds;
+    }
+    
+    /**
+     * Attempts to get all of the LEAs that should have a LEA level extract scheduled.
+     * 
+     * @return a set of the LEA ids that need a bulk extract
+     */
+    @SuppressWarnings("unchecked")
+    public Set<String> getBulkExtractLEAs() {
+        NeutralQuery appQuery = new NeutralQuery(new NeutralCriteria("applicationId", NeutralCriteria.CRITERIA_IN,
+                getBulkExctractApps()));
+        Iterable<Entity> apps = repository.findAll("applicationAuthorization", appQuery);
+        Set<String> edorgIds = new HashSet<String>();
+        for (Entity app : apps) {
+            edorgIds.addAll((Collection<String>) app.getBody().get("edorgs"));
+        }
+        return edorgIds;
     }
 
     /**
