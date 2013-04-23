86cd352921ca37ca0f29bb7d6e3ed8c3ce755508
John Singh <jsingh@megtomcat01.slidev.org>
2013-04-23 09:54:44 -0400

    Email tweak to mini slirp script

--- sli/acceptance-tests/teamFiles/checkoutAndBuild.sh
+++ sli/acceptance-tests/teamFiles/checkoutAndBuild.sh
@@ -5,22 +5,22 @@ if [ -z "$1" ]; then
 else
     branch=$1
 fi
-kill `ps aux | grep 'rails server' | grep -v grep | awk {'print $2'}`
-kill `ps aux | grep 'target/search-indexer-1.0-SNAPSHOT.jar' | grep -v grep | awk {'print $2'}`
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/api
-curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/api
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/dashboard
-curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/dashboard
+#kill `ps aux | grep 'rails server' | grep -v grep | awk {'print $2'}`
+#kill `ps aux | grep 'target/search-indexer-1.0-SNAPSHOT.jar' | grep -v grep | awk {'print $2'}`
+#curl http://tomcat:s3cret@localhost/manager/text/stop?path=/api
+#curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/api
+#curl http://tomcat:s3cret@localhost/manager/text/stop?path=/dashboard
+#curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/dashboard
 curl http://tomcat:s3cret@localhost/manager/text/stop?path=/ingestion-service
 curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/ingestion-service
 #curl http://tomcat:s3cret@localhost/manager/text/stop?path=/mock-zis
 #curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/mock-zis
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/sample
-curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/sample
+#curl http://tomcat:s3cret@localhost/manager/text/stop?path=/sample
+#curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/sample
 #curl http://tomcat:s3cret@localhost/manager/text/stop?path=/sif-agent
 #curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/sif-agent
-curl http://tomcat:s3cret@localhost/manager/text/stop?path=/simple-idp
-curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/simple-idp
+#curl http://tomcat:s3cret@localhost/manager/text/stop?path=/simple-idp
+#curl http://tomcat:s3cret@localhost/manager/text/undeploy?path=/simple-idp
 cd /opt/megatron/sli/
 git checkout -- /opt/megatron/sli/sli/acceptance-tests/test/data/application_fixture.json
 git checkout -- /opt/megatron/sli/sli/acceptance-tests/test/data/realm_fixture.json
@@ -84,12 +84,12 @@ cp /opt/megatron/sli/sli/acceptance-tests/teamFiles/megtomcat01.properties /opt/
 #sed -i.bk5 's#local.slidev.org#megtomcat01.slidev.org#g' properties.yml
 cd /opt/megatron/sli/sli/config/scripts/
 sh resetAllDbs.sh
-cd /opt/megatron/sli/sli/admin-tools/admin-rails/
-bundle install --deployment
-bundle exec rails server -d
-cd /opt/megatron/sli/sli/databrowser/
-bundle install --deployment
-bundle exec rails server -d
+#cd /opt/megatron/sli/sli/admin-tools/admin-rails/
+#bundle install --deployment
+#bundle exec rails server -d
+#cd /opt/megatron/sli/sli/databrowser/
+#bundle install --deployment
+#bundle exec rails server -d
 #cd /opt/megatron/sli/tools/odin
 #bundle install --deployment
 #bundle exec rake test
@@ -99,12 +99,12 @@ export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 mvn -version
 mvn clean package install -DskipTests -Dpmd.skip=true
-curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/api&war=file:/opt/megatron/sli/sli/api/target/api.war'
-curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/dashboard&war=file:/opt/megatron/sli/sli/dashboard/target/dashboard.war'
+#curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/api&war=file:/opt/megatron/sli/sli/api/target/api.war'
+#curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/dashboard&war=file:/opt/megatron/sli/sli/dashboard/target/dashboard.war'
 curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/ingestion-service&war=file:/opt/megatron/sli/sli/ingestion/ingestion-service/target/ingestion-service.war'
 #curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/mock-zis&war=file:/opt/megatron/sli/sli/sif/mock-zis/target/mock-zis.war'
-curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/sample&war=file:/opt/megatron/sli/sli/SDK/sample/target/sample.war'
+#curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/sample&war=file:/opt/megatron/sli/sli/SDK/sample/target/sample.war'
 #curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/sif-agent&war=file:/opt/megatron/sli/sli/sif/sif-agent/target/sif-agent.war'
-curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/simple-idp&war=file:/opt/megatron/sli/sli/simple-idp/target/simple-idp.war'
-cd /opt/megatron/sli/sli/search-indexer/
-scripts/local_search_indexer.sh restart target/search_indexer.tar.gz -Dsli.conf=/opt/tomcat/apache-tomcat-7.0.34/conf/sli.properties -Dsli.encryption.keyStore=/opt/tomcat/apache-tomcat-7.0.34/encryption/ciKeyStore.jks -Dlock.dir=data/
+#curl 'http://tomcat:s3cret@localhost/manager/text/deploy?path=/simple-idp&war=file:/opt/megatron/sli/sli/simple-idp/target/simple-idp.war'
+#cd /opt/megatron/sli/sli/search-indexer/
+#scripts/local_search_indexer.sh restart target/search_indexer.tar.gz -Dsli.conf=/opt/tomcat/apache-tomcat-7.0.34/conf/sli.properties -Dsli.encryption.keyStore=/opt/tomcat/apache-tomcat-7.0.34/encryption/ciKeyStore.jks -Dlock.dir=data/
