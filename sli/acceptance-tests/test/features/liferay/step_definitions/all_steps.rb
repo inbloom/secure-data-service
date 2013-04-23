2960e7e81319d5105f1d8d63f97c62f4fa25d738
Ryan Farris <rfarris@wgen.net>
2013-04-23 18:58:35 -0400

    E2E tests: debugging code for picard

--- sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
+++ sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
@@ -84,9 +84,6 @@ Then /^I should be on Portal home page$/ do
     assertWithWait("EULA pop up did not get dismissed") { !@driver.page_source.include?("d_popup") }
   else
     puts "EULA has already been accepted"
-    puts "-----------------"
-    puts @driver.page_source
-    puts "-----------------"
   end
 end
 
@@ -96,6 +93,11 @@ end
 
 Then /^I should be on the admin page$/ do
   title = @driver.find_element(:class, "sli_home_title").text
+  puts "-----------------"
+  puts "title #{title}"
+  puts "-----------------"
+  puts @driver.page_source
+  puts "-----------------"
   assert(title == "ADMIN", "User is not in the admin page")
 end
 
