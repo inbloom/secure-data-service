e8806b5d87de1e228793a9d468d467ccff75d33a
Ryan Farris <rfarris@wgen.net>
2013-04-24 21:22:21 -0400

    E2E debugging: just trying random things

--- sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
+++ sli/acceptance-tests/test/features/liferay/step_definitions/all_steps.rb
@@ -62,11 +62,10 @@ Then /^I should be on Portal home page$/ do
   home = @driver.find_elements(:class, "sli_home_title")
   assert(home.length == 1, "User is not on the portal home page. Current URL: " + @driver.current_url)
 
-  (1..5).each do |i|
-    puts "Checking for EULA... attempt #{i}"
-    break if (@driver.find_elements(:class, "d_popup").length > 1)
-    sleep 2
-  end
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  wait.util {
+    @driver.execute_script("return document.readyState;") == "complete"
+  }
 
   if (@driver.find_elements(:class, "d_popup").length > 1)
     accept = @driver.find_element(:css, "[class*='aui-button-input-submit']")
