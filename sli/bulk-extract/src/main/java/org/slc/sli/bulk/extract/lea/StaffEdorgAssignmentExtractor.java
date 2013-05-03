60c3bb0a82e216b89aa69cd9e0754c6ac7a7a211
Ryan Latta <rlatta@wgen.net>
2013-05-03 13:51:47 -0400

    Adding staff entities to extract.

---
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/lea/StaffEdorgAssignmentExtractor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012 Shared Learning Collaborative, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.slc.sli.bulk.extract.lea;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.slc.sli.bulk.extract.extractor.EntityExtractor;
+import org.slc.sli.common.constants.EntityNames;
+import org.slc.sli.common.constants.ParameterConstants;
+import org.slc.sli.domain.Entity;
+import org.slc.sli.domain.Repository;
+import org.springframework.data.mongodb.core.query.Query;
+
+public class StaffEdorgAssignmentExtractor implements EntityExtract {
+    
+    private EntityExtractor extractor;
+    private LEAExtractFileMap map;
+    private Repository<Entity> repo;
+    private ExtractorHelper extractorHelper;
+    private EntityToLeaCache cache;
+    
+    public StaffEdorgAssignmentExtractor(EntityExtractor extractor, LEAExtractFileMap map, Repository<Entity> repo,
+            ExtractorHelper extractorHelper, EntityToLeaCache entityToLeaCache) {
+        this.extractor = extractor;
+        this.map = map;
+        this.repo = repo;
+        this.extractorHelper = extractorHelper;
+        this.cache = entityToLeaCache;
+    }
+
+    @Override
+    public void extractEntities(Map<String, Set<String>> leaToEdorgCache) {
+        Iterator<Entity> associations = repo.findEach(EntityNames.STAFF_ED_ORG_ASSOCIATION, new Query());
+        while (associations.hasNext()) {
+            Entity association = associations.next();
+            if (!extractorHelper.isStaffAssociationCurrent(association)) {
+                continue;
+            }
+            for (String lea : leaToEdorgCache.keySet()) {
+                String edorg = (String) association.getBody().get(ParameterConstants.EDUCATION_ORGANIZATION_REFERENCE);
+                if (leaToEdorgCache.get(lea).contains(edorg)) {
+                    // Write it
+                    extractor.extractEntity(association, map.getExtractFileForLea(lea),
+                            EntityNames.STAFF_ED_ORG_ASSOCIATION);
+                    
+                    // Cache it
+                    cache.addEntry((String) association.getBody().get(ParameterConstants.STAFF_REFERENCE), lea);
+                }
+            }
+
+
+        }
+        
+    }
+    
+}
