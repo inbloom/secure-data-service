60f3a0428b7e6725298bc50491a47e135715adc2
ayarmak <ayarmak@wgen.net>
2013-05-07 09:36:28 -0400

    us5330: add course transcript conversion to smooks...

--- sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/CourseTranscriptCombiner.java
+++ sli/ingestion/ingestion-core/src/main/java/org/slc/sli/ingestion/transformation/CourseTranscriptCombiner.java
@@ -24,8 +24,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import org.slc.sli.ingestion.NeutralRecord;
 
@@ -35,8 +33,8 @@ import org.slc.sli.ingestion.NeutralRecord;
  * @author jcole
  * @author shalka
  */
-@Scope("prototype")
-@Component("courseTranscriptTransformationStrategy")
+//@Scope("prototype")
+//@Component("courseTranscriptTransformationStrategy")
 public class CourseTranscriptCombiner extends AbstractTransformationStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(CourseTranscriptCombiner.class);
