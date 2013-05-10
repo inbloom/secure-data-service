dd4450fecba2ec72b550804ad23e07e503f4cc3e
John Singh <jsingh@megtomcat01.slidev.org>
2013-05-10 09:11:11 -0400

    removing db profile

--- sli/acceptance-tests/teamFiles/checkIngestedCounts.sh
+++ sli/acceptance-tests/teamFiles/checkIngestedCounts.sh
@@ -8,5 +8,3 @@ unzip -p /opt/datasets/$1 manifest.json | tee manifest.json
 echo
 /home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby sli-verify.rb Midgar manifest.json
 rm -f manifest.json
-echo
-/home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby ../../../tools/data-tools/mongo_data_profile.rb
