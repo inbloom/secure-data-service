c8176d50fada54d744c54e4f9988e591f6f41aaf
John Singh <jsingh@wgen.net>
2013-05-10 11:53:25 -0400

    More daily perf script tidying

--- sli/acceptance-tests/teamFiles/checkIngestedCounts.sh
+++ sli/acceptance-tests/teamFiles/checkIngestedCounts.sh
@@ -8,7 +8,3 @@ unzip -p /opt/datasets/$1 manifest.json | tee manifest.json
 echo
 /home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby sli-verify.rb Midgar manifest.json
 rm -f manifest.json
-echo
-/home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby ../../../tools/data-tools/mongo_data_profile.rb 02f7abaa9764db2fa3c1ad852247cd4ff06b2c0a
-/home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby ../../../tools/data-tools/mongo_data_profile.rb sli
-/home/jsingh/.rvm/rubies/ruby-1.9.3-p362/bin/ruby ../../../tools/data-tools/mongo_data_profile.rb ingestion_batch_job
