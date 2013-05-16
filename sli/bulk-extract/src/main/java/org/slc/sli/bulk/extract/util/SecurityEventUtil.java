959de74a194b496a9b0e0da8b6af0bf06269663d
Thomas Shewchuk <tshewchuk@wgen.net>
2013-05-16 16:20:40 -0400

    US5725: Added security logging for tenant-level extract.  Refactored SecurityEventUtil.createSecurityEvent to static.

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/util/SecurityEventUtil.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/util/SecurityEventUtil.java
@@ -15,17 +15,18 @@
  */
 package org.slc.sli.bulk.extract.util;
 
-import org.slc.sli.common.util.logging.LogLevelType;
-import org.slc.sli.common.util.logging.SecurityEvent;
-import org.slc.sli.common.util.tenantdb.TenantContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import org.slc.sli.common.util.logging.LogLevelType;
+import org.slc.sli.common.util.logging.SecurityEvent;
+import org.slc.sli.common.util.tenantdb.TenantContext;
 
 /**
  * Util class to create a security event.
@@ -37,40 +38,36 @@ public class SecurityEventUtil {
 
     private static final Logger LOG = LoggerFactory.getLogger(SecurityEventUtil.class);
 
-    private String thisNode;
-    private String thisProcess;
-
-    /**
-     * SecurityEventUtil constructor, initializes the host and the process name.
-     */
-    public SecurityEventUtil() {
-        try {
-            InetAddress host = InetAddress.getLocalHost();
-            thisNode = host.getHostName();
-        } catch (UnknownHostException e) {
-            LOG.info("Could not find hostname/process for SecurityEventLogging!");
-        }
-        thisProcess = ManagementFactory.getRuntimeMXBean().getName();
-    }
-
     /**
      * Utility method to create a security event object.
      *
-     * @param loggingClass name of the originating class
-     * @param logMessage   security event log message
-     * @param actionDesc   description of action being performed
-     * @param logLevel     log level of the security event message
-     * @return
+     * @param loggingClass
+     *            name of the originating class
+     * @param logMessage
+     *            security event log message
+     * @param actionDesc
+     *            description of action being performed
+     * @param logLevel
+     *            log level of the security event message
+     *
+     * @return SecurityEvent
+     *            new SecurityEvent associated with parameters
      */
-    public SecurityEvent createSecurityEvent(String loggingClass, String logMessage, String actionDesc, LogLevelType logLevel) {
+    public static SecurityEvent createSecurityEvent(String loggingClass, String logMessage,
+            String actionDesc, LogLevelType logLevel) {
         SecurityEvent event = new SecurityEvent();
 
         event.setTenantId(TenantContext.getTenantId());
         event.setAppId("BulkExtract");
         event.setActionUri(actionDesc);
         event.setTimeStamp(new Date());
-        event.setProcessNameOrId(thisProcess);
-        event.setExecutedOn(thisNode);
+        event.setProcessNameOrId(ManagementFactory.getRuntimeMXBean().getName());
+        try {
+            event.setExecutedOn(InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            LOG.info("Could not find hostname/process for SecurityEventLogging!");
+            event.setExecutedOn("localhost");
+        }
         event.setClassName(loggingClass);
         event.setLogLevel(logLevel);
         event.setLogMessage(logMessage);
