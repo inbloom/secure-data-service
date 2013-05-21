0b7074edf11466ae1a9f43bb988a586180ecd830
John Singh <jsingh@wgen.net>
2013-05-20 20:03:09 -0400

    us5736 - fixture data updater script, tweaked after review comments

--- sli/acceptance-tests/updateFixtureData.rb
+++ sli/acceptance-tests/updateFixtureData.rb
@@ -9,6 +9,81 @@ require 'logger'
 # run using that file: ruby updateFixtureData.rb fixture_directories
 # scripts assumes new line at end of each line, and each line is unique
 
+  def process_edOrg_file(edOrgs_file, migrated_data)
+    @log.info "EducationOrganization file: "+edOrgs_file
+    if File.exists?('updated_edOrgs.json')
+      @log.error "updated_edOrgs.json already exists"
+      exit 1
+    end
+    File.open(edOrgs_file).each do |line|
+      #where fixture data does not parse as json
+      begin
+        json = JSON.parse(line)
+      rescue Exception => e
+        @log.error "Unable to parse line as json: "+line
+        e.backtrace.each { |trace_line| @log.error trace_line }
+        next
+      end
+      #only create EdOrgs for schools
+      if  json["type"]=="school"
+        if migrated_data.has_key?(json["_id"])
+          if migrated_data.has_key?(json["metaData"])
+            json["metaData"].merge!(Hash["edOrgs"=>migrated_data[json["_id"]]])
+          edge case where no metaData
+          else
+            json["metaData"]=Hash["edOrgs"=>migrated_data[json["_id"]]]
+          end
+        else
+          if migrated_data.has_key?("metaData")
+            json["metaData"].merge!(Hash["edOrgs"=>json["_id"]])
+          edge case where no metaData
+          else
+            json["metaData"]=Hash["edOrgs"=>json["_id"]]
+          end
+        end
+      end
+      File.open('updated_edOrgs.json', 'a') do |file|
+        file.puts(json.to_json)
+      end
+    end
+    FileUtils.mv('updated_edOrgs.json', edOrgs_file) if File.exist?('updated_edOrgs.json')
+  end
+
+  def process_student_file(students_file)
+    @log.info "Student file: "+students_file
+    migrated_data = Hash.new
+    if File.exists?('updated_students.json')
+      @log.error "updated_students.json already exists"
+      exit 1
+    end
+    File.open(students_file).each do |line|
+      #where fixture data does not parse as json
+      begin
+        json = JSON.parse(line)
+      rescue Exception => e
+        @log.error "Unable to parse line as json: "+line
+        e.backtrace.each { |trace_line| @log.error trace_line }
+        next
+      end
+      if json.has_key?("schools")
+        json["schools"].each do |schools_item|
+          if schools_item.has_key?("edOrgs")
+            migrated_data[schools_item["_id"]]=schools_item["edOrgs"]
+            schools_item.delete("edOrgs")
+          end
+        end
+      end
+      File.open('updated_students.json', 'a') do |file|
+        file.puts(json.to_json)
+      end
+    end
+    FileUtils.mv('updated_students.json', students_file) if File.exist?('updated_students.json')
+    #update associated educationOrganization.json
+    Dir.glob('*educationOrganization{.json,_*.json}').each do |edOrgs_file|
+      process_edOrg_file(edOrgs_file, migrated_data)
+    end
+  end
+
 $stdout.sync = true
 @log = Logger.new($stdout)
 @log.level = Logger::INFO
@@ -21,68 +96,17 @@ end
 directory_file=ARGV[0]
 @log.info "File containing directories: "+directory_file
 
-File.open(directory_file).each do |directory|
+  File.open(directory_file).each do |directory|
     #remove newline
     directory=directory[0..-2]
     @log.info "Directory: "+directory
     Dir.chdir(directory) do
       #get data to move and delete from student.json
       Dir.glob('*student{.json,_*.json}').each do |students_file|
-        @log.info "Student file: "+students_file
-        migrated_data = Hash.new
-        FileUtils.rm_f('updated_students.json')
-        File.open(students_file).each do |line|
-          #where fixture data does not parse as json
-          begin
-            json = JSON.parse(line)
-          rescue Exception => e
-            next
-          end
-          if json.has_key?("schools")
-            json["schools"].each do |schools_item|
-              if schools_item.has_key?("edOrgs")
-                migrated_data[schools_item["_id"]]=schools_item["edOrgs"]
-                schools_item.delete("edOrgs")
-              end
-            end
-          end
-          File.open('updated_students.json', 'a') do |file|
-            file.puts(json.to_json)
-          end
-        end
-        FileUtils.mv('updated_students.json', students_file) if File.exist?('updated_students.json')
-          #update associated educationOrganization.json
-          Dir.glob('*educationOrganization{.json,_*.json}').each do |edOrgs_file|
-          @log.info "EducationOrganization file: "+edOrgs_file
-            FileUtils.rm_f('updated_edOrgs.json')
-            File.open(edOrgs_file).each do |line|
-              #where fixture data does not parse as json
-              begin
-                json = JSON.parse(line)
-              rescue Exception => e
-                next
-              end
-                if migrated_data.has_key?(json["_id"])
-                  if migrated_data.has_key?(json["metadata"])
-                    json["metaData"].merge!(Hash["edOrgs"=>migrated_data[json["_id"]]])
-                  #edge case where no metaData
-                  else
-                    json["metaData"]=Hash["edOrgs"=>migrated_data[json["_id"]]]
-                  end
-                else
-                  if migrated_data.has_key?(json["metadata"])
-                    json["metaData"].merge!(Hash["edOrgs"=>json["_id"]]) if json["type"]=="school"
-                  #edge case where no metaData
-                  else
-                    json["metaData"]=Hash["edOrgs"=>json["_id"]]
-                  end
-                end
-              File.open('updated_edOrgs.json', 'a') do |file|
-                file.puts(json.to_json)
-              end
-            end
-            FileUtils.mv('updated_edOrgs.json', edOrgs_file) if File.exist?('updated_edOrgs.json')
-          end
+        process_student_file(students_file)
       end
     end
-end
+  end
+
+
+
