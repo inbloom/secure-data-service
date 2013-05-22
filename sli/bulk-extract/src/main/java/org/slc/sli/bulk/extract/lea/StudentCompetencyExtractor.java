83a42f415e3c65385a4c5ff2b01c9b64a8b7d1d9
Jonathan Nanney <jnanney@wgen.net>
2013-05-22 14:56:01 -0400

    Added unit tests.

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/lea/StudentCompetencyExtractor.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/lea/StudentCompetencyExtractor.java
@@ -28,14 +28,11 @@ public class StudentCompetencyExtractor implements EntityExtract {
 
 	@Override
 	public void extractEntities(EntityToLeaCache studentSectionToEdOrgCache) {
-		LOG.info("Let's see if this does anything size of cache is {}", studentSectionToEdOrgCache.getEntityIds().size());
 		Iterator<Entity> studentCompetencies = repository.findEach(EntityNames.STUDENT_COMPETENCY, new NeutralQuery());
 		while(studentCompetencies.hasNext()) {
 			Entity studentCompetency = studentCompetencies.next();
-			LOG.info("Trying to get this {}", (String) studentCompetency.getBody().get(ParameterConstants.STUDENT_SECTION_ASSOCIATION_ID));
 			Set<String> leas = studentSectionToEdOrgCache.getEntriesById(
 					(String) studentCompetency.getBody().get(ParameterConstants.STUDENT_SECTION_ASSOCIATION_ID));
-			LOG.info("LEAs for this student competency {}", leas);
 			for (String lea : leas) {
 				entityExtractor.extractEntity(studentCompetency, leaToExtractFileMap.getExtractFileForLea(lea), EntityNames.STUDENT_COMPETENCY);
 			}
