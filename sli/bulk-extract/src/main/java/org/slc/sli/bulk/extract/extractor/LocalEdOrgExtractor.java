6fec5601e81ef75f30c9550148613599a3730e2f
Thomas Shewchuk <tshewchuk@wgen.net>
2013-05-22 12:24:34 -0400

    DE2843: Added new unit test.

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/extractor/LocalEdOrgExtractor.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/extractor/LocalEdOrgExtractor.java
@@ -117,11 +117,11 @@ public class LocalEdOrgExtractor {
 
         genericExtractor = factory.buildStudentAssessmentExtractor(entityExtractor, leaToExtractFileMap, repository, helper);
         genericExtractor.extractEntities(student.getEntityCache());
-        
+
         genericExtractor = factory.buildStudentGradebookEntryExtractor(entityExtractor, leaToExtractFileMap,
                 repository, helper);
         genericExtractor.extractEntities(student.getEntityCache());
-        
+
         // Yearly Transcript
         genericExtractor = factory.buildYearlyTranscriptExtractor(entityExtractor, leaToExtractFileMap, repository, helper);
         genericExtractor.extractEntities(student.getEntityCache());
