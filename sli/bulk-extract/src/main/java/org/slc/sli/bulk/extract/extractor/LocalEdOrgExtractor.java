fd0ad0968ff428d4b845b6a48649b9647eabb03c
Mark Abernathy <mabernathy@wgen.net>
2013-05-24 13:40:04 -0400

    Fix duplication of LEA level student extract

--- sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/extractor/LocalEdOrgExtractor.java
+++ sli/bulk-extract/src/main/java/org/slc/sli/bulk/extract/extractor/LocalEdOrgExtractor.java
@@ -203,10 +203,6 @@ public class LocalEdOrgExtractor {
             ExtractFile file = factory.buildLEAExtractFile(tenantDirectory.getAbsolutePath(), lea,
                     getArchiveName(lea, startTime.toDate()), appPublicKeys, securityEventUtil);
             edOrgToLEAExtract.put(lea, file);
-            for (String child : helper.getChildEdOrgs(Arrays.asList(lea))) {
-                edOrgToLEAExtract.put(child, file);
-            }
-
         }
         return edOrgToLEAExtract;
     }
