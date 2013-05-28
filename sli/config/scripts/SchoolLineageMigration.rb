029a32de2af1c6020377fe450a508e9e6f9b605a
Shan Shan <sshan@wgen.net>
2013-05-28 09:15:27 -0400

    migration script

--- sli/config/scripts/SchoolLineageMigration.rb
+++ sli/config/scripts/SchoolLineageMigration.rb
@@ -1,7 +1,16 @@
+#!/usr/bin/env ruby
+
+# Move Schools.edOrgs fields from Student Collection to EducationOrganization Collection
+
+# Usage: SchoolLineageMigration.rb [<database>]. 
+
+# With no database, lists all the databases.
+# With a database argument, shows collections in that database
+
 require 'mongo'
 
 ############################################################
-# Lineage Migration Script
+# Migration Script
 ############################################################
 
 INGESTION_DB = "localhost"
@@ -15,15 +24,14 @@ def updateStudentEdOrgs(dbName)
    @student_collection.find.each do |row|
         schools = Array.new
         schools = row["schools"]
-        schools.each do |school|
-            edOrgs = Hash.new
-            edOrgs = school["edOrgs"]
-            school.delete("edOrgs")
+        if(schools)
+          schools.each do |school|
+              edOrgs = Hash.new
+              edOrgs = school["edOrgs"]
+              school.delete("edOrgs")
+          end
+          @student_collection.update({"_id" => row["_id"]},{"$set" => {"schools" => schools}})
         end
-        @student_collection.update({"_id" => row["_id"]},{"$set" => {"schools" => schools}})
-   end
-   @student_collection.find.each do |row|
-        puts row
    end
 end
 
@@ -35,65 +43,66 @@ def updateSchoolLineage(school, dbName)
    parentId = school["_id"]
 
    while  parentId
-      parents.push(parentId)
-      @entity = @entity_collection.find({"_id" => parentId})
-      @entity.find.each {|row| parentId = row["body"]["parentEducationAgencyReference"] }
+      @entity = @entity_collection.find_one({"_id" => parentId})
+      if @entity != nil
+        parents.push(parentId)
+        parentId = @entity["body"]["parentEducationAgencyReference"]
+      else
+        parentId = nil
+      end
    end
-   puts "parents = "
-   puts parents
 
    @entity_collection.update({"_id" => school["_id"]}, {"$set" => {"metaData.edOrgs" => parents}})
 end
 
-############################################################
-# Function: calculate and update Lineage for all schools
-# Input: dbName
-# Output: N/A
-############################################################
+# Calculate and update Lineage for all schools
 def updateAllSchoolLineage(dbName)
    @db = @conn[dbName]
    @entity_collection = @db["educationOrganization"]
    @school_collection = @entity_collection.find({"type" => "school"})
-   @school_collection.find.each { |row| updateSchoolLineage(row, dbName)}
+
+   @school_collection.find.each do |row|
+       puts updateSchoolLineage(row, dbName)
+   end
 end
 
-def updateAllDB
-    puts "update All the DBs"
-
-    @conn.database_names.each do |name|
-        if name == "admin" || name == "config" || name == "ingestion_batch_job" || name == "sli"
-            puts "<!--- ignore " + name + "---!>"
-        else
-            puts updateDB(name)
-        end
-    end
-end
-
+# Update Student and EducationOrganization Collection
 def updateDB(dbName)
-    if dbName == "admin" || dbName == "config" || dbName == "ingestion_batch_job" || dbName == "sli"
-       puts "<!--- ignore " + dbName + "---!>"
-    else
-       puts "Update " + dbName
-       puts updateAllSchoolLineage(dbName)
-       puts updateStudentEdOrgs(dbName)
-    end
+    puts updateAllSchoolLineage(dbName)
+    puts updateStudentEdOrgs(dbName)
 end
 
+# Check if database exist
 def dbCheck(dbName)
     names =  @conn.database_names
-    puts names.length
-    puts names.include? dbName
     return names.include? dbName
 end
 
-dbName = ARGV[0]
-puts dbName
-if dbName == "All"||dbName == "all"||dbName == nil
-    puts updateAllDB
-else
-    if dbCheck(dbName) == true
-      puts updateDB(dbName)
-    else
-      puts "<!--- db Name does not exist, please check ---!>"
+# Main driver
+def main(argv)
+	dbNames = argv
+	puts dbNames
+	if 0==dbNames.length
+    	@conn.database_names.each do |name|
+        	if name == "admin" || name == "config" || name == "ingestion_batch_job" || name == "sli"
+            	puts "<--- ignore " + name + "--->"
+       		else
+            	puts updateDB(name)
+        	end
+        end
+        puts "<--- done. --->"
+        return
     end
+
+	for dbName in dbNames	
+    	if dbCheck(dbName) == true
+      		puts updateDB(dbName)
+    	else
+      		puts "<--- db Name does not exist, please check --->"
+   		end
+   	end   	
+   	puts "<--- done. --->"
 end
+
+# Run it
+main(ARGV)
