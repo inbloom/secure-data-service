86bb543584d6cd87b22279b63a0388a5039a55d1
Thomas Shewchuk <tshewchuk@wgen.net>
2013-05-28 14:44:56 -0400

    US5747: Code review fixes.

--- sli/api/src/main/java/org/slc/sli/api/security/context/resolver/EdOrgHelper.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/resolver/EdOrgHelper.java
@@ -91,17 +91,7 @@ public class EdOrgHelper {
         Set<String> directAssoc = getDirectEdorgs(user);
         NeutralQuery query = new NeutralQuery(new NeutralCriteria(ParameterConstants.ID, NeutralCriteria.CRITERIA_IN,
                 directAssoc, false));
-        Set<String> entities = new HashSet<String>();
-        for (Entity entity : repo.findAll(EntityNames.EDUCATION_ORGANIZATION, query)) {
-            if (helper.isLEA(entity)) {
-                entities.add(helper.getTopLEAOfEdOrg(entity).getEntityId());
-            } else if (helper.isSchool(entity)) {
-                entities.add(helper.getTopLEAOfEdOrg(entity).getEntityId());
-            } else { // isSEA
-                entities.addAll(getChildLEAsOfEdOrg(entity));
-            }
-        }
-        return new ArrayList<String>(entities);
+        return getDistrictsAndSEAIfRequested(query, false);
     }
 
     /**
@@ -119,18 +109,7 @@ public class EdOrgHelper {
         Set<String> directAssoc = getDirectEdorgs(user);
         NeutralQuery query = new NeutralQuery(new NeutralCriteria(ParameterConstants.ID, NeutralCriteria.CRITERIA_IN,
                 directAssoc, false));
-        Set<String> entities = new HashSet<String>();
-        for (Entity entity : repo.findAll(EntityNames.EDUCATION_ORGANIZATION, query)) {
-            if (helper.isLEA(entity)) {
-                entities.add(helper.getTopLEAOfEdOrg(entity).getEntityId());
-            } else if (helper.isSchool(entity)) {
-                entities.add(helper.getTopLEAOfEdOrg(entity).getEntityId());
-            } else { // isSEA
-                entities.addAll(getChildLEAsOfEdOrg(entity));
-                entities.add(entity.getEntityId());
-            }
-        }
-        return new ArrayList<String>(entities);
+        return getDistrictsAndSEAIfRequested(query, true);
     }
 
     /**
@@ -144,6 +123,21 @@ public class EdOrgHelper {
     public List<String> getDistricts(Set<String> edOrgs) {
         NeutralQuery query = new NeutralQuery(new NeutralCriteria(ParameterConstants.ID, NeutralCriteria.CRITERIA_IN,
                 edOrgs, false));
+        return getDistrictsAndSEAIfRequested(query, false);
+    }
+
+    /**
+     * Determine the districts, including the SEA, if requested, based upon the query.
+     *
+     * If an SEA is encountered, this is any LEA directly below the SEA, including the SEA.
+     * If an LEA is encountered, this is the top-most LEA, i.e. the LEA directly associated with the SEA.
+     *
+     * @param query - Query for obtaining EdOrgs
+     * @param includeSEA - Determines whether to include the SEA
+     *
+     * @return - List of entity IDs
+     */
+    private List<String> getDistrictsAndSEAIfRequested(NeutralQuery query, boolean includeSEA) {
         Set<String> entities = new HashSet<String>();
         for (Entity entity : repo.findAll(EntityNames.EDUCATION_ORGANIZATION, query)) {
             if (helper.isLEA(entity)) {
@@ -152,6 +146,9 @@ public class EdOrgHelper {
                 entities.add(helper.getTopLEAOfEdOrg(entity).getEntityId());
             } else { // isSEA
                 entities.addAll(getChildLEAsOfEdOrg(entity));
+                if (includeSEA) {
+                    entities.add(entity.getEntityId());
+                }
             }
         }
         return new ArrayList<String>(entities);
