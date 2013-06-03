0f1e9716ccbc5f320cf75168dd79c245f50123b7
Oleg Krook <okrook@wgen.net>
2013-06-03 11:32:56 -0400

    XSD Parser fix

--- sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/parser/impl/EdfiRecordUnmarshallerTest.java
+++ sli/ingestion/ingestion-core/src/test/java/org/slc/sli/ingestion/parser/impl/EdfiRecordUnmarshallerTest.java
@@ -133,7 +133,7 @@ public class EdfiRecordUnmarshallerTest {
     }
 
 //    This should be uncommented once DE2873 has been fixed
-//    @Test(expected = XmlParseException.class)
+    @Test(expected = XmlParseException.class)
     public void testRejectIfTagIsNotClosed2() throws Throwable {
         rejectRecord("parser/InterchangeStudentParent/StudentHasOneTagNotClosed2.xml",
                 "edfiXsd-SLI/SLI-Interchange-StudentParent.xsd");
