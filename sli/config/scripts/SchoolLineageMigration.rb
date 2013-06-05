d5dcd1ea01610a142d7428a6931e85a6a8bfc54f
Shan Shan <sshan@wgen.net>
2013-06-05 16:05:49 -0400

    Create index for edOrg collection -- migration script  ++ handle arguments

--- sli/config/scripts/SchoolLineageMigration.rb
+++ sli/config/scripts/SchoolLineageMigration.rb
@@ -14,24 +14,21 @@ require 'mongo'
 ############################################################
 
 @conn = nil
-host = 'localhost'
-port = '27017'
 
 SCHOOL_FIELD = "schools"
 EDORGS_FIELD = "edOrgs"
 METADATA_FIELD = "metaData"
 PARENT_FIELD = "parentEducationAgencyReference"
-
+STUDENT_COLLECTION = "student"
+EDORG_COLLECTION = "educationOrganization"
 
 def updateStudentEdOrgs(dbName)
    @db = @conn[dbName]
-   @student_collection = @db["student"]
+   @student_collection = @db[STUDENT_COLLECTION]
    @student_collection.find.each do |row|
-        schools = Array.new
         schools = row[SCHOOL_FIELD]
         if(schools)
           schools.each do |school|
-              edOrgs = Hash.new
               edOrgs = school[EDORGS_FIELD]
               school.delete(EDORGS_FIELD)
           end
@@ -42,29 +39,29 @@ end
 
 def updateSchoolLineage(school, dbName)
    @db = @conn[dbName]
-   @entity_collection = @db["educationOrganization"]
+   @edOrg_collection = @db[EDORG_COLLECTION]
 
    parents = Array.new
    parentId = school["_id"]
 
    while  parentId
-      @entity = @entity_collection.find_one({"_id" => parentId})
-      if @entity != nil
+      @edOrg = @edOrg_collection.find_one({"_id" => parentId})
+      if @edOrg != nil
         parents.push(parentId)
-        parentId = @entity["body"][PARENT_FIELD]
+        parentId = @edOrg["body"][PARENT_FIELD]
       else
         parentId = nil
       end
    end
 
-   @entity_collection.update({"_id" => school["_id"]}, {"$set" => {"metaData.edOrgs" => parents}})
+   @edOrg_collection.update({"_id" => school["_id"]}, {"$set" => {"metaData.edOrgs" => parents}})
 end
 
 # Calculate and update Lineage for all schools
 def updateAllSchoolLineage(dbName)
    @db = @conn[dbName]
-   @entity_collection = @db["educationOrganization"]
-   @school_collection = @entity_collection.find({"type" => "school"})
+   @edOrg_collection = @db[EDORG_COLLECTION]
+   @school_collection = @edOrg_collection.find({"type" => "school"})
 
    @school_collection.find.each do |row|
        puts updateSchoolLineage(row, dbName)
@@ -73,27 +70,19 @@ end
 
 # Update Student and EducationOrganization Collection
 def updateDB(dbName)
-  if dbCheck(dbName) == true
     updateAllSchoolLineage(dbName)
     updateStudentEdOrgs(dbName)
-  else
-    puts "    " + " db [" + dbName + "]  does not exist, please check"
-    exit
-  end
+    puts "Creating index on " + EDORG_COLLECTION
+    @conn[dbName][EDORG_COLLECTION].create_index({"metaData.edOrgs" => Mongo::ASCENDING}, {:sparse => true })
 end
 
-# Check if database exist
-def dbCheck(dbName)
-    names =  @conn.database_names
-    return names.include? dbName
-end
 
 # Example: this_script.rb --all host:myhost.com port:12345
 #          this_script.rb host:myhost.com port:12345 db1 db2 db3
 #
 def parseArgs(argv)
 
-  result = { "all" => false, "mongo_host" => "localhost", "mongo_port" => 27017, "dbNames" => [] }
+  result = { "all" => false, "mongo_host" => "localhost", "mongo_port" => 27017, "tenants" => [] }
 
   for arg in argv
     if arg == "--all"
@@ -103,7 +92,7 @@ def parseArgs(argv)
       result["mongo_host"] = host_port[0]
       result["mongo_port"] = host_port[1]
     else
-      result["dbNames"] << arg
+      result["tenants"] << arg
     end
   end
   return result
@@ -113,43 +102,56 @@ end
 def main(argv)
   params = parseArgs(argv)
 
-	dbNames = params["dbNames"]
+	tenants = params["tenants"]
   @conn = Mongo::Connection.new(params["mongo_host"], params["mongo_port"])
 
-	if ( 0==dbNames.length and not params["all"] ) or ( dbNames.length >= 1 and params["all"] )
+  # Map tenant name to database by getting records in sli.tenant collection
+  tenant2db = {}
+  @conn['sli']['tenant'].find({}).each do |tenant|
+    name = tenant['body']['tenantId']
+    db = tenant['body']['dbName']
+    tenant2db[name] = db
+  end
+
+  # Make sure any tenants given explicitly actually exist
+  for tname in tenants
+    if not tenant2db.has_key?(tname)
+      puts "No such tenant: " + tname
+      return
+    end
+  end
+
+  # Make sure that either a list of tenants, or "-all" was given (and not both)
+  if ( 0==tenants.length and not params["all"] ) or ( tenants.length >= 1 and params["all"] )
     puts "--------------------------------------------------------------------------------------------"
-    puts "| Databases on server '" + params["mongo_host"] + "':"
-    tenants = []
-    @conn['sli']['tenant'].find({}).each do |tenant|
-      tenants << tenant
+    puts "| Tenant databases on server '" + params["mongo_host"] + "':"
+    tenant2db.each_pair do |tname, dbname|
+      puts "|    " + tname + " " + dbname
     end
-    for tenant in tenants
-      puts "|    " + tenant['body']['tenantId'] + " " + tenant['body']['dbName']
-    end
+
     puts "--------------------------------------------------------------------------------------------"
     puts "| To migrate on student and educationOrganization collection, give argument(s) as follows:\n"
+    puts "|\n"
     puts "|     --all                                     Migrate against all tenant dbs\n"
-    puts "|     [<dbname1>,<dbname2>...]                  Migrate only database in the list\n"
-    puts "|    myhost:myport                              hostname and port defaults to localhost:27017"
+    puts "|       --OR--\n"
+    puts "|     <tenant1> [<tenant2> ...]                 Migrate only database(s) in the list\n"
+    puts "|\n"
+    puts "|    myhost:myport                              Optional hostname and port defaults to localhost:27017"
     puts "--------------------------------------------------------------------------------------------"
     return
   end
 
-
+  # Set tenants to complete list
   if params["all"]
-    @conn['sli']['tenant'].find({}).each do |tenant|
-      tenants << tenant
-    end
-      for tenant in tenants
-        updateDB(tenant['body']['dbName'])
-      end
-      puts "    " + "All done."
-      return
+    tenants = tenant2db.keys
   end
 
-  for dbName in dbNames
-    updateDB(dbName)
+  for tenant in tenants
+    dbname = tenant2db[tenant]
+    puts "Migrating data for tenant: " + tenant + ", database " + dbname
+    updateDB(dbname)
   end
+
   puts "    " + "All done."
 end
 
