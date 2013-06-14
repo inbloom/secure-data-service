1c71a94a88358884ab9f6b07de591ec0c687d7a4
Randy Zingle <rzingle@wgen.net>
2013-06-14 08:44:38 -0400

    Added edge cases to StudentToStudentParentAssociationValidatorTest

--- sli/api/src/test/java/org/slc/sli/api/security/context/validator/StudentToStudentParentAssociationValidatorTest.java
+++ sli/api/src/test/java/org/slc/sli/api/security/context/validator/StudentToStudentParentAssociationValidatorTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.slc.sli.api.resources.SecurityContextInjector;
+import org.slc.sli.api.util.SecurityUtil;
 import org.slc.sli.common.constants.EntityNames;
 import org.slc.sli.domain.Entity;
 
@@ -34,31 +35,62 @@ public class StudentToStudentParentAssociationValidatorTest {
     @Autowired
     private ValidatorTestHelper helper;
       
-    private List<Entity> spas = null;
+    private List<Entity> studentParentAssociationList = null;
     
     @Before
     public void setUp() {
-        spas = Arrays.asList(helper.generateStudentParentAssoc("studentID1", "parentID1"),
-                helper.generateStudentParentAssoc("studentID2", "parentID2"));
+        studentParentAssociationList = Arrays.asList(helper.generateStudentParentAssoc("studentID1", "parentID1"),
+                helper.generateStudentParentAssoc("studentID1", "parentID2"));
 
         Entity student = Mockito.mock(Entity.class);
-        Map<String, List<Entity>> value = new HashMap<String, List<Entity>>();
+        Map<String, List<Entity>> studentParentAssociationMap = new HashMap<String, List<Entity>>();
         String key = "studentParentAssociation";
-        value.put(key, spas);
-        Mockito.when(student.getEmbeddedData()).thenReturn(value);
+        studentParentAssociationMap.put(key, studentParentAssociationList);
+        Mockito.when(student.getEmbeddedData()).thenReturn(studentParentAssociationMap);
         injector.setStudentContext(student);
     }
     
+    /*
+     * Send the two valid studentParentAssociations for the student to the validator.
+     * The validator should return true.
+     */
     @Test
-    public void testValidSingleAssociation() {
-        //String entityType, Set<String> ids
+    public void testValidStudentParentAssociation() {
         Set<String> ids = new HashSet<String>();
-        for (Entity e : spas) {
+        for (Entity e : studentParentAssociationList) {
             String s = e.getEntityId();
             ids.add(s);
         }
         boolean valid = validator.validate(EntityNames.STUDENT_PARENT_ASSOCIATION, ids);
         Assert.assertTrue(valid);
+    }
+    
+    /*
+     * Add an invalid studentParentAssociation to the two valid ones that the student contains.
+     * The validator should return false.
+     */
+    @Test
+    public void testOneInvalidStudentParentAssociation() {
+        Set<String> ids = new HashSet<String>();  
+        for (Entity e : studentParentAssociationList) {
+            String s = e.getEntityId();
+            ids.add(s);
+        }
+        // add the invalid studentParentAssocation - the validator should return false
+        ids.add("invalidID");
+        boolean valid = validator.validate(EntityNames.STUDENT_PARENT_ASSOCIATION, ids);
+        Assert.assertTrue(!valid);
+    }
+    
+    /*
+     * Empty list of ids - should return false from the areParametersValid method in validator
+     */
+    @Test
+    public void testEmptyStudentParentAssociation() {
+        Set<String> ids = new HashSet<String>();  
+        ids.add("invalidID");
+        boolean valid = validator.validate(EntityNames.STUDENT_PARENT_ASSOCIATION, ids);
+        Assert.assertTrue(!valid);
     }
     
 }
