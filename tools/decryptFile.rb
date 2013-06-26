4e257a6eb9f8c3e9bc061362ed2f8854f6c4fed1
Chris Kelly <ckelly@omniti.com>
2013-06-26 16:42:26 -0400

    IS-591 Modified decryption script that is capable of decrypting large files

--- tools/decryptFile.rb
+++ tools/decryptFile.rb
@@ -1,18 +1,13 @@
 require 'openssl'
-
 puts("Usage: rubyDecrypt <private key> <input file> <output file>")
 if ARGV.size != 3
-  exit 
+  exit
 end
 private_key = OpenSSL::PKey::RSA.new File.read ARGV[0]
-input_file = File.open(ARGV[1], 'r')
+input_file = File.open(ARGV[1], 'rb')
 puts "length #{input_file.size}"
-encryptediv = input_file.read(256) 
-#encryptediv = input_file[0,256] 
+encryptediv = input_file.read(256)
 encryptedsecret = input_file.read(256)
-#encryptedsecret = input_file[256,256]
-encryptedmessage = input_file.read()
-#encryptedmessage = input_file[512,input_file.length - 512]
 
 decrypted_iv = private_key.private_decrypt(encryptediv)
 decrypted_secret = private_key.private_decrypt(encryptedsecret)
@@ -21,7 +16,14 @@ aes = OpenSSL::Cipher.new('AES-128-CBC')
 aes.decrypt
 aes.key = decrypted_secret
 aes.iv = decrypted_iv
-plain = aes.update(encryptedmessage) + aes.final
 
-output_file = File.open(ARGV[2], 'w')
-output_file.write(plain)
+output_file = File.open(ARGV[2], 'wb')
+
+loop do
+    r = input_file.read(4096)
+    break unless r
+    plain = aes.update(r)
+    output_file << plain
+end
+
+output_file << aes.final
