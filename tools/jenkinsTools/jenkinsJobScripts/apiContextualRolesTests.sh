a1d53af1d6b0de4640276fc10565235c7ff3107b
npandey <npandey@amplify.com>
2013-06-28 12:55:00 -0400

    Testing fix

--- tools/jenkinsTools/jenkinsJobScripts/apiContextualRolesTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/apiContextualRolesTests.sh
@@ -16,7 +16,7 @@ export DISPLAY=:6.0
 cd $WORKSPACE/sli/acceptance-tests
 export LANG=en_US.UTF-8
 bundle install --deployment
-bundle exec rake FORCE_COLOR=true app_bootstrap_server=ci api_server_url=https://$NODE_NAME.slidev.org api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 ci_idp_redirect_url=https://$NODE_NAME.slidev.org/simple-idp?realm=IL-Daybreak ldap_hostname=ldap.slidev.org ldap_port=389 ldap_use_ssl=false ldap_base=ou=DevTest,dc=slidev,dc=org apiContextualRolesTests TOGGLE_TABLESCANS=true
+bundle exec rake FORCE_COLOR=true app_bootstrap_server=ci api_server_url=https://$NODE_NAME.slidev.org api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 ci_idp_redirect_url=https://$NODE_NAME.slidev.org/simple-idp?realm=IL-Daybreak ldap_hostname=ldap.slidev.org ldap_port=389 ldap_use_ssl=false ldap_base=ou=LocalNew,ou=DevTest,dc=slidev,dc=org apiContextualRolesTests TOGGLE_TABLESCANS=true
 
 EXITCODE=$?
 
