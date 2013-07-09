a8e9faa92180847d1a336627886e3967181c1c1f
Yang Cao <ycao@wgen.net>
2013-07-09 16:39:58 -0400

    parent can write to parent/student entities

--- sli/api/src/main/java/org/slc/sli/api/security/context/ParentAccessValidator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/context/ParentAccessValidator.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import org.slc.sli.api.constants.ResourceNames;
+import org.slc.sli.api.resources.generic.util.ResourceMethod;
 
 @Component
 public class ParentAccessValidator extends AccessValidator {
@@ -90,8 +91,13 @@ public class ParentAccessValidator extends AccessValidator {
     }
     
     @Override
-    protected boolean isWriteAllowed(List<String> path) {
-        return false;
+    protected boolean isWriteAllowed(List<String> path, String method) {
+        if (ResourceMethod.DELETE.toString().equals(method)) {
+            return false; 
+        }
+       
+        String baseEntity = path.get(0);
+        return path.size() <= 2 && (ResourceNames.STUDENTS.equals(baseEntity) || ResourceNames.PARENTS.equals(baseEntity));
     }
     
 }
