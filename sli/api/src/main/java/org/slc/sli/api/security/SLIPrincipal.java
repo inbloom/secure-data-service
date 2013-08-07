2c9a2580179871912d6a4da421c851c3822b8d20
Sabbir Muhit <smuhit@amplify.com>
2013-08-07 14:23:09 -0400

    Revert "US5811: Added default constructor for SLIPrincipal."
    This reverts commit 3032d6ab09618d7e8939fafe430ec00859697425.

--- sli/api/src/main/java/org/slc/sli/api/security/SLIPrincipal.java
+++ sli/api/src/main/java/org/slc/sli/api/security/SLIPrincipal.java
@@ -505,9 +505,5 @@ public class SLIPrincipal implements Principal, Serializable {
          *
          */
         private static final long serialVersionUID = 1L;
-
-        EdOrgContextRightsCache() {
-            super();
-        }
     }
 }
