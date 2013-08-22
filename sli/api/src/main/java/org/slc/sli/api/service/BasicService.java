10ee4dd3c567bb5ab5c471cb348f66c4b41519b0
Tao Ke <tke@amplify.com>
2013-08-22 11:30:11 -0400

    US5812: Fixed to get contextual rights

--- sli/api/src/main/java/org/slc/sli/api/service/BasicService.java
+++ sli/api/src/main/java/org/slc/sli/api/service/BasicService.java
@@ -579,7 +579,7 @@ public class BasicService implements EntityService, AccessibilityCheck {
                 rightAccessValidator.checkAccess(true, isSelf, entity, defn.getType(), auths);
                 rightAccessValidator.checkFieldAccess(neutralQuery, isSelf, entity, defn.getType(), auths);
 
-                results.add(entityRightsFilter.makeEntityBody(entity, treatments, defn, isSelf, auths));
+                results.add(entityRightsFilter.makeEntityBody(entity, treatments, defn, isSelf, auths, context));
             } catch (AccessDeniedException aex) {
                 if (entities.size() == 1) {
                     throw aex;
@@ -1211,7 +1211,10 @@ public class BasicService implements EntityService, AccessibilityCheck {
 
         if (context == SecurityUtil.UserContext.DUAL_CONTEXT) {
             Map<String, SecurityUtil.UserContext> entityContext = getEntityContextMap(Arrays.asList(entity), isRead);
-            context = entityContext.get(entity.getEntityId());
+
+            if (entityContext != null) {
+                context = entityContext.get(entity.getEntityId());
+            }
         }
 
         return rightAccessValidator.getContextualAuthorities(isSelf, entity, context, isRead);
--- sli/api/src/test/java/org/slc/sli/api/service/BasicServiceTest.java
+++ sli/api/src/test/java/org/slc/sli/api/service/BasicServiceTest.java
@@ -229,8 +229,8 @@ public class BasicServiceTest {
         Entity entity2 = new MongoEntity("student", "student2", entityBody2, new HashMap<String,Object>());
         Iterable<Entity> entities = Arrays.asList(entity1, entity2);
         Mockito.when(mockRepo.findAll(Mockito.eq("student"), Mockito.any(NeutralQuery.class))).thenReturn(entities);
-        Mockito.when(mockRightsFilter.makeEntityBody(Mockito.eq(entity1), Mockito.any(List.class), Mockito.any(EntityDefinition.class), Mockito.anyBoolean(), Mockito.any(Collection.class))).thenReturn(entityBody1);
-        Mockito.when(mockRightsFilter.makeEntityBody(Mockito.eq(entity2), Mockito.any(List.class), Mockito.any(EntityDefinition.class), Mockito.anyBoolean(), Mockito.any(Collection.class))).thenReturn(entityBody2);
+        Mockito.when(mockRightsFilter.makeEntityBody(Mockito.eq(entity1), Mockito.any(List.class), Mockito.any(EntityDefinition.class), Mockito.anyBoolean(), Mockito.any(Collection.class), Mockito.any(SecurityUtil.UserContext.class))).thenReturn(entityBody1);
+        Mockito.when(mockRightsFilter.makeEntityBody(Mockito.eq(entity2), Mockito.any(List.class), Mockito.any(EntityDefinition.class), Mockito.anyBoolean(), Mockito.any(Collection.class), Mockito.any(SecurityUtil.UserContext.class))).thenReturn(entityBody2);
 
         Mockito.when(mockAccessValidator.getContextualAuthorities(Matchers.eq(false), Matchers.eq(entity1), Matchers.eq(SecurityUtil.UserContext.TEACHER_CONTEXT), Matchers.eq(true))).thenReturn(teacherContextRights);
 
