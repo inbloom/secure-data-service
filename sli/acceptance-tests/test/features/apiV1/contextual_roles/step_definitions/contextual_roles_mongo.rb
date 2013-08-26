18ebda29726e930634334462c5385ed26721f100
John Singh <jsingh@wgen.net>
2013-08-26 00:54:58 -0400

    us5851 - fix AT for generic edOrg format  - revert

--- sli/acceptance-tests/test/features/apiV1/contextual_roles/step_definitions/contextual_roles_mongo.rb
+++ sli/acceptance-tests/test/features/apiV1/contextual_roles/step_definitions/contextual_roles_mongo.rb
@@ -545,10 +545,7 @@ Given /^I get (\d+) random ids for "([^"]*)" in "([^"]*)"$/ do |number, type, en
   db_name = convertTenantIdToDbName(@tenant)
   db = conn[db_name]
   coll = db.collection(entity)
-  if type == 'school' && entity == 'educationOrganization'
-    entities = coll.find({'body.organizationCategories' => 'school'}, {:fields => %w(_id)}).to_a
-    puts  entities.to_s
-  elseif type == 'educationOrganization' && entity == 'educationOrganization'
+  if type == 'educationOrganization' && entity == 'educationOrganization'
     entities = coll.find({}, {:fields => %w(_id)}).to_a
   else
     entities = coll.find({'type' => type}, {:fields => %w(_id)}).to_a
