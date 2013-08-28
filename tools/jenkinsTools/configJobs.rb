f1f9cc3cbf03a0eb34a9d5037b4ade1207b38168
Oleg Krook <okrook@amplify.com>
2013-08-28 16:14:11 -0400

    Jenkins View Jobs Configuration

--- tools/jenkinsTools/configJobs.rb
+++ tools/jenkinsTools/configJobs.rb
@@ -3,30 +3,18 @@ require 'uri'
 require 'rexml/document'
 require 'json'
 
-@new_branch = ENV['NEW_BRANCH'] || 'Release-1.0.65'
-@config_job = 'Config-Branch-79-Rel'
-@url_base = 'https://jenkins.slidev.org/view/CI-79-Rel/'
+@new_branch = ENV['NEW_BRANCH'] || 'Release-1.0.XX'
+@view_to_configure = ENV['VIEW_TO_CONFIGURE'] || 'CI-XX-Rel'
+
+@client = JenkinsApi::Client.new(:server_url => 'http://jenkins.slidev.org', :server_port => '8080',
+      :username => '', :password => '', :jenkins_path => '/jenkins', :debug => false)
 
 def get_jobs(url)
-  uri = URI.parse(url + "api/json")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true if uri.scheme == "https"
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE if uri.scheme == "https"
-
-  req = Net::HTTP::Get.new(uri.request_uri)
-  req.basic_auth 'jenkinsapi_user', 'test1234'
-
-  response = http.request(req)
-
+  jobs = @client.view.list_jobs(source_view)
+  
   body = JSON.parse(response.body)
 
-  body['jobs'].delete_if {|j| j['name'] == @config_job}
   return body['jobs']
-
-  #matches = body.scan(/"url":"([^"]+)"/)
-  #matches.flatten!
-  #matches.delete_if {|j| j == url}
-  #matches.delete_if {|j| j == @job_url}
 end
 
 def fix_job(base_url)
@@ -53,6 +41,6 @@ def fix_job(base_url)
   end
 end
 
-actual_jobs = get_jobs @url_base
+actual_jobs = get_jobs @view_to_configure
 puts actual_jobs
 actual_jobs.each {|j| fix_job(j['url']) }
