ea7e52c087500786d3866c6eb56aad17ba8d6ae1
John Hinsdale <jhinsdale@wgen.net>
2013-10-02 17:15:19 -0400

    US5861 Render HTML for tree.  It is slow, maybe replace w/ JSON

--- sli/admin-tools/admin-rails/app/controllers/application_authorizations_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/application_authorizations_controller.rb
@@ -21,7 +21,8 @@ class ApplicationAuthorizationsController < ApplicationController
   before_filter :check_rights
 
   ROOT_ID = "root"
-
+  FAKE_PREFIX = "fake_"
+  
   #
   # Edit app authorizations
   #
@@ -33,13 +34,13 @@ class ApplicationAuthorizationsController < ApplicationController
   #
   def edit
 
-    load_apps()
-    load_edorgs()
-    
+    # Get input objects
     edOrgId = session[:edOrgId]
     @edOrg = EducationOrganization.find(edOrgId)
     raise "Edorg '" + edOrgId + "' not found in educationOrganization collection" if @edOrg.nil?
 
+    # Get app data
+    load_apps()
     appId = params[:application_authorization][:appId]
     @app = @apps_map[appId]
     raise "Application '" + appId + "' not found in sli.application" if @app.nil?
@@ -48,36 +49,42 @@ class ApplicationAuthorizationsController < ApplicationController
     # Get developer-enabled edorgsfor the app.  NOTE: Even though the field is
     # sli.application.authorized_ed_orgs[] these edorgs are called "developer-
     # enabled" or just "enabled" edorgs.
-    enabled_ed_orgs = @app.authorized_ed_orgs
+    @enabled_ed_orgs = array_to_hash(@app.authorized_ed_orgs)
 
     # Get edOrgs already authorized in <tenant>.applicationAuthorization.edorgs[]
     # The are the "authorized" (by the edOrg admin) edorgs for the app
     @appAuth = ApplicationAuthorization.find(appId)
-    authorized_ed_orgs = @appAuth.edorgs
+    @authorized_ed_orgs = array_to_hash(@appAuth.edorgs)
+
+    # Load up edOrg data
+    load_edorgs()
 
     roots_info = ""
     @edinf[ROOT_ID][:children].each do |cid|
       roots_info += cid + ": " + @edinf[cid].to_s + "\n"
     end
     
-    @debug = "edorgId=" + edOrgId \
-              + "\n" + "params:\n" + params.to_s() \
-              + "\n" + "@app is:\n" + @app.to_s() \
-              + "\n" + "@app.authorized_ed_orgs=" + @app.authorized_ed_orgs.to_s() \
-              + "\n" + "@appAuth.appid:" + @appAuth.appId.to_s() \
-              + "\n" + "@appAuth.edorgs:" + @appAuth.edorgs.to_s() \
-              + "\n" + "@edinf.keys.length: " + @edinf.keys.length.to_s() \
-              + "\n" + "@root_edorgs.length: " + @root_edorgs.length.to_s() \
-              + "\n" + "Roots children:\n" + roots_info \
-              + "\n" + "@edinf[ROOT_ID]:\n" + @edinf[ROOT_ID].to_s \
-              + ""
+#-#     @debug = "edorgId=" + edOrgId \
+#-#               + "\n" + "params:\n" + params.to_s() \
+#-#               + "\n" + "@app is:\n" + @app.to_s() \
+#-#               + "\n" + "@app.authorized_ed_orgs=" + @app.authorized_ed_orgs.to_s() \
+#-#               + "\n" + "@appAuth.appid:" + @appAuth.appId.to_s() \
+#-#               + "\n" + "@appAuth.edorgs:" + @appAuth.edorgs.to_s() \
+#-#               + "\n" + "@edinf.keys.length: " + @edinf.keys.length.to_s() \
+#-#               + "\n" + "Roots children:\n" + roots_info \
+#-#               + "\n" + "@edinf[ROOT_ID]:\n" + @edinf[ROOT_ID].to_s \
+#-#               + ""
+    @debug = ""
+
+    @edorg_tree_html = "<ul>\n" + render_html(ROOT_ID, 0) + "</ul>\n"
+
   end
   
 
   # NOTE this controller allows ed org super admins to enable/disable apps for their LEA(s)
   # It allows LEA(s) to see (but not change) their app authorizations
   def check_rights
-    unless is_lea_admin? or is_sea_admin?
+    unless is_lea_admin? || is_sea_admin?
       logger.warn {'User is not lea or sea admin and cannot access application authorizations'}
       raise ActiveResource::ForbiddenAccess, caller
     end
@@ -105,6 +112,7 @@ class ApplicationAuthorizationsController < ApplicationController
           end
         end
       end
+
       # Some apps such as dashboard and databrowser may have allowed_for_all_edorgs set
       if app.allowed_for_all_edorgs
         if ! @edorg_apps.has_key?(session[:edOrgId])
@@ -120,7 +128,7 @@ class ApplicationAuthorizationsController < ApplicationController
     legacy_sea_delegation_support = false
     
     @application_authorizations = {}
-    if legacy_sea_delegation_support and is_sea_admin?
+    if legacy_sea_delegation_support && is_sea_admin?
       my_delegations = AdminDelegation.all
       @edorgs = (my_delegations.select{|delegation| delegation.appApprovalEnabled}).map{|cur| cur.localEdOrgId}
       @edorgs = @edorgs.sort{|a,b| a <=> b}
@@ -181,7 +189,7 @@ class ApplicationAuthorizationsController < ApplicationController
       ApplicationAuthorization.cur_edorg = affected_edorg
       @application_authorization = ApplicationAuthorization.find(params[:id], :params => {:edorg => affected_edorg})
       updates[:edorgs] = [affected_edorg]
-      success = success and @application_authorization.update_attributes(updates)
+      success = success && @application_authorization.update_attributes(updates)
       raise "error" if ! success
       break if ! success
     end
@@ -209,17 +217,22 @@ class ApplicationAuthorizationsController < ApplicationController
   end
 
   # Load up all the edOrgs.  Creates:
-  # 
+  #    @edinf - Map of edOrg ID to the tree
+  #    @enabled_ed_orgs - Map of IDs enabled
+  #    @authorized_ed_orgs - Map of IDs authorized
+  #
   def load_edorgs()
+
     @edinf = {}
-    @root_edorgs = {}
     root_ids = []
     
     # Get all edOrgs, include only needed fields
     allEdOrgs = EducationOrganization.findAllInChunks({"includeFields" => "parentEducationAgencyReference,nameOfInstitution,stateOrganizationId,organizationCategories"})
     allEdOrgs.each do |eo|
 
-      @edinf[eo.id] = { :edOrg => eo, :id => eo.id, :name => eo.nameOfInstitution, :children => [] }
+      @edinf[eo.id] = { :edOrg => eo, :id => eo.id, :name => eo.nameOfInstitution, :children => [],
+                        :enabled => @enabled_ed_orgs.has_key?(eo.id), :authorized => @authorized_ed_orgs.has_key?(eo.id)
+                      }
 
       parents = eo.parentEducationAgencyReference
       if parents.nil?
@@ -252,7 +265,8 @@ class ApplicationAuthorizationsController < ApplicationController
 
     # Create fake root edOrg and parent all top level nodes to it
     root_edorg = { :id => ROOT_ID, :parents => [], :children => root_ids,
-                   :edOrg => { :id => ROOT_ID, :name => "All EdOrgs", :parentEducationAgencyReference  => [] }
+                   :enabled => true, :authorized => true, :name => "All EdOrgs", 
+                   :edOrg => { :id => ROOT_ID, :parentEducationAgencyReference  => [], }
                  }
     @edinf[ROOT_ID] = root_edorg
     root_ids.each do |id|
@@ -273,6 +287,11 @@ class ApplicationAuthorizationsController < ApplicationController
     @edinf[id][:nchild] = nchild
     ndesc = nchild
     by_type = {}
+
+    # Sort children by name
+    compare_name = ->(a,b) { @edinf[a][:name] <=> @edinf[b][:name] }
+    @edinf[id][:children].sort!( & compare_name )
+
     @edinf[id][:children].each do |cid|
       count_children(cid)
       ndesc += @edinf[cid][:ndesc]
@@ -281,10 +300,15 @@ class ApplicationAuthorizationsController < ApplicationController
         by_type[ctype][:children].push(cid)
         by_type[ctype][:nchild] += 1
         by_type[ctype][:ndesc] += @edinf[cid][:ndesc] + 1
+        # Aggregate the enabled/authorized status
+        by_type[ctype][:enabled] = by_type[ctype][:enabled] && @edinf[cid][:enabled]
+        by_type[ctype][:authorized] = by_type[ctype][:authorized] && @edinf[cid][:authorized]
       else
-        new_id = "fake_" + @id_counter.to_s
+        new_id = FAKE_PREFIX + @id_counter.to_s
         @id_counter += 1
-        by_type[ctype] = { :id => new_id, :name => ctype, :parents => [ id ], :children => [ cid ], :nchild => 1, :ndesc => @edinf[cid][:ndesc] + 1 }
+        by_type[ctype] = { :id => new_id, :name => ctype, :parents => [ id ], :children => [ cid ], :nchild => 1, :ndesc => @edinf[cid][:ndesc] + 1,
+                           :enabled => @edinf[cid][:enabled], :authorized => @edinf[cid][:authorized]
+                         }
       end
     end
 
@@ -322,10 +346,71 @@ class ApplicationAuthorizationsController < ApplicationController
     return false
   end
 
+  # render_html - Render HTML markup for the node and its children
+  # Render a <li> element for the node, and if children exist, render
+  # a <ul> with <li>'s for the children.
+  # Each <li> will have a checkbox with the edOrg ID, with "checked"
+  # set if the edOrg is currently selected (or if it is a "fake" node
+  # and all it's children are selected).  The <li> will be given
+  # a class="collapsed" if it should be collapsed initially.  This
+  # will be determined by number of children being fewer than some
+  # threshold.
+  # Example <li>:
+  #    <li class="collapsed"><input type="checkbox" id="edorg_1">EdOrg 1
+  def render_html(id, level)
+    # <LI> part
+    indent = "  " * level
+    result = indent + "<li"
+    maxchild = 10
+    expand_level = 3
+    eo = @edinf[id]
+    nchild = eo[:children].length
+    collapsed = nchild > 0 && (level >= expand_level || nchild > maxchild)
+    result += " class=\"collapsed\"" if collapsed
+    result += ">"
+
+    # <input>
+    is_fake = id.start_with?(FAKE_PREFIX)
+    if eo[:enabled]
+      result += "<input type=\"checkbox\""
+      if !id.start_with?(FAKE_PREFIX) && !is_empty(id)
+        result += " id=\"" + id + "\""
+      end
+      result += " checked" if eo[:authorized]
+      result += ">"
+    end
+
+    # Text of the node.  Italicize if not enabled
+    result += "<i>" if !eo[:enabled]
+    result += eo[:name]
+    result += "</i>" if !eo[:enabled]
+    result += "\n"
+    
+    # Children
+    if nchild > 0
+      result += indent + "<ul>\n"
+      eo[:children].each do |cid|
+        result += render_html(cid, level + 1)
+      end
+      result += indent + "</ul>\n"
+    end
+
+    return result
+  end
+  
+  # Convert array to map
+  def array_to_hash(a)
+    result = {}
+    a.each do|elt|
+      result[elt] = true
+    end
+    return result
+  end
+
   # Get best guess at edorg type
   def get_edorg_type(id)
     cats = @edinf[id][:edOrg].organizationCategories
-    if cats.nil? or cats.empty?
+    if cats.nil? || cats.empty?
       return "Unknown"
     end
     cats.uniq!
