5ae7d8dcc879a67f33867458a43f26c9f53ef4dc
Brandon Suzuki <bsuzuki@wgen.net>
2013-10-08 15:27:19 -0400

    us5861 REMOVE ME print out the db being used

--- sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_app_approval.rb
+++ sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_app_approval.rb
@@ -176,6 +176,7 @@ Then /^there are "(.*?)" edOrgs for the "(.*?)" application in the production ap
    appId = record["_id"]
    puts appId.to_s
    db = @conn[convertTenantIdToDbName(PropLoader.getProps['tenant'])]
+   puts "using db : " + convertTenantIdToDbName(PropLoader.getProps['tenant'])
    coll = db.collection("applicationAuthorization")
    record = coll.find_one("body.applicationId" => appId.to_s)
    puts record.to_s
