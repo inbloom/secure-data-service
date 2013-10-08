46cf90e37118dd112bd1eaf3c7e10a719b64f57b
Brandon Suzuki <bsuzuki@wgen.net>
2013-10-08 15:39:23 -0400

    us5861 fix incorrect syntax used to get db

--- sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_app_approval.rb
+++ sli/acceptance-tests/test/features/cross_app_tests/step_definitions/rc_integration_app_approval.rb
@@ -175,7 +175,7 @@ Then /^there are "(.*?)" edOrgs for the "(.*?)" application in the production ap
    puts record.to_s
    appId = record["_id"]
    puts appId.to_s
-   db = @conn[convertTenantIdToDbName(PropLoader.getProps['tenant'])]
+   db = @conn.db(convertTenantIdToDbName(PropLoader.getProps['tenant']))
    puts "using db (" + PropLoader.getProps['tenant'] + "): " + convertTenantIdToDbName(PropLoader.getProps['tenant'])
    coll = db.collection("applicationAuthorization")
    record = coll.find_one("body.applicationId" => appId.to_s)
