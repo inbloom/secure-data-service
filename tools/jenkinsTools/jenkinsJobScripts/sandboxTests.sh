b5adbede4b01fbc4c55369e16832e8dabe4be8b4
Sabbir Muhit <smuhit@amplify.com>
2013-10-18 15:50:34 -0400

    Updating job scripts to use cislave specific subtrees in ldap

--- tools/jenkinsTools/jenkinsJobScripts/sandboxTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/sandboxTests.sh
@@ -23,7 +23,7 @@ export DISPLAY=:6.0
 cd $WORKSPACE/sli/acceptance-tests/
 export LANG=en_US.UTF-8
 bundle install --deployment
-bundle exec rake FORCE_COLOR=true databrowser_server_url=https://${NODE_NAME}.slidev.org:2000 api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 admintools_server_url=https://${NODE_NAME}.slidev.org:2001 ldap_base=ou=CI,dc=slidev,dc=org sampleApp_server_address=https://$NODE_NAME.slidev.org/ sandboxTests TOGGLE_TABLESCANS=1
+bundle exec rake FORCE_COLOR=true databrowser_server_url=https://${NODE_NAME}.slidev.org:2000 api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 admintools_server_url=https://${NODE_NAME}.slidev.org:2001 ldap_base=ou=${NODE_NAME},dc=slidev,dc=org sampleApp_server_address=https://$NODE_NAME.slidev.org/ sandboxTests TOGGLE_TABLESCANS=1
 
 EXITCODE=$?
 
