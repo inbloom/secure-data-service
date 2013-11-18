0aafc9a273dbc1ff7cc55bf1f39f84f7608ac11f
Shan Shan <sshan@wgen.net>
2013-11-18 16:18:30 -0500

    us5865 remove access check from forgot passowrd

--- sli/admin-tools/admin-rails/app/controllers/application_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/application_controller.rb
@@ -205,12 +205,6 @@ class ApplicationController < ActionController::Base
     return false
   end
 
-  def check_allowed_user
-    roles = session[:roles]
-    if roles == nil || !(is_operator? || is_sea_admin? || is_lea_admin? || is_developer? || is_realm_admin? || is_ingestion_user?)
-      raise ActiveResource::ForbiddenAccess, caller
-    end
-  end
 
    def get_app_authorizer_edOrgs
      if is_admin_realm_authenticated?
