c9a2f24645429ed00cbbf3b599287f5b13aca3da
ablum <ablum@wgen.net>
2013-12-02 16:59:15 -0500

    US5984 adding BE properties to Sandbox script

--- tools/jenkinsTools/jenkinsJobScripts/sandboxTests.sh
+++ tools/jenkinsTools/jenkinsJobScripts/sandboxTests.sh
@@ -23,7 +23,7 @@ export DISPLAY=:6.0
 cd $WORKSPACE/sli/acceptance-tests/
 export LANG=en_US.UTF-8
 bundleInstall --deployment
-bundle exec rake FORCE_COLOR=true databrowser_server_url=https://${NODE_NAME}.slidev.org:2000 api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 admintools_server_url=https://${NODE_NAME}.slidev.org:2001 ldap_base=ou=CI,dc=slidev,dc=org sampleApp_server_address=https://$NODE_NAME.slidev.org/ sandboxTests TOGGLE_TABLESCANS=1
+bundle exec rake FORCE_COLOR=true databrowser_server_url=https://${NODE_NAME}.slidev.org:2000 api_server_url=https://$NODE_NAME.slidev.org api_ssl_server_url=https://$NODE_NAME.slidev.org:8443 admintools_server_url=https://${NODE_NAME}.slidev.org:2001 ldap_base=ou=CI,dc=slidev,dc=org sampleApp_server_address=https://$NODE_NAME.slidev.org/ bulk_extract_script=$WORKSPACE/sli/bulk-extract/scripts/local_bulk_extract.sh bulk_extract_properties_file=/opt/tomcat/conf/sli.properties bulk_extract_keystore_file=/opt/tomcat/encryption/ciKeyStore.jks bulk_extract_jar_loc=$WORKSPACE/sli/bulk-extract/target/bulk_extract.tar.gz sandboxTests TOGGLE_TABLESCANS=1
 
 EXITCODE=$?
 
