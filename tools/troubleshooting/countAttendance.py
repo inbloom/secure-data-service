7fc0c2c641f566a7010d2e8637cfa646982dbb0c
Dave Coleman <dcoleman@dcoleman-mac.local>
2013-12-04 18:02:10 -0500

    adding copyright message

--- tools/troubleshooting/countAttendance.py
+++ tools/troubleshooting/countAttendance.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+# Copyright 2012-2013 inBloom, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #This only works for Attendance
 #This script is meant for debugging purposes.  It is not produciton quality
 #This script counts the number of unique cominations of SchoolYear, StudentID, and SchoolId.  These are the components of 
