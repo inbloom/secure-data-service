b4dfe3c2bbdd389f4b3d7dc2b0c12d8510c478b6
Bill Siggelkow <bill.siggelkow@inbloom.org>
2014-01-08 14:29:57 -0500

    Append time to cats_with_lasers for acceptance tests

--- sli/acceptance-tests/test/features/utils/selenium_common.rb
+++ sli/acceptance-tests/test/features/utils/selenium_common.rb
@@ -146,7 +146,7 @@ When /^I select "(.*?)" from the dropdown and click go$/ do |arg1|
 end
 
 After do |scenario|
-  base = "./cats_with_lasers" # Maybe a better name, eh?
+  base = "./cats_with_lasers_#{Time.now.strftime('%H-%M-%S')}" # Maybe a better name, eh?
   url_fn = base + ".txt"
   html_fn = base + ".html"
   screenshot_fn = base + ".png"
