0c6154fb099f99e626ef44b238fe286a65e95c4a
shameem <shameem@osintegrators.com>
2014-02-11 14:40:52 -0500

    made uriMutator changes and merged master

--- sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
+++ sli/api/src/main/java/org/slc/sli/api/security/pdp/UriMutator.java
@@ -1248,14 +1248,9 @@ public class UriMutator {
             programsIds = getSubdocIds(student, EntityNames.STUDENT_PROGRAM_ASSOCIATION, ParameterConstants.PROGRAM_ID);
         }
 
-<<<<<<< HEAD
-
-      /*  if (programsIds == null || programsIds.isEmpty()) {
-=======
-        if (programsIds == null || programsIds.isEmpty()) {
->>>>>>> Revert "Fixed the UriMutator bug, added toString method in MutatedContainer class and fixed the failing tests in UriMutatorTest"
+        /*if (programsIds == null || programsIds.isEmpty()) {
             throw new UriMutationException("No association to any programs");
-        }
+        }*/
 
 
 
@@ -1277,17 +1272,10 @@ public class UriMutator {
             cohortsIds = getSubdocIds(student, EntityNames.STUDENT_COHORT_ASSOCIATION, ParameterConstants.COHORT_ID);
         }
 
-<<<<<<< HEAD
-
        /* if (cohortsIds == null || cohortsIds.isEmpty()) {
             throw new UriMutationException("No association to any cohorts");
         }*/
 
-=======
-        if (cohortsIds == null || cohortsIds.isEmpty()) {
-            throw new UriMutationException("No association to any cohorts");
-        }
->>>>>>> Revert "Fixed the UriMutator bug, added toString method in MutatedContainer class and fixed the failing tests in UriMutatorTest"
         return cohortsIds;
     }
 
