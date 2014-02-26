ee155659d7a5cc7ed29260136b73f7b917db8033
Bill Siggelkow <bill.siggelkow@inbloom.org>
2014-02-26 13:09:40 -0500

    Debug statements

--- sli/acceptance-tests/test/features/ingestion/features/step_definitions/ingestion_steps.rb
+++ sli/acceptance-tests/test/features/ingestion/features/step_definitions/ingestion_steps.rb
@@ -644,9 +644,12 @@ end
 
 Given /^I am using preconfigured Ingestion Landing Zone for "([^"]*)"$/ do |lz_key|
   # if the lz_key is overridden from the command line, use the override value
+  puts "Looking up landing zone for #{lz_key}"
   unless (@ingestion_lz_key_override == nil)
     lz_key = @ingestion_lz_key_override
   end
+  puts "Looking up landing zone for #{lz_key}"
+  puts "Ingestion landing zone identifier map: #{@ingestion_lz_identifier_map.inspect}"
 
   lz = @ingestion_lz_identifer_map[lz_key]
   initializeLandingZone(lz)
@@ -678,7 +681,7 @@ def initializeLandingZone(lz)
   end
 
   @landing_zone_path = lz
-  #puts "Landing Zone = " + @landing_zone_path unless @landing_zone_path.nil?
+  puts "Landing Zone = " + @landing_zone_path
 
   # clear out LZ before proceeding
   if (INGESTION_MODE == 'remote')
