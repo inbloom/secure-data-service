99914e83454015a8c0063a50e450f5ff6a58375b
Chris Kelly <chris.kelly@inbloom.org>
2014-03-06 14:10:21 -0500

    testing BE issue by moving sleep around

--- sli/acceptance-tests/test/features/bulk_extract/features/step_definitions/bulk_extract_api_steps.rb
+++ sli/acceptance-tests/test/features/bulk_extract/features/step_definitions/bulk_extract_api_steps.rb
@@ -71,6 +71,8 @@ Given /^in my list of rights I have BULK_EXTRACT$/ do
 end
 
 When /^I make a full bulk extract API call for edorg "(.*?)"$/ do |arg1|
+  puts 'waiting 3 minutes for ingestion to finish'
+  sleep 180
   restTls("/bulk/extract/#{arg1}")
 end
 
