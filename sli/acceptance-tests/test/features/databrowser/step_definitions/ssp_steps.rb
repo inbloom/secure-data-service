daf4c12825c0b4f6ac35e6cac6b80f1148e3a751
dnelson <dnelson@osintegrators.com>
2014-03-12 12:02:27 -0400

    Cleaned up databrowser tests for ssp

--- sli/acceptance-tests/test/features/databrowser/step_definitions/ssp_steps.rb
+++ sli/acceptance-tests/test/features/databrowser/step_definitions/ssp_steps.rb
@@ -16,35 +16,9 @@ limitations under the License.
 
 =end
 
-
-# Then /^I can search for <Type> with a <Field> and get a <Result>$/ do |table|
-#   table.hashes.each do |hash|
-#     select = @driver.find_element(:tag_name, "select")
-#     all_options = select.find_elements(:tag_name, "option")
-#     all_options.each do |option|
-#       if option.attribute("value") == hash["Type"]
-#         option.click
-#         break
-#       end
-#     end
-#     @driver.find_element(:id, "search_id").clear
-#     @driver.find_element(:id, "search_id").send_keys(hash["Field"])
-#     @driver.find_element(:css, "input[type='submit']").click
-#     errors = @driver.find_elements(:id, "alert")
-#     if hash["Result"] == "Pass"
-#       assert(errors.size == 0, "Should not be a flash error for #{hash["Type"]}/#{hash["Field"]}")
-#     else
-#       assert(errors.size == 1, "There should be an error message for #{hash["Type"]}/#{hash["Field"]}")
-#     end   
-#   end
-# end
-
-@type
-@field
+# Instance variables to let the Search and Result steps communicate
 
 When /^I can search for (.*) with a (.*)$/ do |type, field|
-  @type = type
-  @field = field
   select = @driver.find_element(:tag_name, "select")
   all_options = select.find_elements(:tag_name, "option")
   all_options.each do |option|
@@ -58,12 +32,12 @@ When /^I can search for (.*) with a (.*)$/ do |type, field|
   @driver.find_element(:css, "input[type='submit']").click
 end
 
-Then /^I get result ((.|>)*)$/ do |result| 
+Then /^I get result (.*)$/ do |result| 
     errors = @driver.find_elements(:id, "alert")
     if result == "Pass"
-      assert(errors.size == 0, "Should not be a flash error for #{@type}/#{@field}")
+      assert(errors.size == 0, "Should not be a flash error")
     else
-      assert(errors.size == 1, "There should be an error message for #{@type}/#{@field}")
+      assert(errors.size == 1, "There should be an error message")
     end   
   
 end
