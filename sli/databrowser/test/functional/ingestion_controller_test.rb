b5c7035d2746d44729991504869cc7380bf6ba07
Michael Brush <mbrush@osintegrators.com>
2014-03-29 21:49:39 -0400

    Removed default ingestion tests

--- sli/databrowser/test/functional/ingestion_controller_test.rb
+++ sli/databrowser/test/functional/ingestion_controller_test.rb
@@ -1,14 +1,6 @@
 require 'test_helper'
 
 class IngestionControllerTest < ActionController::TestCase
-  test "should get list" do
-    get :list
-    assert_response :success
-  end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
 
 end
