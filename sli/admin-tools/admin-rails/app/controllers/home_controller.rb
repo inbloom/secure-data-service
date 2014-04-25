1834dc6c6193af0de4a754312efb488a3fa22255
Bill Siggelkow <bill.siggelkow@inbloom.org>
2014-04-25 15:35:25 -0400

    Simplify

--- sli/admin-tools/admin-rails/app/controllers/home_controller.rb
+++ sli/admin-tools/admin-rails/app/controllers/home_controller.rb
@@ -1,31 +1,7 @@
-=begin
-
-Copyright 2012-2013 inBloom, Inc. and its affiliates.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-=end
-
 class HomeController < ApplicationController
 
-
-  SANDBOX_ADMINISTRATOR = "Sandbox Administrator"
-  SLC_OPERATOR = "SLC Operator"
-  SEA_ADMINISTRATOR = "SEA Administrator"
-  LEA_ADMINISTRATOR = "LEA Administrator"
-  SANDBOX_ALLOWED_ROLES = [SANDBOX_ADMINISTRATOR]
-  PRODUCTION_ALLOWED_ROLES = [SLC_OPERATOR, SEA_ADMINISTRATOR, LEA_ADMINISTRATOR]
-
+  SANDBOX_ALLOWED_ROLES = ['Sandbox Administrator']
+  PRODUCTION_ALLOWED_ROLES = ['SLC Operator', 'SEA Administrator', 'LEA Administrator']
 
   def index
     respond_to do |format|
@@ -34,18 +10,9 @@ class HomeController < ApplicationController
   end
 
   def has_users_access?
-    if APP_CONFIG['is_sandbox']
-      allowed_roles = SANDBOX_ALLOWED_ROLES
-    else
-      allowed_roles = PRODUCTION_ALLOWED_ROLES
-    end
+    allowed_roles = APP_CONFIG['is_sandbox'] ? SANDBOX_ALLOWED_ROLES : PRODUCTION_ALLOWED_ROLES
     overlap_roles = allowed_roles & session[:roles]
-    if overlap_roles.length>0
-      true
-    else
-      false
-    end
+    !overlap_roles.empty?
   end
-
 
 end
