e2b52fbee97bb3e8bc63f91138a2f39ed46cdc85
Bill Siggelkow <bill.siggelkow@inbloom.org>
2014-04-25 13:26:43 -0400

    Simplify coding logic for in_progress?

--- sli/admin-tools/admin-rails/app/models/app.rb
+++ sli/admin-tools/admin-rails/app/models/app.rb
@@ -1,22 +1,3 @@
-=begin
-
-Copyright 2012-2013 inBloom, Inc. and its affiliates.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-=end
-
-
 class App < SessionResource
   self.format = ActiveResource::Formats::JsonFormat
   validates_presence_of [:description, :name, :vendor], message: 'must not be blank'
@@ -30,12 +11,7 @@ class App < SessionResource
   end
 
   def in_progress?
-    if self.allowed_for_all_edorgs
-      return false
-    end
-    progress = true
-    self.authorized_ed_orgs.each { |ed_org| progress = false if ed_org.to_i == 0 }
-    progress
+    allowed_for_all_edorgs ? false : authorized_ed_orgs.blank?
   end
 
   schema do
